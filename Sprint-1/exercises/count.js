0ba1cd846919eae6320e286e05caad7d89a46a83
chaitra <chaitra.a.shettys@gmail.com>
2024-10-22 14:42:28 +0100

    Clarified Statement and assignment operator (=) in Line 3

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Line 3 is a Statement where I am reassigning the value of the count variable, = is an 
+// assignment operator where right hand side value i.e, 1(0+1) is assigned to a variable count.
+
