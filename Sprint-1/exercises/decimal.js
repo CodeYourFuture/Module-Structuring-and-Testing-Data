554cc2a923e8c34c5b91a88b62547c4b5247715d
SueMsOl <sogandmsol17@gmail.com>
2024-10-25 19:27:24 +0100

    interpreted syntax / operators / functions inside interpret directory files in Sprint 1 directory.

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -1,5 +1,5 @@
 const num = 56.5678;
-
+console.log(num);
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
@@ -7,3 +7,9 @@ const num = 56.5678;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
+const wholeNumberPart = Math.floor(num);
+console.log("wHole number:"+ wholeNumberPart);
+const decimalPart = num - wholeNumberPart;
+console.log("decimal part:"+ decimalPart);
+const roundedNum = Math.round(num);
+console.log("rounded number:"+ roundedNum);
--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -2,8 +2,12 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+/*due to rules of precedence, parenthesis executes first. Inside parenthesis, subtraction executes first, 
+then addition executes.*/
+console.log(num);
+/*num keeps changing because Math.random() function outputs a different random value between 0 and 1 every time it runs. */
 
-// In this exercise, you will need to work out what num represents?
+// In this exercise, you will need to work out what num represents? answer: a numerical value 
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
