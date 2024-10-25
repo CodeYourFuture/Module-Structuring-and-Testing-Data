f99f9ca346857514b01b654cc0133ef4acd83ea3
SueMsOl <sogandmsol17@gmail.com>
2024-10-25 01:17:44 +0100

    identified and resolved the bugs inside files located in errors directory.

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
-
-const age = 33;
+/*when const is used to create a variable, it can not be reassigned. So for this practice, i replace
+with let to fix this error.*/ 
+let age = 33;
 age = age + 1;
+console.log(age);
