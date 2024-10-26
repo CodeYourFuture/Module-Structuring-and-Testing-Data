389080ae28c297ed7c6434ec385c9c4593a426de
chaitra <chaitra.a.shettys@gmail.com>
2024-10-26 05:30:14 +0100

    Fix Reference error and comment the reason

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,13 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// it will through reference error, 
+// because we are trying to access cityOfBirth variable before initialization in Line 7
+
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton"; 
+
+// Correct way is below
+ 
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
