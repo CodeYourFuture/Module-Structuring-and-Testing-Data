468cbe668cea99642e819301531e76fd443c754a
chaitra <chaitra.a.shettys@gmail.com>
2024-10-26 05:23:54 +0100

    Replace const by let varibale

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,5 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// const age = 33;
+let age = 33; // make the varibale as let because const variable can't be redeclared and re-assigned
 age = age + 1;
