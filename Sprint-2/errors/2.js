afd80cf8ed8a88ab8f72a2e81c1960da059e34d4
Alaa <aliqassabuk@gmail.com>
2024-10-27 16:59:17 +0000

    Errors soloutios

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,16 @@
-
 // Predict and explain first...
 
 // this function should square any number but instead we're going to get an error
+// The parameter should be a variable name, not a specific value like 3.
+// The error will be SyntaxError: Unexpected number.
 
+/*
 function square(3) {
     return num * num;
 }
+*/
 
-
+// correct
+function square(num) {
+  return num * num;
+}
