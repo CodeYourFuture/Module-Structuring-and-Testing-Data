5cd9c4a5baa8f3f7729b3beb020ab32eb883052b
Alaa <aliqassabuk@gmail.com>
2024-10-27 16:30:05 +0000

    Dedug soloutions

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -5,3 +5,5 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// The function multiply(10, 32) logs 320 but returns undefined.
+// The template literal prints "undefined" because the multiply function does not return a value.
--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,5 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//  The return statement immediately exits the function before a + b is evaluated.
+//  The function does not return the sum of a and b but returns undefined.
