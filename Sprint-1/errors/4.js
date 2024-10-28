4897e39898d7757fa208a046b8028a67764cc8c9
Gai <146184211+gai93003@users.noreply.github.com>
2024-10-28 19:35:31 +0000

    Variable names can't start with numbers or contain reserved names.

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const HourClockTime = "20:53";
+const hourClockTime = "08:53";
+
+// This is a wrong declaration syntax, variables can't start with numbers, special characters or contain reserved words.
