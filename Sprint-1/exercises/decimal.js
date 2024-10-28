058e7923d2929e6ba358cab3861c913f40d592f4
Gai <146184211+gai93003@users.noreply.github.com>
2024-10-28 20:10:25 +0000

    I have created the three variables, wholeNumberPart, decimalPart and roundedNum

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -1,5 +1,9 @@
 const num = 56.5678;
 
+const wholeNumberPart = Math.floor(num);
+const decimalPart = (num % 1).toFixed(4);
+const roundedNum = Math.round(num);
+
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
