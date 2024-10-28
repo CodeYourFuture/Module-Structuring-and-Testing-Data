093c9c991910a5460b5b450827189d2abba655bf
azaralampanah2 <azaralampanah2@gmail.com>
2024-10-28 07:10:09 +0000

    complete sprint1/exercises/count.js

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,8 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//"=" operator in line 3 assigns expressions from its right to its left. the initial value of count is 0 and in line 3, it adds up with 1,
+// so the new value of count is 1
+console.log(`the value of count is ${count}`);
+
