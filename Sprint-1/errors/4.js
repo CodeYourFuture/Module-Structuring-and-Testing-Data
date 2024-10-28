4a3c8639fe3bc4914ef879a5ed62b356c70aad7c
azaralampanah2 <azaralampanah2@gmail.com>
2024-10-28 09:12:04 +0000

    complete sprint1/errors/4.js

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,7 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+
+//variables must start with a letter or an underscore(_),so we can use:
+//_12HourClockTime
+//_24hourClockTime
