732e79fe9a06ccc44e93127f69e3486f3260bc06
Amir200524 <amiraminia@yahoo.com>
2024-10-29 22:02:23 +0000

    Number-math

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -7,3 +7,13 @@ const num = 56.5678;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
+
+const num = 56.5678;
+const wholeNumberPart = Math.floor(num); 
+const decimalPart = num - wholeNumberPart; 
+const roundedNum = Math.round(num); 
+// Log the results
+console.log("Whole number part:", wholeNumberPart); // 56
+console.log("Decimal part:", decimalPart); // 0.5678
+console.log("Rounded number:", roundedNum); // 57
+
