d2a4a2567bbe6d6e71717250651e4edf1a0b41e4
Amir200524 <amiraminia@yahoo.com>
2024-10-29 21:55:50 +0000

    (=)pattrern

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//The = operator in this context is not performing equality checking; instead, it is assigning a new value to the variable count.
+//After executing this line, the variable count now holds the value 1.
+//A (=) common pattern used to update variable values in programming.
