ab0c6c70f4e0b6105be165e43b7d4a51021bf9e4
Amir200524 <amiraminia@yahoo.com>
2024-10-29 22:14:35 +0000

    random num

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -7,3 +7,24 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//A.Math.random():
+
+//This function generates a random floating-point number between 0 (inclusive) and 1 (exclusive).
+//This means the value could be something like 0.234 or 0.999, but it will never be 1.
+//B.Math.random() * (maximum - minimum + 1):
+
+//Math.random() is multiplied by (maximum - minimum + 1).
+//In this case, (maximum - minimum + 1) is 100 - 1 + 1, which equals 100.
+//So, Math.random() * 100 will produce a random number between 0 (inclusive) and 100 (exclusive).
+//Math.floor(...):
+
+//C.Math.floor rounds down the resulting floating-point number to the nearest whole number (integer).
+//This means the result of Math.floor(Math.random() * (maximum - minimum + 1)) will be an integer between 0 and 99 (inclusive).
+//D.+ minimum:
+
+//Adding minimum shifts the range from [0, 99] to [1, 100].
+//This ensures the final result, num, is between 1 and 100 (inclusive).
+
+//RESULT
+console.log(num);
