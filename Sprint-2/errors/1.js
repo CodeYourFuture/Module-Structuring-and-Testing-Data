40f3eb9531525e955b5499a2b65a9236cf0a4a9d
azaralampanah2 <azaralampanah2@gmail.com>
2024-10-30 07:26:21 +0000

    complete sprint2/error/1.js

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,17 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  //const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+//console.log(percentage);
+console.log(`${convertToPercentage(0.5)}`);
+//In line 6,syntax error: Identifier 'str' has already been declared: we can't use the same variable name both as a function's argument
+//and as a variable inside the function. I'm not going the variable name in this project as it seems line 7 is a useless line
+//so I just put // to ignore this line
+
+//In line 13, we can't use that syntax, as percentage is a variable inside the convertToPercentage() function and can't be
+//used outside of that function, so I've used line 14 instead
