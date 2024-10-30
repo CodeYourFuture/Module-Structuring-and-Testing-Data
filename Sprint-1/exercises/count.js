43c0f21021c6b5c8f4b79fdbbc296812f1d7d249
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-10-30 16:24:43 +0000

    completing Sprint1/exercises/count.js

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//Answer
+//the variable count will be increase by 1
+
