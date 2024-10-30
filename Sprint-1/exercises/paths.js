6bbe45355eeaaee7de9c455e3aab50fca4377f70
Sajad <sajadsoorni@gmail.com>
2024-10-30 16:05:45 +0000

    Add solutions for extracting file path and name components in path.js
    - Extracted base, directory, and extension parts from filePath variable
    - Extracted firstName, surname, and gender from fullName string
    - Console log outputs added for verification of each component
    
    - Sprint1/exercises/path.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -12,7 +12,28 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The base part of ${filePath} is : ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+//ANswer :
+
+const dir = filePath.slice(0, lastSlashIndex);
+console.log(`The directory part of ${filePath} is : ${dir}`);
+
+const ext = base.slice(base.lastIndexOf(".") + 1);
+console.log(`The extension part of ${filePath} is : ${ext}`);
+
+// My Exercises
+
+const fullName = "Sajad.Soornisofla/Male";
+const lastDot = fullName.lastIndexOf(".");
+const lastSlash = fullName.lastIndexOf("/");
+const firstName = fullName.slice(0, lastDot);
+const gender = fullName.slice(lastSlash + 1);
+const sureName = fullName.slice(lastDot + 1, lastSlash);
+
+console.log(firstName);
+console.log(sureName);
+console.log(gender);
