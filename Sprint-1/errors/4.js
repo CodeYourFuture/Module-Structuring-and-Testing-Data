90d3c11af3666d7bc386b838bc7f57bf27bfb217
rawrnoodles <matthew.lt.law@hotmail.com>
2024-10-30 20:12:15 +0000

    4.js - fixed by adding dollar sign in front of variable.

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const $12HourClockTime = "20:53";
+const $24hourClockTime = "08:53";
+
+// variable names cannot start with numbers, they can only start with a letter, underscore or dollar sign.
+// fixed by adding dollar sign before variable.
