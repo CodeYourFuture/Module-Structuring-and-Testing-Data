0fd575d86a375a3b7a564165c659496a54fea903
Karla G. <k.grajalesluna@gmail.com>
2024-10-30 00:41:33 +0000

    done 1.js

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,13 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// to run this file we need to go in the root and once we are in the file  run, node 1.js   eg =>  /Sprint-1/errors   node 1.js
+
+cost age = 33; // this line is not working because const does not allow you change the data so we can change to let.
 age = age + 1;
+console.log(age);
+
+
+let age = 33;
+age = age + 1;
+console.log(age);
+
