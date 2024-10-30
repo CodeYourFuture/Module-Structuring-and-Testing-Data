2d4c5e72b07f9672ce3861ecab433002e603cf89
azaralampanah2 <azaralampanah2@gmail.com>
2024-10-30 06:46:40 +0000

    complete sprint2/debug/0.js

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
 
 function multiply(a, b) {
+  let multiple1 = a * b;
   console.log(a * b);
-}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+  return multiple1;
+}
+let multiple = multiply(10, 32);
+console.log(`The result of multiplying 10 and 32 is ${multiple}`);
