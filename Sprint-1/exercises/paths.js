b0e8ecfc13a94184c3c6cf73c82cdeb842cc0de2
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-10-30 17:25:48 +0000

    Completing Sprint1/exercises/paths.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -16,3 +16,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+//Answer
+const dir = filePath.slice(0, lastSlashIndex);
+console.log(`${filePath} is ${dir}`);
+
+const ext = base.slice(lastDotIndex);
+console.log(`${filePath} is ${ext}`);
