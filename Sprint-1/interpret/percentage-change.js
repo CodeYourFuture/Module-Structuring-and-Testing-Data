0917b0fe5ca3bcb974e2489f3f1de47048aeaec9
Amir200524 <amiraminia@yahoo.com>
2024-10-30 09:09:55 +0000

    percentege-change

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -12,11 +12,20 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-
+5 lines 
+Number(carPrice.replaceAll(",", "")); (Line 3)
+carPrice.replaceAll(",", "") (within Line 3)
+Number(priceAfterOneYear.replaceAll(",", "")); (Line 4)
+priceAfterOneYear.replaceAll(",", "") (within Line 4)
+console.log(...) (Line 8)
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+Line 4 comma is missing
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // c) Identify all the lines that are variable reassignment statements
-
+Lines 3 & 4
 // d) Identify all the lines that are variable declarations
-
+Lines 1,2,6 & 7
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+replaceAll(",", "") removes all commas from the string, making it possible for Number(...) to convert the string into a valid numerical value that JavaScript can work with in calculations.
+For example: 8,432 to 8432
