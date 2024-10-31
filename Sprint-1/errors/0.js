b9595e1bb2d1dcaa01d93495907046da5e4ed54a
Karla G. <k.grajalesluna@gmail.com>
2024-10-31 23:32:31 +0000

    done errors  2.js

--- Sprint-1/errors/0.js
+++ Sprint-1/errors/0.js
@@ -1,2 +1,5 @@
 // This is just an instruction for the first activity - but it is just for human consumption
 // We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// To make a multi-line comment in JavaScript, you can start with /* and end with */ . Any text
+// in between will be considered a comment and will not be executed as code by the computer.
--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// we just need to move the console.log() to avoid render and expression which was calling the data declared in the variable 
+//ReferenceError: Cannot access 'cityOfBirth' before initialization
