1f6cc32199ebd15afae9ec293667805aa6edf6b7
donarbl <donarablanc@gmail.com>
2024-10-31 18:23:52 +0000

    a SyntaxError in declaring a variable and in the value of 12 versus 20.53

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,2 @@
-const 12HourClockTime = "20:53";
+const 24HourClockTime = "20:53"; // changed to 24HourClockTime instead of 12, as the variable and the value do not match 12/24
 const 24hourClockTime = "08:53";
