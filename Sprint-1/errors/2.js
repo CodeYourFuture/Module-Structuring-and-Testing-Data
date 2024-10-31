959792bf54f2e7eef420cb5eb4ee69b9e325f4f1
donarbl <donarablanc@gmail.com>
2024-10-31 18:00:04 +0000

    corrected referenceError associated to CityOfBirth that was not defined

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,9 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-function defineCityofBirth() {
-    const CityofBirth = "Bolton"; // declaring by let variable and defining CityofBirth ="Bolton"
+function defineCityOfBirth() {
+    const CityOfBirth = "Bolton"; // declaring by let variable and defining CityofBirth ="Bolton"
     console.log(`I was born in ${cityOfBirth}`);
     return cityOfBirth = "Bolton"; // return can be used inside the function
 }
 
-
+defineCityOfBirth();
