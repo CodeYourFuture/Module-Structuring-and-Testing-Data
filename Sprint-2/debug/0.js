1fdd624919f0e6aa285cb5f0eb970eb3975447c8
Armin <nouriarmin24@gmail.com>
2024-10-31 14:54:38 +0000

    i did debug

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
 
-function multiply(a, b) {
-  console.log(a * b);
+function multiply(a, b) {// there is methode for multi of two numbers that it takes when they call it and it will shows
+  // console.log(a * b);
+  return a*b
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);///bug there is because you can not write console.log inside of console log
+const result = multiply(10, 32); // Call the function and store the result in a variable
+console.log(`The result of multiplying 10 and 32 is ${result}`); //
--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  // return;
+  // a + b;
+  //in this return suposu to be something return.
+  return a+b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
