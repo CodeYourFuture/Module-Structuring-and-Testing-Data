ca90c22c5f03c4d5c5f243984cc70f727d28cb64
Gai <146184211+gai93003@users.noreply.github.com>
2024-10-31 22:34:30 +0000

    The variable names were not descriptive enough.

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,4 +1,6 @@
-const HourClockTime = "20:53";
-const hourClockTime = "08:53";
+const twenty4HourClockTime = "20:53";
+const twelveHourClockTime = "08:53";
 
 // This is a wrong declaration syntax, variables can't start with numbers, special characters or contain reserved words.
+
+//I neglect that fact the two variables have the same names since Javascript variable names are not 
