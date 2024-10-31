390776bee7f683bd8730b3382ec8c5f693b47f92
samirahekmati <samirahekmati@gmail.com>
2024-10-31 14:45:54 +0000

    Done: Sprint-1/errors

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+let cardNumber = 4533787178994213;
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,11 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//consider : TypeError: cardNumber.slice is not a function
+//explaining why the code does not work:
+//it only works if you first convert the number into string:
+
+cardNumber = cardNumber.toString()
+const last4Digits = cardNumber.slice(-4);
+console.log("last 4 digits of the card: ", last4Digits)
--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,11 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+
+//The issue here is that variable names in JavaScript cannot start with a number.
+
+
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+//Rename the variables so they don’t start with numbers. You could use letters at the beginning instead, like this:
+
+const hour12ClockTime = "20:53";
+const hour24ClockTime = "08:53";
