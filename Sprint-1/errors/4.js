cf198e45e887a1b1aaf65df4b2b8467b6fbc14ac
Amir200524 <amiraminia@yahoo.com>
2024-10-31 18:45:50 +0000

    commented out

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,5 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+
 //Variable names cannot begin with a number
 //Variable names can contain letters (both uppercase and lowercase), digits (0-9), underscores (_), and dollar signs ($), but they cannot start with a digit.
 
