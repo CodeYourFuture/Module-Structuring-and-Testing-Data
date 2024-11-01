af0062bfe5cc71090f9cd18359bd476d3f5b46b8
nadika <nadikazavodovska@gmail.com>
2024-11-01 16:15:52 +0000

    Declare variables and assign expressions which evaluate the values of dir, ext variables

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -14,5 +14,11 @@ const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
+const dir = filePath.slice(0, lastSlashIndex);
+console.log(`The dir part of ${filePath} is ${dir}`);
+const lastDotIndex = filePath.lastIndexOf(".")
+const ext =  filePath.slice(lastDotIndex + 1);
+console.log(`The ext part of ${filePath} is ${ext}`);
+
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
