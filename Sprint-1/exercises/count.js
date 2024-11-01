9facd61317e893f167957e4ddb0bd893ae84c6c4
Karla G. <k.grajalesluna@gmail.com>
2024-11-01 00:48:14 +0000

    done exercises count.js

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -1,6 +1,10 @@
+// Line 1 is a variable declaration, creating the count variable with an initial value of 0
+// Describe what line 3 is doing, in particular focus on what = is doing
+
 let count = 0;
 
 count = count + 1;
+console.log(count);
 
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// line 3 is taking the first value and reassings this new value to count its value + the number 1
+
