a16785e69d2ff21bc5ef7c4b6a900fed7ca31555
Gai <146184211+gai93003@users.noreply.github.com>
2024-11-01 23:15:49 +0000

    I created a new variable to store the strings created from the cardNumber

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,6 +1,6 @@
 const cardNumber = 4533787178994213;
-cardNumber = cardNumber.toString();
-const last4Digits = cardNumber.slice(-4);
+const cardNumberStrings = cardNumber.toString();
+const last4Digits = cardNumberStrings.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -13,3 +13,5 @@ const last4Digits = cardNumber.slice(-4);
 // I believe the the code is correct. However, the first variable is considered as a string. If operation were to be done on it, the output should first be converted into an integer.
 
 //I used toString method on the cardNumber to turn it into strings and then use slice to retrieve the last 4 digits.
+
+// I converted the cardNumber integers into a string and assigned them to a variable called cardNumberStrings, this way, I can continue using the 'const' declaration instead of using the 'let'
