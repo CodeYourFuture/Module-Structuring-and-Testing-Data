7a5a0298b744190f1b5cad58a97aea4699015b99
SueMsOl <sogandmsol17@gmail.com>
2024-11-01 21:24:32 +0000

    fixed error in 1.js file in errors dir

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -3,11 +3,21 @@
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+/* function convertToPercentage(decimalNumber) {
+   decimalNumber = 0.5; //remove this!
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(decimalNumber); //it must become console.log(convertToPercentage(0.25)); */
+//correct version is written down:
+
+function convertToPercentage(decimalNumber) {
+
+ const percentage = `${decimalNumber * 100}%`;
+
+ return percentage;
+}
+
+console.log(convertToPercentage(0.8));
