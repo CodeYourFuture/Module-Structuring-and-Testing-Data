bcff93955a1eae54b4dbc91d592bd54eb5cf416b
Karla G. <k.grajalesluna@gmail.com>
2024-11-01 20:32:45 +0000

    done exercises decimal.js

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -3,7 +3,26 @@ const num = 56.5678;
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
+
+    const wholeNumberPart = (Math.floor(num));
+
+
 // Create a variable called decimalPart and assign to it an expression that evaluates to 0.5678 ( the decimal part of num )
+
+// parseFloat() picks the longest substring starting from the beginning that generates a valid 
+// number literal. If it encounters an invalid character, it returns the number represented up 
+// to that point, ignoring the invalid character and all characters following it.
+// If the argument's first character can't start a legal number literal per the syntax above, 
+// parseFloat returns NaN.
+
+    const decimalPart = parseFloat((num - wholeNumberPart).toFixed(4));
+
+
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
+
+    const roundedNum = (Math.round(num));
+
 // Log your variables to the console to check your answers
+console.log(wholeNumberPart, decimalPart, roundedNum);
+
