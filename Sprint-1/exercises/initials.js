98c65269b5b7cbe49609ca64263455f43b84a184
nadika <nadikazavodovska@gmail.com>
2024-11-01 15:44:34 +0000

    Declare variable, assign expression with toUpperCase() method to evaluate value and assign back value 'CKJ'

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -2,5 +2,7 @@ let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
 
+let initials = firstName[0].toUpperCase() + middleName[0].toUpperCase() + lastName[0].toUpperCase();
+console.log(initials);
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
