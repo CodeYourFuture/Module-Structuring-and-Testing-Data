1f01abe0d8e9a054512c962a45e2fba984def26b
Karla G. <k.grajalesluna@gmail.com>
2024-11-02 00:05:51 +0000

    done exercises paths.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -9,10 +9,28 @@
 
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
+/* ================  lastIndexOf() ===================
+-we use this to obtain the last position of / where is located the File.txt
+-const base = filePath.slice(lastSlashIndex + 1); with + 1 we start to subtract after /
+Index:       0    1    2  ...    37   38       39      40      41      42
+filePath:    /   U    s   ...    /    f        i       l       e       .txt
+-
+*/
+
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
+console.log(lastSlashIndex); //   44
 const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
+console.log(base); //  file.txt
+// console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-// Create a variable to store the ext part of the variable
+const lastDotIndex = filePath.lastIndexOf(".");
+console.log(lastDotIndex); //   index position 49
+const extractDir = filePath.slice(0, lastDotIndex);
+console.log(`Directory and filename (without extension) ${extractDir}`);
+
+
+// Create a variable to store the ext part of the variable"
+const extensionName = filePath.slice(lastDotIndex + 1);
+console.log(`extension: ${extensionName}`);   //  txt
