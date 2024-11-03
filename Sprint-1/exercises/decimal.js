27b859ce2a0b9e548faf19de0cbfd7bef08dd7c3
Luke Manyamazi <isirluke@gmail.com>
2024-11-03 17:57:09 +0200

    added a function to return a whole number

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -7,3 +7,7 @@ const num = 56.5678;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
+// Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
+const wholeNumberPart = Math.floor(num);
+
+console.log(wholeNumberPart);
