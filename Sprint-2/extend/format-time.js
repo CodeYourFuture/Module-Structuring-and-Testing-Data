c964bdb55608d1697054e4f700281161f74aec07
mohammed22471 <mohammed22471@outlook.com>
2024-11-03 21:56:12 +0000

    fixed bugs

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -2,23 +2,31 @@
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
 function formatAs12HourClock(time) {
-  const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+  time = time.split(':');
+  let hour = parseInt(time[0], 10);
+  let minutes = time[1];
+  
+  if (hour >= 0 && hour < 24) {
+    let period = hour >= 12 ? 'pm' : 'am';
+    hour = hour % 12 || 12; // Converts "0" (midnight) to "12" and formats hours correctly.
+    return `${hour}:${minutes}${period}`;
+  } else {
+    return `${time[0]}:${time[1]} is not a valid time`;
   }
-  return `${time} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+const currentOutput = formatAs12HourClock("00:45");
+const targetOutput = currentOutput
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+const currentOutput2 = formatAs12HourClock("10:00");
+const targetOutput2 = currentOutput2
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+console.log(targetOutput)
+console.log(targetOutput2)
