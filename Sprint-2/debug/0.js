f9c73aecf5c4448f1752d7cce327e309054c56dc
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-03 20:45:26 +0000

    changed console.log to return

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// the multiply function only logs the value of a * b but doesn't return the value, therefore the last console log will print "The result of multiplying 10 and 32 is undefined"
+// changed console.log(a * b); to return a * b;
