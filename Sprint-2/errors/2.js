aacb2b3d73494ad0cd266868060c140fb68e2983
mohammed22471 <mohammed22471@outlook.com>
2024-11-03 14:33:58 +0000

    Error Solution

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,11 @@
 
 // Predict and explain first...
+// does nothing
 
 // this function should square any number but instead we're going to get an error
-
-function square(3) {
+// the function parameter expects a valid variable not a number
+function square(num) {
     return num * num;
 }
 
-
+console.log(square(3));
