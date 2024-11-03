1e22cde698793b069c868ebf97a8d97d815d64a9
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-03 23:27:56 +0000

    two variales are created to one to store the dir and the other one to store the .txt file extention

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -16,3 +16,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+const dir=filePath.slice(0, lastSlashIndex).slice(1);
+const ext = base.slice(base.lastIndexOf(".")); 
+
