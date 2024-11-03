9703ceaca5168c3b1171282dc704dd4901779f5d
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-03 19:14:44 +0000

    2.js - changed parameter 3 to num

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,13 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
+console.log(square(3));
 
+// 3 is a literal number and therefore cannot be a parameter, parameters are placeholder values and can be substituted but literal numbers are fixed values and cannot be a parameter as a result.
+// variable num is not defined.
+// SyntaxError: Unexpected number
+// Variable num error won't show up as it is a reference error.
