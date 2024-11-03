37a5714656b76e38f769ff4c718e7c0e3b291bbd
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-03 18:43:21 +0000

    0.js - fixed by removing let

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -4,6 +4,10 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+// the variable string is already declared within the parameter of the function capitalise.
+// SyntaxError: Identifier 'str' has already been declared tells us that variable str is already declared.
+// fixed by removing let.
