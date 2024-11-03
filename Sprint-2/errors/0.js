fc2f304e976d1778a858c12f18ee839ff2851969
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-11-03 21:48:00 +0000

    Sprint-2
    errors part done

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -1,9 +1,15 @@
 // Predict and explain first...
 
 // call the function capitalise with a string input
-// interpret the error message and figure out why an error is occurring
+// interpret the error message and figure out why an error is occurring.
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  console.log(str)
   return str;
 }
+capitalise("saqib")
+
+// Identifier 'str' has already been declared, error in terminal because
+// The code will throw a SyntaxError due to the re-declaration of the variable str inside the capitalise function.
+// remove let variable inside the function
