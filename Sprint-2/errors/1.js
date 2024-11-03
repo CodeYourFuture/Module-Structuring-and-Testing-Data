241c853c88eeee0eacdc662dcff19a6ff217a8f1
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-03 18:56:44 +0000

    1.js - removed const decimalNumber and added function convertToPercentage(0.5) to console

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,15 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
+
+// decimalNumber is already defined as a variable in the parameter. You can't declare it again.
+// console.log will not output decimalNumber as the variable decimal number only exists within the function convertToPercentage. decimalNumber is only a parameter inside of the function and cannot be accessed globally.
+// SyntaxError: Identifier 'decimalNumber' has already been declared.
+// decimalNumber has already been declared as a variable in the parameter.
+// removed const decimalNumber and changed console.log(decimalNumber); to console.log(convertToPercentage(0.5));
