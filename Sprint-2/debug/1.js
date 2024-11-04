b13844ae11ac40751fb480a4d05cb6dafe03f42e
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-04 20:28:06 +0000

    1.js - fixed by adding a + b; to return.

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,11 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// the return statement doesn't return anything and will print The sum of 10 and 32 is undefined instead
+// return exits the function and therefore a + b; is ignored.
+// fixed 
