a67c88618510149983628919598c820c23d7cabc
donarbl <donarablanc@gmail.com>
2024-11-04 16:09:11 +0000

    created variables for dir and ext part of the exercise

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -4,5 +4,5 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
-const initials = firstName[0]+middleName[0]+lastName[0];// declared an expression called initials with the first letters of initials
+let initials = firstName[0]+middleName[0]+lastName[0];// declared an expression called initials with the first letters of initials, changed from const to let
 console.log (initials);
