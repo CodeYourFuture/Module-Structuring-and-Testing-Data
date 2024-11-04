a085d02f6b90a0403f434acbbb7e771198aea80f
donarbl <donarablanc@gmail.com>
2024-11-04 11:48:03 +0000

    explaining incrementation by 1

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -1,8 +1,7 @@
 let count = 0;
 
-// count = count + 1/;
+// count = count + 1/; the line 3 is showing the incrementation by the count of 1. It updates the value of the count by adding 1 to its declared value
 count++;
-
+console.log(count);
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-console.log(count);
