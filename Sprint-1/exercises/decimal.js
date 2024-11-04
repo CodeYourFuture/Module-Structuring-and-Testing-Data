187fd49a128d38a2d75160b2db0e802abd10ec6a
donarbl <donarablanc@gmail.com>
2024-11-04 12:25:21 +0000

    checked returns values in console

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -9,10 +9,12 @@ const wholeNumberPart = Math.floor(num);// Math.floor ()- returns the largest in
 const decimalPart = (num - wholeNumberPart).toFixed(4);// First, I need to substract the WholeNumberPart from the num, to get the decimals => num - WholeNumberPart
 // the remainder is now 0.5677799999, 
 //however the remainder doesn't show 0.5678, thus, I found on substack flow a function to use .toFixed(), in the parantheses i will put the number 4 to return 4 digits
-console.log (decimalPart);
+
 
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 const roundedNum = Math.round(num);// Math.round() Returns the value of the input rounded to the nearest integer.
 // Log your variables to the console to check your answers
 
-console.log ()
+console.log (wholeNumberPart);
+console.log (decimalPart);
+console.log (roundedNum);
