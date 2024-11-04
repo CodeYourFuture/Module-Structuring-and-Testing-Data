87cdd2464c91294da55afc5b6ef1069e040f5be7
SueMsOl <sogandmsol17@gmail.com>
2024-11-04 01:35:24 +0000

    fixed all bugs on pounds.js

--- Sprint-2/implement/pounds.js
+++ Sprint-2/implement/pounds.js
@@ -4,7 +4,7 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs. 
-function toPounds(penceString) {  //example: 123p
+/* function toPounds(penceString) {  //example: 123p
     let removeP = penceString.slice(0,penceString.length-1); // output: 123
     let getPoundPart = removeP.slice(0,removeP.length-2); //output: 1
     let getPencePart = removeP.slice(-2); //23
@@ -14,9 +14,19 @@ console.log(toPounds("123p"));
 // bug for 23p!. needs padding
 function toPounds2(penceString) {  //example: 23p
     let removeP = penceString.slice(0,penceString.length-1); // output: 23
-    let getPoundPart = removeP.slice(0,removeP.length-2).padStart(1,"0"); //it was output: empty before padding. 
+    let getPoundPart = removeP.slice(0,removeP.length-2).padStart(1,"0"); //it was output: empty before padding. after: 0
     let getPencePart = removeP.slice(-2); //23
     return `${getPoundPart}.${getPencePart} pound`;
 
 }
-console.log(toPounds2("23p"));
+console.log(toPounds("23p")); */
+
+//most updated version with fixed bugs: 
+function toPounds3(penceString) {  //example: 1p
+    let removeP = penceString.slice(0,penceString.length-1); // output: 1
+    let getPoundPart = removeP.slice(0,removeP.length-2).padEnd(1,"0"); //output: 0
+    let getPencePart = removeP.slice(-2).padStart(2,"0"); //it was output:1 before padding. after: 01
+    return `${getPoundPart}.${getPencePart} pound`;
+    
+}
+console.log(toPounds3("1p"));
