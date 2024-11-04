6cd5f666a7183bc9edcfc79c9dd620908d50b24a
sam <cleversam07@gmail.com>
2024-11-04 12:02:37 +0000

    Complete tasks in the interpret folder

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -2,12 +2,12 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ,""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
 
-console.log(`The percentage change is ${percentageChange}`);
+console.log(`The percentage change is ${percentageChange} %`);
 
 // Read the code and then answer the questions below
 
@@ -20,3 +20,29 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+/*
+a) There are 4 function calls in total:
+   1. carPrice.replaceAll(",", "")
+   2. priceAfterOneYear.replaceAll(",", "")
+   3. Number(...) (used in two places)
+
+b) The error occurs on the line where priceAfterOneYear is reassigned.
+   Reason: Missing comma in the replaceAll function call. 
+   Fix: Correct it to priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+
+c) Variable reassignment statements:
+   1. carPrice = Number(carPrice.replaceAll(",", ""));
+   2. priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+
+d) Variable declaration statements:
+   1. let carPrice = "10,000";
+   2. let priceAfterOneYear = "8,543";
+   3. const priceDifference = carPrice - priceAfterOneYear;
+   4. const percentageChange = (priceDifference / carPrice) * 100;
+
+e) The expression Number(carPrice.replaceAll(",", "")):
+   - Removes commas from the carPrice string and converts it to a number.
+   - Purpose: To get a numerical value from the string for calculations.
+*/
--- Sprint-1/interpret/to-pounds.js
+++ Sprint-1/interpret/to-pounds.js
@@ -1,21 +1,31 @@
-const penceString = "399p";
+const penceString = "399p"; // Initialize a string variable with the value "399p", which represents 399 pence.
 
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+// Remove the trailing 'p' from the string
+const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1); 
+// This line uses the substring method to take all characters from the start up to (but not including) the last character 'p'.
+// The result will be "399".
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+// Pad the pence number string to ensure it has at least 3 digits
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); 
+// This line ensures that the string has a minimum length of 3 characters, adding leading zeros if necessary.
+// For example, "5" would become "005", but "399" stays as "399".
 
+// Extract the pounds part from the padded pence string
+const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2); 
+// This line takes all characters except the last two, which represent the pence.
+// For "399", it results in "3", meaning 3 pounds.
+
+ // Extract the pence part from the padded pence string
 const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+  .substring(paddedPenceNumberString.length - 2) // Get the last two characters, which represent the pence.
+  .padEnd(2, "0"); // Ensure that there are always two digits in the pence part, adding a zero if necessary.
+// For "399", this gives "99". If it were "5", it would give "05".
 
-console.log(`£${pounds}.${pence}`);
+// Print the final formatted price in pounds
+console.log(`£${pounds}.${pence}`); 
+// This line combines pounds and pence into a string formatted as "£3.99".
+// It uses template literals to insert the pound and pence values into the string.
+
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
