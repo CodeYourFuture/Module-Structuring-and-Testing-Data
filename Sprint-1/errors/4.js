8811e363bffab0d45ec1097ca67e57ae7e2e6ad3
saraamiiri <amirisara1993@gmail.com>
2024-11-04 10:15:52 +0000

    sprint1/exercise\paths.js

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,3 +1,3 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const hour12HourClockTime = "20:53";
+const hour24hourClockTime = "08:53";
 //its not valid declaration ,variable name  in java script can not start with number 
