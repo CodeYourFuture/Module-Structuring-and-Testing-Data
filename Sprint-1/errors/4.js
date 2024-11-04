03695bb5f989473378f6dfb39785bbb5c20c7986
Simpra11 <mrmabuya11@gmail.com>
2024-11-04 17:45:37 +0200

    Updated my comment about case sensitivity in JavaScript

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -4,4 +4,4 @@ const hourClockTime = "08:53";
 
 /* 1. The first error has to do with naming variables. It is not allowed to have a variable with a name that starts with a digit.
    2. The second error is that a const variable doesn't change and time changes every second, so it's best we declare the hour variable with let.
-   3. The last error is due to the fact that JavaScript is character sensitive. We have to notice how we name our variables. In this case the variable "HourClockTime" differs from the second variable "hourClockTime" and that will cause an error / bug. It is also best practice to use camel casing when naming variables with more than two names and preferably start with a lower case character */
+   3. The last error is due to the fact that JavaScript is case-sensitive. We have to notice how we name our variables. In this case the variable "HourClockTime" differs from the second variable "hourClockTime" and that will cause an error / bug. It is also best practice to use camel casing when naming variables with more than two names and preferably start with a lower case character */
