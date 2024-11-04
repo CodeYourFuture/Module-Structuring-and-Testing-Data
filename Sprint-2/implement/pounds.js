45405caca3e41ef34c065ec43057810cd3e71ef1
SueMsOl <sogandmsol17@gmail.com>
2024-11-04 00:47:29 +0000

    made to-Pounds function code DRYER

--- Sprint-2/implement/pounds.js
+++ Sprint-2/implement/pounds.js
@@ -8,9 +8,6 @@ function toPounds(penceString) {  //example: 123p
     let removeP = penceString.slice(0,penceString.length-1); // output: 123
     let getPoundPart = removeP.slice(0,removeP.length-2); //output: 1
     let getPencePart = removeP.slice(-2); //23
-
-
     return `${getPencePart}.${getPoundPart} pounds`;
 }
-
 console.log(toPounds("123p"));
