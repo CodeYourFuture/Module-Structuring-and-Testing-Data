e4406c585a4ba3a9a572e37c42d43d544502f2ab
Simpra11 <mrmabuya11@gmail.com>
2024-11-04 16:37:40 +0200

    Gave an explanation of what caused the error

--- Sprint-1/errors/0.js
+++ Sprint-1/errors/0.js
@@ -1,2 +1,4 @@
 /* This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem? */
+
+/* The only way to solve this problem is to comment out the entire text, because it's not part of a code but could be a comment about a piece of code. This is so I or other developers can understand what the other developer was trying to achieve by that particular code*/
