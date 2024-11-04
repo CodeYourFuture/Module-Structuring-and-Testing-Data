10f0be11d0896a1f5dc0826f37838f7314a60cd4
sam <cleversam07@gmail.com>
2024-11-04 12:01:26 +0000

    Complete tasks in the errors folder

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+// const age = 33;
+// age = age + 1;
+
+var age =23;
+age =1;
--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+// const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,11 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+const last4Digits = cardNumber.toString().slice(-4);
+
+console.log(last4Digits);
+
+/* Prediction
+The code won’t work because cardNumber is a number, and .slice() only works on strings and arrays.
+To fix this, we need to turn cardNumber into a string first. */
