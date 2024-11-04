1d47aaa3a0b4cee71c02a4678fe9107b69c3e546
Simpra11 <mrmabuya11@gmail.com>
2024-11-04 17:29:19 +0200

    Fixed the error by assigning cardNumber to an array of numbers separated by a comer

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4, -5);
+const cardNumber = [4,5,3,3,7,8,7,1,7,8,9,9,4,2,1,3];
+const last4Digits = cardNumber.slice(-4);
 console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
@@ -8,3 +8,5 @@ console.log(last4Digits);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+/* The error was caused by the fact that .slice is an array method/function. So I had to assign "cardNumber" to an array and separate each number with a comer */
