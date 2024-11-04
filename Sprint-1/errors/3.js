c3b06c201622aaaf56d72c9fb36fdfe212a6e3d2
Simpra11 <mrmabuya11@gmail.com>
2024-11-04 17:12:58 +0200

    Third test

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(0,-4);
+const last4Digits = cardNumber.slice(-4, -5);
 console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
