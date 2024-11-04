3b3a2e98465195d3ba9238ce1d6bed7fa07e25cd
Luke Manyamazi <isirluke@gmail.com>
2024-11-04 04:10:28 +0200

    added a function to get initials from given strings

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -4,5 +4,7 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
-const initialsStr = `${firstName.charAt(0)} ${firstName.charAt(0)}`${firstName.charAt(0)};
+const initialsStr = `${firstName.charAt(0)}${middleName.charAt(
+  0
+)}${lastName.charAt(0)}`;
 console.log(initialsStr);
