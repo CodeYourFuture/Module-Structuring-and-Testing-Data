49185e80ca369158efc78558bff40a63d5ded7be
Simpra11 <mrmabuya11@gmail.com>
2024-11-04 16:56:57 +0200

    Rearranged the code correctly

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,7 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
 
 /* We got a ReferenceError because can not console.log or cannot access "cityOfBirth" before initialization. That's the reason why we got a ReferenceError */
