5504f1402ccd4713dbb20cccbee807a3e69f673e
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-04 14:02:53 +0000

    amended the variable names to camel-case format in sprint1/exercises/path.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -15,8 +15,8 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-const dirpart=filePath.slice(0,lastSlashIndex);
-console.log(`the dir part of ${filePath} is : ${dirpart}`);
+const dirPart = filePath.slice(0, lastSlashIndex);
+console.log(`the dir part of ${filePath} is : ${dirPart}`);
 // Create a variable to store the ext part of the variable
-const lastdotindex=filePath.lastIndexOf(".");
-console.log(`${filePath.slice(lastSlashIndex + 1,lastdotindex)}`)
+const lastDotIndex = filePath.lastIndexOf(".");
+console.log(`${filePath.slice(lastSlashIndex + 1, lastDotIndex)}`);
