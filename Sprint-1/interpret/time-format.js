d66866592755f6796963cd8890fa77176b5d9733
Azharuddin Meyer <azharuddinmeyer92@gmail.com>
2024-11-05 23:12:20 +0200

    completed the intepret exercises

--- Sprint-1/interpret/time-format.js
+++ Sprint-1/interpret/time-format.js
@@ -12,13 +12,20 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+`5`
 
 // b) How many function calls are there?
+`1`
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
+`movieLength % 60 represents the remainingSeconds which is 24 seconds`
+
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+`the totalMinutes expression means the movieLength(8784%60) - remainingSeconds(24) / 60 which equals to 146 total minutes`
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+`I think it represents the total run time of the movie in hours, minutes and remaining seconds - a better variable name could be totalRun`
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+`Yes it will work. The code is designed to work with different values of movieLength because it calculates the number of hours, minutes, and seconds based on the input value`
