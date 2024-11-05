752a796f6e8d404c6bcea26c3a0973e2d963a807
saraamiiri <amirisara1993@gmail.com>
2024-11-05 10:45:07 +0000

    sprint 1/intrept/to punds .js

--- Sprint-1/interpret/to-pounds.js
+++ Sprint-1/interpret/to-pounds.js
@@ -25,3 +25,7 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+Extract the numeric part of the pence string by removing the "p".
+Ensure the number is at least 3 digits long, padding with zeros if necessary.
+Split the number into pounds and pence.
+Format the result as a string in the format £X.XX, with two decimal places for the pence.
