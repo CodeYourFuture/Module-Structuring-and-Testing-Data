29c50473a22276b4cd476b79413e4e452ea35535
ArminNouri <nouriarmin24@gmail.com>
2024-11-05 21:44:59 +0000

    edit explain

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -5,4 +5,4 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 
-// Line 3 is counting one to variable it mean add one to variable also when useing = this it's mean you should use replace a result on variable 
+//Line 3 increments the variable count by 1. The = operator is used to assign the new value (which is the current value of count plus 1) back to count, effectively updating its value. This means that the previous value of count is replaced with the new value after the addition.
