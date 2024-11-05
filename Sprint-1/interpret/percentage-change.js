9cfa855b6ef049cfe38214ff688743846d012786
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-05 12:22:22 +0000

    answer is given for the list of questions

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -3,6 +3,7 @@ let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
 priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ,""));// Will be fixed like this
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +21,20 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+/*
+Answer
+a) There are 4 function calls.
+   Line 4 snd 5
+
+b) The error comes from line 5 because of missing comma between the comman and empthy string replacing argumentt, to fix this we have ato add comma
+like this priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",",""));
+
+c)Two variables are reassigned from string value to numeric value in line 4 and 5.
+
+d) Line 1,2,7, and 8
+
+e)It replaces the comma value to empty or to remove the comma value from the carPrice value 10,000 and change to number that helps to make the 
+calculation. it changes carPrices string value to numeric value 
+*/
