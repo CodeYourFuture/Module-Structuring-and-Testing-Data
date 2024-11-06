c7f47cbc4edc4d34800400ba91e6ad3c44ca30e7
Millena28 <millenakiflom06@gmail.com>
2024-11-06 14:26:15 +0000

    Random .js

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -3,6 +3,9 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+
+console.log(`Num is a random number between ${minimum} and ${maximum}: ${num}`);
+
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
