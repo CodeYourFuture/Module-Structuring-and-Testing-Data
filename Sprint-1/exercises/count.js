15281617ef551b6886923d83c09c14ff103b8283
Millena28 <millenakiflom06@gmail.com>
2024-11-06 13:56:07 +0000

    Fixed count .js

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+
+// Line 3 is performing an assignment, giving a new value to the count variable 
+// The = operator assigns this new value (1) to the count variable.
