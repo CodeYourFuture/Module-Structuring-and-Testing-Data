7abde46da47af4c2a9a34bcf63fbd1f835e1ec07
shreef ahmed <162896374+shreefAhmedM@users.noreply.github.com>
2024-11-06 22:23:27 +0000

    sprin 1 exercises done

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -16,3 +16,6 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+var dir = filePath.slice(0, lastSlashIndex);
+var ext = base.slice(4);
