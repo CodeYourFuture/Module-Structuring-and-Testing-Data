3ce6b53ea19214e31d85da03a94ac9d24520b10b
SueMsOl <sogandmsol17@gmail.com>
2024-11-06 21:24:10 +0000

    updated toPounds3 function in pounds.js

--- Sprint-2/implement/pounds.js
+++ Sprint-2/implement/pounds.js
@@ -24,9 +24,10 @@ console.log(toPounds("23p")); */
 //most updated version with fixed bugs: 
 function toPounds3(penceString) {  //example: 1p
     let removeP = penceString.slice(0,penceString.length-1); // output: 1
-    let getPoundPart = removeP.slice(0,removeP.length-2).padEnd(1,"0"); //output: 0
+    let getPoundPart = removeP.slice(0,removeP.length-2).padStart(1,"0"); //output: 0
     let getPencePart = removeP.slice(-2).padStart(2,"0"); //it was output:1 before padding. after: 01
     return `${getPoundPart}.${getPencePart} pound`;
+
     
 }
 console.log(toPounds3("1p"));
