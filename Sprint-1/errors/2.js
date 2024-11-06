382afaffe5a7543a3bc9eee3b78980ce6891fcbe
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-11-06 19:27:59 +0200

    added my answer and moved the console log line to the bottom

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,5 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+// what's the error? - the console line is above the declaration line
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
