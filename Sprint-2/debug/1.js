fea3e53e5515a96262aabd29306e45174ed1b7c4
sabafarjamfard <farjamfardsaba@yahoo.com>
2024-11-07 17:00:54 +0000

    Fix - Sprint-2

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,14 @@
-// Predict and explain first...
+// Predict ://This is because the sum function includes a return; statement on its own,
+//which prevents the calculation a + b from being executed. As a result, the function doesn’t return a value, so it defaults to undefined.
+
+//explain://The issue is that return; is used alone on line 4, which causes the function to end immediately without executing a + b. In JavaScript, 
+//if a function does not return a value, it defaults to returning undefined. Therefore, when sum(10, 32) is used within the console.log statement, 
+//undefined is displayed instead of the sum.
+
+//Fix://To fix this, place return and the expression a + b on the same line so that the function returns the result of adding a and b:
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -3,7 +3,18 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+//predict://This code defines a function called capitalise that is supposed to take a string input, 
+//capitalize the first letter, and return the modified string with the rest of the letters unchanged.
+
+//explain://In line 6, ${} is used around str[0].toUpperCase(), but ${} only works inside Template Literals 
+//(strings enclosed in backticks). Currently, this usage is incorrect, and JavaScript cannot process it. As a result, the code will likely produce a SyntaxError or ReferenceError
+
+//Fix://To fix this issue, remove ${} and use str[0].toUpperCase() as a standalone expression. 
+//Then, concatenate it with str. slice(1) to get the first character capitalized and the rest of the string unchanged.
+
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let result = str[0].toUpperCase() +str.slice(1);
+  return result;
 }
+console.log(capitalise("hello")) ;
