e9d41d7966a5fd751c681660df3ba712df5481c4
Natalie Patel <natcandy@gmail.com>
2024-11-07 17:51:29 +0200

    Completed Sprint 1 initials.js

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -4,3 +4,9 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+
+var firstInitial = firstName.slice(0,1);
+var secondInitial = middleName.slice(0,1);
+var thirdInitial = lastName.slice(0,1);
+var allInitials = (firstInitial+secondInitial+thirdInitial);
+console.log(allInitials);
