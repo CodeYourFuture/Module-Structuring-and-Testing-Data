2ac16f3ce9a7ed26932467dad2b05702cfa1bdf5
SueMsOl <sogandmsol17@gmail.com>
2024-11-07 22:29:19 +0000

    wrote function in get-card-value file and tested it using JIST framework

--- Sprint-3/implement/example.test.js
+++ Sprint-3/implement/example.test.js
@@ -1,3 +1,4 @@
+//output testing for get-angle-type function:
 function getAngleType(angle) {
     let angleConverted = Number(angle.slice(0,-8));
     if (angleConverted === 90 ){
@@ -19,4 +20,22 @@ function getAngleType(angle) {
 test ('obtuse angle', () => {
     expect(getAngleType("120 degrees")).toBe('Obtuse angle');
 });
-  
+//assertion check for get-card-value function
+function getCardValue(rankSuit) {
+    let rankPart = rankSuit.slice(0,rankSuit.length-1);
+    if (rankPart >= 2 && rankPart <= 9){
+        return rankPart;
+    }
+    else if(["10", "J", "Q", "K"].includes(rankPart)){
+        return 10;
+    }
+    else if(rankPart === "A"){
+        return 11;
+    }
+    else{
+        return "Invalid card rank";
+    }
+}
+test (`expected output is 10`, () => {
+    expect(getCardValue("10♠")).toBe(10);
+});
