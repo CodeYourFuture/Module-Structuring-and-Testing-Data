cc1e1c96128abb48b4695b860ca49d306808a522
Sajad <sajadsoorni@gmail.com>
2024-11-07 22:09:46 +0000

    Errors answers and solutions

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -4,6 +4,13 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+//We get syntax error, because the variable str is being declared twice
+
+// First, as the function parameter ==> function capitalise(str){}
+// Second, as a let variable inside the function, let str.
+
+//To fix this, we remove the let keyword from inside the function!
--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,14 @@
-
 // Predict and explain first...
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
-    return num * num;
+function square(num) {
+  return num * num;
 }
 
+//It will throw a SyntaxError
 
+//In JavScript, parameters shoild be variable names, not literal values!
+
+//To fix this issue, we replace 3 with a valid parameter name like num, then we call the
+//function with 3 as an argument!
