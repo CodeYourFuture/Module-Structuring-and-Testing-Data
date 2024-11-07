03dd6cc596da351bb6a08ee2c2d71f2750abd809
Simpra11 <mrmabuya11@gmail.com>
2024-11-07 21:51:16 +0200

    Edited a comment

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -5,4 +5,4 @@ const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
 
 
-/* We got a ReferenceError because can not console.log or cannot access "cityOfBirth" before initialization. That's the reason why we got a ReferenceError */
+/* We got a ReferenceError because we can not console.log or cannot access "cityOfBirth" before initialization. That's the reason why we got a ReferenceError */
