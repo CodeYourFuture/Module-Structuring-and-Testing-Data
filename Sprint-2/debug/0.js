ac5351d7129084d50cb16b92a1580b7f175d55bd
Amir200524 <amiraminia@yahoo.com>
2024-11-07 20:49:05 +0000

    debug 0

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -5,3 +5,17 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//Line3: A function named multiply is defined. It takes two parameters a and b.
+//Line4: Calling the multiply function
+//Line7: The template literal (${multiply(10, 32)}) tries to include the result of multiply(10, 32) in the string.
+//Output: The result of multiplying 10 and 32 is undefined
+//Fixed code:
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//Explain: Multiply function returns the result instead of just printing
+
+
