80ba22b0d6579f7a49c00b63712cd285460d6c23
Karla G. <k.grajalesluna@gmail.com>
2024-11-07 23:39:49 +0000

    updateing the line in errors

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,7 +1,7 @@
 // const 12HourClockTime = "20:53";
 // const 24hourClockTime = "08:53";
 
-// this is given an error because we can no start an variable with number So we can fix moving the number 
+// this is given an error because we can no start an variable starting naming with number, So we can fix moving the number 
 // in the middle or we can write the number instead declare the integer number
 
 const hour12ClockTime = "20:53";
