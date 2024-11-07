dac5f7f0c753f19cc068fa16a5a9f0b68278dc3e
nadika <nadikazavodovska@gmail.com>
2024-11-07 12:38:58 +0000

    Declare variable globally, change template literal

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -4,6 +4,9 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}` + `${str.slice(1)}`; // I changed the template literal. If "+" is inside the template literal, it’s treated as a regular character, not an operator. To use + as an operator and concatenate strings, put it outside the template literal.”
   return str;
 }
+
+let str = capitalise('hello'); //We need to declare a variable globally to use it out of the function.
+console.log(str);
