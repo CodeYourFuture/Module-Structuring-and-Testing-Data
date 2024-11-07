af3c5395978411ba34d16239ca7df2054ab5b3fc
Simpra11 <mrmabuya11@gmail.com>
2024-11-07 22:05:07 +0200

    Added a comment about how the assignment operator was used to add 1 to the variable count

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+// on line three 1 is added to the count variable by using the assignment operator =
+
+console.log(count);
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
