03f3a4007a47762c57ed9413572ddae4acd21304
samirahekmati <samirahekmati@gmail.com>
2024-11-07 11:21:08 +0000

    Done: Sprint-2/errors

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -2,8 +2,13 @@
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+//
+//Answer: SyntaxError: Identifier 'str' has already been declared
+//change the variable name inside the function body
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
+
+console.log(capitalise("sprint2"))
--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -2,12 +2,14 @@
 
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
+//
+//Answer: SyntaxError: Identifier 'decimalNumber' has already been declared
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  //const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
