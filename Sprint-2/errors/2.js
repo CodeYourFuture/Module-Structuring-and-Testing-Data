a8435c1cac3bf8ce18a192c05947d3e47f5940e4
nadika <nadikazavodovska@gmail.com>
2024-11-07 18:02:41 +0000

    Change argument of function to parameter, declare numSquare variable, assign square() function

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,12 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) { // Changed to accept a parameter 'num' to make it flexible for any input.
     return num * num;
 }
+
+let numSquare = square(3); // Declared the numSquare variable and assigned it the result of calling square() with 3 as the argument.
+console.log(numSquare);
+
 
 
