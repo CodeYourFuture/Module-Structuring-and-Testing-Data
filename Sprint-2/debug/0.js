1ecda0bbef79bd3aaba962ceff2bd0e7c1ef3002
shaden-pr <pratiamalden@gmail.com>
2024-11-08 00:22:47 +0000

    Fixed the debugs

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b; //return instead of logging
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
+//the semicolon will ends the function before a+ b
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
