9fdeb38f36a1f7614efea40de6ef359921d930ef
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2024-11-08 14:06:15 +0000

    correcting errors

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,15 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+// function square(3) {
+//     return num * num;
+// }
+
+// correction 
+// the problem in the above code is it didnt defined the num as a parameter of square.
+
+function square(num) {
     return num * num;
 }
 
-
+console.log(square(3))
