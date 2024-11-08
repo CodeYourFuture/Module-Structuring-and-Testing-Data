3ca4eca4108a73eb478251eef955d04b1aaf7529
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2024-11-08 13:23:33 +0000

    debugging

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,16 @@
 // Predict and explain first...
 
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// // correction
+// the multiply code will not work cause the return is not defined. replace return (a*b) instead of console.log(a*b).
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
