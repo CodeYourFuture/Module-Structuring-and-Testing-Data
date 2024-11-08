518b44da3b256e1b8fd43a74ed7c870c4de093f2
SueMsOl <sogandmsol17@gmail.com>
2024-11-08 19:12:01 +0000

    wrote 6 different functions in password-validator.test.js for each condition (for practice)

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -15,13 +15,61 @@ To be valid, a password must:
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
 
+const { count } = require("console");
 const { hasUncaughtExceptionCaptureCallback } = require("process");
+const { Z_ASCII } = require("zlib");
 
-/* function PassValidChecker() outputs true if all the 6 conditions stated above equal to true. I create a function for each condition first.
-Afterward I use those conditions in my PassValidChecker(). */
+/* function PassValidChecker() outputs true if all the 6 conditions stated above equal to true. For extra practice, I create a function for each condition first.
+Afterward I create PassValidChecker(). */
 
 //first function: it counts the number of characters in the string str.
-let charCount = (str) => str.length;
-test (`expected output `, ()=>{
-    hasUncaughtExceptionCaptureCallback(charCount("sogand")).toBe(6);
-});
+const charCount = (str) => str.length;
+console.log(charCount("sogaand"));
+//test (`expected output `, ()=>{
+ //   expect(charCount("Sog147$0and")).toBe(11);
+// });
+//second function: it counts the number of upper case letters.
+const upperCaseCount = (str) => {
+    let strToArray = str.split("");
+    let upperCase = strToArray.filter(letter => (letter >= 'A' && letter <= 'Z')).length;
+    return upperCase;
+};
+console.log(upperCaseCount("SogandMohAmmadi"));
+//third function: it counts the number of lower case letter.
+const lowerCaseCount = (str) => {
+    let strToArray = str.split("");
+    let lowerCase = strToArray.filter(letter => (letter >= 'a' && letter <= 'z')).length;
+    return lowerCase;
+};
+console.log(lowerCaseCount("moein THE BEST big bRooooooother"));
+//fourth function: it counts the number of numerical data.
+const numCount = (str) => {
+    let count = 0;
+    for (let i = 0; i < str.length; i++){
+        if ( str[i] >= 0 && str[i] <= 9 ) {
+             count++;
+        }
+    }
+    return count;
+}
+console.log(numCount("efhesofhdu12345"));
+//fifth function: it counts the number of symbols.
+const symbolCount = (str) => {
+    let strToArray = str.split("");
+    let symbolInArray = strToArray.filter(letter => (["!", "#", "$", "%", ".", "*", "&"].includes(letter))).length;
+    return symbolInArray;
+}
+console.log(symbolCount("sog!#*"));
+//sixth function: I need to have access to user's previous password recorded in a database. 
+let previousPasswords = ["StarDust3434##"];
+const previousPassCheck = (str,previousPasswords) => {
+    if (previousPasswords.includes(str)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+console.log(previousPassCheck("Star7ust3434##", previousPasswords));
+
+//now it's time to use these functions to write passValidChecker():
