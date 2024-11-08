6b2bce8e128150765358a2ab68e118f155ed3ecc
shaden-pr <pratiamalden@gmail.com>
2024-11-08 01:31:31 +0000

    fixed all the errors

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// with const or let it's not accessible before its declared
+
+
+const cityOfBirth = "Bolton";
 
 console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
