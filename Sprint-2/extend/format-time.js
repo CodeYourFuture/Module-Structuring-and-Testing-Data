9f05cbc7eb6eb3c2765a1a6fedc2833cbf2b8d6b
Luke Manyamazi <isirluke@gmail.com>
2024-11-09 04:42:22 +0200

    provided answers to the given exercises

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -22,3 +22,31 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+const currentOutput3 = formatAs12HourClock("10:00");
+const targetOutput3 = "10:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("21:00");
+const targetOutput4 = "9:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("13:45");
+const targetOutput5 = "1:00 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+);
+
+const currentOutput6 = formatAs12HourClock("19:00");
+const targetOutput6 = "7:00 pm";
+console.assert(
+  currentOutput6 === targetOutput6,
+  `current output: ${currentOutput6}, target output: ${targetOutput6}`
+);
