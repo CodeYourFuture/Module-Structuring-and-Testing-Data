dc7c8f4b6f8fe601a5ff3248cc1b1dd2f6690d3f
Millena28 <millenakiflom06@gmail.com>
2024-11-09 05:05:46 +0000

    Fixed debug/1.js

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,9 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// Return a + b; // Put return expression on one line, otherwise the value will be undefined.
+ 
+function sum(a, b) {
+  return a + b;
+}
+// Fixed the code
