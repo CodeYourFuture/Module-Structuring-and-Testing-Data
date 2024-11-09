0dbd8533a400a24a68c2674b4830eb335136e195
SueMsOl <sogandmsol17@gmail.com>
2024-11-09 00:47:09 +0000

    wrote functions required in 3 files inside revise/implement directory

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,22 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+function isPrime(num){
+    let infiniteNum = [Infinity];
+    count = 0;
+    for (let i = 0; i < Infinity; i++){
+        if (num%infiniteNum[i] === 0){
+            count++
+            if (count === 2){
+                return `${num} is a prime number.`;
+            }
+            else {
+                return `${num} is not a prime number.`;
+            }
+        }
+    }
+}
+//console.log(isPrime(3));
+test ('expected output: 3 is a prime number', ()=>{
+    expect(isPrime(3)).toBe("3 is a prime number.")
+})
