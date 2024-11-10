a9422f39a3d626a526aaf113bf29786b5aac39ae
Amir200524 <amiraminia@yahoo.com>
2024-11-10 19:16:12 +0000

    get-angle

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -1,27 +1,29 @@
-// Implement a function getAngleType
+public class AngleType {
 
-// Acceptance criteria:
+    public static String getAngleType(int angle) {
+        if (angle == 90) {
+            return "Right angle";
+        } else if (angle < 90) {
+            return "Acute angle";
+        } else if (angle > 90 && angle < 180) {
+            return "Obtuse angle";
+        } else if (angle == 180) {
+            return "Straight angle";
+        } else if (angle > 180 && angle < 360) {
+            return "Reflex angle";
+        } else {
+            return "Invalid angle";
+        }
+    }
 
-// Given an angle in degrees,
-// When the function getAngleType is called with this angle,
-// Then it should:
+    public static void main(String[] args) {
+        // Test cases
+        System.out.println(getAngleType(45));    // Output: Acute angle
+        System.out.println(getAngleType(90));    // Output: Right angle
+        System.out.println(getAngleType(120));   // Output: Obtuse angle
+        System.out.println(getAngleType(180));   // Output: Straight angle
+        System.out.println(getAngleType(270));   // Output: Reflex angle
+        System.out.println(getAngleType(360));   // Output: Invalid angle
+    }
+}
 
-// Identify Right Angles:
-// When the angle is exactly 90 degrees,
-// Then the function should return "Right angle"
-
-// Identify Acute Angles:
-// When the angle is less than 90 degrees,
-// Then the function should return "Acute angle"
-
-// Identify Obtuse Angles:
-// When the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle"
-
-// Identify Straight Angles:
-// When the angle is exactly 180 degrees,
-// Then the function should return "Straight angle"
-
-// Identify Reflex Angles:
-// When the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle"
