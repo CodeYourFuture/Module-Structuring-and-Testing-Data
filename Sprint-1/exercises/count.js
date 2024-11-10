a14ab905e2ec669cec13b79da850f28b2cdcfbb2
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-10 15:06:39 +0000

    some clarification added to the given epxression

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,14 @@ count = count + 1; //count increased by one so addion operaiton is perfomed here
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/*  Answer
+
+ The assignment operator(=) is a new value to the variable count.
+ This line is taking the current value of count which is 0,and adding 1 to it, and then reassigning the result back to count.
+
+  steps:-
+  count=count+1;// reading the current value of count
+  count=0+1;// addint 1 to count
+  count=1;// reassigning the result back to count
+*/
