0ed18b9c31ea0bb2bef6a608c9188cfb8a4cb9c6
Azharuddin Meyer <azharuddinmeyer92@gmail.com>
2024-11-10 19:15:30 +0200

    completed errors exercise

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,11 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  // const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
-
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
+
+// The decimalNumber variable is declared twice. 
