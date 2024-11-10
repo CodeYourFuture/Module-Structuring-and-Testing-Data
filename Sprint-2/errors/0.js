571227a1f1999a5e89e02fec73ee929e1aa235c9
Karla G. <k.grajalesluna@gmail.com>
2024-11-10 18:00:19 +0000

    doe 0.js

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -3,7 +3,34 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+  /*  
+  we are creating a variable with (let = nameVariable) but the same time we can not assing
+  the value in the same line.
+  instead we should call directly the (str = valueAssigned) and with this will Worker.
+*/
+
+
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+
+
+/*================== fixed =======================*/
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+console.log(capitalise("alejandra"));
+
+
+/*
+  In this function we are taking a parameter the string "alejadra" this
+  entry in the function and assigned in the str = takin the firs character[0].toUpperCase()
+  and using the function upper... to convert this in capital letter.
+  but we still need the rest of the string and we use the interpolation to concatenate 
+  the rest calling the string from the position 1 with  str.slice(1).
+*/
