2bcca75d296b805cff187ea171e0b86764137b0b
Luke Manyamazi <isirluke@gmail.com>
2024-11-11 20:10:29 +0200

    added exercise 1 answers

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// The code has a syntax error after the return the word, it will give an error when we try to run it.
+// To fix it I will remove the semi-colon and place the a + b in front of the return statement
 
 function sum(a, b) {
   return a + b;
