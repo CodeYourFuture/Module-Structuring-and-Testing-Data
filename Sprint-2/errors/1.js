8b8b83a9e04c9e3c715e101d037b9b35bde220a8
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-11 18:40:39 +0000

    sprint2-errors-1.js-complete

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -11,3 +11,15 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+
+// An error will occur when this program runs due to a variable redeclaration conflict with const for decimalNumber.
+// decimalNumber is first passed as an argument to the function, This redeclaration is not allowed in JavaScript because the variable already exists in the current scope
+// To fix this, we should either remove the const decimalNumber = 0.5; , and pass it as argument like this:
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
