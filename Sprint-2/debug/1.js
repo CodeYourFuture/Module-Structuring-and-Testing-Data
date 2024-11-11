4809a1ceace61fc60fd4be0edec5471e6c1de23a
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-11 18:32:02 +0000

    sprint2-debug-1.js-complete

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,10 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// The line return; ends the function sum early, so anything written after it(like a + b; will not run, this means the function returns undefined
+// To fix this we move a + b after return
+
+function sum(a, b) {
+  return a + b;
+}
