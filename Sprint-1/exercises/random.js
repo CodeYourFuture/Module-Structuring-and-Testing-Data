3e818f3e05fa0aed91a2b48038e3b66119eb8d1c
Simpra11 <mrmabuya11@gmail.com>
2024-11-11 14:25:27 +0200

    Broke down what the variable num represents

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -2,6 +2,7 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+console.log(num); // num represents a random number generated between 1 and 100 (minimum & maximum) : a random number is generated using Math.random() then brought into a whole integer using Math.floor()
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
