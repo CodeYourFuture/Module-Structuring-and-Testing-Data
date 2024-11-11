a3c43a78f054438a3f857f2cf5fdf3d8ebc5b802
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-11 18:38:36 +0000

    sprint2-errors-0.js-complete

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -7,3 +7,15 @@ function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+// This code will throw an error (Identifier 'str' has already been declared) because the let keyword is used to declare a new variable with the same name as an existing parameter
+// In JavaScript, when you pass a parameter to a function, it already acts as a variable within the function's scope. Attempting to redeclare it with let causes an error.
+// To fix this, we can simply assign a new value to str directly without redeclaring it.
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+
+  return str;
+}
+
+console.log(capitalise("hello"));
