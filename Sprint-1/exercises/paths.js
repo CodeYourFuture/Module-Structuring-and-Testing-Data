77f5ffd77f4a48fcfd2b8923e0b7d476e614613b
Simpra11 <mrmabuya11@gmail.com>
2024-11-11 14:09:03 +0200

    I've created a variable to store the dir and ext of a variable

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -14,5 +14,11 @@ const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
+
+
 // Create a variable to store the dir part of the filePath variable
+const filePathDir = filePath.slice(- 41);
+console.log(`The dir part of the ${filePath} is ${filePathDir}`);
 // Create a variable to store the ext part of the variable
+const filePathExt = filePath.slice(-3);
+console.log(`The ext part of the ${filePath} is ${filePathExt}`);
