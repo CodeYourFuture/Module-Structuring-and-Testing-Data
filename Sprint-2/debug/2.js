23c167cd75acb02c2e4dda009c302abd4fd42a49
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-11 18:35:37 +0000

    sprint2-debug-2.js-complete

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -12,3 +12,17 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+// The function getLastDigit is supposed to find the last digit of a number, but it always returns the last digit of 103 which is stored in the variable num
+// But we want to it to work with any number
+// To fix this, we need to allow getLastDigit to take a number as input (also called a parameter). This way, the function can work with any number you give it.
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
