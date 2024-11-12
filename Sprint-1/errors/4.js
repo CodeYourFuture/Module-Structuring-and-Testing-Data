35020b300e80d1e0aa813af269540da7fe1e5935
Natalie Patel <natcandy@gmail.com>
2024-11-12 11:32:38 +0200

    Renamed variable without a number as the first character

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const hour12ClockTime = "20:53";
+const hour24ClockTime = "08:53";
+
+/* The SyntaxError is that variables cannot start with a number.
+Furthermore, time may always change so a constant is not the right variable type
+to use */
