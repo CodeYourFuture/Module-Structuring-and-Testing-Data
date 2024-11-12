02c85a7f7c27ec7b4f41087e0f1191f10211e26b
Luke Manyamazi <isirluke@gmail.com>
2024-11-12 12:02:31 +0200

    added answers to errors 1

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -2,6 +2,9 @@
 
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
+// There are 2 declarations of decimalNumber, one in the parameter and again on const
+// The code will give an error stating that we have declared decimalNumber twice
+// The assignment of 0.5 can lso be done in the console.log rather than as a declaration
 
 function convertToPercentage(decimalNumber) {
   return `${decimalNumber * 100}%`;
