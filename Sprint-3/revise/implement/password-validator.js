3c8f70c399163bc731228fb09cec29f722fdd387
ArminNouri <nouriarmin24@gmail.com>
2024-11-12 11:19:05 +0000

    change password checker  and add function for checking password.

--- Sprint-3/revise/implement/password-validator.js
+++ Sprint-3/revise/implement/password-validator.js
@@ -4,31 +4,13 @@ function passwordValidation(password) {
   //   }
   if (password.length > 5) {
     // Check if the password contains at least one uppercase letter
-    const hasUppercase = /[A-Z]/.test(password);
-    if (!hasUppercase) {
+    if (!validatePassword(password)) {
       return false;
-    }
-
-    // Check if the password contains at least one lowercase letter
-    const hasLowercase = /[a-z]/.test(password);
-    if (!hasLowercase) {
-      return false;
-    }
-
-    // Check if the password contains at least one number
-    const hasNumber = /\d/.test(password);
-    if (!hasNumber) {
-      return false;
-    }
-
-    // Check if the password contains at least one non-alphanumeric symbol
-    const hasSymbol = /[!#$%.&]/.test(password);
-    if (!hasSymbol) {
-      return false;
-    }
-  } else {
-    return false;
-  }
+    } else return true;
+  } else return false;
 }
-
+function validatePassword(password) {
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+.]).{6,20}$/;
+  return regex.test(password);
+}
 module.exports = passwordValidation;
