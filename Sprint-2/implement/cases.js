f28e04d140314b271127ca177427b453567063df
Luke Manyamazi <isirluke@gmail.com>
2024-11-12 14:07:01 +0200

    added a comment for the snake case code

--- Sprint-2/implement/cases.js
+++ Sprint-2/implement/cases.js
@@ -14,6 +14,8 @@
 // You will need to come up with an appropriate name for the function
 // Use the string documentation to help you find a solution
 
+// Below is the code to convert strings to capital letters and add underscores inbetween the words
+
 function upperCaseSnake(str) {
   return str.toUpper().replaceAll(" ", "_");
 }
