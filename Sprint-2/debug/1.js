86c549fb2265f1c43e2b1926200c9b313a157b23
Ameneh-KM <ameneh.keshavarz@gmail.com>
2024-11-12 10:05:21 +0000

    Sprint-2-debug

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,5 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// The console logs "The sum of 10 and 32 is" because the function does not return any value.
+// The return; causes the function to exit immediately. Therefore, the line a+b is never executed.
--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -12,3 +12,17 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// The code returns 3 in all cases as it is returning the last digit of constant num.
+// The function relies on the global variable num as there is no argument in the function.
+// We can modify this by adding a parameter to function and use that parameter to find the last digit of 
+// the given number.
+
+/*const num = 103;
+
+function getLastDigit(number) {
+  return number.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);*/
