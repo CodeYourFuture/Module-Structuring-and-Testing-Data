e068569580f9599682a641ab6243af013ebe7522
Luke Manyamazi <isirluke@gmail.com>
2024-11-12 11:54:37 +0200

    added my answer to error 0

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -2,6 +2,8 @@
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+// The error is happening because the code is declaring th str variable twice
+// To fix this, I will only add the str assignment to the return statement and remove the let str part
 
 function capitalise(str) {
   return `${str[0].toUpperCase()}${str.slice(1)}`;
