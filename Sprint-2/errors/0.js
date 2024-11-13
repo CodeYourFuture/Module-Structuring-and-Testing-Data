18b57932eda32f80ff373293a92edbaca3988f71
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 16:12:10 +0200

    Interpreted the error and fixed it

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -3,7 +3,12 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+//The error is cause by an already declared str variable. The problem is fixed by removing the let keyword.
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+   str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+let myCapString = capitalise("simphiwe");
+console.log(myCapString);
