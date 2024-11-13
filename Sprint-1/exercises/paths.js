8903a5e47002e404b118ae779781e6adea7e4356
fatima safana <asksiyam@gmail.com>
2024-11-13 16:32:22 +0000

    solution path.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -11,8 +11,16 @@
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
+    console.log(lastSlashIndex);
 const base = filePath.slice(lastSlashIndex + 1);
+    console.log(base);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+const dirPart = filePath.substring(45, -1);
+    console.log(`The directory path of ${dirPart}`);
+
+const extPart = dirPart + "ext.txt";
+    console.log(`The ext path is ${extPart}`);
