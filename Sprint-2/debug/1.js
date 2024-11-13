e62ecd0c6700c24df0ea08daacd1335004ba729a
Natalie Patel <natcandy@gmail.com>
2024-11-13 17:14:38 +0200

    Explained and fixed bug

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,8 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+/* In the function, the return statement is closed with a ; with a + b written as a new statement, 
+which outputs as undefined.
+The fix is to have a one-line statement where the code is return a + b;
+*/
