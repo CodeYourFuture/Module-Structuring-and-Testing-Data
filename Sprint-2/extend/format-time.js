a84a08e3ce9cf2fdaabf07aa0fba6ab5ad202567
Millena28 <millenakiflom06@gmail.com>
2024-11-13 09:16:10 +0000

    Added my test codes/format-time.js

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -22,3 +22,29 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+//My testes;
+const currentOutput3 = formatAs12HourClock("09:00");
+const targetOutput3 = "09:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+// test#4
+
+const currentOutput4 = formatAs12HourClock("23:00");
+const targetOutput4 = "11:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+//test#5
+
+const currentOutput5 = formatAs12HourClock("18:00");
+const targetOutput5 = "6:00 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+);
