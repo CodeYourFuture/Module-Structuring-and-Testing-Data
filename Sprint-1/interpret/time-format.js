8c8e9846ce5755163105a243adc8c87940d7b610
Serna Malala <sm.sernamalala@gmail.com>
2024-11-13 14:40:04 +0200

    Almost the same thing that I tried to explain in that line, GPT made it more clearer about what the expression was being used for and why the division was taking place

--- Sprint-1/interpret/time-format.js
+++ Sprint-1/interpret/time-format.js
@@ -18,14 +18,15 @@ console.log(result);
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // according to the documentation the The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.
 
-
 // //According to GPT The expression movieLength % 60 gives the remaining seconds after dividing the total movie length by 60, which provides the seconds left after accounting for full minutes. This is how we get the seconds part for the time in "hours:minutes
 // " format.
 
 // The remaining value of the movieLength will be return as the remaining amounts of minutes since the saved 60 minutes will be equivalent to an hour
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
 
-//The total minutes line takes all the seconds thats are able to fit into miutes and saves that and gives the reminder secconds that dont fit full into 60 seconds which counts as a whole minute.
+//The total minutes line takes all the seconds thats are able to fit into minutes and saves that and gives the reminder secconds that dont fit full into 60 seconds which counts as a whole minute.
+
+//Acording to GPT, the expression (movieLength - remainingSeconds) / 60 calculates the total minutes in the movie by removing the leftover seconds (those that don’t make a full minute) and then dividing by 60. This way, we get the exact number of complete minutes within the total seconds. Almost the same thing that I tried to explain
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 //The result tells us how long the movie is in hours and minutes, a better name would be totalMovieTime
