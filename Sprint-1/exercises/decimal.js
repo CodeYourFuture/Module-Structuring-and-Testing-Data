4c77ac5c79bd1a484e8eb747bb38e2cdaf802466
fatima safana <asksiyam@gmail.com>
2024-11-13 15:43:25 +0000

    solution decimal.js

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -7,3 +7,13 @@ const num = 56.5678;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
+
+let wholeNumberPart = Math.round(num);
+    console.log(wholeNumberPart)
+
+let decimalPart = num - Math.floor(num);
+    console.log(decimalPart);
+
+let roundedNum = Math.round(num);
+     console.log(roundedNum);
+
