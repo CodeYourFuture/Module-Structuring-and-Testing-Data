f7d5ae6191fdf7c59fb660ab6a5ae28bb987d406
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 15:53:11 +0200

    Identified the error and made corrections

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,9 @@
 // Predict and explain first...
 
+// The output is undefined due to the expression, on line 7, placed under the return statement, line 6. The return statement stops the execution of a function and returns a value.
+
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
