6cd0e4e599ca7a67f723045d8465fdf69d4ddc1c
Luke Manyamazi <isirluke@gmail.com>
2024-11-13 22:08:09 +0200

    added assert inputs

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -9,15 +9,15 @@ function formatAs12HourClock(time) {
   return `${time} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
-console.assert(
-  currentOutput === targetOutput,
-  `current output: ${currentOutput}, target output: ${targetOutput}`
-);
+// const currentOutput = formatAs12HourClock("08:00");
+// const targetOutput = "08:00 am";
+// console.assert(
+//   currentOutput === targetOutput,
+//   `current output: ${currentOutput}, target output: ${targetOutput}`
+// );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+const currentOutput2 = formatAs12HourClock("04:50");
+const targetOutput2 = "04:50 am";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
