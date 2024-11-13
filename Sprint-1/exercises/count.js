18d02e80f56b762988f42b1f9e0eb095b3bb1543
Serna Malala <sm.sernamalala@gmail.com>
2024-11-13 15:16:01 +0200

    Learnt that increment and decrement can be used to explain adding or subtracting 1 continuously

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -6,3 +6,7 @@ count = count + 1;
 // Describe what line 3 is doing, in particular focus on what = is doing
 
 //Line 3 is reassigning count to become ann adition of its previous value plus one.
+
+
+// Line 1: Declares a variable count and initializes it with a value of 0.
+// Line 3: Performs an increment operation, increasing the value of count by 1. This is achieved by using the expression count = count + 1, where the result of the addition is then reassigned back to the count variable.
