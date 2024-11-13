c90adf4c78b6543c1f6fc70a6469932533341bd4
Serna Malala <sm.sernamalala@gmail.com>
2024-11-13 15:00:21 +0200

    made changes to the directory varibale, used the slice method and indexing to get the directory from the file path

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -15,6 +15,6 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-const directory = "/Users/mitch/cyf/Module-JS1/week-1/interpret";
+const directory = filePath.slice(0, lastSlashIndex);
 // Create a variable to store the ext part of the variable
 const ext = "";
