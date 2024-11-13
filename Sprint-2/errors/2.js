4b7ea493f658641fbb4a0a84ce9b27911295dfee
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 16:38:24 +0200

    Located and rectified the error and gave an explanation

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,7 +3,7 @@
 
 // this function should square any number but instead we're going to get an error
 
-// We get a SyntaxError due the unexpected number 3 passed on the function as a parameter. Instead we should pass num as a parameter, in that way the function will square any number passed onto it
+// We get a SyntaxError due the unexpected number 3 passed on the function as a parameter. Instead we should pass num as a parameter, in that way the function will square any number passed into it
 
 function square(num) {
     return num * num;
