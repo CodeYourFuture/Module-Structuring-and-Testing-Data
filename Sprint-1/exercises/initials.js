38f2bfbfb317c9965e3329ce1313c35c9c5aad72
Natalie Patel <natcandy@gmail.com>
2024-11-13 16:36:40 +0200

    Wrote shorter code

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -5,8 +5,16 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
+let firstCharacter = firstName[0];
+let secondCharacter = middleName[0];
+let thirdCharacter = lastName[0];
+let initials = (firstCharacter + secondCharacter + thirdCharacter);
+console.log(initials);
+
+
+/* Old attempt that has been updated to the above:
 var firstInitial = firstName.slice(0,1);
 var secondInitial = middleName.slice(0,1);
 var thirdInitial = lastName.slice(0,1);
 var allInitials = (firstInitial+secondInitial+thirdInitial);
-console.log(allInitials);
+console.log(allInitials); */
