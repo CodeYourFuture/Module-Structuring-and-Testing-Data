5405c153adf92de12712b1d1de9035270c29fd5e
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 16:32:36 +0200

    From inspecting and testing the code I located a few errors and fixed them

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -3,11 +3,14 @@
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
 
+// Firstly, the decimalNumber variable has already been declared. Secondly, the function convertToPercentage has not been called correctly, instead console.log was used to log decimalNumber. By removing the const keyword and calling the function correctly allows the code to convert the given decimalNumber to a percentage.
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+let myPercentage = convertToPercentage()
+console.log(myPercentage);
