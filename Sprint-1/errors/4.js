b099af7f513b451f4b48858fb372fd7c728c1461
Serna Malala <sm.sernamalala@gmail.com>
2024-11-13 15:18:38 +0200

    made the varibale names make sense accoring to what they were being assigned

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,4 +1,4 @@
-const twelveHourClockTime = "20:53";
-const twentyFourHourClockTime = "08:53";
+const twentyFourHourClockTime = "20:53";
+const twelveHourClockTime = "08:53";
 
 //make varibales names proper and according to conventions given
