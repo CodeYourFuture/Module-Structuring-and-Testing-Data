93cc1595428d05f587b10285ba760bef5970f670
Millena28 <millenakiflom06@gmail.com>
2024-11-13 06:58:14 +0000

    errors/ 2.js

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -6,5 +6,16 @@
 function square(3) {
     return num * num;
 }
+// Explanation.
+//  First error; Unexpected number
+//The function square is defined with 3 as a parameter name, which is not allowed in JavaScript.
+//Second error; Inside the function, return num * num; is supposed to square the input number.
+//However, there’s no variable named num defined in this function.
+//Fixed  the code:
+function square(num) {
+    return num * num;
+}
+
+console.log(square(3));  // Output: 9
 
 
