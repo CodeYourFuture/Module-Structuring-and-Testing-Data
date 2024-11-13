e734d701e7cc32d2bac89b59c08db9db63630903
NataliePatel <natcandy@gmail.com>
2024-11-13 13:35:35 +0200

    Updated following review

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -8,7 +8,11 @@ const last4Digits = cardNumber.slice(-4);
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
-/* The slice function works with arrays and cardNumber was initially just one
-long number so while the TypeError given during debugging is that cardNumber.slice is not a function,
-it is actually that there is no array for the slice to work. By adding commas to 
-separate the integers in the const array and then running the code, it works */
+/* The TypeError given during debugging is that cardNumber.slice is not a function.
+The code won't work because cardNumber is a number and .slice works with strings and arrays.
+Assuming that cardNumber cannot be modified by adding commas between each character to create an array 
+(this would run the code without bugs but it would not be logical to modify the cardNumber) the solution is to convert
+cardNumber to a string and then either use the .slice or .substring methods.
+Solution: Let last4Digits = cardNumber.toString().slice(12,17); 
+(This solution would only work if the card number has a standard amount of characters, in this case 16
+because the parameters of 12 and 17 for the substring/slice have been programmed.)
