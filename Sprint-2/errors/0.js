62b808c462285fa1b6f6f7ea6c0ff03d57618622
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 14:25:42 +0200

    Figured what's causing an error and interpreted the error message

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -3,7 +3,12 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// The error came from the fact that str could not be declared twice and so I removed the let keyword 
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+let myStr = capitalise("simphiwe");
+console.log(myStr);
