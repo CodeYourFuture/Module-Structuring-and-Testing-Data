58bdfef221aac32f784dd25a64127d41daa13bff
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 13:10:57 +0200

    I predicted an undefined output and fixed the expression to return the function value

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+   return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// The output will be undefined due to the fact that the value from the multiply() function hasn't been returned thus not saved. Also the value will not be reusable from using the console.log function to output the result. This option works mostly when debugging a program/code.
