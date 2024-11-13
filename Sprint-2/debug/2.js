22df32411f89aa8c8eecd4ccc711ae20da5fe8c9
sabafarjamfard <farjamfardsaba@yahoo.com>
2024-11-13 11:12:38 +0000

    Completing Sprint-2/debug/2.js

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -1,9 +1,16 @@
-// Predict and explain first...
+// Predict ://always return the last digit of the constant variable num 
+//(which is 3, from num = 103), regardless of the argument given in each function call.
 
-const num = 103;
+//and explain://The issue is that the getLastDigit function is written to use the global variable num instead of the number passed as an argument
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+ //Fix ://I replaced num with a parameter called number in the getLastDigit function.
+//Now, the function takes number as an argument, converts it to a string, and uses slice(-1) to return the last character (last digit).
+
+
+//const num = 103;
+
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
