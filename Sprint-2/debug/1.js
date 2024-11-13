bc46cae21cef9a43b63565e961886b7289c19545
Simpra11 <mrmabuya11@gmail.com>
2024-11-13 13:46:27 +0200

    Fixed the code from returning undefined. The return statement stops the execution of a function, any code below return will not execute

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,9 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// The code will output undefined due to placing return on line 4 before the expression on line 5. The return statement stops the execution of a function and returns a value and any expression below return will not execute
