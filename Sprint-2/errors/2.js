9b728b893bad043dfad5ef305a576f02ebf6d84a
Natalie Patel <natcandy@gmail.com>
2024-11-14 13:31:22 +0200

    Explained and fixed error

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,16 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(num) {
+function square(3) {
     return num * num;
 }
 
-console.log(square(3));
+/* Functions should not have literal values as the parameters, so writing in the number 3
+is not a good parameter.
+If instead of 3, the parameter is num, then what would be returned would be num * num.
+(Parameters should be used in the function body.)
+
+The console log would call the function while replacing the parameter with a literal value,
+for example:
+console.log(square(3)); which would return 9
+*/
