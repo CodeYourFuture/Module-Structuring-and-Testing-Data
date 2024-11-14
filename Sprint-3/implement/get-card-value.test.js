8bcf58e100b32c09f07ecf71c468ce8f04abfe47
nadika <nadikazavodovska@gmail.com>
2024-11-14 11:37:55 +0000

    Implement function, add console.assert tests

--- Sprint-3/implement/get-card-value.test.js
+++ Sprint-3/implement/get-card-value.test.js
@@ -31,9 +31,59 @@
 // Then it should throw an error indicating "Invalid card rank."
 
 function getCardValue(card){
-    if (Number(card[0]) >= 2 && Number(card[0]) <= 9)
+    if (Number(card[0]) >= 2 && Number(card[0]) <= 9){
         return Number(card[0]);
+    } else if (card.slice(0,2) === "10" || card[0] === "J" || card[0] === "Q" || card[0] === "K"){
+        return 10;
+    } else if (card[0] === "A") {
+        return 11;
+    } else{
+        return "Invalid card rank";
+    }
 }
 
-let card = getCardValue("9♠");
-console.log(card);
+let card = getCardValue("12♠");
+// console.log(card);
+
+// console.assert tests
+// Test 1
+const currentOutput1 = getCardValue("2♠");
+const targetOutput1 = 2;
+console.log(currentOutput1);
+console.assert(currentOutput1 === targetOutput1, `${currentOutput1} is not equal ${targetOutput1}`);
+
+// Test 2
+const currentOutput2 = getCardValue("5♦");
+const targetOutput2 = 5;
+console.log(currentOutput2);
+console.assert(currentOutput2 === targetOutput2, `${currentOutput2} is not equal ${targetOutput2}`);
+
+// Test 3
+const currentOutput3 = getCardValue("9♡");
+const targetOutput3 = 9;
+console.log(currentOutput3);
+console.assert(currentOutput3 === targetOutput3, `${currentOutput3} is not equal ${targetOutput3}`);
+
+// Test 4
+const currentOutput4 = getCardValue("10♣");
+const targetOutput4 = 10;
+console.log(currentOutput4);
+console.assert(currentOutput4 === targetOutput4, `${currentOutput4} is not equal ${targetOutput4}`);
+
+// Test 5
+const currentOutput5 = getCardValue("Q♣");
+const targetOutput5 = 10;
+console.log(currentOutput5);
+console.assert(currentOutput5 === targetOutput5, `${currentOutput5} is not equal ${targetOutput5}`);
+
+// Test 6
+const currentOutput6 = getCardValue("A♦");
+const targetOutput6 = 11;
+console.log(currentOutput6);
+console.assert(currentOutput6 === targetOutput6, `${currentOutput6} is not equal ${targetOutput6}`);
+
+// Test 7
+const currentOutput7 = getCardValue("12♦");
+const targetOutput7 = "Invalid card rank";
+console.log(currentOutput7);
+console.assert(currentOutput7 === targetOutput7, `${currentOutput7} is not equal ${targetOutput7}`);
