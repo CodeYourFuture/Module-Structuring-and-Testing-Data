ecdc054251af857685be200524acc38694720ed1
nadika <nadikazavodovska@gmail.com>
2024-11-14 22:02:13 +0000

    Add function, jest tests

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,24 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+function isPrimeNumber(num){
+    if(num < 2) return false;
+    for(let i = 2; i < num; i++){
+        if(num % i === 0) return false; 
+    }
+    return true;
+}
+
+let primeNum = isPrimeNumber(14);
+console.log(primeNum);
+
+test("check a prime number", () => {
+    expect(isPrimeNumber(0)).toBe(false);
+    expect(isPrimeNumber(-2)).toBe(false);
+    expect(isPrimeNumber(1)).toBe(false);
+    expect(isPrimeNumber(2)).toBe(true);
+    expect(isPrimeNumber(3)).toBe(true);
+    expect(isPrimeNumber(29)).toBe(true);
+    expect(isPrimeNumber(28)).toBe(false);
+})
