d80c34424a3d05c2d33a780fdf7cf65f8c43209b
Simpra11 <mrmabuya11@gmail.com>
2024-11-14 13:54:05 +0200

    For my understanding of Math.random() I created a random integer between - 5 and 3

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -2,7 +2,12 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-console.log(num); // num represents a random number generated between 1 and 100 (minimum & maximum) : a random number is generated using Math.random() then brought into a whole integer using Math.floor()
+//console.log(num); // num represents a random number generated between 1 and 100 (minimum & maximum) : a random number is generated using Math.random() then brought into a whole integer using Math.floor()
+
+const randomNum = Math.floor(Math.random()* (3 - (-5) + 1)) + (-5);
+console.log(randomNum);
+
+// For my understanding of how Math.random() works I created a random integer between - 5 and 3 exclusively. 
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
