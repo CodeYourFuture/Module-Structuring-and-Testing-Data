af716653a89a3d68617373157a03cf40d596fd34
Natalie Patel <natcandy@gmail.com>
2024-11-14 13:31:06 +0200

    Explained and fixed error

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,8 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
-
+console.log(square(3));
