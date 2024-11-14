0396666cd5f03468f3431fb55c227a041491b9a7
Simpra11 <mrmabuya11@gmail.com>
2024-11-14 22:04:59 +0200

    Wrote a number of tests and got a number of bugs (assertion fails)  re missing minutes,  midnight hour (00:00) and a character in front of the hour "01" then fixed the bug

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -2,9 +2,15 @@
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
 function formatAs12HourClock(time) {
-  const hours = Number(time.slice(0, 2));
+  const hours = Number(time.slice(0, 2)); // Gets the hour as a number 
+  const minutes = time.slice(3) // Gets the minutes 
+
+  if(hours === 0){                // Checks for midnight hour (00:00)
+    return `12:${minutes} am`
+  }
+
   if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+    return `${String(hours - 12).padStart(2, "0")}:${minutes} pm`; // Turns hours to two characters '01'
   }
   return `${time} am`;
 }
@@ -16,9 +22,31 @@ console.assert(
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+const currentOutput2 = formatAs12HourClock("22:00");
+const targetOutput2 = "10:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+
+const currentOutput4 = formatAs12HourClock("14:00");
+const targetOutput4 = "02:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+
+
+
+
+let myTime = formatAs12HourClock("00:10");
+console.log(myTime);
