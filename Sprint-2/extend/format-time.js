9d9875e737d171cebf81705e42ff16a565942144
samirahekmati <samirahekmati@gmail.com>
2024-11-14 15:47:20 +0000

    Done: sprint2/extend

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -22,3 +22,29 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+
+//Answer : (my tests)
+
+//test case for midnight and noon:
+const currentOutput4 = formatAs12HourClock("00:00")
+const targetOutput4 = "12:00 am"
+console.assert(
+  currentOutput4===targetOutput4,
+  `curret output: ${currentOutput4}, target output: ${targetOutput4}`
+)
+const currentOutput3 = formatAs12HourClock("12:00")//12
+const targetOutput3 = "12:00 pm"
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+)
+
+//Test cases for times just before transitions
+
+const currentOutput5= formatAs12HourClock("12:59")
+const targetOutput5 = "12:59 pm"
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+)
