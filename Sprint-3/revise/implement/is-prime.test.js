9617648eabf692c5ba3307c830f5353c9be5cc8f
Amir200524 <amiraminia@yahoo.com>
2024-11-14 17:31:34 +0000

    feedback

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -10,3 +10,18 @@ function isPrime(num) {
 
 module.exports = isPrime;
 
+//Feedback 
+//PASS  ./isPrime.test.js
+ // isPrime
+   // ✓ returns false for numbers less than 2 (x ms)
+   // ✓ returns true for small prime numbers (x ms)
+    //✓ returns false for small non-prime numbers (x ms)
+    //✓ returns true for larger prime numbers (x ms)
+   // ✓ returns false for larger non-prime numbers (x ms)
+
+//Test Suites: 1 passed, 1 total
+//Tests:       5 passed, 5 total
+//Snapshots:   0 total
+//Time:        X s
+
+
