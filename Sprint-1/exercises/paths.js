1c432f445b94cfa15aa6e5801aeae905b3ba9368
Simpra11 <mrmabuya11@gmail.com>
2024-11-14 13:13:32 +0200

    Created variables to store the dir part and the ext part of the filePath variable

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -9,16 +9,17 @@
 
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
+const path = require('path')
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
+
 console.log(`The base part of ${filePath} is ${base}`);
 
 
-
 // Create a variable to store the dir part of the filePath variable
-const filePathDir = filePath.slice(- 41);
-console.log(`The dir part of the ${filePath} is ${filePathDir}`);
+const dirPart = path.dirname(filePath);
+console.log(`The dir part of ${filePath} is ${dirPart}`);
 // Create a variable to store the ext part of the variable
-const filePathExt = filePath.slice(-3);
-console.log(`The ext part of the ${filePath} is ${filePathExt}`);
+const extPart = path.extname(filePath);
+console.log(`The ext part of ${filePath} is ${extPart}`);
