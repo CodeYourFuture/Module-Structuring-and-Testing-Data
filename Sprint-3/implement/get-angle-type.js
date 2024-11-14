0ef3b4074370bdae5f18cdcbe87540d11f70af32
sabafarjamfard <farjamfardsaba@yahoo.com>
2024-11-14 16:05:27 +0000

    Completing sprint-3/Implement/get-angle-type.js

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -25,3 +25,34 @@
 // Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+
+function getAngleType(angle) {
+    if (angle === 90) {
+        return "Right angle";
+    } else if (angle < 90 && angle >=0) {
+        return "Acute angle";
+    } else if (angle > 90 && angle < 180) {
+        return "Obtuse angle";
+    } else if (angle === 180) {
+        return "Straight angle";
+    } else if (angle > 180 && angle < 360) {
+        return "Reflex angle";
+    } else {
+        return "Invalid angle";
+    }
+}
+module.exports = getAngleType;
+
+
+ //Test code 
+if (require.main === module) {
+    const getAngleType = require('./get-angle-type');
+
+    console.log(getAngleType(90) === "Right angle" ? "Pass" : "Fail");
+    console.log(getAngleType(45) === "Acute angle" ? "Pass" : "Fail");
+    console.log(getAngleType(120) === "Obtuse angle" ? "Pass" : "Fail");
+    console.log(getAngleType(180) === "Straight angle" ? "Pass" : "Fail");
+    console.log(getAngleType(270) === "Reflex angle" ? "Pass" : "Fail");
+    console.log(getAngleType(360) === "Invalid angle" ? "Pass" : "Fail");
+    console.log(getAngleType(-30) === "Invalid angle" ? "Pass" : "Fail");
+}
