b877b9ff89a1d5f5e16a6c5178a35faca7122f7d
saraamiiri <amirisara1993@gmail.com>
2024-11-14 10:01:01 +0000

    1.js/error

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -11,3 +11,12 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+//The parameter decimalNumber is already declared in the function then and then trying  to redeclare it inside the function.
+//decimal number has been already decleard .
+//in the last line we can not console.log decimal number directly 
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+const result = convertToPercentage(0.5);
+console.log(result); 
