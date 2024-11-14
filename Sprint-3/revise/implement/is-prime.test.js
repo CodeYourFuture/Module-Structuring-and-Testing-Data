3a19cbb177a2c0d42d649186ac41f1338ac38533
Amir200524 <amiraminia@yahoo.com>
2024-11-14 17:17:38 +0000

    is_prime

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,12 @@
-// Given a positive integer num,
-// When the isPrime function is called with num as input,
-// Then it should check if the num is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    
+    return true;
+}
+
+module.exports = isPrime;
+
