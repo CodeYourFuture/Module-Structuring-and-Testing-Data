921b6ce87a86ff07b5d10640a273788da7ff4192
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2024-11-14 22:21:39 +0000

    implementing valid traiangle

--- Sprint-3/implement/is-valid-triangle.js
+++ Sprint-3/implement/is-valid-triangle.js
@@ -1,4 +1,4 @@
-// Implement a function isValidTriangle
+ // Implement a function isValidTriangle
 // Terms
 // the Triangle Inequality says: the sum of any two sides is always greater than the third side.
 // practical examples:
@@ -21,7 +21,7 @@
 // When the sum of any two side lengths is less than or equal to the length of the third side (i.e., a + b <= c, a + c <= b, b + c <= a),
 // Then it should return false because these conditions violate the Triangle Inequality, which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
 
-// scenario: invalid triangle
+// scenario: invalid triangle;
 // Check for Valid Input:
 // Given the sides a, b, and c,
 // When any of the sides are less than or equal to zero,
@@ -33,3 +33,19 @@
 // Then it should return true because the input forms a valid triangle.
 
 // This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+
+
+//solution 
+
+function isValidTriangle(a, b, c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        return "Invalid triangle";
+    }
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return "Invalid triangle";
+    }
+
+    return "Valid triangle";
+}
+
+
