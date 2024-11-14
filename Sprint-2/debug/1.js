e470b9b7be4fc29ee4ca3e7986f91c9d4c12d2cc
saraamiiri <amirisara1993@gmail.com>
2024-11-14 09:16:04 +0000

    sprint2/1.js

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,13 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//the return statement is encountered before the a + b expression.
+//In JavaScript, if the return statement is called with no value, the function implicitly returns undefined.
+//The output template literal is trying to log the result of sum(10, 32).
+//Since sum(10, 32) returns undefined, the log will output undefined as the result.
+//if we want function return sum we should change to :
+
+function sum(a, b) {
+  return a+b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
