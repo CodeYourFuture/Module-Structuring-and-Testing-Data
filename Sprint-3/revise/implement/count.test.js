d0f894d1425ecb83757ed84c9e1ae1be15d70f84
Amir200524 <amiraminia@yahoo.com>
2024-11-14 16:17:47 +0000

    count_char

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -1,17 +1,14 @@
-// implement a function countChar that counts the number of times a character occurs in a string
+function countChar(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    return count;
+}
 
-// Given a string str and a single character char to search for,
-// When the countChar function is called with these inputs,
-// Then it should:
+// Testing the function
+console.log(countChar("aaaaa", "a")); // Scenario: Multiple Occurrences, Output: 5
+console.log(countChar("hello world", "z")); // Scenario: No Occurrences, Output: 0
 
-// Scenario: Multiple Occurrences
-// Given the input string str,
-// And a character char that may occur multiple times with overlaps within str (e.g., 'a' in 'aaaaa'),
-// When the function is called with these inputs,
-// Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
-
-// Scenario: No Occurrences
-// Given the input string str,
-// And a character char that does not exist within the case-sensitive str,
-// When the function is called with these inputs,
-// Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
---
+++ Sprint-3/revise/implement/python.py
@@ -0,0 +1,12 @@
+print('hello word')
+print('amir')
+print(2.5)
+print(2*3)
+print(type(3.5))
+print("python")
+print(type("Amir"))
+print(type('300'))
+age=43
+print(age)
+print(id(age))
+
