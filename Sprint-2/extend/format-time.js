c168bc87510a1b55de399fb7d65c7df0e0d6d754
saraamiiri <amirisara1993@gmail.com>
2024-11-15 10:46:10 +0000

    to-pounds.js

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -2,42 +2,40 @@
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
 function formatAs12HourClock(time) {
-  const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+
     //split the input time string into hours and minute parts.
     let[hours,minute]=time.split(":");
     //convert hours to number 
-    hours=number(hours);
+    hours=Number(hours);
     //set the suffix to"pm" if the hours is 12 or higher ;otherwise ,set it to "am"
     let suffix=hours>=12? "pm": "am";
     //adjust hour value for 12-hour format .
-    if (hours === 0) {
+  if (hours === 0) {
       return `12:00 am`;  // Midnight case
     } else if (hours === 12) {
       return `12:00 pm`;  // Noon case
     } else if (hours > 12) {
       return `${hours - 12}:00 pm`;  // PM times (13:00 - 23:59)
     }
-  return `${time} am`;
+ 
   //ensure hours and minute are two digits ,padding with"0"if needed .
-  hours=hours.tostring().padstart(2,"0");
-  minutes=minutes.padstart(2,"0");
+  hours=hours.toString().padStart(2,"0");
+  minute=minute.padStart(2,"0");
   //return the formatted 12-hour 
-  return `${hours}:${minutes}${suffix}`;
-
-
-
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+  return `${hours}:${minute} ${suffix}`;
+}
+const currentOutput = formatAs12HourClock("01:50");
+const targetOutput = "01:50 am";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 console.log(currentOutput);
+
 const currentOutput2 = formatAs12HourClock("23:00");
 const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+console.log(currentOutput2);
--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -7,25 +7,27 @@ function formatTimeDisplay(seconds) {
   const totalMinutes = (seconds - remainingSeconds) / 60;
   const remainingMinutes = totalMinutes % 60;
   const totalHours = (totalMinutes - remainingMinutes) / 60;
-
+console.log(totalHours);
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(
     remainingSeconds
   )}`;
 }
-
+let time=formatTimeDisplay(61);
+console.log(time)
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
-// a) When formatTimeDisplay is called how many times will pad be called?
+// a) When formatTimeDisplay is called how many times will pad be called?3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
-// b) What is the value assigned to num when pad is called for the first time?
+// b) What is the value assigned to num when pad is called for the first time?0
 
-// c) What is the return value of pad is called for the first time?
+// c) What is the return value of pad is called for the first time?00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-
+//second 1     
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+//01
