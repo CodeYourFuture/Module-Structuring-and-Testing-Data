726bccc51f43d0b1b7203fa899cb6910fadb8300
nadika <nadikazavodovska@gmail.com>
2024-11-15 16:41:55 +0000

    Change function, add jest tests

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,19 +14,37 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+const passwords = ["7wnuT$bH", "!uTd4*M", "456Ww^8"];
 
-function checkPassword(pass){
-    if(pass < 5){
+function checkPassword(pass) {
+    if (pass.length < 5) {
         return false;
-    } else if(!/[0-9]/.test(pass)){
+    } else if (!/[0-9]/.test(pass)) {
         return false;
-    } else if (!/[!#$%.&*]/.test(pass)){
+    } else if (!/[A-Z]/.test(pass)) {
+        return false;
+    } else if (!/[a-z]/.test(pass)) {
+        return false;
+    } else if (!/[!#$%.&*]/.test(pass)) {
+        return false;
+    } else if (passwords.includes(pass)) {
         return false;
     }
     return true;
 }
 
 let userPassword = checkPassword("te^A");
-console.log(userPassword);
+// console.log(userPassword);
 
-
+// jest tests 
+test("check password", () => {
+    expect(checkPassword("te^")).toBe(false);
+    expect(checkPassword("tyi@EQ")).toBe(false);
+    expect(checkPassword("te^yd5")).toBe(false);
+    expect(checkPassword("HIF3&8")).toBe(false);
+    expect(checkPassword("te$sucRYm")).toBe(false);
+    expect(checkPassword("te^")).toBe(false);
+    expect(checkPassword("teRTIN4")).toBe(false);
+    expect(checkPassword("te*YV87")).toBe(true);
+    expect(checkPassword("!uTd4*M")).toBe(false);
+})
