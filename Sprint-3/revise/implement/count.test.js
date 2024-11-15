f0d2e2b15d5cb2b525602795db07ce0b2ebbfd3b
nadika <nadikazavodovska@gmail.com>
2024-11-15 10:17:55 +0000

    Implement function

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -15,3 +15,17 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+function countChar(str, char){
+    let countResult = 0;
+    
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === char) {
+            countResult++;
+        }
+    }
+    return `${char} occurs ${countResult} times in the ${str}.`;
+}
+
+let charInStr = countChar("Hellooo", "O");
+console.log(charInStr)
