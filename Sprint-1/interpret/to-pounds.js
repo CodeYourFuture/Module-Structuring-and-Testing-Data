6635aa3152931b2e47af15e0bab7e0bcfb8676d6
fatima safana <asksiyam@gmail.com>
2024-11-15 06:28:54 +0000

    solutions to-pounds.js

--- Sprint-1/interpret/to-pounds.js
+++ Sprint-1/interpret/to-pounds.js
@@ -4,16 +4,21 @@ const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 );
+  console.log(penceStringWithoutTrailingP);
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  console.log(paddedPenceNumberString);
+
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
+  console.log(pounds);
 
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
+  console.log(pence); 
 
 console.log(`£${pounds}.${pence}`);
 
@@ -24,4 +29,16 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 1. const penceString = "399p": initializes a string variable with the value "399p"
+// 3. initializes a variable that removes the "p" at the end, it uses method .substring
+// which returns the part of the string corresponding to the index and end index
+//-1 excluding the p
+//9. uses the method padstart to add "0" to the start of string penceStringWithoutTrailingP,
+// it returns the same string unchanged because the target length is equal to the current length.
+// this effectively multiplies any value input as penceStringWithoutTrailingP by 100
+//12. uses the substring method to return the first value of the string, this is to get
+// the pound. it also used length -2 to make sure its in a valid range so the program
+// can go through the steps with any value input.
+//18.the code gets the last two digits from the paddedPenceNumberString and ensures that the result
+//is a two-digit string by padding it with 0.
+//23. Print the value of pounds and pence with a period in between using template literals.
