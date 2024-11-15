895ed01ade443b0dc76c75121f396edf1feb1ad0
donarbl <donarablanc@gmail.com>
2024-11-15 15:14:01 +0000

    enterpreting the timeformat in simulator

--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -19,13 +19,16 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+// it will be called three times in return for totalHours, remainingMinutes and remainingSeconds
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-
+ //it's 0
 // c) What is the return value of pad is called for the first time?
+// it's 00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-
+// it's 1 as the remainingSeconds = 1
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+//the return value is 01, as.padStart(2, "0") provides that all numbers are formatted as 2 strings by adding a leading 0 when necessary.
--- test.js
+++ test.js
@@ -1,19 +1,37 @@
-// If products or services are subject to the 20% standard rate of VAT:
-// You must add 20% to the price you charge for the goods or service.
-// You can do this by multiplying the price you charge by 1.2.
-// For example, if your business sells sports equipment for £50, you multiply £50 by 1.2 for a total VAT inclusive price of £60.
-
-// Implement a function that calculates the new price with VAT (Value Added Tax) added on
-
-// Given a number,
-// When I call this function with a number
-// it returns the new price with VAT added on
-
-function calculateVAT(productPrice) {
-  return `${productPrice *1.2}`;
+function pad(num) {
+  return num.toString().padStart(2, "0");
 }
-const priceWithVat = calculateVAT(89);
-console.log (`£${priceWithVat}`);
+
+function formatTimeDisplay(seconds) {
+  const remainingSeconds = seconds % 60;
+  const totalMinutes = (seconds - remainingSeconds) / 60;
+  const remainingMinutes = totalMinutes % 60;
+  const totalHours = (totalMinutes - remainingMinutes) / 60;
+
+  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(
+    remainingSeconds
+  )}`;
+}
+
+// You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
+// to help you answer these questions
+
+// Questions
+
+// a) When formatTimeDisplay is called how many times will pad be called?
+// it will be called three times in return for totalHours, remainingMinutes and remainingSeconds
+
+// Call formatTimeDisplay with an input of 61, now answer the following:
+
+// b) What is the value assigned to num when pad is called for the first time?
+ //it's 0
+// c) What is the return value of pad is called for the first time?
+// it's 00
+
+// d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+// it's 1 as the remainingSeconds = 1
+// e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+//the return value is 01, as.padStart(2, "0") provides that all numbers are formatted as 2 strings by adding a leading 0 when necessary.
 
 
 
