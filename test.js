25f687d3a4c8b443d9427c699af581a6581fc163
donarbl <donarablanc@gmail.com>
2024-11-15 15:23:48 +0000

    test

--- test.js
+++ test.js
@@ -1,41 +1,27 @@
-function pad(num) {
-  return num.toString().padStart(2, "0");
-}
+// Implement a function getAngleType
 
-function formatTimeDisplay(seconds) {
-  const remainingSeconds = seconds % 60;
-  const totalMinutes = (seconds - remainingSeconds) / 60;
-  const remainingMinutes = totalMinutes % 60;
-  const totalHours = (totalMinutes - remainingMinutes) / 60;
+// Acceptance criteria:
 
-  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(
-    remainingSeconds
-  )}`;
-}
+// Given an angle in degrees,
+// When the function getAngleType is called with this angle,
+// Then it should:
 
-// You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
-// to help you answer these questions
+// Identify Right Angles:
+// When the angle is exactly 90 degrees,
+// Then the function should return "Right angle"
 
-// Questions
+// Identify Acute Angles:
+// When the angle is less than 90 degrees,
+// Then the function should return "Acute angle"
 
-// a) When formatTimeDisplay is called how many times will pad be called?
-// it will be called three times in return for totalHours, remainingMinutes and remainingSeconds
+// Identify Obtuse Angles:
+// When the angle is greater than 90 degrees and less than 180 degrees,
+// Then the function should return "Obtuse angle"
 
-// Call formatTimeDisplay with an input of 61, now answer the following:
+// Identify Straight Angles:
+// When the angle is exactly 180 degrees,
+// Then the function should return "Straight angle"
 
-// b) What is the value assigned to num when pad is called for the first time?
- //it's 0
-// c) What is the return value of pad is called for the first time?
-// it's 00
-
-// d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// it's 1 as the remainingSeconds = 1
-// e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-//the return value is 01, as.padStart(2, "0") provides that all numbers are formatted as 2 strings by adding a leading 0 when necessary.
-
-
-
-
-
-
-  
+// Identify Reflex Angles:
+// When the angle is greater than 180 degrees and less than 360 degrees,
+// Then the function should return "Reflex angle"
