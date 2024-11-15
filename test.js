6c326150d7622a7c07a18293630288543b2ad175
donarbl <donarablanc@gmail.com>
2024-11-15 13:27:45 +0000

    function correction for time

--- test.js
+++ test.js
@@ -2,12 +2,37 @@
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
 function convertAs24HoursTCo12hourslock(time) {
-  const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+  const hours = Number(time.slice(0, 2));// hours remain
+    // console.log(hours);
+  const minutes = time.slice(3);
+  // console.log(minutes);
+  if (hours === 24) {
+    if (minutes === '00') {
+      return `12 AM`;
+    }
+    return `12:${minutes} AM`
+  } else if (hours == 0o0) {
+    if (minutes === '00') {
+      return `12 AM`;
+    }
+    return `12:${minutes} AM`
   }
-  return `${time} am`;
-}
+  else if (hours > 12) {
+    if (minutes == '00') {
+      return `${hours - 12} PM`;
+    }
+    return `${hours - 12}:${minutes} PM`;
+  } else if (hours === 12) {
+    if (minutes == '00') {
+      return `12 PM`;
+    }
+    return `12:${minutes} PM`;
+  } else if (hours < 12) {
+    if (minutes == '00') {
+      return `${hours.toString()} AM`;
+    }
+    return `${hours.toString()}:${minutes} AM`;
+  }
 
 const currentOutput = formatAs12HourClock("08:00");
 const targetOutput = "08:00 am";
