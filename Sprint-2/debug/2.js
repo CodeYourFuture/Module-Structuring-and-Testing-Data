a48759b87731e7bcb0117ddcb96f58d59ead2131
Serna Malala <sm.sernamalala@gmail.com>
2024-11-15 11:12:23 +0200

    Debugged the function, made the const variable a let and added parameters to the function so that it works with different inputs

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
+//I predict that the function will return the last value of the string num but it will be constant so it will be 3 all the time
+let num = 103;
 
-const num = 103;
-
-function getLastDigit() {
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
@@ -12,3 +12,5 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+//I had to make a parameter for the function to take in instead of having a constant value that it uses, I made the const let and I added the parameter num to the getLast Digit Function
