716d278cfd2c865d71c41611d7be9645e5848dfb
Karla G. <k.grajalesluna@gmail.com>
2024-11-15 17:52:21 +0000

    done 0.js

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,20 @@
 // Predict and explain first...
 
+/*
+  the function won't return anything because we did not have any return so this will be undefine
+  it will print what is in the console but no in the second console because we are callin the function which will return undefine
+
+
+
+  function multiply(a, b) {
+    console.log(a * b);
+  }
+*/
+// ==================== this will return the arguments sent in the second console ====================
+
 function multiply(a, b) {
-  console.log(a * b);
+  return(a * b);
 }
+
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
