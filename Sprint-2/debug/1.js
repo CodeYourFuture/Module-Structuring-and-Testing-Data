54730eab5c20c0fde1d7aabf8a91fc007737d898
Serna Malala <sm.sernamalala@gmail.com>
2024-11-15 10:33:44 +0200

    Debugged the sum function and removed the errors

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
+//I think the sum function wont return anything, there will be errors because there is a semi-colon in after the return variable
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
