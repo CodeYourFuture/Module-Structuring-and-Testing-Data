068bcc961f4fe03518e1d7072d51f1787058f6d0
Mohammed <mohammed22471@outlook.com>
2024-11-16 11:43:58 +0000

    Sprint3-solutions

--- Sprint-3/revise/implement/ordinal.js
+++ Sprint-3/revise/implement/ordinal.js
@@ -1,18 +1,15 @@
 function ordinal(num) {
-
-	if(num % 11 === 0 || num % 12 === 0 || num % 13 === 0){
-		return num.toString() + "th";
-	}
-	else if(num % 10 === 1){
-		return num.toString() + "st";
-	}
-	else if(num % 10 === 2){
-		return num.toString() + "nd";
-	}
-	else if(num % 10 === 3){
-		return num.toString() + "rd";
-	}
-	else
-		return num.toString() + "th";
+    // Check for the special cases (11th, 12th, 13th)
+    if (num % 100 >= 11 && num % 100 <= 13) {
+        return num.toString() + "th";
+    } else if (num % 10 === 1) {
+        return num.toString() + "st";
+    } else if (num % 10 === 2) {
+        return num.toString() + "nd";
+    } else if (num % 10 === 3) {
+        return num.toString() + "rd";
+    } else {
+        return num.toString() + "th";
+    }
 }
 export default ordinal;
