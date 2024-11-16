71fe9f0c35e77e5d4190bb1f48c74b29b9dd796d
Karla G. <k.grajalesluna@gmail.com>
2024-11-16 12:18:07 +0000

    done 2.js

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -1,10 +1,23 @@
 // Predict and explain first...
 
+/*
+we can not run this function because we settle the variable with const and this won't allow us to reassign 
+the new value which we are trying to test in the console.log()
+
 const num = 103;
 
 function getLastDigit() {
   return num.toString().slice(-1);
 }
+
+*/
+
+//=========== This will  work ==============
+
+function getLastDigit(num) {
+  return num.toString().slice(-1); 
+}
+
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
