ddd56a9c5a9105ff5681086799f1d1cd40588b64
PersianQuant <sc.heidari@gmail.com>
2024-11-16 00:44:10 +0000

    exercise sprint 2 and 3

---
+++ Sprint-2/exercise/sprint2.js
@@ -0,0 +1,20 @@
+function formatAs12HourClock(hour) {
+}
+formatAs12HourClock("08:00") === "8:00 am"
+console.log(42 === 10 + 32);
+
+let mhairiName = "Mhairi";
+typeof mhairiName === "string";
+let mhairiAge = 28;
+let isMhairiOldEnoughToDrive = true;
+let kilometersMhairiDrivesToWork = 9.4;
+"${mhairiName} is 28" === `Mhairi is ${mhairiAge}`;
+"hello Mhairi" === `hello ${mhairiName}`;
+isMhairiOldEnoughToDrive;
+isMhairiOldEnoughToDrive === true;
+mhairiAge >= 18;
+29 <= mhairiAge;
+
+const calculation = 10 + 32;
+const result = 42;
+console.assert(calculation === result);
