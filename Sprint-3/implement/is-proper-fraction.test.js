0756a39c2e490688593a97a66ba3c9da54b65b4c
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-16 15:13:00 +0000

    is-proper-fraction.js - created assert test and jest test

---
+++ Sprint-3/implement/is-proper-fraction.test.js
@@ -0,0 +1,10 @@
+const { isProperFraction } = require('./is-proper-fraction');
+
+test("Test to see if if fractions are proper", () => {
+    expect(isProperFraction(2, 3)).toBe(true);
+    expect(isProperFraction(5, 2)).toBe(false);
+    expect(isProperFraction(3, 2)).toBe(false);
+    expect(() => isProperFraction(3, 0)).toThrow("Error (Denominator cannot be zero)");
+    expect(isProperFraction(-4, 7)).toBe(true);
+    expect(isProperFraction(3, 3)).toBe(false);
+});
