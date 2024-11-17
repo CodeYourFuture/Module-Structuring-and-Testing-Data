f1df38818cedad4e10c06070146d66f143d54069
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-17 10:33:34 +0000

    isPrime file created

---
+++ Sprint-3/revise/implement/isPrime.js
@@ -0,0 +1,11 @@
+const isPrime=(num)=>{
+    if (typeof num !== 'number' || num <= 1 || !Number.isInteger(num))
+      return false; 
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) 
+        return false; 
+    }
+  
+    return true; 
+  }
