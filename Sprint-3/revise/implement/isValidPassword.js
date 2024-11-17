bd93b9a0ba9c47d7bbd774a638fe4d3244a113c8
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-17 10:52:30 +0000

    isValidPassword file created

---
+++ Sprint-3/revise/implement/isValidPassword.js
@@ -0,0 +1,13 @@
+const isValidPassword=(password, passwords)=>{
+    if (typeof password !== 'string' || password.length < 5) {
+      return false; 
+    }
+  
+    const hasUpperCase = /[A-Z]/.test(password); 
+    const hasLowerCase = /[a-z]/.test(password); 
+    const hasNumber = /[0-9]/.test(password);    
+    const hasSymbol = /[!#$%.*&]/.test(password); // Check for non-alphanumeric symbols
+    const isUnique = !passwords.includes(password); // Check if it's not in the previous passwords
+  
+    return hasUpperCase && hasLowerCase && hasNumber && hasSymbol && isUnique;
+  }
