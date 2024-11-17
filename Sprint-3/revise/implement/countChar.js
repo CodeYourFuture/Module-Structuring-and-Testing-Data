b9ff9a301ee90c7e252e682c8e9024de930ac86d
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-17 12:18:08 +0000

    countChar file created

---
+++ Sprint-3/revise/implement/countChar.js
@@ -0,0 +1,14 @@
+const countChar=(str, char)=>{
+    if (typeof str !== 'string' || typeof char !== 'string' || char.length !== 1) {
+      throw new Error('Invalid input. The first argument must be a string, and the second must be a single character.');
+    }
+  
+    let count = 0;
+    for (let h = 0; h < str.length; h++) {
+      if (str[h] === char) {
+        count++;
+      }
+    }
+  
+    return count;
+  }
