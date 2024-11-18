6509d74eab82f0e652c5e4b03e38bbe7a654a007
Alireza1620 <alirezaseifishalamzari@gmail.com>
2024-11-18 10:44:13 +0000

    completing paths.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -15,4 +15,12 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-// Create a variable to store the ext part of the variable
+// Extract the `dir` part (everything before the last slash)
+const dir = filePath.slice(0, lastSlashIndex);
+
+// Find the last dot index in the `base` to extract the `ext` part
+const lastDotIndex = base.lastIndexOf(".");
+const ext = base.slice(lastDotIndex); // Includes the dot
+// Create a variable to store the ext part of the variables
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
