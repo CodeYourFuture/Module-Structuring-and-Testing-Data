70feea41f23d6a432d6cad8ce02bc812b77be9ec
Natalie Patel <natcandy@gmail.com>
2024-11-18 18:41:10 +0200

    Answered all questions

--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -13,16 +13,21 @@ function formatTimeDisplay(seconds) {
   )}`;
 }
 
+console.log(formatTimeDisplay(61));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+/* Pad is being called 3 times to ensure that hours, minutes and seconds all have at least 2 digits
+with a zero to start if there is only 1 digit.*/
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
+// The value assigned to num for the first time when pad is called is totalHours
 
 // c) What is the return value of pad is called for the first time?
 
