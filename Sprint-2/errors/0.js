db9819ece7a0cfee90d45f1b4b54a0fd2527c57a
katshi.mutombo@ogilvy.co.za <katshi.mutombo@ogilvy.co.za>
2024-11-18 17:59:44 +0200

    Error handling complete

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -1,9 +1,27 @@
 // Predict and explain first...
+/*
+Parameter: str: This is the input string that the function will process.
+String Manipulation: 
+- str[0].toUpperCase():
+- str[0]: Accesses the first character of the input string.
+- .toUpperCase(): Converts the first character to uppercase.
+- str.slice(1): Extracts a substring starting from the second character (index 1) to the end of the string.
+
+String Concatenation:
+`${...}${...}``: This is a template literal, a concise way to construct strings.
+The uppercase first character and the remaining substring are concatenated using the template literal.
+
+Return Value: The newly formed string with the capitalized first letter is returned by the function.
+*/
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+
+// the error Uncaught SyntaxError: Identifier 'str' has already been declared this is because str is the parameter 
+// and we are trying to declare a new variable named str inside the function body
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,6 +1,23 @@
 // Predict and explain first...
+/*
+Parameter:
+decimalNumber: This is the input decimal number that the function will process.
+Percentage Calculation:
+
+decimalNumber * 100: Multiplies the decimal number by 100 to convert it to a percentage value.
+${...}%: Uses a template literal to create a string representation of the percentage, appending the '%' symbol.
+
+Return Value: The formatted percentage string is returned by the function.
+
+The convertToPercentage function is intended to convert a decimal number to a percentage string.
+The function directly assigns the value 0.5 to the decimalNumber variable, overriding any input that might be passed to it. 
+*/
 
 // Why will an error occur when this program runs?
+// = The error arises due to redeclaration of the decimalNumber variable within the function scope
+// = decimalNumber is declared using the const keyword
+// = console.log(decimalNumber) statement, this is trying to access the decimalNumber variable, which is only defined within the convertToPercentage function
+
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
