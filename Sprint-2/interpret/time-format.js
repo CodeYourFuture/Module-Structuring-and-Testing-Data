699058cc47fed739cf5ac1837094ac1142f063b1
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2024-11-18 13:58:40 +0000

    completed interpret

--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -12,6 +12,7 @@ function formatTimeDisplay(seconds) {
     remainingSeconds
   )}`;
 }
+console.log(formatTimeDisplay(git 61))
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -19,13 +20,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+//3 times one for hours once for minutes once for seconds
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
+//The first call is for hours. Since 61 seconds equals 0 hours, num = 0
 
 // c) What is the return value of pad is called for the first time?
+// num = 0  pad ensures two digits, so it returns "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+//The last call is for seconds, calculated as 61 % 60 = 1
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+//The final pad call formats 1 into "01"
