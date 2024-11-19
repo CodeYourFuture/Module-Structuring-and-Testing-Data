52a261b58b11a9a3067b4828eeb669d6a893dd98
donarbl <donarablanc@gmail.com>
2024-11-19 10:12:05 +0000

    return instead of console to multiple

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+ return a * b;// if we want to have the result we need to return a *b, instead of console.log
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// (multiple (10,32) doesn't return anything, as it return undefined
+// 
