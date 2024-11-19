c76ea53066c0f168632fb1d3ae97bd7c04c65843
katshi.mutombo@ogilvy.co.za <katshi.mutombo@ogilvy.co.za>
2024-11-19 14:19:58 +0200

    isPrime function to check if the num is prime

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,24 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+function isPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+  
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+}
+
+console.log(isPrime(7));
+console.log(isPrime(10));
+console.log(isPrime(17));
+console.log(isPrime(21));
+console.log(isPrime(2));
+console.log(isPrime(1)); 
