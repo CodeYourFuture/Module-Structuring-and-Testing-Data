6fe797280ac8bc1ce7bcfa49d9d7d2012e91778a
donarbl <donarablanc@gmail.com>
2024-11-19 17:53:05 +0000

    removed let inside of the function

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -4,6 +4,8 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+// when I run in node it gives a syntaxerror, as the variable str has been declared twice
+// removed the let keyword from inside the function 
