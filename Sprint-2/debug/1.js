ff5daf33390694dc13d7020329cd60e47ba442a8
donarbl <donarablanc@gmail.com>
2024-11-19 10:15:37 +0000

    return value is corrected

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,7 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b; //remove semicolomn, as it terminate the function, so a +b doesn't execute. after return and move up a+b, to return the result of addition. 
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); // the final string will be The sum of 10 and 32 is undefined
