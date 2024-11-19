19c22853da38c77f5620b1cc092e9c1a4b7a4ba3
katshi.mutombo@ogilvy.co.za <katshi.mutombo@ogilvy.co.za>
2024-11-19 14:15:37 +0200

    Counts the number of times a character occurs in a string

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -15,3 +15,18 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+function countChar(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === char) {
+        count++;
+      }
+    }
+    return count;
+}
+
+const text = "Hello, World!";
+const charToCount = "l";
+const occurrences = countChar(text, charToCount);
+console.log(`The character '${charToCount}' occurs ${occurrences} times in '${text}'.`);
