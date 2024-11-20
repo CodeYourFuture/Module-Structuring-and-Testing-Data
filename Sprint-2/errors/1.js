4edeaf5cbc7f29823473f15ea405fdee0505aac1
donarbl <donarablanc@gmail.com>
2024-11-20 10:58:45 +0000

    % corrected

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,13 +1,24 @@
-// Predict and explain first...
 
-// Why will an error occur when this program runs?
-// Try playing computer with the example to work out what is going on
+// // Predict and explain first...
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+// // Why will an error occur when this program runs?
+// // Try playing computer with the example to work out what is going on
+
+// function convertToPercentage(decimalNumber) { // const decimalNumber is declared inside a function, whereas it should be outside of the function and the variable name should be different, otherwise it will give an error, or use the let declaration
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);// here the definition name should be different suchas convertToPercentage
+
+function convertToPercentage(decimalNumber) { 
+  
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
+const example = 0.5;
+console.log(convertToPercentage(example));
+console.log(convertToPercentage(0.23));
