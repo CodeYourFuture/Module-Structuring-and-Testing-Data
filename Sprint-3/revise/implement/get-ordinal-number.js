7303a2a890ec5d964b28764d296e680eba5bfc89
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-20 06:56:06 +0000

    writing getOrdinalNumber function in get-ordinal-number.js file

---
+++ Sprint-3/revise/implement/get-ordinal-number.js
@@ -0,0 +1,9 @@
+function getOrdinalNumber(num) {
+    if (num === 1) return "1st";
+    if (num === 2) return "2nd";
+    if (num === 3) return "3rd";
+    return num + "th"; // For all other numbers
+  }
+  
+  module.exports = getOrdinalNumber;
+  
