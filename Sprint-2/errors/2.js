64d773a0c0c5c65be321e134b805af355deaa216
donarbl <donarablanc@gmail.com>
2024-11-20 11:08:14 +0000

    square parameter

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,17 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+// function square(3) {
+//     return num * num;
+// }
+// there is a value input in the function, thus it throw a SyntaxError, in Javascript a value should not be put in the function definition.
+// we need to define a variable name with a value and then put the variable name into a function
+
+let num = 2
+function square(num) {
     return num * num;
 }
-
+console.log(square(6));
+console.log(square(5));
+console.log(square(num));
 
