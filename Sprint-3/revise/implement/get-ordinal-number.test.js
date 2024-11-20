8e9881360b286d78956148c7f767bf6eabc9d081
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-20 13:41:11 +0000

    Writing test cases for getOrdinalNumber function in get-ordinal-number.test.js file

--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -2,3 +2,43 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+// Import the function to test
+
+// get-ordinal-number.test.js
+
+// Import the function to test
+
+// get-ordinal-number.test.js
+
+// Import the function to test
+
+//Answer:
+
+const getOrdinalNumber = require('./get-ordinal-number');
+
+test('returns "1st" for 1', () => {
+  expect(getOrdinalNumber(1)).toBe('1st');
+});
+
+test('returns "2nd" for 2', () => {
+  expect(getOrdinalNumber(2)).toBe('2nd');
+});
+
+test('returns "3rd" for 3', () => {
+  expect(getOrdinalNumber(3)).toBe('3rd');
+});
+
+test('returns "4th" for 4', () => {
+  expect(getOrdinalNumber(4)).toBe('4th');
+});
+
+test('returns "11th" for 11', () => {
+  expect(getOrdinalNumber(11)).toBe('11th');
+});
+
+
+
+
+
+
