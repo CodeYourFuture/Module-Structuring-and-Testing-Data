6d72bd0ff697d08f9e52f6bed62aac5f535e6879
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-20 19:15:52 +0000

    get-ordinal-number.test.js - created program and jest test

--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -1,3 +1,37 @@
+function getOrdinalNumber(number) {
+    const exceptions = [11, 12, 13]; // Special cases for 11th, 12th, 13th
+    const lastDigit = number % 10;
+
+    if (exceptions.includes(number % 100)) {
+        return number + "th";
+    }
+
+    switch (lastDigit) {
+        case 1:
+            return number + "st";
+        case 2:
+            return number + "nd";
+        case 3:
+            return number + "rd";
+        default:
+            return number + "th";
+    }
+}
+
+test("converts numbers to ordinal numbers", function () {
+    expect(getOrdinalNumber(1)).toBe("1st");
+    expect(getOrdinalNumber(2)).toBe("2nd");
+    expect(getOrdinalNumber(3)).toBe("3rd");
+    expect(getOrdinalNumber(4)).toBe("4th");
+    expect(getOrdinalNumber(11)).toBe("11th");
+    expect(getOrdinalNumber(12)).toBe("12th");
+    expect(getOrdinalNumber(13)).toBe("13th");
+    expect(getOrdinalNumber(21)).toBe("21st");
+    expect(getOrdinalNumber(22)).toBe("22nd");
+    expect(getOrdinalNumber(23)).toBe("23rd");
+    expect(getOrdinalNumber(24)).toBe("24th");
+});
+
 // In this week's prep, we started implementing getOrdinalNumber
 
 // continue testing and implementing getOrdinalNumber for additional cases
--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,52 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+function isPrime(number) {
+    if (number <= 1) return false;
+    if (number === 2) return true;
+    if (number % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
+        if (number % i === 0) return false;
+    }
+    return true;
+}
+
+test('returns false for numbers less than or equal to 1', () => {
+    expect(isPrime(-1)).toBe(false);
+    expect(isPrime(0)).toBe(false);
+    expect(isPrime(1)).toBe(false);
+});
+
+test('returns true for the only even prime number (2)', () => {
+    expect(isPrime(2)).toBe(true);
+});
+
+test('returns false for even numbers greater than 2', () => {
+    expect(isPrime(4)).toBe(false);
+    expect(isPrime(100)).toBe(false);
+});
+
+test('returns true for small prime numbers', () => {
+    expect(isPrime(3)).toBe(true);
+    expect(isPrime(5)).toBe(true);
+    expect(isPrime(7)).toBe(true);
+});
+
+test('returns false for small non-prime numbers', () => {
+    expect(isPrime(9)).toBe(false);
+    expect(isPrime(15)).toBe(false);
+    expect(isPrime(21)).toBe(false);
+});
+
+test('returns true for larger prime numbers', () => {
+    expect(isPrime(29)).toBe(true);
+    expect(isPrime(97)).toBe(true);
+    expect(isPrime(101)).toBe(true);
+});
+
+test('returns false for larger non-prime numbers', () => {
+    expect(isPrime(100)).toBe(false);
+    expect(isPrime(200)).toBe(false);
+    expect(isPrime(999)).toBe(false);
+});
