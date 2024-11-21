1599fca5e4dab214588772a632e9b058c02b7e31
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-21 07:18:11 +0000

    Writing test cases for isPrime function in is-prime.test.js

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -3,16 +3,41 @@
 // Then it should check if the num is prime
 
 //Answer:
-function isPrime(num) {
-    if (num <= 1) {
-        return false; 
-    }
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false; 
-        }
-    }
-    return true; 
-}
+const isPrime = require('./isPrime');
 
-module.exports = isPrime; 
+describe('isPrime function', () => {
+    test('should return false for numbers less than or equal to 1', () => {
+        expect(isPrime(1)).toBe(false);
+        expect(isPrime(0)).toBe(false);
+        expect(isPrime(-5)).toBe(false);
+    });
+
+    test('should return true for the only even prime number (2)', () => {
+        expect(isPrime(2)).toBe(true);
+    });
+
+    test('should return true for odd prime numbers', () => {
+        expect(isPrime(3)).toBe(true);
+        expect(isPrime(5)).toBe(true);
+        expect(isPrime(11)).toBe(true);
+        expect(isPrime(17)).toBe(true);
+    });
+
+    test('should return false for even non-prime numbers greater than 2', () => {
+        expect(isPrime(4)).toBe(false);
+        expect(isPrime(8)).toBe(false);
+        expect(isPrime(10)).toBe(false);
+    });
+
+    test('should return false for odd non-prime numbers', () => {
+        expect(isPrime(9)).toBe(false);
+        expect(isPrime(15)).toBe(false);
+        expect(isPrime(21)).toBe(false);
+    });
+
+    test('should handle large prime and non-prime numbers', () => {
+        expect(isPrime(101)).toBe(true); // Large prime
+        expect(isPrime(100)).toBe(false); // Large non-prime
+    });
+});
+
