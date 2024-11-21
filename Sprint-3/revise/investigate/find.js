928e9728ec48e2b0b4e0d4b4e195860537c310d2
donarbl <donarablanc@gmail.com>
2024-11-21 21:25:31 +0000

    test find

--- Sprint-3/revise/investigate/find.js
+++ Sprint-3/revise/investigate/find.js
@@ -20,6 +20,10 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+// answer: the index starts from 0 and increases by 1 each time the loop runs, until it reaches the character or goes through the whole string 
 // b) What is the if statement used to check
+// answer: checks that each character in the string and returns the index of the 1st character that matches the char
 // c) Why is index++ being used?
+// answer: index ++ is used to increment the index value by 1 every iteration
 // d) What is the condition index < str.length used for?
+// answer: it is checking all the characters in the string depending on the length.
