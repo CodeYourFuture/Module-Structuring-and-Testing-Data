e129047613baa4c72588dfd2aaa57bf27c7c51bf
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-21 08:09:45 +0000

    Writing test cases for isValidPassword function in password-validator.test.js file

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,38 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+//Answer:
+const isValidPassword = require('./password-validator'); // Updated path
+
+describe('Password Validation', () => {
+    const previousPasswords = ['Password1!', '12345$', 'HelloWorld123'];
+
+    test('Valid password', () => {
+        expect(isValidPassword('Valid1!', previousPasswords)).toBe(true);
+    });
+
+    test('Password too short', () => {
+        expect(isValidPassword('Pa1!', previousPasswords)).toBe(false);
+    });
+
+    test('Missing uppercase letter', () => {
+        expect(isValidPassword('valid1!', previousPasswords)).toBe(false);
+    });
+
+    test('Missing lowercase letter', () => {
+        expect(isValidPassword('VALID1!', previousPasswords)).toBe(false);
+    });
+
+    test('Missing number', () => {
+        expect(isValidPassword('Pass!', previousPasswords)).toBe(false);
+    });
+
+    test('Missing special character', () => {
+        expect(isValidPassword('Valid12', previousPasswords)).toBe(false);
+    });
+
+    test('Already used password', () => {
+        expect(isValidPassword('Password1!', previousPasswords)).toBe(false);
+    });
+});
+
