2ce2bbaef8a43b333a36427f5613cebb77af6ee3
Amir200524 <amiraminia@yahoo.com>
2024-11-21 19:32:05 +0000

    java to javascript

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -1,13 +1,12 @@
-public class AngleType {
-
-    public static String getAngleType(int angle) {
-        if (angle == 90) {
+class AngleType {
+    static getAngleType(angle) {
+        if (angle === 90) {
             return "Right angle";
         } else if (angle < 90) {
             return "Acute angle";
         } else if (angle > 90 && angle < 180) {
             return "Obtuse angle";
-        } else if (angle == 180) {
+        } else if (angle === 180) {
             return "Straight angle";
         } else if (angle > 180 && angle < 360) {
             return "Reflex angle";
@@ -15,15 +14,14 @@ public class AngleType {
             return "Invalid angle";
         }
     }
-
-    public static void main(String[] args) {
-        // Test cases
-        System.out.println(getAngleType(45));    // Output: Acute angle
-        System.out.println(getAngleType(90));    // Output: Right angle
-        System.out.println(getAngleType(120));   // Output: Obtuse angle
-        System.out.println(getAngleType(180));   // Output: Straight angle
-        System.out.println(getAngleType(270));   // Output: Reflex angle
-        System.out.println(getAngleType(360));   // Output: Invalid angle
-    }
 }
+
+// Test cases
+console.log(AngleType.getAngleType(45));    // Output: Acute angle
+console.log(AngleType.getAngleType(90));    // Output: Right angle
+console.log(AngleType.getAngleType(120));   // Output: Obtuse angle
+console.log(AngleType.getAngleType(180));   // Output: Straight angle
+console.log(AngleType.getAngleType(270));   // Output: Reflex angle
+console.log(AngleType.getAngleType(360));   // Output: Invalid angle
+
 
