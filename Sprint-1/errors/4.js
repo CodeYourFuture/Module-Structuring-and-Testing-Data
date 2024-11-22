384e30983c40b1c22e5f4e02c58addeef0347597
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-22 20:21:38 +0000

    4.js - added extra comment.

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -3,3 +3,4 @@ const $24hourClockTime = "08:53";
 
 // variable names cannot start with numbers, they can only start with a letter, underscore or dollar sign.
 // fixed by adding dollar sign before variable.
+// 12HourClockTime should only show 12 hours format therefore 20:53 is incorrect.
