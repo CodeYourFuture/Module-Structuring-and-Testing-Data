02f6bae4789da2691e22dff6da64c4f7c4be89d2
Serna Malala <sm.sernamalala@gmail.com>
2024-11-22 13:20:27 +0200

    Created a function to return values based on the card value and a test fiile to track the different cases

---
+++ Sprint-3/implement/get-card-value.test.js
@@ -0,0 +1,62 @@
+
+const getCardValue = require("./get-card-value");
+const currentOutput = getCardValue("5♠");
+const targetOutput = 5;
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput).toEqual(targetOutput);
+});
+
+const currentOutput2 = getCardValue("J♦");
+const targetOutput2 = 10;
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput2).toEqual(targetOutput2);
+});
+
+const currentOutput3 = getCardValue("K♦");
+const targetOutput3 = 10;
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput3).toEqual(targetOutput3);
+});
+
+const currentOutput4 = getCardValue("Q♦");
+const targetOutput4 = 10;
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput4).toEqual(targetOutput3);
+});
+
+const currentOutput5 = getCardValue("J♦");
+const targetOutput5 = 10;
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput5).toEqual(targetOutput5);
+});
+
+const currentOutput6 = getCardValue("A♦");
+const targetOutput6 = 11;
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput6).toEqual(targetOutput6);
+});
+
+const currentOutput7 = getCardValue("A♣");
+const targetOutput7 = 11;
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(currentOutput7).toEqual(targetOutput7);
+});
+
+
+test("This will be used to check if all cases are accomodated for when looking at a deck of cards", () => {
+    expect(() => getCardValue("Z♣")).toThrowError(new Error("Invalid card rank."));
+
+    // try {
+    //     getCardValue("Z♣");
+    // }
+    // catch (error) {
+    //     console.log(error);
+    //     expect(error.message).toEqual("Invalid card rank.");
+    // }
+});
