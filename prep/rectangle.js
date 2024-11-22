161f11b53c2ef4d5390e9f690b4b880e9737fcd3
apachitech <72330143+apachitech@users.noreply.github.com>
2024-11-22 11:42:18 +0200

    sprint 3 exercises

---
+++ prep/rectangle.js
@@ -0,0 +1,10 @@
+// rectangle.js
+
+const height = 10;
+const width = 30;
+
+const area = height * width;
+const perimeter = 2* (height + width);
+
+console.log("area", area);
+console.log("perimeter", perimeter);
