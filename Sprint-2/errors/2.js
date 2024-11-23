5d21e03b0b8bb1bf99b7a385577e5ebaf43d371f
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-23 19:56:29 +0000

    square funciton called and updated its parameter

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,10 +3,10 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
-
+console.log(square(3))
 /*
 Variable name can not be numberic . It must be a correct identifier to recive andy number and make a square.
 */
