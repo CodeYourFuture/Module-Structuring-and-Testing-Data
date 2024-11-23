0838f10a45cb8bf77058fbba206780336ef9546a
HaftamuKebedew <haftamukebedew@gmail.com>
2024-11-23 19:23:34 +0000

    function call in console.log  to check the output

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -7,7 +7,7 @@ function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
+console.log(capitalise("code your future"))
 /*
  SyntaxError will be diplayed  because of str varibale redecaration. it is declared twice one as parameter and another one as variable in side function.
 */
