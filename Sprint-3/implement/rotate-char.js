9a9e2054039417f418be236e3bad21715088dbe8
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-23 10:54:53 +0000

    rotate-char-completed

--- Sprint-3/implement/rotate-char.js
+++ Sprint-3/implement/rotate-char.js
@@ -41,3 +41,21 @@ console.log(rotateCharacter("7", 5)); // Output: "7" (unchanged, not a letter)
 // And the function should return the rotated character as a string (e.g., 'z' rotated by 3 should become 'c', 'Z' rotated by 3 should become 'C').
 console.log(rotateCharacter("z", 1)); // Output: "a" (preserves case, but wraps around)
 console.log(rotateCharacter("Y", 2)); // Output: "A" (preserves case, but wraps around)
+
+function rotateCharacter(char, shift) {
+  if (char >= "a" && char <= "z") {
+    const base = "a".charCodeAt(0);
+    const rotatedChar = String.fromCharCode(
+      ((char.charCodeAt(0) - base + shift) % 26) + base
+    );
+    return rotatedChar;
+  } else if (char >= "A" && char <= "Z") {
+    const base = "A".charCodeAt(0);
+    const rotatedChar = String.fromCharCode(
+      ((char.charCodeAt(0) - base + shift) % 26) + base
+    );
+    return rotatedChar;
+  }
+
+  return char;
+}
