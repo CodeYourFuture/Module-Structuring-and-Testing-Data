8b5817cadde162f47ed26aedddd32e7be0577cd1
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-11-24 19:50:50 +0200

    added assertion tests

--- Sprint-3/implement/get-card-value.js
+++ Sprint-3/implement/get-card-value.js
@@ -31,12 +31,15 @@
 // Then it should throw an error indicating "Invalid card rank."
 
 function getCardValue(card) {
+  // Here I am getting the value input and slicing it to get only the numerical value
   let value = card.slice(0, -1);
 
+  // Here I am declaring the cardNumber, faceCard and ace the ace values as per given instructions
   let cardNumber = ["2", "3", "4", "5", "6", "7", "8", "9"];
   let faceCard = ["10", "J", "K", "Q"];
   let ace = "A";
 
+  // this below code will change the string value to int using the parseInt function
   if (cardNumber.includes(value)) {
     return parseInt(value);
   }
@@ -49,8 +52,24 @@ function getCardValue(card) {
     return 11;
   }
 
+  // Here I am catching the error if user input is not part of the defined values
   if (!!cardNumber || !!faceCard || !!ace) {
     return "Error: Invalid card value";
   }
 }
 console.log(getCardValue("16♠"));
+
+// the following are assertion test cases to check if the function is working correctly with different values
+// Valid cases
+console.assert(getCardValue("7♠") === 7, "Test Failed: 7♠ should return 7");
+console.assert(getCardValue("10♣") === 10, "Test Failed: 10♣ should return 10");
+console.assert(getCardValue("Q♦") === 10, "Test Failed: Q♦ should return 10");
+console.assert(getCardValue("A♥") === 11, "Test Failed: A♥ should return 11");
+
+// Edge cases
+console.assert(getCardValue("2♠") === 2, "Test Failed: 2♠ should return 2");
+console.assert(getCardValue("K♦") === 10, "Test Failed: K♦ should return 10");
+
+// Invalid cases
+console.assert(getCardValue("Q♦") === 11, "Test Failed: Q♦ should return 10"); // deliberately made it wrong to check if the functions is working
+console.assert(getCardValue("A♥") === 11, "Test Failed: A♥ should return 11");
