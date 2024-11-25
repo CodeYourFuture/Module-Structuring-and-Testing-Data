4e08e2a4787c1a897ae3d36a2f28913244d8d8e5
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-25 16:58:13 +0000

    Amend/Sprint-3/implement/get-ordinal-number.test.js

--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -17,28 +17,22 @@
 
 const getOrdinalNumber = require('./get-ordinal-number');
 
-test('returns "1st" for 1', () => {
-  expect(getOrdinalNumber(1)).toBe('1st');
+test('returns correct ordinal for single-digit numbers', () => {
+    expect(getOrdinalNumber(1)).toBe('1st');
+    expect(getOrdinalNumber(2)).toBe('2nd');
+    expect(getOrdinalNumber(3)).toBe('3rd');
+    expect(getOrdinalNumber(4)).toBe('4th');
 });
 
-test('returns "2nd" for 2', () => {
-  expect(getOrdinalNumber(2)).toBe('2nd');
+test('handles special cases like 11, 12, and 13', () => {
+    expect(getOrdinalNumber(11)).toBe('11th');
+    expect(getOrdinalNumber(12)).toBe('12th');
+    expect(getOrdinalNumber(13)).toBe('13th');
 });
 
-test('returns "3rd" for 3', () => {
-  expect(getOrdinalNumber(3)).toBe('3rd');
+test('handles numbers greater than 20 correctly', () => {
+    expect(getOrdinalNumber(21)).toBe('21st');
+    expect(getOrdinalNumber(22)).toBe('22nd');
+    expect(getOrdinalNumber(23)).toBe('23rd');
+    expect(getOrdinalNumber(24)).toBe('24th');
 });
-
-test('returns "4th" for 4', () => {
-  expect(getOrdinalNumber(4)).toBe('4th');
-});
-
-test('returns "11th" for 11', () => {
-  expect(getOrdinalNumber(11)).toBe('11th');
-});
-
-
-
-
-
-
