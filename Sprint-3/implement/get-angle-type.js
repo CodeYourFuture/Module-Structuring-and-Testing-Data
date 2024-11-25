c307773fefbeac664c0ad93bba6e9fcde9181cbd
Simpra11 <mrmabuya11@gmail.com>
2024-11-26 01:33:27 +0200

    Updated get-angle-type code

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -6,24 +6,16 @@
 // When the function getAngleType is called with this angle,
 // Then it should:
 
-function getAngleType(degrees){
-    
-    if(degrees === 90){
-        return 'Right angle';
-    }else if(degrees > 0 && degrees < 90){
-        return 'Acute angle';
-    }else if(degrees > 90 && degrees < 180){
-        return 'Obtuse angle';
-    }else if(degrees === 180){
-        return 'Straight angle';
-    }else if(degrees > 180 && degrees < 360){
-        return 'Reflex angle';
-    }else {
-        return 'Invalid input';
-    }
+function getAngleType(angle) {
+    if (angle === 90) return "Right angle";
+    if (angle < 90) return "Acute angle";
+    if (angle > 90 && angle < 180) return "Obtuse angle";
+    if (angle === 180) return "Straight angle";
+    if (angle > 180 && angle < 360) return "Reflex angle";
+    return "Invalid angle";
 }
 
-module.exports = getAngleType;
+module.exports = getAngleType; 
 
 console.log(getAngleType(359));
 
