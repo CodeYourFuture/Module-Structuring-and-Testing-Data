4da681e19a70f8d949636ee4b48b6531f876bd44
samirahekmati <samirahekmati@gmail.com>
2024-11-25 17:14:22 +0000

    repeat : testing with jest

--- Sprint-3/revise/implement/repeat.test.js
+++ Sprint-3/revise/implement/repeat.test.js
@@ -23,3 +23,39 @@
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+
+
+function repeat(string, num){
+
+    let tempString = "";
+    if(num < 0){
+        throw new Error("number is less than zero");
+
+    }
+    for( let i = 0; i < num; i++ ){
+        tempString += string;
+    }
+    return tempString
+}
+
+console.log(repeat("hello", -2))
+
+
+describe("repeat function", () =>{
+    test('repeat String:', () => {
+        expect(repeat("string",2)).toBe("stringstring")
+    })
+
+    test('handle Count of 1', () => {
+        expect(repeat("string",1)).toBe("string")
+    })
+
+    test('Handle Count of 0:', () => {
+        expect(repeat("string",0)).toBe("")
+    })
+
+    test('Negative Count:', () => {
+        expect(repeat("string", -1)).toThrow("error: number is less than zero")
+    })
+}
+)
