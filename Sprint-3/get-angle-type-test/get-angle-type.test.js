e3cb72136346a7eb3a63f98b3c2cc8c0bd8cd83b
Simpra11 <mrmabuya11@gmail.com>
2024-11-25 23:23:11 +0200

    Created get-angle-type test

---
+++ Sprint-3/get-angle-type-test/get-angle-type.test.js
@@ -0,0 +1,10 @@
+
+const getAngleType = require("./get-angle-type");
+
+test("It checks angle types", function () {
+    expect(getAngleType(90)).toBe("Right angle");
+    expect(getAngleType(45)).toBe("Acute angle");
+    expect(getAngleType(135)).toBe("Obtuse angle");
+    expect(getAngleType(180)).toBe("Straight angle");
+    expect(getAngleType(270)).toBe("Reflex angle");
+});
