ec14385d1e3fafc9772f872e1dbfacb7cd883397
Pooriya Ketabi <pooriya.ketabi@gmail.com>
2024-11-25 14:11:34 +0000

    errors 2.js done

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,12 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
+console.log(square(3))
 
+
+// It will return syntax error here, because the function parameter is value, which must be variable name instead.
+// we can replace "3" as the function parameter with variable "num", and then call the function in the last line and define value for variable "num"
