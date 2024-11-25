73d03e4e2bcb7fe083009458e2d2e6dcf0eb4d70
Pooriya Ketabi <pooriya.ketabi@gmail.com>
2024-11-25 16:02:12 +0000

    implement to-pounds.js done

--- Sprint-2/implement/to-pounds.js
+++ Sprint-2/implement/to-pounds.js
@@ -4,3 +4,15 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+
+function toPounds(penseString) {
+    const penseStringWithoutTrailing = penseString.slice(0,penseString.length -1);
+    const penseStringFormatCheck = penseStringWithoutTrailing.padStart(3, "0");
+    const pound = penseStringFormatCheck.slice(0, penseStringFormatCheck.length -2);
+    const pense = penseStringFormatCheck.slice(penseStringFormatCheck.length -2);
+
+    return `£${pound}.${pense}`;
+}
+
+console.log(toPounds("123456p"));
