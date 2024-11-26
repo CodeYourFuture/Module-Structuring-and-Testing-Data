749cdef4b1a25236b604aa6455191d455111202e
Serna Malala <sm.sernamalala@gmail.com>
2024-11-26 18:03:12 +0200

    Created a test file for the password validator

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,52 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+
+const passwordValidator = require("./password-validator");
+
+const currentOutput = passwordValidator("Serna#2003");
+const targetOutput = true;
+
+test('This test will check if the password is valid according to the criteria given', () => {
+    expect(currentOutput).toEqual(targetOutput);
+})
+
+// short password
+const currentOutput1 = passwordValidator("S#2");
+const targetOutput1 = false;
+
+test('This test will check if the password is invalid because it is too short', () => {
+    expect(currentOutput1).toEqual(targetOutput1);
+});
+
+// no special character
+const currentOutput2 = passwordValidator("Serna2003");
+const targetOutput2 = false;
+
+test('This test will check if the password is invalid because it is missing a special character', () => {
+    expect(currentOutput2).toEqual(targetOutput2);
+});
+
+// no number
+const currentOutput3 = passwordValidator("Serna#");
+const targetOutput3 = false;
+
+test('This test will check if the password is invalid because it is missing a number', () => {
+    expect(currentOutput3).toEqual(targetOutput3);
+});
+
+// password is already in array
+const currentOutput4 = passwordValidator("Serna#2003");
+const targetOutput4 = false;
+
+test('This test will check if the password is invalid because it was already used', () => {
+    expect(currentOutput4).toEqual(targetOutput4);
+});
+
+// no uppercase
+const currentOutput5 = passwordValidator("serna#2003");
+const targetOutput5 = false;
+
+test('This test will check if the password is invalid because it is missing an uppercase letter', () => {
+    expect(currentOutput5).toEqual(targetOutput5);
+});
