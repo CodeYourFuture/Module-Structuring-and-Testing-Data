14cfc68bb6bf9b520710aed7b53149a83a495474
Simpra11 <mrmabuya11@gmail.com>
2024-11-27 19:44:33 +0200

    Created a countChar test file

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -1,5 +1,23 @@
 // implement a function countChar that counts the number of times a character occurs in a string
 
+const countChar = require("./countChar");
+
+
+
+test("Checks for Multiple Occurrences, then correctly count overlapping occurrences", () => {
+    const currentOUtput = countChar("azania", "a");
+    const targetOutput = 3;
+
+    expect(currentOUtput).toEqual(targetOutput);
+});
+
+test("Checks for No Occurrences, if the character does not exist, it should return 0", () => {
+    const currentOUtput = countChar("azania", "u");
+    const targetOutput = 0;
+
+    expect(currentOUtput).toEqual(targetOutput);
+});
+
 // Given a string str and a single character char to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
