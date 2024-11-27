4a48765fd0e6680d3a2d00c38efc9e6155f3c1ff
Karla G. <k.grajalesluna@gmail.com>
2024-11-27 15:30:46 +0000

    done 1.js

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,13 +1,35 @@
 // Predict and explain first...
 
-// Why will an error occur when this program runs?
-// Try playing computer with the example to work out what is going on
+/* 
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  Why will an error occur when this program runs? 
+  Try playing computer with the example to work out what is going on
+
+  1. The function won't run because we are not invoking the function,
+  2. we have been declared one variable with const and this can not reassigned after, const decimalNumber also has a fixed value
+  3. To work efficiently we should assign the parameter =+ inside the function and declare the var decimalNumber
+  4. we call the function correctly with its respective argument
+
+    function convertToPercentage(decimalNumber) {
+      const decimalNumber = 0.5;
+      const percentage = `${decimalNumber * 100}%`;
+
+      return percentage;
+    }
+
+    console.log(decimalNumber);
+*/
+
+
+/*================== fixed =======================*/
+
+function convertToPercentage(num) {
+  const decimalNumber = num;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(.2)); //20%
+
+
