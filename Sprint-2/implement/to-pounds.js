54b84aa5558c54baeab8df5f03c5e7769443a07b
sam <cleversam07@gmail.com>
2024-11-27 14:30:00 +0000

    feat: create reusable toPounds function with test cases

--- Sprint-2/implement/to-pounds.js
+++ Sprint-2/implement/to-pounds.js
@@ -4,3 +4,20 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+// Function to convert kilograms to pounds
+function toPounds(kg) {
+    // 1 kg = 2.20462 pounds
+    const pounds = kg * 2.20462;
+  
+    // Return the result
+    return pounds;
+  }
+  
+  // Test cases
+  console.log(toPounds(1));    // Expected output: 2.20462
+  console.log(toPounds(5));    // Expected output: 11.0231
+  console.log(toPounds(100));  // Expected output: 220.462
+  console.log(toPounds(0.5));  // Expected output: 1.10231
+  console.log(toPounds(75));   // Expected output: 165.3465
+  
