ee1141e3638fb3db6fe22b77df6d9886c1bda0d7
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-11-27 05:41:19 +0200

    added the function code to count a character in a given string

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -15,3 +15,21 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+function countChar(str, char) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return `${char} appears ${count} times in "${str}"`;
+}
+
+console.log(countChar("laka", "a")); // a appears 2 times in "laka"
+console.log(countChar("luke", "u")); // u appears 1 times in "luke"
+console.log(countChar("hello", "l")); // l appears 2 times in "hello"
+console.log(countChar("banana", "a")); // a appears 3 times in "banana"
+console.log(countChar("abc", "z")); // z appears 0 times in "abc"
+console.log(countChar("Mississippi", "s")); // s appears 4 times in "Mississippi"
