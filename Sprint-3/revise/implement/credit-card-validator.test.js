522a45a828a65874b74b48d7db82cc4db54e31f3
Simpra11 <mrmabuya11@gmail.com>
2024-11-27 15:52:56 +0200

    Created a few tests for to check my function works well

--- Sprint-3/revise/implement/credit-card-validator.test.js
+++ Sprint-3/revise/implement/credit-card-validator.test.js
@@ -1,6 +1,16 @@
 const creditCardValidator = require("./creditCardValidator");
 
-const currentOUtput = creditCardValidator("1243578569873476");
-const targetOutput = true;
 
-test("Checks if ")
+test("Checks if the number is 16 digits and all are numbers, at least two different digits are represented adn the sum of all digits is greater than 16", () => {
+    const currentOUtput = creditCardValidator("1243578569873476");
+    const targetOutput = true;
+
+    expect(currentOUtput).toEqual(targetOutput)
+});
+
+test("Checks if the final digit is even", () => {
+    const currentOUtput = creditCardValidator("1243578569873479");
+    const targetOutput = false;
+
+    expect(currentOUtput).toEqual(targetOutput)
+});
