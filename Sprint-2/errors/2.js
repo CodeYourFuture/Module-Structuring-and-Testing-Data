f77a0be5a87e9e0f2e18a215412906986751f099
Karla G. <k.grajalesluna@gmail.com>
2024-11-27 15:30:58 +0000

    done 2.js

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,30 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+
+/* 
+    parameter are in the function, in this example we are not receiving any parameter and this we can not write the 
+    argument directly, 
+
+    arguments are in the console.log and we are not calling the function so that's why we are not having any response.
+
+    function square(3) {
+    return num * num;
+
+    }
+
+*/
+
+/*================== fixed =======================*/
+
+
+function square(num) {
     return num * num;
 }
+
+console.log(square(7))
+
+
+
 
 
