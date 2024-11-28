81473f6e53ac8111ecf35af0fe6ac7d229076411
Millena28 <millenakiflom06@gmail.com>
2024-11-28 06:26:46 +0000

    sprint #3 implement and revise exercise

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,54 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+//Then it should:
+// - Return true if num is prime (only divisible by 1 and itself)
+// - Return false if num is not prime
+// - Return false for num <= 1 since they are not prime numbers
+// - Handle invalid inputs by returning false
+
+function isPrime(num) {
+  if (!Number.isInteger(num) || num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+describe('isPrime', () => {
+  test('returns true for prime numbers', () => {
+    expect(isPrime(2)).toBe(true);
+    expect(isPrime(3)).toBe(true); 
+    expect(isPrime(5)).toBe(true);
+    expect(isPrime(7)).toBe(true);
+    expect(isPrime(11)).toBe(true);
+    expect(isPrime(13)).toBe(true);
+  });
+
+  test('returns false for non-prime numbers', () => {
+    expect(isPrime(4)).toBe(false);
+    expect(isPrime(6)).toBe(false);
+    expect(isPrime(8)).toBe(false);
+    expect(isPrime(9)).toBe(false);
+    expect(isPrime(10)).toBe(false);
+  });
+
+  test('returns false for numbers <= 1', () => {
+    expect(isPrime(1)).toBe(false);
+    expect(isPrime(0)).toBe(false);
+    expect(isPrime(-1)).toBe(false);
+  });
+
+  test('returns false for invalid inputs', () => {
+    expect(isPrime('2')).toBe(false);
+    expect(isPrime(null)).toBe(false);
+    expect(isPrime(undefined)).toBe(false);
+    expect(isPrime(3.14)).toBe(false);
+  });
+});
--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,75 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+
+
+
+// List of previously used passwords
+const passwords = ["My123*", "password1", "CYF2024!"];
+
+function isValidPassword(password) {
+
+  // Check if password is at least 5 characters long
+  if (password.length < 5) {
+    return false;
+  }
+
+  let hasUppercase = false;
+  let hasLowercase = false;
+  let hasNumber = false;
+  let hasSpecialChar = false;
+
+  // Loop through each character in the password
+  for (let i = 0; i < password.length; i++) {
+    let char = password[i];
+
+    // Check if the password has an uppercase letter
+    if (char >= 'A' && char <= 'Z') {
+      hasUppercase = true;
+    }
+    // Check if the password has an lowercase letter
+    else if (char >= 'a' && char <= 'z') {
+      hasLowercase = true;
+    }
+    // Check if the password has a number
+    else if (char >= '0' && char <= '9') {
+      hasNumber = true;
+    }
+    // Check if the password has a special character
+    else if (['!', '#', '$', '%', '.', '*', '&'].includes(char)) {
+      hasSpecialChar = true;
+    }
+  }
+
+  // If any required condition is not met, return false
+  if (!hasUppercase || !hasLowercase || !hasNumber || !hasSpecialChar) {
+    return false;
+  }
+
+  // Check if the password has been used before
+  if (passwords.includes(password)) {
+    return false;
+  }
+
+  // If all checks pass, the password is valid
+  return true;
+}
+
+// Test the function
+let testPassword = "NewPassword1!";
+console.log(isValidPassword(testPassword)); // true 
+
+testPassword = "My123*";
+console.log(isValidPassword(testPassword)); // false, previously used 
+
+testPassword = "Ne1*";
+console.log(isValidPassword(testPassword)); // false, only has 4 characters 
+
+testPassword = "myword1!"; 
+console.log(isValidPassword(testPassword)); // //false, does not include uppercase 
+
+testPassword = "NewPassword!";
+console.log(isValidPassword(testPassword)); // false, does not include number 
+
+testPassword = "NewPassword1";
+console.log(isValidPassword(testPassword)); // false, does not include special character 
