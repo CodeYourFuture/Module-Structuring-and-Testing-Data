3eabd5f10ee983ccb8d0448639dc69efa6428311
Simpra11 <mrmabuya11@gmail.com>
2024-11-29 15:10:27 +0200

    Created a password validator function file and got to work properly

---
+++ Sprint-3/revise/implement/passwordValidator.js
@@ -0,0 +1,26 @@
+function passwordValidator(password, passwords){
+
+    if(password.length < 5){
+        return "Password invalid";
+     }
+
+     if(passwords.includes(password)){
+        return "Password is already used"
+     }else {
+        passwords.push(password);
+        console.log("Password added successfully!")
+        console.log("Updated passwords array:", passwords)
+     }
+
+     const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!#@$%&*.]).+$/;
+
+     if(regex.test(password)){
+        return "Password valid";
+     }else{
+        return "Password invalid";
+     }
+}
+
+let  passwords = ["Si@#M3.pro!*z4"];
+
+console.log(passwordValidator("Bad@11.#You%", passwords));
