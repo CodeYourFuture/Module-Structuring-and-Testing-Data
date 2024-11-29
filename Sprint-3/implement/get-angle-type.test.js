45c28db856d43b07130efe90c5c5a4ec37bde727
rawrnoodles <matthew.lt.law@hotmail.com>
2024-11-29 22:28:22 +0000

    get-angle-type.test.js - broke down tests to be more easily readable.

--- Sprint-3/implement/get-angle-type.test.js
+++ Sprint-3/implement/get-angle-type.test.js
@@ -1,24 +1,39 @@
 const { getAngleType } = require('./get-angle-type');
 
-test("Test to see degrees will match correct angle type", function() {
+test("Test to see degrees will match Right angle", function() {
     expect(getAngleType(90)).toEqual("Right angle");
+});
+
+test("Test to see degrees will match Acute angle", function () {
     expect(getAngleType(45)).toBe("Acute angle");
     expect(getAngleType(89)).toBe("Acute angle");
-    expect(getAngleType(120)).toBe("Obtuse angle");
-    expect(getAngleType(150)).toBe("Obtuse angle");
-    expect(getAngleType(180)).toBe("Straight angle");
-    expect(getAngleType(200)).toBe("Reflex angle");
-    expect(getAngleType(359)).toBe("Reflex angle");
     expect(getAngleType(0)).toBe("Acute angle");
     expect(getAngleType(89.999)).toBe("Acute angle");
+});
+    
+test("Test to see degrees will match Obtuse angle", function() {
+    expect(getAngleType(120)).toBe("Obtuse angle");
+    expect(getAngleType(150)).toBe("Obtuse angle");
     expect(getAngleType(90.001)).toBe("Obtuse angle");
     expect(getAngleType(179.999)).toBe("Obtuse angle");
+});
+
+test("Test to see degrees will match Straight angle", function() {
+    expect(getAngleType(180)).toBe("Straight angle");
+});
+
+test("Test to see degrees will match Reflex angle", function() {
+    expect(getAngleType(200)).toBe("Reflex angle");
+    expect(getAngleType(359)).toBe("Reflex angle");
     expect(getAngleType(180.001)).toBe("Reflex angle");
     expect(getAngleType(359.999)).toBe("Reflex angle");
+});
+    
+test("Test to see if angles are invalid", function() {
     expect(getAngleType(-1)).toBe("Invalid angle");
-    expect(getAngleType(361)).toBe("Invalid angle");
-});    
-
+    expect(getAngleType(361)).toBe("Invalid angle");;
+});
+  
 // Implement a function getAngleType
 
 // Acceptance criteria:
