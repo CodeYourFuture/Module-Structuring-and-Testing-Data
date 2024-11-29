6dab22fb53156e81875d59230dfb461c35c9b320
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-11-29 19:08:42 +0200

    added code to validate a password

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,47 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+
+function isPasswordValid(password, prePasswords) {
+  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!#$%.*&]).{5,}$/;
+
+  if (!regex.test(password)) {
+    return false;
+  }
+
+  if (prePasswords.includes(password)) {
+    return false;
+  }
+
+  return true;
+}
+const prePasswords = ["OldPass1!", "123abc!", "Password$"];
+
+console.assert(
+  isPasswordValid("Lu@#2024", prePasswords) === true,
+  "Test Case 1 Failed"
+);
+console.assert(
+  isPasswordValid("OldPass1!", prePasswords) === false,
+  "Test Case 2 Failed"
+);
+console.assert(
+  isPasswordValid("Lu@1!", prePasswords) === true,
+  "Test Case 3 Failed"
+);
+console.assert(
+  isPasswordValid("Lu12345", prePasswords) === true,
+  "Test Case 4 Failed"
+);
+console.assert(
+  isPasswordValid("Luuu@#", prePasswords) === true,
+  "Test Case 5 Failed"
+);
+console.assert(
+  isPasswordValid("lu@#2024", prePasswords) === true,
+  "Test Case 6 Failed"
+);
+console.assert(
+  isPasswordValid("LU@#2024", prePasswords) === true,
+  "Test Case 7 Failed"
+);
