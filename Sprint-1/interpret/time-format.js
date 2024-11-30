5bf59ba489dd8fda0c767bfc654c11d3e146c7a6
azaralampanah2 <azaralampanah2@gmail.com>
2024-11-30 14:18:06 +0000

    Revert "complete sprint3/implement/get-card.js"
    This reverts commit ceab518ec03317de5b9387bab6633b4f6935988c.

--- Sprint-1/interpret/time-format.js
+++ Sprint-1/interpret/time-format.js
@@ -1,4 +1,4 @@
-const movieLength = -1; // length of movie in seconds
+const movieLength = 8784; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -12,18 +12,13 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
-//6
+
 // b) How many function calls are there?
-//1
+
 // c) Using documentation, explain what the expression movieLength % 60 represents
-//it divides movielength by 60 and then returns the remainder . this remainder will be the movie length in seconds
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-//it takes away remaining seconds from the the movielenght and devids the result by 60 to get the movielength in minutes
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-//it retains the movielenght in the format of hour:minutes:seconds
-//we can use some other names insted of result, such as "movieduration"
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-//I tried these codes with different value of movielength(all numbers withou remainder and with remainder) and all worked properly
--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,7 +6,3 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-//this program gets two numbers, which are 10 and 32,add them up together. Then on the console it will show
-//" The sum of 10 and 32 is 32"
-//after running codes I can see the same as prediction
