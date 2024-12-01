2501ef37259f7e12caa03889889020ac39567cb7
rawrnoodles <matthew.lt.law@hotmail.com>
2024-12-01 20:31:56 +0000

    is-prime-test.js - storing the value of Math.sqrt(number) in a variable and referring to the variable in your loop

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -6,7 +6,8 @@ function isPrime(number) {
     if (number <= 1) return false;
     if (number === 2) return true;
     if (number % 2 === 0) return false;
-    for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    const sqrtNumber = Math.sqrt(number);
+    for (let i = 3; i <= sqrtNumber; i += 2) {
         if (number % i === 0) return false;
     }
     return true;
