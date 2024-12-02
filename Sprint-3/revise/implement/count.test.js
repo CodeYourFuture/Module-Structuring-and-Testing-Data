dd7ee8ae6c40a5c82fa0730d9ea5940c80fb6ce5
Sajad <sajadsoorni@gmail.com>
2024-12-02 13:56:49 +0000

    Remove outdated lines of code

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -15,9 +15,6 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
-//---------------------------------------------------------------------------------------------------
-
-//// Converts a number into its English word representation
 
 function countChar(string, charToCount) {
   const count = string.split("").filter((char) => char === charToCount).length;
