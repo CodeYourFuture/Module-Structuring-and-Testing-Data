aa737d06a09f5996960b1e3d0b78c3b4849df238
AFatmaa <fatma.arslantas@outlook.com>
2024-12-02 18:52:14 +0000

    Updated isPrime function

--- Sprint-3/revise/implement/is-prime.js
+++ Sprint-3/revise/implement/is-prime.js
@@ -3,9 +3,18 @@ function isPrime(num) {
     // Check if the input is less than 2, which is not prime
     if (num < 2) return false;
 
-    // Loop from 2 to the square root of the number
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        // If num is divisible by any number in this range, it's not prime
+    // If the number is 2, it's prime
+    if (num === 2) return true;
+
+    // If the number is even and greater than 2, it's not prime
+    if (num % 2 === 0) return false;
+
+    // Calculate square root of the number once
+    const sqrtNum = Math.sqrt(num);
+
+    // Loop from 3 to the square root of the number, check only odd numbers
+    for (let i = 3; i <= sqrtNum; i += 2) {
+        // If num is divisible by any odd number in this range, it's not prime
         if (num % i === 0) return false;
     }
 
