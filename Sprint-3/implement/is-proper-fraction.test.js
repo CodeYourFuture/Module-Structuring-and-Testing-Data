3e07cb1a0609ca6f44b41e9c28c99d3df10270ea
Serna Malala <sm.sernamalala@gmail.com>
2024-12-02 22:00:00 +0200

    Added a couple more test cases that are appropriate for testing the function

--- Sprint-3/implement/is-proper-fraction.test.js
+++ Sprint-3/implement/is-proper-fraction.test.js
@@ -16,4 +16,25 @@ test('This will test if a proper fraction is tested properly', () => {
 
 test('This will test for division by zero', () => {
     expect(() => isProperFraction(5, 0)).toThrow("Denominator cannot be zero");
-}) 
+})
+
+const currentOutput3 = isProperFraction(-2, 9);
+const targetOutput3 = true;
+
+test('This will test if a proper fraction is tested properly', () => {
+    expect(currentOutput3).toEqual(targetOutput3);
+})
+
+const currentOutput4 = isProperFraction(4, 4);
+const targetOutput4 = false;
+
+test('This will test if a proper fraction is tested properly', () => {
+    expect(currentOutput4).toEqual(targetOutput4);
+})
+
+const currentOutput5 = isProperFraction(-7, -8);
+const targetOutput5 = true;
+
+test('This will test if a proper fraction is tested properly', () => {
+    expect(currentOutput5).toEqual(targetOutput5);
+})
