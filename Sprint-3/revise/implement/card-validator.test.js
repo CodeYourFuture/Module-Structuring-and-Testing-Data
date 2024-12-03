5374b146fedcb9012f2811d36182f6fbf22b4108
Simpra11 <mrmabuya11@gmail.com>
2024-12-03 21:49:19 +0200

    Refactored card-validator test and fixed it to check various cases

--- Sprint-3/revise/implement/card-validator.test.js
+++ Sprint-3/revise/implement/card-validator.test.js
@@ -2,16 +2,22 @@
 const cardValidator = require("./cardValidator");
 
 
-test("Checks if the number is 16 digits and all are numbers, at least two different digits are represented adn the sum of all digits is greater than 16", () => {
-    const currentOUtput = cardValidator("1243578569873476");
-    const targetOutput = true;
+test("Checks if the number has 16 digits.", () => {
+    expect(cardValidator("1243578569873476")).toEqual(true)
+});
 
-    expect(currentOUtput).toEqual(targetOutput)
+test("Checks if the sum of all digits is greater than 16", () => {
+    expect(cardValidator("1243578569874798")).toEqual(true)
+});
+
+test("Checks if at least two different digits are represented", () => {
+    expect(cardValidator("1243578569874798")).toEqual(true)
+});
+
+test("Checks if the number is all numbers", () => {
+    expect(cardValidator("1243c78569873b79")).toEqual(false)
 });
 
 test("Checks if the final digit is even", () => {
-    const currentOUtput = cardValidator("1243578569873479");
-    const targetOutput = false;
-
-    expect(currentOUtput).toEqual(targetOutput)
+    expect(cardValidator("1243578569873479")).toEqual(false)
 });
