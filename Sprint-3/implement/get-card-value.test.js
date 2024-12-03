7e9c51481cb5e54d4b38a88e5bc29272a16b5ac2
Simpra11 <mrmabuya11@gmail.com>
2024-12-03 20:56:30 +0200

    Fixed my test to check for valid and invalid card ranks

--- Sprint-3/implement/get-card-value.test.js
+++ Sprint-3/implement/get-card-value.test.js
@@ -1,43 +1,26 @@
 const getCardValue = require("./get-card-value");
 
-const currentOutput = getCardValue("A♠");
-const targetOutput = 11;
 
 test("Checks if cardRank is 'A♠' then returns 11 ", () => {
-    expect(currentOutput).toEqual(targetOutput);
+    expect(getCardValue("A♠")).toEqual(11);
 });
-
-const currentOutput2 = getCardValue("J♦");
-const targetOutput2 = 10;
 
 test("Checks if cardRank is 'J♦' then returns 10 ", () => {
-    expect(currentOutput2).toEqual(targetOutput2);
+    expect(getCardValue("J♦")).toEqual(10);
 });
-
-const currentOutput3 = getCardValue("K♥");
-const targetOutput3 = 10;
 
 test("Checks if cardRank is 'K♥' then returns 10 ", () => {
-    expect(currentOutput3).toEqual(targetOutput3);
+    expect(getCardValue("K♥")).toEqual(10);
 });
-
-const currentOutput4 = getCardValue("Q♣");
-const targetOutput4 = 10;
 
 test("Checks if cardRank is 'Q♣' then returns 10 ", () => {
-    expect(currentOutput4).toEqual(targetOutput4);
+    expect(getCardValue("Q♣")).toEqual(10);
 });
 
-const currentOutput5 = getCardValue("Q♣");
-const targetOutput5 = 10;
-
-test("Checks if cardRank is 'Q♣' then returns 10 ", () => {
-    expect(currentOutput5).toEqual(targetOutput5);
+test("Checks if cardRank is '8' then returns 8 ", () => {
+    expect(getCardValue("8")).toEqual(8);
 });
 
-const currentOutput6 = getCardValue("Q♣");
-const targetOutput6 = 10;
-
-test("Checks if cardRank is '8♣' then returns 8 ", () => {
-    expect(currentOutput6).toEqual(targetOutput6);
+test("Checks if cardRank is '2.1' then returns 'Invalid card rank", () => {
+    expect(getCardValue("2.1♣")).toEqual("Invalid card rank");
 });
