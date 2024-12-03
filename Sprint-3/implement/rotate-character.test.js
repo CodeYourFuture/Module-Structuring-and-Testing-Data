ee7550ebebb96684fd3c8141aacba18b3160f429
Simpra11 <mrmabuya11@gmail.com>
2024-12-03 21:14:33 +0200

    Refactored rotate-character test to look clean

--- Sprint-3/implement/rotate-character.test.js
+++ Sprint-3/implement/rotate-character.test.js
@@ -1,31 +1,19 @@
 const rotateCharacter = require("./rotate-character");
 
 test("Checks for Rotate Lowercase Letters", () => {
-    const currentOutput = rotateCharacter("a", 3);
-    const targetOutput = "d";
-
-    expect(currentOutput).toEqual(targetOutput);
+    expect(rotateCharacter("a", 3)).toEqual("d");
 });
 
 test("Checks for Rotate Uppercase Letters", () => {
-    const currentOutput = rotateCharacter("F", 1);
-    const targetOutput = "G";
-
-    expect(currentOutput).toEqual(targetOutput);
+    expect(rotateCharacter("F", 1)).toEqual("G");
 });
 
 test("Checks for where Non-Letter Characters are left Unchanged", () => {
-    const currentOutput = rotateCharacter(7, 5);
-    const targetOutput = 7;
-
-    expect(currentOutput).toEqual(targetOutput);
+    expect(rotateCharacter(7, 5)).toEqual(7);
 });
 
 test("Checks if a Characters shift with Wraparound", () => {
-    const currentOutput = rotateCharacter("Y", 2);
-    const targetOutput = "A";
-
-    expect(currentOutput).toEqual(targetOutput);
+    expect(rotateCharacter("Y", 2)).toEqual("A");
 });
 
 
