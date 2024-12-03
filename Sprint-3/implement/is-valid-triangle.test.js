8f833aa276129ca48ee47dceb70ad82f1ba36f76
Simpra11 <mrmabuya11@gmail.com>
2024-12-03 21:07:29 +0200

    Refactored the code so it looks clean

--- Sprint-3/implement/is-valid-triangle.test.js
+++ Sprint-3/implement/is-valid-triangle.test.js
@@ -1,22 +1,13 @@
 const isValidTriangle = require("./is-valid-triangle");
 
-test("Checks for Valid Triangle where the sum of any two sides is greater than the third side ", () => {
-    const currentOutput = isValidTriangle(3, 5, 4);
-    const targetOutput = true;
-
-expect(currentOutput).toEqual(targetOutput);
+test("Checks for Valid Triangle where the sum is (a + b > c)", () => {
+    expect(isValidTriangle(3, 5, 4)).toEqual(true);
 });
 
 test("Checks for Invalid Triangle when any of the sides are less than or equal to zero ", () => {
-    const currentOutput = isValidTriangle(0, 2, 4);
-    const targetOutput = false;
-
-expect(currentOutput).toEqual(targetOutput);
+    expect(isValidTriangle(0, 2, 4)).toEqual(false);
 });
 
-test("Checks for Invalid Triangle, when the sum of any two side is less than or equal to the length of the third side ", () => {
-    const currentOutput = isValidTriangle(2, 2, 4);
-    const targetOutput = false;
-
-expect(currentOutput).toEqual(targetOutput);
+test("Checks for Invalid Triangle, where the sum is (b + c <= a)", () => {
+    expect(isValidTriangle(2, 2, 4)).toEqual(false);
 });
