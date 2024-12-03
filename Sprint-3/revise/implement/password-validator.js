3fc5269a4b8c909b615497b9169ea0614561fe8b
Serna Malala <sm.sernamalala@gmail.com>
2024-12-03 12:32:07 +0200

    fixed the regex bits of the function

--- Sprint-3/revise/implement/password-validator.js
+++ Sprint-3/revise/implement/password-validator.js
@@ -3,15 +3,14 @@ let passwords = [];
 function passwordValidator(input) {
 
     // have to go learn about regex or validation methods for this exercise
-    //Cant use this: /[a-z]{1,}[A-Z]{1,}[0-9]{1,}[!#$%.*&]{1,}/
     //The above is for a password that goes in that specific order
 
     //const regexPattern = /[a-z]{1,}[A-Z]{1,}[0-9]{1,}[!#$%.*&]{1,}/;
     //write a regex for each condition
-    const lowercaseRegex = /^(?=.*[a-z])/;
-    const uppercaseRegex = /^(?=.*[A-Z])/;
-    const numberRegex = /^(?=.*[0-9])/;
-    const specialCharacterRegex = /^(?=.*[!#$%.*&])/;
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const numberRegex = /[0-9]/;
+    const specialCharacterRegex = /[!#$%.*&]/;
 
     //test each regex condition
     let lowercaseTest = lowercaseRegex.test(input);
