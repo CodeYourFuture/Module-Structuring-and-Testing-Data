78c066ab71801d7e30372ac5044ba783dd01dfc8
Serna Malala <sm.sernamalala@gmail.com>
2024-12-03 15:36:10 +0200

    corrected the function and the tests as well to accomodate pm times

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -4,31 +4,41 @@
 function formatAs12HourClock(time) {
   let hours = Number(time.slice(0, 2));
   let minutes = Number(time.slice(3, 5));
+  let minuteStr = "";
   //"Cant use NUmber function on minutes because if minutes is 00 it defaults to a single 0 which we cannot use in time to display the minutes" So I need to create an if else statement to take care of these cased myself.
   if ((minutes < 10 && minutes >= 0)) {
-    minutes = `0${minutes}`;
+    minuteStr = `0${minutes}`;
   }
   else if (minutes >= 10 && minutes <= 59) {
-    minutes = minutes;
+    minuteStr = minutes;
+  }
+  else if (minutes > 59) {
+    // code to deal with invalid minute value (although not needed in this exercise).
   }
   else {
     "The minutes on the time is incorrect and out of the normal range."
   }
 
   if (hours > 12) {
-    return `${hours - 12}:${minutes} pm`;
+    if (hours - 12 >= 10) {
+      return `${hours - 12}:${minuteStr} pm`;
+    }
+    else {
+      return `0${hours - 12}:${minuteStr} pm`;
+    }
+
   }
-  else if (hours == Number("00")) {
-    return `12:${minutes} am`;
+  else if (hours === Number("00")) {
+    return `12:${minuteStr} am`;
   }
-  else if (hours == 12) {
-    return `12:${minutes} pm`;
+  else if (hours === 12) {
+    return `${hours}:${minuteStr} pm`;
   }
   else if (hours < 12 && hours >= 1) {
     if (hours < 10 && hours >= 1) {
       hours = `0${hours}`;
     }
-    return `${hours}:${minutes} am`;
+    return `${hours}:${minuteStr} am`;
   }
 
 }
@@ -50,20 +60,21 @@ console.assert(
 const currentOutput3 = formatAs12HourClock("21:00");
 const targetOutput3 = "09:00 pm";
 console.assert(
-  currentOutput2 === targetOutput2,
-  `current output: ${currentOutput2}, target output: ${targetOutput2}`
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
 );
 
 const currentOutput4 = formatAs12HourClock("12:00");
 const targetOutput4 = "12:00 pm";
 console.assert(
-  currentOutput2 === targetOutput2,
-  `current output: ${currentOutput2}, target output: ${targetOutput2}`
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
 );
 
 const currentOutput5 = formatAs12HourClock("23:59");
 const targetOutput5 = "11:59 pm";
 console.assert(
-  currentOutput2 === targetOutput2,
-  `current output: ${currentOutput2}, target output: ${targetOutput2}`
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
 );
+
