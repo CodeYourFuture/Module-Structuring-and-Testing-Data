8e631e0e96b6e323a7d14ba9226a806647918ad1
samirahekmati <samirahekmati@gmail.com>
2024-12-03 13:13:27 +0000

    moved first line of the code to help readiblity

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -1,5 +1,3 @@
-const num = 56.5678;
-
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
@@ -7,7 +5,7 @@ const num = 56.5678;
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
-
+const num = 56.5678;
 const wholeNumberPart = Math.floor(num);
 console.log(wholeNumberPart);
 
