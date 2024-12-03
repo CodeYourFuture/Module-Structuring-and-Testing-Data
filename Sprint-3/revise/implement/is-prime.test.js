5e10a18ece7b2330cf7187e1aee47404cb15cbed
Simpra11 <mrmabuya11@gmail.com>
2024-12-03 22:35:28 +0200

    Refactored code and tested for large prime and non-prime numbers

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -5,59 +5,52 @@
 const isPrime = require("./isPrime");
 
 
-
 test("Checks if the number 2 is a prime number", () => {
-    const currentOutput = isPrime(2);
-    const targetOutput = "2 is prime!";
-
-    expect(currentOutput).toBe(targetOutput);
+    expect(isPrime(2)).toBe("2 is prime!");
 
 });
 
 test("Checks if the number 3 is a prime number", () => {
-    const currentOutput = isPrime(3);
-    const targetOutput = "3 is prime!";
-
-    expect(currentOutput).toBe(targetOutput);
+    expect(isPrime(3)).toBe("3 is prime!");
 
 });
 
 test("Checks if the number 4 is a prime number", () => {
-    const currentOutput = isPrime(4);
-    const targetOutput = "4 is not prime";
-
-    expect(currentOutput).toBe(targetOutput);
+    expect(isPrime(4)).toBe("4 is not prime");
 
 });
 
 test("Checks if the number 6 is a prime number", () => {
-    const currentOutput = isPrime(6);
-    const targetOutput = "6 is not prime";
-
-    expect(currentOutput).toBe(targetOutput);
+    expect(isPrime(6)).toBe("6 is not prime");
 
 });
 
 test("Checks if the number 7 is a prime number", () => {
-    const currentOutput = isPrime(7);
-    const targetOutput = "7 is prime!";
-
-    expect(currentOutput).toBe(targetOutput);
+    expect(isPrime(7)).toBe("7 is prime!");
 
 });
 
 test("Checks if the number 9 is a prime number", () => {
-    const currentOutput = isPrime(9);
-    const targetOutput = "9 is not prime";
-
-    expect(currentOutput).toBe(targetOutput);
+    expect(isPrime(9)).toBe("9 is not prime");
 
 });
 
 test("Checks if the number 13 is a prime number", () => {
-    const currentOutput = isPrime(13);
-    const targetOutput = "13 is prime!";
+    expect(isPrime(13)).toBe("13 is prime!");
 
-    expect(currentOutput).toBe(targetOutput);
+});
+
+test("Checks if the number 23 is a prime number", () => {
+    expect(isPrime(23)).toBe("23 is prime!");
+
+});
+
+test("Checks if the number 50 is a prime number", () => {
+    expect(isPrime(50)).toBe("50 is not prime");
+
+});
+
+test("Checks if the number 103 is a prime number", () => {
+    expect(isPrime(103)).toBe("103 is prime!");
 
 });
