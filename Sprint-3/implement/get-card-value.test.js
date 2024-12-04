dc778fe9fff9172c651fa5965b9a0dd8926a859d
Simpra11 <mrmabuya11@gmail.com>
2024-12-04 13:22:09 +0200

    Updated my test file to check for other possible invalid cases

--- Sprint-3/implement/get-card-value.test.js
+++ Sprint-3/implement/get-card-value.test.js
@@ -17,9 +17,14 @@ test("Checks if cardRank is 'Q♣' then returns 10 ", () => {
     expect(getCardValue("Q♣")).toEqual(10);
 });
 
-test("Checks if cardRank is '8' then returns 8 ", () => {
+test("Checks if cardRank is '8' then returns 'Invalid card rank' ", () => {
     expect(getCardValue("10")).toEqual("Invalid card rank");
 });
+
+test("Checks if cardRank is 'Z' then returns 'Invalid card rank' ", () => {
+    expect(getCardValue("Z")).toEqual("Invalid card rank");
+});
+
 
 test("Checks if cardRank is '2.1' then returns 'Invalid card rank", () => {
     expect(getCardValue("2.1♣")).toEqual("Invalid card rank");
