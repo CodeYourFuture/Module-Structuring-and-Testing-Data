3acefb76d20287e98117a66526eab9bd19594339
Simpra11 <mrmabuya11@gmail.com>
2024-12-04 14:01:43 +0200

    Also updated my test file to return a boolean instead of "is prime" or "is not prime"

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -6,51 +6,51 @@ const isPrime = require("./isPrime");
 
 
 test("Checks if the number 2 is a prime number", () => {
-    expect(isPrime(2)).toBe("2 is prime!");
+    expect(isPrime(2)).toBe(true);
 
 });
 
 test("Checks if the number 3 is a prime number", () => {
-    expect(isPrime(3)).toBe("3 is prime!");
+    expect(isPrime(3)).toBe(true);
 
 });
 
 test("Checks if the number 4 is a prime number", () => {
-    expect(isPrime(4)).toBe("4 is not prime");
+    expect(isPrime(4)).toBe(false);
 
 });
 
 test("Checks if the number 6 is a prime number", () => {
-    expect(isPrime(6)).toBe("6 is not prime");
+    expect(isPrime(6)).toBe(false);
 
 });
 
 test("Checks if the number 7 is a prime number", () => {
-    expect(isPrime(7)).toBe("7 is prime!");
+    expect(isPrime(7)).toBe(true);
 
 });
 
 test("Checks if the number 9 is a prime number", () => {
-    expect(isPrime(9)).toBe("9 is not prime");
+    expect(isPrime(9)).toBe(false);
 
 });
 
 test("Checks if the number 13 is a prime number", () => {
-    expect(isPrime(13)).toBe("13 is prime!");
+    expect(isPrime(13)).toBe(true);
 
 });
 
 test("Checks if the number 23 is a prime number", () => {
-    expect(isPrime(23)).toBe("23 is prime!");
+    expect(isPrime(23)).toBe(true);
 
 });
 
 test("Checks if the number 50 is a prime number", () => {
-    expect(isPrime(50)).toBe("50 is not prime");
+    expect(isPrime(50)).toBe(false);
 
 });
 
 test("Checks if the number 103 is a prime number", () => {
-    expect(isPrime(103)).toBe("103 is prime!");
+    expect(isPrime(103)).toBe(true);
 
 });
