bcb7b45955216ff4ac115c418b1a2d5b59c2a357
fatima safana <asksiyam@gmail.com>
2024-12-05 21:04:45 +0000

    solution 0.js

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+  return(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//The function will multiply a and b. it will return undefined.
+
