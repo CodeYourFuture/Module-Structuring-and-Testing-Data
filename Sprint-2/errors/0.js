9ad5c19ab37b4ba8604a58a81ee85fad59acba12
fatima safana <asksiyam@gmail.com>
2024-12-05 22:22:20 +0000

    errors 0.js

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -1,9 +1,14 @@
 // Predict and explain first...
 
-// call the function capitalise with a string input
+// call the function capitalize with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+function capitalize(str) {
+   return `${str[0].toUpperCase()}${str.slice(1)}`;
+
 }
+
+console.log(capitalize("dog"));
+
+//SyntaxError: Identifier 'str' has already been declared
+//The variable has been declared twice in the same scope.
