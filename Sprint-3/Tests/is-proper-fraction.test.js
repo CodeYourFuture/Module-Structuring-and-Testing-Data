10a66ecca14fb4ae8a31fa7482fc13290f15a6c5
Natalie Patel <natcandy@gmail.com>
2024-12-05 11:27:39 +0200

    Initial commit for is-proper-fraction .js and .test.js files

---
+++ Sprint-3/Tests/is-proper-fraction.test.js
@@ -0,0 +1,11 @@
+/* Testing criteria - to determine that the input is a proper fraction, true or false
+The input will consist of 2 numbers, numerator and denominator:
+If either the numerator or denominator is zero, it must be converted to absolute values.
+If the numerator is small than the denominator, it is a proper fraction: true
+If the numerator is larger than the denominator it is not a proper fraction: false
+If the denominator is 0, it should throw an error: "Denominator cannot be zero"
+If the numerator is 0, it should throw an error: "Numerator cannot be zero"
+If the numerator is equal to the denominator, it is not a proper fraction: false
+
+
+*/
