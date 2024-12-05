514f84ee848eb1eb1fd64bafc859af5bab14f274
fatima safana <asksiyam@gmail.com>
2024-12-05 22:26:54 +0000

    errors 1.js

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,14 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+  //const decimalNumber = 0.5;
+  return percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
+  //return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
+
+//I predict function will return 50%
+// I was wrong :
+// 
