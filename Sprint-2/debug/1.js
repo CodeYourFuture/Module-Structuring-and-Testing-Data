b01689724426773c24a0e9ca599f199995822de8
fatima safana <asksiyam@gmail.com>
2024-12-05 21:08:36 +0000

    1.js

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,11 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// The function will return the sum of the two parameters a and b, which serves as placeholders for values.
+// there was a syntax error 
+
