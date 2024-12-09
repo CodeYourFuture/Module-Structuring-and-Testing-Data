f673b5ec1fca7ab1d5b18a2a9e3f5d27698dba75
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2024-12-09 19:28:29 +0000

    password-validator

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,70 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+function validate(password, previousPasswords = []) {
+  // Check if password is in previous passwords
+  if (previousPasswords.includes(password)) {
+    return false;
+  }
+
+  // Check length
+  if (password.length < 5) {
+    return false;
+  }
+
+  // Check for uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  // Check for lowercase letter
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+
+  // Check for number
+  if (!/[0-9]/.test(password)) {
+    return false;
+  }
+
+  // Check for non-alphanumeric symbol
+  if (!/[!#$%.*&]/.test(password)) {
+    return false;
+  }
+
+  // If all checks pass
+  return true;
+}
+
+
+  // Valid password scenarios
+  test('valid password should return true', () => {
+    expect(validate('Pass1!', [])).toBe(true);
+  });
+
+  // Length validation
+  test('password less than 5 characters should return false', () => {
+    expect(validate('Pa1!', [])).toBe(false);
+  });
+
+  // Uppercase validation
+  test('password without uppercase letter should return false', () => {
+    expect(validate('pass1!', [])).toBe(false);
+  });
+
+  // Lowercase validation
+  test('password without lowercase letter should return false', () => {
+    expect(validate('PASS1!', [])).toBe(false);
+  });
+
+  // Number validation
+  test('password without number should return false', () => {
+    expect(validate('PassWord!', [])).toBe(false);
+  });
+
+  // Symbol validation
+  test('password without symbol should return false', () => {
+    expect(validate('Pass123', [])).toBe(false);
+  });
+
+ 
