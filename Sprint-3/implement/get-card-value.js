27763bd302e6596d4853447c119b57d6063e9baa
azaralampanah2 <azaralampanah2@gmail.com>
2024-12-09 10:34:57 +0000

    amending get-card-value.js to comply with reviewer comments

--- Sprint-3/implement/get-card-value.js
+++ Sprint-3/implement/get-card-value.js
@@ -30,12 +30,35 @@
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
 function getCardValue(cardString) {
-  cardStringPadded = cardString.padStart(3, "0");
-  rank = cardStringPadded.substring(0, 2);
+  let rank = cardString.slice(0, -1);
+  const validRanks = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+
+  // Validate rank
+  if (!validRanks.includes(rank)) {
+    return "Invalid card rank";
+  }
+  if (!isNaN(rank)) {
+    rank = parseInt(rank, 10); // Convert to a number
+  }
+
   if (2 <= rank && rank <= 10) return rank;
-  if (rank == "0A") return 11;
-  if (rank == "0Q" || rank == "0J" || rank == "0K") return 10;
-  else return "Invalid Card rank";
+  if (rank == "A") return 11;
+  if (rank == "Q" || rank == "J" || rank == "K") return 10;
+  //else return "Invalid Card rank";
 }
 //console.log(getCardValue("3B"));
 console.assert(
@@ -46,10 +69,7 @@ console.assert(
   getCardValue("A♠") == "11",
   "the rturn value for `A♠` must be 11"
 );
-console.assert(
-  getCardValue("3♠") == "03",
-  "the rturn value for `3♠` must be 3"
-);
+console.assert(getCardValue("3♠") == "3", "the rturn value for `3♠` must be 3");
 console.assert(
   getCardValue("K♠") == "10",
   "the rturn value for `K♠` must be 10"
@@ -57,4 +77,22 @@ console.assert(
 console.assert(
   getCardValue("J♠") == "10",
   "the rturn value for `J♠` must be 10"
+);
+console.assert(
+  getCardValue("007♠") == "Invalid card rank",
+  "the rturn value for `007♠` must be Invalid Card rank "
+);
+console.assert(
+  getCardValue("0Q♠") == "Invalid card rank",
+  "the rturn value for `0Q♠` must be Invalid Card rank "
+);
+
+console.assert(
+  getCardValue("00Q♠") == "Invalid card rank",
+  "the rturn value for `007♠` must be Invalid Card rank "
+);
+
+console.assert(
+  getCardValue("0Q♠") == "Invalid card rank",
+  "the rturn value for `007♠` must be Invalid Card rank "
 );
