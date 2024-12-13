c0c428c0da16da1683eca2ac0e59cfa7db09314b
Rashaad Ebrahim <rmebrahim@gmail.com>
2024-12-13 20:10:35 +0200

    validatePassword function created and tests created.

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,99 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+
+const validationRegex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!#$%.*&])[A-Za-z\d!#$%.*&]{5,}$/;
+
+function validatePassword(password, previousPasswords = []) {
+  let previousPW = previousPasswords;
+  if (validationRegex.test(password) && !previousPW.includes(password)) {
+    previousPasswords.push(password);
+    return true;
+  }
+  return false;
+}
+
+describe("# validatePassword Tests", () => {
+  describe("## Valid passwords", () => {
+    test.each(["Password#123"])("Have at least 5 characters: %s 👍", (pw) => {
+      expect(validatePassword(pw)).toBe(true);
+    });
+
+    test.each(["javaScript.v1"])(
+      "Have at least one English uppercase letter: %s 👍",
+      (pw) => {
+        expect(validatePassword(pw)).toBe(true);
+      }
+    );
+
+    test.each(["ATLeAST*1"])(
+      "Have at least one English lowercase letter (a-z): %s 👍",
+      (pw) => {
+        expect(validatePassword(pw)).toBe(true);
+      }
+    );
+
+    test.each(["1!Number"])("Have at least one number (0-9): %s 👍", (pw) => {
+      expect(validatePassword(pw)).toBe(true);
+    });
+
+    test.each(["1$ymBol"])(
+      "At least one of the following symbols ('!','#','$','%','.','*','&'): %s 👍",
+      (pw) => {
+        expect(validatePassword(pw)).toBe(true);
+      }
+    );
+
+    test.each`
+      pw           | previousPW
+      ${"NewPw1%"} | ${["PreviousPw1%"]}
+    `(
+      "Not a previous password: $previousPW Input: $pw   👍",
+      ({ pw, previousPW }) => {
+        expect(validatePassword(pw, previousPW)).toBe(true);
+      }
+    );
+  });
+
+  describe("## Invalid passwords", () => {
+    test.each(["Pw#1"])("Have at least 5 characters: %s 👎", (pw) => {
+      expect(validatePassword(pw)).toBe(false);
+    });
+
+    test.each(["html.v1"])(
+      "Have at least one English uppercase letter: %s 👎",
+      (pw) => {
+        expect(validatePassword(pw)).toBe(false);
+      }
+    );
+
+    test.each(["NOCAP%69"])(
+      "Have at least one English lowercase letter (a-z): %s 👎",
+      (pw) => {
+        expect(validatePassword(pw)).toBe(false);
+      }
+    );
+
+    test.each(["No!Number"])("Have at least one number (0-9): %s 👎", (pw) => {
+      expect(validatePassword(pw)).toBe(false);
+    });
+
+    test.each(["nosymBo1"])(
+      "At least one of the following symbols ('!','#','$','%','.','*','&'): %s 👎",
+      (pw) => {
+        expect(validatePassword(pw)).toBe(false);
+      }
+    );
+
+    test.each`
+      pw                | previousPW
+      ${"PreviousPw1%"} | ${["PreviousPw1%"]}
+    `(
+      "Not a previous password: $previousPW Input: $pw   👎",
+      ({ pw, previousPW }) => {
+        expect(validatePassword(pw, previousPW)).toBe(false);
+      }
+    );
+  });
+});
