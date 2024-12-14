14d81faf75c2cef7c3a0404e49170198539d311b
PersianQuant <sc.heidari@gmail.com>
2024-12-14 16:25:23 +0000

    Sprint 2 changes reverted

--- prep/test.js
+++
@@ -1,6 +0,0 @@
-let yearOfBirth = 1990; // declaration
-let currentYear = 2023; // declaration
-//yearOfBirth++;
-//currentYear++; // statement
-console.log(`currentYear: ${currentYear}`);
-console.log(`I am ${currentYear - yearOfBirth} years old`); // statement
