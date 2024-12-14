81f1dc77c70a0a6f071692864a02784ac2e5df54
RahwaZeslusHaile <rahwazeslase@gmail.com>
2024-12-14 22:52:47 +0000

    Explanation reason for the bug with the correct code

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,5 +1,16 @@
 // Predict and explain first...
-// in the function multiply it should be returning the value instead of logging ,so it will be undefined.
+// The issue with this code is that the multiply function does not return a value, but it is trying to use it in a template string which expects a return value.
+
+//To fix this, I should modify the multiply function to return the product of a and b instead of just logging it.
+
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(The result of multiplying 10 and 32 is ${multiply(10, 32)});
+
+
+
 function multiply(a, b) {
   return(a * b);
 }
