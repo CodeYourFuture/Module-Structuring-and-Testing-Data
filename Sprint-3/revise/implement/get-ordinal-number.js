e516fc436f9afc315b399e52625167f974624157
Natalie Patel <natcandy@gmail.com>
2024-12-15 15:05:48 +0200

    Wrote tests for get-ordinal-number

--- Sprint-3/revise/implement/get-ordinal-number.js
+++ Sprint-3/revise/implement/get-ordinal-number.js
@@ -1,26 +1,25 @@
 function getOrdinalNumber (num) {
-    if (isNaN(num) || num <= 0) { //Handles for when the input num is not a number or is negative
-        return "Please enter a valid number";
+    const number = Number(num); //This converts the input into a number to be handled going forward
+    
+    if (isNaN(number) || number <= 0 || !Number.isInteger(number)) { //Handles for when the input num is not a number or is negative or is not an integer
+        throw new Error("Please enter a valid number");
     }
 
-    if (num === null || num === undefined) {
-        throw new Error("Please enter a valid number") //This handles null and undefined inputs   
-        } 
-
+   
 //This will isolate the last character and last two characters to handle what to add to the end of the returned string.
 //1 will return 1st, 2 will return 2nd but 11, 12 and 13 with return 11th, 12th and 13th    
-    const findLastCharacter = num % 10; //The remainder of the division is the last character
-    const findLastTwoCharacters = num % 100; //The remainder of the division are the last two characters
+    const findLastCharacter = number % 10; //The remainder of the division is the last character
+    const findLastTwoCharacters = number % 100; //The remainder of the division are the last two characters
 
     if (findLastTwoCharacters >= 11 && findLastTwoCharacters <= 13) {
-        return '${numb}th'; //For numbers 11 to 13
+        return `${number}th`; //For numbers 11 to 13
     }
 
     switch (findLastCharacter) { //switch is better than if/else to account for all numbers ending in 1, 2 or 3.
-        case 1: return '${num}st';
-        case 2: return '${num}nd';
-        case 3: return '${num}rd';
-        default: return '${num}th'; //default will run when numbers that are not 11, 12 and 13 but all other numbers such as 4.
+        case 1: return `${number}st`;
+        case 2: return `${number}nd`;
+        case 3: return `${number}rd`;
+        default: return `${number}th`; //default will run when numbers that are not 11, 12 and 13 but all other numbers such as 4.
     }
     
 }
--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -7,34 +7,31 @@ const {getOrdinalNumber} = require ('./get-ordinal-number');
 describe("getOrdinalNumber function", () => {
 
 //Error cases
-test("Please enter a valid string", () => {
+test("Please enter a valid number", () => {
     expect(() =>getOrdinalNumber(null)).toThrow("Please enter a valid number"); //must have an entry
     expect(() =>getOrdinalNumber(undefined)).toThrow("Please enter a valid number"); //must be an entry
+    expect(() =>getOrdinalNumber("-1")).toThrow("Please enter a valid number"); //handles for negative numbers
+    expect(() =>getOrdinalNumber("0")).toThrow("Please enter a valid number"); //handles for zero
+
 })   
 
-//Invalid cases
-test("should return 'Please enter a valid number'", () => {
-    expect(getOrdinalNumber("-1")).toBe("Please enter a valid number");
-    expect(getOrdinalNumber("0")).toBe("Please enter a valid number");
-}); 
-
 //Typical cases
-test("should return 'numst' if any number ending in 1 except 11 is input", () => {
+test("should return a string ending in 'st' if any number containing 1 except 11 is input", () => {
     expect(getOrdinalNumber("1")).toBe("1st");
     expect(getOrdinalNumber("21")).toBe("21st");
 }); 
 
-test("should return 'numnd' if any number ending in 2 except 12 is input", () => {
+test("should return a string ending in 'nd' if any number containing 2 except 12 is input", () => {
     expect(getOrdinalNumber("2")).toBe("2nd");
     expect(getOrdinalNumber("22")).toBe("22nd");
 }); 
 
-test("should return 'numrd' if any number ending in 3 except 13 is input", () => {
+test("should return a string ending in 'rd' if any number containing  except 13 is input", () => {
     expect(getOrdinalNumber("3")).toBe("3rd");
     expect(getOrdinalNumber("23")).toBe("23rd");
 }); 
 
-test("should return 'numth' if any other number or 11, 12 and 13 are input", () => {
+test("should return a string ending in 'th' if any number containing 2 except 12 is input or 11, 12 and 13 are input", () => {
     expect(getOrdinalNumber("4")).toBe("4th");
     expect(getOrdinalNumber("11")).toBe("11th");
     expect(getOrdinalNumber("12")).toBe("12th");
