a4e9a9de6e1fd5556860bd007f746408b9b41863
Natalie Patel <natcandy@gmail.com>
2024-12-16 11:53:24 +0200

    Wrote .js and test.js files for repeat function

--- Sprint-3/revise/implement/repeat.test.js
+++ Sprint-3/revise/implement/repeat.test.js
@@ -23,3 +23,32 @@
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+const {repeat} = require('./repeat');
+describe ("repeat function", () => {
+//Error case
+test('should return "Please enter a positive number" if a negative number, null or undefined is input', () => {
+    expect(() =>repeat("-1")).toThrow("Please enter a positive number");
+    expect(() =>repeat(null)).toThrow("Please enter a positive number");
+    expect(() =>repeat(undefined)).toThrow("Please enter a positive number");
+});
+
+//Typical cases
+
+test("should return the string as is if input is 1", () => {
+        expect(repeat("Hello", 1)).toBe("Hello");
+        expect(repeat("12", 1)).toBe("12");
+});
+
+test("should return an empty string if input is 0", () => {
+            expect(repeat("Hello", 0)).toBe("");
+            expect(repeat("12", 0)).toBe("");        
+}); 
+
+test("should return repeated string if input is positive", () => {
+    expect(repeat("Cat", 4)).toBe("CatCatCatCat");
+    expect(repeat("4", 4)).toBe("4444");   
+    expect(repeat("ManySons", 2)).toBe("ManySonsManySons");   
+
+}); 
+
+})
