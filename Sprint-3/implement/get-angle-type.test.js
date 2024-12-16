486d3d442407a160f89f19a868744bda6dfad9a0
fatima safana <asksiyam@gmail.com>
2024-12-16 17:29:41 +0000

    -added node modules -test files -implemented function get-angle-type.js

---
+++ Sprint-3/implement/get-angle-type.test.js
@@ -0,0 +1,9 @@
+const getAngleType = require('./get-angle-type.js');
+
+test('Given an angle in degrees, return the type of angle', () => {
+  expect(getAngleType(90)).toBe("Right angle");
+  expect(getAngleType(45)).toBe("Acute angle");
+  expect(getAngleType(160)).toBe("Obtuse angle");
+  expect(getAngleType(180)).toBe("Straight angle");
+  expect(getAngleType(300)).toBe("Reflex angle");
+});
