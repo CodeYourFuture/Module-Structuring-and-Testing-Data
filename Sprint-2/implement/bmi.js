6df687f827dc6b6e42372329b254909ab674f7ef
Karla G. <k.grajalesluna@gmail.com>
2024-12-19 23:56:38 +0000

    done bmi.js

--- Sprint-2/implement/bmi.js
+++ Sprint-2/implement/bmi.js
@@ -13,3 +13,38 @@
 // Given someone's weight in kg and height in metres
 // Then when we call this function with the weight and height
 // It should return their Body Mass Index to 1 decimal place
+
+// ============================= BMI TEST ===============================
+
+function bodyMassIndex(weight, height) {
+  const kilograms = Number(weight);
+  const metres = Number(height);
+
+  if (kilograms > 0 && metres > 0) {
+    const bmi = kilograms / metres ** 2;
+    return Math.round(bmi * 10) / 10;
+  }
+   return "Invalid input. Please enter valid weight and height.";
+}
+
+
+//Avoid .toFixed() if we need a number (as .toFixed() returns a string).
+console.log(bodyMassIndex(70, 0)); 
+
+
+/*
+    Dynamic Precision with Math.round()
+    Combining Math.pow() with Math.round() offers a dynamic solution for those
+    seeking a more flexible approach that can adapt to rounding to various decimal 
+    places.
+
+    function roundTo(num, precision) {
+    const factor = Math.pow(10, precision)
+    return Math.round(num * factor) / factor
+    }
+    console.log(roundTo(4.687, 0)); // Output: 5
+    console.log(roundTo(4.687, 1)); //one decimal place
+    console.log(roundTo(4.687, 2)); //two decimal places
+    console.log(roundTo(4.687, 3)); // Output: 4.687
+
+*/
