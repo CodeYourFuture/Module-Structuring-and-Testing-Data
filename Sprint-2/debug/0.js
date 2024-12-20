6f58efae8c3f085009b7a157db187fbd374d4483
RihannaP <114884466+RihannaP@users.noreply.github.com>
2024-12-20 16:13:45 +0000

    fixing debug docs

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,12 @@
 // Predict and explain first...
+//* When we execute this code, it calls the multiply function , we will have the log of the function by console.log inside of the
+// function >(320). However because the function doesn't have return statement so the value in the template literal will be undefined.
+
+
+//* To fix this we can put return statement and return the value to the template literal.
 
 function multiply(a, b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
