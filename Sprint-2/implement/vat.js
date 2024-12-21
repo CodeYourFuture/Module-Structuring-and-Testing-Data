12467cc5b0fe7163a850c66cf912c4779fbb955a
Karla G. <k.grajalesluna@gmail.com>
2024-12-21 12:06:24 +0000

    done tp-pounds.js

--- Sprint-2/implement/vat.js
+++ Sprint-2/implement/vat.js
@@ -8,3 +8,17 @@
 // Given a number,
 // When I call this function with a number
 // it returns the new price with VAT added on
+
+
+function addVatporcentage (price) {
+    const vatIncluded = Number(price * 1.2); //converted the str into number
+    console.log(typeof vatIncluded);
+    return `Vat included ${vatIncluded}`;
+
+}
+
+console.log(addVatporcentage("30"));
+
+/*
+    This will return the new price with the vat included and also converted the string into number with the method Number()
+*/
