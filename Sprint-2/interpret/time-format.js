c0e52cb91f19d381406edea6c9229fe5dc7c24d5
Karla G. <k.grajalesluna@gmail.com>
2024-12-23 01:38:15 +0000

    done time-format.js

--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -1,4 +1,5 @@
 function pad(num) {
+  console.log(num);
   return num.toString().padStart(2, "0");
 }
 
@@ -7,25 +8,32 @@ function formatTimeDisplay(seconds) {
   const totalMinutes = (seconds - remainingSeconds) / 60;
   const remainingMinutes = totalMinutes % 60;
   const totalHours = (totalMinutes - remainingMinutes) / 60;
-
-  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(
-    remainingSeconds
-  )}`;
+  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
-// You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
-// to help you answer these questions
+console.log(formatTimeDisplay(61));
 
-// Questions
+/*
+You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
+to help you answer these questions
 
-// a) When formatTimeDisplay is called how many times will pad be called?
+Questions
 
-// Call formatTimeDisplay with an input of 61, now answer the following:
+a) When formatTimeDisplay is called how many times will pad be called?
+    - 3 times, pad is called three times, once for each part of the time: hours, minutes, and seconds.
 
-// b) What is the value assigned to num when pad is called for the first time?
+Call formatTimeDisplay with an input of 61, now answer the following:
+   
 
-// c) What is the return value of pad is called for the first time?
+b) What is the value assigned to num when pad is called for the first time?
+    - 0, because the first pad is totalHours and we don't have any hours yet, only minutes
 
-// d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+c) What is the return value of pad is called for the first time?
+     - "00" and calling formatTimeDisplay(61) = 00:01:01
 
-// e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+    - In this case, the value of num before it is passed to pad is 1 (the remainder when 61 are divided by 60).
+
+e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+    - After pad processes 1, it will return the value "01" because it ensures the number is two digits long (by adding a leading zero)
+*/
