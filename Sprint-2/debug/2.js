e2929c210dbc6170a15da1e14b8878052a6fad48
Karla G. <k.grajalesluna@gmail.com>
2024-12-23 17:02:56 +0000

    update the response on my prediction on  2.js

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -1,27 +1,48 @@
 // Predict and explain first...
 
 /*
-we can not run this function because we settle the variable with const and this won't allow us to reassign 
-the new value which we are trying to test in the console.log()
+  - If we run this code. nothing will happen because the function is not called.
 
-const num = 103;
+  -once we invoke the function this will take the number and converted into string to manipulate and  take the las digit and return the new string 42  => will return 2.
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+    const num = 103;
+
+    function getLastDigit() {
+      return num.toString().slice(-1);
 }
 
 */
 
 //=========== This will  work ==============
+// This program should tell the user the last digit of each number.
+// Explain why getLastDigit is not working properly - correct the problem
+
+
+const num = 103;
 
 function getLastDigit(num) {
-  return num.toString().slice(-1); 
+  return num.toString().slice(-1);
 }
 
-
+console.log(`The last digit of 42 is ${getLastDigit(num)}`);
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
+
+
+//=========== finding the middle digit ==============
+// Index:  0  1  2  3  4  5  6  7  8  9 10 11 12
+// Value:  2  0  5  5  5  6  4  2  3  2  5  6  9
+
+function getMiddleDigit(num) {
+  const str = num.toString();
+  // console.log(str);
+
+  const middleIndex = Math.floor(str.length / 2); // 13/2 = 6.5 math.floor round the number to the nearest integer 6.
+  // console.log(middleIndex);
+
+  return str[middleIndex]; //we can access to the position 6 which contains the value '4'
+}
+
+console.log(`The middle of 105 is ${getMiddleDigit(2055564232569)}`);
