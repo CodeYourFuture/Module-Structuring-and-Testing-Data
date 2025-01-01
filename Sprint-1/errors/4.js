5929da668c6bd460ba545384ca3fb54744d6e551
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-01-01 16:50:24 +0000

    Variable names cannot start with a number

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,6 +1,11 @@
-const 12HourClockTime = "20:53";
-console.log(12HourClockTime)
-const 24hourClockTime = "08:53";
-console.log(24HourClockTime)
+// const 12HourClockTime = "20:53";
+// console.log(12HourClockTime)
+// const 24hourClockTime = "08:53";
+// console.log(24HourClockTime)
 //SyntaxError: Invalid or unexpected token
 //Since the variable starts with number it returns SyntaxError. In java script variables can not start with number.
+const hour12ClockTime = "20:53"; 
+console.log(hour12ClockTime);
+
+const hour24ClockTime = "08:53"; 
+console.log(hour24ClockTime);
