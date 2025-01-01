34f38480d0737c3ae1eff9ff7cc403c0bb0e2ecd
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-01-01 16:33:17 +0000

    Proper use of keywords

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,7 +1,10 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
-console.log(age)
+// const age = 33;
+// age = age + 1;
+// console.log(age)
 //TypeError: Assignment to constant variable.
     //he const keyword is initialized once,can not change or update the constant variable twice
+let age = 33;
+age = age + 1; 
+console.log(age); 
