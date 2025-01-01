45e0e34c214c1701897b78339a5ab919fb1527d1
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-01-01 16:40:06 +0000

    Declare and initialize the variable before using it in the console.log

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,7 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
 // ReferenceError: Cannot access 'cityOfBirth' before initialization
 //this occurs when it is attempted to access the variable before declaring it with in that scope ,so "const cityOfBirth = "Bolton"; should come first.
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
