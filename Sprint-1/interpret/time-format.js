3f474482aadd80d1f93dd3af8848b7af596275ab
Karla G. <k.grajalesluna@gmail.com>
2025-01-02 02:06:11 +0000

    Updated explanation for better undestating on how works the remainingSeconds var

--- Sprint-1/interpret/time-format.js
+++ Sprint-1/interpret/time-format.js
@@ -31,6 +31,10 @@ console.log(result);
 
     b) How many function calls are there?
 
+        -------- 4 function calls:
+        -------- console.log(remainingSeconds);
+        -------- console.log(totalMinutes);
+        -------- console.log(remainingMinutes);
         -------- console.log(result);
 
     c) Using documentation, explain what the expression movieLength % 60 represents
@@ -41,18 +45,21 @@ console.log(result);
 
     d) Interpret line 4, what does the expression assigned to totalMinutes mean?
 
-        -------- This line is taking the leftover remainder in remainingMinutes and leaving just the
-            total amount that we can divide perfectly in whole number to see how many minutes we have.
+        -------- This line subtracts remainingSeconds from movieLength to remove the leftover seconds. 
+            Then, it converts the difference into minutes by dividing the result by 60. 
+            This gives the total number of whole minutes in movieLength.
 
     e) What do you think the variable result represents? Can you think of a better name for this variable?
 
-        ------- Result var represent the total runtime of the movie, the nameVar is fine but we can name it as
-            timeMovie for better readability, as it gives future readers a better idea of the variable's purpose.
+        ------- Result var represents the total runtime of the movie. While "result" is fine, 
+            a name like "timeMovie" or "formattedTime" would be more descriptive and improve readability.
 
     f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
 
-        ------- This code works as expected for the most of positive values of movieLength. Just
-            when I write negative number in the result will show the negative sign. eg. 
+        ------- This code works as expected for the most of positive values of movieLength. 
+            However, if a negative value is provided for movieLength, the result will show negative values, 
+            which might not make sense in the context of time. For example:
             const movieLength = -4258;
             result: -1:-10:-58
+
 */
