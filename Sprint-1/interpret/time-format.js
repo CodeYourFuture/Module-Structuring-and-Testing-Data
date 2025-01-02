3ea9f369af2d652239f87768fc5213044102ed10
Karla G. <k.grajalesluna@gmail.com>
2025-01-02 02:10:01 +0000

    added another var name option in line 55

--- Sprint-1/interpret/time-format.js
+++ Sprint-1/interpret/time-format.js
@@ -52,7 +52,7 @@ console.log(result);
     e) What do you think the variable result represents? Can you think of a better name for this variable?
 
         ------- Result var represents the total runtime of the movie. While "result" is fine, 
-            a name like "timeMovie" or "formattedTime" would be more descriptive and improve readability.
+            a name like "movieRunTime", "timeMovie" or "formattedTime" would be more descriptive and improve readability.
 
     f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
 
