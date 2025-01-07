f4bcf060d88609b9aeaf1f8a6f00f81fa697d9c0
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-07 02:27:22 +0000

    fix find

--- Sprint-3/revise/investigate/find.js
+++ Sprint-3/revise/investigate/find.js
@@ -20,6 +20,15 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+//>>>The index value at the beginning is 0 and increases by 1 with each loop, moving through (index++)until the target character
+//>>> is found or the end of the string is reached.
+
 // b) What is the if statement used to check
+//>>>The if statement checks if the character at the string matches with the target character. If they are match, it returns
+//>>> the current index.
+
 // c) Why is index++ being used?
+//>>>It is used to increment the value of index by 1 in each loop , and checking the next character in the string.
+
 // d) What is the condition index < str.length used for?
+//>>>It stops the loop from running after the end of the string, when string finish(last index) loop will stop.
