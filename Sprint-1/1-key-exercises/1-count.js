af48a3837dfb5ce6e609a95fc3462f7e40ae68b6
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-01-26 17:22:15 +0200

    Added a comment/answer and added the code to get initials.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+//the = is reassigning the new value  to the variable count of count
