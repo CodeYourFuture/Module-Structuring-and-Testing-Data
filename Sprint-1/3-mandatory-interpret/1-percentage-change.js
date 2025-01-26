2db73b6e3273f4ed2d8f95343d2f007687f5c603
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-01-26 22:19:15 +0200

    edited comment

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -12,7 +12,7 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-//there are 5 function call.
+//there is 1 function call.
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
 //line 5 has a error. The error is occuring because there us a missing comma after the the first double quotations. You can fix the error by just adding the comma.
