5293b40712faa037a6154d298ea59c1207dffe10
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-01-26 18:36:43 +0200

    Added an underscore on the variable

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const _12HourClockTime = "20:53";
+const _24hourClockTime = "08:53";
+
+console.log(_12HourClockTime);
+//a variabkke cannot start with a number
