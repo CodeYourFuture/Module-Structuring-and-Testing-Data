67a86104a51b2573c8ddd0024e737b14c2e7e50e
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-01-26 18:19:25 +0200

    Answered the given question.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+//you cannot console log a variable that hasn't been assigned yet because the program runs from the top to bottom.
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
