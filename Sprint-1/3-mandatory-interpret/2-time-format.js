177795abfcf770a159d154f44f6902ba0831334f
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-01-26 22:16:21 +0200

    Added comments to answer the questions

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,21 +1,10 @@
-const movieLength = 10000; // length of movie in seconds
-
-const remainingSeconds = movieLength % 60;
-const totalMinutes = (movieLength - remainingSeconds) / 60;
-
-const remainingMinutes = totalMinutes % 60;
-const totalHours = (totalMinutes - remainingMinutes) / 60;
-
-const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
-console.log(result);
-
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
 //there are 6 variable declarations in the program.
 
 // b) How many function calls are there?
-//there are 6 function calls.
+//there is 1 function call.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,25 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. the const penceStringWithoutTrailingP = penceString.substring( 0,  penceString.length - 1,
+/*the substring(0) retuns 399p because substring (0) is the same as copying the entire string as it has no
+    end parameter it will extract from start to end and 
+    the length -1 with remove the last character meaning this whole function will return 399.*/
+
+// 3. the const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0")
+/* pedStart(3, "0") represent the number of characters a string should have in this case 3, and if the string dont meet the number of characters
+    the start of the string will be filled with "0". But in this case the string length is already 3, therefor there 
+    the padStart will not change anything*/
+
+// 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2
+/* What this function is doing is first calling the function substring, but sing the function has no end parameter 
+      it will extract the whole string, after that the length -2 function is called qith removes the last 2 characters of the function so
+      that the whole functions returns "3" */
+
+// 5. const pence = paddedPenceNumberString  .substring(paddedPenceNumberString.length - 2)  .padEnd(2, "0")
+/*  What this function is firstly it will extract the last 2 characters of the string and 
+      padEnd makes sure that the string has atleats 2 charaters which it does. Therefore this will return "99" */
+
+// 6. console.log(`£${pounds}.${pence}`)
+// this retuns 3.99
