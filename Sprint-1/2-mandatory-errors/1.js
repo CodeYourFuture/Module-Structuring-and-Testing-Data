0eebb5d78aff7b9414bd12f93d7129ba8ba1bdf3
loveth900 <ojay_love@yahoo.com>
2025-01-28 15:20:50 +0000

    i allow reassignment of the age

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+
+
+let age = 33; // Create the age variable with an initial value
+age = age + 1; // Reassign the value by adding 1
+
+console.log(age); // Output: 34
