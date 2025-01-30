c1909c67feb77d1e44bf00fa6adb9201395db989
loveth900 <ojay_love@yahoo.com>
2025-01-30 04:52:03 +0000

    This function is now reusable for any weight input

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,13 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+// Function to convert weight from kilograms to pounds
+function toPounds(weightInKg) {
+    return weightInKg * 2.20462;  // Conversion formula: 1 kg = 2.20462 pounds
+}
+
+// Example usage:
+console.log(toPounds(10));  // Output: 22.0462
+console.log(toPounds(50));  // Output: 110.231
+console.log(toPounds(70));  // Output: 154.324
