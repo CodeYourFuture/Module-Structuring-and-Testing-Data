7f2ed9a8108e6ca886ebe3bf72425c44ade38c15
loveth900 <ojay_love@yahoo.com>
2025-01-30 02:49:24 +0000

    variable cannot start with a number.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "20:53"; // This should be renamed from "12HourClockTime"
+const twentyFourHourClockTime = "08:53"; // Renamed to follow camelCase convention
+
+console.log(`12-hour format: ${twelveHourClockTime}`);
+console.log(`24-hour format: ${twentyFourHourClockTime}`);
