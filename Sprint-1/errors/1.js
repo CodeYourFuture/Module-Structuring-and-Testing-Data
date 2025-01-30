bd96d85c7da4c55064760f768677e248f09e0c23
Amir200524 <amiraminia@yahoo.com>
2025-01-30 08:40:39 +0000

    using let instead of const

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
+// Also we can write it like this
+// const age = 33;
+//console.log(age+1);
