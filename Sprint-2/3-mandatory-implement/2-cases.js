6f7fd2ed9c792ab8a2fcaa5dc52fa52ca4d1ab00
loveth900 <ojay_love@yahoo.com>
2025-01-30 04:49:22 +0000

    the upper case was replace

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,11 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(str) {
+    return str.toUpperCase().replace(/ /g, '_');  // Convert to uppercase and replace spaces with underscores
+}
+
+// Example usage:
+console.log(toUpperSnakeCase("hello there"));  // Output: "HELLO_THERE"
+console.log(toUpperSnakeCase("lord of the rings"));  // Output: "LORD_OF_THE_RINGS"
