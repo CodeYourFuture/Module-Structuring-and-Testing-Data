844e66917c84d963f6367db615a2e3cf2657a24b
Amir200524 <amiraminia@yahoo.com>
2025-01-31 07:53:57 +0000

    Fix multiply function

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,15 @@
 // Predict and explain first...
 
-function multiply(a, b) {
-  console.log(a * b);
+//function multiply(a, b) {
+//  console.log(a * b);
+//}
+
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// At the first, it shows a*b (10*32=320)
+// Secondly, the multiply function doesn't return anything, so it implicitly returns undefined and shows:
+//The result of multiplying 10 and 32 is undefined
+function multiply(a,b){
+  return a*b;
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10,32)} `);
