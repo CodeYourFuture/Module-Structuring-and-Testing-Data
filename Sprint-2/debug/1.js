e8348d52829c824a99ec09b9e81e0839893d92a3
Amir200524 <amiraminia@yahoo.com>
2025-01-31 08:05:50 +0000

    Fix sum function

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,9 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//It is going to show this:The sum of 10 and 32 is undefined
+// Because return is empty. There is a extra (;) between return an a+b.
