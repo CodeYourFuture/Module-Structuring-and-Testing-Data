254cf252b88ee6a0cef558479e89c237c74615a3
loveth900 <ojay_love@yahoo.com>
2025-01-31 15:06:41 +0000

    the input were added

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -4,15 +4,20 @@ function getAngleType(angle) {
 
 }
 
+function getAngleType(angle) {
+  if (angle === 90) return "Right angle";
+  if (angle < 90) return "Acute angle";
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
+  if (angle > 180 && angle < 360) return "Reflex angle";
+  if (angle === 360) return "Full rotation";
+  throw new Error("Invalid angle"); // Handles invalid input
+}
 
-
-
-
-
-
+// Export function for Jest tests
+module.exports = getAngleType;
 
 // Don't get bogged down in this detail
 // Jest uses CommonJS module syntax by default as it's quite old
 // We will upgrade our approach to ES6 modules in the next course module, so for now 
 // we have just written the CommonJS module.exports syntax for you
-module.exports = getAngleType;
