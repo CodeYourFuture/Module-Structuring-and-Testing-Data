0b27e1d08e39083242bc67074f0de4a5e29af467
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-01 17:50:34 +0200

    Commented out unnecessary  code.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -6,10 +6,10 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
-}
+}*/
 
 // =============> write your explanation here
 
