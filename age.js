6492e9b4b1253e3bd4b4953a3daa7b9741d84d4f
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-01 09:51:04 +0000

    declared two variables to find out my age program

---
+++ age.js
@@ -0,0 +1,6 @@
+const year0OfBirth = 1985; //declaration
+const currentYear = 2025; // declaration
+
+
+`I am ${currentYear - year0OfBirth} years old.`;
+console.log(`I am ${currentYear - year0OfBirth} years old.`);
