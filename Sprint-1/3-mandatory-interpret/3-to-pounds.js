13c3f3c8e865a0da7ddbb68ab469fc06c297fa0e
FaraiMuzah <fmuzah@gmail.com>
2025-02-02 20:29:24 +0200

    stretch

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,23 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+//Answer
+
+// 2. The lines 3-6 creates a new string when p is removed from pencestring 
+//    penceString.substring(0,penceString.length - 1) is used to extract the part of the string at the first character index 0
+//    and the end from the evaluation of penceString.length - 1.  Character p which is exluded. This leaves us with 399
+//    
+// 3. The lines 8 pads penceStringWithoutTrailingP(399) making it three characters long. 
+//    Three 0s were to be added but the string already has three characters making the result be 399
+
+// 4. The lines 9-12 gets the pound value from paddedPenceNumberString(399)
+//    it takes the substring starting from index 0 and ends with the evaluation of
+//    paddedPenceNumberString.length - 2. The result will be 3
+
+// 5. The lines 14-16 gets the last two digits of paddedPenceNumberString(399)
+//    substring(paddedPenceNumberString.length - 2 is used to have two characters which will be 99
+//    padEnd(2, "0") makes sure that the string has two characters by adding trailing zeros
+//    and since 99 is two characters the result will be 99
+
+// 6. The line 18 uses template literals and prints the final formatted string to the console. The results is £3.99
--- Sprint-1/4-stretch-explore/objects.md
+++ Sprint-1/4-stretch-explore/objects.md
@@ -4,13 +4,17 @@ In this activity, we'll explore some additional concepts that you'll encounter i
 
 Open the Chrome devtools Console, type in `console.log` and then hit enter
 
-What output do you get?
+What output do you get? Answer: ƒ log() { [native code] }
 
-Now enter just `console` in the Console, what output do you get back?
+Now enter just `console` in the Console, what output do you get back? 
+console {debug: ƒ, error: ƒ, info: ƒ, log: ƒ, warn: ƒ, …}
 
 Try also entering `typeof console`
 
 Answer the following questions:
 
-What does `console` store?
+What does `console` store? Answer: console is an object containing various methods for logging information to the browser's console, as well as for debugging purposes
 What does the syntax `console.log` or `console.assert` mean? In particular, what does the `.` mean?
+Answer: console refers tp the object that contains methods/functions for treating data
+        .log and .assert refers to the functions the the console objct
+        . allows acess to the method of te function in the console object
