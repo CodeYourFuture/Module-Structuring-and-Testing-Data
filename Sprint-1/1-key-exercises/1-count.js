c4f699f8ee2c2d4bde7bc72ca4c15f17fd456669
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-02-02 17:51:33 +0000

    Sprint 1 - 1-key exercise

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//Answer :
+//Since the variable declaration is let on line 1, line 3 is declaring a new value for the variable count by changing it's value to count + 1.
+//The = sign is declaring the value of a variable in both of these instances.
