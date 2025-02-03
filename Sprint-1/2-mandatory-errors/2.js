18433bb2dcbcab307fe04d3f1a19d342f84ee2c6
Ihoryan <ihorbolzhelarskyi78@gmail.com>
2025-02-03 13:37:53 +0000

    done 2-mandatory-errors

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// ReferenceError: Cannot access 'cityOfBirth' before initialization
+// Variable has to be declared before it is printed or used
--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,6 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = +cardNumber.toString().slice(-4);
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +8,8 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// TypeError: cardNumber.slice is not a function
+// We can t use slice() method to numbers, only strings or arrays
+// At the beginning we need to change the value of our variable to string by tostring() method;
+// Then by adding + we convert the extracted string back into a number.
