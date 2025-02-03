eccd767b77b68a409e429d7923e19bed7708a08b
sabafarjamfard <farjamfardsaba@yahoo.com>
2025-02-03 10:07:32 +0000

    Fix Sprint-1 (mandatory-errors)

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+// what's the error ?//variable cityOfBirth is referenced in the console.log statement before it has been defined.i need to declare variables before using them .
 
-console.log(`I was born in ${cityOfBirth}`);
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
