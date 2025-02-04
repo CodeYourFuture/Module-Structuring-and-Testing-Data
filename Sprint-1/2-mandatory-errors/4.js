992d5b562aae7b7065920656dba68d8fc51c92e0
Farai Muzanenhamo <fmuzah@gmail.com>
2025-02-04 19:41:16 +0200

    exercise4_corrected

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -4,7 +4,14 @@
 //Answer
 //The above code starts with a number and javascript naming does not allow that.
 // Also the naming is not camel case since H is in Uppercase not lowercase
-const hourClockTime12 = "20:53";
+/*const hourClockTime12 = "20:53";
 const hourClockTime24 = "08:53";
 console.log(hourClockTime12);
 console.log(hourClockTime24);
+*/
+
+//Correction
+const hourClockTime24 = "20:53";
+const hourClockTime12 = "08:53";
+console.log(hourClockTime24);
+console.log(hourClockTime12);
