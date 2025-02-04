77f099ed2f9dd5ee5883112c91108402d1a4f8dc
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-04 15:29:06 +0000

    const declaration error solved exercise 4

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const $12HourClockTime = "20:53";
+console.log($12HourClockTime);
+
+
+const $24hourClockTime = "08:53";
+console.log($24hourClockTime);
+
+
+
+//First character of a variable declaration must be a letter, underscore, or dollar sign.
