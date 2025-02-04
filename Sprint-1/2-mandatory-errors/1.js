3d9046437268b586c6181d0e939d0ebccb96cdd4
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-04 14:39:21 +0000

    Corrected the variable declaration

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
+
+/* const is used for variable that can be assigned only one value, I have replaced const with let, 
+that can take multiple values */
