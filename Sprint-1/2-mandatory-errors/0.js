806cc56074891c5f78ed9a15bca57447df21b2bf
aiahibrahim <ayababikeribrahim@gmail.com>
2025-02-04 13:42:00 +0000

    0_errors_solution2

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,9 +1,10 @@
 
-
-/*
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
 
 
-1. by make a comment 
+/*
+1.error is:SyntaxError: Unexpected identifier and This error occurs because the instruction 
+is not valid JavaScript code. JavaScript can't interpret plain text as executable code.
+2. we can solve this problem by make a comment 
 */
