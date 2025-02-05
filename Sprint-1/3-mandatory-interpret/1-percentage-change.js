bd6ffe9a6bd046db5a99710c89e26d28c7dde0f4
sheetalkharab <sheetalrana89@gmail.com>
2025-02-05 22:18:51 +0000

    explain percentage-change file

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -20,3 +20,25 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+/*
+a) There are 5 function call.carPrice =(carPrice.replaceAll(",", ""));
+    Number(carPrice.replaceAll(",", ""));
+    (priceAfterOneYear.replaceAll("," "")
+    Number(priceAfterOneYear.replaceAll("," ""));
+    console.log(`The percentage change is ${percentageChange}`);
+    
+b)  SyntaxError: missing ) after argument list. There is a missing , between two arguments in replaceAll()function.
+    so it will be on line 5 Number(priceAfterOneYear.replaceAll("," ,""));
+
+c)   carPrice = Number(carPrice.replaceAll(",", ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));  are the two lines where variables are reassigned
+
+d)  let carPrice = "10,000";
+    let priceAfterOneYear = "8,543";
+    const priceDifference = carPrice - priceAfterOneYear;
+    const percentageChange = (priceDifference / carPrice) * 100; are variable declaration lines.
+
+e) Number(carPrice.replaceAll(",","")) is  replacing , in variable carPrice and reassigning this new value to carPrice variable.  
+
+*/
