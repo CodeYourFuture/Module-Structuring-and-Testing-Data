77a606b22cc50f4b006315131a3bcaa18e172124
sheetalkharab <sheetalrana89@gmail.com>
2025-02-05 13:56:55 +0000

    find out dir and ext value

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -13,11 +13,14 @@ const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
+const lastDotIndex= filePath.lastIndexOf(".");
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
-
-const dir = ;
-const ext = ;
+//Everything before the last /
+const dir =filePath.slice(0,lastSlashIndex) ;
+const ext =filePath.slice(lastDotIndex+1); //Everything after the last . in file name
+console.log(dir);
+console.log(ext);
 
 // https://www.google.com/search?q=slice+mdn
