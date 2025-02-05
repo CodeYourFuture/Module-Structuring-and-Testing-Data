1a13889dec36e9f1f1aed7dd9ca99833ff0ceb4f
hatef Eidi <hatef.121.h@gmail.com>
2025-02-05 23:01:03 +0000

    const or let? Answer

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+
+console.log(age);
+
+//In this case we cann't use const variable if we want to change its value later on, so it should be let
