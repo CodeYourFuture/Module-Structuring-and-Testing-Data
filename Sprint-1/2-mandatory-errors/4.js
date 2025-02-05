43cca1d29a79a859f3dd6e47f97c69a4cd60a48b
sheetalkharab <sheetalrana89@gmail.com>
2025-02-05 22:18:06 +0000

    need to change in variable name

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+//SyntaxError: Invalid or unexpected token.variable name can't start with number.so need to remove 12 and 24 on line 1 and 2.
+// change variable names like const HourClockTime = "20:53";
+// const hourClockTime = "08:53";
