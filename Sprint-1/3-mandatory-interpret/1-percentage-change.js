16fc3e3495ac710feaad3221dc9550aba411146b
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-05 11:07:36 +0000

    Error solved and questions answered 1-percentage-change exercise

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,9 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+console.log(typeof(carPrice));
+console.log(carPrice);
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +14,17 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// four functions 
+// line 4 and line 5
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// There is a comma missing on line 5 in the replaceAll() function.
 
 // c) Identify all the lines that are variable reassignment statements
+//line 4, 5, 
 
 // d) Identify all the lines that are variable declarations
+// line 1, 2, 7, 8 
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// In the carPrice the comma is. replace with nothing, and then the result converted to number type.
