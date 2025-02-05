b39d3fc1d12c10f967d251e38e69cab70ba9e5c2
sheetalkharab <sheetalrana89@gmail.com>
2025-02-05 14:44:36 +0000

    slice function changes

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,9 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+/* cardNumber.slice(-4) is giving negative integer and index start at 0. so it's giving error.
+TypeError: cardNumber.slice is not a function at Object.
+consider: slice function work on arrays and string not on number. If we change it in string it will work.
+so we need to update in line 1 const cardNumber = "4533787178994213"; and make it string
+*/
