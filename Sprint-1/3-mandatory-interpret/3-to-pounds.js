7073fa44fedde83956c9bd6f4986e01618c29e3e
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-05 12:35:26 +0000

    step-by-step description added 3-to-pounds exercise

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -24,4 +24,12 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 1. const penceString = "399p": initializes a string variable with the value "399p"
+/* 2. const penceStringWithoutTrailingP created to obtain a string without the trailing p from the end -1
+   3. const paddedPenceNumberString declared to ensure that the number is three charters long as our example is 399, if not, zeros would be added at the start.
+   4. const pounds declared to provide the first character from the number our ex :3 assumes as for pounds
+   5. const pence declared to provide the last two characters from the number, and padEnd() 2 characters with 0 if required.
+  but not needed in our case.
+   6. The price is formatted in pounds and pence, and pounds sign added as well using template literals.
+  
+   */
