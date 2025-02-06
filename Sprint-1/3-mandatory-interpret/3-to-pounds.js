22e82d7019af497099917de30a9a32cf3aa0c9a8
hatef Eidi <hatef.121.h@gmail.com>
2025-02-06 18:22:06 +0000

    to pounds-Sprint1- Answer

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,8 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1): extract every characters in penceString variable except the last one (penceString,length -1)
+// 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"): ensures that the number is at least 3 digit long and if not adds 0 before the number not to change the value.
+// 4.const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2): extract everything but the last 2 digits (-2) from paddedPenceNumberString variable to get the pound number
+// 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0"): similar to pound, it extract the last two digits of paddedPenceNumberString to get the pence number, If there’s only one digit, it ensures two digits by adding a trailing zero
+// 6. console.log(`£${pounds}.${pence}`): in this line the pound value will be displayed to the user in this format: £ pound. pence
