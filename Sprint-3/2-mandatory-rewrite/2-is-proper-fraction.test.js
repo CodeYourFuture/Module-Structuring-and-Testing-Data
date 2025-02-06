b00d6b647d0cc175e6cb4c7b0652190022ebf870
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-06 11:51:12 +0200

    Added tests

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("Should be able to identify Improper fractions (23/3)", () => {
+  expect(isProperFraction(23, 3)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("Should be able to identify Negative fractions (-20/21)", () => {
+  expect(isProperFraction(-20, 21)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("Should be able to Identify equal Numerator and Denominator (3/3) ", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
