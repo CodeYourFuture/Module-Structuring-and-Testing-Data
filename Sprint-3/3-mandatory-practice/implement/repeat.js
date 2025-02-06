f6ebeaf1cc92eb35f46bf9f9aea34d2be2ca6110
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-06 11:50:54 +0200

    Added tests

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,7 @@
-function repeat() {
-  return hellohellohello;
+function repeat(str, count) {
+  let result = str.repeat(count);
+  return result;
 }
+console.log(repeat("My Name ", 5));
 
 module.exports = repeat;
