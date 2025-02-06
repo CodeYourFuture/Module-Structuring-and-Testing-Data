962b2e2a2783a86bc0f98b46dd1f780a2e55bab1
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-06 14:14:29 +0200

    Implemented a function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-    return 5
+  let strOccurance = 0;
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      strOccurance++;
+    }
+  }
+  return strOccurance;
 }
-
+console.log(countChar("Name", "a"));
+console.log(countChar("NAME", "a"));
+console.log(countChar("Name", "O"));
 module.exports = countChar;
