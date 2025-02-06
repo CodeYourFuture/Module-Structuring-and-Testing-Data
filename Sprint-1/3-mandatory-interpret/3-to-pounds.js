097d88ebdedb6f05800dd612342628099d6b4313
sab325 <sabshrestha324@gmail.com>
2025-02-06 20:58:12 +0000

    done 3-mandatory-interpret

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -20,8 +20,25 @@ console.log(`£${pounds}.${pence}`);
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
 
-// You need to do a step-by-step breakdown of each line in this program
+// with the value "399p"You need to do a step-by-step breakdown of each line in this program
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 1. const penceString = "399p": initializes a string variable 
+// this one initializes the penceString value with of 399 in pence.
+
+//const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
+ //In this line, trailing comma is removed from the penceString and substring remove the first line(0) 
+// and one character before the end which is (penceString.length-1) it will left "399"
+
+//const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+//This line make sure the string "penceStringWithoutTrailingP" has 3 character and also adding "0" if necessary and it also penceStringWithoutTrailingP is "99" also padded to "099".
+
+//const pounds = paddedPenceNumberString.substring(0,  paddedPenceNumberString.length - 2);
+//This line remove the part of the string which is pound and substring from the start(0) to characters before the end paddedPenceNumberString.   
+
+ //const pence = paddedPenceNumberString .substring(paddedPenceNumberString.length - 2) .padEnd(2, "0");
+  //This line of code extracts 2 characters of the string "paddedPenceNumberString" also pads the results to make sure it has 2 characters by adding 0 to format properly.
+  
+//console.log(`£${pounds}.${pence}`);
+// It will print price in pound and pence in format. which will print 3.99
