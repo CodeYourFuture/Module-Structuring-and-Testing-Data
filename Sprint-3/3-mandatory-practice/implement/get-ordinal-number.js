3b159380e2905b59040fd467d59ad5b084a5bf3e
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-06 18:01:36 +0200

    Implemented a function to get ordinal number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,19 @@
 function getOrdinalNumber(num) {
- return "1st";
-}
+  let x = num % 10;
+  let y = num % 100;
 
+  if (x === 1 && y !== 11) {
+    return num + "st";
+  } else if (x === 2 && y !== 12) {
+    return num + "nd";
+  } else if (x === 3 && y !== 13) {
+    return num + "rd";
+  } else return num + "th";
+}
+console.log(getOrdinalNumber(15));
+console.log(getOrdinalNumber(21));
+console.log(getOrdinalNumber(3));
+console.log(getOrdinalNumber(11));
+console.log(getOrdinalNumber(22));
+console.log(getOrdinalNumber(111));
 module.exports = getOrdinalNumber;
