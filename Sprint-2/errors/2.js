f16fd4c5901e7ed71087f3e034e9ce4672a265cd
Amir200524 <amiraminia@yahoo.com>
2025-02-07 16:46:52 +0000

    Fix function parameter error

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,11 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
 
+//The function is supposed to square a number, but it will not work because of a syntax error. At line 6.
+//Function parameters must be variable names, not numbers. like:num,Number,...
+console.log(square(200));
