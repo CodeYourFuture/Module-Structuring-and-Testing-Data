18601b698038c65e86522baad538e73bb5f55ba0
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-08 14:24:29 +0000

    cleanup

--- age.js
+++
@@ -1,6 +0,0 @@
-const year0OfBirth = 1985; //declaration
-const currentYear = 2025; // declaration
-
-
-`I am ${currentYear - year0OfBirth} years old.`;
-console.log(`I am ${currentYear - year0OfBirth} years old.`);
