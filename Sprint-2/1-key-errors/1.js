d332e5bfb1494821113b8e547ebd8a975d87fab2
FaraiMuzah <fmuzah@gmail.com>
2025-02-08 15:13:58 +0200

    1.js

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,33 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> decimalNumber is declared as a parameter for the function. A redeclaration is done using const and this causes an error
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+//function convertToPercentage(decimalNumber) {
+//const decimalNumber = 0.5;
+//const percentage = `${decimalNumber * 100}%`;
 
+// return percentage;
+//}
+
+//console.log(decimalNumber);
+
+// =============> The function is named convertToPercentage with a parameter decimalNumber.
+// A redeclaration is done of decimalNumber which stores the the value 0.5 but will cause an error since we can't redeclare in javascript
+// A variable percentage is declared which will stores the evaluation of a decimal number to a percentage
+// return will end the function and give the percentage value
+// console.log will result in an error because it is outside the function and also not defined
+
+// Finally, correct the code to fix the problem
+// =============>
+const decimalNumber = 0.5;
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+let result = convertToPercentage(decimalNumber);
+console.log(result);
