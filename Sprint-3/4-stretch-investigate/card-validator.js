f3cadd6c1a71997d32b59806712b024dc56f8e7a
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-08 07:12:17 +0200

    Added comments to explain my code

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,18 +1,27 @@
 function cardValidator(card) {
+  // this checks if the card length is strictly 16, i not it returns false.
   if (card.length !== 16) {
     return false;
   }
-
+  //This checks to see if card only contains numbers, if not, it returns false
   if (/\D/.test(card)) {
     return false;
   }
+  /*  
 
+then return true if sum is above or equal 16 and the last digit is even. */
+
+  //Firstly I created a variable called last digit, this is my fist step in checking if last digit is even.
+  //parseInt changes the card into an integer, and then I select the last character
   let lastDigit = parseInt(card.charAt(card.length - 1));
+  //I created another variable called sum
   let sum = 0;
+  //Implemented a for loop,saying for let num be characters of the card.
   for (let num of card) {
+    //and sum them all up adding to my variable sum.
     sum += parseInt(num);
   }
-
+  //return true if sum is above or equal 16 and the last digit is even.
   return sum >= 16 && lastDigit % 2 === 0;
 }
 
