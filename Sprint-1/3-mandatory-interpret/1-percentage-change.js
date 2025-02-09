969407a2619f8fbeb7446216cb068faeb8f2a850
aiahibrahim <ayababikeribrahim@gmail.com>
2025-02-09 14:11:00 +0000

    1_percentage_change_FunctionNumFixed

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -14,14 +14,16 @@ console.log(`The percentage change is ${percentageChange}`);
 
  a) How many function calls are there in this file? Write down all the lines where a function call is made
 
-     There 2 fuction calles.
+     There 5 function calls:
     1. carPrice = Number(carPrice.replaceAll(",", ""));
+        a. number(  ) b.  .replaceAll
     2. priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
-
+        a. number(  ) b. . replaceAll
+    3. console.log(  )  
 b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
     1.
       SyntaxError: missing ) after argument list On line 5
-      the error accure because of the missing comma between the first argument and the second argument 
+      the error accrue because of the missing comma between the first argument and the second argument 
       we can fix it by adding a comma : (priceAfterOneYear.replaceAll("," , ""));
 
 c) Identify all the lines that are variable reassignment statements
@@ -33,7 +35,7 @@ d) Identify all the lines that are variable declarations
 e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
      first carPrice.replaceAll(",","") will replace the comma in the string 10,000 to 10000
      then number  ("10000") will convert the string "10000" to the number 10000 
-     the purpose of this exprssion to convert the value of carePrice to a numeric value so we can use in the
-      mathimatical Operations without getting the error Not a number "NAN"
+     the purpose of this expression to convert the value of carePrice to a numeric value so we can use in the
+      mathematical Operations without getting the error Not a number "NAN"
 
 */
