7415876d51f4d412308b58b98445a999c5c49eb2
FaraiMuzah <fmuzah@gmail.com>
2025-02-09 19:34:47 +0200

    updated

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here. Answer: We will get an error that say undefine
 
 function sum(a, b) {
   return;
   a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here. Answer: The return value stops the execution early without a + b being executed
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+//Answer: New code
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,24 +11,27 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+let result = formatTimeDisplay(61);
+console.log(result);
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> write your answer here. Answer: Once for totalHours, once for remainingMinutes, and once for remainingSeconds. A total of 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here. Answer: totalHours = (61 -1) / 60 = 1 / 60 = 0. num = 0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here. Answer : pad(0) evaluates to '0'.padStart(2, 0) and this evaluates to "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here. Answer: Last call to pad is for remaining Seconds which is 1
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here. Answer: pad(1) evaluates to '1'.padStart(2, 0) and this evaluates to "01"
