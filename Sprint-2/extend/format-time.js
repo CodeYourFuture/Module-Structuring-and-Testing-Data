9ea04220c1e1c51c8192763beb99c94e4fba8f88
Amir200524 <amiraminia@yahoo.com>
2025-02-09 18:24:07 +0000

    Test Cases Using Assertions

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -22,3 +22,27 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+const currentOutput4 = formatAs12HourClock("12:00");
+const targetOutput4 = "12:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4
+   `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+const currentOutput5 = formatAs12HourClock("07:32");
+const targetOutput5 = "07:32 am";
+console.assert(
+  currentOutput5 === targetOutput5
+   `current output: ${currentOutput5}, target output: ${targetOutput5}`
+);
+const currentOutput6 = formatAs12HourClock("23:20");
+const targetOutput6 = "11:20 pm";
+console.assert(
+  currentOutput6 === targetOutput6,
+  `current output: ${currentOutput6}, target output: ${targetOutput6}`
+);
