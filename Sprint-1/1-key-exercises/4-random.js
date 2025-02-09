104698ae977cab203245c6ef9f1bcb0383bb5a87
Segun <segunfolayan@yahoo.com>
2025-02-09 22:30:39 +0000

    more update up to 2

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,8 +2,11 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+console.log(num);
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+// Math.floor() method always rounds down and returns the largest integer less than or equal to a given number
+// Math.random() method returns a floating-point pseudo-random number in the range 0 to less than 1 
