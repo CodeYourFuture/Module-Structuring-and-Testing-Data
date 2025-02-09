74649af9057db65a0a602c8a2390d7c0602c5d74
sab325 <sabshrestha324@gmail.com>
2025-02-09 18:02:56 +0000

    sprint2 debugging

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,14 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here: there is ; error
 
+
+
+// =============> write your explanation here: function will be undefined because of ";" by removing ; placing a+b in same line will return function.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a+b;
+  
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,7 @@
-// Predict and explain first...
+// Predict and explain first...because of const num, it won't change output.
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// =============> Write your prediction here: 3 
 
 const num = 103;
 
@@ -14,11 +14,22 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> write the output here: 3 for all console.log.
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> write your explanation here: 3 for all of the the console.log because it won't take any argument it will always take const argument.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+
+
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+because of line 6.
