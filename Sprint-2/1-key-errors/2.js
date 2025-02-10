4f3e0acb26771f5e575b642eaa5f97e99aad5519
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-10 21:29:04 +0000

    answer for the 3rd question in sprint 2 |1-key-errors

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,10 +1,9 @@
-
 // Predict and explain first BEFORE you run any code...
-
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
+//i think the problem appear because of wrong function syntax when defining a function.
+//i think the error is we didnt declare num inside the function and we cant just put a number directly as a parameter without calling the function
 function square(3) {
     return num * num;
 }
@@ -12,9 +11,15 @@ function square(3) {
 // =============> write the error message here
 
 // =============> explain this error message here
+the error says function square(3) unexpected number . this means we can not use this number directly as a parameter without defining it.
 
 // Finally, correct the code to fix the problem
-
 // =============> write your new code here
+/*function square(num) {
+    return num * num;
+}
+console.log(square(3));
+*/
+
 
 
