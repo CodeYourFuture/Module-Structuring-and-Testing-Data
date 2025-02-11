e0bdf9a29d77ee4a27690da1d39e8a66e3487379
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-11 22:55:48 +0000

    the variable inside the function should not match the parameter name-Global variable-key Error-Answer

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,36 @@
 // Predict and explain first...
-
 // Why will an error occur when this program runs?
+// the error occurs because the variable decimalNumber is not declared globally.
+
 // =============> write your prediction here
+//Predict: the program throws an error, because the variable decimalNumber is not declared globally.
 
 // Try playing computer with the example to work out what is going on
 
+
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> write your explanation here
+// When we call the function we should pass a value, which in this case it does not exist outside the function
+// constant decimalNumber =0.5 should not be inside the function, which will give us error, because it is local scop if it's inside the function
+// we can't re-declare the parameter and a variable with the same name in a function
+
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+const decimalNumber = 0.5;
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(decimalNumber));
