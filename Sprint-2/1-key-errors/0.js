ff31cedc1a9a0983bfab78513fea54aef12aed72
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-11 22:15:39 +0000

    declared variable error-sprint2-answer

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,27 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//  We thought it going to show undefined, because the parameter in the function was the same which was empty so the code can't evaluate because it is an empty string.
 
 // call the function capitalise with a string input
-// interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+
+// function capitalise(str) {
+  
+//    let str=`${str[0].toUpperCase()}${str.slice(1)}`;   
+//    return str; 
+// }
 
 // =============> write your explanation here
+// interpret the error message and figure out why an error is occurring
+// The error message was that str is already declare in the parameter. so we either had to change the parameter or the variable's name in the function or just return the expression without saving it into a variable
+
+
 // =============> write your new code here
+let string="millenaandifeanyand hatef";
+
+function capitalise(str) {
+  
+  return `${str[0].toUpperCase()}${str.slice(1)}`;   
+   
+}
+console.log(capitalise(string));
