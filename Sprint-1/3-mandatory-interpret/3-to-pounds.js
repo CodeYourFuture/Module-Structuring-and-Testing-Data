a06b9e6d244cfbb345cbad5a02f5dd94358d2d9e
Millena28 <millenakiflom06@gmail.com>
2025-02-11 08:57:42 +0000

    Have done the mandatory-interprete

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,21 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. const penceStringWithoutTrailingP = penceString.substring( 0,penceString.length - 1); this removes 
+// the p from the value. the substring() takes two arguments , in which it starts at index 0 (the first character)and excludes the last 
+//  character .
+
+// 3. const paddedPenceNumberString: padStart(3, "0") ensures that the string penceStringWithoutTrailingP has at least 3 characters by adding leading zeros (if necessary).
+//If the string is already 3 or more characters, it stays the same. If it's shorter, it adds "0" characters to the left until the string is 3 characters long.
+
+// 4.const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+//This line extracts the pounds from the padded string by taking everything except the last two digits.
+//substring(0, paddedPenceNumberString.length - 2) extracts the substring starting at index 0 and ending two characters before the end.
+//The purpose here is to separate the whole pounds from the pence part of the string.
+//For example, if paddedPenceNumberString = "399", it will extract "3" (the pounds).
+// 5substring(paddedPenceNumberString.length - 2) takes the last two characters of the string.
+//padEnd(2, "0") ensures that if the pence part is only one digit (e.g., "9" from "399"), it will be padded with a "0" at the end, making it two digits ("09").
+//For example, if paddedPenceNumberString = "399", it will extract "99" as pence.
+
+// 5.This line uses template literal to format the output with a pound symbol  and the pound and pence values.then logs the result to the console.
