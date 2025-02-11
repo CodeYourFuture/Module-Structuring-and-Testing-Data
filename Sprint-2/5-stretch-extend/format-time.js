69c7a692774a22100114da6a2363193c31970f49
Ihoryan <ihorbolzhelarskyi78@gmail.com>
2025-02-11 00:07:43 +0000

    done format-time

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,22 +4,34 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+  const minutes = Number(time.slice(3, 5));
+  if (hours == 0) {
+    return `${hours + 12}:${minutes.toString().padStart(2, "0")} am`;
+  } else if (hours == 12) {
+    return `${hours}:${minutes.toString().padStart(2, "0")} pm`;
+  } else if (hours > 12) {
+    return `${hours - 12}:${minutes.toString().padStart(2, "0")} pm`;
   }
-  return `${time} am`;
+  return `${hours}:${minutes.toString().padStart(2, "0")} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+const currentOutput = formatAs12HourClock("08:07");
+const targetOutput = "8:07 am";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+const currentOutput2 = formatAs12HourClock("00:00");
+const targetOutput2 = "12:00 am";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("12:09");
+const targetOutput3 = "12:09 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
 );
