263be6409ddd1dd4f6940ee3fb9036c7fdc8cb91
Droid-An <andrmail6@gmail.com>
2025-02-11 14:02:33 +0000

    done 3-mandatory-interpret section

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",",""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,26 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// 3
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// console.log(`The percentage change is ${percentageChange}`);
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// error occurring in line "priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));"
+// the error occurred because js didn't understand where argument list ends
+// we just need to place a comma between items in list
 
 // c) Identify all the lines that are variable reassignment statements
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",",""));
+
 
 // d) Identify all the lines that are variable declarations
+// let carPrice = "10,000";
+// let priceAfterOneYear = "8,543";
+// const priceDifference = carPrice - priceAfterOneYear;
+// const percentageChange = (priceDifference / carPrice) * 100;
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// the purpose of this expression is to convert string data type to integer data type
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -23,5 +23,24 @@ console.log(`£${pounds}.${pence}`);
 // You need to do a step-by-step breakdown of each line in this program
 // Try and describe the purpose / rationale behind each step
 
-// To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+/*
+To begin, we can start with
+1. const penceString = "399p": initialises a string variable with the value "399p"
+2.
+3. const penceStringWithoutTrailingP = penceString.substring( : declares a variable equals to 'penceString', but without p at the end
+4. substring starts from first character
+5. ends on the last character before 'p'
+6.
+7.
+8. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); : pads the string with "0" until the total length of string reaches the length 3
+9. const pounds = paddedPenceNumberString.substring( : declares a variable 'pounds' equals to 'paddedPenceNumberString' but without last two digits
+10. substring starts from first character
+11. ends on the third character from the end.
+12.
+13.
+14. const pence = paddedPenceNumberString:  declares a variable 'pence' equals to last two digits of 'paddedPenceNumberString'
+15. returns the part of the 'paddedPenceNumberString' string from the second digit up to the end
+16. pads the string with "0" in the end until the total length of string reaches the length 2
+17.
+18. writes in the console price in pounds
+*/
