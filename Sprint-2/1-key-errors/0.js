ed863a0718a999d79f9005fbf2ac02cf09d88dc6
emladeno <mladenova.elvira@yahoo.com>
2025-02-12 14:44:23 +0000

    predict and explain

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//The code will throw a SyntaxError because str is being declared twice in the function scope
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+//JavaScript does not allow redeclaring function parameters using let or const within the same scope.
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
@@ -10,4 +12,12 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+//The function parameter str is already defined when the function is called.
+//Inside the function, the line let str = ... attempts to redeclare str, causing a SyntaxError.
+//To fix this, we can either:
+//Use a different variable name inside the function.
+//Modify str directly without redeclaring it.
 // =============> write your new code here
+//function capitalise(str) {
+ // str = `${str[0].toUpperCase()}${str.slice(1)}`;
+ // return str;
