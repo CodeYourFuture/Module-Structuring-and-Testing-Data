29f1edc27d002ac5535d7b3e4eb2e8387dbfbb53
AmirMontazeri <mr.amir.mti@gmail.com>
2025-02-12 09:56:19 +0000

    sprint1-tasks-mandatory-errors-2.js-done

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,6 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+// It's because cityOfBirth used before it is declared. in javaScript codes read line by line.
+// so if we declaring the variable before console.log, the code gonna work.
