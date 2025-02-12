62323abd3a8b0421ff4c02b74ce711eefe9bf9d2
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-12 23:40:44 +0000

    function of turning any string  into UPPER_SNAKE_CASE- Answer

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,7 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function convertToUpperCaseSnakeCase(input) {
+  return input.toUpperCase().split(' ').join('_');
+}
