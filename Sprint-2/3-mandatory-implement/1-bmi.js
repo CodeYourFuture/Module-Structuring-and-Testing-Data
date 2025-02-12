e79c19070a6b47466efb3a0c4834d7b35dd4077e
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-12 23:39:14 +0000

    BMI to first decimal place- Answer

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -1,7 +1,6 @@
 // Below are the steps for how BMI is calculated
 
 // The BMI calculation divides an adult's weight in kilograms (kg) by their height in metres (m) squared.
-
 // For example, if you weigh 70kg (around 11 stone) and are 1.73m (around 5 feet 8 inches) tall, you work out your BMI by:
 
 // squaring your height: 1.73 x 1.73 = 2.99
@@ -15,5 +14,6 @@
 // It should return their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
-    // return the BMI of someone based off their weight and height
+  return (weight / (height * height)).toFixed(1);
 }
+
