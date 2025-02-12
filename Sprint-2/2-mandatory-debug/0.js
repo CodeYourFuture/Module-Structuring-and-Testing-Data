276fb01a0ae4ab471db817bc56986e57b9874b12
Droid-An <andrmail6@gmail.com>
2025-02-12 19:16:57 +0000

    done mandatory debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,23 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// I think the code isn't working correctly because we are treating the 'multiply' function as if it returns a value. 
+// But in this case function does not return anything - it just prints the result to the console.
+// To fix this, I would replace 'console.log' with 'return'
 
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,23 +2,41 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// I think every call to this function will return '3' because it returns the last digit of 'num'. Since 'num' is already set to 103, the last digit is always 3.
 
-const num = 103;
 
-function getLastDigit() {
+// const num = 103;
+
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
+// Explain why the output is the way it is
+// =============> write your explanation here
+// because every time code returns the last digit of the same variable
+
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
