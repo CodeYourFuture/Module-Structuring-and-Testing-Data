3a25d99919352607066c73087b4f425fc5a1c565
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-12 23:46:36 +0000

    format time- stretch extend- finding bugs of Midday-Answer

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -7,14 +7,27 @@ function formatAs12HourClock(time) {
   if (hours > 12) {
     return `${hours - 12}:00 pm`;
   }
+
+  else if (hours === 12) {
+    return `${time} pm`;
+
+  }
+ //  We added else if because we want to check if the hours are equal to 12, if they are we want to return the time with pm
   return `${time} am`;
 }
-
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+const currentOutput = formatAs12HourClock("12:00");
+const targetOutput = "12:00 pm";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
+);
+
+
+const currentOutput1 = formatAs12HourClock("08:00");
+const targetOutput1 = "08:00 am";
+console.assert(
+  currentOutput1 === targetOutput1,
+  `current output: ${currentOutput1}, target output: ${targetOutput1}`
 );
 
 const currentOutput2 = formatAs12HourClock("23:00");
@@ -22,4 +35,11 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "00:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
 );
