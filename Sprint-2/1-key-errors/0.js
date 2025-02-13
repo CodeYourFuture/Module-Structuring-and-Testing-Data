50a8634ab6584016049cf922e303581ddf730a56
Millena28 <millenakiflom06@gmail.com>
2025-02-13 00:18:00 +0000

    Done the key-errors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,8 +1,12 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// I thought it  was going to show undefined. 
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+
+//The error message was that str is already declare in the parameter. 
+//so we either had to change the parameter or the variable's name in the function 
+//or just return the expression without saving it into a variable.
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
@@ -11,3 +15,9 @@ function capitalise(str) {
 
 // =============> write your explanation here
 // =============> write your new code here
+
+let string = "millena mesfin"
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
+console.log(capitalise(string));
