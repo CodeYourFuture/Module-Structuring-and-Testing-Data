e2fbfc25a084264657db40a58d9bfb936239c6bf
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-02-13 20:14:07 +0000

    2_mandatoryDebug_solution

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,10 @@
 // Predict and explain first...
 //  =============> write your prediction here
+/*
+the function sum(10,32) will return undefined because the return statement if the function
+doesn't return anything and the return statement ends the function execution immediately 
+so the line a+b will never get executed 
+*/
 
 function sum(a, b) {
   return;
@@ -9,5 +14,19 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+/*
+the return statement in the sum function is ending the function early 
+before the addition of a + b can execute 
+since there is no value being returned, undefined is returned by default 
+*/
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+/*
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+*/
