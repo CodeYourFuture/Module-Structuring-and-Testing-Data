08a560963b881672c1547e9df48d37a8d0402251
loveth900 <ojay_love@yahoo.com>
2025-02-13 15:39:14 +0000

    the get card value

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -3,3 +3,28 @@ function getCardValue(card) {
     return 11;
 }
 module.exports = getCardValue;
+
+function getCardValue(card) {
+    // Convert the card to uppercase to make the function case insensitive
+    card = card.toUpperCase();
+
+    // Check for number cards (2 to 10)
+    if (!isNaN(card) && card >= 2 && card <= 10) {
+        return parseInt(card);
+    }
+
+    // Check for face cards
+    if (["J", "Q", "K"].includes(card)) {
+        return 10;
+    }
+
+    // Check for Ace
+    if (card === "A") {
+        return 11;
+    }
+
+    // If input is invalid, throw an error
+    throw new Error("Invalid card value");
+}
+
+module.exports = getCardValue;
