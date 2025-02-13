77c92fe24a4dc9728be2ad9c4c8219750d2fb730
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-13 07:24:48 +0000

    Dir path correction

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -16,7 +16,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 
-const dir = filePath.slice(1,lastSlashIndex); 
+const dir = filePath.slice(0,lastSlashIndex); 
 console.log(`The dir part of the ${filePath} is ${dir}`);
 
 // Created a variable, lastDotIndex to help me find the ext variable.
