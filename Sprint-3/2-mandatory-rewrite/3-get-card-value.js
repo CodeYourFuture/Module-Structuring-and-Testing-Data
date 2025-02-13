b5bac55c249342e716c2fab4d058f311e0d2cf9e
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-13 20:06:06 +0000

    This commit is done for question 3 of Sprint3 folder 2

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,8 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    let rank = card.slice(0, -1);
+    if (rank === "A") return 11;
+    else if (rank>="2" && rank<="9") return Number(rank);
+    else if (rank==="10" || rank==="J" || rank==="Q" || rank==="K") return 10;
+    else return "Invalid card rank";
 }
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,27 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
+    const aceOfSpades = getCardValue("A♠");
+    expect(aceOfSpades).toEqual(11);
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for 5 of Spades", () => {
+    const aceOfSpades = getCardValue("5♠");
+    expect(aceOfSpades).toEqual(5);
+    });
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Q of Spades", () => {
+    const aceOfSpades = getCardValue("Q♠");
+    expect(aceOfSpades).toEqual(10);
+    });
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Spades", () => {
+    const aceOfSpades = getCardValue("A♠");
+    expect(aceOfSpades).toEqual(11);
+    });
 // Case 5: Handle Invalid Cards:
+test("should return Invalid card rank for Invalid Inputs", () => {
+    const aceOfSpades = getCardValue("M♠");
+    expect(aceOfSpades).toEqual("Invalid card rank");
+    });
