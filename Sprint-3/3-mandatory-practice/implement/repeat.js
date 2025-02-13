4916032fedc34a15204b748aba3be401c94dabdf
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-13 21:38:40 +0000

    this commit is made for the final question of section 3  : repeated string

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,7 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(strToRepeat,howMuchRep) {
+    if(howMuchRep>=0) return strToRepeat.repeat(howMuchRep);
+    else return "Negative Counts are not valid";
+   
 }
 
 module.exports = repeat;
