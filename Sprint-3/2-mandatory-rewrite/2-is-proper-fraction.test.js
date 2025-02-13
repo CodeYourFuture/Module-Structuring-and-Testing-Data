296c83121909ddb77a20cf4dacc583b8c5ed548b
eyuell21 <abrahameyuel4@gmail.com>
2025-02-13 21:18:55 +0000

    completed mandatory-rewrite sprint 3

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,15 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("Should be able to identify Improper fractions (5/2)", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
-
+test("Should be able to identify Improper fractions (-5/2)", () => {
+  expect(isProperFraction(-5, 2)).toEqual(false);
+});
 // Case 4: Identify Equal Numerator and Denominator:
+test("Should be able to identify Improper fractions (5/5)", () => {
+  expect(isProperFraction(5, 5)).toEqual(false);
+});
