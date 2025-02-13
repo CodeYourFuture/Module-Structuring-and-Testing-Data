4c2d762a2b75fa8c0c6efeb19fa0ffffb572da39
emladeno <mladenova.elvira@yahoo.com>
2025-02-13 18:49:06 +0000

    card value test cases

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,25 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return correct value for number cards (2-10)", () => {
+    expect(getCardValue("5♥")).toEqual(5);
+    expect(getCardValue("9♦")).toEqual(9);
+  });
+  
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards (J, Q, K, 10)", () => {
+    expect(getCardValue("J♣")).toEqual(10);
+    expect(getCardValue("Q♠")).toEqual(10);
+    expect(getCardValue("K♥")).toEqual(10);
+    expect(getCardValue("10♦")).toEqual(10);
+  });
+  
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace (A)", () => {
+    expect(getCardValue("A♦")).toEqual(11);
+  });
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+    expect(() => getCardValue("Z♠")).toThrow("Invalid card rank");
+  });
