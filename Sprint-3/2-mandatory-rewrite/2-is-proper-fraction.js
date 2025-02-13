b23f54fa5b13ab3b3df29fb70f1a3cdecc44a7a9
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-13 19:57:10 +0000

    This commit is done for question 2 of Sprint3 folder 2

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,13 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
     // add your completed function from key-implement here
+    else if (numerator > denominator) return false; // Improper Fraction check
+    else if (numerator<0 && denominator>numerator) return true;// Negative Fraction check
+    else if (numerator===denominator) return false;//equalFraction
+    //down here are part of the Stretch goals
+    else if (numerator === 0 ) return true; //Proper fraction
+    else if (denominator === 0) return false; //Invalid input
+    else if (numerator < 0 && denominator < 0 && numerator > denominator) return true; //Negative cancellation
 }
 
 module.exports = isProperFraction;
