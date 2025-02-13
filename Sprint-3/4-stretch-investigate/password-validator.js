fd6aa55a19fc32b7a74d053413a28c42d0fe3295
eyuell21 <abrahameyuel4@gmail.com>
2025-02-13 21:21:28 +0000

    completed stretch-investigate on sprint 3

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,25 @@
+// function passwordValidator(password) {
+//     return password.length < 5 ? false : true
+// }
+
+const passwordRequirements = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{5,20}$/;
+
+let passwordsUsed = [];
+
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+    if(passwordRequirements.test(password) && !passwordsUsed.includes(password)){
+        if (!passwordsUsed.includes(password)) {
+            passwordsUsed.push(password);
+        }
+        return true
+    }
+
+    if (!passwordRequirements.test(password) || passwordsUsed.includes(password) ) {
+        return false
+    }
+
 }
+
 
 
 module.exports = passwordValidator;
--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -15,12 +15,75 @@ To be valid, a password must:
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
 const isValidPassword = require("./password-validator");
+
 test("password has at least 5 characters", () => {
     // Arrange
     const password = "12345";
     // Act
     const result = isValidPassword(password);
     // Assert
+    expect(result).toEqual(false);
+}
+);
+
+
+test("Have at least one English uppercase letter", () => {
+    // Arrange
+    const password = "A12345";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+// Have at least one English lowercase letter (a-z)
+
+test("Have at least one English lowercase letter (a-z)", () => {
+    // Arrange
+    const password = "A12345a";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+// Have at least one number (0-9)
+
+
+test("Have at least one number (0-9)", () => {
+    // Arrange
+    const password = "A12345a";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+// Have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
+
+
+test("Have at least one non-alphanumeric symbols", () => {
+    // Arrange
+    const password = "A12345a#";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
     expect(result).toEqual(true);
+}
+);
+
+// Must not be any previous password in the passwords array.
+
+
+test("Have at least one English uppercase letter", () => {
+    // Arrange
+    const password = "A12345a#";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(false);
 }
 );
