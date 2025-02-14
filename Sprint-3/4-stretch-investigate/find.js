144ec9c739b6d60cc67e4b56fa4de2beac5d05f3
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-14 14:01:52 +0000

    this commit is done for sprint3 stretch-investigate| find.js

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,11 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+//the index variable starts from 0 and updates within the while loop until it reaches str.length value with an increment 1 (index ++)
+//the update continues as long as it didn't find a char in str  on that specific index. if it finds the character it immediately returns that index and stops the function.
 // b) What is the if statement used to check
+//it is used to check if the character is found in that specific index(i.e str[index]) while looping using the while loop.
 // c) Why is index++ being used?
+//it is used to change/increment the value of index so the while loop executes for all index values
 // d) What is the condition index < str.length used for?
+//it is used to loop from initial value of the index as long as it is less than length of the string/str.
