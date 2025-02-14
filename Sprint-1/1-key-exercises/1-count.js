133d42a559615c6a01ac0734843a5c516910c8cc
Bahasyd <baha.syd@gmail.com>
2025-02-14 02:12:13 +0000

    supplement

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -8,4 +8,12 @@ This line (line 3) increments the variable counter to 1.
 
 "=" does not mean "equal" as in math (in math = means equation, and in programming it means assignment).
 The "=" operator takes the value on the right (count + 1) and stores it in the variable count.
-This overwrites the old value of count.
+This overwrites the old value of count. 
+
+conclusions:
+The line count = count + 1;:
+
+Takes the old value of count, adds 1 and stores it back into count.
+The = operator updates the variable with the new value.
+
+I found the Equivalent for count = count + 1. It is count += 1 or count++.
