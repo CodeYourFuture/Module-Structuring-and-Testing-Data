2959d0894156197a956a6c46f834eb242738522c
Bahasyd <baha.syd@gmail.com>
2025-02-14 10:42:40 +0000

    !!!rules

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,14 @@
 
 const age = 33;
 age = age + 1;
+ 
+/*Use const if the variable value should not change.
+Use let if you plan to change the value.
+Changing the constant will cause the code to execute correctly.*/
+
+let age = 33;
+age = age + 1;  
+// or same option//  
+age++;
+
+console.log(age);
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+/* it won't work. we need use symbols before numbers underline (_) or dollar ($).
+const _12HourClockTime = "20:53"; or const $12HourClockTime = "20:53";
+const _24hourClockTime = "08:53"; or const $24HourClockTime = "20:53";*/
