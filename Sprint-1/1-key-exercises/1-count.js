6ebbc45a313c7dddfbc5b3ea87c4b4d2a0298b53
Bahasyd <baha.syd@gmail.com>
2025-02-14 10:01:11 +0000

    addition

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,16 +4,17 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-This line (line 3) increments the variable counter to 1.
 
-"=" does not mean "equal" as in math (in math = means equation, and in programming it means assignment).
-The "=" operator takes the value on the right (count + 1) and stores it in the variable count.
-This overwrites the old value of count. 
+//This line (line 3) increments the variable counter to 1.
 
-conclusions:
-The line count = count + 1;:
+//"=" does not mean "equal" as in math (in math = means equation, and in programming it means assignment).
+//The "=" operator takes the value on the right (count + 1) and stores it in the variable count.
+//This overwrites the old value of count. 
 
-Takes the old value of count, adds 1 and stores it back into count.
-The = operator updates the variable with the new value.
+//conclusions:
+//The line count = count + 1;:
 
-I found the Equivalent for count = count + 1. It is count += 1 or count++.
+//Takes the old value of count, adds 1 and stores it back into count.
+//The = operator updates the variable with the new value.
+
+//I found the Equivalent for count = count + 1. It is count += 1 or count++.
