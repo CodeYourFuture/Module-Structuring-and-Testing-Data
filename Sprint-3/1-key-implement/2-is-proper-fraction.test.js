65f6e13605efdb45871474684cf200ac16a5be56
sabafarjamfard <farjamfardsaba@yahoo.com>
2025-02-14 10:11:27 +0000

    Completing Sprint-3(1-key-implement)/2-is-proper-fraction.js and proper-fraction.test.js

---
+++ Sprint-3/1-key-implement/2-is-proper-fraction.test.js
@@ -0,0 +1,17 @@
+const isProperFraction = require("./2-is-proper-fraction");
+
+test("Identifies Proper Fraction", () => {
+  expect(isProperFraction(2, 3)).toBe(true);
+});
+
+test("Identifies Improper Fraction", () => {
+  expect(isProperFraction(5, 2)).toBe(false);
+});
+
+test("Identifies Negative Proper Fraction", () => {
+  expect(isProperFraction(-4, 7)).toBe(true);
+});
+
+test("Identifies Equal Numerator and Denominator as Improper", () => {
+  expect(isProperFraction(3, 3)).toBe(false);
+});
