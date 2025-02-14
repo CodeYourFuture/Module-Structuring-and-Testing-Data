6b790bb1286095a0d08458968e0a6862c6e82676
loveth900 <ojay_love@yahoo.com>
2025-02-14 15:14:51 +0000

    the card value was declared

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,30 @@
 const getCardValue = require("./3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
 
 // Case 2: Handle Number Cards (2-10):
 // Case 3: Handle Face Cards (J, Q, K):
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
+
+
+// Case 1: Ace of Spades:
+// When the card is Ace of Spades (A♠),
+// Then the function should return 11
+
+// Case 2: Handle Number Cards (2-10):
+// When the card is a number between 2 and 10 (with or without suit),
+// Then the function should return the card's numerical value
+
+
+// Case 3: Handle Face Cards (J, Q, K):
+// When the card is a face card (J, Q, K) with or without suit,
+// Then the function should return 10
+
+// Case 4: Handle Ace (A):
+// When the card is an Ace (A) with or without suit,
+// Then the function should return 11
+
+// Case 5: Handle Invalid Cards:
+// When the card is invalid (e.g., symbols, out-of-range numbers, empty string),
+// Then the function should throw an error with the message "Invalid card value"
+
