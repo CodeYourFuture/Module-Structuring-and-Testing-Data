de236b7098679ec01779e12b3c180d38aefea11a
AmirMontazeri <mr.amir.mti@gmail.com>
2025-02-14 13:20:58 +0000

    sprint2-mandatory-errors-1.js-done

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,14 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// because return; is in own line without a and b, probably we get undefined.
+
+// =============> write your explanation here
+//When JavaScript sees return, it stops the function immediately nad a + b never run.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
