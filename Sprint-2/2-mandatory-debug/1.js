b7de87bf19abf4ae3da2151d1ca0fb95c68ae2dc
Bahasyd <baha.syd@gmail.com>
2025-02-15 16:04:54 +0000

    2.3

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+/*function sum(a, b) {
+  return; a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
+
+// =============> write your explanation here
+/*In JavaScript, if you follow return with a semicolon 
+or move the statement to a new line, the interpreter treats this as the end of the statement. So the code a + b after
+ return is not executed. We should delete a semicolon after return. */
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
