e64b5e41d09f26e743e7259cfb32aadcf5a4615d
Elchascode <elchaschool@gmail.com>
2025-02-15 20:31:50 +0000

    declared a number variable on this project

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,26 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// Answer: the error would be the "num" is not declared
 
-function square(3) {
-    return num * num;
-}
+// let number = 5
+// function square(3) {
+//     return num * num;
+// }
+
 
 // =============> write the error message here
 
 // =============> explain this error message here
+// Answer: Users\elcha\OneDrive\Desktop\CYF\Module_structuring_and_testing\Module-Structuring-and-Testing-Data\Sprint-2\1-key-errors\2.js:10 function square(3) {
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+let number = 5
+function square(num) {
+    return num * num;
+}
 
+console.log(square(number));
