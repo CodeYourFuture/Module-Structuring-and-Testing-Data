238ca63d9b0f9e71cf6918a0d58ac958da82378f
christelleb <christelleboten@gmail.com>
2025-02-15 11:15:37 +0000

    4-randoms commit

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,8 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Here, Math.rondom creates a random number between 0 to 1 and Math.floor rounds donw to the nearest integer. 
+// When creating a random number between 0 and 1, we will multiply it by the (100 - 1 + 1), then round it down to the nearest integer
+// Then add all of that to 1.
+
