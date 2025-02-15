2a1f2de6f86e4edc4bbf342f50f3dec262a012c0
sab325 <sabshrestha324@gmail.com>
2025-02-15 16:26:30 +0000

    explanation about expression, operand and operator

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,20 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-It will evaluate the right hand side argument and add the varaible which is 0+1 = 1
+
+// In this code variable is declare as count = 0,  expression increments the value of count by 1 and variable count is updated to be count + 1.
+
+
+//An expression is any valid unit of code that have value. In this code , we have different types of expressions:
+// Assignment, arithmetic and compound assignment.
+// An operand is a value or variable that an operator acts upon in an expression.
+
+//count = 0	(Assignment	count) 
+//count + 1	(Arithmetic	count) 
+//count = count + 1	(Compound Assignment)
+
+// count 0 =
+//count 1 =
+// count + 1 = (this is operand) all this (=, + and = are operator)
+// So in this code count = 0 is an assignment expression because it assigns a value (0) to the variable count.
+//The = operator is the assignment operator.
