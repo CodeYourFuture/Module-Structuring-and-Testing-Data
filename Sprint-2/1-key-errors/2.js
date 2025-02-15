853f5eeb10fdcb544e08985baff9c2c8ab9c612a
Bahasyd <baha.syd@gmail.com>
2025-02-15 14:38:57 +0000

    3

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -11,10 +11,20 @@ function square(3) {
 
 // =============> write the error message here
 
+// SyntaxError: Unexpected number
+
 // =============> explain this error message here
+
+/*SyntaxError: Unexpected number means that JavaScript encountered a number where 
+it expected an identifier (the name of a variable, parameter, or other valid expression).*/
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+
+function square(num) {
+    return num * num;
+}
+console.log(square(5)); //25
 
 
