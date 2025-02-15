79db05abcfd675f671fe6159945d375d32ce5998
loveth900 <ojay_love@yahoo.com>
2025-02-15 08:16:24 +0000

    done

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -4,3 +4,7 @@ function passwordValidator(password) {
 
 
 module.exports = passwordValidator;
+
+console.log(passwordValidator("1234"));   // Output: false (too short)
+console.log(passwordValidator("12345"));  // Output: true  (valid)
+console.log(passwordValidator("abcdef")); // Output: true  (valid)
