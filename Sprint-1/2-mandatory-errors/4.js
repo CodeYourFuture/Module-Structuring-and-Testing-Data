0bf725632dbf8364e53659233f670a3596bbec36
sab325 <sabshrestha324@gmail.com>
2025-02-15 19:24:33 +0000

    fixed camelcase

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,5 +1,5 @@
 const twelveHourClockTime = "20:53";
-const twentyFourhourClockTime = "08:53";
+const twentyFourHourClockTime = "08:53";
 console.log(twelveHourClockTime);
-console.log(twentyFourhourClockTime);
+console.log(twentyFourHourClockTime);
 //it says syntax error because variable should start with letter or underscore.
