9b4819f9676e325b4a1eab02b38c1df15bb71901
Bahasyd <baha.syd@gmail.com>
2025-02-15 15:26:22 +0000

    2.2

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -2,13 +2,20 @@
 
 // =============> write your prediction here
 
-function multiply(a, b) {
+/*function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);*/
 
 // =============> write your explanation here
+//We added return statement to the function
+
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return (a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
