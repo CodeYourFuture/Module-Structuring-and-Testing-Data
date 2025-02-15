b1f3d0f7d9141ddd4aa9d67dee04f8ba59718b2f
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-02-15 15:15:47 +0000

    Updated answers for key-exercises 1 -4

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,15 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex);
+const lastDotIndex = base.lastIndexOf(".")
+const ext = base.slice(lastDotIndex);
+
+console.log(`Dir: ${dir}`);
+console.log(`Ext: ${ext}`);
+
 
 // https://www.google.com/search?q=slice+mdn
+
+ 
+
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+
+// The num represents a random number between 1 and 100
+// Math.random() generates a decimal from 0 to 1
+// (maximum - minimum + 1) sets the range. For example, 100 - 1 + 1 = 100, so the range ends up being 100
+// Math.floor() rounds a decimal down to the nearest whole number, ensuring a result between 0 and 99
