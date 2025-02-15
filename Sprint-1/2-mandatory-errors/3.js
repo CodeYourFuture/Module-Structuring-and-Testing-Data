feca213fbdc8cbb2d8bc4e35f24b1545889923b5
sab325 <sabshrestha324@gmail.com>
2025-02-15 19:46:06 +0000

    modify through type conversion

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,6 +1,6 @@
-const cardNumber = "4533787178994213";
-const last4Digits = cardNumber.slice(-4);
-console.log(last4Digits);
+const cardNumber = 4533787178994213;   
+const last4Digits = String(cardNumber).slice(-4); // convert to string and extract last 4 digit
+console.log(last4Digits); //print4213
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
@@ -9,4 +9,8 @@ console.log(last4Digits);
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
 //IN line 1  their was't  any string that may be the reason.
-With out changing code its says TypeError
+//With out changing code its says TypeError
+
+/* how would you modify the code (through type conversion) so that it can still extract the last 4 digits from its value.
+To use slice() method we need to use string.
+
