6e0fa51661d35d205dc7ca3707fbbe648f43b15c
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-02-15 15:36:40 +0000

    Completed mandatory interpret file: questions 1-3

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -13,13 +13,30 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
+// There are 6 variable declarations
+
+
 // b) How many function calls are there?
+
+// There's only one function call - console.log(result);
+
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+// The expression gives the remainder after dividing the movie length by 60
+
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+
+// Line 4 is remaining seconds from the movie length, then divides it by 60 to get the total minutes.
+
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
+// The result variable shows the movie duration in string format "HH:MM:SS" 
+// A better name for this variable could be timeStamp
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+// The code only works for values that are positve, but not for negative number or 0.
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,18 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. Line 3: const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+// Removes the "p" at the end, leaving just "399" as a string.
+
+// 3. Line 8: const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+// Ensures the pence value has three digits by adding a 0 ast the start.
+
+// 4. Line 9: const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
+// Removes everything except the last two digits to get the pound amount (i.e: "399" keeps the "3", which is the £3)
+
+// 5. Line 14: const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+// Gets the last two digits pence and ensures they are always two digits.
+
+// 6. Line 18: console.log(`£${pounds}.${pence}`);
+// This prints the final price £3.99
