29eb4488fd7ae4a8abafa778562edf6ed6e10dea
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-16 17:23:16 +0000

    2 - answer question E

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -30,4 +30,6 @@ console.log(result);
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
+// It represents the current time in the movie, so maybe 'timestamp' could be a better name.
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
