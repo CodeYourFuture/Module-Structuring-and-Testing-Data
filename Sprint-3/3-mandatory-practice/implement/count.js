4a3ca28b494555e3f9944b139d103e03cd808a7e
emladeno <mladenova.elvira@yahoo.com>
2025-02-16 19:20:55 +0000

    implementing a function for the test

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,14 @@
 function countChar(stringOfCharacters, findCharacter) {
-    return 5
-}
+        let count = 0; // Start with count at 0
+      
+        for (let i = 0; i < str.length; i++) { // Loop through each character in the string
+          if (str[i] === char) { // If the character matches the one we're looking for
+            count++; // Increase the count
+          }
+        }
+      
+        return count; // Return the total count
+      }
+
 
 module.exports = countChar;
