6a8d65ba4594b5fa1dbb531e7eb868a90f95530e
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-16 16:52:33 +0000

    answer question A

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -12,6 +12,7 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// 4 function calls, in lines 4, 5, 8, and 10.
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
 
