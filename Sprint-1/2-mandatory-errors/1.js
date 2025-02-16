66ae0d69b75fa1f7575e9eb0030204e8a9642ca1
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-16 15:51:10 +0000

    explain reassignment to const error.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,5 @@
 
 const age = 33;
 age = age + 1;
+
+// You cannot reassign a constant variable. To create a variable that needs to be reassigned, the 'let' keyword should be used instead of 'const'.
