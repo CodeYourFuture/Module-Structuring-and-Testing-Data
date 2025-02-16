4355afc7b524b82f9bdb92a43b58b209d8ee6be7
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-16 16:18:33 +0000

    fix: slice is not a function error.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,4 +1,4 @@
-const cardNumber = 4533787178994213;
+const cardNumber = [4, 5, 3, 3, 7, 8, 7, 1, 7, 8, 9, 9, 4, 2, 1, 3];
 const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
@@ -7,3 +7,10 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// Prediction: slice takes an index or index range, and I don't think you can have negative indexes, so that will be the error.
+// Outcome: says '.slice' is not a function
+// Fix: slice is an array method not a string method. convert card number into an array.
+
+// Output
+console.log(last4Digits);
