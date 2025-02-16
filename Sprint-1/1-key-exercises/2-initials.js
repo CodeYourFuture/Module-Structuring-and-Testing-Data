465e60ef4eaa0b0d30b89c1b0671237f0a283813
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-16 16:32:28 +0000

    small changes.

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,12 +5,16 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
+// https://www.google.com/search?q=get+first+character+of+string+mdn
+
+// MY SOLUTION START //
+
 // Get first character of each name, store it in a string, and remove spaces with trim().
 let initials = `${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(
   0
-)}`.trim(); 
+)}`.trim();
 
 // View output to check if 'initals' variable matches the expected output 'CKJ'.
 console.log(initials);
 
-// https://www.google.com/search?q=get+first+character+of+string+mdn
+// MY SOLUTION END //
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,3 +1,3 @@
-// Variables can't start with a number.
+// Variable names can't start with a number.
 const hour24ClockTime = "20:53";
 const hour12ClockTime = "08:53";
