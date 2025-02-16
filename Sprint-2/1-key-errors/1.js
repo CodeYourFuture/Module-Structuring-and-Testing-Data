071ae483fb1edfff2df122d5de7bc523cfaf56d1
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-16 23:50:18 +0000

    Answer | sprint-2 | key-errors 1-1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,10 +2,10 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
+//  Answer | Variable decimalNumber declared twice in the convertToPercentage frame
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+/* function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
@@ -13,8 +13,19 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+*/
 
 // =============> write your explanation here
-
+// Answer | Added the variable declaration in the global frame and called the function with the parameter already declared.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+
+function convertToPercentage(decimalNumber) {
+  
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+const decimalNumber = 0.5;
+console.log(convertToPercentage(decimalNumber));
