9993b5a54164962fd6f3c593c3c1e86bc3f342cb
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-16 00:35:43 +0000

    Please ignore this

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,18 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// Predict and explain first...
+// =============> write your prediction here
+// that we will have a*b and the next line undefined
+
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+// Line 10 gets executed first, because we call the function multiply in this line, it will direct to line 6 and execute the function and show the argument of the function which is 32 and 10, then it will return undefined because the function does not return anything.
+// Finally, correct the code to fix the problem
+// We should return the value of the function instead of console.log it.
+//  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`); 
