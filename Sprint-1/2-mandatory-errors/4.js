bdd7693ef58ae11d28fd534ac46262f38a9f2daf
Faithy4444 <faithy4444@gmail.com>
2025-02-17 22:15:17 +0200

    time

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,4 +1,6 @@
-const twelveHourClockTime = "20:53";
-const twentyFourHourClockTime = "08:53 pm";
+
 
 //SyntaxError because variable cannot start with a number
+
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53 pm";
