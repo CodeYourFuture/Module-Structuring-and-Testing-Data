fca7809a45f925466789fc96ec884db2203fccfa
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-02-17 23:57:26 +0000

    Answered all questions and fixed the errors in the Key Errors file

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// I predict a SyntaxError because str is declared twice in the function.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +12,13 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+
+// The capitalise function takes str as a paramenter but tries redeclaring it with the let variable, which isn't allowed in Javascript
+
+
 // =============> write your new code here
+
+function capitalise(str) {
+   str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
