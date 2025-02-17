47039dce6169d4c934eb6ffc9adb16c99496b734
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-17 00:21:16 +0000

    Answer | sprint-2 | debug 2-0

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -2,13 +2,20 @@
 
 // =============> write your prediction here
 
-function multiply(a, b) {
+// Answer | There will be an error. 
+// console.log will just print the multiplication result in the console, will not pass the multiplication value.
+/* function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
+*/
 // =============> write your explanation here
-
+// The console.log in the multiply frame should be replaced with return so that the function will return the value of the function when called.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return(a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
