6c1c52551b50fe0ef306be74be26b315ce4bdebd
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-17 00:39:46 +0000

    Answer | sprint-2 | debug 2-1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
-function sum(a, b) {
+// Undefined error as the return statement is followed by ";", line 6 it will not be executed and not return anything. 
+/*function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
+*/
 // =============> write your explanation here
+//return statement corrected, added on the same line before ';' 
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
