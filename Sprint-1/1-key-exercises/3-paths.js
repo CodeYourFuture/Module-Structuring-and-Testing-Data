2386cdd6f4d48a765723f9d8b71d5bd26e0bc171
Aden Stanley Jones <adenj1802@gmail.com>
2025-02-17 20:58:35 +0200

    Mandatory-errors completed

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -11,13 +11,18 @@
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
-const base = filePath.slice(lastSlashIndex + 1);
+const base = filePath.slice(lastSlashIndex + 2);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(7, lastSlashIndex);
+const lastDotIndex = filePath.lastIndexOf(".")
+const ext = filePath.slice(lastDotIndex);
+console.log( `this is the dir part ${dir}`);
+console.log(`This is the ext part ${ext}`);
 
-// https://www.google.com/search?q=slice+mdn
+
+// the lastSlashIndex variable finds the last slash in the file path using the lastIndexOf function
+// with the slice function it removes the number of characters based on what is inside of the brackets
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,6 +2,14 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+console.log(num);
+
+//const mininum is assigned to number 1
+//const maximum is assigned to number 100
+// const num has the expression Math.floor
+// which rounds the answer within Math.random multiplied maximum minus minimum + 1
+// the answer of that expression is then added to the minimum
+// what program basically does is that it selects a random number between 1 and 100
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
