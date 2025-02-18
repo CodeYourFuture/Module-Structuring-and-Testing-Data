d2b4a998b826e9f138f0029b069e1b46bad9a760
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-02-18 18:06:34 +0000

    Completed Mandatory implement files by fixing errors and answering all the questions

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,16 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+
+function toPounds(penceString) {
+
+    let penceStringWithoutTrailingP = penceString.slice(0, - 1);
+    let pounds = Number(penceStringWithoutTrailingP) / 100;
+    return "£" + pounds.toFixed(2);
+}
+
+console.log(toPounds("399p"))
+console.log(toPounds("5000p"))
+console.log(toPounds("45p"))
+console.log(toPounds("5p"))
