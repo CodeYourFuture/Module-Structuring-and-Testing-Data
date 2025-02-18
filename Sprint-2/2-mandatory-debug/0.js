ddb94134676488a0f5f508f3fca03852a3f565cd
Amir200524 <amiraminia@yahoo.com>
2025-02-18 16:00:27 +0000

    function multiply

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,23 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+//It does not return a value and  it does not return a value.
 
+//function multiply(a, b) {
+ // console.log(a * b);
+//}
+
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+//The function does not return a value; it only prints it.
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b; 
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
 
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
