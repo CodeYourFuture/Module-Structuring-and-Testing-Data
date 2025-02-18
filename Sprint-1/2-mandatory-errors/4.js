6c29b8118eb8bcab4aaa84e883d7a3c987065480
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-18 14:02:14 +0000

    Naming Variables

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,4 +1,5 @@
 const twelveHourClockTime = "20:53";
 const twentyFourHourClockTime = "08:53";
 
-//Answer: we can declare or name our variables using numeric values
+//Answer: we can declare or name our variables using numeric values, but we can't start with a number, so we can't name our variables as 20:53 or 08:53
+// Furthermore, the variable's name doesn't match the content of the variables and the naming should be opposite, to match the content of the values.
