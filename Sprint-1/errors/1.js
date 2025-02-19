994e2f118e9ea4a77a43b4131bb63ad1f02013e6
Mansoor <mansoor_m19@hotmail.co.uk>
2025-02-19 17:21:57 +0000

    used let varriable to reassign value instead of connst

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
+// const variable cannot be changed and thus we need to use let variable if we want to reassign any value.
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
