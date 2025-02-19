1238ae577a9ba1bbb8471fb5207259747b5b0404
Priscilla <priscilla.emebo@gmail.com>
2025-02-19 17:12:09 +0000

    type of error

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,5 +1,7 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
 
+
+SyntaxError: Unexpected identifier 'is'
 Unexpected identifier occurs when code includes a plain English sentence instead of valid JavaScript syntax.
 This happened because a comment was included without being properly formatted
