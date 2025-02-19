08cbaff40daaedf4a6adbc82af76ffbc8622fae5
Bahasyd <baha.syd@gmail.com>
2025-02-19 02:22:51 +0000

    3-3mandatory

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,10 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+function toPounds(pence) {
+    const pounds = pence / 100;
+    return `£${pounds.toFixed(2)}`;
+}
+console.log(toPounds(399)); // £3.99
+console.log(toPounds(500)); // £5.00
+console.log(toPounds(1234)); // £12.34
