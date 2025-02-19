aeccacf0caa13641e226901b4f5079ec41d10e18
sheetalkharab <sheetalrana89@gmail.com>
2025-02-19 22:04:11 +0000

    change in getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,13 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    let rank = card.slice(0, -1);// extract rank everything except last character(suit)
+    if (rank === "A") {
+        return 11
+    }else if(!isNaN(rank) && rank>="2" && rank<="9"){
+       return parseInt(rank, 10);
+    }else if (rank === "K" || rank === "J" || rank === "Q" || rank === "10") {
+       return 10;
+    }else{
+       throw new Error("Invalid card rank.");
+    }
 }
 module.exports = getCardValue;
