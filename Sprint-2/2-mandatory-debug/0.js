ccb0cf160d5d6c94e72089b5510a1b4b6e9a4682
obert <obertilimandzi@gmail.com>
2025-02-19 21:26:49 +0200

    fixed the undefined error

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+  // There will be a result of "multiply" in the console output so will get "320"
 
-function multiply(a, b) {
-  console.log(a * b);
+/*function multiply(a, b) {
+  console.log(a * b); //print
 }
-
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);*/
 
 // =============> write your explanation here
+  // cosole.log was printing the the result and so nothing was returning the function
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
