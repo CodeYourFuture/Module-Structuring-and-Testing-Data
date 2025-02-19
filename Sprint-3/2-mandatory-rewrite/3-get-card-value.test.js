f24f3e985d67a9e6ffd21455d71c4183d217c472
sheetalkharab <sheetalrana89@gmail.com>
2025-02-19 22:04:45 +0000

    write test cases for getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,27 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for Five of Hearts", () => {
+    const fiveofHearts = getCardValue("5♥");
+    expect(fiveofHearts).toEqual(5);
+    });
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Face Cards", () => {
+    const faceCards = getCardValue("10♦");
+    expect(faceCards).toEqual(10);
+    });
+
+
 // Case 4: Handle Ace (A):
+test("should return 10 for handleAce", () => {
+    const handleAce = getCardValue("A♠");
+    expect(handleAce).toEqual(11);
+    });
+
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card input", () => {
+    expect(() => getCardValue(" ")).toThrow("Invalid card rank.");
+    expect(() => getCardValue("X♠")).toThrow("Invalid card rank.");
+  });
+  
