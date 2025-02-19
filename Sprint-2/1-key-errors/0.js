e4e77b04c4cf6ea7552bde89bd9fd6a6cfe244dd
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-19 21:18:33 +0000

    1-key-errors: fix code.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -6,12 +6,10 @@ capitalise("String input.");
 
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
-
 // =============> write your explanation here
 // The error occurs because you cannot declare a variable with a name that exists in the same scope.
 
 // =============> write your new code here
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
