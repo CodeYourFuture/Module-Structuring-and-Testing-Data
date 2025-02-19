7a13c497789fc1e261fb855b9d12614e21000fd0
obert <obertilimandzi@gmail.com>
2025-02-19 21:03:36 +0200

    i corrected the code

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,24 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+    // there will be an error because there is an integer as a parameter
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+  //  return num * num;}
 
 // =============> write the error message here
+    //SyntaxError: Unexpected number
 
 // =============> explain this error message here
+    // There is supposed to be letters passed as parameters and not integers 
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
+
+console.log(square(9));
 
