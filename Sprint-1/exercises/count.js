395bfe5c82222147f56fb838658d183a30e5a770
Mansoor <mansoor_m19@hotmail.co.uk>
2025-02-19 18:16:45 +0000

    added multiple line comment to describe the code

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/*Line 3 uses the = operator to assign a value to a variable. In this example, we are reassigning the value of previously declared variable by using the = operator, 
+then calling the same variable and adding 1 to it. */
