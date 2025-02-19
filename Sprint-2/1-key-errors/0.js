1c84fed774e41520a7419851ece7e480d1d6376e
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-19 21:13:41 +0000

    1-key-errors: call function.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -2,6 +2,8 @@
 //  Capitalise 1st letter of a string.
 
 // call the function capitalise with a string input
+capitalise("String input.");
+
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
