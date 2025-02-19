e8e4a395deef96dea8ef82c79ff2cc259258f6e4
Mansoor <mansoor_m19@hotmail.co.uk>
2025-02-19 17:31:53 +0000

    the error was created due to calling the variable before it was  initalised.

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// the variable needs to be declared first before it can be called. I
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
