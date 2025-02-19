506ed81d9e458fde75572d4c8f4086b2f0e2c667
Priscilla <priscilla.emebo@gmail.com>
2025-02-19 17:08:24 +0000

    Error-0

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+
+Unexpected identifier occurs when code includes a plain English sentence instead of valid JavaScript syntax.
+This happened because a comment was included without being properly formatted
