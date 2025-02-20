099e4bd7339ba1bd60d295f4d51c4a375dbcbb3d
Priscilla <priscilla.emebo@gmail.com>
2025-02-20 07:26:41 +0000

    pence-to-pounds

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,35 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+/* 2) const penceStringWithoutTrailingP = penceString.substring(
+      0,
+      penceString.length - 1
+      ); : Removes the trailing P
+  .substring(0, penceString.length - 1) extracts a substring from the beginning up to (but not including) the last character
+  Therefore removing P and only 399 is left as a string
+
+  3) const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    This makes sure that the string has at least 3 digits and leading zeros can be added if needed,
+    
+  4) const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+This code extracts everything except the last two characters 
+The last two characters represent pence while the first part of the string represent the value in pounds
+So the code extracts the value in pounds
+
+5) const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+  This code extracts the value in Pence and makes sure it has 2 values
+
+  .substring(paddedPenceNumberString.length - 2) extracts the last two characters which is the value of pence
+  while .padEnd(2, "0") makes sure the pence value is always have two digits.
+
+  6) console.log(`£${pounds}.${pence}`);
+     This is the format for the return value
+     It logs the data to the console and returns the value of the given parameter in this format: console.log(`£${pounds}.${pence}`);
+
+  */
