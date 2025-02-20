af48a45a01c99f53442e913990b55be6a00ef44c
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 15:46:10 +0000

    2-mandatory-debug/1.js: fix code.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -2,15 +2,14 @@
 //  =============> write your prediction here
 // Answer: function returns nothing (undefined), because it is on a different line from the return statement and isn't wrapped in parentheses ().
 
-function sum(a, b) {
-  return;
-  a + b;
-}
-
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
 // =============> write your explanation here
 // Answer: function calls of sum() return undefined as the statement is on a separate line from the return statement without braces ().
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// Answer:
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
