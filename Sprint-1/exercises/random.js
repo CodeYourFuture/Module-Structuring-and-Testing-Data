0e6e06cf1bc5a4adb5e01b9bd47e4a8e2be72efd
Mansoor <mansoor_m19@hotmail.co.uk>
2025-02-20 21:03:35 +0000

    added multiple line comment explaining how the code is executed.

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -5,5 +5,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
+/*
+ So before Math.floor method is exectued, we first run  math.random which randomly picks a number between 0-1, and then it is multiplied by 100. 
+ Next, math.floor rounds down the number to the  nearest integer. Finally, 1 is added to the final value and stored inside num variable. 
+*/
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(num);
