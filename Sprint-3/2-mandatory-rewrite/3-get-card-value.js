ba5c4686639cca38cf50b850d945a09951510973
Elchascode <elchaschool@gmail.com>
2025-02-20 23:42:55 +0000

    corrected a code

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,18 +1,13 @@
 function getCardValue(card) {
-if (rank === "A"){
-     return 11;  // Ace is worth 11
-  }
-
-  else if (rank >= 2 && rank <= 10 ) {
+  let rank = card.slice(0, -1);
+  if (rank === "A") {
+    return 11; // Ace is worth 11
+  } else if (rank >= 2 && rank <= 10) {
     return Number(rank); // convert number to be between 2-10
+  } else if (["J", "Q", "K"].includes(rank)) {
+    return 10; // face card is worth 10
+  } else {
+    throw new Error("Invalid card rank"); // throw an error when rank is invalid
   }
-  else if ([rank = "J", "Q", "K",].includes(rank)) {
-    return 10;  // face card is worth 10
-  }
-  
-  else{
-    throw new Error("Invalid card rank");  // throw an error when rank is invalid
-}
-
 }
 module.exports = getCardValue;
