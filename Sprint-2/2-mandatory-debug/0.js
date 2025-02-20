d88c4c52efbd6c1041a112e482304d77104cc870
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 15:31:26 +0000

    2-mandatory-debug/0.js: fix code - remove console.log() statement in function, and add a return statement.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -3,14 +3,13 @@
 // =============> write your prediction here
 // Answer: logs the product of 2 numbers to the console.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
-
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
 // =============> write your explanation here
 // Answer: multiples 2 numbers, and logs them to the console. However the function doesn't include a return statement, so the output of the function will default to undefined.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
