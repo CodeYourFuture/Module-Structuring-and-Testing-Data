6c47d6b3944197c44f65ce8e5339b0800b02d061
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 16:01:07 +0000

    2-mandatory-debug/2.js: fix the program.

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -4,16 +4,6 @@
 // =============> Write your prediction here
 // Answer: Error because parameters aren't defined, but arguments are passed in.
 
-const num = 103;
-
-function getLastDigit() {
-  return num.toString().slice(-1);
-}
-
-console.log(`The last digit of 42 is ${getLastDigit(42)}`);
-console.log(`The last digit of 105 is ${getLastDigit(105)}`);
-console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
 // Now run the code and compare the output to your prediction
 // =============> write the output here
 // Answer: It just gets the last digit of the variable 'num'.
@@ -22,10 +12,17 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 // =============> write your explanation here
 // Answer: Parameters aren't defined, so arguments being passed in arent used, so it just uses the 'num' variable referenced within the function as opposed to the arguments passed in.
 
+// This program should tell the user the last digit of each number.
+// Explain why getLastDigit is not working properly - correct the problem
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 // Answer:
+const num = 103;
 
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
-// Answer:
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
