a78888dae4fe457a2f5576e78a6e9a69a0a54964
Priscilla <priscilla.emebo@gmail.com>
2025-02-20 05:24:34 +0000

    percentage-change

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -20,3 +20,21 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+/* 
+a) Function calls were made in lines 4, 5 and 10
+
+b) The error is coming from line 5
+Type of error is SyntaxError: missing ) after argument list
+The function call replaceAll("," "") is missing a comma between arguments. 
+It should be written as replaceAll(",", "")
+
+c) Lines that are variable reassignment statements are: lines 4 and 5
+The existing variables were assigned new values
+
+d) Lines that are variable declarations are: lines 1, 2, 7, and 8
+
+e) The expression Number(carPrice.replaceAll(",","")) is used for converting a formatted string that contains commas into a numerical value so it can be used in mathematical calculations
+carPrice.replaceAll(",", ""): The replaceAll function removes all commas from the string "10,000", resulting in "10000".
+Number("10000"): The Number function converts the string "10000" into a numeric value 10000.
