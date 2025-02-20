fcc3ab84512c876ba7b1b71dbb5fc44499d0abf5
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-20 15:25:02 +0000

    Comments added to password validator function

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,26 +1,39 @@
-
+//this array keeps track of passwords that are validated
 let passwords=[];
 function passwordValidator(password) {
+    // password length is less than 5 characters
     if (password.length < 5) {
         return 'Your password should be at least 5 characters';
     }
+
+    // password has no uppercase letter
     else if (!/[A-Z]/.test(password)) {
         return 'Your password should have at least one uppercase letter';
     } 
+
+    // password has no lowercase letter
     else if(!/[a-z]/.test(password)){
         return 'Your password should have at least one lowercase letter';
     }
+
+    // password has no number
     else if(!/[0-9]/.test(password)){
         return 'Your password should have at least one number';
     }
+
+    // password has no non-alphanumeric symbols
     else if(!/[!@#$%^&*]/.test(password)){
         return 'Your password should have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")';
     }
+
+    //if the password passes all above conditions
     else {
-        
+        // if the password is not found in the passwords array
         if (passwords.includes(password)) {
             return 'Your password has already been used';
         } 
+
+        // if the password is found in the passwords array
         else {
             return 'Your password is updated successfully';
             passwords.push(password);
