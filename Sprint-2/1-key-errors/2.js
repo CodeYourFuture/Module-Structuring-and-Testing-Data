99bd7280bfd1efda3f29859441076c8b9c445c9e
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 15:04:41 +0000

    1-key-errors/2.js: fix code.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,14 +1,9 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 // Cant set a value as a parameter (error)
-
-function square(3) {
-    return num * num;
-}
 
 // =============> write the error message here
 // SyntaxError: Unexpected number. Identifier expected.
@@ -19,5 +14,8 @@ function square(3) {
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+  return num * num;
+}
 
-
+console.log(square(3));
