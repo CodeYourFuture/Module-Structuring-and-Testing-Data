2114b3c9f06db97d5086c7df467eb3e32f024089
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-02-20 11:07:48 +0000

    finish key-errors\1.js

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,29 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> decimalNumber is declared twice, once as a parameter and once as a variable.
+// =============> console.log(decimalNumber); should be console.log(convertToPercentage(0.5));
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> SyntaxError: Identifier 'decimalNumber' has already been declared
+// =============> this error because the variable decimalNumber was declared twice.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+const decimalNumber = 0.5;
+console.log(convertToPercentage(decimalNumber));
