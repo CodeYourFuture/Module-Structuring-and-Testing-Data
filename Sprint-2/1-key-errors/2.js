430d7dcf06bbd77330cc74723b1bf0052bdd05bc
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 15:15:01 +0000

    1-key-errors/2.js: add answer comment to each answer.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,17 +3,18 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-// Cant set a value as a parameter (error)
+// Answer: Cant set a value as a parameter (error)
 
 // =============> write the error message here
-// SyntaxError: Unexpected number. Identifier expected.
+// Answer: SyntaxError: Unexpected number. Identifier expected.
 
 // =============> explain this error message here
-// The error message means a number is being used in a place where it is syntactically invalid.
+// Answer: The error message means a number is being used in a place where it is syntactically invalid.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+// Answer:
 function square(num) {
   return num * num;
 }
