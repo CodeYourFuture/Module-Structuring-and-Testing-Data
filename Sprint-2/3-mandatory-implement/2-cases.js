970eb925cb21097d7676aaed5bf0f43bae785bd2
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 16:25:37 +0000

    3-mandatory-implement/2-cases.js: implement solution.

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,9 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+// Answer:
+function upperSnakeCase(string) {
+  return string.toUpperCase().split(" ").join("_");
+}
+
+console.log(upperSnakeCase("I am a string"));
