fc0fc94f76c4fb64868f172e6c3553ca23ea32d0
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-02-20 21:01:34 +0000

    1_getAngleType_solution

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -8,18 +8,54 @@
 // Then, write the next test! :) Go through this process until all the cases are implemented
 
 function getAngleType(angle) {
-   if (angle === 90) return "Right angle";
-   // read to the end, complete line 36, then pass your test here
-}
+  
+   if (angle === 90){
+      return "Right angle";
+   }
+   else if (angle < 90) {
+    return "Acute angle";
+   }
+   else if (angle > 90 && angle < 180) {
+    return "Obtuse angle";
+   }
+   else if (angle === 180) {
+    return "Straight angle";
+   }
+   else if (angle > 180 && angle < 360) {
+    return "Reflex angle";
+   }
+  }
 
-// we're going to use this helper function to make our assertions easier to read
-// if the actual output matches the target output, the test will pass
 function assertEquals(actualOutput, targetOutput) {
   console.assert(
     actualOutput === targetOutput,
     `Expected ${actualOutput} to equal ${targetOutput}`
   );
 }
+const right = getAngleType(90);
+assertEquals(right, "Right angle");
+
+// Case 2: Identify Acute Angles:
+const acute = getAngleType(45);
+assertEquals(acute, "Acute angle");
+
+// Case 3: Identify Obtuse Angles:
+const obtuse = getAngleType(120);
+assertEquals(obtuse,"Obtuse angle")
+
+// Case 4: Identify Straight Angles:
+const straight = getAngleType(180);
+assertEquals(straight,"Straight angle")
+
+// Case 5: Identify Reflex Angles:
+const reflex = getAngleType(250);
+assertEquals(reflex,"Reflex angle")
+
+
+
+
+
+
 
 // Acceptance criteria:
 
@@ -30,27 +66,3 @@ function assertEquals(actualOutput, targetOutput) {
 // Case 1: Identify Right Angles:
 // When the angle is exactly 90 degrees,
 // Then the function should return "Right angle"
-const right = getAngleType(90);
-assertEquals(right, "Right angle");
-
-// Case 2: Identify Acute Angles:
-// When the angle is less than 90 degrees,
-// Then the function should return "Acute angle"
-const acute = getAngleType(45);
-assertEquals(acute, "Acute angle");
-
-// Case 3: Identify Obtuse Angles:
-// When the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle"
-const obtuse = getAngleType(120);
-// ====> write your test here, and then add a line to pass the test in the function above
-
-// Case 4: Identify Straight Angles:
-// When the angle is exactly 180 degrees,
-// Then the function should return "Straight angle"
-// ====> write your test here, and then add a line to pass the test in the function above
-
-// Case 5: Identify Reflex Angles:
-// When the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle"
-// ====> write your test here, and then add a line to pass the test in the function above
