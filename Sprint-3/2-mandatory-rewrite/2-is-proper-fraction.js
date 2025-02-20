044733299fde30e82c3ace958866975d5645b33a
Elchascode <elchaschool@gmail.com>
2025-02-20 18:18:14 +0000

    added function from key-implement

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,11 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
     // add your completed function from key-implement here
+    else if (denominator > numerator) return false;
+    else if (5 > 2) return false;
+    else if (Math.abs(numerator) < denominator) return Math.abs;
+    else if (numerator === denominator) return false;
+    // else if (numerator > )
 }
 
 module.exports = isProperFraction;
