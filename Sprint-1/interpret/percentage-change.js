b763b3b2bcba0a673662e1a1c5d1b11d2da2f32b
Mansoor <mansoor_m19@hotmail.co.uk>
2025-02-20 23:44:26 +0000

    fixed the syntax error on line 5 and added comments explaing the code

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,12 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-
+// Line 4 and 5
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-
+// line 5 was missing a comma in replaceAll function which seperates pattern from replacement
 // c) Identify all the lines that are variable reassignment statements
-
+// line 4,5
 // d) Identify all the lines that are variable declarations
-
+//line 1,2,7,8
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// The replaceAll returns a new string by finding all the matches of the pattern with a new replacement
