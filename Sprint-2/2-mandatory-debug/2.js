9b4abe80d2bcc6e49f8867e1dedcf9bde2725d93
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 15:57:07 +0000

    2-mandatory-debug/2.js: compare prediction to outcome.

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,7 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
-// Error because parameters aren't defined, but arguments are passed in.
+// Answer: Error because parameters aren't defined, but arguments are passed in.
 
 const num = 103;
 
@@ -16,10 +16,16 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+// Answer: It just gets the last digit of the variable 'num'.
+
 // Explain why the output is the way it is
 // =============> write your explanation here
+// Answer:
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+// Answer:
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// Answer:
