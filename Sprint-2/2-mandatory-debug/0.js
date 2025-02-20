aa985d17b9a88c7a45853352cb8859641f196f53
Sul <97115172+sul-devs@users.noreply.github.com>
2025-02-20 15:23:58 +0000

    2-mandatory-debug/0.js: write explanation of program.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-// logs the product of 2 numbers to the console.
+// Answer: logs the product of 2 numbers to the console.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -10,6 +10,7 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// Answer: multiples 2 numbers, and logs them to the console.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
