9295c66effd6f98489dfb3c992ead81598d93273
Segun <segunfolayan@yahoo.com>
2025-02-21 20:24:42 +0000

    update 1/21/02

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -2,10 +2,10 @@
 //  =============> write your prediction here
 // ${sum(10, 32)} will return undefined.
 
-//function sum(a, b) {
-  //return;
-  //a + b;
-//}
+function sum(a, b) {
+  return;
+  a + b;
+}
 
 //console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,6 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// The code will only output for the number declared first
 
 const num = 103;
 
@@ -15,10 +16,26 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+//Output
+//The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
 // Explain why the output is the way it is
 // =============> write your explanation here
+// The code will also return the last digit of num because the code does not accept parameters
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+const num = 103;
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
