1b7f4ed33866ea873f6be40f73d87ec7bf44ed6e
sheetalkharab <sheetalrana89@gmail.com>
2025-02-21 18:10:02 +0000

    write test cases for  passwordValidator function

--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -15,11 +15,73 @@ To be valid, a password must:
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
 const isValidPassword = require("./password-validator");
+const previousPasswords = ["A1b!23", "OldPass1!"];
+
 test("password has at least 5 characters", () => {
     // Arrange
     const password = "12345";
     // Act
-    const result = isValidPassword(password);
+    const result = isValidPassword(password, previousPasswords);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+test("password is missing an upperCase letter", () => {
+    // Arrange
+    const password = "12345a!";
+    // Act
+    const result = isValidPassword(password, previousPasswords);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+test("password is missing an lowerCase letter", () => {
+    // Arrange
+    const password = "123A!B";
+    // Act
+    const result = isValidPassword(password, previousPasswords);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+test("password is missing a number", () => {
+    // Arrange
+    const password = "abcde!";
+    // Act
+    const result = isValidPassword(password, previousPasswords);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+test("password is missing a special character", () => {
+    // Arrange
+    const password = "abcd123";
+    // Act
+    const result = isValidPassword(password, previousPasswords);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+test("password should not be in previous password list", () => {
+    // Arrange
+    const password = "A1b!23";
+    // Act
+    const result = isValidPassword(password, previousPasswords);
+    // Assert
+    expect(result).toEqual(false);
+}
+);
+
+test("valid password meets all criteria", () => {
+    // Arrange
+    const password = "Hello12!3";
+    // Act
+    const result = isValidPassword(password, previousPasswords);
     // Assert
     expect(result).toEqual(true);
 }
