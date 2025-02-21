cc668a29ec61d60076613e6ed1a6ff7831c1e708
Amir200524 <amiraminia@yahoo.com>
2025-02-21 09:48:50 +0000

    get angel type function

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,6 +1,14 @@
 function getAngleType(angle) {
   if (angle === 90) return "Right angle";
   // replace with your completed function from key-implement
+  if (angle < 90) return "Acute angle"; // Less than 90°
+  if (angle < 180) return "Obtuse angle"; // Between 90° and 180°
+  if (angle === 180) return "Straight angle"; // Exactly 180°
+  return "Reflex angle"; // Greater than 180° but less than 360°
+
+
+// Jest test export (for unit testing)
+module.exports = getAngleType;
 
 }
 
