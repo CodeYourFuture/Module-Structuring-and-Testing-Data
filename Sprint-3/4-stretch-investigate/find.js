259067b05d3a207bb9982324b10d163c062ddd11
sheetalkharab <sheetalrana89@gmail.com>
2025-02-21 18:07:15 +0000

    explain functionality using python visualizer

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,17 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+/*
+At starting of program index= undefined and start from 0 then 1 and increase by 1 every time till 7 as start looking for char 'u'
+and it find u in string at 7th index and return 7. for char 'z' it will look in whole string and index will start from 0 to 16 as string length is 16.
+but 'z' is not present so return -1.
+*/
 // b) What is the if statement used to check
+/*
+it will check the current char Index  match char.if it match it will return index of that char from String else return -1
+*/
 // c) Why is index++ being used?
+// To increment index value .so we can check whole string and loop can continue till last char of string .
+// every time with index++ ity gave new index for next character in String.
 // d) What is the condition index < str.length used for?
+//It insure loop will stop when our index should be greater then length of string.eg: 17<16 it will stop as string length is 16.
