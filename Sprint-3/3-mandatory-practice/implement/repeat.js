c22e8c8f61dbd8739c9d9a918bd6bc0063bff91b
Amir200524 <amiraminia@yahoo.com>
2025-02-21 18:11:58 +0000

    repeat function and test file

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,12 @@
-function repeat() {
-    return "hellohellohello";
+
+function repeat(str, count) {
+    if (typeof str !== "string" || typeof count !== "number") {
+        return "Invalid input"; 
+    }
+    if (count < 0) {
+        return "Count cannot be negative"; 
+    }
+    return str.repeat(count); 
 }
 
 module.exports = repeat;
