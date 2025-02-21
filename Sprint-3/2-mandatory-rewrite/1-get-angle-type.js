1d1706631859547c5de6085fad9a357d1a294abc
Amir200524 <amiraminia@yahoo.com>
2025-02-21 10:43:42 +0000

    get card value function and test

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -22,5 +22,4 @@ module.exports = getAngleType;
 // Don't get bogged down in this detail
 // Jest uses CommonJS module syntax by default as it's quite old
 // We will upgrade our approach to ES6 modules in the next course module, so for now 
-// we have just written the CommonJS module.exports syntax for you
-module.exports = getAngleType;
+// we have just written the CommonJS module.exports syntax for 
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,21 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    // Case 2: Handle Number Cards (2-10)
+    if (card.length === 2 && !isNaN(card[0])) {
+        return parseInt(card[0]);
+    }
+
+    // Case 3: Handle Face Cards (J, Q, K)
+    if (card === "J" || card === "Q" || card === "K") {
+        return 10;
+    }
+
+    // Case 4: Handle Ace (A)
+    if (card === "A") {
+        return 11;
+    }
+
+    // Case 5: Handle Invalid Cards
+    return null; // Invalid card returns null
 }
+
 module.exports = getCardValue;
