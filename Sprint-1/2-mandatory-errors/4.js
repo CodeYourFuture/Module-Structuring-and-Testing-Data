db2d2554e91ca44b6812cc60b4511c8dc4dabc2c
saraamiiri <amirisara1993@gmail.com>
2025-02-21 07:58:39 +0000

    4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,3 +1,3 @@
 const _12HourClockTime = "20:53";
 const _24hourClockTime = "08:53";
-//variable name can not start with number 
+//variable name can not start with number we can use letter or underscore 
