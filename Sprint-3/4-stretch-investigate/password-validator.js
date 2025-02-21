58bc89b94b5115e4ac942e758b62344ffabab231
Amir200524 <amiraminia@yahoo.com>
2025-02-21 18:50:48 +0000

    password validator function and test file

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,25 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
-}
+const passwords = ["previousPassword1", "anotherOldPassword"];
 
+function passwordValidator(password) {
+    if (password.length < 5) {
+        return false;
+    }
+    if (!/[A-Z]/.test(password)) {
+        return false;
+    }
+    if (!/[a-z]/.test(password)) {
+        return false;
+    }
+    if (!/[0-9]/.test(password)) {
+        return false;
+    }
+    if (!/[!#$%.&*]/.test(password)) {
+        return false;
+    }
+    if (passwords.includes(password)) {
+        return false;
+    }
+    return true;
+}
 
 module.exports = passwordValidator;
--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -14,13 +14,41 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
-const isValidPassword = require("./password-validator");
+const passwordValidator = require("./password-validator");
+
 test("password has at least 5 characters", () => {
-    // Arrange
     const password = "12345";
-    // Act
-    const result = isValidPassword(password);
-    // Assert
+    const result = passwordValidator(password);
     expect(result).toEqual(true);
-}
-);
+});
+
+test("password contains at least one uppercase letter", () => {
+    const password = "123A5";  // Contains uppercase "A"
+    const result = passwordValidator(password);
+    expect(result).toBe(true);
+});
+
+test("password contains at least one lowercase letter", () => {
+    const password = "123a5";  // Contains lowercase "a"
+    const result = passwordValidator(password);
+    expect(result).toBe(true);
+});
+
+test("password contains at least one number", () => {
+    const password = "aA!12";  // Contains number "1"
+    const result = passwordValidator(password);
+    expect(result).toBe(true);
+});
+
+test("password contains at least one special character", () => {
+    const password = "aA!12";  // Contains special character "!"
+    const result = passwordValidator(password);
+    expect(result).toBe(true);
+});
+
+test("password has not been used before", () => {
+    const password = "previousPassword1";  // A previously used password
+    const result = passwordValidator(password);
+    expect(result).toBe(false);
+});
+
