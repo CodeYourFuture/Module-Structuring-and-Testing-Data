3028fd3573e45cb8f46c26fd412b65d6b303bfef
Amir200524 <amiraminia@yahoo.com>
2025-02-21 10:23:00 +0000

    proper fraction function and test file

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,22 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
+    
+    if (Math.abs(numerator) < Math.abs(denominator)) {
+        return true;
+    }
     // add your completed function from key-implement here
+
+    if (denominator === 0) {
+        return "Undefined"; 
+    }
+
+   
+
+    // Case 3: Improper fraction (absolute numerator >= absolute denominator)
+    if (Math.abs(numerator) >= Math.abs(denominator)) {
+        return false;
+    }
 }
+
+
 
 module.exports = isProperFraction;
