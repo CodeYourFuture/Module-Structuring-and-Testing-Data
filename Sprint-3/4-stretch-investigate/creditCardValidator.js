83a068d4afec6939034dee4010ab61516276b1d6
sheetalkharab <sheetalrana89@gmail.com>
2025-02-21 18:05:43 +0000

    write validateCardNumber function

---
+++ Sprint-3/4-stretch-investigate/creditCardValidator.js
@@ -0,0 +1,31 @@
+   //Here are the rules for a valid number:
+
+    //Number must be 16 digits, all of them must be numbers.
+    // You must have at least two different digits represented (all of the digits cannot be the same).
+    // The final digit must be even.
+    // The sum of all the digits must be greater than 16.
+
+// check cardNumber must be 16 digits and all of them must be numbers.
+function validateCardNumber(cardNumber){
+    if(cardNumber.length !==16 || !/^d+$/ .test(cardNumber)){
+        return false;
+    }
+// The final digit must be even.    
+    let lastDigit = cardNumber.slice(0, cardNumber.length-1);
+    if(lastDigit % 2 !==0){
+        return false;
+    }
+// The sum of all the digits must be greater than 16.
+    let digitSum = 0;
+    for(let i=0; i< cardNumber.length; i++){
+        digitSum += parseInt(cardNumber[i]);
+    }
+    return digitSum > 16;// return false
+ 
+//  must have at least two different digits in cardNumber
+    if (cardNumber.split('').every(digit => digit === cardNumber[0])) {
+       return false;  // Invalid if all digits are the same
+    }
+   
+
+}
