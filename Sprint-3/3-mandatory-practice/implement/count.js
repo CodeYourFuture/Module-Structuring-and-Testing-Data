dd11749bd8adc3bcca0eea29902927107cd868fb
Droid-An <andrei.filippov.aaf@gmail.com>
2025-02-22 13:38:11 +0000

    removed unnecessary console.log commands

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,10 +1,7 @@
 function countChar(stringOfCharacters, findCharacter) {
   charList = stringOfCharacters.split("");
-  console.log(charList);
   count = charList.filter((char) => char == findCharacter);
-  console.log(count);
 
   return count.length;
 }
-console.log(countChar("aaaaaa", "a"));
 module.exports = countChar;
