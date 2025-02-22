fedef5663513e085b7ea0de23c245ffd42221ee5
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-22 11:45:06 +0000

    add punctuation

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -5,4 +5,4 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 
-Line 3 is like telling, take the current value of count, add 1 to it and then store that new value back in count.
+"Line 3 is like telling, take the current value of count, add 1 to it and then store that new value back in count."
