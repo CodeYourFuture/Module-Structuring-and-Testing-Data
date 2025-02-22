aa20f1bbfe4fa0be092a123c7d256181e4cc4eb0
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-22 11:10:45 +0000

    reprsenting num

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,5 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
-
+"minimum is 1"
+"maximum is 100"
+"num is essentially a randomly selected number"
+Math.floor(Math.random() * 100) + minimum;
+Math.floor(57.3256) + minimum;
+57 + minimum;
 console.log(num)
