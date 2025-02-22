43b6091935b046721df7eb60493bb4264236b89c
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-02-22 12:53:44 +0000

    Sprint-3_1-key-element_1-get-angle-type.js

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -8,9 +8,18 @@
 // Then, write the next test! :) Go through this process until all the cases are implemented
 
 function getAngleType(angle) {
-   if (angle === 90) return "Right angle";
-   // read to the end, complete line 36, then pass your test here
+  if (angle === 90) return "Right angle";
+  // read to the end, complete line 36, then pass your test here
+  else if (angle > 0 && angle < 90) return "Acute angle";
+  else if (angle > 90 && angle <= 120) return "Obtuse Angle";
+  else if (angle > 120 && angle <= 180) return "Straight Angle";
+  else if (angle > 180 && angle <= 360) return "Reflex Angle";
+  else
+    return "This angle is not correct. Please check your input and try again";
 }
+//Printing following for visualization
+console.log(`Should print Acute Angle : ${getAngleType(40)}`);
+console.log(`Should print Invalid Message : ${getAngleType(400)}`);
 
 // we're going to use this helper function to make our assertions easier to read
 // if the actual output matches the target output, the test will pass
@@ -44,13 +53,18 @@ assertEquals(acute, "Acute angle");
 // Then the function should return "Obtuse angle"
 const obtuse = getAngleType(120);
 // ====> write your test here, and then add a line to pass the test in the function above
+assertEquals(obtuse, "Obtuse Angle");
 
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
+const straight = getAngleType(180);
 // ====> write your test here, and then add a line to pass the test in the function above
+assertEquals(straight, "Straight Angle");
 
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+const reflex = getAngleType(185);
 // ====> write your test here, and then add a line to pass the test in the function above
+assertEquals(reflex, "Reflex Angle");
