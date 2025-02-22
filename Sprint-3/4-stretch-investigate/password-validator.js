9d9edccf1c4bf0b27e9053ad0ef23f948e86e84a
Fradoka <franklindkamela@gmail.com>
2025-02-22 09:13:51 +0000

    done sprint 3

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,5 +1,26 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+    const passwords = ["Test&1", "P@ssw0rd!", "$ample5"];
+
+    if (password.length < 5) return false;
+
+    let upperCase = false;
+    let lowerCase = false;
+    let numberChar = false;
+    const specialChar = "!£%$&*?#";
+    let special = false;
+
+    for (let char of password){
+        if (char >= "A" && char <="Z") upperCase = true;
+        else if (char >="a" && char <="z") lowerCase = true;
+        else if (char >= "0" && char <= "9") numberChar = true;
+        else if (specialChar.indexOf(char) !== -1) special = true;
+    }
+
+    if (!(upperCase && lowerCase && numberChar && special)) return false;
+
+    if (passwords.indexOf(password) !== -1) return false;
+
+    else return true;
 }
 
 
