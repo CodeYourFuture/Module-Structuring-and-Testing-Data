1b9fe01530e813527c1548517c0b475d6c19e05b
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-22 14:01:26 +0000

    identfing name rule

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+const HourClockTime12 = "20:53" 
+const HourClockTime24 = "08:53"
+
+console.log(HourClockTime12)
+console.log(HourClockTime24)
