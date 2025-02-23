57a4dbe587e0409fefcf47cef5acd19419770f09
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-23 23:48:59 +0000

    get-angle-type | mandatory-rewrite-answer

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -10,7 +10,7 @@
 function getAngleType(angle) {
    if (angle === 90) return "Right angle";
    // read to the end, complete line 36, then pass your test here
-   else if (angle < 90) return "Acute angle";
+   else if (angle > 0 && angle < 90 ) return "Acute angle";
    else if (angle > 90 && angle < 180) return "Obtuse angle";
    else if (angle === 180) return "Straight angle";
    else return "Reflex angle";
