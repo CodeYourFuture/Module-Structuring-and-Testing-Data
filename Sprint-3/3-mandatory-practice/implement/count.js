73292f778b371085432f1a67b973b4e398c3f7bb
Elchascode <elchaschool@gmail.com>
2025-02-23 12:47:00 +0000

    implemented a for loop in countChar function to loop through the input.

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,18 @@
 function countChar(stringOfCharacters, findCharacter) {
-    return 5
-}
+    let count = 0; // this start counting from 0
+    for (let index = 0; index < stringOfCharacters.length; index++) {
+        // Loops through every character in the stringOfCharacters.
+       if (stringOfCharacters[index] === findCharacter) {
+        // check if the current index in stringCharacter matches the findCharacter
+        count++;
+        
+       }
+        
+    }
+        
+    return count;
 
-module.exports = countChar;
+    }
+  
+
+module.exports = countChar; // this means this function is available for other file, and can also be tested here.
