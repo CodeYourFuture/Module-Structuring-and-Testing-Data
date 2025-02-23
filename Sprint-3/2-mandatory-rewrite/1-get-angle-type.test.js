aeec9cb5afb929c82dac0a6ab2ef9444f8b8d111
sab325 <sabshrestha324@gmail.com>
2025-02-23 18:44:15 +0000

    mandatory rewrite

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -4,6 +4,24 @@ test("should identify right angle (90°)", () => {
   expect(getAngleType(90)).toEqual("Right angle");
 });
 
+test("should identify acute angle (45°)", () => {
+  expect(getAngleType(45)).toEqual("Acute angle");
+});
+
+test("should identify obtuse angle (90°)", () => {
+  expect(getAngleType(120)).toEqual("Obtuse angle");
+});
+
+test("should identify straight  angle (180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
+
+test("should identify reflex angle (90°)", () => {
+  expect(getAngleType(200)).toEqual("Reflex angle");
+});
+
+
+
 // REPLACE the comments with the tests
 // make your test descriptions as clear and readable as possible
 
--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,10 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+    if (numerator < denominator) return true;
+    if (numerator > denominator) return false;
+    if (numerator === denominator) return false;
 }
+    // add your completed function from key-implement here
+
 
 module.exports = isProperFraction;
