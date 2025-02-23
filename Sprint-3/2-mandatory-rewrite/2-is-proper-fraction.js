53a6706d2aab006a08903692ec40cb606c056e3a
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-02-23 15:22:39 +0000

    Sprint-3_2-mandatory-rewrite_2-is-proper-fraction.js and 2-is-proper-fraction.test.js

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,11 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+  // if (numerator < denominator) return true;
+  // add your completed function from key-implement here
+  return (
+    numerator < denominator &&
+    Number.isInteger(numerator) &&
+    Number.isInteger(denominator)
+  );
 }
 
 module.exports = isProperFraction;
--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -6,6 +6,24 @@ test("should return true for a proper fraction", () => {
 
 // Case 2: Identify Improper Fractions:
 
+test("should return false for fraction where Numerator is grater than Denominator", () => {
+  expect(isProperFraction(10, 2)).toEqual(false);
+});
+
 // Case 3: Identify Negative Fractions:
+test("should return true for fraction where Numerator and Denominator are negative integer as long as Denominator is greater than Numerator", () => {
+  expect(isProperFraction(-6, -3)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for fraction where Numerator is equal to Denominator", () => {
+  expect(isProperFraction(50, 50)).toEqual(false);
+});
+//Stretch Case 1 : Identify Decimal Numerator
+test("should return False for fraction where Numerator is decimal integer", () => {
+  expect(isProperFraction(2.2, 4)).toEqual(false);
+});
+//Stretch Case 2
+test("should return false where Denominator isn't provided", () => {
+  expect(isProperFraction(5)).toEqual(false);
+});
