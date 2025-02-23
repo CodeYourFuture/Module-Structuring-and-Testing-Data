831f19480b7500b9d609a4a919712b313602ed8f
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-02-23 14:31:19 +0000

    Sprint-3_2-mandatory-rewrite_1-get-angle-type.js and 1-get-angle-type.test.js

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,18 +1,16 @@
 function getAngleType(angle) {
   if (angle === 90) return "Right angle";
   // replace with your completed function from key-implement
-
+  else if (angle > 0 && angle < 90) return "Acute angle";
+  else if (angle > 90 && angle <= 120) return "Obtuse Angle";
+  else if (angle > 120 && angle <= 180) return "Straight Angle";
+  else if (angle > 180 && angle <= 360) return "Reflex Angle";
+  else
+    return "This angle is not correct. Please check your input and try again";
 }
-
-
-
-
-
-
-
 
 // Don't get bogged down in this detail
 // Jest uses CommonJS module syntax by default as it's quite old
-// We will upgrade our approach to ES6 modules in the next course module, so for now 
+// We will upgrade our approach to ES6 modules in the next course module, so for now
 // we have just written the CommonJS module.exports syntax for you
 module.exports = getAngleType;
--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -10,15 +10,39 @@ test("should identify right angle (90°)", () => {
 // Case 2: Identify Acute Angles:
 // When the angle is less than 90 degrees,
 // Then the function should return "Acute angle"
+test("should identify Acute Angles(0° to 89°)", () => {
+  expect(getAngleType(40)).toEqual("Acute angle");
+});
 
 // Case 3: Identify Obtuse Angles:
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
-
+test("should identify Obtuse Angles(91° to 179°)", () => {
+  expect(getAngleType(115)).toEqual("Obtuse Angle");
+});
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
+test("should identify Straight Angle(180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight Angle");
+});
 
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+test("should identify Reflex Angles(181° to 360°)", () => {
+  expect(getAngleType(190)).toEqual("Reflex Angle");
+});
+
+// //SS
+// const acute = getAngleType(45);
+// assertEquals(acute, "Acute angle");
+
+// const obtuse = getAngleType(120);
+// assertEquals(obtuse, "Obtuse Angle");
+
+// const straight = getAngleType(180);
+// assertEquals(straight, "Straight Angle");
+
+// const reflex = getAngleType(185);
+// assertEquals(reflex, "Reflex Angle");
