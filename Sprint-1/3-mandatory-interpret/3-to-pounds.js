19fd501ad336af8c6b3c0ab627190b05d4e9f82e
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-23 22:06:02 +0000

    add punctuation

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,19 +1,11 @@
 const penceString = "399p";
-
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+  
+const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
 
 console.log(`£${pounds}.${pence}`);
 
@@ -25,3 +17,7 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. Remove the 'p': = This removes the 'p' at the end, so we just have "399"
+// 3. Pad with zeros: = This adds zeros at the start if needed, ensures that the pence value has at least 3 digits("50p ~ 005")
+// 4. Split pounds and pence: = "399" ~ "3"("99"are the pence)
+// 5. final output: = Input: "399" ~ Output: "3.99" 
