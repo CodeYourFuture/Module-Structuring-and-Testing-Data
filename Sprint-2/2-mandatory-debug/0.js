1e373a47c39f8e44547cd2fc8b53c05ce3146f81
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-24 21:56:39 +0000

    write a new code

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -6,9 +6,15 @@
  // console.log(a * b);
 //}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
     //We told the function to return the answer, so now it can be used in the sentence.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b; 
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`); 
