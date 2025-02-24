6228c5c11eab67e6372fb7ba6ab86c8bbf3a6853
Elchascode <elchaschool@gmail.com>
2025-02-24 22:34:13 +0000

    added a  repeat function

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,25 @@
-function repeat() {
+function repeat(str, count) {
+    if (count < 0) {
+        throw new Error("count can not be negative"); // if count is < than 0 throw an error
+        
+    }
+
+    if (count === 0) {
+        return "";   // return an empty str when count is === 0
+        
+    }
+
+    if (count === 1) {
+        return str;  // return str when count is === 1
+        
+    }
+
+    let repeatedStr = "";
+    for (let index = 0; index < count; index++) {
+        repeatedStr += str;  // repeat str and count
+        
+    }
+
     return "hellohellohello";
 }
-
 module.exports = repeat;
