a0ba97b543b8ba8c495c96fd8fad0ced4a6bb0c6
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-24 22:25:33 +0000

    predict and explain the problem

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,7 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
-
+   // Wrong out put.
 const num = 103;
 
 function getLastDigit() {
@@ -15,8 +15,12 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+   //The last digit of 42 is 3
+   //The last digit of 105 is 3
+   //The last digit of 806 is 3
 // Explain why the output is the way it is
 // =============> write your explanation here
+   // The function always returns the last digit of num(103)
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
