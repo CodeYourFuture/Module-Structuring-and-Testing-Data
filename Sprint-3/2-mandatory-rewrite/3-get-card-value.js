3ad3913d3c83fe54e6944d7372c994e29cfc1f70
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-24 11:24:49 +0000

    get-card-value|mandatory-rewrite-answer

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,9 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    let rank = card.slice(0, -1); // Extract the rank by removing the suit
+    if (rank === "A") return 11;
+    else if (rank >= 2 && rank <= 9) return Number(rank);
+    else if (["K", "Q", "J", "10"].includes(rank)) return 10;
+    else return "Invalid Card Rank";
 }
+
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,26 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return the numeric value corresponding to the rank", () => {
+    const fiveofHearts = getCardValue("5♥");
+    expect(fiveofHearts).toEqual(5);
+})
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10, as these cards are worth 10 points each in blackjack", () => {
+    const queenofHarts = getCardValue("Q♥");
+    expect(queenofHarts).toEqual(10); 
+})
+
 // Case 4: Handle Ace (A):
+test("should return 11, as the Ace is worth 11 points", () => {
+    const aceofHearts = getCardValue("A♥");
+    expect(aceofHearts).toEqual(11);
+})
+
 // Case 5: Handle Invalid Cards:
+test("should return invalid card rank", () => {
+    const joker = getCardValue("1🃏");
+    expect(joker).toEqual("Invalid Card Rank");
+})
+
