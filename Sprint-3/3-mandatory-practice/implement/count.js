43851cb2e10fcf78b928c41fce9e54ea89128d96
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-24 11:58:04 +0000

    count|mandatory-practice-answer

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,15 @@
-function countChar(stringOfCharacters, findCharacter) {
-    return 5
+function countChar(str, char) {
+    let count = 0; // start with a count of zero
+
+  // goes through each letter in the string
+  for (let i = 0; i < str.length; i++) {
+    // if the current letter is the same as the one we're looking for
+    if (str[i] === char) {
+      count++; // increase the count by one
+    }
+  }
+
+  return count; // return the final count
 }
 
 module.exports = countChar;
--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -11,9 +11,9 @@ const countChar = require("./count");
 // Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
 
 test("should count multiple occurrences of a character", () => {
-  const str = "aaaaa";
-  const char = "a";
-  const count = countChar(str, char);
+  let str = "aaaaa";
+  let char = "a";
+  let count = countChar(str, char);
   expect(count).toEqual(5);
 });
 
@@ -22,3 +22,9 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should return 0, as no occurrences of a character found", () => {
+  let str = "home";
+  let char = "a";
+  let count = countChar(str, char);
+  expect(count).toEqual(0);
+});
