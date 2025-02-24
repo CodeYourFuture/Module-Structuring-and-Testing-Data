28d22582167b4795ef73d2cc8a16accdaf8f0789
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-24 22:06:41 +0000

    predict and explain the problem.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+    // The function returns nothing
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +9,6 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+   // return should be followed by a value ( return a + b;).
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
