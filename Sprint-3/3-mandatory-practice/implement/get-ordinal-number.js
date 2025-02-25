692444e14d59a55ae56f090ff5a4dce0414b35bb
Priscilla <cpjm0033@gmail.com>
2025-02-25 23:49:40 +0000

    ordinal number function

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,25 @@
 function getOrdinalNumber(num) {
- return "1st";
+  if (typeof num !== "number" || num < 1) {
+    throw new Error("Input must be a positive number");
+  }
+
+  const lastDigit = num % 10;
+  const lastTwoDigits = num % 100;
+
+  if ([11, 12, 13].includes(lastTwoDigits)) {
+    return `${num}th`;
+  }
+
+  switch (lastDigit) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
+  }
 }
 
 module.exports = getOrdinalNumber;
