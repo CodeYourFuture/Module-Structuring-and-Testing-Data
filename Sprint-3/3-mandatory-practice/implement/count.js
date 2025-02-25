63b3c25d64fad388971779fb0f3b9de50f2af8a8
Priscilla <cpjm0033@gmail.com>
2025-02-25 19:01:16 +0000

    count function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,14 @@
-function countChar(stringOfCharacters, findCharacter) {
-    return 5
+function countChar(str, char) {
+  console.log(`str: '${str}', char: '${char}', char length: ${char.length}`);
+
+  if (typeof str !== "string") {
+    throw new Error("Invalid input: str must be a string");
+  }
+  if (typeof char !== "string" || char.length !== 1) {
+    throw new Error("Invalid input: char must be a single character string");
+  }
+
+  return str.split("").filter((c) => c === char).length;
 }
 
 module.exports = countChar;
