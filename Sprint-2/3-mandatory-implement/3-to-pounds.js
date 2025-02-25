b1e12e7e115f3a711d18b6d109262d389f986fb4
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-02-25 11:31:06 +0000

    Function to convert pence to pounds

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,12 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(pence) {
+    return `£${(pence / 100).toFixed(2)}`;
+}
+
+console.log(toPounds(399));  
+console.log(toPounds(250));  
+console.log(toPounds(1000)); 
+console.log(toPounds(75));   
