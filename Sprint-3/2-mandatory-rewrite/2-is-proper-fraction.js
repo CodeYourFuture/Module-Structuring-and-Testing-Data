57256038e07868fe9b0194fc370e30f7def396af
Priscilla <cpjm0033@gmail.com>
2025-02-25 11:51:53 +0000

    properFraction2

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,10 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+  if (numerator < denominator) return true;
+  // add your completed function from key-implement here
+}
+
+function isProperFraction(numerator, denominator) {
+  return Math.abs(numerator) < Math.abs(denominator);
 }
 
 module.exports = isProperFraction;
