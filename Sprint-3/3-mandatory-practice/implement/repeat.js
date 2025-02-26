dc63f11a040e12bda6ddf9f8c9d199336fd167c4
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-02-26 14:46:40 +0000

    Completed mandatory practice/implement - repeat test files

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,9 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+    if (count < 0) {
+        throw new Error("Count can't be negative");
+    }
+    return str.repeat(count);
 }
+
 
 module.exports = repeat;
