619125a4ec15bec7c890617b9bd77035112cb8b3
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-02-26 15:14:03 +0000

    finish 2-mandatory-rewrite/2-is-proper-fraction

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,9 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
     // add your completed function from key-implement here
+    if (denominator === 0) return "Error: Denominator cannot be 0";
+    if (Math.abs(numerator)  < denominator) return true;
+    if (Math.abs(numerator) >= denominator) return false;
 }
 
 module.exports = isProperFraction;
--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,14 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
-
+test("should return false if the numerator is greater than or equal to the denominator", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 // Case 3: Identify Negative Fractions:
-
+test("should return true if the numerator is negative", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false if the numerator and denominator are equal", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
