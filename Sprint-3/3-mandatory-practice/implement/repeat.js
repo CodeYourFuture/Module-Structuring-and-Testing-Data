7f0517ef8b97e3e9262eb75944e40e014cbe9cbf
Priscilla <cpjm0033@gmail.com>
2025-02-26 00:30:51 +0000

    repeat function

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,12 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (typeof str !== "string") {
+    throw new Error("First argument must be a string");
+  }
+  if (typeof count !== "number" || count < 0) {
+    throw new Error("Second argument must be a non-negative number");
+  }
+
+  return str.repeat(count);
 }
 
 module.exports = repeat;
