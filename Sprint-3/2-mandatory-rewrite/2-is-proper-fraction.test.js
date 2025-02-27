52148a616938be5ef776ed43d323b868e00c1b75
Millena28 <millenakiflom06@gmail.com>
2025-02-27 08:23:27 +0000

    Done the mandatory-practice

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -1,19 +1,23 @@
 const isProperFraction = require("./2-is-proper-fraction");
-const isImproperFraction = require("./2-is-improper-fraction");
-const isEqualNumeratorAndDenominator = require("./2-is-equal-numerator-denominator");
+
 
 test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
 });
 
 // Case 2: Identify Improper Fractions:
-test("should return true for a Improper fraction", () => {
+test("should return true for an Improper fraction", () => {
   expect(isImproperFraction(6, 3)).toEqual(true);
 });
 
+test("should return true for a negative improper fraction", () => {
+  expect(isImproperFraction(-7, 4)).toEqual(true);
+});
+
 // Case 3: Identify Negative Fractions:
-test("should return true for a negative fraction", () => {
-  expect(isImproperFraction(-5, 3)).toEqual(true);
+test("should return true for a negative proper fraction", () => {
+  expect(isProperFraction(3, -5)).toEqual(true);
+  expect(isProperFraction(-3, 5)).toEqual(true);
 });
 
 // Case 4: Identify Equal Numerator and Denominator:
