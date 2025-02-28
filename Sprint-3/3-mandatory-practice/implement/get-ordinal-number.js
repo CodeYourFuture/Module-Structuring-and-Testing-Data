2fdc4e3f8ca01e1e19265f5aee0ae4770c048ed7
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-02-28 09:30:44 +0000

    finish 3-mandatory-practice/get-ordinal-number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,18 @@
 function getOrdinalNumber(num) {
- return "1st";
-}
+    if (num === 11 || num === 12 || num === 13) {
+        return `${num}th`;
+      }
+      const lastDigit = num % 10;
+      if (lastDigit === 1) {
+        return `${num}st`;
+      }
+      if (lastDigit === 2) {
+        return `${num}nd`;
+      }
+      if (lastDigit === 3) {
+        return `${num}rd`;
+      }
+      return `${num}th`;
+    }
 
 module.exports = getOrdinalNumber;
