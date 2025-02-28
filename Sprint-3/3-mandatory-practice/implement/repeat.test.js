4ea450e01d87f14fb7cb53bd62c3bee945657eb7
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-02-28 10:03:55 +0000

    fix test line place

--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -20,8 +20,8 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
-test("should throw an Error 'Negative counts are not valid' when count is negative",()=>{
-    expect( ()=> repeat("hello",-1)).toThrow('Negative counts are not valid');
+test("should return the string when count is 1",()=>{
+    expect( repeat("hello", 1)).toEqual("hello");
 });
 
 // case: Handle Count of 0:
@@ -35,6 +35,6 @@ test("should return empty string when count is 0",()=>{
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
-test("should return empty string when count is 0",()=>{
-    expect( repeat("hello", 1)).toEqual("hello");
+test("should throw an Error 'Negative counts are not valid' when count is negative",()=>{
+    expect( ()=> repeat("hello",-1)).toThrow('Negative counts are not valid');
 });
