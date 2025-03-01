7b832a5f8013164ac403cc613747f5747043fed8
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-01 22:10:16 +0000

    Sprint 2/2-mandatory-debug

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,28 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//I think there will be some error running this code because of incorrect syntax of line 9 and line 10
+//The output I received after running this code was :
+//The sum of 10 and 32 is undefined
+//I believe removing the semicolon on line 9 will fix the issue.
 
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+//The semicolon represents it is the end of code or command we are giving to the computer.
+//In the exercise, putting semicolon after return without including the value meant that we gave computer command but ended it before adding any value
+//Correcting the semicolon has fixed this code.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+//The output now is :
+//The sum of 10 and 32 is 42
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,23 +2,42 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+//I think this code will convert given number to string and return the last digit of the value based on index -1
 
-const num = 103;
+// const num = 103;
 
-function getLastDigit() {
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
+// Explain why the output is the way it is
+// =============> write your explanation here
+//I think the output is this way because we have declared num as a const variable, it should be declared as a parameter
+//So each time it will return different output that matches with input
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+//As expected declaring num as parameter instead of variable has fixed the problem.
+//My new output is
+//The last digit of 42 is 2
+// The last digit of 105 is 5
+// The last digit of 806 is 6
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
