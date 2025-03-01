a9722ed9eba807477e5b499a73f5ce13425549b6
Cristina Leascu <cristinaleascu@gmail.com>
2025-03-01 23:40:52 +0000

    get-ordinal-number|mandatory-practice-answer

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,18 @@
 function getOrdinalNumber(num) {
- return "1st";
+
+let twoDigitNum = num % 10; 
+let threeDigitNum = num % 100;
+
+if (twoDigitNum === 1 && threeDigitNum !== 11) return  (`${num}st`);
+else if (twoDigitNum === 2 && threeDigitNum !== 12) return  (`${num}nd`);
+else if (twoDigitNum === 3 && threeDigitNum !== 13) return  (`${num}rd`);
+else return (`${num}th`);
 }
 
+
 module.exports = getOrdinalNumber;
+
+
+
+
+
--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -9,5 +9,27 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Then the function should return "1st"
 
 test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+  expect(getOrdinalNumber(1)).toEqual("1st"); // test to handle 1st case
+});
+
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd"); // test to handle case like 1, 11, 111
+});
+test("should return '11th' for 11", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th"); //test to handle 11th special case
+});
+
+test("should return '12th' for 12", () => {
+  expect(getOrdinalNumber(12)).toEqual("12th"); // test to handle 12th special case
+});
+
+test("should return '122nd' for 122", () => {
+  expect(getOrdinalNumber(122)).toEqual("122nd"); // test to handle case like 1, 11, 111
+});
+
+test("should return '123rd' for 123", () => {
+  expect(getOrdinalNumber(123)).toEqual("123rd");
+});
+test("should return '199th' for 199", () => {
+    expect(getOrdinalNumber(199)).toEqual("199th");
+  });
