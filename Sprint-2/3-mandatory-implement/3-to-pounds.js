cb378f723870a4f8d11445ad6ff16d824b2b46a6
Bahasyd <baha.syd@gmail.com>
2025-03-01 07:16:15 +0000

    with perceInt

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,6 +4,17 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+function toPounds(penceString) {
+    const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
+    const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+    const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+    return `£${pounds}.${pence}`;
+}
+console.log(toPounds("399p"));  // £3.99
+console.log(toPounds("5p"));    // £0.05
+
+//code with apply perceInt:
 function toPounds(penceStr) {
     const pence = perceInt(penceStr.slice(0,-1), 10);
     return `£${(pence/100).toFixed(2)}`;
