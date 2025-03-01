3919963e45724463c3a0758400d1b7895ba2c9a4
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-01 22:07:57 +0000

    Sprint 2_1-key-errors

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,32 @@
 // Predict and explain first...
-
 // Why will an error occur when this program runs?
+//due to identifier redeclaration on line 11
 // =============> write your prediction here
+//Working with previous example, I believe similar Syntax error will be returned as decimalNumber has already been declared
+// in  line 10 as a parameter so it shouldn't be redeclared again in line 9.
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> write your explanation here
+//The function convertTo Percentage's goal is to convert number value given to decimalNumber identifier by multiplying it by 100.
+//The code seems to be otherwise alright except for the redeclaration on line 11.
+//Console.log is also set with incorrect parameter to print out what we want to see
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+//
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage());
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,35 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
-function square(3) {
-    return num * num;
-}
+// The parameter doesn't declare num identifier for the function to return value
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+// /Users/samuntasunuwar/CYF/Structuring and testing data/Module-Structuring-and-Testing-Data/Sprint-2/1-key-errors/2.js:8
+// function square(3) {
+//                 ^
 
+// SyntaxError: Unexpected number
+//     at wrapSafe (node:internal/modules/cjs/loader:1512:18)
+//     at Module._compile (node:internal/modules/cjs/loader:1534:20)
+//     at Object..js (node:internal/modules/cjs/loader:1699:10)
+//     at Module.load (node:internal/modules/cjs/loader:1313:32)
+//     at Function._load (node:internal/modules/cjs/loader:1123:12)
+//     at TracingChannel.traceSync (node:diagnostics_channel:322:14)
+//     at wrapModuleLoad (node:internal/modules/cjs/loader:217:24)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:170:5)
+//     at node:internal/main/run_main_module:36:49
 // =============> explain this error message here
-
+//Error message is giving us clue as to where the problem on this code is. It is a syntax error which mean the problem is within code structure.
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+  return num * num;
+}
+console.log(square(5));
