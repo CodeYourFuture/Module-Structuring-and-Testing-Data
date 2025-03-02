9360469fa989cab2cb52b8667c0155850c95b62c
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-03-02 10:14:02 +0000

    add more tests

--- Sprint-3/4-stretch-investigate/card-validator.test.js
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -1,9 +1,29 @@
 const isValidNumber = require("./card-validator");
 
+test('Should return false if the digits are not 16', () => {
+    expect(isValidNumber(222222222222222)).toBe(false);
+});
+
+test('Should return false if the 16 digits are not all numbers', () => {
+    expect(isValidNumber(222222222222222.)).toBe(false);
+});
+
 test('Should return false if the 16 digits are the same', () => {
     expect(isValidNumber(2222222222222222)).toBe(false);
 });
 
-test('Should return false if the 16 digits are the same', () => {
+test('Should return false if the final digit is not even', () => {
+    expect(isValidNumber(2222222222222221)).toBe(false);
+});
+
+test('Should return false if the sum < 16', () => {
     expect(isValidNumber(1011111111111111)).toBe(false);
 });
+
+
+
+
+// - Number must be 16 digits, all of them must be numbers.
+// - You must have at least two different digits represented (all of the digits cannot be the same).
+// - The final digit must be even.
+// - The sum of all the digits must be greater than 16.
