cc0dd4fbf5f1d9a2efc65b732a58607e5f9b591e
Cristina Leascu <cristinaleascu@gmail.com>
2025-03-02 00:28:35 +0000

    repeat|mmdatory-practice-answer

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,12 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count > 0) {
+    const repeatedStr = str.repeat(count);
+    return repeatedStr;
+  } else if (count == 0) {
+    return "";
+  } else {
+    throw new Error("Error: negative numbers are not allowed");
+  }
 }
 
 module.exports = repeat;
