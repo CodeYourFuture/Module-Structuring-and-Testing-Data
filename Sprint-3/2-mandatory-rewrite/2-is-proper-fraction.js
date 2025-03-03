c5108ac9437c17f24ad6aea45f10e8462cbe5b48
sheetalkharab <sheetalrana89@gmail.com>
2025-03-03 11:32:34 +0000

    implement in built function abs() in code

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,15 +1,10 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) {
-        return true;
-    }else if(numerator>denominator){
-        return false; 
-    }else if(numerator<0 && denominator>0){
-        return true; 
-    }else if(numerator===denominator){
-        return false;
-    }else{
-        return false;
+    if(denominator===0){
+        return false;//in fraction denominator 0 is undefined
     }
+    let absoluteNumerator = Math.abs(numerator);
+    let absoluteDenominator = Math.abs(denominator);
+    return absoluteNumerator < absoluteDenominator;
     // add your completed function from key-implement here
 }
 
