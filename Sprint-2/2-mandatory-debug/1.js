26ecd4ad705b8bfb9b1da69488810ccf818c38f0
saraamiiri <amirisara1993@gmail.com>
2025-03-03 13:33:15 +0000

    2-debug 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -9,5 +9,11 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+//return and a+b in 2 seprate line,he line a + b; is effectively unreachable and will not be executed. 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
