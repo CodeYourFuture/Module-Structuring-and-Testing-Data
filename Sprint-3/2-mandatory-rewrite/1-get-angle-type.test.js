4d9423b36321b55a8c5e80561f26fe530d1dad78
obie <obertilimandzi@gmail.com>
2025-03-03 21:47:59 +0200

    code done with all the comments removed

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -1,39 +1,25 @@
 const getAngleType = require("./1-get-angle-type");
 
+
 test("should identify right angle (90°)", () => {
   expect(getAngleType(90)).toEqual("Right angle");
 });
 
-// REPLACE the comments with the tests
-// make your test descriptions as clear and readable as possible
-
-// Case 2: Identify Acute Angles:
-// When the angle is less than 90 degrees,
-// Then the function should return "Acute angle"
 
 test("This should identify Acute angles (<90°)", () => {
   expect(getAngleType(20)).toEqual("Acute angle");
 });
 
-// Case 3: Identify Obtuse Angles:
-// When the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle"
 
-test("This will identify an Obtuse angle if an angle is greater than 90 degrees and less than 180 degrees (angle >90° && angle <180°)", () => {
+test("identify an Obtuse angle if an angle is greater than 90 degrees and less than 180 degrees (angle >90° && angle <180°)", () => {
   expect(getAngleType(100)).toEqual("Obtuse angle");
 });
 
-// Case 4: Identify Straight Angles:
-// When the angle is exactly 180 degrees,
-// Then the function should return "Straight angle"
 
 test("this will identify an angle that is exactly 180° (angle === 180°)", () => {
   expect(getAngleType(180)).toEqual("Straight angle");
 });
 
-// Case 5: Identify Reflex Angles:
-// When the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle"
 
 test("Should identify angle that is greater than 180 degrees and less than 360 degrees (angle >180° && angle <360°)", () => {
   expect(getAngleType(200)).toEqual("Reflex angle");
