310664c8c0e2e2409fe040772e1c7066d5172a9c
sheetalkharab <sheetalrana89@gmail.com>
2025-03-03 11:35:07 +0000

    change in some test case

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -17,7 +17,17 @@ test("should return true for a negative fraction", () => {
 
 
 // Case 4: Identify Equal Numerator and Denominator:
-test("should return false for a equal fraction", () => {
+test("should return false for a equal numerator and denominator", () => {
   expect(isProperFraction(3, 3)).toEqual(false);
+});
+
+// Denominator is zero (undefined case)
+test("should return false for a fraction with denominator 0", () => {
+  expect(isProperFraction(1, 0)).toEqual(false);
+});
+
+//improper negative fraction with negative denominator
+test("should return false for an improper fraction with negative denominator", () => {
+  expect(isProperFraction(5, -3)).toEqual(false);
 });
 
