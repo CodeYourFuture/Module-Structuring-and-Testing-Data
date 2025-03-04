de1027f4ef7e6720a4b3ea73b87a7ea29baea755
sheetalkharab <sheetalrana89@gmail.com>
2025-03-04 16:16:41 +0000

    update method with regex

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -2,7 +2,7 @@ function getCardValue(card) {
     let rank = card.slice(0, -1);// extract rank everything except last character(suit)
     if (rank === "A") {
         return 11
-    }else if(!isNaN(rank) && rank>="2" && rank<="9"){
+    }else if (/^[2-9]$|^10$/.test(rank)){// insure rank is a valid integer between 2 to 10
        return parseInt(rank, 10);
     }else if (rank === "K" || rank === "J" || rank === "Q" || rank === "10") {
        return 10;
