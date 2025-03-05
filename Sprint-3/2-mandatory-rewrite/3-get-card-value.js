10da114f8b1cd4b7c9cc573efe63b31c12339fe9
obie <obertilimandzi@gmail.com>
2025-03-05 15:39:23 +0200

    added my code for the card value

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,15 @@
 function getCardValue(card) {
     // replace with your code from key-implement
-    return 11;
-}
+    function getCardValue(card) {
+        const rank = card.slice(0, -1);
+        if (rank === "A") return 11;
+        if (!isNaN(rank) && Number(rank) >= 2 && Number(rank) <= 9) return Number(rank);
+        if (rank === "10" || rank === "J" || rank === "Q" || rank === "K") return 10;
+        else {
+          throw new Error("Card not valid.");                 // card not valid if input is not in the scope of the card range/rank
+        }
+       
+    }
+
+}    return 11;
 module.exports = getCardValue;
