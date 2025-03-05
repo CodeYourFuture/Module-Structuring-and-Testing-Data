4d5092e2d7e1b5516b62821fada0451caec26b86
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-05 20:36:19 +0000

    refactored get ordinal number function

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,10 +1,11 @@
 function getOrdinalNumber(num) {
   numToString = String(num);
-  if (numToString[numToString.length - 1] == 1 && num != 11) {
+  lastDigit = numToString[numToString.length - 1]
+  if (lastDigit == 1 && num != 11) {
     return `${num}st`;
-  } else if (numToString[numToString.length - 1] == 2 && num != 12) {
+  } else if (lastDigit == 2 && num != 12) {
     return `${num}nd`;
-  } else if (numToString[numToString.length - 1] == 3 && num != 13) {
+  } else if (lastDigit == 3 && num != 13) {
     return `${num}rd`;
   } else {
     return `${num}th`;
