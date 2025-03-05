af7243be8673331d85705ce7287d7b26bc373708
sripathman <sripathmann@gmail.com>
2025-03-05 23:22:03 +0000

    sprint-2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,30 @@
 
 // Predict and explain first BEFORE you run any code...
-
-// this function should square any number but instead we're going to get an error
+/*3 is not a variable.it cause SyntaxError
+*/ 
+// this function should square any number but instead we're going to
+//  get an error
 
 // =============> write your prediction of the error here
-
+/*SyntaxError */ 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-
+/*SyntaxError: Unexpected number*/
 // =============> explain this error message here
-
+/* When defining a function, the parameters are placeholders for values 
+that will be passed into the function when it's called. These placeholders 
+must be valid variable names so that the function can refer to 
+them internally. */
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
+let result = square(3); 
+console.log(result); 
 
--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
-
+/*the function called inside of function */
 // =============> write your prediction here
-
+/*this function does not return a value */
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -9,6 +9,12 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
-
+/* function should call out side of function
+the function should retutn a value when we called out side of function*/ 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b; 
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
