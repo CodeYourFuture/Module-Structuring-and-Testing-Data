ee1a81dadfa39db33f4cebb1975e6695412c0edf
sarawone <sarawone.poudal@gmail.com>
2025-03-07 23:11:51 +0000

    removing / from dir

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(0,lastSlashIndex+1);
+const dir = filePath.slice(0,lastSlashIndex);
 console.log (`The dir part of the file path ${dir}`);
 const ext = filePath.slice(-3);
 console.log(`The ext part of file path ${ext}`);
