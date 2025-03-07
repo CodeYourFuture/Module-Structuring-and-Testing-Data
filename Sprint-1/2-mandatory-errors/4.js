ae2082c98dec36850081574469871341e8b9a4b8
emladeno <mladenova.elvira@yahoo.com>
2025-03-07 12:00:03 +0000

    variables and time corrected

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+//Variable names cannot start with a number in JavaScript.And the input is not at the correct time.
+//corrected:
+const hourClockTime24 = "20:53";
+const hourClockTime12 = "08:53";
