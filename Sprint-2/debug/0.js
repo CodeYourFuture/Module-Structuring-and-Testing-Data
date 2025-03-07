47bb6707d89e8c515c4cad5837546b31550f746f
Mansoor <mansoor_m19@hotmail.co.uk>
2025-03-07 16:10:34 +0000

    added comments explaining the program code

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -5,3 +5,5 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//multiply is a function which takes two paremeter. it then uses console.log and output multiplication of the two parameter. In this case the return value will be 320.
--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -6,3 +6,5 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// console.log calls the function sum with the two requiring parameters, and it then returns undefined. The return statement is used to return a value, but because there is no value after the return statement it then returns undefined.
