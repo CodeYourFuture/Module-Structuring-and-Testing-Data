8544e060a66b38849d21383ffead38143bfcb231
Mansoor <mansoor_m19@hotmail.co.uk>
2025-03-07 19:30:01 +0000

    added comments explaining the program's code

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,10 +1,11 @@
 // Predict and explain first...
-// A function convertToPercentage is created which takes a decimalNumber. We then create two variable but there will be a problem as it uses the same variable name as the function parameter.
-// We also dont need to redeclare the function, as the information will be provided by the user and thus it can be removed. Then, we create a variable called percentage which takes parameter, multiplies it by 100, and adds % sign to convert  the decimal number to percentage.
-//Finally, console.log needs to call the function name with a given parameter for it to work, rather than calling the variable name.
-// Why will an error occur when this program runs?
 
-// Try playing computer with the example to work out what is going on
+//A function convertToPercentage is created which takes a decimal number. We then create variable but there will be a problem as it uses the same variable name as the function parameter.
+//Therefore, we can remove that line  as the function parameter will provide the decimal number. Then, we create a variable called percentage which takes parameter, multiplies it by 100, and adds % sign to convert the decimal number to percentage.
+//Finally, console.log needs to call the function name with a given parameter for it to work, rather than calling the variable name.
+
+// Why will an error occur when this program runs?
+//There will be a syntax error because we are creating a variable inside a function with the same name as the function parameter.
 
 function convertToPercentage(decimalNumber) {
   const percentage = `${decimalNumber * 100}%`;
