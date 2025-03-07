bbc33c486a27c255ab89d487ded1aaed0073bb52
Mansoor <mansoor_m19@hotmail.co.uk>
2025-03-07 19:44:16 +0000

    fixed a syntax error caused by function parameter

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,8 @@
-
 // Predict and explain first...
+// we create a function that takes a parameter and  returns the value multiplied by itself to give a square number.
 
 // this function should square any number but instead we're going to get an error
-
-function square(3) {
-    return num * num;
+// It is because we are using an integer '3' which is not a valid paremeter. In order for it to accept an integer we can name the parameter num which will fix the error.
+function square(num) {
+  return num * num;
 }
-
-
