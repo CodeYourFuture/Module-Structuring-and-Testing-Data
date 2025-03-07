148e90129389eaf5d14a47f4fe046778098129e7
Segun <segunfolayan@yahoo.com>
2025-03-07 19:19:56 +0000

    ordinal

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,11 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(word, number) {
+    return word.repeat(number);
 }
 
 module.exports = repeat;
+
+
+
+//let str = "Hello" ;
+//console.log(str.repeat(3)); 
+//console.log(repeat("menubar", 3))
