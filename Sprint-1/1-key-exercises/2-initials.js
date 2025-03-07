327e6414dbb280c4647a88bbc6c1ed6b4e783a43
sarawone <sarawone.poudal@gmail.com>
2025-03-07 22:31:42 +0000

    correct  interpolated string literal

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,8 +5,13 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = `${firstName.charAt(0)+middleName.charAt(0)+lastName.charAt(0)}`;
-console.log("The letters are "+initials);
+// declaring variables for each name and take out fist character
+
+let fname_initials = `${firstName.charAt(0)}`;
+let midname_initials = `${middleName.charAt(0)}`;
+let lastname_initials= `${lastName.charAt(0)}`;
+
+console.log("The letters are ",fname_initials,midname_initials,lastname_initials);
 
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
