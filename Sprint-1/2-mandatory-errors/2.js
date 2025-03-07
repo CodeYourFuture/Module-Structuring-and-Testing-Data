f52ecc38c99eaa5fba951a4648721ba1fa45a2a4
emladeno <mladenova.elvira@yahoo.com>
2025-03-07 11:47:34 +0000

    correcting the error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,8 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+The code brings the error “cityOfBirth is not defined” because the cityOfBirth is used before it is defined.Line 5 should come before line 4 to have the proper result.
+
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
