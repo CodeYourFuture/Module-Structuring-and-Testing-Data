db4027a65c2898bac6e53268b3a989efdc36f887
Giosef Ferraro <jo-92-01@live.it>
2025-03-08 12:06:31 +0000

    sprint 1

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//The line count = count + 1; is an assignment operation. It takes the current value of count, adds 1 to it, and then assigns the result back to count, effectively increasing its value by 1.
