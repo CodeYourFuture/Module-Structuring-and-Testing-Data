c6d4653c56e55865eacd15cf35a67a66319e7d6f
AmirMontazeri <mr.amir.mti@gmail.com>
2025-03-08 14:31:00 +0000

    sprint3-tasks-mandatory-practice-count.js-updated

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,12 @@
 function countChar(stringOfCharacters, findCharacter) {
+  if (findCharacter === "") {
+    throw new Error("findCharacter cannot be an empty string");
+  }
   return stringOfCharacters.split(findCharacter).length - 1;
 }
 
+console.log(countChar("a", "a")); 
+console.log(countChar("a", "b")); 
+console.log(countChar("", "a"));
+console.log(countChar("", ""));
 module.exports = countChar;
