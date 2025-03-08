3fe24ae320340674df2b4487007ae20106774eca
loveth900 <ojay_love@yahoo.com>
2025-03-08 07:22:02 +0000

    fixed

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -1,15 +1,20 @@
-let firstName = "Creola";
-let middleName = "Katherine";
-let lastName = "Johnson"; 
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = "CKJ"
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
 
+
+function getInitials(firstName, middleName, lastName) {
+    return firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0);
+}
+
+let firstName = "Creola";
+let middleName = "Katherine";
+let lastName = "Johnson";
+
+let initials = getInitials(firstName, middleName, lastName);
+
 console.log(initials); // Output: "CKJ"
-
-
