8b8ccbdd9acdcdf27f51336b27c19925591e62da
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-09 16:37:09 +0000

    Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,14 @@
 function isProperFraction(numerator, denominator) {
-  // if (numerator < denominator) return true;
-  // add your completed function from key-implement here
+  // if (
+  //   numerator < denominator &&
+  //   Number.isInteger(numerator) &&
+  //   Number.isInteger(denominator)
+  // )
+  //   return true;
+  // else return false;
+  //refactored code (I am using Number.Integer method to make sure that decimal integers return as false)
+  numerator = Math.abs(numerator);
+  denominator = Math.abs(denominator);
   return (
     numerator < denominator &&
     Number.isInteger(numerator) &&
