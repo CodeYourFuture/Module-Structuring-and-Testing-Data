d69b74ea85850af528841c31decd795031fd2c63
Bahasyd <baha.syd@gmail.com>
2025-03-09 17:08:43 +0000

    2-3

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,17 @@
 function getCardValue(card) {
     // replace with your code from key-implement
-    return 11;
-}
-module.exports = getCardValue;
+        if (!isNaN(card)) {
+            return parseInt(card);
+        }
+        switch (card) {
+            case 'J':
+            case 'Q':
+            case 'K':
+                return 10; 
+            case 'A':
+                return 11; 
+            default:
+                throw new Error("Invalid card value"); 
+        }
+    }
+    module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,27 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+
+test("should return 2 for 2 of Hearts", () => {
+    expect(getCardValue("2♥")).toEqual(2);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Diamonds", () => {
+    expect(getCardValue("J♦")).toEqual(10);
+});
+
+test("should return 10 for Queen of Clubs", () => {
+    expect(getCardValue("Q♣")).toEqual(10);
+});
+
+test("should return 10 for King of Spades", () => {
+    expect(getCardValue("K♠")).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Spades", () => {
+    expect(getCardValue("A♠")).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error for empty string", () => {
+    expect(() => getCardValue("")).toThrow("Invalid card value");
+});
