cd5fb32933d8b7b12cff798996f5403d256aaabe
PawelBroilo <88758214+PawelBroilo@users.noreply.github.com>
2025-03-09 18:35:39 +0000

    angle-types.js <!--
    You must title your PR like this:
    
    COHORT_NAME | FIRST_NAME LAST_NAME | REPO_NAME | WEEK
    
    For example,
    
    NW4 | Carol Owen | HTML-CSS-Module | Week1
    
    Complete the task list below this message.
    If your PR is rejected, check the task list.
    
    -->
    
    ## Learners, PR Template
    
    Self checklist
    
    - [ ] I have committed my files one by one, on purpose, and for a reason
    - [ ] I have titled my PR with COHORT_NAME | FIRST_NAME LAST_NAME | REPO_NAME | WEEK
    - [ ] I have tested my changes
    - [ ] My changes follow the [style guide](https://curriculum.codeyourfuture.io/guides/contributing/)
    - [ ] My changes meet the [requirements](./README.md) of this task
    
    ## Changelist
    
    Briefly explain your PR.
    
    ## Questions
    
    Ask any questions you have for your reviewer.

---
+++ Java Script Angles.js
@@ -0,0 +1,12 @@
+function getAngleType(angle) {
+    if (angle === 90) return "Right Angle";
+function AngleType(90);
+}
+
+function assertEquals(actualOutput, targetOutput) {
+    console.assert(
+    actualOutput === targetOutput,
+      `Expected ${actualOutput} to equal ${targetOutput}`
+    );
+}
+
