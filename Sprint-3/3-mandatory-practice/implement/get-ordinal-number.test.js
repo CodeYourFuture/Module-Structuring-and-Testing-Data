735363bf8b5f5e9f635fa906994507bd18184ca3
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-09 16:43:27 +0000

    Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -10,16 +10,20 @@ const getOrdinalNumber = require("./get-ordinal-number");
 
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(101)).toEqual("101st");
+  expect(getOrdinalNumber(99991)).toEqual("99991st");
 });
 
 // Case 2
 test("should return '11th' for 11", () => {
   expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(211)).toEqual("211th");
 });
 
 // Case 3
 test("should return '3rd' for 3", () => {
   expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(113)).toEqual("113th");
 });
 //Case 4
 test("should return '13th' for 13", () => {
