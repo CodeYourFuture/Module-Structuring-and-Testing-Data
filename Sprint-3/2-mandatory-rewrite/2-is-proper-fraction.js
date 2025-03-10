5bd3a82593c8fdea1b49a0b6eeba4353caed85d9
Priscilla <cpjm0033@gmail.com>
2025-03-10 02:10:00 +0000

    removed duplicate function

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,9 +1,4 @@
 function isProperFraction(numerator, denominator) {
-  if (numerator < denominator) return true;
-  // add your completed function from key-implement here
-}
-
-function isProperFraction(numerator, denominator) {
   return Math.abs(numerator) < Math.abs(denominator);
 }
 
