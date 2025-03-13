df418a09ab255fd68c9df6825d56776b6e0f1b12
mohammed-alzaki <mohammedalzaki@outlook.com>
2025-03-13 15:42:50 +0000

    secondError

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+
+////Erorr : ReferenceError: Cannot access 'cityOfBirth' before initialization
+//Fix 
+// Is to put the variable declaration line before the console.log()
