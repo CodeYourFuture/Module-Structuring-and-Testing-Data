b77825e9427c7d2df43a607f9e5270975939e0fa
sarawone <sarawone.poudal@gmail.com>
2025-03-13 23:10:14 +0000

    fixing the structure

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,22 +1,23 @@
 // Predict and explain first...
-//  =============> capitalise the first letter and take out as it from second till end .
+//  =============> Ans : capitalise the first letter and take out as it from second till end .
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 
-function capitalise(str) {
-  let name = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return name;
-}
+//function capitalise(str) {
+  //let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  //return str;
+//}
 
-const Fname = capitalise("sarawone");
-console.log(`The result is ${Fname}`);
+
 
 // =============> write your explanation here
    // Ans: the error is cause of assigning the same variable in again in the function  (str)
 // =============> 
-// function capitalise(str) {
-  //let name = `${str[0].toUpperCase()}${str.slice(1)}`;
-  // return name;
-//}
+ function capitalise(str) {
+  let name = `${str[0].toUpperCase()}${str.slice(1)}`;
+   return name;
+}
+const Fname = capitalise("sarawone");
+console.log(`The result is ${Fname}`);
