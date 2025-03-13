084f425ecfc9764faebbae92c545b39b675203c4
mohammed-alzaki <mohammedalzaki@outlook.com>
2025-03-13 15:31:41 +0000

    count

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// In line three count is a constant variable but since we can not reassign constant variables we will use let here,
+//  We want to reassigning count to whatever the count value was before in this case 0 and we want to add 1 to that value meaning => 0 + 1 = 1.
