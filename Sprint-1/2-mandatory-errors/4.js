488646d2652d33aa97d66408d4d950b6465074e7
mohammed-alzaki <mohammedalzaki@outlook.com>
2025-03-13 15:45:45 +0000

    fourthError

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+/* const 12HourClockTime = "20:53";
+const 24hourClockTime = "08:53"; */
+
+//// Erorr : SyntaxError: Invalid or unexpected token
+// A varible name cannot start with a number 
+//First fix => const HourClockTime12 = "20:53";
+//const hourClockTime24 = "08:53"
+//Second fix:
+const twelveHourClockTime = "20:53";  
+const twentyFourHourClockTime = "08:53"; 
