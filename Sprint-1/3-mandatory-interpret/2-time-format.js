bb4ca32107552e31a5b6559d997dbfd1bcf116ee
sarawone <sarawone.poudal@gmail.com>
2025-03-13 02:01:04 +0000

    updating the comments in the code

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 100784; // length of movie in seconds
+const movieLength = 7350; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -12,15 +12,15 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
-        // 5
+        // 6
 // b) How many function calls are there?
-        //no function calls
+        //0
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
     // getting the remainder
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-        // substract remaining minutes from total minuts and divided by 60
+        // substract remaining second from total second and divided by 60 to get total minutes of the movie 
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
         // duration will be better 
