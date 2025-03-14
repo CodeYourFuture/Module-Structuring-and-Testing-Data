09786705a6b9a978b3bbeae814ff7a73d91f50e3
mohammed-alzaki <mohammedalzaki@outlook.com>
2025-03-14 10:08:53 +0000

    debuggingSum

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//Here we have return; and => a + b ; in seprate lines closed with semicolon.
 
-function sum(a, b) {
+/* function sum(a, b) {
   return;
   a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); */
 
 // =============> write your explanation here
+//AS we know that return end and stops the execution of the function witch mean that => a + b
+//will not be executed and line 8 is useless since the function stops at line 7.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
