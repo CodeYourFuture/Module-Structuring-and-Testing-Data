07744d9b6dfd53662d75fe93f10f09afea7d02ce
mohammed-alzaki <mohammedalzaki@outlook.com>
2025-03-14 09:54:51 +0000

    strError

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,19 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/* function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
-}
+} */
 
 // =============> write your explanation here
+// An error occured => SyntaxError: Identifier 'str' has already been declared
+//because in javascript we cant declare a variable more than once within scope .
+
 // =============> write your new code here
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+   return str;
+ }
+ 
+ console.log(capitalise("cool"))
