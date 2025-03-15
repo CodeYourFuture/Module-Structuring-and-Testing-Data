29cfa4bb41175fa75115aa6e21b421b184c64eb8
jerry-yorsalem <yorsalemmesmer9@gmail.com>
2025-03-15 12:04:45 +0000

    implement a function repeat

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,13 @@
-function repeat() {
-    return "hellohellohello";
+//function repeat() {
+   // return "hellohellohello";
+//}
+
+function repeat(str, times) {
+    if (times < 0) {
+        throw new Error("Count cannot be negative");
+    }
+    return str.repeat(times);
 }
+
 
 module.exports = repeat;
