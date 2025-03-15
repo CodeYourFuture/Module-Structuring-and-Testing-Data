0f6767177b13dbad3c0754d4366bf8f4435ea393
sarawone <sarawone.poudal@gmail.com>
2025-03-15 10:49:40 +0000

    changing to original version

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -8,16 +8,6 @@
 // └──────┴──────────────┴──────┴─────┘
 
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
-// The diagram below shows the different names for parts of a file path on a Unix operating system
-
-// ┌─────────────────────┬────────────┐
-// │          dir        │    base    │
-// ├──────┬              ├──────┬─────┤
-// │ root │              │ name │ ext │
-// "  /    home/user/dir / file  .txt "
-// └──────┴──────────────┴──────┴─────┘
-
-// (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
