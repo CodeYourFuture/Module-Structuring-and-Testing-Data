9e4d56bfe1bfcf78a87c5d7940c13139a955d274
sarawone <sarawone.poudal@gmail.com>
2025-03-15 10:51:08 +0000

    changing to original version

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,12 +7,3 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
-console.log (`The fourth step is ${step4}`);
-
-const step5 = step4 + minimum    //add with the minimun number 
-console.log (`The final step is ${step5}`);
-
-// In this exercise, you will need to work out what num represents?
-// Try breaking down the expression and using documentation to explain what it means
-// It will help to think about the order in which expressions are evaluated
-// Try logging the value of num and running the program several times to build an idea of what the program is doing
