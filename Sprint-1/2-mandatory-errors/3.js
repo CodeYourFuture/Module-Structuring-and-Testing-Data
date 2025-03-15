99115544c07af5db81c18f37b0000aa2b30f809d
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-15 12:16:44 +0000

    complete the 2-mandatory-errors

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,6 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// I predicted of the slice method is not right for it. We can use the slice method for arrays. 
+// We can convert the card number to String and then we can use split method to find the last 4 digits. 
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+//We can not start it with numbers. The variable name should start with alphabet. 
