8609859f7eed73764cc57545081b62da5d9c0ae9
Mansoor <mansoor_m19@hotmail.co.uk>
2025-03-16 17:29:52 +0000

    Created a function which calculates BMI of person

--- Sprint-2/implement/bmi.js
+++ Sprint-2/implement/bmi.js
@@ -3,7 +3,11 @@
 // The BMI calculation divides an adult's weight in kilograms (kg) by their height in metres (m) squared.
 
 // For example, if you weigh 70kg (around 11 stone) and are 1.73m (around 5 feet 8 inches) tall, you work out your BMI by:
+function BMI(weightInKg, heightInMeter) {
+  return (weightInKg / (heightInMeter * heightInMeter)).toFixed(1);
+}
 
+console.log(BMI(70, 1.73));
 // squaring your height: 1.73 x 1.73 = 2.99
 // dividing 70 by 2.99 = 23.41
 // Your result will be displayed to 1 decimal place, for example 23.4.
