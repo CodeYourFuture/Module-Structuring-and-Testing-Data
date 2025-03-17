26fd07c83e6ca0b39c8122379bc9b8f8f69809f6
sarawone <sarawone.poudal@gmail.com>
2025-03-17 22:33:22 +0000

    modify the function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,6 +1,14 @@
 function countChar(stringOfCharacters, findCharacter) {
+
+    if (findCharacter.length == 1)
+    {
     const count = stringOfCharacters.split(findCharacter);
     return  count.length-1;
+    }
+    else 
+    {
+        return 'The finding character should not more than one character'
+    }
 }
 
 
