15b7da4b9a197b89584641dc35c393e2e62dab27
emladeno <mladenova.elvira@yahoo.com>
2025-03-17 18:55:44 +0000

    I have committed the  changes

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,6 +1,6 @@
 function repeat(str, count) {
   if (count < 0) {
-    throw new Error("Count cannot be negative.");
+    throw new Error("Count must be a non-negative integer.");
   }
   
   if (count === 0) {
