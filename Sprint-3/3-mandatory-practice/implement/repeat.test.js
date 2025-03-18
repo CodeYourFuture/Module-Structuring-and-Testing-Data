c104a2bc6276e35b5cb662588c6ddb1fb3ca41f0
Segun <segunfolayan@yahoo.com>
2025-03-18 16:26:18 +0000

    NewUpdated

--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -20,6 +20,14 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
+test("should reproduce the string with no repetitions", () => {
+    const str = "hello";
+    const count = 1;
+    const repeatedStr= repeat(str,count);
+    expect(repeatedStr).toEqual("hello")
+}
+);
+
 
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
@@ -30,7 +38,7 @@ test("should repeat the string count times", () => {
     const count = 0;
     const repeatedStr = repeat(str, count);
     
-    expect(repeatedStr).toEqual(" ");
+    expect(repeatedStr).toEqual("");
     });
 
 
