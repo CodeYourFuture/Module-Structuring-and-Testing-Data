ba6ae28a6193781705e070faac8335d0201d4456
obert <obertilimandzi@gmail.com>
2025-03-20 13:16:26 +0200

    fixed the code its working now

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// there was a hoisting error the variable was only initialized after the console.log so i moved the declaration to the top
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
