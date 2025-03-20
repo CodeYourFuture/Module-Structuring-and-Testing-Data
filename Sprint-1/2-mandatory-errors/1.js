eb314285e904159c6f5e65e99e71537ece3d1009
obert <obertilimandzi@gmail.com>
2025-03-20 13:09:45 +0200

    comment out previous code then added new one

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+// const age = 33;
+// age = age + 1;
+
+let myAge = 30;
+myAge = myAge + 1;
+
+console.log(myAge);
