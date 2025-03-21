720204055e98a136c8c1d77f377ce198eee500d5
obert <obertilimandzi@gmail.com>
2025-03-21 09:05:11 +0200

    replied to all suggestions

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,6 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-//function capitalise(str) {
-  //let output = `${str[0].toUpperCase()}${str.slice(1)}`;
-  //return str;}
-
 // =============> write your explanation here
   // The code will not display anyting since there is no console.log
   // I see that the variable has been named twice so there will be an error
