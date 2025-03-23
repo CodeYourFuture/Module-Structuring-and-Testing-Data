9050f928524877dc64f4303bbeaeda7ec0f7fc4c
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-23 12:25:40 +0000

    2-mandatory-rewrite 2-is-proper-fraction files update

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -1,11 +1,21 @@
 const isProperFraction = require("./2-is-proper-fraction");
 
-test("should return true for a proper fraction", () => {
-  expect(isProperFraction(2, 3)).toEqual(true);
+test("should return true for a proper fraction (2/3)",() => {
+    expect(isProperFraction(2,3)).toBe(true);
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return true for a improper fraction (3/1)",() => {
+    expect(isProperFraction(3,1)).toBe(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative fraction (-3/5)",() => {
+    expect(isProperFraction(-3,5)).toBe(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return true for a equal numerator and denominator fraction (5/5)",() => {
+    expect(isProperFraction(5,5)).toBe(false);
+});
+
