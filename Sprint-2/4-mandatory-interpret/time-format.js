982c6c491083d0140387fd6e0bc728fe6a07e650
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-25 03:08:21 +0000

    4-mandatory-interpret time-format file update

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,6 +11,8 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(88));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
@@ -18,17 +20,22 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// The pad function is called three times inside the formatTimeDisplay function. One for totalHours, one for remainingMinutes and one for the remainingSeconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// it is 0
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// it is 0
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The last time pad is called is for remainingSeconds, which is calculated as remainingSeconds = 88 % 60 = 1 when formatTimeDisplay(88) is called.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The last call to pad is for remainingSeconds, which is 1. The padStart(2, "0") will pad this number to make it two digits. And return value =>"28" seconds.
