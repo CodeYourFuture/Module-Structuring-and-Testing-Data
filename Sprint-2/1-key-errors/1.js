921db185beb0b5b5b516856ba8e1a4043e02fed1
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-25 00:28:31 +0000

    1-key-errors file 1 update

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -5,16 +5,27 @@
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> write your explanation here
+// The function convertToPercentage(decimalNumber) accepts a parameter decimalNumber
+//Inside the function, we have decimalNumber redeclared in the line 9, which creates a conflict
+//we should remove the redeclaration of decimalNumber inside the function. Instead, use the parameter directly.and pass a decimal number as input => 0.5 in the function call
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+const decimalNumber = 0.5;
+console.log(convertToPercentage(decimalNumber)); // Output is "50%"
