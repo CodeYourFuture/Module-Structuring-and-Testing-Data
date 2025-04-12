92d260158180f9cc9e3669fb2a09a3f37364f81d
Sripathmanathan <111650472+sripathman@users.noreply.github.com>
2025-04-12 12:47:10 +0100

    Delete Sprint-1 directory

--- Sprint-1/2-mandatory-errors/1.js
+++
@@ -1,9 +0,0 @@
-// trying to create an age variable and then reassign the value by 1
-
-const age = 33;
-age = age + 1;
-/*  In JavaScript (and many other languages), const declares a constant,
- meaning its value cannot be changed after it's initially assigned. */
- let age = 33; 
-age = age + 1; 
-console.log(age); 
--- Sprint-1/2-mandatory-errors/2.js
+++
@@ -1,9 +0,0 @@
-// Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
-//const cityOfBirth = "Bolton";
-/*Always declare your variables (especially const and let)
- before you attempt to use them*/
- const cityOfBirth = "Bolton";
- console.log(`I was born in ${cityOfBirth}`);
