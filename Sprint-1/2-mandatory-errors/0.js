f8d8165176ed852d04008db22bf14f265f49bc55
Sripathmanathan <111650472+sripathman@users.noreply.github.com>
2025-04-12 12:45:56 +0100

    Delete Sprint-1 directory

--- Sprint-1/2-mandatory-errors/0.js
+++
@@ -1,5 +0,0 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
-in javascript .
-// This is just an instruction for the first activity - but it is just for human consumption
-// We don't want the computer to run these 2 lines - how can we solve this problem?
