f15959bd4b171f0e7d35526f357e1d0d12c53e86
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-05-19 13:40:50 +0100

    update 2-mandatory-errors

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,7 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//Answer:
+//There are two main ways to write comments 
+//single line comments starts with // 
+//Multi-line comment starts with /* and end with */
