472a038838e9bb4f270d8d3dd315524dbbf8ec91
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-05-19 12:20:23 +0100

    update 1-key-exercises

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,19 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+//Answer:
+// num is a randomly generated integer between 1 and 100 (inclusive).
+console.log(num);
+
+// Breakdown:
+// 1. Math.random() generates a float between 0 (inclusive) and 1 (exclusive).
+// 2. (maximum - minimum + 1) ensures the range includes the maximum (100 - 1 + 1 = 100).
+// 3. Multiplying Math.random() by 100 scales the range to [0, 100).
+// 4. Math.floor() rounds down to the nearest integer.
+// 5. + minimum shifts the range from [0, 99] to [1, 100].
+
+
+
+
