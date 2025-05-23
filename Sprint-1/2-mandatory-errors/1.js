b8eb50c460f4d56dbbdcb1e1d6560c7865fff0bd
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-05-23 16:38:56 +0100

    Done: Predicting and interpreting errors

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,19 @@
 // trying to create an age variable and then reassign the value by 1
+let age = 33;
+age = age + 1;
 
+/* 
+ERROR DEBUGGING
 const age = 33;
 age = age + 1;
+
+  
+Error Type: TypeError: Assignment to constant variable.
+
+This error occurs because it's tried to change the value of a variable declared with const.
+
+Variables declared with const are constant, meaning their value cannot be reassigned after initialization.
+
+In this case, the variable age is declared with const and it's tried to update it with age = age + 1; which is not allowed.
+
+  */
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,30 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
+
+
+/*   
+    ERROR DEBUGGING
+
+    const 12HourClockTime = "20:53";
+    const 24hourClockTime = "08:53";
+
+                      My Prediction:
+    The variable names `12HourClockTime` and `24hourClockTime` start with digits, 
+    which is not allowed in JavaScript variable names. Variable names must begin 
+    with a letter, underscore `_`, or dollar sign `$`.
+
+    So, I predict the code will throw a SyntaxError like:
+        → SyntaxError
+
+                     What I Got:
+    → SyntaxError: Invalid or unexpected token
+
+                    Explanation:
+    JavaScript does not allow variable names to begin with a number. 
+    When you try to declare a variable like `const 12HourClockTime`, 
+    JavaScript throws a SyntaxError because it's invalid syntax.
+
+
+
+*/
