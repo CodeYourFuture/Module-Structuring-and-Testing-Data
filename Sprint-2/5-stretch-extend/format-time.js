cf5d6eede496a0f938867df5a9cf682a823448e7
shreefAhmedM <livecoding.33@gmail.com>
2025-05-25 12:17:18 -0700

    sprint2 exercies has been

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,10 +4,22 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+  const minutes = time.slice(3); 
+  let currTime = "am";
+  let HoursModefied = hours;
+
+  if (hours === 0) {
+    HoursModefied = 12;
+  } else if (hours === 12) {
+    currTime = "pm";
+  } else if (hours > 12) {
+    HoursModefied = hours - 12;
+    currTime = "pm";
   }
-  return `${time} am`;
+
+  const formattedHour = HoursModefied < 10 ? "0" + HoursModefied : HoursModefied.toString();
+  return `${formattedHour}:${minutes} ${currTime}`; 
+   
 }
 
 const currentOutput = formatAs12HourClock("08:00");
@@ -17,9 +29,38 @@ console.assert(
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
+
 const currentOutput2 = formatAs12HourClock("23:00");
 const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+const currentOutput3 = formatAs12HourClock("01:00");
+const targetOutput3 = "01:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("14:00");
+const targetOutput4 = "02:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("18:00");
+const targetOutput5 = "06:00 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+);
+
+const currentOutput6 = formatAs12HourClock("00:00");
+const targetOutput6 = "12:00 am";
+console.assert(
+  currentOutput6 === targetOutput6,
+  `current output: ${currentOutput6}, target output: ${targetOutput6}`
+);
+ 
