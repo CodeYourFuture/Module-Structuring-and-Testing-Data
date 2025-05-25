d527612e5f7d4a9ee2ee17aca22230823a9000da
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-05-25 13:02:43 +0100

    Done : JavaScript prediction and debugging exercises

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,40 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+/*
+
+320
+The result of multiplying 10 and 32 is undefined
 
 function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+*/
 // =============> write your explanation here
+/*
+The multiply function logs the result of a * b but does not return it.
+
+So when multiply(10, 32) called inside the template literal, it logs 320 to the console as a side effect.
+
+But since the function doesn't return anything, its return value is undefined.
+
+Therefore, the template literal evaluates to:
+
+"The result of multiplying 10 and 32 is undefined"
+
+*/
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//result
+//The result of multiplying 10 and 32 is 320
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -3,6 +3,13 @@
 // Predict the output of the following code:
 // =============> Write your prediction here
 
+/*
+The last digit of 42 is 3  
+The last digit of 105 is 3  
+The last digit of 806 is 3
+
+
+
 const num = 103;
 
 function getLastDigit() {
@@ -15,10 +22,31 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+
+The last digit of 42 is 3
+The last digit of 105 is 3
+The last digit of 806 is 3 */
+
 // Explain why the output is the way it is
 // =============> write your explanation here
+/*
+The function getLastDigit() does not accept any parameters, so even though it is called like getLastDigit(42), the 42 is ignored.
+
+The function only uses the globally defined const num = 103.
+
+It always returns the last digit of 103, which is "3" — this is why all 3 outputs are the same
+*/
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+ //getLastDigit is not working properly because it uses the globally defined const num = 103
+ 
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
