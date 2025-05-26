d753067fb0e7b9eab7a71e6a6128d71c6b5e2976
LoaiRadwan <loairadwan6@gmail.com>
2025-05-26 11:14:19 +0100

    predict , explain and correct the functions in 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+//we wrote console.log twice for the same thing in the function and when we called
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +10,13 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// when we call the function multiply it will print the result, that means we don't have the result  so this will give an undefined value
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+// function multiply(a, b) {
+//   return `${a * b}`;
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
