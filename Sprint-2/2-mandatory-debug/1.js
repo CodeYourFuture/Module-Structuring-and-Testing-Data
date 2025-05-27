a2246d8a181fdaca692c3182ce8a3393dfc4631e
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-05-27 13:21:16 +0100

    update 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//In JavaScript, if you write return and then go to a new line, JavaScript will automatically add a semicolon after return. This means the function will return undefined, and the code like a + b on the next line won’t run at all.
+
+
+
+
+// =============> write your explanation here
+//The expression and the return keyword should be in the same line 
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 
 function sum(a, b) {
-  return;
+  return 
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
