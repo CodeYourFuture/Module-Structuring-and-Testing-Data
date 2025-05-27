ee6cd5d5b1f00fb7367cdb9afb11662831ba7fb1
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-05-27 10:39:16 +0100

    update 1-key-errors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+//When we declare a function parameter it implicitly declare str as a variable therefore we should not redeclare str variable inside the function 
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+// SyntaxError: Identifier 'str' has already been declared 
+// as expected JS throws syntaxError which means it is not allowed to redeclare str variable in the same scope 
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
 
 // =============> write your explanation here
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise('book'))
+
+//The function works because we reassign the parameter str instead of redeclaring it.
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,36 @@
 // Predict and explain first...
-
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+//Here again we are facing the same problem as 0.js. The variable decimalNumber is implicitly declared and we should not redeclare it.
+
 
 // Try playing computer with the example to work out what is going on
+// =============> write your explanation here
+/*Imagine calling the function:
+convertToPercentage(0.7);
+We'll go line by line like the computer:
+The computer sees the parameter decimalNumber = 0.7
+Then it tries to run this line:
+const decimalNumber = 0.5;
+❗ Problem: JavaScript throws an error here, because we're trying to declare a new variable called decimalNumber, which already exists as a parameter.
+The computer stops and throws a SyntaxError: Identifier 'decimalNumber' has already been declared.*/
+
+
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+/*If hardcoding is intentional we can use the default parameter : 
+function convertToPercentage(decimalNumber = 0.5) {
+  return `${decimalNumber * 100}%`;
+}
+*/
