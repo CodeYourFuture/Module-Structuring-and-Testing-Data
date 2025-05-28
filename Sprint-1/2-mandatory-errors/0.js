5886ee87ace0df545b0c8cb4ef41499d702aa0d4
galyna-k <hkzlvs@gmail.com>
2025-05-28 16:09:18 +0100

    update 0.js
    comment out text to prevent SyntaxError

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// Explanation:
+// These lines are instructions, not JavaScript code
+// Added // to ignore them and prevent SyntaxError when running the file
