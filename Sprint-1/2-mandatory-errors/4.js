cc72238aaea5fb87f354c6911eed3806f85d2a15
galyna-k <hkzlvs@gmail.com>
2025-05-28 16:27:38 +0100

    update 4.js
    fix SyntaxError by renaming variables starting with numbers

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// Rename variables to valid identifiers because names cannot start with a number
+const ClockTime_12Hour = "08:53";
+const ClockTime_24Hour = "20:53";
+
+// Output the values
+console.log("Clock time in 12h format:", ClockTime_12Hour);
+console.log("Clock time in 24h format:", ClockTime_24Hour);
