fd190980569bed073132352f5391dd85d2b424e4
gerbil1511 <gerbil1511@gmail.com>
2025-05-30 00:30:17 +0100

    update ext variable with correct code

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -21,7 +21,7 @@ console.log(`The dir part of ${filePath} is ${dir}`);
 
 // Create a variable to store the ext part of the variable
 // pseudocode: get the index of the last "." then get the string from that index to the end of the string (since we know the filename has a "." extension)
-const ext = base.lastIndexOf(".");
+const ext = base.slice(base.lastIndexOf(".") + 1);
 console.log(`The ext part of ${filePath} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
