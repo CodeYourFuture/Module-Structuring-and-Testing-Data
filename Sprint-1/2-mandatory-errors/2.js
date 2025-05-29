e003f33e648d2843ce236bdec9774476f1521e27
gerbil1511 <gerbil1511@gmail.com>
2025-05-30 00:39:07 +0100

    add further explaination to comment to define 'hoisting' in javascript

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -6,6 +6,8 @@ console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
 */
 
-// The error in the above code happens because the variable `cityOfBirth` is being used before it has been declared. In JavaScript, variables declared with `const` or `let` are not hoisted, meaning they cannot be accessed BEFORE their declaration. Therefore we need to declare the variable BEFORE using it in the console.log statement.
+/*
+The error in the above code happens because the variable `cityOfBirth` is being used before it has been declared. In JavaScript, variables declared with `const` or `let` are not hoisted, meaning that they are not able to be accessed BEFORE their declaration ('hoisting' is JavaScript's default behavior of moving all declarations to the top of the current scope (to the top of the current script or the current function)). Therefore we need to declare the variable BEFORE using it in the console.log statement for the script to run properly.
+*/
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
