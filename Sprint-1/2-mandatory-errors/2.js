ae1857d97dc03c8949852cffd34badb513fcec5c
gerbil1511 <gerbil1511@gmail.com>
2025-05-29 23:43:23 +0100

    re-arrange order of code provided to declare the variable first before console logging it

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,11 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
+/*
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+*/
+
+// The error in the above code happens because the variable `cityOfBirth` is being used before it has been declared. In JavaScript, variables declared with `const` or `let` are not hoisted, meaning they cannot be accessed BEFORE their declaration. Therefore we need to declare the variable BEFORE using it in the console.log statement.
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
