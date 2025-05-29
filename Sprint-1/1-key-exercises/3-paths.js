97c4f2b03ded1d802750b4ccb30c80f8aa1b6899
Amy-Rose <amyrlcollins@gmail.com>
2025-05-29 16:36:57 +0100

    key exercises done

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -12,12 +12,16 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The base part is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-// Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+firstSlashIndex = filePath.indexOf("/");    //the first / indicates the root...I think
+const dir = filePath.slice(firstSlashIndex+1,lastSlashIndex+1);     //extract the part after the first / and to the last /
+console.log(`The directory of the file path is ${dir}`);
+
+// Create a variable to store the ext part of the variable
+const ext = base.slice(base.lastIndexOf("."));      //find the last . in the file path, NB their may be more than one
+console.log(`The extension of the file path is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
