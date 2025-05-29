596cafcbb4f2cca30c54506c42834f1d96baa947
Amy-Rose <amyrlcollins@gmail.com>
2025-05-29 17:11:06 +0100

    finished mandatory exercises 1 of 2

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,14 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
+// ReferenceError: Cannot access 'cityOfBirth' before initialization
+
+// line 4 (console.log) runs before line 5 (const defined)
+// when console.log runs, there is no variable called cityOfBirth
+// reverse the lines so that the variable is defined before anything attempts to use it
+
+const cityOfBirth = "Northampton";
 console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+// const cardNumber = 4533787178994213;
+// const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,21 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// PREDICTION:
+// I actually don't know how slice works well enough yet to guess! Is it maybe only returning the last 3 values?
+
+// ACTUAL ERROR
+// TypeError: cardNumber.slice is not a function
+
+// CONSIDER
+// Ohhhh, .slice is a string/array method. cardNumber is a number not an array.
+
+// UPDATE:
+
+const cardNumber = "4533787178994213";  //enter card number as a string
+
+// console.log(typeof(cardNumber));
+
+last4Digits = cardNumber.slice(-4); //can now use string method
+console.log(last4Digits);
