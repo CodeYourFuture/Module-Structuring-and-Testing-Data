17f7ed3d2512dac54eeb22eecb99064fc46864da
gerbil1511 <gerbil1511@gmail.com>
2025-05-29 23:40:08 +0100

    adjust variable declaration in provided code to ensure no errors are thrown

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,12 @@
 // trying to create an age variable and then reassign the value by 1
 
+/*
 const age = 33;
 age = age + 1;
+*/
+// This code will throw an error because it is trying to reassign a value to a constant variable.
+
+let age = 33;
+age = age + 1;
+// This code will not throw an error because it is using a let variable, which can be reassigned.
+console.log(age);
