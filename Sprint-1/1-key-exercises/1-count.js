209ea8ffbe13c26c7d195e9a57e78d4e4eceea19
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-05-29 18:38:08 +0100

    explained line 3 - variable reassignment

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,9 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/*
+ Answer: in line 3, the variable count gets reassigned a new value which is the sum of the old value and 1.
+In other words, the variable count is now adding 1 to its old value to obtain a new value.
+*/
+
