5c1b48b34f2030b03fcc53af7266ea1a3f79f46d
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-05-30 16:44:01 +0100

    declaration keyword changed to let to solve error

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,15 @@
 
 const age = 33;
 age = age + 1;
+
+/*
+I got this error message "TypeError: Assignment to constant variable." after running 
+my code. It happened because when assigned a value to an identifer using const -short 
+for constant - it emplies that we have no intention of changing the value. it remains 
+constant. the second line the variable get reassigned a new value and therefore the 
+error message above showed up.
+To solve this issue the variable age could simply be declared via "let" as seen below:
+*/
+
+let age = 33;
+age = age + 1;
