547fbebac8b165faeefe6660923c18411e913b25
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-05-30 22:30:49 +0100

    renamed variables to avoid syntax error

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+// to avoid the syntax error after running the above lines of code do as below:
+
+const ClockTime12Hour = "20:53";
+const ClockTime24hour = "08:53";
