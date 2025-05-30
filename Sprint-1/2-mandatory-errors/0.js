8ab5a4ef6cfff054ca77a6f4cbbe86ec32e12d02
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-05-30 10:00:07 +0100

    Commented to avoid running my lines of code

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// we can simply comment the 2 lines, just like what I am doing here! 
+// single line comments use "//" multi-line comments use/* */ 
