e8108f8038a9e7df71f1576ec34e24930b17851a
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-05-30 16:31:19 +0100

    SyntaxError solved by commenting

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,5 +1,17 @@
 This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+We don't want the computer to run these 2 lines - how can we solve this problem? 
 
-// we can simply comment the 2 lines, just like what I am doing here! 
-// single line comments use "//" multi-line comments use/* */ 
+/* 
+I got "SyntaxError: Unexpected identifier" error message. It happen because the 2 lines don't make sense.
+the computor sees the first word and it assumes a variable or a function is being declared but then when 
+it reads the resut of the sentence it breaks as this is not how an identifier is being used and formed in 
+a programme.
+To avoid this issue, we can just simply comment the 2 lines, just like what I am doing here! 
+
+*/
+
+/* 
+This is just an instruction for the first activity - but it is just for human consumption
+We don't want the computer to run these 2 lines - how can we solve this problem? 
+*/
+
