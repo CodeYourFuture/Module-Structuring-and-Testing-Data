e16cbdc42e7da49aec0f1cd1b66d48f0fc840bcf
AdnaanA <thetriplex247@gmail.com>
2025-05-30 15:29:05 +0100

    fixed declaration of CityofBirth

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// The error is that the variable cityOfBirth is not defined before it is used.
+// To fix this, we need to declare the variable cityOfBirth before using it.
+// Declare the variable cityOfBirth and assign it the value "Bolton"
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
