215b30910daad75666aa2034e569a4cb13b1508e
Sisu860 <meharisisay6@gmail.com>
2025-06-01 00:05:18 +0100

    Add counter increment logic

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-//The equal sign is reassigning the count variable with the answer of count + 1
+//Line 3 is doing: - its reads the current value of count (which is 0), 
+// then it adds 1 to that value (0 + 1 = 1)
+// then, it reassigns that result (1) back into count variable.
+// The = symbol is the assignment operator, Take the value on the right side, and store it into the variable on the left side.
