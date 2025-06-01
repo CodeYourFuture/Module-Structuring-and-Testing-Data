9a72d78fb7d88e3c8e5279c819dfc24cd5993aa1
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-06-01 20:18:09 +0200

    errors section complete

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,9 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+  // In the function, str is used as an argument and therefore already declared. 
+  // Inside the function, str is being re-declared. It is also not good practice to mutate the argument variable.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +13,13 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+
+  // The error message received is "SyntaxError: Identifier 'str' has already been declared"
+  // This confirms my predication.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisedStr;
+}
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,23 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+    // A number cannot be used as a function argument when the function is declared.
+    // The variable num has also not been declared.
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+    // SyntaxError: Unexpected number
 
 // =============> explain this error message here
+    // The error indicates that the number 3 has been placed in the wrong place.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
+    function square(num) {
+    return num * num;
+}
 
