2de2850ffce1ec76bbce386398a3b78ac431d01c
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-06-01 11:19:00 +0100

    update 5-stretch-extend

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -2,12 +2,26 @@
 // Make sure to do the prep before you do the coursework
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
-function formatAs12HourClock(time) {
+/*function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
   if (hours > 12) {
     return `${hours - 12}:00 pm`;
   }
   return `${time} am`;
+}*/
+
+function formatAs12HourClock(time) {
+  const hours = Number(time.slice(0, 2));
+  const minutes = time.slice(3,5);
+  if (hours === 0) {
+    return `12:${minutes} am`;
+  } else if (hours === 12) {
+    return `12:${minutes} pm`;
+  } else if (hours > 12) {
+    return `${hours-12}:${minutes} pm`
+  } else {
+    return `${hours.toString().padStart(2, "0")}:${minutes} am`;
+  }
 }
 
 const currentOutput = formatAs12HourClock("08:00");
@@ -22,4 +36,18 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("12:00");
+const targetOutput4 = "12:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
 );
