b06911fb860a1d7f81aa788a3c90882de2e06058
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-06-02 13:53:08 +0100

    Done:Mandatory-interpret and stretch-extend

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -1,5 +1,5 @@
 function pad(num) {
-  return num.toString().padStart(2, "0");
+  return num.padStart(2, "0");
 }
 
 function formatTimeDisplay(seconds) {
@@ -18,17 +18,33 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+/*3 times
+(Once for hours, once for minutes, once for seconds) */
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
-
+console.log(formatTimeDisplay(61))
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+/*
+0
+(The first call is pad(totalHours) which is pad(0))
+ */
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
-
+/*
+"00"
+(0 → string → padded to 2 digits → "00")
+*/
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-
+/*
+1
+(The last call is pad(remainingSeconds) → pad(1))
+ */
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+/*
+"01"
+(The number 1 → string "1" → padded to 2 digits → "01")
+*/
