004e88b6b72c4aee5aff59fe11b61da376f68ad2
AdnaanA <thetriplex247@gmail.com>
2025-06-03 12:14:05 +0100

    modified:   Sprint-1/3-mandatory-interpret/1-percentage-change.js 	modified:   Sprint-1/3-mandatory-interpret/2-time-format.js

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -11,21 +11,26 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // Read the code and then answer the questions below
 
-// a) How many function calls are there in this file? Write down all the lines where a function call is made => linnes 4,5, 7, and 8
-// There are four function calls in this file:
-// - Line 4: carPrice.replaceAll(",", "")
-// - Line 5: priceAfterOneYear.replaceAll(",", "")
-// - Line 7: Number(carPrice.replaceAll(",", ""))
-// - Line 8: Number(priceAfterOneYear.replaceAll(",", ""))
+// a) How many function calls are there in this file? Write down all the lines where a function call is made => lines 4,5, 7, and 8
+    // There are four function calls in this file:
+    // - Line 4: carPrice.replaceAll(",", "")
+    // - Line 5: priceAfterOneYear.replaceAll(",", "")
+    // - Line 7: Number(carPrice.replaceAll(",", ""))
+    // - Line 8: Number(priceAfterOneYear.replaceAll(",", ""))
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?=>
-    // The error occurs on line 5 because there is a syntax error in the replaceAll function call. The second argument should be an empty string, but there is a space before the closing double quote.
-// To fix this, remove the space before the closing double quote on line 5:
-// priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", "")); // Corrected line
+    // The error occurs on line 5 because there is a syntax error in the replaceAll function call. The second argument should be an empty string, 
+    // but there is a space before the closing double quote.
+    // To fix this, remove the space before the closing double quote on line 5:
+    // priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", "")); // Corrected line
 
-// c) Identify all the lines that are variable reassignment statements => lines 4 and 5
+// c) Identify all the lines that are variable reassignment statements => 
+    // lines 4 and 5
 
-// d) Identify all the lines that are variable declarations => lines 1, 2, 7, and 8
+// d) Identify all the lines that are variable declarations => 
+    // lines 1, 2, 7, and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
-// The expression Number(carPrice.replaceAll(",", "")) is converting the string representation of the car price (which includes commas) into a number by first removing the commas using replaceAll and then converting the resulting string to a number using the Number function. This allows for numerical calculations to be performed on the car price.
+    // The expression Number(carPrice.replaceAll(",", "")) is converting the string representation of the car price (which includes commas) 
+    // into a number by first removing the commas using replaceAll and then converting the resulting string to a number using the Number 
+    // function. This allows for numerical calculations to be performed on the car price.
--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,21 +12,29 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program? => There are six variable declarations in this program:
-// - Line 1: movieLength
-// - Line 3: remainingSeconds
-// - Line 4: totalMinutes
-// - Line 6: remainingMinutes
-// - Line 7: totalHours
-// - Line 9: result 
+    // - Line 1: movieLength
+    // - Line 3: remainingSeconds
+    // - Line 4: totalMinutes
+    // - Line 6: remainingMinutes
+    // - Line 7: totalHours
+    // - Line 9: result 
 
 // b) How many function calls are there? => There is only one function call in this program:
-// - Line 10: console.log(result)
+    // - Line 10: console.log(result)
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
+    // The expression `movieLength % 60` calculates the remainder when `movieLength` (the length of the movie in seconds) is divided by 60.
+    // This operation is used to determine the number of remaining seconds after converting the total movie length into minutes.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+    // The expression `(movieLength - remainingSeconds) / 60` calculates the total number of minutes in the movie by first subtracting the remaining seconds
+    // from the total movie length (in seconds) and then dividing the result by 60. This gives the total number of complete minutes in the movie.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+    // The variable `result` represents the formatted time of the movie in the format "hours:minutes:seconds". A better name for this variable could be `formattedMovieTime`
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+    // The code will work for all non-negative integer values of `movieLength`. It will correctly calculate the hours, minutes, and seconds for any valid movie length.
+    // However, if `movieLength` is negative, the calculations will still work but may not make sense in the context of a movie's length. 
+    // If `movieLength` is a non-integer (e.g., a float), the results will still be calculated, but the output may not be meaningful in terms of time representation.
