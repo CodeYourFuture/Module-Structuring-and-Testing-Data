98cbe8575e85673d7002f510b3523fa39793643e
Amy-Rose <amyrlcollins@gmail.com>
2025-06-03 11:43:26 +0100

    done errors 2js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,30 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> it's currently being passed an integer not a variabl; the variable num doesn't exist 
 
-function square(3) {
-    return num * num;
-}
+// || ORIGINAL CODE
+
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+//  SyntaxError: Unexpected number
 
 // =============> explain this error message here
+//  we're passing a number to the function when something else is expected
+//  my VS Code has a red squiggly under 3 and says "identifier expected"
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+
+function square(num) {
+    return num*num;
+}
+
+squared = square(3);
+console.log(squared);
 
 
