e405237c57f3bbec870b88c31f12a45cc9d7e763
Amy-Rose <amyrlcollins@gmail.com>
2025-06-03 11:52:03 +0100

    done debug 1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,29 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//    return statement not given anything to return
+// a+b after return statement, not assigned to anything. no red line though. is this valid code somehow? 0.0
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+//  || ORIGINAL CODE
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+//  The sum of 10 and 32 is undefined
+// Same as before, function call returns undefined by default
+
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============>
+
+function sum(a,b) {
+  return(a+b);
+}
+
+num1 = 10;
+num2 = 32;
+
+console.log(`The sum of ${num1} and ${num2} is ${sum(num1,num2)}!`);
