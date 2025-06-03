2636d6d3f57378e8f39cc810668c8396d741dd22
Amy-Rose <amyrlcollins@gmail.com>
2025-06-03 11:38:50 +0100

    done errors 1js

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,29 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> Has the same problem again, decimalNumber is passed in and then code attempts to declare it
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) { // ====> function called with decimalNumber, so this variable is already present in the function scope
+//   const decimalNumber = 0.5; // ===> attempting to declare decimalNumber again
+//   const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//   return percentage;
+// }
 
-console.log(decimalNumber);
+// console.log(decimalNumber); // ====> presumably we want to be logging the returned percentage value 
 
 // =============> write your explanation here
+//  SyntaxError: Identifier 'decimalNumber' has already been declared
 
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============>
+
+function convertToPercentage(decimalNumber) { 
+  return `${decimalNumber * 100}%`;
+}
+
+percentage = convertToPercentage(0.38);
+
+console.log(percentage);
