f280b7eaad998a52454d6441cdc7145505cf95bf
Amy-Rose <amyrlcollins@gmail.com>
2025-06-03 14:33:41 +0100

    done mandatory time-format

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -1,14 +1,15 @@
 function pad(num) {
+  //adds leading 0s to string until it reaches length 2
   return num.toString().padStart(2, "0");
 }
 
 function formatTimeDisplay(seconds) {
-  const remainingSeconds = seconds % 60;
-  const totalMinutes = (seconds - remainingSeconds) / 60;
-  const remainingMinutes = totalMinutes % 60;
-  const totalHours = (totalMinutes - remainingMinutes) / 60;
+  const remainingSeconds = seconds % 60; //seconds remaining after whole minutes removed from timeInSeconds
+  const totalMinutes = (seconds - remainingSeconds) / 60; //number of whole minutes remaining when remaining seconds are removed from timeInSeconds
+  const remainingMinutes = totalMinutes % 60; //minutes remaining after whole minutes removed
+  const totalHours = (totalMinutes - remainingMinutes) / 60; //whole hours after remaining minutes removed
 
-  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
+  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`; //00hh:00mm:00ss
 }
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
@@ -17,18 +18,22 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> 3, all on line 12
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
+console.log(formatTimeDisplay(61)); // ==> 00:01:01
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> 1
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> 01
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> 1
+//              looking at the output from the console log above, the number of seconds remaining is 1
+//              this was computed at line 7: remainingSeconds = seconds % 60;
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> 01
+//                .padStart(2,"0") means pad the string (created from num) with 0s, at the beginning of the string, until the length of the string is = 2
