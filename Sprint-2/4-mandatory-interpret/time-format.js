5dc98439720541a4835caa8d342ebd8ad82a1db7
gerbil1511 <gerbil1511@gmail.com>
2025-06-04 20:04:55 +0100

    add answers regarding time format code fromto questions based on time format code in the file

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -17,18 +17,27 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+/*
+when the function is returned pad will be called 3 times - for totalHours, remainingMinutes and remainingSeconds
+*/
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
+console.log(formatTimeDisplay(61)); //result is "00:01:01"
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// When pad is called for the first time the value assigned to num is "0"  as num is 61 which is the same as 61 seconds which means the hours were 0.
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+/*
+ When pad is called for the first time the return value is formatted to "00" based upon the code in the pad function (num.toString().padStart(2, "0") - it converts the number to a string and adds "0" to the left of the string, if the original string is less than 2 characters, until it is 2 characters long)
+*/
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+/*
+The value assigned to num when pad is called for the last time is 1. This is because the function takes an input of 61 and the remainingSeconds variable takes the remainder when 61 is divided by 60 or in other words the number of seconds left after dividing 61 seconds into full minutes (which is 1 minute with 1 second remaining)
+*/
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+/*
+When pad is called for the last time the return value assigned to num is "01" because as noted earlier  the code in the pad function (num.toString().padStart(2, "0") converts the number to a string and adds "0" to the left of the string, if the original string is less than 2 characters, until it is 2 characters long.
+*/
