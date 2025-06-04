80b4d72cd812ff31f715f58a38669c743a094dd7
= <=>
2025-06-04 16:47:39 +0100

    Fixing errors

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+
+// To do that we must select the lines and  press ctrl + / 
