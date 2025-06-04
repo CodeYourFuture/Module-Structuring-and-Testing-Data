51b2550d7e954d3007467dcbceb7a15ae1fad682
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-04 11:30:45 +0200

    identified typeError

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,12 @@
 
 const age = 33;
 age = age + 1;
+
+// TypeError: Assignment to constant variable.
+// The error occurs because 'age' is declared as a constant using 'const',
+// which means its value cannot be reassigned. Attempting to change its value results in a TypeError.
+// To fix this error, we can change 'const' to 'let' or 'var' to allow reassignment.
+// Corrected code:
+let age = 33;
+age = age + 1;
+// Now, 'age' can be reassigned without any error. 
