469b2c8177cf79652dc0b90195aa66877c687fc7
gerbil1511 <gerbil1511@gmail.com>
2025-06-04 14:08:30 +0100

    Create a function that takes a string input & returns the string in upper snake case.

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,24 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+/*
+Firstly declare a function strToUpperSnakeCase() that takes a placeholder argument of "str". Next, in the function declare the variable strToUpperCase that changes all characters of the string input to capital letters. Then declare a variable strToSnakeCase that will take the value of strToUpperCase and then use the method replaceAll() on it, so that instances of " " (the spaces between the words) will all be replaced with "_". Next, return that strToSnakeCase variable. Finally, console.log() (print)the function with a string input of your choice.
+*/
+
+// function strToUpperSnakeCase(str) {
+//   const strToUpperCase = str.toUpperCase();
+//   const strToSnakeCase = strToUpperCase.replaceAll(" ", "_");
+//   return strToSnakeCase;;
+// }
+
+/*
+ This can also be refactored by chaining the methods directly to the "str" input therefore eliminating the need for variables:
+ */
+
+function strToUpperSnakeCase(str) {
+  return str.toUpperCase().replaceAll(" ", "_");
+}
+
+console.log(strToUpperSnakeCase("hello there"));
+console.log(strToUpperSnakeCase("lord of the rings"));
