4bd7fe6d2abe2c097b40aa1875072665145077c4
Innocent_Niwatwine <innocent.pare3@gmail.com>
2025-06-04 11:40:16 +0300

    Inteprete 2

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -13,13 +13,25 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
+// They are 6 Variable Declarations
+
+
 // b) How many function calls are there?
+
+// It is one function call.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+// movieLength % 60 calculates the number of remaining seconds of the movielength if divided by 6o which makes a minute
+
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
 
+// This line subtracts the leftover seconds from the total time and converts the remaining time into full minutes.
+
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+The variable result represents total run time for the movieLength, I would suggest MovieCompleteTime as a better name
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+There would be some odd results, remainingMinutes and remainingSeconds can return a single digit.
