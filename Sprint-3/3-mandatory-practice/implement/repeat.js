3a5ebed39d34b61aa77d1b0bff5bff64466b283d
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-06-04 22:52:15 +0200

    practice\implement completed

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,10 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    throw new Error("Negative counts are not valid");
+  }
+  return str.repeat(count);
 }
 
 module.exports = repeat;
+
+console.log(repeat("hello", 1));
