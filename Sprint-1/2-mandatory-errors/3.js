f545d77083de4ed96eab7280067e2330bbbbfdeb
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-05 17:50:28 +0100

    Using and explaining the .slice() method.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,12 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//My prediction was correct that the last4Digits variable will not work because .slice() methods work only for arrays and strings not numbers.
+
+//console.log(last4Digits);
+
+// To updates the expression 
+const newCardNumber = "4533787178994213";
+const newLast4Digits = newCardNumber.slice(-4);
+console.log(newLast4Digits);
