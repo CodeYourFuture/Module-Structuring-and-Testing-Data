cc1247ee002981d95adf2045a6afa812ca7e41b0
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-05 16:17:41 +0100

    interpreted and answered visualizer questions

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -17,18 +17,22 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> write your answer here: 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here: 0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here: "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here: "1". This is because pad(remainingSeconds) is when pad was called for the last time. 
+// since the function formatTimeDisplay is invoked with 61 as an input. the variable remainingSeconds evaluates to 61 % 60 = 1. 
+// Therefore, P(1) is also invoked and as a result 1 is passed as value for the "num" input.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here: "01". When pad(1) is called, the body of the function converts 1 from integer to
+// a string, and then adds "0" to the left aiming to have a string with a total length of 2. Thus, "0" added to "1" to 
+// the left of it forming a string with a total length of 2.
