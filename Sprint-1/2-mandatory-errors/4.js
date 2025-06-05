303d714dcf4284861148e8971a544bd0b92be203
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-05 17:56:38 +0100

    Commenting the wrong lines

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,5 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
 
 // The error is that the variables starts with a number, which is not allowed in JavaScript and most programming languages.
 
