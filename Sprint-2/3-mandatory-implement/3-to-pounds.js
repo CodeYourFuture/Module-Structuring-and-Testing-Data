aedd6aed0bc9393d589422cddf5fe2f1184a864c
Amy-Rose <amyrlcollins@gmail.com>
2025-06-05 11:17:47 +0100

    added possibility of number input or penceToPounds

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,24 +4,27 @@
 // You should call this function a number of times to check it works for different inputs
 
 
-function penceToPounds(penceString) {
+function penceToPounds(pence) {
 
-    penceString = penceString.replaceAll("p", ""); //remove any ps present
+  pence = pence.toString()
+
+  pence = pence.replaceAll("p", ""); //remove any ps present
 
 
-    penceString = penceString.replace(/^0+/, ""); //remove any leading 0s 
+  pence = pence.replace(/^0+/, ""); //remove any leading 0s 
 
-    penceString = penceString.padStart(3, "0"); // if string has a length<3, pad it with leading 0s until it reaches length 3 (983p =>=> 983, 40p ==> 40 ==> 040)
+  pence = pence.padStart(3, "0"); // if string has a length<3, pad it with leading 0s until it reaches length 3 (983p =>=> 983, 40p ==> 40 ==> 040)
 
-    return `£${penceString.slice(0, -2)}.${penceString.slice(-2).padEnd(2, "0")}`
+  return `£${pence.slice(0, -2)}.${pence.slice(-2).padEnd(2, "0")}`
     
 }
 
-const myPennies = '369p';
+const myPennies = 369;
 console.log(penceToPounds(myPennies)); //expect £3.69
 console.log(penceToPounds('963')); //expect £9.63
 console.log(penceToPounds('183000p')) //expect £1830.00
 console.log(penceToPounds('000762')); //expect £7.62
+console.log(penceToPounds('63')); //expect £0.63
 console.log(penceToPounds('abcdefg')) //further input validation would be needed
 
 // changelist:
@@ -29,6 +32,7 @@ console.log(penceToPounds('abcdefg')) //further input validation would be needed
 // removed unnecessary extra variable declarations
 // added line to remove any leading 0s in the original string
 // using .slice() instead of .substring() because it's easier to read
+// added .toString() at the beginning of the function to allow it to take number inputs
 
 
 
