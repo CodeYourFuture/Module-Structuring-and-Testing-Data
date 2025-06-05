39fb6b62ae2950a38c3782136de1520eb951bb6a
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-05 17:55:21 +0100

    The correct way that variables should be renamed.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+// The error is that the variables starts with a number, which is not allowed in JavaScript and most programming languages.
+
+// This is the correct way.
+const HourClockTime = "20:53";
+const hourClockTime = "08:53";
