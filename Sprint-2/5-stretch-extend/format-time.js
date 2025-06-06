0fc3eaa8222c7a87a62faf96a051d33ff1216dfd
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-06 19:20:26 +0100

    changed code to deal with some edge cases

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,11 +4,19 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
-  }
+  if ( 24 > hours && hours > 12) {
+    return `${hours - 12}:00 pm`;      
+  } else if(hours === 00 || hours === 24) {     // Lines 9-10 added to tackle edge cases of when dealing with "00.00" or "24:00" as function inputs.
+    return "12:00 am";
+  } else {
   return `${time} am`;
 }
+}
+
+const currentOutput0 = formatAs12HourClock("00.00");  // Lines 16-18, assertion test passed for the edge cases of "00.00" & "24:00" as a function input.
+const targetOutput0 = "12:00 am";
+console.assert(currentOutput0 === targetOutput0, `current output: ${currentOutput0}, target output: ${targetOutput0}`);
+
 
 const currentOutput = formatAs12HourClock("08:00");
 const targetOutput = "08:00 am";
@@ -23,3 +31,6 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+
+console.log(formatAs12HourClock("24:00"));
