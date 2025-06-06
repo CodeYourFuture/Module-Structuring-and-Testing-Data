d7d36b1ee15fc15185dae7e26004dd36a1d172a0
Emilianouz <emiliano.uru.uk@gmail.com>
2025-06-06 09:39:29 +0100

    Part3-Interpretation tasks. All were interpreted and answered.

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,18 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
-
+//   Lines: 1, 3, 4, 9. Total 4
 // b) How many function calls are there?
-
+//   One, in the line 10
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
-
+//   Obtain as a result the reminder of the division of movieLength between 60
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-
+//   The expression 
+//   find difference of the movieLength minus the remainingSeconds to obtain the amount of seconds exactly divisible by 60
+//   do the division to obtain the total in minutes, which is the value assigned to totalMinutes
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-
+//    The variable result represents the duration of the film, expressed with the format: HH:MM:SS
+//    A better name could be movieDuration.
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+//    It works for all values of movieLength. With long names, the number of hours can be hundreds.
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -24,4 +24,22 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 1. Const penceString = "399p": initializes a string variable with the value "399p"
+// 2. Empty
+// 3. Const penceStringWithoutTrailingP initializes and assigned the value of function penceString.substring()
+// 4. First position of string
+// 5. End position of string taken with the length of the string minus the last character p using -1
+// 6. Close function penceString.substring
+// 7. Empty
+// 8. Const paddedPenceNumberString initializes and assigned the value of function penceStringWithoutTrailingP.padStart(
+// padding the string with "0" until it reaches the length 3
+// 9. Const pounds initializes and assigned the value of function paddedPenceNumberString.substring(
+//10. the number is extracted from the first character 
+//11. to the length of the string except the last two characters.
+//12. End of function
+//13. Empty
+//14. Const pence initializes and assigned the value of function paddedPenceNumberString.substring()
+//15. using the length of the paddedPenceNumberString minus 2 for decimal (pens) values.
+//16. Padding with 00 until two characters
+//17. Empty
+//18. Print on console the value using the characters £ representing pounds and dot separating pens.
