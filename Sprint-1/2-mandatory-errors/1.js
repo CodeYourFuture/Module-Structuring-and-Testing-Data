7e5508b4e7bb5ae37b4fedac31ccaabc128e04db
Emilianouz <emiliano.uru.uk@gmail.com>
2025-06-06 09:36:07 +0100

    Part2-mandatory-errors. Error checked, interpreted, explained and some solved.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+// The age variable was declared as const; as a result, it can't be reassigned. 
+// AS solution the variable was declared using 'let'.
--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// When the message was tried to be printed the system couldn't access to 'cityOfBirth'
+// The declaration of the const cityOfBirth must be done before.
