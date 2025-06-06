330e7767a9ce2352a4797d215654ae5c6e29d597
Alexandru Pocovnicu <alexandru.pocovnicu27@gmail.com>
2025-06-06 18:02:37 +0100

    added comment about what line 3 is doing

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+// the "=" assigns value to count which is the sum of count + 1 and which now is "1" 
+
+
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
