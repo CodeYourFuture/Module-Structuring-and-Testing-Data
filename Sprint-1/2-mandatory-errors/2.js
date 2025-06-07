0def05a9a3be087dd15084794069957e5639bca0
Reza Jahanimir <jahanimir.r@gmail.com>
2025-06-07 13:35:47 +0100

    understating how does system reads the coder

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,15 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// the system reads the code top to bottom and because the variable has been create/define before 
+// the consol.log it can not recognize the variable
+
+//Wrong
+//console.log(`I was born in ${cityOfBirth}`);
+//const cityOfBirth = "Bolton";
+
+//correct
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+
