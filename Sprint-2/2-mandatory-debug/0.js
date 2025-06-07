b3d05fa13ad5be91148ef63f81e43d686efeec97
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-06-07 20:26:20 +0300

    sprint_1 folder 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,26 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// error will be in result of code execution,
+// result of multiplication will be displayed in wrong place(in separate line, not dedicated string)
 
+/*
 function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
+*/
 // =============> write your explanation here
+
+// error will appear because function multiply does not return string, 
+// and in last console.log part '${multiply(10, 32)}' will be undefined
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// result of multiplication will be undefined
+/*
 function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
+*/
 // =============> write your explanation here
+
+// function sum() has return statement without variable ( it should return result of addition )
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
