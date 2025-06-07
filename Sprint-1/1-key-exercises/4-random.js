19ff93af017ea903ded91fb693e079c76ec66f94
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-06-07 11:46:15 +0100

    Explanation of random

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,7 +3,10 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+console.log(num);
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+// There are two variable which are min and max in this file. in line 4 there is an expression
+// to create a number randomly between minimum and maximum.
