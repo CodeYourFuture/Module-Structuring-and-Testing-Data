259306e998718302ef0abdb2cd5abef8bf17c396
Reza Jahanimir <jahanimir.r@gmail.com>
2025-06-07 15:11:03 +0100

    learn about Variable Naming Rules

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,15 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const HourClockTime24 = "20:53";
+const hourClockTime12 = "08:53";
+
+// Go Variable Naming Rules
+// A variable can have a short name (like x and y) or a more descriptive name (age, price, carname, etc.).
+
+// Go variable naming rules:
+
+// A variable name must start with a letter or an underscore character (_)
+// A variable name cannot start with a digit
+// A variable name can only contain alpha-numeric characters and underscores (a-z, A-Z, 0-9, and _ )
+// Variable names are case-sensitive (age, Age and AGE are three different variables)
+// There is no limit on the length of the variable name
+// A variable name cannot contain spaces
+// The variable name cannot be any Go keywords
