4d7b48b7ffc55cf1c5c8be479bf50bf88e39f11d
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-08 16:58:26 +0100

    - 1.js   I have fixed the error in the code and used let command instead of const.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,12 @@
 
 const age = 33;
 age = age + 1;
+
+/*This code is not going to run as age is defined by const and the value of age
+ would have to stay as 33. To add 1 to the age we can use let instead of const. 
+ let allowes changes to variables , but const only keeps values constant.*/
+
+ let age = 33;
+ age = age + 1;
+
+ console.log(age);
