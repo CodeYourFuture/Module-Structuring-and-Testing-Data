b0c4234568dfb72d3b0f4171496dcb02cb6cc830
aida eslami <ayda.eslami@gmail.com>
2025-06-09 00:50:39 +0100

    Mandatory-errors

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+// const cardNumber = 4533787178994213;
+// const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,14 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+
+
+// ANSWER : Code is not working because Slice() function is for STRING and we should convert number to string and then ask for last 4 digits.
+
+
+const cardNumber = 4533787178994213;
+let last4Digits = cardNumber.toString()
+last4Digits=last4Digits.slice(-4)
+
+console.log(last4Digits)
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+
+//ANSWER: variable should not start with number 
+
+const HourClock24 = "20:53";
+const HourClock12= "08:53";
+
+console.log("The time is: "+HourClock12 + " or",HourClock24)
