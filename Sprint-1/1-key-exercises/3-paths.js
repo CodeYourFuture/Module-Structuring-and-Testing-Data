75a6613738afd67dde30f87b87382e6e07345bce
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-08 16:11:17 +0100

    - 3-paths.js created a variable to store dir part of the filepath variable. created a variable to store ext part of the variable.

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -12,12 +12,19 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+// Get base (e.g., "file.txt")
+const lastSlashIndex = filePath.lastIndexOf("/");
+
+// Get dir (everything before "file.txt")
+const dir = filePath.slice(0, lastSlashIndex);
+const ext = base.slice(lastDotIndex);
+
+console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
