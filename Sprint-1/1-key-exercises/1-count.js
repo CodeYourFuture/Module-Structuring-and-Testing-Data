145c8fde37bdd61a73004f396a225c9561077dbc
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-08 10:18:45 +0100

    added a comment

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -8,3 +8,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// '=' assigns a new value to the variable 'count'
