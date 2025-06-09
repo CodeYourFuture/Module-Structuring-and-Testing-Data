8363de8ff01b80e08b5f543993ac118e779014ad
Sisu860 <meharisisay6@gmail.com>
2025-06-09 15:45:04 +0100

    Implement password validator function with comprehensive Jest tests

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -1,18 +1,3 @@
-function find(str, char) {
-  let index = 0;
-
-  while (index < str.length) {
-    if (str[index] === char) {
-      return index;
-    }
-    index++;
-  }
-  return -1;
-}
-
-console.log(find("code your future", "u"));
-console.log(find("code your future", "z"));
-
 // The while loop statement allows us to do iteration - the repetition of a certain number of tasks according to some condition
 // See the docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
 
@@ -23,3 +8,19 @@ console.log(find("code your future", "z"));
 // b) What is the if statement used to check
 // c) Why is index++ being used?
 // d) What is the condition index < str.length used for?
+
+// Function to find the index of the first occurrence of a character in a string
+function find(str, char) {
+  let index = 0;
+
+  while (index < str.length) { // d) Loop continues while index is less than the string length
+    if (str[index] === char) { // b) Checks if the current character matches the target character
+      return index;            // If match found, return the index
+    }
+    index++;                   // c) Move to the next character
+  }
+  return -1;                   // If not found, return -1
+}
+
+console.log(find("code your future", "u")); // Output: 7
+console.log(find("code your future", "z")); // Output: -1
--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -1,26 +1,43 @@
-/* 
-Password Validation
+/*
+Password Validator Test Cases
 
-Write a program that should check if a password is valid
-and returns a boolean
-
-To be valid, a password must:
-- Have at least 5 characters.
-- Have at least one English uppercase letter (A-Z)
-- Have at least one English lowercase letter (a-z)
-- Have at least one number (0-9)
-- Have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
-- Must not be any previous password in the passwords array. 
-
-You must breakdown this problem in order to solve it. Find one test case first and get that working
+To verify the passwordValidator function, the following cases are tested:
 */
-const isValidPassword = require("./password-validator");
-test("password has at least 5 characters", () => {
-    // Arrange
-    const password = "12345";
-    // Act
-    const result = isValidPassword(password);
-    // Assert
-    expect(result).toEqual(true);
-}
-);
+
+// Importing the function to test
+const passwordValidator = require("./password-validator");
+
+// Case 1: Password with at least 5 characters
+test("should return true for password with at least 5 characters and all conditions met", () => {
+    expect(passwordValidator("Abc1!")).toEqual(true);
+});
+
+// Case 2: Password too short
+test("should return false if password has less than 5 characters", () => {
+    expect(passwordValidator("A1!")).toEqual(false);
+});
+
+// Case 3: Missing uppercase letter
+test("should return false if password lacks uppercase letter", () => {
+    expect(passwordValidator("abc1!")).toEqual(false);
+});
+
+// Case 4: Missing lowercase letter
+test("should return false if password lacks lowercase letter", () => {
+    expect(passwordValidator("ABC1!")).toEqual(false);
+});
+
+// Case 5: Missing number
+test("should return false if password lacks a number", () => {
+    expect(passwordValidator("Abcde!")).toEqual(false);
+});
+
+// Case 6: Missing symbol
+test("should return false if password lacks required symbol", () => {
+    expect(passwordValidator("Abc12")).toEqual(false);
+});
+
+// Case 7: Password is in previousPasswords array
+test("should return false if password is a previous password", () => {
+    expect(passwordValidator("Pass123!")).toEqual(false);
+});
