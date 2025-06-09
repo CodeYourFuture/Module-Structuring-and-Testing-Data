4ffc52878e664da90eb31edc4bee08ee6b6f5a80
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-06-09 16:53:40 +0100

    Done : Add explanation comments for find function

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,19 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+/*let index = 0; The variable index starts at 0.
+On each loop iteration, it increases by 1 using index++ until the string's length*/
 // b) What is the if statement used to check
+/*
+if (str[index] === char)
+checks: Is the current character equal to the one we are looking for that is passed as a parameter?
+If yes, it return its position using return index.
+*/
 // c) Why is index++ being used?
+// Because we need to  It increases index by 1 on each loop and update its value in each iteration to prevent from infinite loop
+
 // d) What is the condition index < str.length used for?
+/*
+It is the stopping condition for the while loop.
+It ensures that the loop only runs as long as there are characters left in the string.
+*/
