21a6fe1e30004c0f9924ad5951c728ee80e14f99
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-06-09 10:49:54 +0100

    pound format questions are answered

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,20 +1,23 @@
-const penceString = "399p";
+const penceString = "99p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 );
+console.log(penceStringWithoutTrailingP);
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+console.log(paddedPenceNumberString);
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
-
+console.log(pounds);
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
 
+console.log(pence);
 console.log(`£${pounds}.${pence}`);
 
 // This program takes a string representing a price in pence
@@ -25,3 +28,10 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+/* 2. in penceStringWithoutTrailingP assignment, last character of penceString which is 'p' is removed
+   3. in paddedPenceNumberString assignment, the value completed to 3 digit if the length is shorter than three digit
+   4. in pounds assignment, pennies part of penceString is removed.
+   5. in pence assignment, pounds part of penceString is removed and it is completed one digit if the length is shorter than three digit
+   6. in console.log function, pounds and pence are concatenated and converted price format
+
+*/
