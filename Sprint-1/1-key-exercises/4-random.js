6cc735fc4a57599c680c130a0e41187f9c67a3c0
Anuar Duisenbek <coolboy949494@gmail.com>
2025-06-09 20:32:05 +0100

    fixed: 1-key-exers and 2-mandatory-errors

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,9 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// This code generates a random whole number between 1 and 100.
+// Math.random() returns a decimal between 0 and 1 (not including 1).
+// We multiply it by 100 to get a number between 0 and 99.999...
+// Math.floor() removes the decimal part, so we get an integer.
+// We add 1 to shift the range to 1–100 instead of 0–99.
