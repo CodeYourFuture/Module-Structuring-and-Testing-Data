85c6adf481b074c9ec3e9fa310466a7f1ead2854
AdnaanA <thetriplex247@gmail.com>
2025-06-09 10:54:20 +0100

    modified:   Sprint-2/1-key-errors/0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,27 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The code will throw an error because the variable 'str' is being redeclared with 'let' inside the function,
+// which is not allowed since 'str' is already defined as a parameter of the function.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}*/
+
+// =============> write your interpretation here
+// The error message indicates that 'str' has already been declared, which is true because 'str' is 
+// both a parameter of the function and being redeclared with 'let' inside the function. As predicted, above.
+// To fix this, we can simply remove the 'let' keyword inside the function since 'str' is already defined as a parameter.
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
+// call the function capitalise with a string input
+console.log(capitalise("hello world"));
 // =============> write your explanation here
 // =============> write your new code here
