d8e529f5abf39679a732ee3fcb5b7e23fa7b7629
Sara Tahir <saratahir34@gmail.com>
2025-06-09 10:59:20 +0100

    Interpreted the error- Print statement comes after declaration

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,11 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+//The error is that the variable is declared after the print statement. 
+//In javascript the variables declared with const or let should be at the top of code
+//block. Here they are trying to access a variable before its declaration line is
+//executed.
+//To fix it we do this
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
