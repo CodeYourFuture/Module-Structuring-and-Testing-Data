5e3979f3d5c5b63e2845a94861b66ece6c5304e3
Sara Tahir <saratahir34@gmail.com>
2025-06-09 11:18:25 +0100

    Variables in Js cannot start with a number

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+//Variable names cannot start with a number in javascript. Also 20:53 is a 24 hour format and 08:53 is a 
+//12 hour format.
