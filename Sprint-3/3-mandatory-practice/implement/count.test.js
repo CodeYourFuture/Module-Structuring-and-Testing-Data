033c42c00f2ee3026419cd1e7a0e373fe645c5f2
Sisu860 <meharisisay6@gmail.com>
2025-06-09 14:07:15 +0100

    Implement countChar function and add tests

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -1,5 +1,6 @@
 // implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
+
 // Given a string str and a single character char to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
@@ -22,3 +23,34 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("should return 0 if character is not found", () => {
+  const str = "Hello World";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// Scenario: Case Sensitive Check
+test("should be case sensitive when counting characters", () => {
+  const str = "aAaAaA";
+  const char = "A";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+// Scenario: Single Occurrence
+test("should count a single occurrence correctly", () => {
+  const str = "abcdef";
+  const char = "d";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
+
+// Scenario: Empty String
+test("should return 0 for empty string input", () => {
+  const str = "";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
