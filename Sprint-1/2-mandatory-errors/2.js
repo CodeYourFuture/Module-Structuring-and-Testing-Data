696f155f6627d1dc03989cf69577a0413fcaaf83
Shannon <shannond050@outlook.com>
2025-06-10 23:01:18 +0200

    Fix for the Error var reassignment and Calling var before being defined

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,8 +1,11 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
+/*
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
-
+*/
 //Error: Cannot access 'cityOfBirth' before initialization
 // its trying to call the variable before its even been defined
+// if you want to fix change the order to start with defining the variable
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
