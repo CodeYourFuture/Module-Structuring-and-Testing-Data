6f97d3e2c3cfd2dd4edf75e92f292d7e85a2dedc
mo-muchunu <mo.muchunu@gmail.com>
2025-06-10 15:19:26 +0100

    Comments explanation

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+
+console.log("We can solve this problem by 'commenting' out the first two lines of code using '//' so that the computer ignores them when running the program.");
