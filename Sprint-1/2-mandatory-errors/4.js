61c564746fd432465ad1a080922695182e62a69b
Seddiq Azam <seddiq.azam@students.codeyourfuture.io>
2025-06-10 20:45:59 +0100

    Update 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const hour24ClockTime = "20:53";
+console.log(hour24ClockTime);
+
+const hour12ClockTime = "08:53";
+console.log(hour12ClockTime);
+
+// The variables were renamed because JavaScript does not allow variables to start with numbers
+// camelCase format used for variable names
