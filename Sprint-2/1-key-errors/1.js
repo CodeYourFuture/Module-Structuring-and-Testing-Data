5b4a50633f0ca4234bd3cafef6dc13917db468f8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-10 14:52:07 +0100

    made a prediction(wrong) and corrected the code

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,28 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+/* =============> write your prediction here: because on line 11 decimalNumber isn't between 2 curly brackets,also
+decimalNumber already used as parameter so it can't be declared again inside the function*/
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+/*function convertToPercentage(decimalNumber) {
+  //decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
+console.log(convertToPercentage(7));//ReferenceError: decimalNumber is not defined
+console.log(convertToPercentage(0.5));
 // =============> write your explanation here
 
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+ =============> write your new code here:*/
+function convertToPercentage(decimalNumber) {
+decimalNumber = 0.5;
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+console.log(convertToPercentage());
