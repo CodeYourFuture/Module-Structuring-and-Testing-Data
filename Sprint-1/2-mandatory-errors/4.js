c48484d320145db9a42c105f727aca3af59cc73e
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-10 19:26:35 +0100

    "modified:   ../1-key-exercises/4-random.js, modified:   ../2-mandatory-errors/4.js"

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -3,6 +3,6 @@ const hourClockTime = "08:53";
 
 // Mistake is that the variables' name start with number.
 // There are some rules how to name variables:
-// 1. Name shouldn't start with number.
-// 2. Variable name shouldn't contain any spacial characters or spaces.
-// 3.  Variable name shouldn't be a specific key word.
+// 1.Name shouldn't start with number.
+// 2.Variable name shouldn't contain any spacial characters or spaces.
+// 3.Variable name shouldn't be a specific key word.
