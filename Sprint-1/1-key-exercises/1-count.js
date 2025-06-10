c77a685a63dfe26a1ba714f91e278ce761735582
Seddiq Azam <seddiq.azam@students.codeyourfuture.io>
2025-06-10 17:22:03 +0100

    Update 1-count.js
    Detailed answer to questions

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -3,4 +3,10 @@ let count = 0;
 count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// Describe what line 3 is doing, in particular, focus on what = is doing
+
+
+//The answers:
+//In line 1, the declared value is set to 0. In line 3, the value is redefined and set to count + 1. So the initial count value was 0, and the new value is 0 + 1. 
+//In programming terms, the value of count increments by 1. 
+//The equal sign is an assignment operation. It assigns the value on the right side of the = to the variable on the left side. 
