6e043abae86db693a9f021ff8d58bbb46f3558c8
Dawūd Vermeulen <127528069+Dave-Vermeulen@users.noreply.github.com>
2025-06-10 08:00:43 +0000

    count.js in Sprint 1 done. Peer programming.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,11 @@
-let count = 0;
+let count = 0; 
 
 count = count + 1;
+// count is being reassigned a new value of count + 1
+count++;
+count += 1;  
+count +1;
+// all these increment 1
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
