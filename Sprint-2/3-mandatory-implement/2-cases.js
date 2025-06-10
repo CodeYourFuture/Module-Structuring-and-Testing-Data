2330e68077384be29072bc8b8e1641ffa95ab519
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-06-10 21:24:19 +0100

    Exercise 3-mandatory-implement done

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,12 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function convertUpper_snake_case(str) {
+  const caseConvert = str.toUpperCase();
+  const convertToSnakeCase = caseConvert.replaceAll(" ", "_");
+  return convertToSnakeCase;
+}
+
+const caseConverter = convertUpper_snake_case("lord of the rings");
+console.log(caseConverter);
