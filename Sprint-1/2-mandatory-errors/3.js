aa257246de91170bc65e289a682115ece887a616
Seddiq Azam <seddiq.azam@students.codeyourfuture.io>
2025-06-10 20:34:03 +0100

    Update 3.js

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,16 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+
+
+// Answers:
+
+/// The code isn't working because the variable `cardNumber` is just a number, not a string or an array.
+// The `slice` method only works on strings or arrays.
+// So if we call `const last4Digits = cardNumber.slice(-4);`, it gives an error because `.slice` is not a function for numbers.
+// Yes, I predicted the error because the variable `cardNumber` is not assigned a string.
+
+const last4Digits = cardNumber.toString().slice(-4);
+
+console.log(last4Digits);
