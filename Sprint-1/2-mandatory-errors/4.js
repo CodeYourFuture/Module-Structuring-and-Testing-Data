cf6097d7593619029dfe8bdb2b5132e7ab56af42
plaetor <asanij@gmail.com>
2025-06-10 16:30:27 +0100

    done task 2 sprint 1 exercises

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const 12HourClockTime = "20:53"; //js does nott allow variable names to start with a number
+const 24hourClockTime = "08:53"; //it is better to use the number in the middle or at the end of the variable name.
+//example:
+const clockTime12Hour = "20:53"; //this is a valid variable name
+const clockTime24Hour = "08:53"; //this is also a valid variable name
