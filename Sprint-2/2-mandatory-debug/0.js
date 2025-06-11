c7bbe5da3641835d3d50e13c1487b945d7d828fb
aida eslami <ayda.eslami@gmail.com>
2025-06-11 15:30:26 +0100

    Mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,23 @@
 // Predict and explain first...
+// function should multiply a by b and print the answer. and it DOESN'T return .
+//  So in the Console.log we don't have the result.
 
 // =============> write your prediction here
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// we don't have result for function because there is no return in function . "The result of multiplying 10 and 32 is undefined"
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
