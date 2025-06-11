ebbab63ec4e999e06b51f9fb1f05a2bb31ee28fd
nurzat <nurzatcode@gmail.com>
2025-06-11 18:05:55 +0100

    update 3js

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,13 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+
+//It will throw an error because cardNumber is a number, not a string — and .slice() is a method for strings and arrays, not numbers.
+
+const cardNumber = 4533787178994213;
+const last4Digits = cardNumber.toString().slice(-4);
+
+console.log(last4Digits);
+
+//outcome 4213
