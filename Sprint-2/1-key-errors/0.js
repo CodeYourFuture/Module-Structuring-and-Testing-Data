cedb7b2c63a78b4bef0f6485edcf2d89c35480d5
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-06-11 10:07:30 +0100

    Block errors MSTD_Sprint2 predictions and interpretations.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+/*
+The variable 'str' must be defined before it be used in a function. As a result, an error of Identifier will occur.
+*/
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +12,13 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+// The error is occurring because the parameter 'str' is being redeclared with 'let' inside the function.
+// This causes a conflict and results in an error. To fix this, we can simply remove the 'let' keyword
+// when reassigning the value to 'str'.
+
 // =============> write your new code here
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+//console.log(capitalise("Texto"))
