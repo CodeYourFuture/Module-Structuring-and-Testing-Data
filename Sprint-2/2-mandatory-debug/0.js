83b460019c6541a1849b0bd77ca313c107c9e6ca
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-11 16:28:16 +0100

    Sprint-2: 2-mandatory-debug: 0.js, 1.js, 2.js tasks

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// My prediction: the result will be undefined.
+
+// function multiply(a, b) {
+//  console.log(a * b);
+//}
+
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// Because we use console.log to store the multiplication
+// of a and b. To store result we should use return and record, print console.log.
+
+// Finally, correct the code to fix the problem
 
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
