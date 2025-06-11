d0eef51abf15933316475ac45c77131bdca9a938
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-11 15:27:38 +0100

    Sprint-2: 1-key-errors: 0.js, 1.js, 2.js tasks

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,20 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// Possible error: There is strait away defined number 3 as function parameter, but should be num.
+// I think this type error we called SyntaxError.
 
-function square(3) {
-    return num * num;
+// function square(3) {
+//    return num * num;
+//}
+
+// SyntaxError: Unexpected number
+
+// My prediction was valid.
+
+function square(num) {
+  return num * num;
 }
 
-// =============> write the error message here
-
-// =============> explain this error message here
-
-// Finally, correct the code to fix the problem
-
-// =============> write your new code here
-
-
+console.log(square(5));
