57c9692d1c05c754f34ff5d757702739fab8964d
nurzat <nurzatcode@gmail.com>
2025-06-11 18:13:00 +0100

    update 1-percentage

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -20,3 +20,37 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+a) 5 total function calls
+carPrice.replaceAll(",", "")
+
+Number(...) on the result of #1
+
+priceAfterOneYear.replaceAll("," "") ❌ (this is broken — see part b)
+
+Number(...) on the result of #3 (also broken)
+
+console.log(...)
+
+b)This line has a syntax error: there's an extra quote in replaceAll("," "")
+It should be: replaceAll(",", "")
+
+c) carPrice = Number(carPrice.replaceAll(",", ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+ These 2 lines are reassignments — the values are being updated after declaration.
+
+ d)This is a nested expression that does two things:
+
+ carPrice.replaceAll(",", ""):
+ 
+ Replaces all commas with empty strings.
+ 
+ Example: "10,000" → "10000" (makes it usable as a number)
+ 
+ Number(...):
+ 
+ Converts the resulting string "10000" into the actual number 10000
+ 
+ 
+
