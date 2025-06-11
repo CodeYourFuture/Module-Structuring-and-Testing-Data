725e52ed34cc5745f7c2527f388f49e9d6c7b588
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-12 00:49:31 +0100

    Fix the identifier typo.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const HourClockTime = "20:53";
+const hourClockTime = "08:53";
+
+// A JavaScript identifier must start with a letter, underscore (_), or dollar sign ($). 
+// They can't start with a digit! Only subsequent characters can be digits (0-9).
