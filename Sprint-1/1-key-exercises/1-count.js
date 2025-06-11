5b5cc4fccd1230c4bb84291e812ab7a50f8eac58
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-11 11:59:53 +0100

    Update comment for count assignment

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,9 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/*
+line 3 adds 1 to the current value of 'count', then 
+assigns the new value back to the same 'count' variable.
+*/
+
