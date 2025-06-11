efb209fb2c4a423a18f0c7259c5c234e42731dad
roja alagurajan <alagurajan.roja@gmail.com>
2025-06-11 18:41:58 +0100

    mandatory error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,13 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
+/*old code*/
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+//The error is that the variable is declared after the print statement. 
+//In javascript the variables declared with const or let should be at the top of code
+
+
+//corrected code
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
+
+/*Previous code
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.slice(-4); */
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,15 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+
+//TypeError: cardNumber.slice is not a function
+//the prediction that .slice() wouldn't work on a number.The code fails because of the data type mismatch (number vs. string).
+
+//The error occurs because slice() is a string method and cardNumber is a number, so calling cardNumber.slice(-4) causes a TypeError since numbers don’t have .slice().
+
+//Corrected Code
+const cardNumber = 4533787178994213;
+const last4Digits = cardNumber.toString().slice(-4);
+
+console.log(last4Digits);
