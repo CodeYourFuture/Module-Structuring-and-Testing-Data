bde701ad2993ede3b515470b8919eb7d53790711
Praise88 <praisemagidi7@gmail.com>
2025-06-11 21:50:22 +0200

    completed Sprint-1, 1-key-exercises

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+line 3 is assigning another different value to count by adding 1 to its current value. The = is not working like an equalizer it is
+the count value. 
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,9 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(num)
+
+When breaking down the expression it starts by the third brackets where maximum - minimum + 1, maximum represents a 
+100 and minimum represents a 1. From there you multiply the answer by Math.random, where a random number between 0 
+and 1 is selected randomly. Another minimum is added to that answer. Now Math.floor rounds the answer to the nearest 
+whole number, and that value is what you get when you run the code.
