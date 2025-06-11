56c18f3c72631509d3b2ffb4d60c676b6de9ce4b
roja alagurajan <alagurajan.roja@gmail.com>
2025-06-11 18:45:19 +0100

    invalid variabes

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+
+//12HourClockTime is an invalid variable name in JavaScript — variable names cannot start with a digit.
+
+//"20:53" is actually a 24-hour format, not 12-hour
