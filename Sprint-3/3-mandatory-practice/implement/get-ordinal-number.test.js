5167a24c489cf14759192af173971300a8d160d0
gerbil1511 <gerbil1511@gmail.com>
2025-06-12 18:04:11 +0100

    update functions in files and add tests for certain conditions.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -9,5 +9,44 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Then the function should return "1st"
 
 test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+  expect(getOrdinalNumber(1)).toEqual("1st");
+});
+
+// additional cases for testing:
+
+// normal tests
+test("should return 'st', 'nd', 'rd' or 'th'", () => {
+  const normalInput = [
+    [1, "1st"],
+    [2, "2nd"],
+    [3, "3rd"],
+    [4, "4th"],
+    [11, "11th"],
+    [12, "12th"],
+    [13, "13th"],
+    [21, "21st"],
+    [22, "22nd"],
+    [23, "23rd"],
+    [100, "100th"],
+    [101, "101st"],
+    [102, "102nd"],
+    [103, "103rd"],
+    [111, "111th"],
+    [112, "112th"],
+    [113, "113th"],
+    [1000, "1000th"],
+  ];
+
+  for (const [input, expected] of normalInput) {
+    expect(getOrdinalNumber(input)).toEqual(expected);
+  }
+});
+
+// invalid tests
+test("should throw an error for invalid ordinal numbers", () => {
+  const invalidInputs = ["hello", "123abc", "", null, undefined, [], {}];
+
+  for (const input of invalidInputs) {
+    expect(() => getOrdinalNumber(input)).toThrow("Invalid ordinal number");
+  }
+});
--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -10,23 +10,54 @@ const repeat = require("./repeat");
 // Then it should repeat the str count times and return a new string containing the repeated str values.
 
 test("should repeat the string count times", () => {
-    const str = "hello";
-    const count = 3;
-    const repeatedStr = repeat(str, count);
-    expect(repeatedStr).toEqual("hellohellohello");
-    });
+  const str = "hello";
+  const count = 3;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("hellohellohello");
+});
 
 // case: handle Count of 1:
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
 
+test("should return the string without repetition", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("hello");
+});
+
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
+test("should return an empty string ''", () => {
+  const str = "hello";
+  const count = 0;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("");
+});
 
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+
+test("should throw an error for negative count", () => {
+  const str = "hello";
+  const count = -1;
+  expect(() => repeat(str, count)).toThrow(
+    "Count must be a non-negative integer"
+  );
+});
+
+// case: Invalid Count Type:
+// Given a target string str and an invalid count type (e.g., a string or an object),
+// When the repeat function is called with these inputs,
+// Then it should throw an error or return an appropriate error message, as the count must be a non-negative integer.
+test("should return the message 'Count is an invalid input'", () => {
+  const str = "hello";
+  const count = "three";
+  expect(() => repeat(str, count)).toThrow("Count is an invalid input");
+});
