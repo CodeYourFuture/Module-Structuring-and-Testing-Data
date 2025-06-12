3d2dea4297672255608adccb8473d0ee3d2fa091
Reza Jahanimir <jahanimir.r@gmail.com>
2025-06-12 08:58:55 +0100

    local and global scope

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// the console.log inside the function will run but the one in global will raise the undefine error
+// we will get an undefine error because we are not retuning the value
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +11,14 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// To use the final result of a function, we must return the value. Otherwise, it will raise an undefined error.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  const result = a * b;
+  console.log(result); 
+  return result; 
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
