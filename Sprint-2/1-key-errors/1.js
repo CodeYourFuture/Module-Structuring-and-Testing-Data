34f56c1d9c5bc25bd7e67057b073c29a6382e384
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-12 15:29:23 +0100

    The correct way to cll a function.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,30 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// It will throw syntaxError, the code will not work.> write your prediction here
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+/*function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(decimalNumber); */
 
-// =============> write your explanation here
+// The first error: the code will not work because as the previous exercise we cannot redeclare a variable that already assigned as function parameter.
+// The second error: to use the function correctly to display in console we need to call the function name not the variable name.> write your explanation here
 
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+//   > write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const decimalNumber2 = 0.5;
+  const percentage = `${decimalNumber2 * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
