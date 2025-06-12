9d438511fd7ebe8a6d17d30d90c28e9b7abbfec1
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-12 23:09:24 +0100

    -1.js Debug I have corrected the error in the code.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
+/* I predict that the result will be undefined. There is a line breaker 
+after the return function it will not return the sum of (a, b). */
 
 function sum(a, b) {
   return;
@@ -9,5 +11,13 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+/* When I run the code in the terminal the output I get says (The sum of 10 and 32 is undefined)
+this means that the line breaker ; after the return function has stopped the sum operation from functioning*/
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
