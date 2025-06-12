12ef9ffbfa2ccab421268c13a25b8a9963bb821d
Reza Jahanimir <jahanimir.r@gmail.com>
2025-06-12 08:56:53 +0100

    can't redeclare a variable

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> decimalNumber is already the name of the function parameter. 
+// You can't redeclare a variable with the same name in the same scope. and we do not even need to 
+// define a new variable 
 
 // Try playing computer with the example to work out what is going on
 
@@ -12,9 +14,19 @@ function convertToPercentage(decimalNumber) {
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(50));
 
-// =============> write your explanation here
+// =============> i removed the variable 
+
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+
+console.log(convertToPercentage(50));
