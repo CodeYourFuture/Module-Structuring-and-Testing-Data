55ef4a14365ceb7eb6caf2901d9606974b6d03da
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-12 10:47:19 +0100

    Handling Redeclared variables in JS.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// The code will throw a Syntax error: Identifier > write your prediction here
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -7,7 +7,15 @@
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
+} 
+
+// The error occurs because the variable str is declared twice within the same scope, in this function we used the variable as a function parameter,
+//  then trying to redeclare it again as a variable, JavaScript does not allow it, and will throw error.> write your explanation here
+// =============> write your new code here
+
+function capitalise(str) {
+  let correctStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return correctStr;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise("jamal")); // Output: Hell
