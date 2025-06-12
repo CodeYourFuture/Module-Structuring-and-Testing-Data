3ba47cb6a3f4a2711338b2eaaf2910ff2cb9c007
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-12 12:20:38 +0100

    Going through the code step-by-step then added the explanation as comment

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -3,17 +3,27 @@ const penceString = "399p";
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
-);
+); // Expected output: "399"
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); // Expected output: "399"
+
+console.log(penceStringWithoutTrailingP)
+console.log(paddedPenceNumberString)
+
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
-);
+); // Expected output: "3"
+
+console.log(pounds)
+
 
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
+
+console.log(pence);
+
 
 console.log(`£${pounds}.${pence}`);
 
@@ -24,4 +34,29 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+/*  1. const penceString = "399p";: initialises a string variable with the value "399p"
+    2. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);: 
+    I will go step by step through this line
+        a.  '.length' return the length of the sting, in our case the length of the string is 3 (starting the string from 0)
+            then we subtract the value by 1. Why is that?
+            Because the '.substring()' is excluding the end index and we do not 'p' to show
+        b.  '.substring()' is returning value from the start index up to and excluding the end index,
+            in our case we start the index from 0 until 2
+        c.  We assign the value to 'penceStringWithoutTrailingP' variable
+      
+    3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");:
+        a.  '.padStart' this will add 0 if the string is less than 3 characters, otherwise keep the same string
+            So in our case the sting is '339' which is already 3 characters so it will return same and assign 
+            the value to 'paddedPenceNumberString'.
+    4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);:
+        a. is returning value from the start index up to and excluding the end index,
+            in our case we start the index from 0 until 'paddedPenceNumberString.length - 2' 
+            which start from zero and do not include the last 2 one. 
+
+    5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+        a.  Here pence equal to the last two digit of paddedPenceNumberString and if the value is already 
+            equal to or longer than targetLength, it is returned unchanged so in our example our value is 99 so 
+            it return unchanged.
+
+*/
+
