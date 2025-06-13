1023191fef25f730e7bfbab9dbed0fe63a1565c8
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-13 21:43:14 +0100

    Key Errors | Fixed capitalise function and added explanation

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
-//  =============> write your prediction here
-
+//  =============> The following code will result in an error message:
+SyntaxError: It has been established that the identifier "str" has already been declared.
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -9,5 +9,9 @@ function capitalise(str) {
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> The function capitalize is intended to return the input string with the first character capitalized.
+However, the original version results in a syntax error due to the declaration of str as a function parameter, which precludes its redeclaration using let within the same scope.
+// =============> function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
