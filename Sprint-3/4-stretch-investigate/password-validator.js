fb1f76e93f931ddeae7107b13d3515e6b3e672c3
galyna-k <hkzlvs@gmail.com>
2025-06-13 13:07:14 +0100

    update in 4-stretch-investigate password-validator.js
    - implement full password validation rules
    - add Jest tests

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,25 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
+function passwordValidator(password, previousPasswords = []) {
+  // Has at least 5 characters.
+  const hasMinLength = password.length >= 5;
+  // Has at least one English uppercase letter (A-Z)
+  const hasUppercase = /[A-Z]/.test(password);
+  // Has at least one English lowercase letter (a-z)
+  const hasLowercase = /[a-z]/.test(password);
+  // Has at least one number (0-9)
+  const hasNumber = /[0-9]/.test(password);
+  // Has at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
+  const hasSymbol = /[!#$%.*&]/.test(password);
+  // Must not be any previous password in the passwords array
+  const isNotUsedBefore = !previousPasswords.includes(password);
+
+  return (
+    hasMinLength &&
+    hasUppercase &&
+    hasLowercase &&
+    hasNumber &&
+    hasSymbol &&
+    isNotUsedBefore
+  );
 }
 
-
-module.exports = passwordValidator;
+  module.exports = passwordValidator;
--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Password Validation
 
 Write a program that should check if a password is valid
@@ -10,17 +10,61 @@ To be valid, a password must:
 - Have at least one English lowercase letter (a-z)
 - Have at least one number (0-9)
 - Have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
-- Must not be any previous password in the passwords array. 
+- Must not be any previous password in the passwords array.
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
 const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
     // Arrange
-    const password = "12345";
+    const password = "1a2B3$";
     // Act
     const result = isValidPassword(password);
     // Assert
     expect(result).toEqual(true);
 }
 );
+
+test("password has at least 5 characters", () => {
+    const password = "Ab1!";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+test("password has at least one uppercase letter", () => {
+    const password = "ab1!x";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+test("password has at least one lowercase letter", () => {
+    const password = "AB1!X";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+test("password has at least one number", () => {
+    const password = "Abc!x";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+test("password has at least one symbol", () => {
+    const password = "Abc12";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+test("password must not be used before", () => {
+    const password = "Ab1!x";
+    const previousPasswords = ["Ab1!x", "Xyz123!"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(false);
+});
+
+test("password must not be used before", () => {
+    const password = "New1!";
+    const previousPasswords = ["Ab1!x", "Xyz123!"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(true);
+});
