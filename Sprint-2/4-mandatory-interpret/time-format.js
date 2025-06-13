aa1f8ee16b852d6209b685f0b778a756c2a0c710
Tewodros Bekere <tewodros.bekere16@gmail.com>
2025-06-13 18:44:49 +0100

    answered 4-mandatory-interpret/time-format.js

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,24 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// # 3 times, once for each of totalHours, remainingMinutes, and remainingSeconds
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// 1, which is the value of totalHours when seconds is 61
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// "01", because pad converts the number 1 to a string and pads it with a leading zero to make it two characters long
+// Call formatTimeDisplay with an input of 3661, now answer the following:
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// 1, which is the value of remainingSeconds when seconds is 3661. The totalHours is 1, remainingMinutes is 1, and remainingSeconds is 1.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// "01", because pad converts the number 1 to a string and pads it with a leading zero to make it two characters long
+console.log(formatTimeDisplay(61)); // "00:01:01"
