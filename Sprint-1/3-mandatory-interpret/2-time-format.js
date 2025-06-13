30061f1526dc1bc98896e272584665c286dd98af
c-mayamba <chrismayamba@gmail.com>
2025-06-13 17:31:23 +0200

    Interpret-2 solved

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,22 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// There are 6 variable declarations in this program.
 
 // b) How many function calls are there?
+// There is one function call in this program: `console.log()`.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+// The expression `movieLength % 60` calculates the remainder when `movieLength` is divided by 60.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// The expression calculates the total number of minutes in the movie by subtracting the remaining seconds from the total movie length 
+// and then dividing by 60 to convert seconds to minutes.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+// The variable `result` represents the formatted time of the movie in the format "hours:minutes:seconds".
+// A better name for this variable could be `movieDuration`. 
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// Yes, the code works for all values of `movieLength`, I tried 10000, 9300, and 8220.
