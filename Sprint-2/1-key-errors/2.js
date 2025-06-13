7620265b028ed368bf98a544d47d36a742c0de02
Declan Williams <declanwilliams793@gmail.com>
2025-06-13 14:11:11 +0100

    fix: correct parameter name in square function to prevent syntax error and update console log example

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,29 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// The function is defined incorrectly using a number as a parameter name so it will throw an error when called because looking for an identifier but finds a number instead.
 
-function square(3) {
+/* function square(3) {
     return num * num;
 }
+*/
 
 // =============> write the error message here
+/* function square(3) {
+                   ^
 
+SyntaxError: Unexpected number
+*/
 // =============> explain this error message here
+// SyntaxError: This means there is something wrong with the way the code is written, so JavaScript cannot understand it.
+// unexpected number: This means that the code is trying to use a number where it expects an identifier (like a variable name). In this case, the function parameter is incorrectly named as a number (3) instead of a valid identifier.
 
 // Finally, correct the code to fix the problem
+// to fix the problem, we need to change the parameter name to a valid identifier, like "num" or "number".
 
 // =============> write your new code here
 
-
+function square(num){
+    return num * num;
+}
+console.log(square(10));
