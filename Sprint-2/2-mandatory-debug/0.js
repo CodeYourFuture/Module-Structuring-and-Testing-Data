1640e2c106b3eb84bea04668c00de0e5709a8195
Tewodros Bekere <tewodros.bekere16@gmail.com>
2025-06-13 11:33:49 +0100

    update 0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-// The code will log the result of multiplying 10 and 32, but it will not return a value from the multiply function.
-// The function multiply will log the product of a and b, but it will not return any value.
+// # The code will log the result of multiplying 10 and 32, but it will not return a value from the multiply function.
+// # The function multiply will log the product of a and b, but it will not return any value.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -11,8 +11,8 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
-// The function `multiply` logs the product of `a` and `b`, but it does not return a value.
-// This means that when we try to log the result of `multiply(10, 32)`, it will log `undefined` because the function does not return anything.
+// # The function `multiply` logs the product of `a` and `b`, but it does not return a value.
+// # This means that when we try to log the result of `multiply(10, 32)`, it will log `undefined` because the function does not return anything.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
