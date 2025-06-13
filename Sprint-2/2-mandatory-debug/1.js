cc83e05cd25a3aa4780a9335296f4849b4b9b3a8
mo-muchunu <mo.muchunu@gmail.com>
2025-06-13 07:50:20 +0100

    Corrected sum function to properly return the sum instead of undefined

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// There'll be an undefined error in final output.
 
 function sum(a, b) {
   return;
@@ -9,5 +10,14 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The sum operation comes after the return statement, but because there's a semicolon after (return;)
+// the function stops before it gets to the 'a + b' sum. This means nothing is returned, so the result is undefined.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
