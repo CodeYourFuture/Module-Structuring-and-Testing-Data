c1c09a6c9b33da310c852bfb777030d53033ba44
Declan Williams <declanwilliams793@gmail.com>
2025-06-13 18:05:06 +0100

    update comments to clarify expected output and explanation in getLastDigit function

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,6 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// I expect each of the calls to return the last digit of "num" which is "3" its defined as a constant and we are using that const in the return statement.
 
 const num = 103;
 
@@ -15,8 +16,15 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+// Output:
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
 // Explain why the output is the way it is
 // =============> write your explanation here
+// The output is the same for all three calls because the function getLastDigit does not take any parameters. It always uses the constant "num" which is defined as 103, and will always returns the last digit of 103, which is "3".
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
