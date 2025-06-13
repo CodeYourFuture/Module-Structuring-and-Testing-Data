4d6d7d191cec33eb32f143da8169e31ceedc8b35
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-13 18:47:08 +0100

    -3-to-pounds.js

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,25 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+
+const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1);
+
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+const pounds = paddedPenceNumberString.substring( 0, paddedPenceNumberString.length - 2);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+  return `£${pounds}.${pence}`;
+}
+
+console.log(toPounds("399p"));// £3.99
+console.log(toPounds("5p"));  //£0.05  
+console.log(toPounds("689p"));//£6.89
+console.log(toPounds("250p"));//2.50
