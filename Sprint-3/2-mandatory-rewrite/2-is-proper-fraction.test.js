4b0534af48bb9e15edbcb8cb7f8d06a67416acfe
Amy-Rose <amyrlcollins@gmail.com>
2025-06-14 11:01:49 +0100

    2 rewrite 2 proper fraction: added jest tests and they pass

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,20 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5,4)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for negative proper fractions", () => {
+  expect(isProperFraction(-2,3)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for fractions with equal numerator and denominator", () => {
+  expect(isProperFraction(3,3)).toEqual(false);
+});
+
+test("should return false for negative improper fractions", () => {
+  expect(isProperFraction(-5,4)).toEqual(false);
+});
