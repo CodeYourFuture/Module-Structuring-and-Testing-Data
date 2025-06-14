5dac570947de710e60e05fcfb073d70112638501
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-06-14 13:09:25 +0100

    Added code from ./1-key implement/3-get-card-value.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,9 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+  // replace with your code from key-implement
+  let rank = card[0];
+  if (rank === "A") return 11;
+  else if (rank >= "2" && rank <= "9") return Number(rank);
+  else if (rank == "10" || rank == "J" || rank == "Q" || rank == "K") return 10;
+  else return "Invalid Card rank";
 }
 module.exports = getCardValue;
