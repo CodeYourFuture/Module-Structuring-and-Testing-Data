1325fc52c230c4e6b25809cdf1c4e564d49cc09d
AdnaanA <thetriplex247@gmail.com>
2025-06-14 10:32:36 +0100

    modified:   Sprint-1/1-key-exercises/4-random.js

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -1,6 +1,7 @@
 const minimum = 1;
 const maximum = 100;
 
+
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
@@ -15,3 +16,16 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Finally, we add the minimum value (1) to ensure that the random number falls within the desired range of 1 to 100.
 
 console.log(num);
+
+// Using an array to define the range
+const range = [1, 100]
+
+const num2 = Math.floor(Math.random() * (range[1] - range[0] + 1)) + range[0];
+
+console.log(num2);
+// num2 is the same as num, but it uses an array to define the range.
+// This demonstrates that you can use arrays to define ranges for random number generation.
+// The expression (range[1] - range[0] + 1) calculates the range of numbers we want to generate, which is 100 - 1 + 1 = 100.
+// The Math.floor() function rounds down the result to the nearest whole number.
+// Finally, we add the minimum value (range[0]) to ensure that the random number falls within the desired range of 1 to 100.
+// Interval notation: [1, 100]
