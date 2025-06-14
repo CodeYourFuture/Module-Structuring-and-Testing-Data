4463f020339d23d6554938da98f44ee79906975f
Anuar Duisenbek <coolboy949494@gmail.com>
2025-06-14 13:40:49 +0100

    solved 1-key errors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//
+// I predict we will make mistake because str has been already declared
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -11,3 +13,7 @@ function capitalise(str) {
 
 // =============> write your explanation here
 // =============> write your new code here
+
+function capitalise(str) {
+  return (str = `${str[0].toUpperCase()}${str.slice(1)}`);
+}
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -3,6 +3,10 @@
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
+/*SyntaxError: Identifier 'decimalNumber' has already been declared*/
+//Because same name is used again with const decimalNumber = 0.5;
+// inside the function.
+
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
@@ -16,5 +20,16 @@ console.log(decimalNumber);
 
 // =============> write your explanation here
 
+// console.log(decimalNumber); line is outside the function,
+//  so the variable decimalNumber is not defined in that scope.
+//  This will cause a ReferenceError.
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(500));
