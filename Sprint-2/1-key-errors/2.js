83d4a0c24f570aedd8ff01e02a3c52b50ee95167
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-14 21:47:32 +0100

    Key Errors | Fixed syntax error in 2.js by using a valid parameter name

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,21 @@
 
 // Predict and explain first BEFORE you run any code...
+// I predict we’ll get a syntax error because `3` is not a valid parameter name in a function definition.
 
 // this function should square any number but instead we're going to get an error
+// I predict we’ll get a syntax error because `3` is not a valid parameter name in a function definition.
 
-// =============> write your prediction of the error here
-
-function square(3) {
+  function square(3) {
     return num * num;
-}
-
-// =============> write the error message here
-
-// =============> explain this error message here
+// SyntaxError: Unexpected number.
+// You cannot name a function parameter with a number like `3`.
+// Parameters must be valid variable names (e.g., `num`).
 
 // Finally, correct the code to fix the problem
+// Here's a new code
+function square(num) {
+  return num * num;
+}
 
-// =============> write your new code here
-
+console.log(square(3));
 
