1de6300693e5e24e9331c09ea5661a481914ce8a
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-06-14 14:32:36 +0300

    sprint_3 folder 3-mandatory-practice

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,11 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, num) {
+    if (num < 0) {
+        throw new Error("Negative counts are not valid.");
+    } else if (num == 0) {
+        return '';
+    } else {
+        return str.repeat(num);
+    }
 }
 
 module.exports = repeat;
