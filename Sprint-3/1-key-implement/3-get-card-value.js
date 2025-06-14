9981e3a756aa20a15b9aaac56257d3a3aa19dd7f
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-06-14 14:21:26 +0300

    sprint_3 folder 2-mandatory-rewrite + fix for 3-get-card-value.js from folder 1

--- Sprint-3/1-key-implement/3-get-card-value.js
+++ Sprint-3/1-key-implement/3-get-card-value.js
@@ -9,14 +9,17 @@
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
 function getCardValue(card) {
   let suits = ["♠", "♥", "♦", "♣"];
-  if (!suits.includes(card[-1]) || Number(card[0]) < 2) {
-    throw new Error("Invalid card rank");
+  if (!suits.includes(card[card.length - 1])) {
+    throw new Error("Invalid card rank.");
+  }
+  if (card.substring(0, card.length - 1) !== "10" && !isNaN(Number(card[0])) && Number(card[0]) < 2) {
+    throw new Error("Invalid card rank.");
   }
   if (card[0] === 'A') {
     return 11;
   } else if (card == '10' || card[0] === 'J' || card[0] === 'Q' || card[0] === 'K') {
     return 10;
-  } else if (Number(card[0]) > 2 && Number(card[0]) < 9) {
+  } else if (Number(card[0]) >= 2 && Number(card[0]) <= 9) {
     return Number(card[0]);
   }
 }
