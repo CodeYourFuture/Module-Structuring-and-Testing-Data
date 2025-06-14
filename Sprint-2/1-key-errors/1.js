2a18e2c5fe95e219c342913a8d1bbc8555a30246
Declan W <declanwilliams793@gmail.com>
2025-06-14 16:00:05 +0100

    explained in comments why errors occured and then refactored the code to fix the convert to percentage function works correctly

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,9 +2,12 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// I expect a Syntax error to be thrown due to the const "decimalNumber" has already been declaired in the parameters of the function.
+// I also expect an error from the "console.log(decimalnumber)" as its calling "decimalNumber" from outside the function
 
 // Try playing computer with the example to work out what is going on
 
+/*
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
@@ -12,9 +15,19 @@ function convertToPercentage(decimalNumber) {
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(decimalNumber); 
+*/
 
 // =============> write your explanation here
+// When running the code i get thrown a Syntax error: "identifer 'decimalNumber' has already been declared". To solve this you must remove the "const decimalNumber = 0.5" from the function as its already been defined in the parameters of the function.
+// after testing the code and fixing the syntax error we then recived another error: "ReferenceError: decimalNumber is not defined" To solve this you must change the "console.log(decimalNumber)" to "console.log(convertToPercentage())".
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5)); // equals to 50%
