e095144866b4da955b21709846aef989f63ff4f2
mo-muchunu <mo.muchunu@gmail.com>
2025-06-14 09:17:47 +0100

    Added input condition to 'toPounds' function

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -5,60 +5,40 @@
 
 // You should call this function a number of times to check it works for different inputs
 
-const penceString = "399p";
+/* 
+ =========> Shorten variable names for readability
+ 'penceString' --> 'pStr'
+ 'penceStringWithoutTrailingP' --> 'penceWithoutP'
+ 'paddedPenceNumberString' --> 'paddedPence'
 
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
-
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
-
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
-
-console.log(`£${pounds}.${pence}`);
-
-// To refactor this code, I will:
-// =========> Shorten variable names for readability
-// 'penceString' --> 'pStr'
-// 'penceStringWithoutTrailingP' --> 'penceWithoutP'
-// 'paddedPenceNumberString' --> 'paddedPence'
-
-// =========> Reduce/ remove redundant string methods and properties
-// '.substring()' --> .slice() is more versatile, simpler for code trimming and unlike .substring(), doesn't need .length calculation  
-// '.length' --> .slice() can count from the end of the string, making .length unnecessary. 
-// '.padEnd()' --> Redundant because 'padStart(3)' guarantees enough digits.
-
-
-function toPounds(pStr) {
-    const penceWithoutP = pStr.substring(0,pStr.length - 1);
-    const paddedPence = penceWithoutP.padStart(3, "0");
-    const pounds = paddedPence.substring(0,paddedPence.length - 2);
-    const pence = paddedPence.substring(paddedPence.length - 2).padEnd(2, "0");
-
-    return `£${pounds}.${pence}`
-
-}
+ =========> Reduce/ remove redundant string methods and properties
+ '.substring()' --> .slice() is more versatile, simpler for code trimming and unlike .substring(), doesn't need .length calculation  
+ '.length' --> .slice() can count from the end of the string, making .length unnecessary. 
+ '.padEnd()' --> Redundant because 'padStart(3)' guarantees enough digits
+ 'pStr.toString()' --> To make sure input is a string
+ '(!value.endsWith('p')) {value += 'p';}' --> Add 'p' at the end if it's missing
+*/
 
 // =========> Refactored Code:
 
 function toPounds(pStr) {
-    const penceWithoutP = pStr.slice(0, -1);
+    
+    let value = pStr.toString();
+
+    if (!value.endsWith('p')) {
+        value += 'p';
+    }
+
+    const penceWithoutP = value.slice(0, -1);
     const paddedPence = penceWithoutP.padStart(3, "0");
     const pounds = paddedPence.slice(0, -2);
     const pence = paddedPence.slice(-2);
-
+    
     return `£${pounds}.${pence}`;
 }
 
-console.log(toPounds("41586p")); // Output: £415.86
-
-console.log(toPounds("15p")); // Output: £0.15
-
-console.log(toPounds("7813649p")); // Output: £78136.49
+/*
+console.log(toPounds("41586p"));  --> Output: £415.86
+console.log(toPounds("15"));  --> Output: £0.15
+console.log(toPounds(7813649));  --> Output: £78136.49
+*/
