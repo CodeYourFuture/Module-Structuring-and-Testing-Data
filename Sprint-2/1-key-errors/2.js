45549bd85b4cfb221148cbe4fa3e836118057134
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-15 19:26:29 +0100

    fix the parameter of the function and print the output to the terminal

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,22 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+                // We will got an error because the parameter of the function
+                // need to string type not number so it can be something like num 
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-
+                // SyntaxError: Unexpected number
 // =============> explain this error message here
+// The error message tells that we wrote a syntax that JavaScript cannot understand
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
-
+                // function square(num) {
+                //     return num * num;
+                // }
+                // result = console.log(square(4));
