0e310079c426c009927bc57629216edf03107391
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-15 20:31:47 +0100

    Implement | Created reusable toPounds function

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -2,5 +2,19 @@
 
 // You will need to take this code and turn it into a reusable block of code.
 // You will need to declare a function called toPounds with an appropriately named parameter.
+function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.slice(0, -1); 
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 
+  const pounds = paddedPenceNumberString.slice(0, -2);
+  const pence = paddedPenceNumberString.slice(-2).padEnd(2, "0");
+
+  return `£${pounds}.${pence}`;
+}
 // You should call this function a number of times to check it works for different inputs
+   
+// Test calls:
+// The code "console.log(toPounds("399p"));" shows the price of £3.99.
+// The code "console.log(toPounds("50p"));" shows that the amount is £0.50.
+// The console.log function displays the value "4p" (which is equivalent to £0.04).
+// The code "console.log(toPounds("1234p"));" shows the price of £12.34.
