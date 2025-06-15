71751a27d99b0b4ff376337486e387455d947fc3
Sara Tahir <saratahir34@gmail.com>
2025-06-15 19:17:06 +0100

    A number cannot be the name of a parameter. Explained error and code updated.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,29 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
-function square(3) {
+//we dont have num defined here.
+//we need a valid parameter name here
+//The error will be related to the fact that we don't have a valid  parameter name while defining the function.
+//The code is returning num*num but num is not defined .
+//Instead of the Number 3 we should have num as a parameter of the function.
+/*function square(3) {
     return num * num;
-}
+}*/
 
 // =============> write the error message here
-
+//SyntaxError: Unexpected number
 // =============> explain this error message here
+// //The error will be related to the fact that we don't have a valid  parameter name while defining the function.
+//we cannot have number as the name of a parameter
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
 
+function square(num) {
+    return num * num;
+}
+let num = 7
+result = square(num)
+console.log(result)
