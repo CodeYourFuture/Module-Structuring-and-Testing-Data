c3d876a16a6d8a0e7903d8c3fca7739fe975b898
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-15 18:41:53 +0200

    implemented the getOrdinalNumber function

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,10 @@
 function getOrdinalNumber(num) {
- return "1st";
+  const lastTwo = num % 100;
+  const lastDigit = num % 10;
+  if (lastDigit === 1 && lastTwo !== 11) {
+    return num + "st";
+  }
+  return num + "th";
 }
 
 module.exports = getOrdinalNumber;
