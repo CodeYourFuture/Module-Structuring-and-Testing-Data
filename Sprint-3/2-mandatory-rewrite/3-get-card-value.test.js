4e9bd10fcaf1da4108c4eb03efd4e1503adb131f
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-15 17:31:13 +0200

    rewrote tests for getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,24 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 2 for 2 of Hearts", () => {
+    const twoOfHearts = getCardValue("2♥");
+    expect(twoOfHearts).toEqual(2);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Diamonds", () => {
+    const jackOfDiamonds = getCardValue("J♦");
+    expect(jackOfDiamonds).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Spades", () => {
+    const aceOfSpades = getCardValue("A♠");
+    expect(aceOfSpades).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+    expect(() => getCardValue("Z♣")).toThrow("Invalid card rank.");
+});
