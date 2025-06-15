fa97fb888e1d8c41bb77d16f6c3491c05eb956a5
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-06-15 20:13:11 +0100

    Completed missing tasks: 2-mandatory-errors 3-mandatory-Interpret

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,7 @@
 
 const age = 33;
 age = age + 1;
+/* const creates a variable that can’t be reassigned, so if you want to change the value later, use let instead. */
+let age = 33;
+age = age + 1;
+console.log(age);
