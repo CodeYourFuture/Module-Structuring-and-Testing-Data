a9cc9a818ce11ca87643c9d4efc818d3e778fb5a
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-15 18:45:32 +0200

    updated the test file for getOrdinalNumber function

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -10,4 +10,29 @@ const getOrdinalNumber = require("./get-ordinal-number");
 
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+});
+    
+// Case 2: Identify the ordinal number for 11
+// When the number is 11,
+// Then the function should return "11th"
+
+test("should return '11th' for 11", () => {
+    expect(getOrdinalNumber(11)).toEqual("11th");
+});
+
+// Case 3: Identify the ordinal number for 21
+// When the number is 21,
+// Then the function should return "21st"
+
+test("should return '21st' for 21", () => {
+    expect(getOrdinalNumber(21)).toEqual("21st");
+}
+);
+
+// Case 4: Identify the ordinal number for 111
+// When the number is 111,
+// Then the function should return "111th"
+
+test("should return '111th' for 111", () => {
+    expect(getOrdinalNumber(111)).toEqual("111th");
+});
