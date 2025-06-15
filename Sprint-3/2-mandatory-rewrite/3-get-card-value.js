d27358f1eb0ec3d12dde618792de89761519b4cf
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-15 17:28:37 +0200

    updated the getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,9 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    const rank = card.slice(0, -1);
+    if (rank === "A") return 11;
+    if (!isNaN(rank) && Number(rank) >= 2 && Number(rank) <= 10) return Number(rank);
+    if (["J", "Q", "K", "10"].includes(rank)) return 10;
+    throw new Error("Invalid card rank.");
 }
+
 module.exports = getCardValue;
