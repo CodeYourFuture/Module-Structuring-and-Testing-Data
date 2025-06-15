e1d14d0b0ff2ea9f30695c6c5bcbd53bd86f4090
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-15 18:35:19 +0100

    Implement | Added toUpperSnakeCase function with examples

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,10 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+function toUpperSnakeCase(input) {
+  return input.toUpperCase().split(" ").join("_");
+}
+
+// Example usage:
+console.log(toUpperSnakeCase("hello there"));           // "HELLO_THERE"
+console.log(toUpperSnakeCase("lord of the rings"));     // "LORD_OF_THE_RINGS"
