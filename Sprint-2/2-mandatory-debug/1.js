53a60223d8ea37386125998e2e1ce3612e85045a
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-15 19:52:32 +0100

    fixing the returning value

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
+      // The sum value will be undefined because we are returning nothing from the 
+      //  function
 
 function sum(a, b) {
   return;
@@ -9,5 +11,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+      // The sum value will be undefined because we are returning nothing from the 
+      //  function
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+            function sum(a, b) {
+              return a + b;
+            }
+
+            console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
