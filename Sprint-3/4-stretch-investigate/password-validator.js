93cb99ce58c92e658ec0d57a7207f6185da00ad8
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-16 09:23:16 +0200

    implemented the passwordValidator function

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,17 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
-}
+function passwordValidator(password, previousPasswords = []) {
+    // Check if password is a string and at least 5 characters
+    if (typeof password !== 'string' || password.length < 5) return false;
 
+    // Check for uppercase, lowercase, number, and special character
+    if (!/[A-Z]/.test(password)) return false;
+    if (!/[a-z]/.test(password)) return false;
+    if (!/[0-9]/.test(password)) return false;
+    if (!/[!#$%.&*]/.test(password)) return false;
+
+    // Check if password was previously used
+    if (previousPasswords.includes(password)) return false;
+
+    return true;
+}
 
 module.exports = passwordValidator;
