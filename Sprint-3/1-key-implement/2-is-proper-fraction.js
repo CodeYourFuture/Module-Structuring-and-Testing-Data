c9eb29cfccb89b09e27314d36a84cacb9ef7ec81
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-16 18:55:15 +0100

    -2-is-proper-fraction.js I have implemented a function and tested the file using jest

--- Sprint-3/1-key-implement/2-is-proper-fraction.js
+++ Sprint-3/1-key-implement/2-is-proper-fraction.js
@@ -8,7 +8,8 @@
 // write one test at a time, and make it pass, build your solution up methodically
 
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
+    return Math.abs(numerator) < Math.abs(denominator);/* Math.abs helps to convert negative numbers to absolute numbers. 
+    -6 becomes 6 and it will be easier to make the comparison between numerator and denominator.*/ 
 }
 
 // here's our helper again
@@ -40,6 +41,7 @@ assertEquals(improperFraction, false);
 // target output: true
 // Explanation: The fraction -4/7 is a proper fraction because the absolute value of the numerator (4) is less than the denominator (7). The function should return true.
 const negativeFraction = isProperFraction(-4, 7);
+assertEquals(negativeFraction, true);
 // ====> complete with your assertion
 
 // Equal Numerator and Denominator check:
@@ -47,7 +49,21 @@ const negativeFraction = isProperFraction(-4, 7);
 // target output: false
 // Explanation: The fraction 3/3 is not a proper fraction because the numerator is equal to the denominator. The function should return false.
 const equalFraction = isProperFraction(3, 3);
+assertEquals(equalFraction, false);
 // ====> complete with your assertion
 
 // Stretch:
 // What other scenarios could you test for?
+/* if both numerator and denominator are negative.
+nominator =-3 
+denominator = -9)
+-3/-9 are both converted to 3/9 using the Math.abs
+const negFraction = isProperFraction(-3 , -9);
+assertEquals(negFraction, true);
+
+zero numerator:
+if numerator = 0
+denominator = 7
+the 0 is less than 7 so the test is true 
+const zeroFraction = isProperFraction(0, 7);
+assertEquals(zeroFraction, true);
