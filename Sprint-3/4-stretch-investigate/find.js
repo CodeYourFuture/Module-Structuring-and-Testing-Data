47d9d5963ad378c89fdef9b15780a38466d63cbb
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-16 08:16:16 +0200

    analysed the find function

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,17 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+// The index variable starts at 0 and increments by 1 in each iteration of the while loop 
+// until it either finds the character or reaches the end of the string.
+
 // b) What is the if statement used to check
+// The if statement checks whether the current character in the string (str[index]) is equal 
+// to the target character (char).
+
 // c) Why is index++ being used?
+// The index++ is used to move to the next character in the string during each iteration 
+// of the while loop.
+
 // d) What is the condition index < str.length used for?
+// The condition index < str.length is used to ensure that the while loop continues 
+// as long as there are characters left to check in the string.
