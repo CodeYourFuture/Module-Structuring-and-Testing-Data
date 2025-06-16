0174569d4f6e201aa087c4daff69b2d34981981f
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-16 09:29:27 +0200

    updated the test file for passwordValidator

--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -14,13 +14,64 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+
 const isValidPassword = require("./password-validator");
+
+// Case 1: password has at least 5 characters (but fails other requirements)
 test("password has at least 5 characters", () => {
-    // Arrange
-    const password = "12345";
-    // Act
+    const password = "12345"; // Too simple, missing other criteria
     const result = isValidPassword(password);
-    // Assert
+    expect(result).toEqual(false);
+});
+
+// Case 2: password has at least one uppercase letter
+test("password has at least one uppercase letter", () => {
+    const password = "Password1!";
+    const result = isValidPassword(password);
     expect(result).toEqual(true);
-}
-);
+});
+
+// Case 3: password has at least one lowercase letter
+test("password has at least one lowercase letter", () => {
+    const password = "PASSWORD1!";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+// Case 4: password has at least one number
+test("password has at least one number", () => {
+    const password = "Password!";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+// Case 5: password has at least one special character
+test("password has at least one special character", () => {
+    const password = "Password1";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+// Case 6: password is not in the previous passwords array
+test("password is not in the previous passwords array", () => {
+    const password = "NewPassword1!";
+    const previousPasswords = ["12345", "Password1!", "Test@123"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(true);
+});
+
+// Case 7: password is in the previous passwords array
+test("password is in the previous passwords array", () => {
+    const password = "Test@123";
+    const previousPasswords = ["12345", "Password1!", "Test@123"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(false);
+});
+
+// Case 8: valid password with all conditions met
+test("valid password with all conditions met", () => {
+    const password = "Valid1!Password";
+    const previousPasswords = ["12345", "Password1!", "Test@123"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(true);
+});
