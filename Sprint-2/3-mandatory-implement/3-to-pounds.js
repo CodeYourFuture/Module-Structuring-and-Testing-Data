848b133e295e6f8e0064e116af1732fe8c0d016e
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-16 01:20:12 +0100

    create function to represent price in pounds

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,23 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+  console.log(`£${pounds}.${pence}`);
+}
+toPounds((penceString = "399p"));
+toPounds((penceString = "102p"));
+toPounds((penceString = "1025p"));
+
