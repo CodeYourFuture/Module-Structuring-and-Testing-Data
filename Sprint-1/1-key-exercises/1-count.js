520d24a283105eeab1b4f8aa627b4eba3334a16e
avatarit <salah@ait.ps>
2025-06-16 18:01:04 +0100

    Add clarification comments in 1-count.js regarding the increment operation

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -8,3 +8,7 @@ count = count + 1;
 // Line 3 is updating the value of the count variable by adding 1 to its current value. The = operator is used for assignment, meaning it takes the value on the right (count + 1) and assigns it to the variable on the left (count).
 // The expression count + 1 calculates the new value, and then the = operator assigns this new value back to the count variable.
 // The count variable now holds the value 1, which is the result of the initial value (0) plus 1.
+
+// Can you find out what one-word programming term describes the operation on line 3?
+// The answer is count++
+
