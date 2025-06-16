c7a00b770cebfba9f85c895fe70be9d7432bd9b7
Ahmad <ahmadehsas14345@gmail.com>
2025-06-16 18:12:06 +0100

    completed initials exercise using string indexing

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,7 +5,12 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+let initials = firstName[0] + middleName[0] + lastName[0];
+console.log(initials)
+// fistName[0] gets the first character of the string "Creola" C
+// middleName[0] gets the second character of the name "Katherine" K
+// lastName[0] gets the third character of the name "Johnson" J
+
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
