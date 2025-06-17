f5522e711f8c7c8e93631c8cf4bce1b964409cff
Samiuk <lahlahhoussam80@gmail.com>
2025-06-17 23:41:09 +0100

    Add explanation for incrementing count variable by 1 after initialization.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,4 @@ count = count + 1;
 
 //
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Line 3 Take the current value of count, and then add 1, and store the result back in count .
+// Line 3 Take the current value of count, and then add 1, and store the result back in count.
