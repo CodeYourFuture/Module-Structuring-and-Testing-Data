9328618be54dd37af6f5c6cfb034c66f69e598b4
Ahmad <ahmadehsas14345@gmail.com>
2025-06-17 15:38:50 +0100

    fixed typeEroor by converting number to string before slicing  last 4 digits.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,6 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const cardNumber = "4533787178994213";
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +8,7 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+// TypeError: cardNumber.slice is not a function. it was not the error i predicted, and  the difference is that i predicted that the code is correct just add console.log variable.
+// How to fix this  error: first convert number "toString", then use .slice(-4);.
+// The "toString()" method converts number to string.
+// java script has trouble with large number so in this case we add the number in string("").
