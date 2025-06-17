a9f60fe5bb73c7b6d639dfd4a3eb690e60d5ec37
Ahmad <ahmadehsas14345@gmail.com>
2025-06-17 02:14:03 +0100

    Add explanation of how num is calculated as a random number between munimum and maximum.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,11 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// The variable "num" stores a random whole number between 1 and 100.
+// The expression works as follows:
+// 1: Math.random() generates a decimal 0 to 1 (not including 1)
+// This decimal is multiplied by (maximum - minimum + 1) which gives a number between 0 and 99.999.
+// Math.floor() then rounds this down to a whole number between 0 to 99.
+// Adding "minimum" shifts the result so the final number is  between 1 to 100.
+// for this reason num changes every time when you run the program always gives the number between 1 to 100.
