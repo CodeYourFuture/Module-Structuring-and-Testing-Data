fd7497e4d57a74491cfc6ca09dd72f0c4e95eee9
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-17 21:21:41 +0100

    feat: Tested then implemented the function `getOrdinalNumber`

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,21 @@
 function getOrdinalNumber(num) {
- return "1st";
+    if (typeof num !== "number" || !Number.isInteger(num) || num < 0) {
+        throw new Error("Num must be a positive integer")
+    }
+    if (num % 100 >= 11 && num % 100 <= 13) {
+        return num + "th";
+    }
+    lastDigit= num % 10;
+    switch (lastDigit) {
+        case 1:
+            return num + "st";
+        case 2:
+            return num + "nd";
+        case 3:
+            return num + "rd";
+            default:
+                return num + "th";
+    }
 }
 
 module.exports = getOrdinalNumber;
