9a056c09d90153648ce9d0cd35187eb505ef8516
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-06-17 20:30:49 +0100

    Implement the function getOrdinalNumber() to convert the number into ordinal,  also tested it with different test cases in jest

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,23 @@
 function getOrdinalNumber(num) {
- return "1st";
+  let ordinal = "";
+  switch (true) {
+    case num == 1 || (num % 10 == 1 && num != 11):
+      ordinal = `${num}st`;
+      break;
+    case num == 11:
+      ordinal = `${num}th`;
+      break;
+    case num == 2 || num == 22:
+      ordinal = `${num}nd`;
+      break;
+    case num == 3 || (num % 10 == 3 && num != 13):
+      ordinal = `${num}rd`;
+      break;
+    default:
+      ordinal = `${num}th`;
+      break;
+  }
+  return ordinal;
 }
 
 module.exports = getOrdinalNumber;
