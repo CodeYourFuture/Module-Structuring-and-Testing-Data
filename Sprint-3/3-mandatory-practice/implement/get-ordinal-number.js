6bb7c1169edad1a4df023dec55874430646cc8b9
gerbil1511 <gerbil1511@gmail.com>
2025-06-17 18:08:33 +0100

    extract the common values into well named variables so code is more readable

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -5,16 +5,21 @@ function getOrdinalNumber(n) {
   /*
     There is a mathematical approach to determine the correct order suffix for ordinal numbers using modulo opertions. Most numbers follow a rule based on the lst digit, ie. if the last digit is 1, 2 or 3 use "st", "nd", "rd" respectively and for everything else use "th". The exception to the rule is for numbers ending in 11, 12, or 13, they always end in "th". For the last digit of a number use modulo 10 (e.g. 21 % 10 = 1, 102 % 10 = 2, etc), for the last two digits of a number use modulo 100 (e.g. 111 % 100 = 11, 11)
     */
-  if (n % 100 === 11 || n % 100 === 12 || n % 100 === 13) {
+
+  //extract common values into a variable
+  const lastDigit = n & 10;
+  const lastTwoDigits = n % 100;
+
+  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
     return n + "th";
   }
-  if (n % 10 === 1) {
+  if (lastDigit === 1) {
     return n + "st";
   }
-  if (n % 10 === 2) {
+  if (lastDigit === 2) {
     return n + "nd";
   }
-  if (n % 10 === 3) {
+  if (lastDigit === 3) {
     return n + "rd";
   }
   return n + "th"; //everything else in numbers
