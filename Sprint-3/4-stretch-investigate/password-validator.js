36327a5443d767c0d779dcb155b3b4751bbf91fd
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-06-17 21:32:36 +0100

    Function passwordValidator has been implemented to check for the valid password and tested for each case using jest

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,29 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
+function passwordValidator(password, previousPasswords = []) {
+  switch (true) {
+    case password.length < 5:
+      return false;
+      break;
+    case !/[A-Z]/.test(password):
+      return false;
+      break;
+    case !/[a-z]/.test(password):
+      return false;
+      break;
+    case !/[0-9]/.test(password):
+      return false;
+      break;
+    case !/[!#$%.&*]/.test(password):
+      return false;
+      break;
+    // Check if the password is in the previous passwords array
+    case previousPasswords.includes(password):
+      return false;
+      break;
+    // If all conditions are met, return true
+    default:
+      return true;
+      break;
+  }
 }
-
 
 module.exports = passwordValidator;
