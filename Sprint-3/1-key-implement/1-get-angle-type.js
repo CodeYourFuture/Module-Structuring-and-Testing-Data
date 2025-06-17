9cc8b665cc429b2095b3c2f31062b42861a41b33
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-17 10:25:33 +0100

    1-get-angle-type.js

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -9,7 +9,7 @@
 
 function getAngleType(angle) {
    if (angle === 90) return "Right angle";
-   if (angle < 90) return "Acute angle";
+   if (angle < 90) return "Acute angle"
    if (angle > 90 && angle < 180) return "Obtuse angle";
    if (angle === 180) return "Straight angle";
    if (angle > 180 && angle < 360) return "Reflex angle";
