becf97ac33e982b534c0524bac54827e4cb032af
gerbil1511 <gerbil1511@gmail.com>
2025-06-17 15:55:18 +0100

    correct faceCards array and following if statement to handle card number 10

--- Sprint-3/1-key-implement/3-get-card-value.js
+++ Sprint-3/1-key-implement/3-get-card-value.js
@@ -9,7 +9,7 @@
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
 function getCardValue(card) {
   // use an array to hold the values of the face cards.
-  const faceCards = ["10", "J", "Q", "K"];
+  const faceCards = ["J", "Q", "K"];
 
   /*
   use slice to extract the rank (the numeric or face card value) from the "card" value. Start at the beginning index of the string (0) and go up to but not including the last character (-1 means one from the end)
@@ -17,7 +17,7 @@ function getCardValue(card) {
   const rank = card.slice(0, -1);
 
   if (rank === "A") return 11; //handles Ace cards
-  if (rank >= 2 && rank < 10) return Number(rank); //handles cards 2-9
+  if (rank >= 2 && rank <= 10) return Number(rank); //handles cards 2-10
 
   // use the includes() method to check if the value of the variable rank is one of the faceCards array values.
   if (faceCards.includes(rank)) return 10; //handles face cards
