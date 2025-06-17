8b9040a30bf20ae339284261bf059fc88edb337a
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-17 16:13:00 +0100

    fix: Correct countChar to return actual count.

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-    return 5
+    let count = 0;
+    for (let i = 0; i < stringOfCharacters.length; i++) {
+        if (stringOfCharacters[i] === findCharacter) {
+            count++;
+        }
+    }
+    console.log(`The character "${findCharacter}" appears ${count} times in the string.`);
+    return count
 }
+
 
 module.exports = countChar;
