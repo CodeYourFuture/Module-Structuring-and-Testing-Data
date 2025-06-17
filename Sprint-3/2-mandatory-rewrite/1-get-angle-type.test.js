dea34f83944112ad9cc786e8d192cb646718a4d9
Declan Williams <declanwilliams793@gmail.com>
2025-06-17 19:56:05 +0100

    Add tests for identifying obtuse angles in getAngleType function

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -12,15 +12,23 @@ test("should identify right angle (90°)", () => {
 // Then the function should return "Acute angle"
 test("should identify as an acute angle (less than 90 degrees)", () => {
   expect(getAngleType(45)).toEqual("Acute angle");
-})
+});
 
 // Case 3: Identify Obtuse Angles:
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
+test("should identify as an obtuse angle (greater then 90 degrees and less then 180 degrees)", () => {
+  expect(getAngleType(90.38)).toEqual("Obtuse angle");
+  expect(getAngleType(179.99)).toEqual("Obtuse angle");
+  expect(getAngleType(139)).toEqual("Obtuse angle");
+});
 
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
+test("should identify as a straight angle (exactly 180 degrees)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+})
 
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
