02ba073a9ce38dfd302969aca66c7e487374fed9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 09:55:29 +0100

    written all the test cases, all passed

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,21 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for 5 of heart",()=>{
+    const fiveofHearts = getCardValue("5♥");
+    expect(fiveofHearts).toEqual(5); 
+})
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for J of spades",()=>{
+    const jofSpades=getCardValue("J♠");
+    expect(jofSpades).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Heart",()=>{
+    const aceofHeart = getCardValue("A♥");
+    expect(aceofHeart).toEqual(11);
+})
 // Case 5: Handle Invalid Cards:
+test("should return invalid cards for anything else",()=>{
+    expect(getCardValue("l")).toBe("Invalid card rank")
+})
