5889529855d17827b7470e83b8c0c82277a198bf
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 12:55:36 +0100

    updated the code to cover the test cases

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,21 @@
 function getOrdinalNumber(num) {
- return "1st";
+    if (isNaN(num)) {
+      return NaN;
+    }
+if(num>=11 && num<=13){
+    return num+"th"
+}
+
+if(num===1){
+    return num+"st"
+}
+if (num === 2) {
+  return num + "nd";
+}
+if (num === 3) {
+  return num + "rd";
+}
+return (num)+"th"
 }
 
 module.exports = getOrdinalNumber;
