32d6f560fd9c1df82de10450da554e3d686c04fa
gerbil1511 <gerbil1511@gmail.com>
2025-06-18 11:11:27 +0100

    adjust function repeat(str,count) for clarity

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -2,15 +2,10 @@ function repeat(str, count) {
   if (typeof count !== "number" || !Number.isInteger(count) || isNaN(count)) {
     throw new Error("Count is an invalid input");
   }
-  if (count > 0) {
-    return str.repeat(count);
-  }
-  if (count === 0) {
-    return "";
-  }
   if (count < 0) {
     throw new Error("Count must be a non-negative integer");
   }
+  return str.repeat(count);
 }
 
 module.exports = repeat;
