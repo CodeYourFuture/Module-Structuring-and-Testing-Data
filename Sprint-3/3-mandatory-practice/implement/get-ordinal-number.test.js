cabdf43dc99bc30ff64fddcf235681796c759178
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 12:55:08 +0100

    implemented and tested additional cases

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -11,3 +11,33 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
     });
+// Case 2: Identify the ordinal number for 2
+// When the number is 2,
+// Then the function should return "2nd"
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+// Case 3: Identify the ordinal number for 3
+// When the number is 3,
+// Then the function should return "3rd"
+test("should return '3rd' for 3", () => {
+    expect(getOrdinalNumber(3)).toEqual("3rd");
+  });
+  // Case 4: Identify the ordinal number for 11,12,13
+// When the number is 11,12,13,
+// Then the function should return "11th","12th","13th"
+test("should return '11th' for 11", () => {
+    expect(getOrdinalNumber(11)).toEqual("11th");
+  });
+    // Case 5: Identify the ordinal number for 6
+// When the number is 6
+// Then the function should return "6th"
+test("should return '6th' for 6", () => {
+    expect(getOrdinalNumber(6)).toEqual("6th");
+  });
+// Case 6: Identify NaN
+// When the number is NaN
+// Then the function should return "NaN"
+test("should return 'NaN' for NaN", () => {
+    expect(isNaN(getOrdinalNumber(NaN))).toEqual(true);
+  });
