eed9cfa2be97c0604770d8a2e9914bf9e8d5247b
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-18 12:02:45 +0100

    get-ordinal-number.js mandatory-practice/implement

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,14 @@
 function getOrdinalNumber(num) {
- return "1st";
+    const lastTwoNum = num % 100;
+    const lastDigit = num % 10;
+
+    if (lastTwoNum >= 11 && lastTwoNum <= 13) return `${num}th`;
+
+    if(lastDigit === 1) return `${num}st`;
+    if(lastDigit === 2) return `${num}nd`;
+    if(lastDigit === 3) return `${num}rd`;
+
+    return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
