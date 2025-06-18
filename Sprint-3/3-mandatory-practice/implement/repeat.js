441c19a93f5627f765054fb7439174cecd360d20
Amy-Rose <amyrlcollins@gmail.com>
2025-06-18 12:31:35 +0100

    implement, repeat: tests written and code passing

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,12 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(string,repetitions) {
+
+    if (repetitions<0){throw new Error("you cannot enter a negative number of repetitions")};
+
+    repeatsInArray = Array.from({length:repetitions}, (_,i) => string);
+    return repeatsInArray.join("");
 }
+
+// manyHellos = repeat("hello",5);
+// console.log(manyHellos);
 
 module.exports = repeat;
--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -11,7 +11,7 @@ const repeat = require("./repeat");
 
 test("should repeat the string count times", () => {
     const str = "hello";
-    const count = 3;
+    let count = 3;
     const repeatedStr = repeat(str, count);
     expect(repeatedStr).toEqual("hellohellohello");
     });
@@ -21,12 +21,31 @@ test("should repeat the string count times", () => {
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
 
+test("a count of 1 should return the string without repetition", () => {
+    const str2 = "beans";
+    const count2 = 1;
+    const repeatedStr2 = repeat(str2, count2);
+    expect(repeatedStr2).toEqual("beans");
+})
+
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
 
+test("a count of 0 should return an empty string, regardless of str input", () => {
+    const str3 = "meow";
+    const count3 = 0;
+    const repeatedStr3 = repeat(str3,count3);
+    expect(repeatedStr3).toEqual("");
+})
+
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+test("a negative count should throw an error", () => {
+    const str4 = "djent";
+    const count4 = -4;
+    expect(()=> repeat(str4,count4)).toThrow();
+})
