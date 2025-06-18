b6e348f14d2554d84a7b913f3d25d8e69bb00e2e
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-18 12:59:18 +0100

    Add/update get-ordinal-number.test.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -11,3 +11,26 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
     });
+test("should return '2nd' for 2", () => {
+    expect((getOrdinalNumber(2))).toEqual("2nd");
+    });
+
+test("should return '3rd' for 3", () => {
+    expect((getOrdinalNumber(3))).toEqual("3rd");
+    });
+
+test("should return '4th' for 4", () => {
+    expect((getOrdinalNumber (4))).toEqual("4th");
+    });
+
+test("should return '11th' for 11", () => {
+    expect((getOrdinalNumber(11))).toEqual("11th");
+    });
+
+test("should return '22nd' for 22", () => {
+    expect((getOrdinalNumber(22))).toEqual("22nd");
+    });
+
+test("should return '33rd' for 33", () => {
+    expect((getOrdinalNumber(33))).toEqual("33rd");         
+});
