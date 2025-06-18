9bfd882f27c8c5c25a78f1651814d44acda51afa
avatarit <salah@ait.ps>
2025-06-19 00:56:02 +0100

    Add explanation for error due to redeclaration of decimalNumber in convertToPercentage function

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,6 +2,8 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// The error will occur because the variable `decimalNumber` is being redeclared with `const
+// inside the function `convertToPercentage`, which is not allowed since `decimalNumber` is already defined as a parameter of the function.
 
 // Try playing computer with the example to work out what is going on
 
