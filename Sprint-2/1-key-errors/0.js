b470233ffe4304aaa34369911fb84c235e58ed56
[Halimatou Waghe] <[waghehalimatou@gmail.com]>
2025-06-18 21:55:07 +0100

    1-key errors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// I think the code will not rum properly because let in line 10 should be declared before the function in line 9 is called.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +12,14 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+
+// The error is: Uncaught SyntaxError: Identifier 'str' has already been declared. It means that we cannot declare a variable with the same name as a parameter inside the function. In this case, We don't need the declaration let as str is already declared in the function's parameter.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+
+console.log(capitalise("hello world"));
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,27 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+//The error will probably be related to the digit 3 in the function because 3 is not considered as a number in javascript. We need to explicitly declare it as a number for the code to run.
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+// The error message is: Uncaught SyntaxError: Unexpected number 
 
 // =============> explain this error message here
+//The error message means that we cannot use a digit number as a parameter name in a function. In this case, we need to use a valid variable name that Javascript recognises like num instead of the digit number 3.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+
+function square(num) {
+    return num * num;
+}
+
+console.log(square(3));
+
 
 
