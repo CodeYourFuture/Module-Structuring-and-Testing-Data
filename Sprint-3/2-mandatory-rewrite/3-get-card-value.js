1fe202bb70e3e1f0b397e255b220a6e4fd27470d
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-18 13:10:09 +0100

    Update getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,8 +1,8 @@
 function getCardValue(card) {
     const rank = card.slice(0, -1); 
     if (rank === 'A') return 11; 
-    if (!isNaN(rank)) return Number(rank); 
     if (['J', 'Q', 'K', '10'].includes(rank)) return 10;
+    if (!isNaN(rank)) return Number(rank); 
     throw new Error("Invalid card rank.");
     
 }
