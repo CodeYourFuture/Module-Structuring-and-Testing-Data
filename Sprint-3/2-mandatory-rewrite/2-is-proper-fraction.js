d81fcbbd6178557df6b0835ffc7b7d7def1b9961
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 08:54:05 +0100

    added the completed function

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,5 +1,9 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
+    if (isNaN(numerator) || isNaN(denominator)) return NaN;
+    if (Math.abs(numerator) < Math.abs(denominator)) return true;
+    if (Math.abs(numerator) > Math.abs(denominator)) return false;
+    if (Math.abs(numerator) === Math.abs(denominator)) return false;
+    
     // add your completed function from key-implement here
 }
 
