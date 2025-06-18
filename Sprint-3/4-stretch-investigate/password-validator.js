247dc6181417f7319bea5415b26d1d893b4067bc
Amy-Rose <amyrlcollins@gmail.com>
2025-06-18 14:33:55 +0100

    stretch password validator: wrote tests and coded to pass tests

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,5 +1,29 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+
+    previousPasswords = ["Be&n5","Be&n56969"];
+    if (previousPasswords.includes(password)){throw new Error("You can't reuse an old password")}
+
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!#$%.*&]).{5,}$/.test(password);
+
+    /* || REGEXP BREAKDOWN:
+
+    /^              start of string
+
+    .               matches any single character (except newline)
+
+    .*              any single character, occuring 0 or more times
+
+    (?=.*[a-z])     lookahead for anything (.*) followed by at least one lowercase letter
+
+    (?=.*[A-Z])     as above but for uppercase letters
+
+    (?=.*[!#$%.*&]) as above but for specific special characters
+
+    .{5,}           match any character (except newline) that occurs 5 or more times
+
+    $/ end of string
+
+    */
 }
 
 
--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -1,26 +1,42 @@
-/* 
-Password Validation
-
-Write a program that should check if a password is valid
-and returns a boolean
-
-To be valid, a password must:
-- Have at least 5 characters.
-- Have at least one English uppercase letter (A-Z)
-- Have at least one English lowercase letter (a-z)
-- Have at least one number (0-9)
-- Have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
-- Must not be any previous password in the passwords array. 
-
-You must breakdown this problem in order to solve it. Find one test case first and get that working
-*/
 const isValidPassword = require("./password-validator");
-test("password has at least 5 characters", () => {
-    // Arrange
-    const password = "12345";
-    // Act
+
+// CASE 1: Password should be at least 5 characters long
+// testing a password that isn't long enough
+test("password needs at least 5 characters", () => {
+    const password = "1234";
     const result = isValidPassword(password);
-    // Assert
-    expect(result).toEqual(true);
+    expect(result).toEqual(false);
 }
 );
+
+// CASE 2: Password should contain at least one English uppercase letter (A-Z)
+// testing a password that doesn't have any uppercase letters
+test("password needs at least one uppercase letter", () => {
+    expect(isValidPassword("abcdef")).toEqual(false);
+})
+
+
+// CASE 3: Password must contain at least one English lowercase letter (a-z)
+test("password needs at least one lowercase letter", () => {
+    expect(isValidPassword("ABCDEF")).toEqual(false);
+})
+
+// CASE 4: Password must contain at least one number (0-9)
+test("password needs at least one number", () => {
+    expect(isValidPassword("abcDEF")).toEqual(false);
+})
+
+// CASE 5: Password must have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
+test("password needs at least one of these special characters: ! # $ % . * &", () => {
+    expect(isValidPassword("abcDEF42")).toEqual(false);
+})
+
+// CASE 6: Must not be any previous password in the passwords array
+test("password can't be a password that has been used previously", () => {
+    expect(()=>isValidPassword("Be&n56969")).toThrow();
+})
+
+// CASE 7: Password follows all the rules
+test("password needs at least one of these special characters: ! # $ % . * &", () => {
+    expect(isValidPassword("abcDEF42!")).toEqual(true);
+})
