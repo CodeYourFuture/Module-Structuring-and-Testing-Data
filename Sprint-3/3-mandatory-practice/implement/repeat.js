3b0d5296ac4ed2bb9140c9896cf63051cbbf53cd
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 13:24:28 +0100

    updated the code according to the test cases

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,8 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str,count) {
+    if(count<0){
+        return "negative number"
+    }
+    return str.repeat(count);
 }
-
+//console.log(repeat("hello",3))
 module.exports = repeat;
