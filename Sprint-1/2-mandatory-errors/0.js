9218f67a34f248c7d7016c0528a555ef9cab7851
Hendrine Zeraua <puumotu@hotmail.com>
2025-06-18 17:36:29 +0100

    Examples shown how to comment instructions or anything related to code that does not need to be executed by writing 2 slashes(//) before the line starts.

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+//In such a case that instructions are given to humans instead of computer, it should be commented.  Meaning that any text between // and the end of the line is ignored by the compiler (will not be executed).
+
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
