66ab144ef8c8183b47c53d44850a7424b54853d7
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 17:36:57 +0100

    wrote the test cases

--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -17,10 +17,38 @@ You must breakdown this problem in order to solve it. Find one test case first a
 const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
     // Arrange
-    const password = "12345";
+    const password = "123bB45%";
     // Act
     const result = isValidPassword(password);
     // Assert
     expect(result).toEqual(true);
 }
 );
+test("password has at least one uppercase letter (A-Z)", () => {
+  const password = "abcB123%"; 
+  const result = isValidPassword(password);
+  expect(result).toEqual(true);
+});
+test("password has at least one lowercase letter (a-z)", () => {
+  const password = "abcB123%";
+  const result = isValidPassword(password);
+  expect(result).toEqual(true);
+});
+test("password has at least one number (0-9)", () => {
+  const password = "abcB123%";
+  const result = isValidPassword(password);
+  expect(result).toEqual(true);
+});
+test('password has at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")', () => {
+  const password = "abcB123%";
+  const result = isValidPassword(password);
+  expect(result).toEqual(true);
+});
+     const previousPasswords=["12345r%M"]
+     test("password must not be any previous password", () => {
+       const previousPasswords = ["12345r%M"];
+       const password = "12345r%M";
+       // Simulate your validator taking previousPasswords as a second argument
+       const result = isValidPassword(password, previousPasswords);
+       expect(result).toEqual(false);
+     });
