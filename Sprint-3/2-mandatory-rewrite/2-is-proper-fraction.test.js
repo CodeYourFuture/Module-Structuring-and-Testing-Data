777b1e709d9730bba04e4480747d4804bebbb6a8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 09:22:08 +0100

    CREATED TEST CASES FOR THE FUNCTION

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,19 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for improper fraction",()=>{
+  expect(isProperFraction(5,2)).toEqual(false);
+})
 
 // Case 3: Identify Negative Fractions:
+test("should return true for negative fraction",()=>{
+  expect(isProperFraction(-4,7)).toEqual(true);
+})
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator",()=>{
+  expect(isProperFraction(3,3)).toEqual(false);
+})
+test("should return NaN for non-numbers",()=>{
+  expect(isProperFraction(3,NaN)).toEqual(NaN);
+})
