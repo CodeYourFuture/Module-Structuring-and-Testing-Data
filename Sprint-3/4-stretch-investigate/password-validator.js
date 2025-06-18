93f777e04d1641866918e3b67f2c5d125ae01283
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-18 17:37:19 +0100

    wrote the code according to test cases

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,14 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
-}
 
-
-module.exports = passwordValidator;
+   
+    function passwordValidator(password,previousPasswords=[]) {
+        if (previousPasswords.includes(password)) return false;
+        const hasMinLength = password.length >= 5;
+        const hasUpper = /[A-Z]/.test(password);
+        const hasLower = /[a-z]/.test(password);
+        const hasNumber = /[0-9]/.test(password);
+        const hasSymbol = /[!#$%.*&]/.test(password);
+      
+        return hasMinLength && hasUpper && hasLower && hasNumber && hasSymbol;
+      }
+      
+      module.exports = passwordValidator;
