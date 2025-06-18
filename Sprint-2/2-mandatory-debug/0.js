de8d819b3fda7d60ef5dddffad0a4cca696fa99c
Shannon <shannond050@outlook.com>
2025-06-18 19:19:40 +0200

    result of multiplying

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,21 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+//the function will run but return nothing cause theres nothing being returned in this function
+/*
 function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);*/
 
 // =============> write your explanation here
+//theres nothing being returned in this function
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
