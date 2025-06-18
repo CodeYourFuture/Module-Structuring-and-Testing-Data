a22739c5f7a358eb94bb4debdd874b400e1b30b4
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-19 00:26:58 +0100

    password-validator.js stretch-investigate

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,5 +1,13 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
+function passwordValidator(password, previousPasswords = []) {
+    if (password.length < 5) return false;
+
+    const passwordUpper = /[A-Z]/.test(password);
+    const passWordLower = /[a-z]/.test(password);
+    const passwordNumber =/[0-9]/.test(password);
+    const passwordSpecial =/[!#$%.&*]/.test(password);
+    const notPreviousPassword = !previousPasswords.includes(password);
+
+    return passwordUpper && passWordLower && passwordNumber && passwordSpecial && notPreviousPassword
 }
 
 
