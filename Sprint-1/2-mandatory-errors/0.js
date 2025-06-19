4f906b13b438377753b7848a61ac9c2f74fa41f9
HoussamLh <156331030+HoussamLh@users.noreply.github.com>
2025-06-19 07:48:08 +0100

    Update 0.js
    Added comments to exclude instructional lines from execution

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,12 @@
+ // we can turn the lines into comments. we can use:
+
+// for single-line comments, or
+
+/* for multi-line comments. */
+
+/*
+
 This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+We don't want the computer to run these 2 lines - how can we solve this problem?  
+
+*/
