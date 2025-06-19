ab7bfffe2cf759546030a4753e0b6c894d01e17c
Praise88 <praisemagidi7@gmail.com>
2025-06-19 23:28:07 +0200

    completed Sprint-2/2-mandatory-debug

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+the return statement is not defined 
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +9,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+when you write return; and then go to a new line, it immediately ends the function and ignores everything after it
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
