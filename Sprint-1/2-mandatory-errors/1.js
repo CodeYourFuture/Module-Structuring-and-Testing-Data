5e31d7c8bf1b57d4057959248d567ef3f0bf47c5
HoussamLh <156331030+HoussamLh@users.noreply.github.com>
2025-06-19 07:32:14 +0100

    Update 1.js
    Updated age variable to let to allow incrementing its value

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,5 @@
 // trying to create an age variable and then reassign the value by 1
+// Changed age declaration from const to let to allow reassignment
 
-const age = 33;
+let age = 33;
 age = age + 1;
