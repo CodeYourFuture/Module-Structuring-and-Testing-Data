36eef674601ac782483118815ed0ee81dd085395
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-19 11:13:54 +0100

    fix: updating the variables first letter from uppercase to lowercase.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -4,5 +4,5 @@
 // The error is that the variables starts with a number, which is not allowed in JavaScript and most programming languages.
 
 // This is the correct way.
-const HourClockTime = "20:53";
-const hourClockTime = "08:53";
+const firstHourClockTime = "20:53";
+const secondHourClockTime = "08:53";
