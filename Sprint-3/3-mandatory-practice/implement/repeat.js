531b3edea05e49a5ad8c36dfa4dc233c76c14d49
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-19 20:27:38 +0100

    implemented and tested the repeat function

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,11 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+    if(count > 0) {
+        return str.repeat(count); 
+    } else if (count === 0) {
+        return " ";
+    } else {
+        throw new Error("Invalid number. Please enter a positive number or 0 for count");
+    }     
 }
 
 module.exports = repeat;
