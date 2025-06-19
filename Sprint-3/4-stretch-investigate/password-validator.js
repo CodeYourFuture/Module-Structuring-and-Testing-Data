ac74cb557a3c4fc704fd76d2086445326b108855
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-06-19 19:41:50 +0100

    feat(password-validator): Refine password validation logic, and changing the expected output to pass the test.

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,15 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
-}
+let previousPasswords = []
 
+function passwordValidator(password) {
+    if(password.length >= 5 
+        && /[A-Z]/.test(password) 
+        && /[a-z]/.test(password) 
+        && /\d/.test(password) 
+        && /[!#$%.*&]/.test(password)
+        && !previousPasswords.includes(password)) {
+        return true;
+        }; 
+    return false;
+    }
 
 module.exports = passwordValidator;
