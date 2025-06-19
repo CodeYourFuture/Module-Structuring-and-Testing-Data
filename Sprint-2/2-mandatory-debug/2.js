91dbe2f23899d5814ec7513d90500e4620789a80
Shannon <shannond050@outlook.com>
2025-06-19 16:35:52 +0200

    getLastDigit fix

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,7 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
-
+/*
 const num = 103;
 
 function getLastDigit() {
@@ -12,13 +12,28 @@ function getLastDigit() {
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
+*/
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+//The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
 // Explain why the output is the way it is
 // =============> write your explanation here
+// the return statement is using the num which is declared as a variable with a value before the function
+// because getLastDigit does not have a parameters assigned to it.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// num had already been declared as variable with the value of 103 and not set as the parameter for the function
+// so when getLasDigit is called the it draws directly from the variable num
