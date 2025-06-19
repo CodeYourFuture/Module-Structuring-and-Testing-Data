c198c8dd5585fea86c95c37a17c9665d866ba066
AdnaanA <thetriplex247@gmail.com>
2025-06-19 14:01:08 +0100

    modified:   Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,29 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for 5 of Hearts", () => {
+    const fiveOfHearts = getCardValue("5♥");
+    expect(fiveOfHearts).toEqual(5);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Diamonds", () => {
+    const jackOfDiamonds = getCardValue("J♦");
+    expect(jackOfDiamonds).toEqual(10);
+});
+test("should return 10 for Queen of Spades", () => {
+    const queenOfSpades = getCardValue("Q♠");
+    expect(queenOfSpades).toEqual(10);
+});
+test("should return 10 for King of Hearts", () => {
+    const kingOfHearts = getCardValue("K♥");
+    expect(kingOfHearts).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Clubs", () => {
+    const aceOfClubs = getCardValue("A♣");
+    expect(aceOfClubs).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+    expect(() => getCardValue("Z♠")).toThrow("Invalid card rank");
+});
