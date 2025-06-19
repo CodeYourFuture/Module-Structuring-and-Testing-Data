e6d0a792455d8877b44b620d3119aa3f2f84ee28
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-19 16:27:16 +0100

    updated 2-time-format.js changed the name of the variable.

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -39,22 +39,22 @@ console.log(result);
 
 // c) 
 movieLength % 60
-// This expression works out the remainder of the movieLenght after its devided by 60.
-// the movieLenght devided by 60 equals 24 seconds, so expression have converted the whole
+// This expression works out the remainder of the movieLength after its divided by 60.
+// the movieLength divided by 60 equals 24 seconds, so expression have converted the whole
 // number (8784) to minutes to be divided by 60 to get 24 seconds.
 
 // d) This line refers to the declared variable totalMinutes. The expression is subtracting 
-// the movieLenght from the reminingSeconds (remainder) 24 seconds to then devide that total 
-//by 60. The result will rpresent the totalMinutes. (8784-24) / 60
+// the movieLength from the remainingSeconds (remainder) 24 seconds to then divide that total 
+//by 60. The result will represent the totalMinutes. (8784-24) / 60
 
-// e) The variable result represent the time lenght in the hour:minute:second format.
-// A better name can be formatedResult.
+// e) The variable result represent the time length in the hour:minute:second format.
+// A better name can be formattedTime
 
 // f) When I run the code with 0 as the value of movielength the final output for the code is
 // 0:0:0   I think the 0 value is working well with the code.
 
 // I have tried new value for movielength as 1946, the output was:
-// 0:32:26  This ouput seems to be alright as well.
+// 0:32:26  This output seems to be alright as well.
 
 // I have also tried a negative values and the output was negative.
 // movielength = -30946;
