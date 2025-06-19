80eefd3688640656ac5af2797776a67be5150795
HoussamLh <156331030+HoussamLh@users.noreply.github.com>
2025-06-19 07:51:35 +0100

    Update 2.js
    Declare variable before using in template string.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,5 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+// what's the error ?   The error here is due to using a variable before it's declared.
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
