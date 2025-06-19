f4e88f30e7afd2ca6896f441da7bb7d4ce067c09
Praise88 <praisemagidi7@gmail.com>
2025-06-19 22:47:23 +0200

    completed Sprint-2/1-key-errors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
-// call the function capitalise with a string input
+I think the error will be that str has already been declared 
+// call the function capitalize with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+//function capitalize(str) {
+ // let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+// return str;
+//}
 
 // =============> write your explanation here
+in line 8 they are trying to declare str again
 // =============> write your new code here
+
+function capitalize(str) {
+  let capitalize = `${str[0].toUpperCase()}${str.slice(1)}`;
+ return capitalize;
+}
+console.log(capitalize(`praise`));
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
 
-// Why will an error occur when this program runs?
+// Why will an error occur when this program runs
 // =============> write your prediction here
-
+the const variable is being declared twice
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
@@ -14,7 +14,14 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
 
+// =============> write your explanation here
+because the percentage is being declared twice the code will not run smoothly, console.log(decimalNumber) will crash the program because it's trying to access a variable that only exists inside the function
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(7.9));
