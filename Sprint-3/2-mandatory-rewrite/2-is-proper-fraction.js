f658887e17d335bf0cf17cd4e13e462ec8fe44a5
AdnaanA <thetriplex247@gmail.com>
2025-06-19 13:55:54 +0100

    modified:   Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js 	modified:   Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,9 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
     // add your completed function from key-implement here
+    // A proper fraction is a fraction where the absolute value of the numerator is less than the absolute value of the denominator
+    if (Math.sign(numerator) === -1 && Math.sign(denominator) === -1) {
+        return true; // both negative, so the fraction is proper
+    }
 }
 
 module.exports = isProperFraction;
