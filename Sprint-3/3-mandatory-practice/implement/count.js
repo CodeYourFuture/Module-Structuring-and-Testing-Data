5634292d229b5e059a9fa3f33fcba80588607689
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-19 15:31:46 +0100

    implemented and tested the char count

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,10 @@
-function countChar(stringOfCharacters, findCharacter) {
-    return 5
+function countChar(str, char) {
+    if (str.includes(char) === true) {
+        return str.split(char).length -1;
+    } else {
+        return 0;
+    }
+    
 }
 
 module.exports = countChar;
