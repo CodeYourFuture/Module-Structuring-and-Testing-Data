09f828e0591e282b9855daabd6089936fe033549
c-mayamba <chrismayamba@gmail.com>
2025-06-20 23:12:18 +0200

    Mandatory-debug-0 solved2

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -10,7 +10,7 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> The result of multiplying 10 and 32 is undefined because
-// has not been properly defined to return a value.
+// 'multiply' has not been properly defined to return a value.
 
 // Finally, correct the code to fix the problem
 //  =============> Here's the new code:
