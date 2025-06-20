4b0860cb4229ac93f7bc607222e301cfeb82c53b
mo-muchunu <mo.muchunu@gmail.com>
2025-06-20 13:36:21 +0100

    Wrote unit tests for 'getCardValue' function covering all card types

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,15 @@
+// replace with your code from key-implement
+
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+  const rank = card.slice(0, -1); // remove last character 
+  const numberCards = Number(rank);
+
+  if (rank === "A") return 11;
+  if (["K", "Q", "J", "10"].includes(rank)) return 10; // To handle face cards
+  if (!isNaN(numberCards) && numberCards >= 2 && numberCards <= 10) { // Check that numberCards is a valid number and ensure they are within range (2–10)
+    return numberCards;
+  }
+  return "Invalid card rank";
 }
+
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,34 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
+  const aceofSpades = getCardValue("A♠");
+  expect(aceofSpades).toEqual(11);
+});
 
 // Case 2: Handle Number Cards (2-10):
+test("should handle number cards within range (2-10)", () => {
+  const numberCards = getCardValue("7♦");
+  expect(numberCards).toEqual(7);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should handle face cards (J, K, Q) to return a value of 10", () => {
+  const faceCards = ["K♠", "Q♥", "J♦", "10♣"];
+  for (const card of faceCards) {
+  expect(getCardValue(card)).toEqual(10);
+  }
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for an Ace", () => {
+  const aceCard = getCardValue("A♥");
+  expect(aceCard).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should return 'Invalid card rank' for cards that a neither a number nor a recognized face", () => {
+  const invalidCards = ["Y", "11", "Y♠", "15♣"];
+  for (const card of invalidCards) {
+  expect(getCardValue(card)).toEqual("Invalid card rank");
+  }
+});
