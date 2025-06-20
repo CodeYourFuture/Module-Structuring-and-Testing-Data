16512e49977404c53d2f406fdb9fc5632dcd6c4a
delmorallopez <124817272+delmorallopez@users.noreply.github.com>
2025-06-20 19:50:08 +0100

    Review-Exercices-Sprint-1

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -3,10 +3,4 @@
 let age = 33;
 age = age + 1;
 
-<<<<<<< Updated upstream
 console.log(age); // Output: 34
-=======
-
-// const = A constant reference to a value, Cannot be reassigned to a new value. Good for values that should not change (like configuration, functions, etc.)
-// let = A block-scoped variable that can be reassigned. Useful when the value is expected to change.
->>>>>>> Stashed changes
