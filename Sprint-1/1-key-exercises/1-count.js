b0469be729068270db37c1dcf1e7d1ae272873f6
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-20 16:11:20 +0100

    updated = described line 3

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -6,7 +6,7 @@ count = count + 1;
 // Describe what line 3 is doing, in particular focus on what = is doing
 
 /*
- Answer: in line 3, the variable count gets reassigned a new value which is the sum of the old value and 1.
+ Answer: line 3 shows increment operation - the variable count gets reassigned a new value which is the sum of the old value and 1.
 In other words, the variable count is now adding 1 to its old value to obtain a new value.
 */
 
