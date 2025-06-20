37fac9cc1c7b77383469343d53a2e9fa5555db3e
c-mayamba <chrismayamba@gmail.com>
2025-06-20 23:17:28 +0200

    Mandatory-debug-1 solved

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The syntax of the function `sum` is incorrect.
 
 function sum(a, b) {
   return;
@@ -8,6 +8,12 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> "a + b" should be on the same line as the return statement.
+
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> Here's the new code:
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
