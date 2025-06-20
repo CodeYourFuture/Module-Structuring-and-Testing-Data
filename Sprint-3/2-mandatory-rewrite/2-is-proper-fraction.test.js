b4ea116101fe771b4c7b0e8718bc97e2a71d778b
mo-muchunu <mo.muchunu@gmail.com>
2025-06-20 08:06:17 +0100

    Added 'isProperFraction' code and decriptive test cases

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(9, 5)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative fraction", () => {
+  expect(isProperFraction(-6, 10)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for a fraction with equal numerator and denominator", () => {
+  expect(isProperFraction(4, 4)).toEqual(false);
+});
