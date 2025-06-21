0a80b524858c7ca53316cee01d6241ff33cf6ddf
mo-muchunu <mo.muchunu@gmail.com>
2025-06-21 02:18:54 +0100

    Implemented 'repeat' function with negative count check and comprehensive tests

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,6 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+    if (count < 0) return "Invalid input";
+    return str.repeat(count);
 }
 
 module.exports = repeat;
