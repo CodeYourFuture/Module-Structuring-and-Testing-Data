94f85ba0819feda66aa40c482794ce33a3564557
Mirabel Adom <mirabel.adom@gmail.com>
2025-06-21 23:02:20 +0100

    Anwered que 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "08:53";
+const twentyFourHourClockTime = "20:53";
+
+// Variable names must start with a letter, underscore symbol or , but cannot start with a number.
