3a2b6c7508979459cec0af6502bac7ff6712cd08
obie <obertilimandzi@gmail.com>
2025-06-21 12:22:36 +0200

    i need to create a pull request for 1 and 2

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,11 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    const rank = card.slice(0, -1)
+
+    if (rank === "A" || rank == "A") return 11;
+    if (rank >= "2" && rank <= "9") return Number(rank);
+    if (rank === "10" || rank ==="Q"|| rank === "J" || rank === "K") return 10; 
+    return "Invalid card rank";
 }
+
+
 module.exports = getCardValue;
