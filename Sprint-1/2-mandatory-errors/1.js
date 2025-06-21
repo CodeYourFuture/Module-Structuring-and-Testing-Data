06e4cfb56e4469c842c53763129af025c74c578e
Emeke <chukwuemekeajuebor247@gmail.com>
2025-06-21 07:43:38 +0100

    2-1,2-2

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,6 @@
 
 const age = 33;
 age = age + 1;
+
+//TypeError: Assignment to constant variable. 
+// The second line clearly will not work since age was declared to be constant and CANNOT change.
