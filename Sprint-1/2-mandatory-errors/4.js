0cbc5adfd39ed736bf69f73c4c5332c62c297b7f
nurzat <nurzatcode@gmail.com>
2025-06-21 14:11:00 +0100

    update 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,5 +1,19 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
 
-+const twelveHourClockTime = "20:53";
-+const twentyFourHourClockTime = "08:53";
+
+
+//output the values
+
+
+
+console.log("Clock time in 12h format:", clockTime_12Hour);
+
+
+console.log("Clock time in 24h format:",clockTime_24Hour);
+
+
+
+
+
+
