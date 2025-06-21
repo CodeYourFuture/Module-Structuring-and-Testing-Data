c7d60fc0e6e2a5b2b013114bd3b03dc26ba7c4a7
Sara Tahir <saratahir34@gmail.com>
2025-06-21 17:44:51 +0100

    Wrote tests for Angles. Ran them with Jest. All tests are passing

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -1,10 +1,24 @@
 const getAngleType = require("./1-get-angle-type");
 
-test("should identify right angle (90°)", () => {
-  expect(getAngleType(90)).toEqual("Right angle");
-});
+  test("should identify right angle (90°)", () => {
+  expect(getAngleType(90)).toEqual("Right angle");});
 
-// REPLACE the comments with the tests
+  test("should identify acute angle (45°)", () => {
+  expect(getAngleType(45)).toEqual("Acute angle");});
+
+  test("should identify obtuse angle (120°)", () => {
+  expect(getAngleType(120)).toEqual("Obtuse angle");});
+
+  test("should identify straight angle (180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");});
+
+  test("should identify reflex angle (220°)", () => {
+  expect(getAngleType(220)).toEqual("Reflex angle");});
+
+  
+
+
+
 // make your test descriptions as clear and readable as possible
 
 // Case 2: Identify Acute Angles:
