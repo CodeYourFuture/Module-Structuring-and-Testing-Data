652a66bd7c06db37c836ea7bc90f30616d0a1645
nurzat <nurzatcode@gmail.com>
2025-06-21 13:46:37 +0100

    update 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -6,9 +6,10 @@ age = age + 1;
 
 // trying to create an age variable and then reassign the value by 1
 
-let age = 33;
+let age = 33;   //use 'let' instead 'const' that needs to be reassigned
 age = age + 1;
 
-console.log(age);
+console.log(age);  //log the updated age value
+
 //output:34 
 
