79812f21a335aa274423d5c0572ae6a4605bc46e
Emeke <chukwuemekeajuebor247@gmail.com>
2025-06-21 07:56:57 +0100

    2-3,2-4

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+// In the above, error will occur as  Variable names are not allowed to start with a number.
+// This means that we must modify the names slightly. 
+
+const Hour24ClockTime = "20:53";
+const Hour12ClockTime = "08:53";
+
+console.log(Hour24ClockTime)
