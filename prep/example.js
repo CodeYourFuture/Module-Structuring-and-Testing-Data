bc143cef416bae1b5539f4a7f7d31147aefb236f
Abayie <jonathanboahene@gmail.com>
2025-06-21 07:41:16 +0100

    prep-files

--- prep/example.js
+++ prep/example.js
@@ -3,3 +3,12 @@
 // 	return percent
 // }
 // console.log(decimalTopercent(0.5) + "%");
+
+const decimalNumber = 0.5;
+
+function convertToPercentage() {
+  const percentage = `${decimalNumber * 100}%`;
+}
+
+convertToPercentage(0.5);
+console.log(percentage);
