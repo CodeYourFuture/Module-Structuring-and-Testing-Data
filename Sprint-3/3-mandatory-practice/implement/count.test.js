193bea455121db5274c7d28f1d2a4b59debc7686
mo-muchunu <mo.muchunu@gmail.com>
2025-06-21 02:56:13 +0100

    Implemented 'countChar' function with tests for multiple and zero occurrences

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -22,6 +22,7 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
 test("should return 0 when character does not occur", () => {
   const str = "Tomorrow";
   const char = "t";
