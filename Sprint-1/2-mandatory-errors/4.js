e8b8bb9286faeb8cdea2b8d8ddda1d4c8a631144
Abayie <jonathanboahene@gmail.com>
2025-06-22 14:55:02 +0100

    4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+// variables in js can't begin with numbers
+const twelveHourClockTime = "20:53";
+const twenty24hourClockTime = "08:53";
