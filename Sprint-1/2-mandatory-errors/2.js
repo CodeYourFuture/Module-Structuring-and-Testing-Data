eff1a8f7e9f03792d7acc7c0650ea1af90074b80
Abayie <jonathanboahene@gmail.com>
2025-06-22 14:42:29 +0100

    2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,13 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+/* 
+This happens because in JavaScript, variables declared with `let` or `const` are not accessible before their declaration 
+due to a behavior called the **temporal dead zone**. 
+If you try to use such a variable before it’s initialized, you get a `ReferenceError`. 
+To avoid this, always declare and assign your variables before using them.
+
+*/
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
