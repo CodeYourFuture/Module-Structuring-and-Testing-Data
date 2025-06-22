a7e94bed3bfb66409ce3c81bf5cdafc0207599f7
mayowa0-7 <fmayowauthman@gmail.com>
2025-06-22 14:34:42 +0100

    i committed the names

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-line 3 increases the variable by adding +1 to the initial variable
+line 3 increases the variable by adding +1 to the initial 
