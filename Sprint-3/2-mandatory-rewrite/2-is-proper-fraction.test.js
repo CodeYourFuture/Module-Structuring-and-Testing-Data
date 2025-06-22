696094213dbc2e8211ce1c7039b284e711e7ab12
Praise88 <praisemagidi7@gmail.com>
2025-06-22 13:56:09 +0200

    completed Sprint-3/2-mandatory-rewrite

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for a improper fraction", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for negative numerator with less value than denominator", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,24 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for 5 of Hearts", () => {
+  const fiveOfHearts = getCardValue("5♥");
+  expect(fiveOfHearts).toEqual(5);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for jack of clubs", () => {
+    const  jackOfClubs = getCardValue("J♣");
+    expect(jackOfClubs).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for ace of spades", () => {
+    const aceOfSpades = getCardValue("A♠");
+    expect(aceOfSpades).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should throw error for invalid card rank", () => {
+  expect(() => getCardValue("Z♠")).toThrow("Invalid card rank");
+});
