d3021a96fac00eb5f4440f137c77275cf7c8af29
obie <obertilimandzi@gmail.com>
2025-06-22 22:15:26 +0200

    finished the count function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,15 @@
 function countChar(stringOfCharacters, findCharacter) {
-    return 5;
+    let count = 0;
+
+    for(let i = 0; i < stringOfCharacters.length; i++) {
+        if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+    }
+    return count;
 }
 
 module.exports = countChar;
+
+// how many times a charactor appears in a string
+// what are the inputs and outputs
