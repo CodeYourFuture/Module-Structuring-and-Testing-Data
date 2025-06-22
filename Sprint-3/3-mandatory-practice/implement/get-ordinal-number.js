3a879a4582e36760e1b609f34535c12b0b51d5ff
petergmlui <pgmlui@gmail.com>
2025-06-22 15:36:16 +0100

    committing progress in 3-mandatory-practice

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,41 @@
 function getOrdinalNumber(num) {
- return "1st";
+  let numStr = num.toString(); // convert input to char string
+  let numLastDigit = numStr.slice(-1); // get the last char
+
+  // if ends in 1
+  if (numLastDigit == "1") {
+    if (num == 11) {
+      // catch special case 11
+      return numStr + "th";
+    } else {
+      return numStr + "st";
+    }
+  }
+
+  // if ends in 2
+  else if (numLastDigit == "2") {
+    if (num == 12) {
+      // catch special case 12
+      return numStr + "th";
+    } else {
+      return numStr + "nd";
+    }
+  }
+
+  // if ends in 3
+  else if (numLastDigit == "3") {
+    if (num == 13) {
+      // catch special case 13
+      return numStr + "th";
+    } else {
+      return numStr + "rd";
+    }
+  }
+
+  // other
+  else {
+    return numStr + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,15 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  let result = ""; // initialize an empty str
+
+  for (i = 0; i < count; i++) {
+    result = result + str; // repeat the string
+  }
+
+  if (count < 0) {
+    result = "Negative counts are not valid."; // catch negative numbers
+  }
+
+  return result;
 }
 
 module.exports = repeat;
