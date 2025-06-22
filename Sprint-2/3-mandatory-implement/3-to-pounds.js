2a174e507daf2c04fd198ba6fae7e285c232a3a4
Emeke <chukwuemekeajuebor247@gmail.com>
2025-06-22 17:50:45 +0100

    3-3

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,26 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+
+function convertToPounds (penceString) {
+//penceString = "399p";
+penceStringWithoutTrailingP = penceString.substring(0,  penceString.length - 1);
+
+paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+
+pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+
+return (`£${pounds}.${pence}`);
+}
+console.log(convertToPounds("845p"))
+
+// This program takes a string representing a price in pence
+// The program then builds up a string representing the price in pounds
+
+// You need to do a step-by-step breakdown of each line in this program
+// Try and describe the purpose / rationale behind each step
+
+// To begin, we can start with
+// 1. const penceString = "399p": initialises a string variable with the value "399p"
