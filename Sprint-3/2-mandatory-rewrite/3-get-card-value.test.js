900385e7ca37584223be452429b39eb0304fcff1
Declan Williams <declanwilliams793@gmail.com>
2025-06-23 16:34:44 +0100

    Add tests for number card values 2-10 in getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,17 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return the correct value for number cards between 2 and 9", () => {
+    expect(getCardValue("2♥")).toEqual(2);
+    expect(getCardValue("3♦")).toEqual(3);
+    expect(getCardValue("4♣")).toEqual(4);
+    expect(getCardValue("5♠")).toEqual(5);
+    expect(getCardValue("6♥")).toEqual(6);
+    expect(getCardValue("7♦")).toEqual(7);
+    expect(getCardValue("8♣")).toEqual(8);
+    expect(getCardValue("9♠")).toEqual(9);
+    expect(getCardValue("10♥")).toEqual(10);
+});
 // Case 3: Handle Face Cards (J, Q, K):
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
