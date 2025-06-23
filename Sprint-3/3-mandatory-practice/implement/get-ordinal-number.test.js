2376b6f1ed300229cb41c178a00195a9eba0b737
Sara Tahir <saratahir34@gmail.com>
2025-06-23 12:54:01 +0100

    Wrote test cases and function for ordinal numbers

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -11,3 +11,18 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
     });
+test("should return '11th' for 11", () => {
+    expect(getOrdinalNumber(11)).toEqual("11th");
+    });
+test("should return '3rd' for 3", () => {
+    expect(getOrdinalNumber(3)).toEqual("3rd");
+    });
+test("should return '4th' for 4", () => {
+    expect(getOrdinalNumber(4)).toEqual("4th");
+    });
+test("should return '28th' for 28", () => {
+    expect(getOrdinalNumber(28)).toEqual("28th");
+    });
+test("should return '32nd' for 32", () => {
+    expect(getOrdinalNumber(32)).toEqual("32nd");
+    });
