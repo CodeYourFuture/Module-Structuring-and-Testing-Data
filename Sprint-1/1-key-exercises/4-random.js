c93411c753faebff2621238f759a24ba19c81862
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-23 17:31:24 +0100

    updated my responses

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -9,9 +9,9 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 
 /* 
-1. Since Math.random() returns an integer in the interval [0, 1). This means num eventually represents any random integer in the interval [0, 100]. 
-2. Math.floor() rounds down the expression "Math.random() * (maximum - minimum + 1)"", leading to an integer in the interval [0,100).
+1. Since Math.random() returns an integer in the interval [0, 1). This means num eventually represents any random integer in the interval [1, 100]. 
+2. Math.floor() rounds down the expression "Math.random() * (maximum - minimum + 1)"", leading to an integer in the interval [0,99].
    
-3. So num returns an integer in the interval [0,100) + 1 which eventually means num return [0, 100]
+3. So num returns an integer in the interval [0,99] + 1 which eventually means num return [1, 100]
 4. Done!
 */
