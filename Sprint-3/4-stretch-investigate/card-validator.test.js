b53833fed2a978d1f352917bad0821b435920c30
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-23 22:24:04 +0100

    Add card-validator test file

---
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -0,0 +1,35 @@
+const cardValidator = require("./card-validator");
+
+test("card number has 16 digits" , () => {
+
+   const cardNumber = "3467854322366666"; 
+
+   const result = cardValidator(cardNumber);
+
+   expect(result).toBe(true);
+
+});
+
+test("card number contain numbers : NO letters allowed" , () => {
+    const cardNumber = "1234a77787h88833";
+    const result = cardValidator(cardNumber);
+    expect(result).toBe(false);
+});
+
+test("card number has the same number : invalid card digits" , () => {
+    const cardNumber = "1111111111111111";
+    const result = cardValidator(cardNumber);
+    expect(result).toBe(false);
+});
+
+test("card sum is less than 16 digits: invalid card digits" , () => {
+    const cardNumber ="1111111111111110";
+    const result = cardValidator(cardNumber);
+    expect(result).toBe(false);
+});
+
+test("card number ends with odd number: invalid card number" , () => {
+    const cardNumber = "2222222222222221";
+    const result = cardValidator(cardNumber);
+    expect(result).toBe(false);
+});
