a73f36189682e26ebbb46a976249b35f32711e50
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-23 21:25:47 +0100

    Implement the function and adding test cases

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,15 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
-
+test("should return false for a improper fraction", () => {
+  expect(isProperFraction(5, 3)).toEqual(false);
+});
 // Case 3: Identify Negative Fractions:
+test("should return false for a improper fraction", () => {
+  expect(isProperFraction(-5, 10)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for a improper fraction", () => {
+  expect(isProperFraction(10, 10)).toEqual(false);
+});
