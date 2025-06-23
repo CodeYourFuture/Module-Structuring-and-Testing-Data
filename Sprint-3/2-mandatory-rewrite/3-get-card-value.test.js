67c92c265392430dec3b5090a3fccf92e3ce24c7
Declan Williams <declanwilliams793@gmail.com>
2025-06-23 16:39:37 +0100

    Add tests for face cards (J, Q, K) in getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -18,5 +18,10 @@ test("should return the correct value for number cards between 2 and 9", () => {
     expect(getCardValue("10♥")).toEqual(10);
 });
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards (J, Q, K)", () => {
+    expect(getCardValue("J♠")).toEqual(10);
+    expect(getCardValue("Q♦")).toEqual(10);
+    expect(getCardValue("K♣")).toEqual(10);
+});
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
