56d3ab42459362af6edb19422b6fba4ad0921bf0
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-23 12:33:28 +0100

    updated_Sprint-1\2-mandatory-errors\4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,5 +1,5 @@
-const Hour12ClockTime = "20:53";
-const Hour24ClockTime = "08:53";
+const hour12clocktime = "20:53";
+const hour24clocktime = "08:53"; 
 
 const convertTo12HourFormat = (time) => {
   const [hours, minutes] = time.split(":").map(Number);
@@ -22,8 +22,8 @@ const convertTo24HourFormat = (time) => {
     .toString()
     .padStart(2, "0")}`;
 };
-console.log(`12-hour format: ${convertTo12HourFormat(Hour24ClockTime)}`); // 12-hour format: 08:53 AM
-console.log(`24-hour format: ${convertTo24HourFormat(Hour12ClockTime)}`); // 24-hour format: 20:53
+console.log(`12-hour format: ${convertTo12HourFormat(hour24clocktime)}`); // 12-hour format: 08:53 AM
+console.log(`24-hour format: ${convertTo24HourFormat(hour12clocktime)}`); // 24-hour format: 20:53
 // The code correctly converts between 12-hour and 24-hour formats
 // The function convertTo12HourFormat converts a 24-hour time string to a 12-hour format with AM/PM
 // The function convertTo24HourFormat converts a 12-hour time string with AM/PM to a 24-hour format
