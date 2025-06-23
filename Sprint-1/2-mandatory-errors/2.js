feaaac8cefdc83f2c0936df6913da8e2f4e17841
mirabellemorah <myrakuti@gmail.com>
2025-06-23 14:59:20 +0100

    Added answers to mandatory errors

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,13 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
+//console.log(`I was born in ${cityOfBirth}`);
+//const cityOfBirth = "Bolton";
+
+/*The above shows shows 'ReferenceError: Cannot access 'cityOfBirth' before initialization
+    at Object.' This happens, according to Mozilla "when a lexical variable was accessed before it was initialized."
+
+    To access it, it has to be flipped like this:  */
+
+    const cityOfBirth = "Bolton";
+    console.log(`I was born in ${cityOfBirth}`);
--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.slice(-4);
+
+/*Instructions */
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,17 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+/* My answers:
+
+1. My prediction on why the code isn't working is that it isn't being called or declared. Right now const stores the numbers but there is no console.log() to show the output
+
+2. After running it shows "TypeError: cardNumber.slice is not a function"
+
+3. Ah I see, my prediction was partially correct that it would not work but my reason and logic were wrong. What's different is that slice() only works on strings or arrays, not numbers. So to fix this, I. have to convert the number to a string first so I can slice it. See below a solution below:
+
+*/
+
+const cardNumber = 4533787178994213;
+const last4Digits = String(cardNumber).slice(-4);
+console.log(last4Digits); // expected to output "4213"
