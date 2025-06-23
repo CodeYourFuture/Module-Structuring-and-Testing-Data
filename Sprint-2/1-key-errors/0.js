4bd74dee3f475864ee5c32b606cde7344460b531
Farah-Stu <selma.salam45@gmail.com>
2025-06-23 23:47:30 +0100

    fixing the code's error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,23 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The instruction wants to define a function with the input (str)
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+// the error is in the use of let str. (str) is already an input and you can't use it again.
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  let str= `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
 // =============> write your explanation here
+//In order to fix the problem we need to declare let with capitalise instead of str. 
+// Because if we use the parameter (str), it creates a naming conflict in the same scope.
+  
+
 // =============> write your new code here
+function capitlise(str) {
+  let capitalise =`${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
