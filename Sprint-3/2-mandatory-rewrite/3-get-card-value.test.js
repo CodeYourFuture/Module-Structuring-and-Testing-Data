06e2a86a404f52c1115297b1ec1a116689714a91
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-23 22:36:36 +0100

    Implement the function and adding test cases

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,18 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return numeric value corresponding to the rank", () => {
+    const fiveofHearts = getCardValue("5♥"); 
+    expect(fiveofHearts).toEqual(5);
+})
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for J, Q, K, as these cards are worth 10 points each in blackjack.", () => {
+    const faceCards = getCardValue("J♠")
+    expect(faceCards).toEqual(10);
+})
 // Case 4: Handle Ace (A):
+test("should return 11 as Ace as worth 11 points", () => {
+    const aceCard = getCardValue("Ace♦")
+    expect(aceCard).toEqual(11);
+})
 // Case 5: Handle Invalid Cards:
