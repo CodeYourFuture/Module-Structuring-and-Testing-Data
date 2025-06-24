ce4fb74584b8194f7c516f2dba0dda6cbe831903
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-25 00:09:26 +0100

    update sprint-3\4\password-validator.js

--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -6,6 +6,7 @@ and returns a boolean
 
 To be valid, a password must:
 - Have at least 5 characters.
+
 - Have at least one English uppercase letter (A-Z)
 - Have at least one English lowercase letter (a-z)
 - Have at least one number (0-9)
@@ -14,7 +15,7 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
-const isValidPassword = require("./password-validator");
+/*const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
     // Arrange
     const password = "12345";
@@ -22,5 +23,60 @@ test("password has at least 5 characters", () => {
     const result = isValidPassword(password);
     // Assert
     expect(result).toEqual(true);
-}
-);
+});*/
+
+// Have at least 5 Character
+
+test("password has at least 5 characters", () => {
+    const password = "T2t!";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+//Have at least one English uppercase letter (A-Z)
+
+test("password has at least one uppercase letter", () => {
+    const password = "test@12";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+//Have at least one English lowercase letter (a-z)
+
+test("password has at least one lowercase letter", () => {
+    const password = "TEST@WE12";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+//Have at least one number (0-9)
+
+test("password has at least one number", () => {
+    const password = "TEst@x!";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+// Have at least one of the following non-alphanumeric symbols: ("!", "#", "$", "%", ".", "*", "&")
+
+test("password has at least one symbol", () => {
+    const password = "Abc12";
+    const result = isValidPassword(password);
+    expect(result).toEqual(false);
+});
+
+//Must not be any previous password in the passwords array. 
+
+test("password must not be used before", () => {
+    const password = "Test@123";
+    const previousPasswords = ["Test@123", "Test123!"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(false);
+});
+
+test("password must not be used before", () => {
+    const password = "New@Test@123!";
+    const previousPasswords = ["Test@123", "Test123!"];
+    const result = isValidPassword(password, previousPasswords);
+    expect(result).toEqual(true);
+});
