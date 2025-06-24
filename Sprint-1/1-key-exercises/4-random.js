53ce300efba7e8c63253edd9968d32bd1baa696c
SuWebOnes <surafealaw@gmail.com>
2025-06-24 12:12:18 +0100

    update: clarify comment on Math.random() range in 4-random.js

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -10,7 +10,7 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 console.log(num);
 // Output: A random number between 1 and 100 (inclusive both 1 and 100)
 //num is a random number generated between the minimum 1 and maximum values 100.
-// The expression Math.random() generates a random floating-point number between 0 (inclusive) and 1 (exclusive).
+// The expression Math.random() generates a random floating-point number between [0,1).
 // The expression (maximum - minimum + 1) calculates the range of numbers we want to generate, which is 100 - 1 + 1 = 100.
 // The expression Math.floor() rounds down the result to the nearest whole number.
 // Finally, we add the minimum value to ensure that the random number falls within the desired range.
