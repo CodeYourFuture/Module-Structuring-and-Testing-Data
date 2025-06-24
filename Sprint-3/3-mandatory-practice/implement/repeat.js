a6058f756b52d76fba6e08223308561847592a3e
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-24 19:13:53 +0100

    Implement 'repeat' function and adding test cases.

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,8 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count == 1) return `${str}`;
+  else if (count == 0) return "";
+  else if (count < 0) return "negative counts are not valid";
+  else return `${str.repeat(count)}`;
 }
 
 module.exports = repeat;
