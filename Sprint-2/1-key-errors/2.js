a821c8ab352b8a2f0d4f128e9b4b0484bdcd4934
Farah-Stu <selma.salam45@gmail.com>
2025-06-24 22:29:03 +0100

    updating key errors

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,29 @@
 
 // Predict and explain first BEFORE you run any code...
+//I think we should define number first before returning it.
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here.
+//number undefined.
+
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-
+//Unexpected number
 // =============> explain this error message here
-
+// It is not allowed to use numbers in a variable parameter. 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
 
+function square(number) {
+  const result = number * number;
+  return result; // ← semicolon instead of colon
+}
+
+console.log(square(3));
