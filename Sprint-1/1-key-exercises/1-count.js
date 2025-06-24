fbee6420755d215313d1c589fa8c4c6658a09905
SuWebOnes <surafealaw@gmail.com>
2025-06-24 12:14:51 +0100

    update: refine comment explanation for count variable reassignment in 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -5,4 +5,4 @@ count = count + 1;
 console.log(count); // Output: 1
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-//Line 3 is reassigning the count variable to a new value which is the current value plus 1, "=" is assigning operator that assigning value of right side which mens count plus 1 to the left side value of count variable
+//Line 3 is reassigning the count variable to a new value which is the current value increment by 1, "=" is assigning operator that assigning value of right side which mens count increment 1 to the left side value of count variable
