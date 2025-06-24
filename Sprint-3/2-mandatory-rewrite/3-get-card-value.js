de2a5dec023c83840a1a222cedf9c3bb0d71e0bf
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 18:24:48 +0100

    updated Sprint-3\2\3-get-card-value and test.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,17 @@
 function getCardValue(card) {
     // replace with your code from key-implement
-    return 11;
+   // return 11;
+    
+    if (card === 'A') {
+        return 11; // Ace can be 11 or 1, but we'll
+    } 
+    else if (card === 'K' || card === 'Q' || card === 'J') {
+        return 10; // Face cards are worth 10
+    } 
+    else {
+        return parseInt(card); // Number cards are worth their face value
+    }
+        
 }
-module.exports = getCardValue;
+console.log(getCardValue('K')); // Should return 11
+module.exports = getCardValue
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,41 @@
-const getCardValue = require("./3-get-card-value");
+ const getCardValue = require("./3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
+
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for 5 of Diamonds", () => {
+    const fiveOfDiamonds = getCardValue("5♦");
+    expect(fiveOfDiamonds).toEqual(5);
+});
+test("should return 10 for 10 of Diamonds", () => {
+    const tenOfDiamonds = getCardValue("10♦");
+    expect(tenOfDiamonds).toEqual(10);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Face Cards", () => {
+    const faceCards = getCardValue("K♣");
+    expect(faceCards).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Spades", () => {
+    const aceOfSpades = getCardValue("A♠");
+    expect(aceOfSpades).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should return 0 for invalid card '1♠'", () => {
+    const invalidCard = getCardValue("1♠"); 
+    expect(invalidCard).toEqual(0);
+});
+
+test("should return 0 for invalid input 'Z♠'", () => {
+    const invalidInput = getCardValue("Z♠"); 
+    expect(invalidInput).toEqual(0);
+});
+
+test("should return 0 for empty string", () => {
+    const emptyString = getCardValue(""); 
+    expect(emptyString).toEqual(0);
+}); 
