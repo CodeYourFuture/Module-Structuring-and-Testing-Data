e3ebe30832431127988cdba3a3232ad165e831b6
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 19:46:58 +0100

    update sprint-3\3\count.js and count.test.js

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -9,7 +9,6 @@ const countChar = require("./count");
 // And a character char that may occur multiple times with overlaps within str (e.g., 'a' in 'aaaaa'),
 // When the function is called with these inputs,
 // Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
-
 test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
   const char = "a";
@@ -21,4 +20,11 @@ test("should count multiple occurrences of a character", () => {
 // Given the input string str,
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
-// Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+// Then it should return 0.
+test("should return 0 if character does not occur", () => {
+  const str = "hello";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
