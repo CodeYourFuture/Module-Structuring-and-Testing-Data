6e882020abe0a23814e43950c094a90757e4c6ee
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 20:20:46 +0100

    update sprint-3\3\get-ordinal-number.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,23 @@
 function getOrdinalNumber(num) {
- return "1st";
+  const remainder10 = num % 10;
+  const remainder100 = num % 100;
+
+  if (remainder100 >= 11 && remainder100 <= 13) {
+    return `${num}th`;
+  }
+
+  switch (remainder10) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
+  }
 }
+
+console.log(getOrdinalNumber(105));
 
 module.exports = getOrdinalNumber;
