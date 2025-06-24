04360b449bf551bf6adfd5be3e112c37794852c5
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 23:20:50 +0100

    update sprint-3\4\find.js

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,21 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+
+// The index commences at 0 and is incremented by 1 on each iteration of the loop via the use of the index operator (++).
+
 // b) What is the if statement used to check
+
+// The function performs a comparison of the current character in the string (str[index]) with the character specified in the search parameter.
+
 // c) Why is index++ being used?
+
+// The purpose of this function is to increment the index, thereby enabling the loop to progress to the subsequent character.
+
+// In the absence of this mechanism, the loop would be perpetually engaged in the verification of the same character, resulting in an infinite loop.
+
 // d) What is the condition index < str.length used for?
+
+// This ensures that the loop will only run while index is a valid position in the string.
+
+// Prevents accessing characters outside the string bounds (which would be undefined).
