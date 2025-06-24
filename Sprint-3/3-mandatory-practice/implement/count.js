7e10cb8185a3f1287e24f1611a6e62e69d2eb2bb
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 22:29:01 +0100

    update sprint-3\3\count.js and count.test.js

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,17 @@
 function countChar(stringOfCharacters, findCharacter) {
-    return 5
+   //return 5
+   let count = 0;
+    for (let char of stringOfCharacters) {
+        if (char === findCharacter) {
+            count++;
+        }
+    }
+    return count;
 }
+
+console.log(countChar("hello world", "l")); // ➜ 3
+console.log(countChar("Mississippi", "s")); // ➜ 4
+console.log(countChar("banana", "n"));  
+console.log(countChar("", ""));
 
 module.exports = countChar;
