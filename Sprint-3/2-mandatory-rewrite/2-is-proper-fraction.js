796a8cf28940e1a7af0631a00f4246b9831c3447
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 13:49:24 +0100

    update sprint-3\2\2-is-proper-fraction.js and test

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,13 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
     // add your completed function from key-implement here
+    if (denominator === 0) return false; // Avoid division by zero
+    if (numerator === 0) return true; // Zero is considered a proper fraction
+    if (numerator < 0 || denominator < 0) return false; // Negative fractions are not proper
+    return Math.abs(numerator) < Math.abs(denominator);
 }
+
+console.log(isProperFraction(0, 1));
+console.log(isProperFraction(5,3));
 
 module.exports = isProperFraction;
--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -2,10 +2,28 @@ const isProperFraction = require("./2-is-proper-fraction");
 
 test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
+  expect(isProperFraction(1, 2)).toEqual(true);
 });
 
 // Case 2: Identify Improper Fractions:
 
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 3)).toEqual(false);
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
+
 // Case 3: Identify Negative Fractions:
 
+ test("should return false for negative fractions", () => {
+  expect(isProperFraction(-2, 3)).toEqual(false);
+  expect(isProperFraction(2, -3)).toEqual(false);
+  expect(isProperFraction(-2, -3)).toEqual(false);
+});
+
 // Case 4: Identify Equal Numerator and Denominator:
+
+test("should return false when numerator and denominator are equal", () => {
+  expect(isProperFraction(4, 4)).toEqual(false);
+});
+
+
