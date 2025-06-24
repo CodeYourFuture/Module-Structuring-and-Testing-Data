c8ed1bb31b74407a92a93db3506deb6cb8c02397
iTeddy16 <tewodros.bekere16@gmail.com>
2025-06-24 22:53:58 +0100

    update Sprint-3\3\repeat.js and repeat.test.js

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,12 @@
-function repeat() {
-    return "hellohellohello";
+//function repeat() {
+    //return "hellohellohello";
+function repeat(str = "hello", times = 3) {
+    return str.repeat(times);
 }
+
+console.log(repeat());               
+console.log(repeat("test", 1));
+console.log(repeat("bye", 2));       
+console.log(repeat("ha", 5));   
 
 module.exports = repeat;
