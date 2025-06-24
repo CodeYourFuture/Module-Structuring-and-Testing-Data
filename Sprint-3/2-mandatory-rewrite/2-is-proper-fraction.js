594c884edc289c2f4b4aa13095435702601b1e99
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-24 19:41:01 +0100

    Sprint-3 | Completed isProperFraction with input validation

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,10 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+    // A proper fraction is when absolute numerator is less than absolute denominator
+    if (typeof numerator !== "number" || typeof denominator !== "number" || denominator === 0) {
+        throw new Error("Invalid input");
+    }
+
+    return Math.abs(numerator) < Math.abs(denominator);
 }
 
 module.exports = isProperFraction;
