657aca62058015d3106247f0a1b41b80a42f956b
[Halimatou Waghe] <[waghehalimatou@gmail.com]>
2025-06-24 11:35:26 +0100

    Implement: is proper fraction

--- Sprint-3/1-key-implement/2-is-proper-fraction.js
+++ Sprint-3/1-key-implement/2-is-proper-fraction.js
@@ -8,7 +8,11 @@
 // write one test at a time, and make it pass, build your solution up methodically
 
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
+  if (numerator < denominator) return true;
+  if (numerator >= denominator) return false;
+  if (numerator < 0 && Math.abs(numerator) < denominator) return true;
+  if (numerator === denominator) return false;
+  if (numerator < 0 && Math.abs(numerator) >= denominator) return false;
 }
 
 // here's our helper again
@@ -41,6 +45,7 @@ assertEquals(improperFraction, false);
 // Explanation: The fraction -4/7 is a proper fraction because the absolute value of the numerator (4) is less than the denominator (7). The function should return true.
 const negativeFraction = isProperFraction(-4, 7);
 // ====> complete with your assertion
+assertEquals(negativeFraction, true);
 
 // Equal Numerator and Denominator check:
 // Input: numerator = 3, denominator = 3
@@ -48,6 +53,15 @@ const negativeFraction = isProperFraction(-4, 7);
 // Explanation: The fraction 3/3 is not a proper fraction because the numerator is equal to the denominator. The function should return false.
 const equalFraction = isProperFraction(3, 3);
 // ====> complete with your assertion
+assertEquals(equalFraction, false);
 
 // Stretch:
 // What other scenarios could you test for?
+
+// Negative Improper Fraction check:
+// Input: numerator = -6, denominator = 4
+// target output: false
+// Explanation: The fraction -6/4 is not a proper fraction because the absolute value of the
+// numerator (6) is greater than the denominator (4). The function should return false.
+const negativeImproperFraction = isProperFraction(-6, 4);
+assertEquals(negativeImproperFraction, false);
