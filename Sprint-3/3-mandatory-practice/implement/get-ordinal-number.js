89f20a4bba8372c95e2ab7691faf6d970ea42099
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-24 20:21:09 +0100

    Implement getOrdinalNumber function to return correct ordinal strings

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,15 @@
 function getOrdinalNumber(num) {
- return "1st";
+  const suffixes = ["th", "st", "nd", "rd"];
+  const v = num % 100;
+  if (v >= 11 && v <= 13) {
+    return num + "th";
+  }
+  switch (num % 10) {
+    case 1: return num + "st";
+    case 2: return num + "nd";
+    case 3: return num + "rd";
+    default: return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
