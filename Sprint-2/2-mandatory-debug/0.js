46e79332e1e5d6064ff38821725dd0a35e43cc1e
nurzat <nurzatcode@gmail.com>
2025-06-25 21:14:27 +0100

    fix the undefined value printed to the output

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,9 +1,15 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+// What I predict is that the console.log which is outside the function will pass the parameters 10 and 32 to the function
+// Then the console.log(a * b) which is inside the function will print out 
+ // to the terminal the multiplication of a by b
 function multiply(a, b) {
-  console.log(a * b);
+  const multiplicationResult =  (a * b);
+  return multiplicationResult;
+
+
+  
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
