8ccf3b2784953df70786d1cc3b0aa3be6d949891
Kaligrey <eminakturk07@gmail.com>
2025-06-25 15:28:57 +0100

    Mandatory-errors part is finished.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+//Code was correct, I just shifted the const part to the top of the code block.
