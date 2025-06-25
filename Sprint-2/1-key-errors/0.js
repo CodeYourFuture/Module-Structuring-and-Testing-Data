bce7a36a60e87da56f951536218cc410d4717d14
nurzat <nurzatcode@gmail.com>
2025-06-25 20:32:10 +0100

    Fix variable redeclaration error  in capitalize function

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// The code will throw an error because the variable `str` is being declared twice in the same scope.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+capitalise("hello,this is a test");
+
+//
 
 // =============> write your explanation here
 // =============> write your new code here
+//// The error occurs because the variable `str`is declared twice in the same scope,which is not allowed in JavaScript.
