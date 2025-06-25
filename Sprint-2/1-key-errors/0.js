3ddd8e9daf6abd9f15ba75b2efacf52f8cc762d5
mirabellemorah <myrakuti@gmail.com>
2025-06-25 14:51:23 +0100

    Written answers to "Key Errors"

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+/* 💡Answer: To be honest, I get confused when I see template literals and understand the older version of writing JS better, using operators, but I am dedicated to trying my best in this. I see a lot of the use of 'str' in this and I suspect this is almost correct but may fail because of the variable name 'str' used one too many times, as a parameter and a variable. I know that slice deletes parts of a string and 'toUpperCase' could be to capitalise things. */
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/* function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+  */
 
-// =============> write your explanation here
+// =============> What I get is a "SyntaxError: Identifier 'str' has already been declared". This means that in line 10 of my code, I have declared the variable 'str' twice, once as a parameter and once as a variable. This is not allowed in JavaScript, as each variable must have a unique name within its scope. To fix this, I need to remove the 'let' keyword from the second declaration of 'str', so that I am simply reassigning the value to the existing parameter instead of trying to declare it again.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  str = str[0].toUpperCase() + str.slice(1);
+  return str;
+}
+
+console.log(capitalise("wheew! Done it without template literals :)")); // Should print "Wheew! Done it without template literals :)""
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,23 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> 💡Answer: Mhmm there seems to be an error in line 8 because you can't use a number/integer alone as the start name of a parameter. It should be more of a name or string or add the number at the end of the value/name
 
-function square(3) {
+/* function square(3) {
     return num * num;
 }
+    */
 
-// =============> write the error message here
+// =============> write the error message here: "SyntaxError: Unexpected number" and "ReferenceError: num is not defined"
 
-// =============> explain this error message here
+// =============> explain this error message here: Using a number alone is not a valid identifier name in JavaScript. Identifiers must start with a letter, underscore (_), or dollar sign ($). Also , the variable `num` is not defined anywhere in the function, which leads to a ReferenceError when trying to use it.
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+// =============>
+function square(Now3) {
+  return Now3 * Now3;
+}
 
-
+console.log(square(3)); // Output: 9
