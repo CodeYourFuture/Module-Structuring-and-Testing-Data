783668e0da5e7f79eca054773bdad4fad294005a
sedazam <sedazam@gmail.com>
2025-06-25 17:29:53 +0100

    modified:   Sprint-3/1-key-implement/1-get-angle-type.js

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -64,3 +64,27 @@ assertEquals(straight, "Straight angle");
 const reflex = getAngleType(270);
 assertEquals(reflex, "Reflex angle");
 // ====> write your test here, and then add a line to pass the test in the function above
+
+// [Running] node "/Users/seddiqazam/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-3/1-key-implement/tempCodeRunnerFile.js"
+// /Users/seddiqazam/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-3/1-key-implement/tempCodeRunnerFile.js:1
+// node 1-get-angle-type.js
+//      ^
+
+// SyntaxError: Unexpected number
+//     at wrapSafe (node:internal/modules/cjs/loader:1662:18)
+//     at Module._compile (node:internal/modules/cjs/loader:1704:20)
+//    at Object..js (node:internal/modules/cjs/loader:1895:10)
+//    at Module.load (node:internal/modules/cjs/loader:1465:32)
+//    at Function._load (node:internal/modules/cjs/loader:1282:12)
+//    at TracingChannel.traceSync (node:diagnostics_channel:322:14)
+//    at wrapModuleLoad (node:internal/modules/cjs/loader:235:24)
+//    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:171:5)
+//    at node:internal/main/run_main_module:36:49
+//
+// Node.js v22.16.0
+//
+//[Done] exited with code=1 in 0.051 seconds
+
+//[Running] node "/Users/seddiqazam/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-3/1-key-implement/1-get-angle-type.js"
+
+// [Done] exited with code=0 in 0.05 seconds
