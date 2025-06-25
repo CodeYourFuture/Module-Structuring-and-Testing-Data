644c8fa9d18ddff04a6c3bbb32e90645ce63aed5
Hendrine Zeraua <puumotu@hotmail.com>
2025-06-25 09:18:53 +0100

    Fix function parameter redeclaration error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+// - The code will throw a SyntaxError due to variable redeclaration.
 // call the function capitalise with a string input
+// - The function provided contains a syntax error because it attempts to redeclare
+// - the parameter str within the function using let, which is not permitted
+// - in JavaScript. Below is the corrected version of the function, followed by an example of how it can be called.
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
@@ -10,4 +13,19 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+// - When code run, this "SyntaxError: Identifier 'str' has
+// - already been declared" will show
+// - In the function capitalise(str), the parameter str is already declared.
+// - But inside the function, you're trying to declare a new variable
+// - with the same name using let str = ....
+// - JavaScript doesn't allow declaring a new variable with the
+//  same name as a function parameter using let or const within the same scope.
 // =============> write your new code here
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+  capitalise(hello);
+  console.log(capitalise('hello'));
+  // I used "hello" as an example input when
+  //  calling the capitalise function:
+  // Output: 'Hello'
