af03c01488fe55b7f30b100c0ca9e1c43f290549
Hilolarustam <mohlaroy86@gmail.com>
2025-06-28 00:06:21 +0100

    fixed the sum problem.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -9,5 +9,13 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+//The return; statement ends the function immediately.
+//The next line (a + b;) is never executed because it's after the return and on a new line.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
