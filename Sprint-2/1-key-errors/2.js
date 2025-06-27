4af13ebe20c72a11c289c68cbad4dfd852e6d391
Hilolarustam <mohlaroy86@gmail.com>
2025-06-27 23:35:11 +0100

    function square fixed

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,24 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here 
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+Function square(3)
 
 // =============> explain this error message here
-
+// 3 is not approprate parametr name.
+// num is not defined, 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+ function square(num)
+ return num*num 
+
+ console.log(square(3));
 
 
