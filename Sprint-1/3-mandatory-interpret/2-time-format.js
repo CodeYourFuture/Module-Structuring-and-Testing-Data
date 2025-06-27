bba94cd12339fe921a17045a87af2ca840821664
Mirabel Adom <mirabel.adom@gmail.com>
2025-06-28 00:05:15 +0100

    Updated time format

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -23,3 +23,8 @@ console.log(result);
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// I tested the code with values like 1, 45, 75, 1800, and 10000. The results were accurate for all of them.
+// For example, 75 returned 0:1:15, and 1800 returned 0:30:0, which are correct.
+// I also tried using -5000, and the output was -1:-23:-20, which doesn’t make sense for a time format.
+// This shows the code does not handle negative numbers correctly.
+// In general, it works well with positive whole numbers
