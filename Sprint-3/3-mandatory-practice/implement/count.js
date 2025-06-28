1036e1ee359517c7195a72410f7d518469526314
Sisu860 <meharisisay6@gmail.com>
2025-06-28 14:31:44 +0100

    corrected the test result

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,10 +1,14 @@
 function countChar(stringOfCharacters, findCharacter) {
-    // This method works by splitting the string by the `findCharacter`.
-    // The number of resulting substrings will be one more than the number
-    // of times the `findCharacter` appeared.
-    // Example: "banana".split('a') results in ["b", "n", "n", ""], which has length 4.
-    // The character 'a' appeared 3 times. So, length - 1 gives the count.
-    return stringOfCharacters.split(findCharacter).length - 1;
+  // This method works by splitting the string by the `findCharacter`.
+  // The number of resulting substrings will be one more than the number
+  // of times the `findCharacter` appeared.
+  // Example: "banana".split('a') results in ["b", "n", "n", ""], which has length 4.
+  // The character 'a' appeared 3 times. So, length - 1 gives the count.
+  if (findCharacter.length === 0) {
+    return stringOfCharacters.length;
+  }
+
+  return stringOfCharacters.split(findCharacter).length - 1;
 }
 
 module.exports = countChar;
