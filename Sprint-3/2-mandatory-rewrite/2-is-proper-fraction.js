5bd333efe49e94ed708339eaafdefe00cacd1ce9
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-28 14:38:08 +0100

    simplified the conditions of my function

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,9 +1,9 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator && (numerator/denominator) > 0) return true;
-    if (numerator > denominator) return false;
-    if (numerator < denominator && (numerator/denominator) < 0) return "Negative Fraction";
-    if (numerator === denominator ) return "Not really a fraction";
-    
+    if (numerator < denominator) {
+        return true;
+    } else {
+        return false;
+    }    
 }
 
 module.exports = isProperFraction;
