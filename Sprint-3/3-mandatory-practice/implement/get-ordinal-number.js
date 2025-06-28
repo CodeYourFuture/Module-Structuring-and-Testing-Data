ce039c57f59057a1d0bf1d797889bb79339944ca
sedazam <sedazam@gmail.com>
2025-06-28 06:51:03 +0100

    modified:   Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js 	modified:   Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,28 @@
 function getOrdinalNumber(num) {
- return "1st";
+  if (typeof num !== "number") {
+    return undefined; // Return undefined if the input is not a number
+  }
+  let result;
+  if (num === 1) {
+    return "1st";
+  } else if (num === 2) {
+    return "2nd";
+  } else if (num === 3) {
+    return "3rd";
+  } else if (num >= 4 && num <= 20) {
+    return num + "th";
+  } else if (num % 10 === 1) {
+    // % gives the last digit of the number
+    return num + "st";
+  } else if (num % 10 === 2) {
+    return num + "nd";
+  } else if (num % 10 === 3) {
+    return num + "rd";
+  }
+  const s = ["th", "st", "nd", "rd"];
+  const v = num % 100; // Get the last two digits of the number
+  result = num + (s[(v - 20) % 10] || s[v] || s[0]); // Determine the correct suffix based on the last two digits
+  return result;
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -9,5 +9,37 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Then the function should return "1st"
 
 test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+  expect(getOrdinalNumber(1)).toEqual("1st");
+});
+
+// Case 2: Identify the ordinal number for 2
+// When the number is 2,
+// Then the function should return "2nd"
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+// Case 3: Identify the ordinal number for 3
+// When the number is 3,
+// Then the function should return "3rd"
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+
+// Case 4: Identify the ordinal number for numbers between 4 and 20
+// When the number is between 4 and 20 (inclusive),
+// Then the function should return the number with "th" appended
+test("should return '4th' for 4", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+});
+
+test("should return '20th' for 20", () => {
+  expect(getOrdinalNumber(20)).toEqual("20th");
+});
+
+// Case 5: Identify the ordinal number for numbers greater than 20
+// When the number is greater than 20,
+// Then the function should return the number with "st", "nd", "rd", or "th" appended based on the last digit
+test("should return '21st' for 21", () => {
+  expect(getOrdinalNumber(21)).toEqual("21st");
+});
