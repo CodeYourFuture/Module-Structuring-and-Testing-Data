68ec82a9738300650872e8e7797f323a2450e3bb
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-28 13:07:54 +0100

    updated function to include more edge cases

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,12 +4,15 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
+  const mins = Number(time.slice(3));
   if ( 24 > hours && hours > 12) {
-    return `${hours - 12}:00 pm`;      
+    return `${hours - 12}:${mins} pm`;      
   } else if(hours === 00 || hours === 24) {     // Lines 9-10 added to tackle edge cases of when dealing with "00.00" or "24:00" as function inputs.
-    return "12:00 am";
+    return `12:${mins} am`;
+  } else if (hours === 12) {
+    return `12:${mins} pm`;
   } else {
-  return `${time} am`;
+  return `${hours}:${mins} am`;
 }
 }
 
@@ -30,6 +33,28 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("00:59");
+const targetOutput3 = "12:59 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("12:59");
+const targetOutput4 = "12:59 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+
+const currentOutput5 = formatAs12HourClock("23:59");
+const targetOutput5 = "11:59 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
 );
 
 
