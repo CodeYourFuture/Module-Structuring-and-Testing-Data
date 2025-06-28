1472a6e2023ca5d57996f40f37157229499c435e
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-28 14:18:00 +0100

    changed to make angles <=0 evaluates to "invalid"

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,6 +1,6 @@
 function getAngleType(angle) {
   if (angle === 90) return "Right angle";
-  if (angle < 90) return "Acute angle";
+  if (0 < angle && angle < 90) return "Acute angle";
   if (angle > 90 && angle < 180) return "Obtuse angle";
   if (angle === 180) return "Straight angle";
   if (angle > 180 && angle < 360) return "Reflex angle";
