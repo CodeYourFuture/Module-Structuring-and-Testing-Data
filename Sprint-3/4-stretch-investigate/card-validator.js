8305998e8a8d3f97266538259cd12491e40a7c3e
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-28 14:50:37 +0100

    Implement a function to  create a 16 digit number. Add test cases to verify code

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,38 +1,56 @@
 function cardNumberValidator(cardNumber) {
-    let numberInArray = cardNumber.toString().split(''); // put the card number in array.
-    // return numberInArray;
-    // let lengthOfCard = cardNumber.toString().length; // get the length of the array but first put the card number in string
-    
-    if (numberInArray.length !== 16) {
-        return false;
+  if (cardNumber.length !== 16) {
+    return false;
+  }
+  const numberInArray = cardNumber.toString().split(""); // convert the cardNumber to sting then into an array
+  // check if all numbers are numbers
+  let isAllNumbers = true;
+  for (let index = 0; index <= numberInArray.length - 1; index++) {
+    const currentNumber = numberInArray[index];
+    if (isNaN(currentNumber)) {
+      isAllNumbers = false;
+      break;
     }
-    if (Number.isNaN(Number(cardNumber))) {
-        return false;
+  }
+  if (!isAllNumbers) {
+    return false; // Not all digits are numbers
+  }
+  // check if at least two different digits present (all of the digits cannot be the same)
+  let hasDifferentDigits = false;
+  for (let i = 1; i < numberInArray.length; i++) {
+    if (numberInArray[i] !== numberInArray[0]) {
+      hasDifferentDigits = true;
+      break;
+    }
+  }
+  if (!hasDifferentDigits) {
+    return false; // all digits are the same
+  }
+  // check if sum of all the digits are greater than 16.
+    let sum = 0;
+    for (let i = 0; i < numberInArray.length; i++) {
+      sum += Number(numberInArray[i]);
+    }
+    if (sum <= 16) {
+      return false; // the sum is not greater than 16
+    }
+    // check if the final digit is even.
+    let lastDigit = numberInArray[numberInArray.length - 1] % 2;
+    if (lastDigit !== 0) {
+      return false;
     }
     return true;
-        
-    // Check 2: At least two different digits
-    // let firstDigit = numberInArray[0]; 
-    // for (let i = 1; i < lengthOfCard; i++ ) { // compare the value
-    //     if (numberInArray[i] !== firstDigit) {
-    //         return true;
-    //     }
-    //     else return false
-    // }
-    // return false;
-    
 }
 
-console.log(cardNumberValidator("111111a111111111"));
+console.log(cardNumberValidator("111111111111111a"));
 module.exports = cardNumberValidator;
 
-// console.log(cardNumberValidator("1234567890123456"))
 /*
 pseudocode:
 create a function to validate the card number.
 Number must be 16 digits, 
 All numbers must be numbers.
 You must have at least two different digits represented (all of the digits cannot be the same).
-The final digit must be even.
 The sum of all the digits must be greater than 16.
+The final digit must be even.
 */
--- Sprint-3/4-stretch-investigate/card-validator.test.js
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -7,15 +7,26 @@ const cardNumberValidator = require("./card-validator");
 
 // case 1: check the card number has 16 digits
 test("card number should be 16 digits", () => {
-    expect(cardNumberValidator(1234567890123456)).toEqual(true);
+    expect(cardNumberValidator(12345678901234560)).toEqual(false);
 });
 
 // case 2: check all digits of the card number has to be numbers.
 test("card number should contains only numeric digits", () => {
-    expect(cardNumberValidator(a1111111b1111111)).toEqual(false);
+    expect(cardNumberValidator("a1111111b1111111")).toEqual(false);
 });
 
 // case 3: check the card number has at least 2 different digits
-// test("card number should be 16 digits", () => {
-//     expect(cardNumberValidator("1111111111111111")).toEqual(false);
-// });
+test("card number should have at least 2 different digits", () => {
+    expect(cardNumberValidator("1111111111111111")).toEqual(false);
+});
+
+// case 4: check the card number has at least 2 different digits
+test("The sum of all digits should be grater than 16", () => {
+    expect(cardNumberValidator("0000000000000022")).toEqual(false);
+});
+
+// case 5: check the card number has at least 2 different digits
+test("The final digits should be even", () => {
+    expect(cardNumberValidator("1234567891234561")).toEqual(false);
+});
+
