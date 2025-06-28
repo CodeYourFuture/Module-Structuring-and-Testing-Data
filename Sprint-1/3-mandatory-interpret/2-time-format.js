fea47a8c4449e50844d6e6bd392f5e7a5f2272a2
Abayie <jonathanboahene@gmail.com>
2025-06-29 00:01:52 +0100

    2-time-format.js

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -23,3 +23,48 @@ console.log(result);
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+
+/* SOLUTION
+
+a) **How many variable declarations are there in this program?**  
+There are **5** variable declarations: `movieLength`, `remainingSeconds`, `totalMinutes`, `remainingMinutes`, `totalHours`, and `result`.
+
+b) **How many function calls are there?**  
+- The template literal uses `${...}` expressions, but these are not function calls.  
+- The only function call is `console.log(result);`.  
+So, there is **1** function call.
+
+c) **Using documentation, explain what the expression `movieLength % 60` represents**  
+The `%` operator returns the remainder after dividing `movieLength` by `60`. In this context, it gives the number of seconds left after converting as many full minutes 
+as possible from the total seconds.
+
+d) **Interpret line 4, what does the expression assigned to `totalMinutes` mean?**  
+`(movieLength - remainingSeconds) / 60` subtracts the leftover seconds from the total, then divides by 60 to get the total number of complete minutes in the movie.
+
+e) **What do you think the variable `result` represents? Can you think of a better name for this variable?**  
+`result` represents the movie length formatted as `hours:minutes:seconds`. A better name could be `formattedTime` or `movieLengthHMS`.
+
+f) **Try experimenting with different values of `movieLength`. Will this code work for all values of `movieLength`? Explain your answer**  
+The code works for positive integer values of `movieLength`. However, for values less than 60, `totalHours` and `remainingMinutes` will be `0`, which is correct. 
+For negative or non-integer values, the output may not make sense. Also, the output does not pad single-digit minutes or seconds with a leading zero (e.g., `1:2:3` instead of `01:02:03`). 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
