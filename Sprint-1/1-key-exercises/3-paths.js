cbac6c9d409ae1aefe7f6139b879928204a4746f
pathywang <pathywang@gmail.com>
2025-06-28 13:09:35 +0100

    initial message

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -16,8 +16,11 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+// Get everthing before base
+const dir = filePath.slice(0,lastSlashIndex);
 
-const dir = ;
+//Get everything after the last dot in the base
+const lastDotIndex = base.lastIndexOf(".")
 const ext = ;
 
 // https://www.google.com/search?q=slice+mdn
