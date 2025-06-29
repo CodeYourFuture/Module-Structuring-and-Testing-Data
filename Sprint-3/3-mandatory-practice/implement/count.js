489b22f441b2fdef96600371e40bd0e3bd1ec862
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-06-30 00:01:10 +0100

    simplified my function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,10 +1,5 @@
 function countChar(str, char) {
-    if (str.includes(char) === true) {
         return str.split(char).length -1;
-    } else {
-        return 0;
-    }
-    
 }
 
 module.exports = countChar;
