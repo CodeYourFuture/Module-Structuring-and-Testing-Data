a5d88d4f8d5171763592b05202687577d0c3d7f1
Waleed Yahya <waleedbluejay7@gmail.com>
2025-06-30 03:49:12 +0100

    1. got filename and the directory plus the file extention.

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -9,15 +9,28 @@
 
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
-const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
-const lastSlashIndex = filePath.lastIndexOf("/");
-const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
+// const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
+// const lastSlashIndex = filePath.lastIndexOf("/");
+// const base = filePath.slice(lastSlashIndex + 1);
+// console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 
-const dir = ;
-const ext = ;
+// Get base (file name)
+const lastSlashIndex = filePath.lastIndexOf("/");
+const base = filePath.slice(lastSlashIndex + 1);
+
+// Get dir (directory path)
+const dir = filePath.slice(0, lastSlashIndex);
+
+// Get ext (file extension)
+const lastDotIndex = base.lastIndexOf(".");
+const ext = base.slice(lastDotIndex);
+
+console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
