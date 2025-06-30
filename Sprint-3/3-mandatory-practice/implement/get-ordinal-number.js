9c6607c586e884e498710be4d18e3a2ce8fc8913
Farah-Stu <selma.salam45@gmail.com>
2025-07-01 00:06:55 +0100

    updating ordinal num file

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,25 @@
 function getOrdinalNumber(num) {
- return "1st";
+    const strNum= num.toString();
+    if(strNum.slice(-2)==="11") {
+        return strNum + "th";
+    }
+if(strNum.slice(-2)==="12") {
+        return strNum + "th";
+    }   
+if(strNum.slice(-2)==="13") {
+        return strNum + "th";
+    }
+if(strNum.slice(-1)==="1") {
+        return strNum + "st";
+    }
+if(strNum.slice(-1)==="2") {
+        return strNum + "nd";
+    }
+if(strNum.slice(-1)==="3") {
+        return strNum + "rd";
+    }
+    
 }
+
 
 module.exports = getOrdinalNumber;
