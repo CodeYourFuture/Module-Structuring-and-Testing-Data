0d5917b0a79f06baca92133258452e9251769a29
pathywang <pathywang@gmail.com>
2025-06-30 17:55:08 +0100

    correct error

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,14 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//slice() only works for string or Array,cardNumber is number so i predict that the computer will give TypeError. When i 
+//run this Code, my prediction is right. In order to fix it, first cast to string first:
+
+const cardNumber=4533787178994213;
+const last4Digits = cardNumber.toString.slice(-4);
+
+//then conver to number
+const last4Digits = Number(cardNumber.toString.slice(-4));
+console log(last4Digits);
+
