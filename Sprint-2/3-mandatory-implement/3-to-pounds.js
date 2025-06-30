7a09127aba2f2051c83d0359a531842b3cae5990
Ahmad <ahmadehsas14345@gmail.com>
2025-06-30 03:27:58 +0100

    i turned the code into reusable block of code and i called the function many times for different inputs.

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,15 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+  const noP = penceString.slice(0, -1);
+  const padded = noP.padStart(3, "0");
+  const pounds = padded.slice(0, -2);
+  const pence = padded.slice(-2).padEnd(2, "0");
+  return `£${pounds}.${pence}`;
+}
+console.log(toPounds("5p")); // £0.05
+console.log(toPounds("70p")); // £0.70
+console.log(toPounds("399p")); // £3.99
+console.log(toPounds("1000p")); // £10.00
