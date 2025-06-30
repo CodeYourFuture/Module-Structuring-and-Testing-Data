401a0d1ee087df21bcb9271e2f317dececf522f2
Farah-Stu <selma.salam45@gmail.com>
2025-07-01 00:51:22 +0100

    update repeat test file

--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -3,11 +3,18 @@ const repeat = require("./repeat");
 // Given a target string str and a positive integer count,
 // When the repeat function is called with these inputs,
 // Then it should:
+test("should return positive integer count", () => {
+    const str = "hello";
+    const count = 3;
+    const repeatedStr=repeat(str,count);
+    expect(repeatedStr).toEqual("hellohellohello");
+});
 
 // case: repeat String:
 // Given a target string str and a positive integer count,
 // When the repeat function is called with these inputs,
 // Then it should repeat the str count times and return a new string containing the repeated str values.
+
 
 test("should repeat the string count times", () => {
     const str = "hello";
@@ -20,13 +27,31 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
+test("should return a target string when count equals 1", () => {
+    const str = "hello";
+    const count = 1;
+    const repeatedStr = repeat(str, count);
+    expect(repeatedStr).toEqual("hello");
+    });
 
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
+test("should return target string when count equals 0", () => {
+    const str = "hello";
+    const count = 0;
+    const repeatedStr = repeat(str, count);
+    expect(repeatedStr).toEqual("");
+});
 
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+test("should throw an error as negative integer counts are not valid", () => {
+    const str= "hello";
+    const count= -3;
+    expect(() => repeat(str, count)).toThrow("throw an error as negative counts are not valid");
+}); 
+
