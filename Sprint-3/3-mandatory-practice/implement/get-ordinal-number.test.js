da6f74fe569a5fc7a2050dcb51509ddb31763dae
Farah-Stu <selma.salam45@gmail.com>
2025-07-01 00:07:11 +0100

    update ordinal num file test

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -11,3 +11,15 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
     });
+
+    test("should return '2nd' for 2", () => {
+        expect(getOrdinalNumber(2)).toEqual("2nd");
+    });
+
+    test("should return '23rd' for 23", () => {
+        expect(getOrdinalNumber(23)).toEqual("23rd");
+    });
+
+    test("should return '11th' for 11", () => {
+        expect(getOrdinalNumber(11)).toEqual("11th");
+    });     
