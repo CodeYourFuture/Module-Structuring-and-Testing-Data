43cff66cd54c7b4213a903659dec636a5ad091e3
pathywang <pathywang@gmail.com>
2025-07-01 18:35:00 +0100

    explain error

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> console.log(a*b) is inside the function, so it does not return anything instead return undefined by default
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,16 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> When we call multiply(10, 32) inside the template string, it first runs console.log(320) inside the function.
+// Then it tries to insert the function’s return value (which is undefined) into the string.
+// So, we’ll see:csharp
+//               Copy
+//               Edit
+//               320
+//              The result of multiplying 10 and 32 is undefined
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> my new code:
+function multiply(a,b) {return(a*b)};
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
