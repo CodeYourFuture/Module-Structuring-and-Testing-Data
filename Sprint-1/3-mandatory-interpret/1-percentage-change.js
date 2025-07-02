b5fd505aba7a28452b6b4eea24b20bb763099166
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-02 03:03:15 +0100

    saved the changes

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -1,13 +1,13 @@
-let carPrice = "10,000";
-let priceAfterOneYear = "8,543";
+// let carPrice = "10,000";
+// let priceAfterOneYear = "8,543";
 
-carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
 
-const priceDifference = carPrice - priceAfterOneYear;
-const percentageChange = (priceDifference / carPrice) * 100;
+// const priceDifference = carPrice - priceAfterOneYear;
+// const percentageChange = (priceDifference / carPrice) * 100;
 
-console.log(`The percentage change is ${percentageChange}`);
+// console.log(`The percentage change is ${percentageChange}`);
 
 // Read the code and then answer the questions below
 
