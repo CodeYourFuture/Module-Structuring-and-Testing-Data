0d2e786a24ae7b86196f06f3db32ae3d7c61409c
avatarit <salah@ait.ps>
2025-07-02 20:22:04 +0100

    Refactor getAngleType function to streamline angle classification logic

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -4,15 +4,12 @@ function getAngleType(angle) {
   if (angle > 90 && angle < 180) return "Obtuse angle";
   if (angle === 180) return "Straight angle";
   if (angle > 180 && angle < 360) return "Reflex angle";
+  if (angle === 360) return "Full rotation";
+  return "Invalid angle"; 
   // replace with your completed function from key-implement
 
 }
 module.exports = getAngleType;
-
-
-
-
-
 
 
 
