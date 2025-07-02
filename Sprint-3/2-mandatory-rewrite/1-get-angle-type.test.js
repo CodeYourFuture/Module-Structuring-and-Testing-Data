c693e17b3f4d3a948ed7eb2a799163dc319be7c9
avatarit <salah@ait.ps>
2025-07-03 00:42:52 +0100

    Refactor angle type tests for clarity by removing redundant comments

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -8,38 +8,42 @@ test("should identify right angle (90°)", () => {
 // make your test descriptions as clear and readable as possible
 
 // Case 2: Identify Acute Angles:
+// When the angle is less than 90 degrees,
+// Then the function should return "Acute angle"
 test("should identify acute angles (less than 90°)", () => {
   expect(getAngleType(45)).toEqual("Acute angle");
   expect(getAngleType(1)).toEqual("Acute angle");
   expect(getAngleType(89)).toEqual("Acute angle");
 });
 
-// When the angle is less than 90 degrees,
-// Then the function should return "Acute angle"
+
 
 // Case 3: Identify Obtuse Angles:
+// When the angle is greater than 90 degrees and less than 180 degrees,
+// Then the function should return "Obtuse angle"
 test("should identify obtuse angles (between 90° and 180°)", () => {
   expect(getAngleType(120)).toEqual("Obtuse angle");
   expect(getAngleType(91)).toEqual("Obtuse angle");
   expect(getAngleType(179)).toEqual("Obtuse angle");
 });
-// When the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle"
+
 
 // Case 4: Identify Straight Angles:
+// When the angle is exactly 180 degrees,
+// Then the function should return "Straight angle"
 test("should identify straight angle (180°)", () => {
   expect(getAngleType(180)).toEqual("Straight angle");
 });
 
-// When the angle is exactly 180 degrees,
-// Then the function should return "Straight angle"
+
 
 // Case 5: Identify Reflex Angles:
+// When the angle is greater than 180 degrees and less than 360 degrees,
+// Then the function should return "Reflex angle"
 test("should identify reflex angles (between 180° and 360°)", () => {
   expect(getAngleType(270)).toEqual("Reflex angle");
   expect(getAngleType(181)).toEqual("Reflex angle");
   expect(getAngleType(359)).toEqual("Reflex angle");
 });
 
-// When the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle"
+
