3d3b84b7ab188f0eef903b99977d97aafeda01ca
= <=>
2025-07-02 15:16:37 +0100

    updating files

--- Sprint-1/1-key-exercises/1-count.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-
-count = count + 1;
-
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
-
-// Line 3 increases the  value of the variable content by adding 1
--- Sprint-1/2-mandatory-errors/4.js
+++
@@ -1,2 +0,0 @@
-const eveningTime = "20:53";
-const morningTime = "08:53";
