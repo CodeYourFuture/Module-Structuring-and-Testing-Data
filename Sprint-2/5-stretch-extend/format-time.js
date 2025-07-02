f1b81f37bdd925d8006dc3fa6a33cc5b6e3f27da
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-02 19:19:58 +0100

    time format exercise is updated

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,8 +4,15 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
+  const minutes = Number(time.slice(3, 5));
   if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+    return `${(hours - 12).toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} pm`;
+  }
+  if (hours == 12) {
+    return `${12}:${minutes.toString().padStart(2, "0")} pm`;
+  }
+  if (hours == 00) {
+    return `${12}:${minutes.toString().padStart(2, "0")} am`;
   }
   return `${time} am`;
 }
@@ -32,22 +39,36 @@ console.assert(
 );
 
 const currentOutput4 = formatAs12HourClock("15:00");
-const targetOutput4 = "3:00 pm";
+const targetOutput4 = "03:00 pm";
 console.assert(
   currentOutput4 === targetOutput4,
   `current output4: ${currentOutput4}, target output: ${targetOutput4}`
 );
 
-const currentOutput5 = formatAs12HourClock("00:00");
-const targetOutput5 = "00:00 am";
+const currentOutput5 = formatAs12HourClock("15:05");
+const targetOutput5 = "03:05 pm";
 console.assert(
   currentOutput5 === targetOutput5,
   `current output5: ${currentOutput5}, target output: ${targetOutput5}`
 );
 
 const currentOutput6 = formatAs12HourClock("12:00");
-const targetOutput6 = "12:00 am";
+const targetOutput6 = "12:00 pm";
 console.assert(
   currentOutput6 === targetOutput6,
   `current output6: ${currentOutput6}, target output: ${targetOutput6}`
+);
+
+const currentOutput7 = formatAs12HourClock("00:00");
+const targetOutput7 = "12:00 am";
+console.assert(
+  currentOutput7 === targetOutput7,
+  `current output7: ${currentOutput7}, target output: ${targetOutput7}`
+);
+
+const currentOutput8 = formatAs12HourClock("11:37");
+const targetOutput8 = "11:37 am";
+console.assert(
+  currentOutput8 === targetOutput8,
+  `current output8: ${currentOutput8}, target output: ${targetOutput8}`
 );
