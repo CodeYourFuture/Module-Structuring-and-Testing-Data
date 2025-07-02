587495b502ed03653e42d0e4bc6a95b598ee3ab8
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-02 12:08:11 +0100

    added predictions

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,12 +5,21 @@
 
 // =============> write your prediction of the error here 
 
+// =============> the parameter can not be a number
+// Valid JavaScript Identifiers
+// Must start with a letter (a-z, A-Z), underscore (_), or dollar sign ($).
+// Cannot start with a number (0-9).
+// Can contain letters, numbers, underscores, or dollar signs after the first character.
+// Cannot be a reserved keyword (e.g., if, else, function, return).
+
+
+
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-Function square(3)
+// function square(3) {return num * num }
 
 // =============> explain this error message here
 // 3 is not approprate parametr name.
@@ -18,9 +27,10 @@ Function square(3)
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
- function square(num)
- return num*num 
+ //function square(num){
+ //return num*num ; 
+ //}
 
- console.log(square(3));
+ //console.log(square(4));
 
 
