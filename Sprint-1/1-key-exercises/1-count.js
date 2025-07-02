168186a8b853bfdf87e55dbf554ffff80b8f5863
PandiSimatupang <pandikisaran@gmail.com>
2025-07-03 00:49:28 +0100

    add console.log() to verify the value of count

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+console.log(`value of count is ${count}`)
+
 // what line 3 doing is add 1 to current value of "count" and saving or assigning the new value (which is 1) as new value
