1957cac359af494c4e90a08707adeada81dadac4
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-02 10:34:53 +0100

    tested successfully

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,35 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    const rank = card.slice(0, -1); // remove the suit emoji
+    if (rank === "A") return 11;
+    if (["K", "Q", "J", "10"].includes(rank)) return 10;
+
+  const num = parseInt(rank);
+
+  if (num >= 2 && num <= 9) return num;
+
+  throw new Error("Invalid card rank");
 }
+function assertEquals(actualOutput, targetOutput) {
+  console.assert(
+    actualOutput === targetOutput,
+    `Expected ${actualOutput} to equal ${targetOutput}`
+  );
+}
+
+const aceofSpades = getCardValue("A♠");
+assertEquals(aceofSpades, 11);
+const fiveofHearts = getCardValue("5♥");
+assertEquals(fiveofHearts, 5);
+const jackOfDiamonds = getCardValue("J♦");
+assertEquals(jackOfDiamonds, 10);
+
+const queenOfClubs = getCardValue("Q♣");
+assertEquals(queenOfClubs, 10);
+
+const kingOfSpades = getCardValue("K♠");
+assertEquals(kingOfSpades, 10);
+
+
+
+
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -9,3 +9,27 @@ test("should return 11 for Ace of Spades", () => {
 // Case 3: Handle Face Cards (J, Q, K):
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
+
+test("should return the correct value for munber cards (2-10)", () => {
+    expect(getCardValue("3♣")).toEqual(3);
+    expect(getCardValue("7♠")).toEqual(7);
+    expect(getCardValue("10♦")).toEqual(10);
+    });  
+
+test("should return 10 for face cards (J, Q, K)", () => {
+    expect(getCardValue("Q♣")).toEqual(10);
+    expect(getCardValue("K♥")).toEqual(10);
+    expect(getCardValue("J♦")).toEqual(10);
+    });  
+test("should return 11 for Ace cards (A)", () => {
+    expect(getCardValue("A♠")).toEqual(11);
+    expect(getCardValue("A♣")).toEqual(11);
+    expect(getCardValue("A♦")).toEqual(11);
+    });      
+
+test("should throw an error for invalid cards", () => {
+  expect(() => getCardValue("1♦")).toThrow("Invalid card rank");
+  expect(() => getCardValue("L♥")).toThrow("Invalid card rank");
+  expect(() => getCardValue("13♠")).toThrow("Invalid card rank");
+  expect(() => getCardValue("R♣")).toThrow("Invalid card rank");
+});    
