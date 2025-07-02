3c0525b192ce919e9f482b54fa0e812ff0a96639
PandiSimatupang <pandikisaran@gmail.com>
2025-07-03 00:43:46 +0100

    add comment on the last line of file 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// what line 3 doing is add 1 to current value of "count" and saving or assigning the new value (which is 1) as new value
