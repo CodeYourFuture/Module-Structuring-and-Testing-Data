7db7ca55dc365f590aef63d194c63345ff664310
Sara Tahir <saratahir34@gmail.com>
2025-07-03 22:00:18 +0100

    Changed after review.

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,11 +14,11 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
-function touppersnakecase (string){
+function toUpperSnakeCase(string){
     return string
     .toUpperCase()
    .replace(/\s+/g, '_');
 }
 let string = "lord of the rings"
-let result = touppersnakecase(string)
+let result = toUpperSnakeCase(string)
 console.log (result)
