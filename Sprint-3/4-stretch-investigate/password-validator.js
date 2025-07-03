094657bc23febf37ba2a07180af1b886556b5ead
SuWebOnes <surafealaw@gmail.com>
2025-07-03 01:17:26 +0100

    Update: fix password validation logic and comment out test cases

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,32 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
+// function passwordValidator(password) {
+//   return password.length < 5 ? false : true;
+// }
+
+// module.exports = passwordValidator;
+const previousPasswords = ["Password123!", "Welcome2023$", "Admin*1"];
+
+function isValidPassword(password) {
+  const hasMinLength = password.length >= 5;
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasDigit = /[0-9]/.test(password);
+  const hasSymbol = /[!#$%.*&]/.test(password);
+  const isNotPrevious = !previousPasswords.includes(password);
+
+  return (
+    hasMinLength &&
+    hasUppercase &&
+    hasLowercase &&
+    hasDigit &&
+    hasSymbol &&
+    isNotPrevious
+  );
 }
 
-
-module.exports = passwordValidator;
+module.exports = isValidPassword;
+// Example usage:
+// console.log(isValidPassword("Abdi10!")); // true
+// console.log(isValidPassword("12345")); // false
+// console.log(isValidPassword("123")); // false
+// console.log(isValidPassword("abcde")); // false
+// console.log(isValidPassword("")); // false
