b74d0a0301f5f1a676b61168ec1f6b5c25b2473b
Innocent_Niwatwine <innocent.pare3@gmail.com>
2025-07-03 09:52:36 +0300

    Repeat

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,9 @@
-function repeat() {
-    return "hellohellohello";
-}
-
+function repeat(str, count) {
+    if (count === 0) return "";
+    if (count < 0) {
+      throw new Error("Not Allowed");
+    } else {
+      return str.repeat(count);
+    }
+  }
 module.exports = repeat;
