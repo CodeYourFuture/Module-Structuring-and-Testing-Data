e8b4dfe8d95bc2aceb8b579184f9b5b15bf1c2f3
Innocent_Niwatwine <innocent.pare3@gmail.com>
2025-07-03 09:50:07 +0300

    Ordinal Number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,17 @@
 function getOrdinalNumber(num) {
- return "1st";
-}
+    const lastDigits = num % 10;
+    const lastTwoDigits = num % 100;
+  
+    if (lastTwoDigits >= 11 && lastTwoDigits <= 13) return num + "th";
+    if (lastDigits === 1) {
+      return num + "st";
+    } else if (lastDigits === 2) {
+      return num + "nd";
+    } else if (lastDigits === 3) {
+      return num + "rd";
+    } else {
+      return num + "th";
+    }
+}  
 
 module.exports = getOrdinalNumber;
