d5d4140c1eea12ca291e03e138807d44777bd246
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-03 10:17:24 +0100

    repeat test complete

--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -13,20 +13,35 @@ test("should repeat the string count times", () => {
     const str = "hello";
     const count = 3;
     const repeatedStr = repeat(str, count);
-    expect(repeatedStr).toEqual("hellohellohello");
+    expect(repeat(str, count)).toEqual("hellohellohello");
     });
 
 // case: handle Count of 1:
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
+test("should repeat the string count is 1", () => {
+    const str = "hello";
+    const count = 1;
+    expect(repeat(str,count)).toEqual("hello");
+    });
 
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
+test("should return an empty string when count is 0", () => {
+    const str = "hello";
+    const count = 0;
+    expect(repeat(str ,count)).toEqual("");
+    });
 
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+test("should throw an arror when count is negative", () => {
+    const str = "hello";
+    const count = -1;
+    expect(() => repeat("hello", -1)).toThrow("Count must be a non-negative integer");
+    });
