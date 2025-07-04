4d42edcf374ee3fed011f7935644ebcc3afce2a1
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-07-04 20:14:57 +0100

    Implement repeat function and add tests for required cases

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,11 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    throw new Error("count must be a non-negative integer");
+  } else if (count === 0) {
+    return "";
+  } else if (count > 0) {
+    return str.repeat(count);
+  }
 }
 
 module.exports = repeat;
