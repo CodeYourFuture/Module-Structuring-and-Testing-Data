2ee4da2108e1ac106531b7b26530be6afa315643
Farah-Stu <selma.salam45@gmail.com>
2025-07-04 20:27:30 +0100

    fixing conole.log issue

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -31,9 +31,9 @@ function getTheLastDigit(number){
   return number.toString().slice(-1);
 
 }
-console.log(`The last digit of 42 is ${getLastDigit(42)}`);
-console.log(`The last digit of 105 is ${getLastDigit(105)}`);
-console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+console.log(`The last digit of 42 is ${getTheLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getTheLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getTheLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
