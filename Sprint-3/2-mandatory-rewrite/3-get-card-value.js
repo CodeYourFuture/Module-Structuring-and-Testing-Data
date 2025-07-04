2d559cfb7ac9fe7114917a05fe8e81c307446b85
pathywang <pathywang@gmail.com>
2025-07-04 18:14:37 +0100

    copy code

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,17 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+   const rank = card.slice(0, -1); // removes suit
+
+  if (rank === "A") return 11;
+  if (!isNaN(rank)) return parseInt(rank); // 2-10
+  if (["J", "Q", "K"].includes(rank)) return 10;
+
+  throw new Error("Invalid card rank"); 
 }
+function assertEquals(actualOutput, targetOutput) {
+  console.assert(
+    actualOutput === targetOutput,
+    `Expected ${actualOutput} to equal ${targetOutput}`
+  );
+}
+
 module.exports = getCardValue;
