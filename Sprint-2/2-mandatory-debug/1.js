13e562dd77e2fb492fd17d1d0a6967c50eff446a
Farah-Stu <selma.salam45@gmail.com>
2025-07-04 20:02:40 +0100

    removing the brackets

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -14,7 +14,7 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
 
-function sum(a, b){
-  return(a+b);
+function sum(a, b) {
+  return a + b;
 }
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
