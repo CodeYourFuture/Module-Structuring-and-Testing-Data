486f3ff5ba4b7498e17977a2a010a769d4ce057c
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-07-04 20:04:47 +0100

    Add Jest tests for isProperFraction function

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -4,8 +4,14 @@ test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
 });
 
-// Case 2: Identify Improper Fractions:
+test("should return false for a improper fraction", () => {
+  expect(isProperFraction(5, 3)).toEqual(false);
+});
 
-// Case 3: Identify Negative Fractions:
+test("should return true for negative proper fraction", () => {
+  expect(isProperFraction(-2, 3)).toEqual(true);
+});
 
-// Case 4: Identify Equal Numerator and Denominator:
+test("should return false when numerator equals denominator", () => {
+  expect(isProperFraction(2, 2)).toEqual(false);
+});
