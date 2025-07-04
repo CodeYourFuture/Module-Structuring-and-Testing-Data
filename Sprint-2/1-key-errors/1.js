b83536371f47ff3a7b9f67991e12ff11b530160c
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-04 09:07:43 +0100

    removed const value to avoid redeclaration.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -22,9 +22,10 @@
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  decimalNumber = 0.5; // Remove 'const' to avoid redeclaration
+ // decimalNumber = 0.5; // Remove 'const' to avoid redeclaration
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 console.log(convertToPercentage(0.5)); // Output: "50%"
+console.log(convertToPercentage(0.1));
