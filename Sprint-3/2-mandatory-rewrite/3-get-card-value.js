8dd332e69166184ffe289fc60df051df156cea6e
Reza Jahanimir <jahanimir.r@gmail.com>
2025-07-04 23:30:34 +0100

    made the function look simpler and cleaner

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -10,20 +10,8 @@ function getCardValue(card) {
   if (rank === "A") return 11;
   if (["K", "Q", "J", "10"].includes(rank)) return 10;
   
-  // It should be a string of digits, not starting with zero, and between 2-10
-  if (!/^[1-9][0-9]*$/.test(rank)) {
-    throw new Error("Invalid card rank");
-  }
-  // Convert rank to a number and check if it's between 2 and 10
-  const numericRank = Number(rank);
-  if (
-    !isNaN(numericRank) &&
-    Number.isInteger(numericRank) &&
-    numericRank >= 2 &&
-    numericRank <= 10
-  ) {
-    return numericRank;
-  }
+  // Digits 2–9
+  if ("23456789".includes(rank)) return Number(rank);
 
   throw new Error("Invalid card rank");
 }
