684fb3bdff52569a52742d36d757d498bf004f75
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-07-04 21:08:07 +0100

    updated card-validator.js

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,43 +1,34 @@
 function cardValidator(cardNumber) {
-    //Check if length is exactly 16
-    if (cardNumber.length !== 16) {
-      return false;
-    }
-  
-    //Check all characters are digits
-    for (let i = 0; i < cardNumber.length; i++) {
-        if (cardNumber[i] < '0' || cardNumber[i] > '9') 
-            return false;
-      }
-    
-    // Check not all digits are the same
-    
-    let allSame = true;
-for (let i = 1; i < cardNumber.length; i++) {
-  if (cardNumber[i] !== cardNumber[0]) {
-    allSame = false;
-    break;
-  }
-}
-  if (allSame) {
+  //Check if length is exactly 16
+  if (cardNumber.length !== 16) {
     return false;
   }
-    //Check sum of all digits is greater than 16
-    const sum = cardNumber
-      .split("")
-      .reduce((acc, digit) => acc + Number(digit), 0);
-    if (sum <= 16) {
-      return false;
+  let sum = 0;
+  let allSame = true;
+
+  for (let i = 0; i < cardNumber.length; i++) {
+    const char = cardNumber[i];
+
+    //Check all characters are digits
+    if (cardNumber[i] < "0" || cardNumber[i] > "9") return false;
+
+    const digit = Number(char);
+    sum += digit;
+
+    // Check not all digits are the same
+    if (cardNumber[i] !== cardNumber[0]) {
+      allSame = false;
     }
-  
-    //Check last digit is even
-    const lastDigit = Number(cardNumber[cardNumber.length - 1]);
-    if (lastDigit % 2 === 0) {
-      return false;
-    }
-  
-    return true;
   }
-  
-  module.exports = cardValidator;
-  
+  if (allSame) return false;
+  if (sum <= 16) return false;
+
+  const lastDigit = Number(cardNumber[cardNumber.length - 1]);
+  if (lastDigit % 2 === 0) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = cardValidator;
