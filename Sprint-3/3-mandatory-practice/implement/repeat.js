856c91336a93414ad082c41fe246107bd74446d9
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-07-04 21:49:01 +0100

    updated repeat.js

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,8 +1,14 @@
 function repeat(str, count) {
-    if (count < 0) {
-        throw new Error("Negative integer not accepted.");
-    }
-    return str.repeat(count);
+  if (count < 0) {
+    throw new Error("Negative integer not accepted.");
+  }
+
+  let total = "";
+  for (let i = 0; i < count; i++) {
+    total += str;
+  }
+
+  return total;
 }
 
 module.exports = repeat;
