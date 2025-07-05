9a8ef0f015c3124af66310a5e960c1324d48f7e0
pathywang <pathywang@gmail.com>
2025-07-05 12:26:18 +0100

    new code

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,12 @@
-function countChar(stringOfCharacters, findCharacter) {
-    return 5
+function countChar(stringOfCharacters, findCharacter) {let count = 0;
+    let position = 0;
+
+    while ((position = stringOfCharacters.indexOf(findCharacter, position)) !== -1) {
+        count++;  
+        position++; 
+    }
+
+    return count;
 }
 
 module.exports = countChar;
