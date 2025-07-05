8fae862c926b3147c4c6186d9ca2a40b2a049a4e
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-05 02:16:53 +0100

    1. Interpreted the code in time-format.js 2. Answered the questions clearly and excuted the code to see the results.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,26 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// The function formatTimeDisplay will call pad three times: once for totalHours, once for remainingMinutes, and once for remainingSeconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// The value assigned to num when pad is called for the first time is 1. This is because totalHours is calculated as (61 - 1) / 60, which equals 1.
+// The first call to pad is for totalHours, which is 1.
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// The return value of pad when called for the first time is "01". This is because pad converts the number 1 to a string and pads it with a leading zero to ensure it has at least two digits.
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The value assigned to num when pad is called for the last time is 1. This is because remainingSeconds is calculated as 61 % 60, which equals 1.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The return value assigned to num when pad is called for the last time is "01".
+//  This is because pad converts the number 1 to a string and pads it with a leading zero to ensure it has at least two digits.
+// The final output of formatTimeDisplay(61) will be "00:01:01", where each component is padded to two digits.
+console.log(formatTimeDisplay(61)); // Output: "00:01:01"
