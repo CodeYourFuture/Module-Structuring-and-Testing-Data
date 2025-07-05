998a7ee7bb555ef489ba78bce8adafc8fa2e7f60
PandiSimatupang <pandikisaran@gmail.com>
2025-07-05 22:37:26 +0100

    comment and study exercise sprint-1/4-random.js

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -5,5 +5,11 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
+
+//Math.random() -> generates zero base random number
+// we need to multiply with digit number needed to allow possible number generated
+//Math.floor() method will round up to the base integer generated
+
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(num);
