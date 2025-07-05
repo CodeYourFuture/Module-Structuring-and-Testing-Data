f8f251b14484bc1ecb992c5016f8e7fa81794f14
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-07-05 20:26:46 +0100

    Refactor card value tests by category and add edge cases for invalid formats

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,37 +1,38 @@
 const getCardValue = require("./3-get-card-value");
 
+// Case 1: Ace
 test("should return 11 for Ace of Spades", () => {
-  const aceofSpades = getCardValue("A♠");
-  expect(aceofSpades).toEqual(11);
+  expect(getCardValue("A♠")).toEqual(11);
 });
 
-// Case 2: Handle Number Cards (2-10)
-test("should return 5 for 5 of Hearts", () => {
-  const fiveOfHearts = getCardValue("5♥");
-  expect(fiveOfHearts).toEqual(5);
+// Case 2: Number Cards (2–10)
+test("should return correct value for number cards (2-10)", () => {
+  expect(getCardValue("2♣")).toEqual(2);
+  expect(getCardValue("5♠")).toEqual(5);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("10♥")).toEqual(10);
 });
 
-test("should return 10 for 10 of Clubs", () => {
-  const tenOfClubs = getCardValue("10♣");
-  expect(tenOfClubs).toEqual(10);
-});
-
-// Case 3: Handle Face Cards (J, Q, K)
-test("should return 10 for Jack of Diamonds", () => {
+// Case 3: Face Cards (J, Q, K)
+test("should return 10 for face cards", () => {
   expect(getCardValue("J♦")).toEqual(10);
-});
-
-test("should return 10 for Queen of Hearts", () => {
   expect(getCardValue("Q♥")).toEqual(10);
-});
-
-test("should return 10 for King of Spades", () => {
   expect(getCardValue("K♠")).toEqual(10);
 });
 
-// Case 4: Handle Ace (already tested above)
-
-// Case 5: Handle Invalid Cards
-test("should throw an error for invalid card rank", () => {
+// Case 4: Invalid Ranks
+test("should throw an error for invalid rank Z", () => {
   expect(() => getCardValue("Z♠")).toThrow("Invalid card rank");
+});
+
+test("should throw an error for malformed number like 0x02", () => {
+  expect(() => getCardValue("0x02♠")).toThrow("Invalid card rank");
+});
+
+test("should throw an error for decimal number like 2.1", () => {
+  expect(() => getCardValue("2.1♠")).toThrow("Invalid card rank");
+});
+
+test("should throw an error for malformed format like 00_02", () => {
+  expect(() => getCardValue("00_02♠")).toThrow("Invalid card rank");
 });
