55aaa5f6f9f796c204239f2a69c303fb125be7ec
PandiSimatupang <pandikisaran@gmail.com>
2025-07-05 22:45:50 +0100

    solve exec. sprint-1/2-mandatory-errors/2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+//it's not working because variable cityOfBirth was not defined.
+//change the position of -- const cityOfBirth = "Bolton"; -- before the logging will fix the problem
