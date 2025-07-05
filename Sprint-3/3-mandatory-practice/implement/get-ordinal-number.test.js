10fd91cfe5ed4ab319804d785d6c863854f8e9bf
emladeno <mladenova.elvira@yahoo.com>
2025-07-05 19:10:06 +0100

    grouped the tests into meaningful categories.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -8,14 +8,38 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
+// Numbers ending in 1 (but not 11)
+test("should return 'st' for numbers ending in 1, except 11", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
-    });
- test("should return '2nd' for 2", () => { expect(getOrdinalNumber(2)).toEqual("2nd");
- });
-test("should return '3rd' for 3", () => { expect(getOrdinalNumber(3)).toEqual("3rd");
- });
-test("should return '4th' for 4", () => { expect(getOrdinalNumber(4)).toEqual("4th");
- });
-test("should return '12th' for 12", () => { expect(getOrdinalNumber(12)).toEqual("12th");
- });
+    expect(getOrdinalNumber(21)).toEqual("21st");
+    expect(getOrdinalNumber(141)).toEqual("141st");
+  });
+  
+  
+  test("should return 'nd' for numbers ending in 2, except 12", () => {
+    expect(getOrdinalNumber(2)).toEqual("2nd");
+    expect(getOrdinalNumber(22)).toEqual("22nd");
+    expect(getOrdinalNumber(82)).toEqual("82nd");
+  });
+  
+
+  test("should return 'rd' for numbers ending in 3, except 13", () => {
+    expect(getOrdinalNumber(3)).toEqual("3rd");
+    expect(getOrdinalNumber(23)).toEqual("23rd");
+    expect(getOrdinalNumber(203)).toEqual("203rd");
+  });
+  
+  
+  test("should return 'th' for 11, 12, 13", () => {
+    expect(getOrdinalNumber(11)).toEqual("11th");
+    expect(getOrdinalNumber(12)).toEqual("12th");
+    expect(getOrdinalNumber(13)).toEqual("13th");
+  });
+  
+
+  test("should return 'th' for all other numbers", () => {
+    expect(getOrdinalNumber(5)).toEqual("5th");
+    expect(getOrdinalNumber(7)).toEqual("7th");
+    expect(getOrdinalNumber(10)).toEqual("10th");
+    expect(getOrdinalNumber(100)).toEqual("100th");
+  });
