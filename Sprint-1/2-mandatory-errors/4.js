a4496e3c116f5030ab4af82b02588dcded2d2905
PandiSimatupang <pandikisaran@gmail.com>
2025-07-05 23:05:40 +0100

    solve exec. sprint-1/2-mandatory-errors/4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const ClockTime_12Hour = "20:53";
+const ClockTime_24hour = "08:53";
+
+//variable name convention issues...
+//variable name should not started a number
