d77cae37b2148dc3f1aef9ecc35a6815052a50dc
mo-muchunu <mo.muchunu@gmail.com>
2025-07-06 15:25:08 +0100

    Re-ordered function and reformated test layout for improved efficiency

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,21 +1,19 @@
 function getOrdinalNumber(num) {
-    const endsIn = num % 10; // Using modulo operator to isolate last number as remainder in order to evaluate correct suffix  
-    const lastTwoDigits = num % 100; // Extract last 2 digits to ensure special cases are correctly labelled
+    if (num < 0) return "Invalid input";                 // Reject negative inputs early because ("-10th", "-3rd") do not follow ordinal number logic
 
+    if (num === 0) return "0th";                         // Handle zero as a special case- rarely used but is a valid ordinal number in programming when referencing first item in an array ("zeroth element") 
+
+    const lastTwoDigits = num % 100;                     // Extract last 2 digits to correctly label special cases like 11, 12, and 13
+    const endsIn = num % 10;                             // Using modulo to isolate last number and evaluate correct suffix  
     if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
-    return num + "th"; // Applying ordinal number logic for special cases: 11th, 12th, 13th.
+        return num + "th";
     }
-    if (num < 0) return "Invalid input"; // Reject negative numbers because ("-10th", "-3rd") do not necessarily follow ordinal number logic nor grammatically standard.
-    if (num === 0) return "0th"; // Handle zero as a special case - although rarely used, "0th" is a valid ordinal number in programming in reference to indexing because the "zeroth element" is the first item in an array.
+
     if (endsIn === 1) return num + "st";
     if (endsIn === 2) return num + "nd";
-    if (endsIn === 3) return num + "rd"; 
-    
-    return num + "th"; // All other numbers without exceptions.
+    if (endsIn === 3) return num + "rd";
+
+    return num + "th";                                    // All other numbers
 }
 
-module.exports = getOrdinalNumber; 
-
-
-
-  
+module.exports = getOrdinalNumber;
