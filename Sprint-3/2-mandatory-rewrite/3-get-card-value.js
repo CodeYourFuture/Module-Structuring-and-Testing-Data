5a4035eff0b86e6af72bafec06a5f34783611787
mo-muchunu <mo.muchunu@gmail.com>
2025-07-06 15:59:18 +0100

    Added stricter validation for number cards within allowed range

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,15 +1,20 @@
 // replace with your code from key-implement
 
 function getCardValue(card) {
-  const rank = card.slice(0, -1); // remove last character 
-  const numberCards = Number(rank);
+  const rank = card.slice(0, -1);                                       // Remove last character/ suit symbol 
+  const numberCards = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];   // Define number cards in standard deck rather than relying on 'Number(rank)' to convert and check rank range.
 
-  if (rank === "A") return 11;
-  if (["K", "Q", "J", "10"].includes(rank)) return 10; // To handle face cards
-  if (!isNaN(numberCards) && numberCards >= 2 && numberCards <= 10) { // Check that numberCards is a valid number and ensure they are within range (2–10)
-    return numberCards;
-  }
+  if (rank === "A") return 11; 
+  if (["K", "Q", "J"].includes(rank)) return 10;                       // To handle face cards
+  if (numberCards.includes(rank)) return Number(rank);                 // Check that numberCards are valid numbers and ensure they are within range (2–10)
+  
   return "Invalid card rank";
 }
 
 module.exports = getCardValue;
+
+/*
+console.log(getCardValue("0x02♠"));
+console.log(getCardValue("2.1♠"));
+console.log(getCardValue("00_02♠"));
+*/
