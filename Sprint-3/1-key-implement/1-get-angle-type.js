a0004a8782914a952ce5bae38b83ef7648fbb363
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-07 00:22:26 +0100

    1. Added 4 if conditions to the function getAngleType(angle). 2. Identified Obtuse angles when it's greater than 90 degrees and less than 180, and added a line to test the function. 3. added a line to test all the if conditions in the program.

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -9,6 +9,11 @@
 
 function getAngleType(angle) {
    if (angle === 90) return "Right angle";
+    if (angle < 90) return "Acute angle";
+    if (angle > 90 && angle < 180) return "Obtuse angle";
+    if (angle === 180) return "Straight angle";
+    if (angle > 180 && angle < 360) return "Reflex angle";
+
    // read to the end, complete line 36, then pass your test here
 }
 
@@ -42,9 +47,10 @@ assertEquals(acute, "Acute angle");
 // Case 3: Identify Obtuse Angles:
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
-const obtuse = getAngleType(120);
 // ====> write your test here, and then add a line to pass the test in the function above
-
+const obtuse = getAngleType(120);
+assertEquals(obtuse, "Obtuse angle");
+console.log("Obtuse angle test passed!");
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
@@ -54,3 +60,6 @@ const obtuse = getAngleType(120);
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
 // ====> write your test here, and then add a line to pass the test in the function above
+
+
+console.log("All tests passed! 🎉");
