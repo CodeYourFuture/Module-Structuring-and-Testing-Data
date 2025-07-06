2f6b738566ac13d4450f99f32dd6241a99ee8dc4
Saleh Yousef <saleh.omar.uk@gmail.com>
2025-07-06 20:50:36 +0100

    Update 3-get-card-value.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,8 +1,11 @@
 function getCardValue(card) {
-  if (card === "A") return 11;
-  if (["K", "Q", "J", "10"].includes(card)) return 10;
-  const num = Number(card);
-  if (num >= 2 && num <= 9 && card === num.toString()) return num;
+  const rank = card.slice(0, -1);
+  
+  if (rank === "A") return 11;
+  if (["K", "Q", "J", "10"].includes(rank)) return 10;
+  const num = Number(rank);
+  if (num >= 2 && num <= 9) return num;
+  
   throw new Error("Invalid card rank.");
 }
 module.exports = getCardValue;
