4882807697ad6ffc2efe577f5595c03ed61738bd
Saleh Yousef <saleh.omar.uk@gmail.com>
2025-07-06 14:38:45 +0100

    Update 1-get-angle-type.js

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,4 +1,5 @@
 function getAngleType(angle) {
+   if (angle <= 0 || angle >= 360) return "Invalid angle";
    if (angle === 90) return "Right angle";
    if (angle < 90) return "Acute angle";
    if (angle > 90 && angle < 180) return "Obtuse angle";
