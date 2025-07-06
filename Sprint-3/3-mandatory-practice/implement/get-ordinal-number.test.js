4f3c9227f8c2c14d932a3fc1c5eec863fd195ce8
Saleh Yousef <saleh.omar.uk@gmail.com>
2025-07-06 21:54:31 +0100

    Update get-ordinal-number.test.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -4,36 +4,29 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
 
-// Case 1: Identify the ordinal number for 1
-// When the number is 1,
-// Then the function should return "1st"
+// Case 1: Identify numbers that end with 1 but are not 11
 
-test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
+test.each([1, 21, 31])("should return '%ist' for %i", (num) => { 
+    expect(getOrdinalNumber(num)).toEqual(`${num}st`);
     });
 
-// Case 2: identify the ordinal number for 2
-// when the number is ,
-// then the function should return "2nd"
+// Case 2: identify the ordinal numbers that end with 2 but are not 12 
 
-test("should return '2nd' for2", () => { 
-    expect(getOrdinalNumber(2)).toEqual("2nd");
+test.each([2, 22, 32])("should return '%ind' for %i", (num) => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}nd`);
     });
 
-// Case 3: identify the ordinal number for 30
-// when the number is ,
-// then the function should return "30th"
-
-test("should return '30th' for30", () => { 
-    expect(getOrdinalNumber(30)).toEqual("30th");
+// Case 3: identify the ordinal numbers that end with 3 but not 13
+test.each([3, 23, 33])("should return '%ird' for %i", (num) => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}rd`);
     });
 
-// Case 4: 3rd
-test("should return '3rd' for 3", () => { 
-  expect(getOrdinalNumber(3)).toEqual("3rd");
+// Case 4: everything else with th
+test.each([4, 5, 6, 10, 14, 20, 40])("returns '%ith' for %i", (num) => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}th`);
 });
 
-// Case 5: 11th (special teen case)
-test("should return '11th' for 11", () => { 
-  expect(getOrdinalNumber(11)).toEqual("11th");
+// Case 5: 11th 12th 13th (special teen case)
+test.each([11, 12, 13])("should return '%ith' for %i", (num) => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}th`);
 });
