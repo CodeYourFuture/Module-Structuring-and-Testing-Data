e2a37f87270b0c967e694cb7632225c535cdc07e
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-06 01:10:44 +0100

    I added a console.log(num); in line 2 and executed the script to find out the value of "num" when pad is called the frist time is 0.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -1,6 +1,7 @@
 function pad(num) {
-  return num.toString().padStart(2, "0");
-}
+   console.log(num); 
+  return num.toString().padStart(2, "0"); 
+} 
 
 function formatTimeDisplay(seconds) {
   const remainingSeconds = seconds % 60;
@@ -9,7 +10,7 @@ function formatTimeDisplay(seconds) {
   const totalHours = (totalMinutes - remainingMinutes) / 60;
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
-}
+} 
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -24,7 +25,7 @@ function formatTimeDisplay(seconds) {
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-// The value assigned to num when pad is called for the first time is 1. This is because totalHours is calculated as (61 - 1) / 60, which equals 1.
+// The value assigned to num when pad is called for the first time is 0. 
 // The first call to pad is for totalHours, which is 1.
 
 // c) What is the return value of pad is called for the first time?
