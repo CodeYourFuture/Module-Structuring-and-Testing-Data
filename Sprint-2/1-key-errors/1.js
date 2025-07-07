65e1a8ee32f09873b9ffbff8ea7fbf4ba211076c
PandiSimatupang <pandikisaran@gmail.com>
2025-07-07 21:30:52 +0100

    end of Sprit-1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,17 +2,24 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
+// decimalNumber variable was declared twice. First as parameter and second as const in function body
+// use different variable name, either in parameter or the one in function body.
+// any way this function always return the same value because the variable is constant and define inside th function,
+// usually a function take a variable from function parameters.
+// console.log() will fail to print because no parameter define first.
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  //-----> should be place as parameter and not as a constant ,,,,,const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+decimalNumber = 0.27;
+
+console.log(decimalNumber); //this is fine, just define the parameter it needs.
+console.log(convertToPercentage(decimalNumber));
 
 // =============> write your explanation here
 
--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,66 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+//=============================================================
+// const penceString = "9p"; //init variable
+
+// const penceStringWithoutTrailingP = penceString.substring(
+//   0,
+//   penceString.length - 1
+// ); //remove p character at the end of the variable
+// console.log(penceStringWithoutTrailingP);
+
+// const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); //
+// console.log(paddedPenceNumberString); //give format of 3 zeroes if the number is less than 3 digits.
+
+// const pounds = paddedPenceNumberString.substring(
+//   0,
+//   paddedPenceNumberString.length - 2
+// ); //get ponds value conversion changes for given pence
+
+// console.log(pounds);
+
+// const pence = paddedPenceNumberString
+//   .substring(paddedPenceNumberString.length - 2)
+//   .padEnd(2, "0"); //get the reminds after conversion to pounds
+
+// console.log(`£${pounds}.${pence}`);
+
+//------------------------------------------------------------------------------------------
+
+function pensToPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  ); //remove p character at the end of the variable
+  //console.log(penceStringWithoutTrailingP);
+
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); //
+  //console.log(paddedPenceNumberString); //give format of 3 zeroes if the number is less than 3 digits.
+
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  ); //get ponds value conversion changes for given pence
+
+  //console.log(pounds);
+
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0"); //get the reminds after conversion to pounds
+
+  return `£${pounds}.${pence}`;
+}
+
+//test----1
+let moneyA = "90p";
+console.log(`${pensToPounds(moneyA)}`);
+
+//test----2
+moneyA = "710p";
+console.log(`${pensToPounds(moneyA)}`);
+
+//test----3
+moneyA = "1210p";
+console.log(`${pensToPounds(moneyA)}`);
