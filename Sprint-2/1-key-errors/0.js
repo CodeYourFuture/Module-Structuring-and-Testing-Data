100b9fd4884b2e661b6ece7ee6c19ed3d438539d
PandiSimatupang <pandikisaran@gmail.com>
2025-07-07 22:56:38 +0100

    reset to origin  file Sprint-2/1-key-errors/0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,21 +1,13 @@
 // Predict and explain first...
 //  =============> write your prediction here
-//the function will turn the first given string into Uppercase.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let strWithUpperCase = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return strWithUpperCase;
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
 }
 
 // =============> write your explanation here
-// str[0].toUpperCase() ---> gets the first character and uses build-in method to convert a char to uppercase
-// str.slice(1) ----> will slice the array or sting "start from 1" to the rest of array or string
-// we get an error because we "re-declare" the str variable as parameter and as call back of expression
-
 // =============> write your new code here
-
-const myStr = "i forgot add this string";
-console.log(capitalise(myStr));
