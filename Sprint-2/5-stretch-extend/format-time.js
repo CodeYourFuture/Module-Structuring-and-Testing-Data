8b56abf8d3b70276b5abe24efa8bda4b7e3769f0
PandiSimatupang <pandikisaran@gmail.com>
2025-07-08 00:33:23 +0100

    solve Sprint-2/5-stretch-extend/format-time.js

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,7 +4,12 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
+  if (time === "00:00" || time === "24:00") {
+    //put the edge case on the first checking if-else-case
+    return `12:00 am`;
+  } else if (time === "12:00") {
+    return `12:00 pm`;
+  } else if (hours > 12) {
     return `${hours - 12}:00 pm`;
   }
   return `${time} am`;
@@ -22,4 +27,25 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("24:00");
+const targetOutput4 = "12:00 am";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("12:00");
+const targetOutput5 = "12:00 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
 );
