bab1b655b6a7550e2c95d38194941d8cb954a5b6
roja alagurajan <alagurajan.roja@gmail.com>
2025-07-07 17:06:00 +0100

    card_value_test

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -5,7 +5,27 @@ test("should return 11 for Ace of Spades", () => {
     expect(aceofSpades).toEqual(11);
     });
 
-// Case 2: Handle Number Cards (2-10):
+// Case 2: Handle Number Cards (2-10)
+test("should return 5 for 5 of Hearts", () => {
+  expect(getCardValue("5♥")).toEqual(5);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Diamonds", () => {
+  expect(getCardValue("J♦")).toEqual(10);
+});
+
+test("should return 10 for Queen of Spades", () => {
+  expect(getCardValue("Q♠")).toEqual(10);
+});
+
+test("should return 10 for King of Hearts", () => {
+  expect(getCardValue("K♥")).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Clubs", () => {
+  expect(getCardValue("A♣")).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error for empty string", () => {
+  expect(() => getCardValue("")).toThrow("Invalid card rank.");
+});
