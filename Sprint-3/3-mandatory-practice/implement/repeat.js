5e6e9779734df48622b664873d9bd3a69e48696b
roja alagurajan <alagurajan.roja@gmail.com>
2025-07-07 17:22:06 +0100

    repeated

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,7 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    throw new Error("Invalid input: count must be non-negative");
+  }
+  return String(str).repeat(count);
 }
-
 module.exports = repeat;
