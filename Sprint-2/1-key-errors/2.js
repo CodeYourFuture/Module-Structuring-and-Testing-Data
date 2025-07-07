ce9e5c1d14436bc702eb3d153c4ea8a1dff7036f
PandiSimatupang <pandikisaran@gmail.com>
2025-07-07 23:01:23 +0100

    reset Sprint-2/1-key-errors/2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,27 +1,18 @@
+
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-//the function will return square for every given number.
 
-// function square(3) {
-//     return num * num;
-// }
-
-// =============> write the error message here
-// ans: Unexpected number
-
-// =============> explain this error message here
-// ans: function need num to be defined, instead parameter was given 3 as literal value that does not point to any variable as parameter
-// Finally, correct the code to fix the problem
-
-// =============> write your new code here
-
-function square(num) {
+function square(3) {
   return num * num;
 }
 
-mynum = 3; //define the num
+// =============> write the error message here
 
-console.log(square(mynum)); //verify the result
+// =============> explain this error message here
+
+// Finally, correct the code to fix the problem
+
+// =============> write your new code here
