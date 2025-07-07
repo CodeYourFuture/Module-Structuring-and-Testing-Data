f9786852f25768caf562469ab7f279b6b9487b10
Emin <eminakturk07@gmail.com>
2025-07-07 12:15:45 +0100

    4.js syntax error is fixed.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -6,7 +6,7 @@
 
 // correct way is:
 
-const.twelveHourClockTime = "20:53";
+const twelveHourClockTime = "20:53";
 const twentyFourHourClockTime = "08:53";
 
 console.log(`The time in 12-hour format is ${twelveHourClockTime} and in 24-hour format is ${twentyFourHourClockTime}`);
