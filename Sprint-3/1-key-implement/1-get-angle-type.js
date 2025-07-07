3d3ed47fa6b9843f476bcc2714b2246ef8047e1b
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-07 10:48:15 +0100

    Fixing 1-get-angle-type.js

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -8,6 +8,7 @@
 // Then, write the next test! :) Go through this process until all the cases are implemented
 
 function getAngleType(angle) {
+  if (typeof(angle) !== "number" || angle<=0 || angle >360 ) return "Invalid angle"
   if (angle === 90) return "Right angle";
   if (angle === 45) return "Acute angle";
   if (angle === 120) return "Obtuse angle";
@@ -65,3 +66,23 @@ const reflex = getAngleType(181);
 assertEquals(reflex, "Reflex angle");
 const reflex2 = getAngleType(359);
 assertEquals(reflex2, "Reflex angle");
+
+// Case 6: No valid input:
+// When the angle is greater than 360 degrees and less than 0 degrees,
+// Then the function should return "Invalid angle"
+// ====> write your test here, and then add a line to pass the test in the function above
+const outOfRange = getAngleType(-5);
+assertEquals(outOfRange, "Invalid angle");
+const outOfRange2 = getAngleType(361);
+assertEquals(outOfRange2, "Invalid angle");
+
+// Case 6: Not number input:
+// When the angle is not a number,
+// Then the function should return "Invalid angle"
+// ====> write your test here, and then add a line to pass the test in the function above
+const notNumberInput = getAngleType("-5");
+assertEquals(notNumberInput, "Invalid angle");
+const notNumberInput2 = getAngleType(true);
+assertEquals(notNumberInput2, "Invalid angle");
+const notNumberInput3 = getAngleType("Hello");
+assertEquals(notNumberInput3, "Invalid angle");
