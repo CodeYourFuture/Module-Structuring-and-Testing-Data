494b2e5ce17ddc351e5c440b1792e734914de01a
Emin <eminakturk07@gmail.com>
2025-07-07 16:35:29 +0100

    mandatory debug part is solved.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,21 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> My prediction is code wont return anything because the function does not have a return statement.
 
-function multiply(a, b) {
+/* function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
+console.log(`The result of multiplying 10 and 32 is `${multiply(10, 32)}`);
+*/
+// =============> Once return statement is added, it will return the result of the multiplication.
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> 
+
+  function multiply(a,b) 
+{
+return a*b;
+}
+
+console.log(`result of multiplying 2 times 2 = ${multiply(2, 2)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> Problem here is that sum does not return no value because return statement and a + b is in different lines.
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
+*/
+// =============> Once we put a + b back in return statement it will work with no problem.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) 
+{
+  return a + b;
+}
+
+console.log(`The sum of 45 and 12 is ${sum(45, 12)}`); // Output: "The sum of 10 and 32 is 42"
