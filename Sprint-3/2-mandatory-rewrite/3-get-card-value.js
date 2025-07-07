4ed95d23e6b4f43aa69082363ba27022bf9582b0
roja alagurajan <alagurajan.roja@gmail.com>
2025-07-07 17:03:31 +0100

    get_card_alue

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,11 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
-}
+    const rankChar = card.slice(0, -1);
+    const rankInt = parseInt(rankChar);
+
+    if (rankChar === "A") return 11;
+    if (["K", "Q", "J", "10"].includes(rankChar)) return 10;// If rank is 'K', 'Q', or 'J' (King, Queen, Jack), return 10 points
+    if (rankInt >= 2 && rankInt <= 9 && rankChar === rankInt.toString()) return rankInt;
+
+    throw new Error("Invalid card rank.");
+} 
 module.exports = getCardValue;
