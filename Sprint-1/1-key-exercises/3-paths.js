d17fa1d282b97b0591dc498cb97e6bfe59848625
SuWebOnes <surafealaw@gmail.com>
2025-07-07 20:43:37 +0100

    update: Implement dir and ext extraction logic in 3-paths.js

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,13 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
-
-// https://www.google.com/search?q=slice+mdn
+const dir = filePath.slice(0, lastSlashIndex);// The code uses the lastIndexOf method to find the position of the last slash in;
+console.log(`The dir part of ${filePath} is ${dir}`);
+filePath.slice(0, lastSlashIndex);
+const ext = filePath;
+filePath
+  .slice(lastSlashIndex + 1)
+  .split(".")
+  .pop();
+// The code above uses the lastIndexOf method to find the position of the last slash in the filePath string.
+// The slice method is then used to extract the base part of the filePath string, which is the part after the last slash.
