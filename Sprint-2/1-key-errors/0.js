f6d89d10693fcbce9b419a2472544cdad9ab1cc7
Emin <eminakturk07@gmail.com>
2025-07-07 16:14:13 +0100

    0.js error fixed

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,28 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// I predict the code is for capitalising the first letter of a string.
+
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+// the error is "The symbol "str" has already been declared."
+
+/* function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
+*/
 // =============> write your explanation here
+// Error occurs because the str was mentioned as a parameter in the definition which is already a variable name in the function body.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisedStr;
+}
+
+// to call the function : 
+
+console.log(capitalise("naber?")); // Output: "Naber?"
