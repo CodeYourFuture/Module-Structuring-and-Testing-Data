f2f56befe54dfe48e50a00023cd799f0dcab5a4c
Emin <eminakturk07@gmail.com>
2025-07-07 12:06:46 +0100

    first answer is increment added answer comment section.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -7,3 +7,4 @@ count = count + 1;
 
 // SOLUTION:
 // Line 3 is updating the value of the count by adding 1 to its value.
+// line 3 uses the assignment operator which is = and while count increases by one, increment stores additional 1 to the count variable.
