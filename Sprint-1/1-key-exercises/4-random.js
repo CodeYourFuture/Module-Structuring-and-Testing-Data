48ea7fc482aea50819ee720bea5eb73434936886
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-07-07 19:40:08 +0100

    update the functionality of Math.floor()

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -6,7 +6,7 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // Math.random() returns a floating point number between 0 and 1
 // (maximum - minimum + 1) scales a random number to a specific range. The result is a random decimal number between 0 and (maximum - minimum + 1)
-// Math.floor() round the scaled random number to the nearest whole number
+// Math.floor() rounds down the scaled random number so that it cannot go beyond the range.
 // + minimum shifts the range from 0...N to minimum... maximum. It ensures the result starts from the desired minimum value.
 
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
