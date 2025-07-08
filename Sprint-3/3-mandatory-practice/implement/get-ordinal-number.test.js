89f44737f3a9b88b7d2b6a318cd622afab65f9e1
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-08 11:52:05 +0100

    Identify the ordinal numbers test cases updated

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -8,24 +8,43 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
-    });
-test("should return '2nd' for 2", () => {
-    expect(getOrdinalNumber(2)).toEqual("2nd");
-    });
-test("should return '3rd' for 3", () => {
-    expect(getOrdinalNumber(3)).toEqual("3rd");
-    });
-test("should return 'th' for 11", () => {
-    expect(getOrdinalNumber(11)).toEqual("th");
-    });
-test("should return 'th' for 12", () => {
-    expect(getOrdinalNumber(12)).toEqual("th");
-    });
-test("should return 'th' for 13", () => {
-    expect(getOrdinalNumber(13)).toEqual("th");
-    });
-test("should return 'th' for 4", () => {
-    expect(getOrdinalNumber(4)).toEqual("th");
-    });
+describe("Identify the ordinal number for st", () => {
+  [
+    { input: 1, expected: "1st" },
+    { input: 21, expected: "1st"},
+    { input: 151, expected: "1st" },
+  ].forEach(({ input, expected }) =>
+    it("should return '1st' for end with 1 except 11", () => expect(getOrdinalNumber(input)).toEqual(expected))
+  );
+});
+
+describe("Identify the ordinal number for nd", () => {
+  [
+    { input: 2, expected: "2nd" },
+    { input: 32, expected: "2nd"},
+    { input: 82, expected: "2nd" },
+  ].forEach(({ input, expected }) =>
+    it("should return '2nd' for end with 2 except 12", () => expect(getOrdinalNumber(input)).toEqual(expected))
+  );
+});
+
+describe("Identify the ordinal number for rd", () => {
+  [
+    { input: 3, expected: "3rd" },
+    { input: 1543, expected: "3rd"},
+    { input: 43, expected: "3rd" },
+  ].forEach(({ input, expected }) =>
+    it("should return '3rd' for end with 3 except 13", () => expect(getOrdinalNumber(input)).toEqual(expected))
+  );
+});
+
+describe("Identify the ordinal number for th", () => {
+  [
+    { input: 11, expected: "th" },
+    { input: 12, expected: "th"},
+    { input: 13, expected: "th" },
+    { input: 4, expected: "th" },
+  ].forEach(({ input, expected }) =>
+    it("should return 'th' for other numbers", () => expect(getOrdinalNumber(input)).toEqual(expected))
+  );
+});
