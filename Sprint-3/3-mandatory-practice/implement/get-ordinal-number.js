a62c043f52842bcbf13a7bb5e4d04c3acd908111
sedazam <sedazam@gmail.com>
2025-07-08 14:40:23 +0100

    modified:   Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -2,27 +2,20 @@ function getOrdinalNumber(num) {
   if (typeof num !== "number") {
     return undefined; // Return undefined if the input is not a number
   }
-  let result;
-  if (num === 1) {
-    return "1st";
-  } else if (num === 2) {
-    return "2nd";
-  } else if (num === 3) {
-    return "3rd";
-  } else if (num >= 4 && num <= 20) {
-    return num + "th";
-  } else if (num % 10 === 1) {
-    // % gives the last digit of the number
+  const lastTwoDigits = num % 100; // Get the last two digits of the number
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return num + "th"; // Special case for 11th, 12th, and 13th
+  }
+  const lastDigit = num % 10; // Get the last digit of the number
+  if (lastDigit === 1) {
     return num + "st";
-  } else if (num % 10 === 2) {
+  } else if (lastDigit === 2) {
     return num + "nd";
-  } else if (num % 10 === 3) {
+  } else if (lastDigit === 3) {
     return num + "rd";
   }
-  const s = ["th", "st", "nd", "rd"];
-  const v = num % 100; // Get the last two digits of the number
-  result = num + (s[(v - 20) % 10] || s[v] || s[0]); // Determine the correct suffix based on the last two digits
-  return result;
+  return num + "th"; // Default case for all other numbers
 }
 
+console.log(getOrdinalNumber(111)); // "111th"
 module.exports = getOrdinalNumber;
