5d7b8900b0d54ff303fd1d8321e1a9388e42b776
Amy-Rose <amyrlcollins@gmail.com>
2025-07-08 17:37:46 +0100

    practice/implement get ordinal number: tests written and code passes them

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,33 +1,33 @@
 function getOrdinalNumber(num) {
 
-    /* 
-    numAsStr = toString(num)
-    lastDigit = numAsStr.charAt(last.Digit.length-1)
-    
-    if (lastDigit === "1"):
-        if (number is 11):
-            return += th
-        return += st
-        
-    if (lastDigit === "2"):
-        return += nd
-
-    if (number ends in 3):
-        return += rd
-
-    if (number ends in 4,5,6,7,8,9,0):
-        return += th
-    */
-
-
-    if (num == 11) {
-    result = `${num}th`;}
-
-    else {
-    result = `${num}st`;
+  const numAsStr = num.toString();
+  const lastDigit = numAsStr.slice(-1);
+  
+  if (lastDigit === "1") { 
+    if (num % 100 === 11) {     
+        // % 100 basically 'removes' any digits that aren't in the integer or tens places, e.g. 15611 % 100 = 11, or: the remainder of 15611/100 is 11.
+      return num + "th";    // numbers ending in 11 get a different suffix
     }
+    return num + "st";  //numbers ending in 1 otherwise get st
+  }
+  
+  if (lastDigit === "2") {
+    if (num % 100 === 12) {
+      return num + "th";        // numbers ending in 12 get a different suffix 
+    }
+    return num + "nd";
+  }
+  
+  if (lastDigit === "3") {
+    if (num % 100 === 13) {
+      return num + "th";
+    }
+    return num + "rd";
+  }
+  
+  // For digits 4,5,6,7,8,9,0 or the special cases 11,12,13
+  return num + "th";
 
-    return result;
 }
 
 
--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -13,12 +13,41 @@ test("should return '1st' for 1", () => {
     });
 
 
-// 1st, 11th, 21st, 31st
-// 2nd
-// 3rd
-// 4th, 5th, 6th, 7th, 8th, 9th, 10th
+// numbers ending in 1 => st
+test.each([21,31,121])("should return 'st' for every number ending in 1, except 11", 
+    (num) => {
+    expect(getOrdinalNumber(num)).toEqual(`${num}st`);}
+);
 
-// ends in 1 => st, EXCEPT for 11 => 11th
-//ends in 2 => nd
-// end in 3 => rd
+
+// numbers ending in 2 => nd
+test.each([2,22,32,122])("should return 'nd' for every number ending in 2", 
+    (num) => {
+    expect(getOrdinalNumber(num)).toEqual(`${num}nd`);}
+);
+
+
+// numbers ending in 3 => rd
+test.each([3,23,33,123,1123])("should return 'rd' for every number ending in 3", 
+    (num) => {
+    expect(getOrdinalNumber(num)).toEqual(`${num}rd`);}
+);
+
+// 4th, 5th, 6th, 7th, 8th, 9th, 10th
+test.each([4,5,6,7,8,9,10])("should return 'th' for numbers 4-10",
+    (num) => {
+        expect(getOrdinalNumber(num)).toEqual(`${num}th`);
+    }
+);
+
+// 11th
+test("should return '11th' for the number 11", () => {
+    expect(getOrdinalNumber(11)).toEqual('11th');
+});
+
 // ends in 4,5,6,7,8,9,0 => th
+test.each([14,15,16,17,18,19,20,124,125,126,127,128,129,1230])("should return 'th' for numbers ending in 4,5,6,7,8,9 or 0", 
+    (num) => {
+        expect(getOrdinalNumber(num)).toEqual(`${num}th`);
+    }
+);
