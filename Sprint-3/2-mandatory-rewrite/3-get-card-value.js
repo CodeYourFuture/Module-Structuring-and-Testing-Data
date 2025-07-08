f6376eab94c98417ec140c346bd5d5fe9f5cc760
sedazam <sedazam@gmail.com>
2025-07-08 16:14:30 +0100

    modified:   Sprint-3/2-mandatory-rewrite/3-get-card-value.js 	modified:   Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,12 +1,11 @@
 function getCardValue(card) {
-  let rank = card.slice(0, -1); // Extract the rank part of the card
+  let rank = card.slice(0, -1);
   if (rank === "A") return 11;
-
   if (["J", "Q", "K"].includes(rank)) return 10;
-  const num = Number(rank);
-  if (!isNaN(num) && num >= 2 && num <= 10) {
-    return num;
-  }
+
+  const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  if (numbers.includes(rank)) return parseInt(rank);
   throw new Error("Invalid card");
 }
+
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,32 +1,32 @@
 const getCardValue = require("./3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
+test("should return 11 for Ace of Spades", function () {
   const aceOfSpades = getCardValue("A♠");
   expect(aceOfSpades).toEqual(11);
 });
 
 // Case 2: Handle Number Cards (2-10):
-test("should return the value of number cards (2-10)", () => {
+test("should return the value of number cards (2-10)", function () {
   expect(getCardValue("2♣")).toEqual(2);
   expect(getCardValue("5♠")).toEqual(5);
   expect(getCardValue("10♥")).toEqual(10);
 });
 
 // Case 3: Handle Face Cards (J, Q, K):
-test("should return 10 for Face cards J, Q, K", () => {
+test("should return 10 for Face cards J, Q, K", function () {
   expect(getCardValue("J♠")).toEqual(10);
   expect(getCardValue("Q♥")).toEqual(10);
 });
 
 // Case 4: Handle Ace (A):
-test("should return 11 for Ace of Diamonds", () => {
+test("should return 11 for Ace of Diamonds", function () {
   expect(getCardValue("A♦")).toEqual(11);
 });
 
 // Case 5: Handle Invalid Cards:
-test("should throw an error for invalid card", () => {
+test("should throw an error for invalid card", function () {
   expect(() => getCardValue("Z♠")).toThrow("Invalid card");
 });
-test("should throw an error for empty card", () => {
+test("should throw an error for empty card", function () {
   expect(() => getCardValue("")).toThrow("Invalid card");
 });
