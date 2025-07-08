4f7b6e10e43747b6accc2fde32f275811898c8a6
Peter Lui <pgmlui@gmail.com>
2025-07-08 14:02:44 +0100

    Update 1-get-angle-type.test.js
    updated test cases to test boundary values

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -10,27 +10,33 @@ test("should identify right angle (90°)", () => {
 // Case 2: Identify Acute Angles:
 // When the angle is less than 90 degrees,
 // Then the function should return "Acute angle"
-test("should identify right angle (45°)", () => {
+test("should identify acute angle when: 0 < angle < 90", () => {
+  expect(getAngleType(0.001)).toEqual("Acute angle");
   expect(getAngleType(45)).toEqual("Acute angle");
+  expect(getAngleType(89.999)).toEqual("Acute angle");
 });
 
 // Case 3: Identify Obtuse Angles:
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
-test("should identify right angle (125°)", () => {
+test("should identify obtuse angle when: 90 < angle < 180", () => {
+  expect(getAngleType(90.001)).toEqual("Obtuse angle");
   expect(getAngleType(125)).toEqual("Obtuse angle");
+  expect(getAngleType(179.999)).toEqual("Obtuse angle");
 });
 
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
-test("should identify right angle (180°)", () => {
+test("should identify straight angle (180°)", () => {
   expect(getAngleType(180)).toEqual("Straight angle");
 });
 
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
-test("should identify right angle (245°)", () => {
-  expect(getAngleType(245)).toEqual("Reflex angle");
+test("should identify reflex angle when: 180 < angle < 360", () => {
+  expect(getAngleType(300)).toEqual("Reflex angle");
+  expect(getAngleType(359.999)).toEqual("Reflex angle");
+  expect(getAngleType(180.001)).toEqual("Reflex angle");
 });
