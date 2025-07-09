b4f0f0c285051c33c5373ef5c18ea4662bd059b1
PandiSimatupang <pandikisaran@gmail.com>
2025-07-09 01:10:31 +0100

    solve Sprint-3/3-mandatory-practice/implement/repeat

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,10 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, rep) {
+  myStr = "";
+  for (let i = 0; i < rep; i++) {
+    myStr = myStr + str;
+  }
+  if (rep < 0) return "negative counts are not valid";
+  return myStr;
 }
 
 module.exports = repeat;
