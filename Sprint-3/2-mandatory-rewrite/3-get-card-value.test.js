7076fc0fbeb7e830ef30b5b9c0b78cf66aee724d
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-10 02:41:46 +0100

    Handled Number Cards (2-10):

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,23 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 5 for 5 of Hearts", () => {
+    const fiveofHearts = getCardValue("5♥");
+    expect(fiveofHearts).toEqual(5);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Diamonds", () => {
+    const jackOfDiamonds = getCardValue("J♦");
+    expect(jackOfDiamonds).toEqual(10);
+});
+test("should return 10 for Queen of Clubs", () => {
+    const queenOfClubs = getCardValue("Q♣");
+    expect(queenOfClubs).toEqual(10);
+});
+test("should return 10 for King of Spades", () => {
+    const kingOfSpades = getCardValue("K♠");
+    expect(kingOfSpades).toEqual(10);
+});
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
