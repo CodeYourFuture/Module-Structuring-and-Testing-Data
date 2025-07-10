ebc99d8e4cc4c31e62fee9c2d134c9cffdc00376
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-11 00:41:36 +0100

    Function was handling the case for 1st. I expanded to  handle other numbers as needed.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,29 @@
 function getOrdinalNumber(num) {
  return "1st";
 }
+// This function currently only handles the case for 1st.
+// You can expand it to handle other numbers as needed.
+// For example, you might want to add cases for 2nd, 3rd, etc., and handle exceptions for numbers like
+// 11th, 12th, and 13th which have special rules in English.
+// To do this, you can use a switch statement or if-else conditions to check the value of num and return the appropriate ordinal string.
+// Example of handling more cases:
+
+function getOrdinalNumber(num) {
 
 module.exports = getOrdinalNumber;
+    if (num === 1) {
+        return "1st";
+    } else if (num === 2) {
+        return "2nd";
+    } else if (num === 3) {
+        return "3rd";
+    } else if (num % 10 === 1 && num % 100 !== 11) {
+        return num + "st";
+    } else if (num % 10 === 2 && num % 100 !== 12) {
+        return num + "nd";
+    } else if (num % 10 === 3 && num % 100 !== 13) {
+        return num + "rd";
+    } else {
+        return num + "th";
+    }
+}
