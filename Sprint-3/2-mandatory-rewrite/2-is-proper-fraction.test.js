68f5551761bc785666e07ef3ea07e89902f273cb
Ahmad <ahmadehsas14345@gmail.com>
2025-07-10 13:36:14 +0100

    Identified Improper, Negative and equal fractions.

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -6,6 +6,15 @@ test("should return true for a proper fraction", () => {
 
 // Case 2: Identify Improper Fractions:
 
+const improperFraction = isProperFraction(5, 2);
+assertEquals(improperFraction, false); // improper fraction is the fraction where the numerator is greater than or equal to the denominator.
+
 // Case 3: Identify Negative Fractions:
 
+const negativeFraction = isProperFraction(-4, 7);
+assertEquals(negativeFraction, true); // negative fraction is the fraction where the absolute value of the numerator is less than the denominator.
+
 // Case 4: Identify Equal Numerator and Denominator:
+
+const equalFraction = isProperFraction(3, 3);
+assertEquals(equalFraction, false); // equal fraction is the fraction where the numerator is equal to the denominator.
