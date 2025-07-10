1a74ee828f29909974e5895a617ec1fc0f3113a9
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-11 00:29:37 +0100

    Implemented a function repeat that that given a target string str and a positive integer count.

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,18 @@
 function repeat() {
     return "hellohellohello";
 }
+// Implement a function repeat
+// Given a target string str and a positive integer count,
+// When the repeat function is called with these inputs,
+// Then it should repeat the str count times and return a new string containing the repeated str values.
+function repeat(str, count) {
+    // Validate the count
+    if (count < 0) {
+        throw new Error("Count must be a non-negative integer");
+    }
+    
+    // Repeat the string count times
+    return str.repeat(count);
+}
 
 module.exports = repeat;
