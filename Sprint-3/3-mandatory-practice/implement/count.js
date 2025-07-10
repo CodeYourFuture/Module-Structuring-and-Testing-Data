49b7c0030fc2aa3aa38a48ebf0642c87fc6f63a7
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-10 03:04:26 +0100

    initialized counter to keep track of occurrences

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,8 @@
 function countChar(stringOfCharacters, findCharacter) {
     return 5
+    // Initialize a counter to keep track of occurrences
+    let count = 0;
+
 }
 
 module.exports = countChar;
