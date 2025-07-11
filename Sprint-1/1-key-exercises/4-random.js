afbb1a45eff543b8037e6a05daf433b0773a702f
Nahom Mesfin <mnahom545@gmail.com>
2025-07-11 13:08:45 +0100

    random number generator

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,12 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(`The random number is: ${num}`);
+// The expression Math.random() generates a random floating-point number between 0 (inclusive) and 1 (exclusive).
+// Multiplying this by (maximum - minimum + 1) scales the range to the desired interval.
+// Adding minimum shifts the range to start from the minimum value.
+// The final result is a random integer between minimum and maximum, inclusive.
+// The expression ensures that the random number is always within the specified range of 1 to   // 100, inclusive of both endpoints.
+// The use of Math.floor() rounds down the result to the nearest whole number, ensuring that
+// the final value is an integer.
+// The final value of num will be a random integer between 1 and 100, inclusive.
