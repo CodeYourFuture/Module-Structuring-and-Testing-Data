edea88130f58c57bedc7ac55fa948876fd8dd7a9
Ahmad <ahmadehsas14345@gmail.com>
2025-07-11 23:59:46 +0100

    the test is passed for get-ordinal-number.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -8,4 +8,4 @@ console.log(getOrdinalNumber(1)); // The output should be "1st"
 // in this function, we got the ordinal number for 1.
 // we used a simple if statement to check if the number is 1 and return "1st".
 
-module.exports = getOrdinalNumber; //
+module.exports = getOrdinalNumber;
