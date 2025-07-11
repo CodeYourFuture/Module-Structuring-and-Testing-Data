1e2318bd86bbe7575b8a1c2924d97027f291d7a0
Ahmad <ahmadehsas14345@gmail.com>
2025-07-11 23:38:17 +0100

    completed the function getOrdinalNumber.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,11 @@
 function getOrdinalNumber(num) {
- return "1st";
+  if (num === 1) {
+    return "1st";
+  }
 }
+console.log(getOrdinalNumber(1)); // The output should be "1st"
 
-module.exports = getOrdinalNumber;
+// in this function, we got the ordinal number for 1.
+// we used a simple if statement to check if the number is 1 and return "1st".
+
+module.exports = getOrdinalNumber; //
