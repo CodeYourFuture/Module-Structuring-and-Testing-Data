dbf4e9252b5ebf593360e34df7fa8cab9c7fb2f3
Ahmad <ahmadehsas14345@gmail.com>
2025-07-12 00:23:22 +0100

    Implemented the function and the test was passed

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,6 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(word, times) {
+  return word.repeat(times);
 }
+console.log(repeat("hello", 3)); // The output should be "hellohellohello"
 
 module.exports = repeat;
