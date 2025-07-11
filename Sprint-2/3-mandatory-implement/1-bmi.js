b7f1a4f8c1e93edfbea89bf8b38a6001789c929b
asaniDev <asanidev9@gmail.com>
2025-07-11 20:36:14 +0100

    3-mandatory-implement done

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -16,4 +16,7 @@
 
 function calculateBMI(weight, height) {
     // return the BMI of someone based off their weight and height
+    return ((weight / (height * height)).toFixed(1));
 }
+
+console.log(calculateBMI(70, 1.73)); // Example usage
--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,9 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(inputString){
+  return inputString.replaceAll(" ", "_").toUpperCase();
+}
+
+console.log(toUpperSnakeCase("nice to meet you"));
