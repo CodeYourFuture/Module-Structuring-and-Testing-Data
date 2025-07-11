6c4aa13170c06559c4b09bd650606fbf4907cb47
Farah-Stu <selma.salam45@gmail.com>
2025-07-11 22:21:34 +0100

    updating and testing numbers

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -8,16 +8,22 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
+    test("append 'st' to numbers ending in 1" , () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
+    expect(getOrdinalNumber(21)).toEqual("21st");
+    expect(getOrdinalNumber(121)).toEqual("121st"); 
     });
 
-    test("should return '2nd' for 2", () => {
+    test("append 'nd' to numbers ending in 2, except those ending in 12", () => {
         expect(getOrdinalNumber(2)).toEqual("2nd");
+        expect(getOrdinalNumber(22)).toEqual("22nd");
+        expect(getOrdinalNumber(132)).toEqual("132nd");
     });
 
-    test("should return '23rd' for 23", () => {
+    test("append 'rd' to numbers ending in 3 , except those ending in 13", () => {
         expect(getOrdinalNumber(23)).toEqual("23rd");
+        expect(getOrdinalNumber(13)).toEqual("13th");
+        expect(getOrdinalNumber(3)).toEqual("3rd");
     });
 
     test("should return '11th' for 11", () => {
