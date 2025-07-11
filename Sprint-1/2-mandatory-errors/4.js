05215f52be359ffaffe82fdc6063be11939d9cf5
Nahom Mesfin <mnahom545@gmail.com>
2025-07-11 14:11:44 +0100

    variable names

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+// variable names cannot start with a number
+const twelveHourClockTime = "20:53";   // valid and readable
+const twentyFourHourClockTime = "08:53"; // valid and readable
+console.log(`12-hour clock time: ${twelveHourClockTime}`);
+console.log(`24-hour clock time: ${twentyFourHourClockTime}`);
