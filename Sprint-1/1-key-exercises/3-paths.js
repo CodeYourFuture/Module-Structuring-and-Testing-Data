635a1a0e326b47155dd4468316a0bf96e4913dc6
Surafel <surafealaw@gmail.com>
2025-07-12 11:15:40 +0100

    Update 3-paths.js
    cleaning the comments of sprint-1 works on sprint-3

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -15,17 +15,9 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-
-
-const dir = filePath.slice(0, lastSlashIndex);// The code uses the lastIndexOf method to find the position of the last slash in;
-console.log(`The dir part of ${filePath} is ${dir}`);
-filePath.slice(0, lastSlashIndex);
-
-filePath
-  .slice(lastSlashIndex + 1)
-  .split(".")
-  .pop();
 // Create a variable to store the ext part of the variable
 
-const ext=base.slice(base.lastIndexOf(".") + 1); // The code uses the lastIndexOf method to find the position of the last dot in the base variable.
-console.log(`The ext part of ${filePath} is ${ext}`); 
+const dir = ;
+const ext = ;
+
+// https://www.google.com/search?q=slice+mdn
