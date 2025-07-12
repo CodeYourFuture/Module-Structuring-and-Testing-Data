2fc4c9090bbea1089e88625fefad24edbcdeb065
Surafel <surafealaw@gmail.com>
2025-07-12 11:49:21 +0100

    Update 3-paths.js

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -12,19 +12,16 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
 
-// Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
-
-const dir = filePath;
+const ext=base.slice(base.lastIndexOf(".") + 1); // The code uses the lastIndexOf method to find the position of the last dot in the base variable.
+// Create a variable to store the dir part of the filePath variable
+const dir = filePath.slice(0, lastSlashIndex);// The code uses the lastIndexOf method to find the position of the last slash in;
 filePath.slice(0, lastSlashIndex);
-const ext = filePath;
 filePath
   .slice(lastSlashIndex + 1)
   .split(".")
   .pop();
-// The code above uses the lastIndexOf method to find the position of the last slash in the filePath string.
-// The slice method is then used to extract the base part of the filePath string, which is the part after the last slash.
-
-// https://www.google.com/search?q=slice+mdn
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The ext part of ${filePath} is ${ext}`); 
