017c77fe59231a24112fff9cabdff03d18ede537
SalehOmar-Y <saleh.omar.uk.com>
2025-07-12 15:20:18 +0100

    prep deleted

--- prep/age.js
+++
@@ -1,7 +0,0 @@
-const yearOfBirth = 1960; // declaration
-let currentYear = 2025; // declaration
-
-console.log(`I am ${currentYear - yearOfBirth} years old`);
- // statement
-// This statement will print the age in the terminal
-// The output will be "I am 65 years old" if the current year is 2025   
--- prep/arithmetic.js
+++
@@ -1,4 +0,0 @@
-const result = Math.round(10.3);
-const result2 = Math.round(10.7);
-console.log(result);  // Output: 10
-console.log(result2); // Output: 11
