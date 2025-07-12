19e006e21794d08d327cc5ea1fc0aa37dc8ae8fa
Farah-Stu <selma.salam45@gmail.com>
2025-07-12 10:33:36 +0100

    updating the card value code

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -8,17 +8,13 @@ test("should return number for number cards (2-10)",() => {
     expect(getCardValue("10♣")).toEqual(10);
 });
 // Case 3: Handle Face Cards (J, Q, K):
-test("should return 10 for Jack of Diamonds", () => {
-    expect(getCardValue ("J♦")).toEqual(10);
+test("should return 10 for face cards J, Q, K", () => {
+    ["J", "Q", "K"].forEach(rank => {
+    expect(getCardValue (`${rank}♦`)).toEqual(10);
+    });
 });
 
-test("should return 10 for Queen of Clubs", () => {
-    expect(getCardValue("Q♣")).toEqual(10);
-});
 
-test("should return 10 for King of Spades", () => {
-    expect(getCardValue("K♠")).toEqual(10);
-});
     
 // Case 4: Handle Ace (A):
 test("should return 11 for Ace", () => {
@@ -29,4 +25,3 @@ test("should return 11 for Ace", () => {
 test("should throw error for invalid card rank", () => {
     expect(() => getCardValue("L")).toThrow(new Error("Invalid card rank"));
 });
-
