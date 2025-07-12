2f51f5babd9a4e62e440216f26d4a987523fb042
Ahmad <ahmadehsas14345@gmail.com>
2025-07-12 17:23:41 +0100

    Replaced the word Const line to Const Variable on 9.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,10 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
 
 // Reference error:can not access "cityOfBirth" before initialization
-// in this code the variable "cityOfBirth" is used before it is been declared" 
-// solution: Move  the const line  above  the console.log
+// in this code the variable "cityOfBirth" is used before it is been declared"
+// solution: Move  the const variable  above  the console.log
