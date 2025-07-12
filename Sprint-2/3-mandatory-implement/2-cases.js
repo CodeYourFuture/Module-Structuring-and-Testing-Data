6e2a97e5e8bbb08738555fc79bfc70f909850ef8
Ahmad <ahmadehsas14345@gmail.com>
2025-07-12 21:27:34 +0100

    Implemented the function called toUpperSnakeCase.

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -11,6 +11,17 @@
 
 // Another example: "lord of the rings" should be "LORD_OF_THE_RINGS"
 
+function toUpperSnakeCase(input) {
+  return input.toUpperCase().replace(/ /g, "_");
+}
+
+console.log(toUpperSnakeCase("hello there")); //  "HELLO_THERE"
+console.log(toUpperSnakeCase("lord of the rings")); //  "LORD_OF_THE_RINGS"
+// in the above code, we are using the toUpperCase method to convert the string to uppercase
+// and then using  the replace method to replace spaces with underscores.
+// This is a simple implementation of the function  to convert a string to UPPER_SNAKE_CASE.
+// here replace(/ /g, "_") is used to replace all spaces in the string with underscores.
+
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
