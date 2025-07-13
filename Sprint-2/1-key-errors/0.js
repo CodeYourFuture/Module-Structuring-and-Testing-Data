d957dd1b593d800996c9496518224ca81a8f3ea3
Samiuk <lahlahhoussam80@gmail.com>
2025-07-13 15:38:37 +0100

    Avoid redeclaring 'str' inside capitalise function

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> // =============> write your prediction here
+//  I predict the code will throw an error because the variable `str` is being declared twice in the same function scope.
+
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -10,4 +12,16 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+// The function already receives `str` as a parameter. 
+// Inside the function, it tries to declare `let str = ...`, 
+// which causes a conflict because JavaScript does not 
+// allow redeclaring the same variable name in the same scope using `let`. 
+// This leads to a `SyntaxError: Identifier 'str' has already been declared`.
+
+
 // =============> write your new code here
+// =============> write your new code here
+function capitalise(str) {
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisedStr;
+}
