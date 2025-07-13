eed725e48d1c4222e229487b1f2d95071dc9bd66
Surafel <surafealaw@gmail.com>
2025-07-14 00:23:36 +0100

    Update 2-is-proper-fraction.js
    add condition to heck if denominator is zero

--- Sprint-3/1-key-implement/2-is-proper-fraction.js
+++ Sprint-3/1-key-implement/2-is-proper-fraction.js
@@ -8,6 +8,10 @@
 // write one test at a time, and make it pass, build your solution up methodically
 
 function isProperFraction(numerator, denominator) {
+  if (denominator === 0) {
+    // Denominator can't be zero in a valid fraction
+    return false;
+  }
   // if (numerator < denominator) return true; it will not work for negative numbers
   return Math.abs(numerator) < Math.abs(denominator);
 }
