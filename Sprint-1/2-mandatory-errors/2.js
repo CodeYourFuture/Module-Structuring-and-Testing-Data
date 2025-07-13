d477732a5048cd134a81b472e941003b3b47b9f0
Samiuk <lahlahhoussam80@gmail.com>
2025-07-13 11:38:40 +0100

    move variable declaration before use to avoid ReferenceError

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?   The error here is due to using a variable before it's declared.
 
-console.log(`I was born in ${cityOfBirth}`);
+
+// Fixed: Declare the variable before using it
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
