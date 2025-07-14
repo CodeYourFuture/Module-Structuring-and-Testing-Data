17c240eb562a6d0b8b08c11a790eca8e82f0ae1a
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-14 17:11:22 +0100

    added jest to main directory and createted test with jest for angle type function

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -10,15 +10,21 @@ test("should identify right angle (90°)", () => {
 // Case 2: Identify Acute Angles:
 // When the angle is less than 90 degrees,
 // Then the function should return "Acute angle"
-
+test("should identify actute angle (55°)", () => {
+  expect(getAngleType(55)).toEqual("Acute angle");
+});
 // Case 3: Identify Obtuse Angles:
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
-
+test("should identify obtuse angle (95°)", () => {
+  expect(getAngleType(95)).toEqual("Obtuse angle");
+});
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
-
+test("should identify straight angle (180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
