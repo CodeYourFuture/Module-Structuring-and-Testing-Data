2456dd397841e0cf1a7f41922067bfc895c8b716
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-14 20:54:46 +0100

    created test for card value function using jest

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,11 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+  // replace with your code from key-implement
+  let rank = card.slice(0, -1);
+
+  if (rank === "A") return 11;
+  else if (rank === "K" || rank === "Q" || rank === "J" || rank === "10")
+    return 10;
+  else if (Number(rank) >= 2 && Number(rank) <= 9) return Number(rank);
+  else return "Invalid card rank.";
 }
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,35 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
+  const aceofSpades = getCardValue("A♠");
+  expect(aceofSpades).toEqual(11);
+});
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 10 for 10 of Hearts", () => {
+  const tenOfHearts = getCardValue("10♥");
+  expect(tenOfHearts).toEqual(10);
+});
+test("should return 5 for 10 of Spades", () => {
+  const fiveOfHearts = getCardValue("5♠");
+  expect(fiveOfHearts).toEqual(5);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Kings of Spades", () => {
+  const tenOfHearts = getCardValue("K♠");
+  expect(tenOfHearts).toEqual(10);
+});
+test("should return 10 for Queen of Hearts", () => {
+  const queenOfHearts = getCardValue("Q♥");
+  expect(queenOfHearts).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Hearts", () => {
+  const AceOfHearts = getCardValue("A♥");
+  expect(AceOfHearts).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should return Invalid Card for letters", () => {
+  const AceOfHearts = getCardValue("SS");
+  expect(AceOfHearts).toEqual("Invalid card rank.");
+});
