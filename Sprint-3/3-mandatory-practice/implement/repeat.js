4f777e011b283910aad0536d9779c8e5d45b89a5
avatarit <salah@ait.ps>
2025-07-14 01:57:39 +0100

    Implement repeat function with input validation and update tests for negative count handling

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,13 @@
+function repeat(str, times) {
+    if (typeof str !== 'string' || typeof times !== 'number' || times < 0) {
+        throw new Error("Invalid input: str must be a string and times must be a non-negative integer");
+    }
 
-function repeat() {
-    return "hellohellohello";
+    if (times === 0) {
+        return '';
+    }
+
+    return str.repeat(times);
 }
+
 module.exports = repeat;
