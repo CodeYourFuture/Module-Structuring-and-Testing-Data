790e134cb2a452b368444ebae8c2eb228ddfbf89
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-14 18:52:21 +0100

    created test using jest for is proper fractions

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,20 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
-
+test("should return flase for inproper fraction", () => {
+  expect(isProperFraction(256, 40)).toEqual(false);
+});
+test("should return flase for inproper fraction", () => {
+  expect(isProperFraction(8, 4)).toEqual(false);
+});
 // Case 3: Identify Negative Fractions:
-
+test("should return true for negative fraction (-2,4)", () => {
+  expect(isProperFraction(-2, 4)).toEqual(true);
+});
+test("should return false for negative fraction (-10,-4)", () => {
+  expect(isProperFraction(-10, -4)).toEqual(false);
+});
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return flase for inproper fraction", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
