fbf11cc18f73b9c0352e1b13e45d4c5f3d5c7045
Tarawally <41825140+Tarawally@users.noreply.github.com.>
2025-07-14 23:26:52 +0000

    mandatory errors

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// Constants are not allowed to be reassigned, let fixes this issue.
+let age = 33;
 age = age + 1;
+console.log(age);
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
+// Variable names in JavaScript cannnot start with a number.
+// This code throws a SyntaxError when run.
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+// The variable names should be vaild identifiers e.g.
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
