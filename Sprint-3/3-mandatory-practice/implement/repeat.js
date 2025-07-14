dc7f37535f6619fe37b96edf5b3532e96fbcb1ea
avatarit <salah@ait.ps>
2025-07-14 04:47:49 +0100

    Fix error message for negative count in repeat function and update corresponding test case

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,13 +1,2 @@
 function repeat(str, times) {
-    if (typeof str !== 'string' || typeof times !== 'number' || times < 0) {
-        throw new Error("Invalid input: str must be a string and times must be a non-negative integer");
-    }
-
-    if (times === 0) {
-        return '';
-    }
-
-    return str.repeat(times);
-}
-
-module.exports = repeat;
+    if (typeof str !== 'string' || typeof times
--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -44,5 +44,4 @@ test("should repeat the string count times", () => {
 test("throws error for negative count", () => {
     const str = "Hey";
     const count = -3;
-    expect(() => repeat(str, count)).toThrow("Invalid input: str must be a string and times must be a non-negative integer");
-});
+    expect(() => repeat(str, count)).toThrow("Invalid input: str must be a string and times must be a non-negative int
