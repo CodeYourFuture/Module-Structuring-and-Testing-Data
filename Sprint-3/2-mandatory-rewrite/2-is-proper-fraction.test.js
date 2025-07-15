188ee091ba2824163afe9a45532bfa06de6348ad
asaniDev <asanidev9@gmail.com>
2025-07-16 00:03:04 +0100

    mandatory rewrite task done

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,20 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 3)).toEqual(false);
+})
 
 // Case 3: Identify Negative Fractions:
+test("Should return true for a negative proper fraction", () => {
+  expect(isProperFraction(-1, 2)).toEqual(true);
+})
+
+test("Should return false for a negative improper fraction", () => {
+  expect(isProperFraction(-3, 2)).toEqual(false);
+})
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("Should return false for equal numerator and denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+})
