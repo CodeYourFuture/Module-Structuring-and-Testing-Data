5f11640a61d2fb4f8bcadf2c8832e1e33193b9e3
Tarawally <41825140+Tarawally@users.noreply.github.com.>
2025-07-15 15:00:33 +0000

    docs(mandatory-errors): restructure inline comment for clarity

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,6 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-// Varibles must be declared before they are used.
+// Variables must be declared before they are used.
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,8 +1,8 @@
-// Variable names in JavaScript cannnot start with a number.
+// Variable names in JavaScript cannot start with a number.
 // This code throws a SyntaxError when run.
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
 
-// The variable names should be vaild identifiers e.g.
+// The variable names should be valid identifiers, e.g.
 const twelveHourClockTime = "20:53";
 const twentyFourHourClockTime = "08:53";
