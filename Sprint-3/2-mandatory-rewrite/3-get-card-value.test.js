3c1595e38ed6b6bdd9730cc50ef96baa4935fffb
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-16 11:12:11 +0100

    Add tests for getCardValue including edge cases and errors

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,27 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return numeric value for number cards", () => {
+  expect(getCardValue("2♦")).toEqual(2);
+  expect(getCardValue("10♥")).toEqual(10);
+  expect(getCardValue("7♣")).toEqual(7);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack, Queen, and King", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♦")).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Hearts", () => {
+  expect(getCardValue("A♥")).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+  expect(() => getCardValue("1♠")).toThrow("Invalid card rank.");
+  expect(() => getCardValue("Z♣")).toThrow("Invalid card rank.");
+  expect(() => getCardValue("11♦")).toThrow("Invalid card rank.");
+  expect(() => getCardValue("")).toThrow("Invalid card rank.");
+});
+
