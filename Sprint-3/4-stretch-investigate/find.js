ea002b3d185e2309cef4a85921ca96589d323065
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-16 15:47:58 +0100

    add while loop character finder with explanatory comments

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -10,8 +10,27 @@ function find(str, char) {
   return -1;
 }
 
+function find(str, char) {
+  let index = 0; // Start checking from the first character in the string
+
+  // Loop through each character until the end of the string
+  while (index < str.length) {
+    // Check if the current character matches the one we're looking for
+    if (str[index] === char) {
+      return index; // If found, return its position
+    }
+    index++; // Move to the next character
+  }
+
+  return -1; // If the character wasn't found, return -1
+}
+
+
 console.log(find("code your future", "u"));
 console.log(find("code your future", "z"));
+
+console.log(find("code your future", "u")); // Should return 7 (first "u")
+console.log(find("code your future", "z")); // Should return -1 (not found)
 
 // The while loop statement allows us to do iteration - the repetition of a certain number of tasks according to some condition
 // See the docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
