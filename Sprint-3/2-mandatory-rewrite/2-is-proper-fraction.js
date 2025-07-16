37729c9e2c01dfdc55d2771065d6fe2530faea95
Innocent_Niwatwine <innocent.pare3@gmail.com>
2025-07-16 21:36:54 +0300

    Rewriting to simplify a function

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,7 +1,6 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
-  // add your completed function from key-implement here
-  if (numerator >= denominator) return false;
+  if (numerator < 0 || denominator < 0) return false;
+  return numerator < denominator;
     // add your completed function from key-implement here
 }
 
