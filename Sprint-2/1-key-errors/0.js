22e00ee7b9db079fb67e671de91b0ef0b8d0d33d
Tarawally <41825140+Tarawally@users.noreply.github.com.>
2025-07-16 17:02:32 +0000

    docs(errors-0): Predict and explain error in capitalise function

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,15 +1,15 @@
 // Predict and explain first...
-//  =============> Capatlises the first letter of a string.
+//  =============> Capitalises the first letter of a string.
 
-// call the function capitalise with a string input
-// interpret the error message and figure out why an error is occurring
+// Call the function capitalise with a string input.
+// Interpret the error message and work out why an error is occurring.
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> The variable 'str' is being declared twice, once as a parameter and again with 'let' resulting in a syntax error.
+// =============> The variable 'str' is being declared twice, once as a parameter and again with 'let', resulting in a syntax error.
 // =============> function capitalise(str) {
 // =============>   let word = `${str[0].toUpperCase()}${str.slice(1)}`;
 // =============>   return word;
