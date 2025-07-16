4f1c3fc489573065e3f51207fe5950be8f062c43
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-16 12:41:12 +0100

    Implement countChar function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,16 @@
-function countChar(stringOfCharacters, findCharacter) {
-    return 5
-}
+// The test should fail because the current function always returns 5.
+// function countChar(stringOfCharacters, findCharacter) {
+//  return 5
+// }
 
-module.exports = countChar;
+// module.exports = countChar;
+// Here the placeholder(5) is replaced to implement the function to pass the tests
+function countChar(stringOfCharacters, findCharacter) {
+    let count = 0;
+    for (let char of stringOfCharacters) {
+      if (char === findCharacter) count++;
+    }
+    return count;
+  }
+  
+  module.exports = countChar;
