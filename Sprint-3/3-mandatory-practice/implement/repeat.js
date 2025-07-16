b1775b24c1773f217dd7d37320a3b9385aeb3d02
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-16 20:38:36 +0100

    added functionality for repeat function that takes into account negative values.

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,6 +1,10 @@
 function repeat(str, count) {
+  if (count < 0) {
+    return "Negative numbers are not accepted";
+  }
   return str.repeat(count);
 }
 
+console.log(repeat("hello", -100));
 console.log(repeat("hell0", 0));
 module.exports = repeat;
