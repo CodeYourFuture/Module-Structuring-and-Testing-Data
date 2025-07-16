87e59b4ae08462042251d2c627fcc23345bc389d
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-16 18:14:07 +0100

    Add positive test case to ensure valid card numbers are accepted.

--- Sprint-3/4-stretch-investigate/card-validator.test.js
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -7,7 +7,7 @@ const cardNumberValidator = require("./card-validator");
 
 // case 1: check the card number has 16 digits
 test("card number should be 16 digits", () => {
-    expect(cardNumberValidator(12345678901234560)).toEqual(false);
+    expect(cardNumberValidator("12345678901234560")).toEqual(false);
 });
 
 // case 2: check all digits of the card number has to be numbers.
@@ -29,4 +29,10 @@ test("The sum of all digits should be grater than 16", () => {
 test("The final digits should be even", () => {
     expect(cardNumberValidator("1234567891234561")).toEqual(false);
 });
+
+// case 6: "valid card number should return true"
+test("valid card number should return true", () => {
+  expect(cardNumberValidator("1234567890123468")).toEqual(true);
+});
+
 
