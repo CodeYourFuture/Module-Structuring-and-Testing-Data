c72f1cb86da9fcefbb020b8735655b3cc2cbf0af
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-16 20:22:13 +0100

    created test for getOrdinal number function using Jest

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -3,7 +3,7 @@ function getOrdinalNumber(num) {
   if (lastTwoNumber === 11 || lastTwoNumber === 12 || lastTwoNumber === 13)
     return `${num}th`;
   let lastDigit = num % 10;
-  if (lastDigit === 1) return ` ${num}st`;
+  if (lastDigit === 1) return `${num}st`;
   else if (lastDigit === 2) return `${num}nd`;
   else if (lastDigit === 3) return `${num}rd`;
   else return `${num}th`;
