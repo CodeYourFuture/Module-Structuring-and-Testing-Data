9865786f9404613cad6a21e2881ed0b4266b0850
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-16 14:16:57 +0100

    add test cases for getOrdinalNumber including exceptions

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -11,3 +11,28 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
     });
+    
+test("should return '2nd' for 2", () => {
+        expect(getOrdinalNumber(2)).toEqual("2nd");
+      });
+      
+ test("should return '3rd' for 3", () => {
+        expect(getOrdinalNumber(3)).toEqual("3rd");
+      });
+      
+ test("should return '4th' for 4", () => {
+        expect(getOrdinalNumber(4)).toEqual("4th");
+      });
+      
+ test("should return '11th', '12th', '13th' for exceptions", () => {
+        expect(getOrdinalNumber(11)).toEqual("11th");
+        expect(getOrdinalNumber(12)).toEqual("12th");
+        expect(getOrdinalNumber(13)).toEqual("13th");
+      });
+      
+ test("should return '21st', '22nd', '23rd', '24th'", () => {
+        expect(getOrdinalNumber(21)).toEqual("21st");
+        expect(getOrdinalNumber(22)).toEqual("22nd");
+        expect(getOrdinalNumber(23)).toEqual("23rd");
+        expect(getOrdinalNumber(24)).toEqual("24th");
+      });
