aec46fd19a957a32cd438931fe17a4f15cc47f91
Nahom Mesfin <mnahom545@gmail.com>
2025-07-16 15:13:19 +0100

    time-format

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,23 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// 3 times, once for each of hours, minutes, and seconds - pad(totalHours), pad(remainingMinutes), and pad(remainingSeconds).
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// The value assigned to num when pad is called for the first time is 0.
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// The return value of pad when called for the first time is "00", because it converts the number 0 to a string and pads it with leading zeros to ensure it has a length of at least 2 characters.
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The value assigned to num when pad is called for the last time in this program is 1. This is because the last call to pad is for remainingSeconds, which is calculated as 61 % 60, resulting in 1. The function then pads this value to ensure it has a length of at least 2 characters, resulting in "01".
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The return value assigned to num when pad is called for the last time in this program is "01". This is because the function converts the number 1 to a string and pads it with leading zeros to ensure it has a length of at least 2 characters, resulting in "01".
+// This is the final output for the seconds part of the time format, which is displayed as part of the formatted time string.
