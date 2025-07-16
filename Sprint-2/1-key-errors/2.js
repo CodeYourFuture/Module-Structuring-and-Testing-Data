fcb215c4f86f0f38f81c5b4f3cc39558970c21ea
Abayie <jonathanboahene@gmail.com>
2025-07-17 00:38:32 +0100

    2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,24 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// The code will throw a syntax error because you cannot use a number as a function parameter.
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
+
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// Function parameters must be variable names, not values. Using a number as a parameter is invalid syntax in JavaScript -mdn
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
-
+console.log(square(7));
