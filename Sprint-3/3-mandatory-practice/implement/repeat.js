4e1421abd9983bcb215baa4a4df22da8430ea1f6
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-16 17:07:12 +0100

    Refactor repeat function formatting for clarity

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,8 +1,7 @@
 function repeat(str, count) {
-  if (count == 1) return `${str}`;
+  if (count > 0) return `${str.repeat(count)}`;
   else if (count == 0) return "";
   else if (count < 0) return "negative counts are not valid";
-  else return `${str.repeat(count)}`;
 }
 
 module.exports = repeat;
