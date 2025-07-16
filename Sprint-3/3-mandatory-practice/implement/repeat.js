d8530a7abf900df2051cb3db580e2614e3f52be4
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-16 14:25:47 +0100

    implement repeat function with validation

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,15 @@
-function repeat() {
-    return "hellohellohello";
-}
+// function repeat() {
+//  return "hellohellohello";
+// }
 
-module.exports = repeat;
+// module.exports = repeat;
+
+function repeat(str, count) {
+    if (count < 0) {
+      throw new Error("Count must be a non-negative integer");
+    }
+  
+    return str.repeat(count);
+  }
+  
+  module.exports = repeat;
