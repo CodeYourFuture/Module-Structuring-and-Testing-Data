8665943bf7d014e26a590ea539a87d2ea5fa41c4
PandiSimatupang <pandikisaran@gmail.com>
2025-07-16 22:46:56 +0100

    change const to let Sprint-1/2-mandatory-errors/1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,7 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
 //create variable age
-const age = 33;
+let age = 33;
 
 //reassign variable age by 1 (+1)
 age = age + 1;
+
+console.assert(age); //Oh no.. that is mr. const
