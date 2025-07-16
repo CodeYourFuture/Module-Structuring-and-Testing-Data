9e994f7be8de4c1b81c8072142535377f01ca97a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-16 21:55:19 +0100

    adjusted tests and code according to review

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,11 +1,18 @@
 function getAngleType(angle) {
+  if (angle === 0) return "Not an angle";
+  if (angle > 0 && angle < 90) return "Acute angle";
   if (angle === 90) return "Right angle";
-  if (angle < 90) return "Acute angle";
   if (angle > 90 && angle < 180) return "Obtuse angle";
   if (angle === 180) return "Straight angle";
-  if (angle > 180 && angle <= 360) return "Reflex angle";
-  // replace with your completed function from key-implement
+  if (angle > 180 && angle <= 360) return "Reflex angle"; 
+  if (angle >360) return "Revolutional angle";
+  
+    
+  
 
+ 
+  
+  // replace with your completed function from key-implement.
 }
 
 
--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -1,28 +1,36 @@
 const getAngleType = require("./1-get-angle-type");
 
-test("should identify right angle (90°)", () => {
+test("should return not an angle (angle=0)", () => {
+  expect(getAngleType(0)).toEqual("Not an angle");
+});
+
+test("should identify right angle (angle=90°)", () => {
   expect(getAngleType(90)).toEqual("Right angle");
 });
 
 // REPLACE the comments with the tests
-// make your test descriptions as clear and readable as possible
+// make your test descriptions as clear and readable as possible.
 
 
-test("should identify acute angle(45°)",()=>{
+test("should identify acute angle (0 < angle < 90)",()=>{
   expect(getAngleType(45)).toEqual("Acute angle");
 });
 
 
-test("should indentify obtuse angle(120°)",()=>{
+test("should identify obtuse angle(90° < angle < 180°)",()=>{
   expect(getAngleType(120)).toEqual("Obtuse angle")
 });
 
 
-test("should identify straight angle(180°)",()=>{
+test("should identify straight angle(angle=180°)",()=>{
   expect(getAngleType(180)).toEqual("Straight angle")
 });
 
 
-test("should identify reflex angle(230°)",()=>{
+test("should identify reflex angle(180<angle<=360)",()=>{
   expect(getAngleType(230)).toEqual("Reflex angle")
+});
+
+test("should identify revolutional angle(angle>360)", () => {
+  expect(getAngleType(390)).toEqual("Revolutional angle");
 });
