c2bb55da0bcdc6e28130d275e817f689c279e9b9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-17 10:51:23 +0100

    updated tests and code to include all possibilities

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -2,20 +2,24 @@ function getOrdinalNumber(num) {
     if (isNaN(num)) {
       return NaN;
     }
-if(num>=11 && num<=13){
-    return num+"th"
+     
+const strNum=String(num);
+const lastDigit=Number(strNum.slice(-1));
+const lastTwoDigits=Number(strNum.slice(-2));
+
+if (lastTwoDigits >= 11 && lastTwoDigits <=13) return num + "th";
+
+if(lastDigit===1)return num + "st";
+    
+if ( lastDigit===2)  return num + "nd";
+ 
+ if (lastDigit === 3) return num + "rd";
+
+ return num+"th"
+  
 }
 
-if(num===1){
-    return num+"st"
-}
-if (num === 2) {
-  return num + "nd";
-}
-if (num === 3) {
-  return num + "rd";
-}
-return (num)+"th"
-}
 
 module.exports = getOrdinalNumber;
+
+
--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -6,34 +6,45 @@ const getOrdinalNumber = require("./get-ordinal-number");
 
 // Case 1: Identify the ordinal number for 1
 // When the number is 1,
-// Then the function should return "1st"
+// Then the function should return "1st".
 
-test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+test("append 'st' to numbers ending in 1, except those ending in 11", () => {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(141)).toEqual("141st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+});
 // Case 2: Identify the ordinal number for 2
 // When the number is 2,
 // Then the function should return "2nd"
-test("should return '2nd' for 2", () => {
+test("append 'nd' to numbers ending in 2, except those ending in 12", () => {
   expect(getOrdinalNumber(2)).toEqual("2nd");
-});
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(132)).toEqual("132nd");
+  });
 // Case 3: Identify the ordinal number for 3
 // When the number is 3,
 // Then the function should return "3rd"
-test("should return '3rd' for 3", () => {
-    expect(getOrdinalNumber(3)).toEqual("3rd");
-  });
+test("append 'rd' to numbers ending in 3, except those ending in 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(133)).toEqual("133rd");
+
+});
   // Case 4: Identify the ordinal number for 11,12,13
 // When the number is 11,12,13,
 // Then the function should return "11th","12th","13th"
-test("should return '11th' for 11", () => {
-    expect(getOrdinalNumber(11)).toEqual("11th");
-  });
+// test("should append 'th' to numbers ending in:10,11,12,13 ", () => {
+//     expect(getOrdinalNumber(11)).toEqual("11th");
+//     expect(getOrdinalNumber(10)).toEqual("10th");
+//     expect(getOrdinalNumber(13)).toEqual("13th");
+//   });
     // Case 5: Identify the ordinal number for 6
 // When the number is 6
 // Then the function should return "6th"
-test("should return '6th' for 6", () => {
+test("should append 'th' to numbers whose last digits are between 4 and 13 inclusive", () => {
     expect(getOrdinalNumber(6)).toEqual("6th");
+    expect(getOrdinalNumber(19)).toEqual("19th");
+    expect(getOrdinalNumber(204)).toEqual("204th");
   });
 // Case 6: Identify NaN
 // When the number is NaN
