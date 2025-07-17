dc23cbf508d24cdc28ff0a0d3610926053498b6a
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-17 23:35:49 +0100

    Revert unintended changes in Sprint-1 files

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -10,20 +10,14 @@
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
-
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
-const dir = filePath.slice(0, lastSlashIndex);
-const lastDotIndex = base.lastIndexOf(".");
-const ext = base.slice(lastDotIndex + 1);
-
-console.log(`The directory part is: ${dir}`);
-console.log(`The file extension is: ${ext}`);
-
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
+const dir = ;
+const ext = ;
 
 // https://www.google.com/search?q=slice+mdn
--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -9,19 +9,6 @@ const percentageChange = (priceDifference / carPrice) * 100;
 
 console.log(`The percentage change is ${percentageChange}`);
 
-// carPrice = Number(carPrice.replaceAll(",", ""));
-// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
-// const percentageChange = (priceDifference / carPrice) * 100;
-
-// There is no function being called in this line above.
-// This is just a mathematical expression that divides 
-// priceDifference by carPrice, then multiplies by 100 to get a percentage.
-// Because a function call in JavaScript is something that looks like,
-// functionName(...)
-// But in  this (priceDifference / carPrice) * 100, there are no parentheses
-// after a function name or no function names being used here.
-// So this line does not contain a function call.
-
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
