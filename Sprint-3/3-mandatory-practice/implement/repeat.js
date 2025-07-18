5f6f46d80ba110e8c2e272c93fb90d7ccdeb008b
Mirabel Adom <mirabel.adom@gmail.com>
2025-07-18 19:22:48 +0100

    repeat.js - answered

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,13 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (typeof str !== "string") {
+    throw new Error("First argument must be a string");
+  }
+
+  if (typeof count !== "number" || count < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
+
+  return str.repeat(count);
 }
 
 module.exports = repeat;
