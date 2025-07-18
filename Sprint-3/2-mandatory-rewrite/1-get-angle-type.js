d062af51758bb49bbe93e999abd1b66f003e3d31
Mirabel Adom <mirabel.adom@gmail.com>
2025-07-18 19:10:33 +0100

    1-get-angle-type.js - new update

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,7 +1,10 @@
 function getAngleType(angle) {
-  if (angle === 90) return "Right angle";
-  if (angle === 45) return "Acute angle";
-  if (angle === 120) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
+  if (angle > 0 && angle < 90) return "Acute angle";
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle > 180 && angle < 360) return "Reflex angle";
+
+  return "Invalid angle";
 }
 
 // Don't get bogged down in this detail
