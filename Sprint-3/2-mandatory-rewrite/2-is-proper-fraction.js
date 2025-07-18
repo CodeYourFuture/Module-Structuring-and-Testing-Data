5f9ef73b9b8a6843e520b7b7b065dbfda665ff55
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-18 01:03:42 +0100

    refactor my if-statement for better readability

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,7 +1,5 @@
 function isProperFraction(numerator, denominator) {
   if (Math.abs(numerator) < Math.abs(denominator)) return true;
-  else if (numerator === denominator) return false;
-  else if (numerator > denominator) return false;
   else return false;
 }
 
