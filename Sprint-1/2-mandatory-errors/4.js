12703520a8fb15c549e71374117725310e4218d2
pathywang <pathywang@gmail.com>
2025-07-19 12:22:02 +0100

    error correct

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -4,5 +4,5 @@ const 24hourClockTime = "08:53";
 // cost 12HourClockTime = "20.53" is wrong code because 20 is more than 12 and variable name can not start with digital in JavaScript.
 // The right coe should be:
 
-const TwelveHourClockTime = "8:53"
+const TwelveHourClockTime = "08:53"
 const TwentyFourHourClockTime = "20:53"
