9ab417a47c7eef7171da8d80c359bd7018b1c8ac
pathywang <pathywang@gmail.com>
2025-07-19 12:53:09 +0100

    code ijmprovement

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,13 @@
-/"hello Mhairi" === `hello ${mhairiName}`;
+"hello Mhairi" === `hello ${mhairiName}`;
 "${mhairiName} is 28" === `Mhairi is ${mhairiAge}`;
 
 // My answer is:
 const mhairiName = "Mhairi";
 const mhairiAge = 28;
 
-const sentence1= `hello ${mhairiName}`;
-console.log(sentence1); // ➜ Output: hello Mhairi
+const sentence1= `Hello ${mhairiName},nice to meet you`;
+console.log(sentence1); // ➜ Output: Hello Mhairi, nice to meet you
 
 
-const sentence= `${mhairiName} is ${mhairiAge}`;
-console.log(sentence);  // ➜ Output: Mhairi is 28
+const sentence= `${mhairiName} is ${mhairiAge} years old`;
+console.log(sentence);  // ➜ Output: Mhairi is 28 years old
