fbfed674bb34ea8a98700f4471d9b082ef37e9c1
Nahom Mesfin <mnahom545@gmail.com>
2025-07-19 14:06:21 +0100

    get-card-value test

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,9 @@
 function getCardValue(card) {
     // replace with your code from key-implement
-    return 11;
+  const rank = card.slice(0, -1); // all characters except last
+    if (rank === "A") return 11;
+    if (rank >= '2' && rank <= '9') return Number(rank);
+    if (rank === "10" || rank === "J" || rank === "Q" || rank === "K") return 10;
+    throw new Error("Invalid card rank.");
 }
 module.exports = getCardValue;
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -6,6 +6,15 @@ test("should return 11 for Ace of Spades", () => {
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 7 for Seven of Hearts", () => {
+  expect(getCardValue("7♥")).toEqual(7);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Queen of Diamonds", () => {
+  expect(getCardValue("Q♦")).toEqual(10);
+});
 // Case 4: Handle Ace (A):
-// Case 5: Handle Invalid Cards:
+// Case 5: Handle Invalid Cards:(already covered in the first test)
+test("should throw error for invalid card rank", () => {
+  expect(() => getCardValue("Z♣")).toThrow("Invalid card rank");
+});
