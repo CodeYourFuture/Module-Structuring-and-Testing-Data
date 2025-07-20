4e984743a49d05a8c58e03dd3a98d9136f93898a
PandiSimatupang <pandikisaran@gmail.com>
2025-07-20 23:57:31 +0100

    put if(rep<0) at the first line  Sprint-3/2-mandatory-rewrite/repeat.js

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,9 +1,11 @@
 function repeat(str, rep) {
+  if (rep < 0) return "negative counts are not valid"; //filter
+
   myStr = "";
   for (let i = 0; i < rep; i++) {
     myStr = myStr + str;
   }
-  if (rep < 0) return "negative counts are not valid";
+
   return myStr;
 }
 
