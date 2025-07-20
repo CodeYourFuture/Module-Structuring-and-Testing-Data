75516afdce1a7979b7de5a013606d93f0420b17b
PandiSimatupang <pandikisaran@gmail.com>
2025-07-20 23:42:48 +0100

    modify switch-case Sprint-3/2-mandatory-rewrite/get-ordinal-number.js

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,17 +1,38 @@
+// function getOrdinalNumber(num) {
+//   //get the absolute value and turn into string
+//   let myNum = Math.abs(num).toString();
+//   let myNumLastChar = myNum.at(-1);
+
+//   if (myNum < 10) {
+//     if (myNum === "1") return `${num}st`;
+//     else if (myNum === "2") return `${num}nd`;
+//     else if (myNum === "3") return `${num}rd`;
+//     else return `${num}th`;
+//   } else if (myNum >= 10) {
+//     if (myNumLastChar === "1") return `${num}st`;
+//     else if (myNumLastChar === "2") return `${num}nd`;
+//     else if (myNumLastChar === "3") return `${num}rd`;
+//     else return `${num}th`;
+//   }
+// }
+
 function getOrdinalNumber(num) {
-  //get the absolute value and turn into string
-  let myNum = Math.abs(num).toString();
-  let myNumLastChar = myNum.at(-1);
-  if (myNum < 10) {
-    if (myNum === "1") return `${num}st`;
-    else if (myNum === "2") return `${num}nd`;
-    else if (myNum === "3") return `${num}rd`;
-    else return `${num}th`;
-  } else if (myNum >= 10) {
-    if (myNumLastChar === "1") return `${num}st`;
-    else if (myNumLastChar === "2") return `${num}nd`;
-    else if (myNumLastChar === "3") return `${num}rd`;
-    else return `${num}th`;
+  const lastTwo = num % 100;
+  const lastDigit = num % 10;
+
+  if (lastTwo >= 11 && lastTwo <= 13) {
+    return `${num}th`;
+  }
+
+  switch (lastDigit) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
   }
 }
 
