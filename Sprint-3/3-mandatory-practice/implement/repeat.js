e76a59ccfc0e7ad1bb881a29b5672305f62736bd
Ahmad <ahmadehsas14345@gmail.com>
2025-07-21 15:19:27 +0100

    update the function to hundle invalid input.

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,4 +1,7 @@
 function repeat(word, times) {
+  if (times < 0) {
+    throw new Error("Times must be a non-negative integer");
+  }
   return word.repeat(times);
 }
 console.log(repeat("hello", 3)); // The output should be "hellohellohello"
