93e7226ba97561c2056ce3e22a9dda81df2d315c
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-21 22:14:54 +0100

    edited on the if condition deleted the frist 3 if condition for they already included in the  others.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -10,20 +10,14 @@ function getOrdinalNumber(num) {
 
 function getOrdinalNumber(num) {
 
-module.exports = getOrdinalNumber;
-    if (num === 1) {
-        return "1st";
-    } else if (num === 2) {
-        return "2nd";
-    } else if (num === 3) {
-        return "3rd";
-    } else if (num % 10 === 1 && num % 100 !== 11) {
+     if (num % 10 === 1 && num % 100 !== 11) {
         return num + "st";
-    } else if (num % 10 === 2 && num % 100 !== 12) {
+    }if (num % 10 === 2 && num % 100 !== 12) {
         return num + "nd";
-    } else if (num % 10 === 3 && num % 100 !== 13) {
+    }if (num % 10 === 3 && num % 100 !== 13) {
         return num + "rd";
     } else {
         return num + "th";
     }
 }
+module.exports = getOrdinalNumber;
