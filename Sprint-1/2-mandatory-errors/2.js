d7dbde9d0d7007e3e992f8a496dc5f11c101b0df
pathywang <pathywang@gmail.com>
2025-07-21 20:10:20 +0100

    add description

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// we should put variable declaration first then print out as JS languages rules, can not write the other way around
 
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
