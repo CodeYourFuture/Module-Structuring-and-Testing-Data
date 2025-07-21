7d1d62c39fb94de1e927938998221e1a251e4682
Emeke <chukwuemekeajuebor247@gmail.com>
2025-07-21 06:11:55 +0100

    3-3-get-ordinal number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,18 @@
 function getOrdinalNumber(num) {
- return "1st";
+  let numStr = num.toString(); // Convert number to string
+ let lastDigit = numStr[numStr.length - 1]; // Get last digit as string
+ let lastTwoDigits = numStr.slice(-2); // Get last two digits as string
+
+  //We have three special cases for the numbers 11, 12, 13
+  if (lastTwoDigits === "11" || lastTwoDigits === "12" || lastTwoDigits === "13") {
+    return numStr + "th";
+  }
+
+  //We use last digit to determine ordinal for all other numbers
+  if (lastDigit === "1") return numStr + "st";
+  else if (lastDigit === "2") return numStr + "nd";
+  else if (lastDigit === "3") return numStr + "rd";
+  else return numStr + "th";
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -11,3 +11,39 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
     });
+
+
+//test case for numbers ending with -11
+test("should return '-th' for numbers ending with 11", () => {
+    expect(getOrdinalNumber(911)).toEqual("911th");
+    });
+
+//test case for numbers ending with -12
+test("should return '-th' for numbers ending with 12", () => {
+    expect(getOrdinalNumber(3012)).toEqual("3012th");
+    });
+
+//test case for numbers ending with -13
+test("should return '-th' for numbers ending with 13", () => {
+    expect(getOrdinalNumber(12513)).toEqual("12513th");
+    });
+
+//test case for all other number ending in 1 
+test("should return '-st' for any other number ending in 1", () => {
+    expect(getOrdinalNumber(541)).toEqual("541st");
+    });
+
+//test case for all other number ending in 2 
+test("should return '-nd' for any other number ending in 2", () => {
+    expect(getOrdinalNumber(32)).toEqual("32nd");
+    });
+
+//test case for all other number ending in 3 
+test("should return '-rd' for any other number ending in 3", () => {
+    expect(getOrdinalNumber(3)).toEqual("3rd");
+    });
+
+//test case for all other number to end with "-th" 
+test("should return '-th' for all other numbers", () => {
+    expect(getOrdinalNumber(777)).toEqual("777th");
+    });
