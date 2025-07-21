f39781ab4aa7f56955328b58484c835228465815
Samiuk <lahlahhoussam80@gmail.com>
2025-07-21 19:01:14 +0100

    Revert Sprint-1 folder to CYF's original version

--- Sprint-1/2-mandatory-errors/1.js
+++
@@ -1,5 +0,0 @@
-// Trying to create an age variable and then reassign the value by 1
-
-let age = 33;          // Declare a variable called age and assign it the value 33
-age = age + 1;         // Reassign age to be its current value plus 1
-console.log(age);      // Output: 34
