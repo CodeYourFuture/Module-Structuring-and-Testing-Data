fcfbbac1ccc49a6c368cd718c96e2f15d8dcc7ab
Samuel <41825140+Tarawally@users.noreply.github.com>
2025-07-22 12:09:15 +0100

    refactor: simplify `isProperFraction` function logic

--- Sprint-3/1-key-implement/2-is-proper-fraction.js
+++ Sprint-3/1-key-implement/2-is-proper-fraction.js
@@ -9,7 +9,7 @@
 
 function isProperFraction(numerator, denominator) {
   // A proper fraction is a fraction where the absolute value of the numerator is less than the denominator.
-  if (Math.abs(numerator) < denominator) {
+  return Math.abs(numerator) < denominator;
     return true;
   }
   return false;
