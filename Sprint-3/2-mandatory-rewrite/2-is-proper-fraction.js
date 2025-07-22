bf0809ace48c780ccd0936148f304d8b77dd0b31
delmorallopez <124817272+delmorallopez@users.noreply.github.com>
2025-07-22 18:40:51 +0100

    Sprint3/Mandatory-rewrite

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,8 +1,17 @@
 function isProperFraction(numerator, denominator) {
   if (denominator == 0){
-  return false
+    return false
   }
-  return numerator < denominator;
-  }
+  return Math.abs(numerator) < Math.abs(denominator);
+  }          
 
+// here's our helper again
+function assertEquals(actualOutput, targetOutput) {
+  console.assert(
+    actualOutput === targetOutput,
+    `Expected ${actualOutput} to equal ${targetOutput}`
+  );
+}
 module.exports = isProperFraction;
+
+
