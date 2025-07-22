775cac2c38af0d7c08ee52fc957b998385dc9ea5
Dawud Vermeulen <127528069+Dave-Vermeulen@users.noreply.github.com>
2025-07-22 20:29:16 +0000

    added a fix for  capitalize function

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -3,14 +3,18 @@
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
-function capitalise(str) {
+function capitalize(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
 // =============> write your explanation here
-// the function takes string arg it gets the first character of the string and capitalises it. the .slice method gets the rest of the string and concats it the capitalized first char. all this is assinged to var called str and returns the str.
-// the error says the var str has already been used in the function, this is because the var str is declared in the function and then again in the return statement.
+// the function takes string arg it gets the first character of the string and capitalises it. the .slice method gets the rest of the string and concats the capitalized first char. all this is assigned to var called str and returns the str.
+// the error says the var str has already been used in the function, this is because the var str is declared in the function and then again in the return statement. we could fix it by removing the var keyword in the return statement.
 // =============> write your new code here
+
+// function capitalize(str) {
+//   return `${str[0].toUpperCase()}${str.slice(1)}`;
+// }
 
 console.log(capitalise('hello earth'));
