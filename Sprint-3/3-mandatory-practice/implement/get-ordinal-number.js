045941b705ae50286ebceedcda3c99ea13c7d21b
asaniDev <asanidev9@gmail.com>
2025-07-22 02:10:51 +0100

    completed mandatory-practice/implement

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,43 @@
 function getOrdinalNumber(num) {
- return "1st";
+  //identify the ordinal number for numbers below 11
+  if (num < 11 && num > 0) {
+    switch (num) {
+      case 1:
+        return "1st";
+      case 2:
+        return "2nd";
+      case 3:
+        return "3rd";
+      case 4:
+        return "4th";
+      case 5:
+        return "5th";
+      case 6:
+        return "6th";
+      case 7:
+        return "7th";
+      case 8:
+        return "8th";
+      case 9:
+        return "9th";
+      case 10:
+        return "10th";
+    }
+  } else if(num > 10 && num < 21) {
+      return num + "th";
+  } else if (num > 20) { 
+    const lastDigit = num % 10;
+    switch (lastDigit) {
+      case 1:
+        return num + "st";
+      case 2:
+        return num + "nd";
+      case 3:
+        return num + "rd";
+      default:
+        return num + "th";
+    }
+  }
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,17 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+    // This function is designed to repeat a given string a specified number of times.
+    if (count < 0) {
+        return "invalid count, should be a positive number";
+    } else if (count === 0) {
+        return "";
+    } else {
+        let repeatedStr = "";
+        for (let i = 0; i < count; i++) {
+            repeatedStr += str;
+        }
+        return repeatedStr;
+    }
+    
 }
 
 module.exports = repeat;
