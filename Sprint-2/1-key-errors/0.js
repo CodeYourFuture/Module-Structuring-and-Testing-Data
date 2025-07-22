1056db816f3b29b0e33d3fe8cc221e7e4891d9d5
pathywang <pathywang@gmail.com>
2025-07-22 12:16:42 +0100

    answer and fix error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,21 @@
-"hello Mhairi" === `hello ${mhairiName}`;
-"${mhairiName} is 28" === `Mhairi is ${mhairiAge}`;
+// Predict and explain first...
+// it should show syntax error
 
-// My answer is:
-const mhairiName = "Mhairi";
-const mhairiAge = 28;
+// call the function capitalise with a string input
+// interpret the error message and figure out why an error is occurring
 
-const sentence1= `Hello ${mhairiName},nice to meet you`;
-console.log(sentence1); // ➜ Output: Hello Mhairi, nice to meet you
+function capitalise(str) {
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+
+// the input "sty" is the same as output "str" which is not allowed in JS
+// my new code:
+
+function capitalise(str) {
+    return(`${str[0].toUpperCase()}${str. slice(1)}`)
+}
+console.log(capitalise("morning"))
+console.log(capitalise("heavy"))
 
 
-const sentence= `${mhairiName} is ${mhairiAge} years old`;
-console.log(sentence);  // ➜ Output: Mhairi is 28 years old
