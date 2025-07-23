e749f57c4571464db4bbbf1c73cbfd709ad79c2b
Ahmad <ahmadehsas14345@gmail.com>
2025-07-23 17:58:13 +0100

    update the tests for all valid integers.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -8,12 +8,26 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-//test("should return '1st' for 1", () => {
-//expect(getOrdinalNumber(1)).toEqual("1st");
-//});
-
 test("append 'st' to numbers ending in 1, except those ending in 11", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(121)).toEqual("121st");
+});
+
+test("appends 'nd' to numbers ending in 2, except those ending in 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(112)).toEqual("112th"); // special case
+});
+
+test("appends 'rd' to numbers ending in 3, except those ending in 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(113)).toEqual("113th"); // special case
+});
+
+test("appends 'th' to numbers ending in 11, 12, or 13", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
 });
