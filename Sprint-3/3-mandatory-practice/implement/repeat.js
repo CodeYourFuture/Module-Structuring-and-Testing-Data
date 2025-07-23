c033ab4ebd410ee0b337729e856c2713103f95ad
delmorallopez <124817272+delmorallopez@users.noreply.github.com>
2025-07-23 15:39:50 +0100

    Sprint3/mandatory-practice-review

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,22 +1,12 @@
 function repeat(word, times) {
 
-    if (times == 0) {
-        return "";
-    }
-
-    if (times == 1) {
-    return word;
-    }
-
     if (times < 0) {
            return "Invalid count: Count must be a non-negative integer.";
     }
-    
     else return word.repeat(times);
     // The repeat method is used to repeat the string 'words' 'times' times.
     // If times is less than or equal to 1, it returns the original string.
     // Otherwise, it returns the string repeated 'times' times. 
     
 }
-
 module.exports = repeat;
