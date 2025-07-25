d34b46413488073c623816aecd58af99a95ef261
Ahmad <ahmadehsas14345@gmail.com>
2025-07-25 17:22:48 +0100

    Add test code to cover all valid integers.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -17,17 +17,36 @@ test("append 'st' to numbers ending in 1, except those ending in 11", () => {
 test("appends 'nd' to numbers ending in 2, except those ending in 12", () => {
   expect(getOrdinalNumber(2)).toEqual("2nd");
   expect(getOrdinalNumber(22)).toEqual("22nd");
-  expect(getOrdinalNumber(112)).toEqual("112th"); // special case
 });
 
 test("appends 'rd' to numbers ending in 3, except those ending in 13", () => {
   expect(getOrdinalNumber(3)).toEqual("3rd");
   expect(getOrdinalNumber(23)).toEqual("23rd");
-  expect(getOrdinalNumber(113)).toEqual("113th"); // special case
 });
 
-test("appends 'th' to numbers ending in 11, 12, or 13", () => {
+test("appends 'th' to numbers ending in 11, 12, or 13 and all others not ending in 1, 2, or 3", () => {
   expect(getOrdinalNumber(11)).toEqual("11th");
   expect(getOrdinalNumber(12)).toEqual("12th");
   expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(111)).toEqual("111th");
+  expect(getOrdinalNumber(112)).toEqual("112th");
+  expect(getOrdinalNumber(113)).toEqual("113th");
+
+  //general `th` cases
+
+  expect(getOrdinalNumber(0)).toEqual("0th");
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(24)).toEqual("24th");
+  expect(getOrdinalNumber(100)).toEqual("100th");
+  expect(getOrdinalNumber(104)).toEqual("104th");
+});
+
+test("handles edge cases for higher numbers", () => {
+  expect(getOrdinalNumber(1011)).toEqual("1011th");
+  expect(getOrdinalNumber(1012)).toEqual("1012th");
+  expect(getOrdinalNumber(1013)).toEqual("1013th");
+  expect(getOrdinalNumber(1021)).toEqual("1021st");
+  expect(getOrdinalNumber(1022)).toEqual("1022nd");
+  expect(getOrdinalNumber(1023)).toEqual("1023rd");
 });
