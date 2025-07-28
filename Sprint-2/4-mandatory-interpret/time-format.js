04e3118834184f31cdd74033c7fff4fea5b462ce
JenniferIsidienu <jennieisidienu@gmail.com>
2025-07-28 21:33:52 +0100

    Sprint 2 coursework

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,24 +11,45 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
-// You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
+// You will need to play computer with this example - use the 
+// Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
-// a) When formatTimeDisplay is called how many times will pad be called?
+// a) When formatTimeDisplay is called how many 
+// times will pad be called?
 // =============> write your answer here
+// 3 times. It is called once for hours, 
+// once for minutes and once for seconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
-// b) What is the value assigned to num when pad is called for the first time?
+// b) What is the value assigned to num when 
+// pad is called for the first time?
 // =============> write your answer here
+//  The value assigned to num when pad is 
+// called for the first time is 0.
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// The return value of pad when called for the first time is "00". 
+// This is because it pads the number 0 with leading zeros to 
+// ensure it has a length of 2 characters.
 
-// d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+// d) What is the value assigned to num when pad 
+// is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The value assigned to num when pad is called for the last time is 1.
+// This is because the last call to pad is for the remaining seconds
+// which is 61 % 60 = 1.
 
-// e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// e) What is the return value assigned to num when 
+// pad is called for the last time in this program?  
+// Explain your answer
 // =============> write your answer here
+// The return value assigned to num when pad is called for 
+// the last time is "01".
+// This is because it applies .padStart(2, "0"), which returns 
+// a two character string, with a leading zero to ensure 
+// it has a minimum length of 2.
