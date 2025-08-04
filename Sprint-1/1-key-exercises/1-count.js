16e88b9eabe89fec809f6694b1e42e603b250a1b
Emin Akturk <116726768+eminakturk@users.noreply.github.com>
2025-08-04 16:48:33 +0100

    Delete Sprint-1/1-key-exercises/1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-count = count + 1;
-
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
-
-// SOLUTION:
-// Line 3 is updating the value of the count by adding 1 to its value.
