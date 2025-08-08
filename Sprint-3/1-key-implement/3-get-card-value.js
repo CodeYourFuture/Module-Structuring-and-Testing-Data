06d77a2986bfc175431f8e1c6b462fe289232e69
Nahom Mesfin <mnahom545@gmail.com>
2025-08-08 21:17:38 +0100

    fixed handling error

--- Sprint-3/1-key-implement/3-get-card-value.js
+++ Sprint-3/1-key-implement/3-get-card-value.js
@@ -10,7 +10,7 @@
 function getCardValue(card) {
   const rank = card.slice(0, -1); // all characters except last
     if (rank === "A") return 11;
-    if (rank >= '2' && rank <= '9') return Number(rank);
+    if (/^[2-9]$/.test(rank)) return Number(rank);
     if (rank === "10" || rank === "J" || rank === "Q" || rank === "K") return 10;
     throw new Error("Invalid card rank.");
 }
