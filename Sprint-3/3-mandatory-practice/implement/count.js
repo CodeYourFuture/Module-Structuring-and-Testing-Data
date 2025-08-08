cb2c7cc5d4c7c25cda8a048023a4fab89bb951c2
Nahom Mesfin <mnahom545@gmail.com>
2025-08-08 22:21:03 +0100

    fixed identation

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,5 @@
 function countChar(stringOfCharacters, findCharacter) {
-    let count = 0;
+  let count = 0;
   for (let i = 0; i < stringOfCharacters.length; i++) {
     if (stringOfCharacters[i] === findCharacter) {
       count++;
