89b1da07caae0f2255122b71c3e84dd9804f4aa0
Nahom Mesfin <mnahom545@gmail.com>
2025-08-08 23:48:13 +0100

    add case for 21st

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,7 +1,13 @@
 function getOrdinalNumber(num) {
- if (num === 1) return "1st";
-  if (num === 2) return "2nd";
-    if (num === 3) return "3rd";
+ const lastDigit = num % 10;
+ const lastTwoDigits = num % 100;
+
+ if (lastTwoDigits >= 11 && lastTwoDigits <= 13)
+  return num + "th";
+
+ if (lastDigit === 1) return num + "st";
+ if (lastDigit === 2) return num + "nd";
+ if (lastDigit === 3) return num + "rd";
   return num + "th";
 }
 
