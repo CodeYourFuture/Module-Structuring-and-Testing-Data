6f2e14756f34e677897a837e76f0e69b5f31f844
Dawud Vermeulen <127528069+Dave-Vermeulen@users.noreply.github.com>
2025-08-10 14:01:09 +0200

    Update 3-get-card-value.js
    remove the unnecessary comments. clean code.

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,11 +1,11 @@
 function getCardValue(card) {
     // replace with your code from key-implement
-    const rank = card.slice(0, -1); // works to extract the rank and ignores the suit
-        if (rank === "A") return 11 //works
+    const rank = card.slice(0, -1);
+        if (rank === "A") return 11
         else if (["J", "Q", "K", "10"].includes(rank)) 
-            {return 10} // doesnt work
+            {return 10}
         else if (/^[2-9]$/.test(rank)) {
-            return Number(rank)} // this resturns a string not a number and therefore fails.10 doesnt need to be here it can be in line 5. this doesnt work, refactor.
-        else { throw new Error("Invalid card rank.");} // this is right but doesnt work as expected
+            return Number(rank)} 
+        else { throw new Error("Invalid card rank.");}
 }
 module.exports = getCardValue;
