bfedc82d30262f630cd193c2275f2f2112f32c76
Dawud Vermeulen <127528069+Dave-Vermeulen@users.noreply.github.com>
2025-08-10 13:58:26 +0200

    Update get-ordinal-number.js
    update for the sake of the PR comments. fix to handle the over sights.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,8 +1,14 @@
 function getOrdinalNumber(num) {
-    if (num === 1) return "1st";
-    if (num === 2) return "2nd";
-    if (num === 3) return "3rd";
-    return num + "th"; // handles all cases >= 4
+    // need to handle 21st, 22nd, 23rd, and all other numbers
+    if (num % 100 >= 11 && num % 100 <= 13) {
+        return num + "th";
+    }
+    const lastDigit = num % 10;
+
+    if (lastDigit === 1) return num + "st";
+    if (lastDigit === 2) return num + "nd";
+    if (lastDigit === 3) return num + "rd";
+    return num + "th"; // handles all other cases
 }
 
 module.exports = getOrdinalNumber;
