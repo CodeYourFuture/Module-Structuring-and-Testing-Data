93ae4e9dd0e77709762e4eeb8dac403238a35681
Samiuk <lahlahhoussam80@gmail.com>
2025-09-11 00:19:58 +0100

    Improve error message in getCardValue to include invalid card

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,13 +1,13 @@
 function getCardValue(card) {
   const rank = card.slice(0, -1); // remove suit
 
-  if (!rank) throw new Error("Invalid card rank");
+  if (!rank) throw new Error(`Invalid card rank: ${card}`);
 
   if (rank === "A") return 11;
   if (["K", "Q", "J"].includes(rank)) return 10;
   if (/^(?:[2-9]|10)$/.test(rank)) return Number(rank);
 
-  throw new Error("Invalid card rank");
+  throw new Error(`Invalid card rank: ${card}`);
 }
 
 module.exports = getCardValue;
