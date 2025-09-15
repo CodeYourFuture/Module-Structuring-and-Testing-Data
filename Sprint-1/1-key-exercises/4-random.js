b3fa1cd7f69fb0297bd9e1b5edf3b3373e447d3e
TzeMingHo <tzemingho@gmail.com>
2025-09-15 16:38:02 +0100

    answering 4-random

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Math.floor - Rounds a number DOWN to the nearest integer
+// Math.random - generates a random floating number > 0 and < 1
+// (maximum - minimum + 1) = 100 - 1 + 1 = 100
+// So, num represents a rounded down random integer generated between 1 and 100 + 1
+
+console.log(num);
