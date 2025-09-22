e0e0e1053c7441a7d1303ed5687062fdf0d527d6
AdnaanA <thetriplex247@gmail.com>
2025-09-22 12:16:30 +0100

    fixed mandatory errors

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,7 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+/* This is just an instruction for the first activity - but it is just for human consumption
+We don't want the computer to run these 2 lines - how can we solve this problem? */
+
+// we can use // to comment out a single line
+// or we can use /* to start a multi-line comment
+// and end it with */
+
