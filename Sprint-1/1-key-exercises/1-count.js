bae8316ea3a596fefd09e5bcaf506281b979664c
AdnaanA <thetriplex247@gmail.com>
2025-09-22 10:49:14 +0100

    modified:   Sprint-1/1-key-exercises/1-count.js     Answered question about the assignment     and also tested the code by logging the count variable to console

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,14 @@
 let count = 0;
 
-count = count + 1;
+count = count + 3;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Line 3 is updating the value of the count variable by adding 1 to its current value. The = operator 
+// is used to assign the new value (count + 1) back to the count variable.
+// we can also say that the = operator is taking the value on the right side (count + 1) and storing it 
+// in the variable on the left side (count).
+
+// We can see the result of our code by logging the count variable to the console
+console.log(count);
