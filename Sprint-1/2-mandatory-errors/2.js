afc781619e8bc8404470fb56aaf219a0b2a49c45
AdnaanA <thetriplex247@gmail.com>
2025-09-22 12:21:13 +0100

    mandatory error-3

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// we need to declare the variable before we use it in the console.log statement
 
-console.log(`I was born in ${cityOfBirth}`);
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
