23e660e1740adf623ceadac0f998e2ca899c74ad
AdnaanA <thetriplex247@gmail.com>
2025-09-22 12:43:41 +0100

    variable names fixed

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const a24HourClockTime = "20:53";
+const a12hourClockTime = "08:53";
+
+// The 12HourClockTime variable should store the time in 12-hour format
+// The 24hourClockTime variable should store the time in 24-hour format
+
+// However, the code isn't working
+// the error is that the variable names are not valid because they start with a number
+// Variable names cannot start with a number
+
+console.log(a24HourClockTime); // Should print: 08:53
+console.log(a12hourClockTime); // Should print: 20:53
