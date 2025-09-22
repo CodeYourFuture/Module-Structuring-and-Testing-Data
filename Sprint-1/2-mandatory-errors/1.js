0a275326d3b6034d399af8ca622fc1428e6df378
AdnaanA <thetriplex247@gmail.com>
2025-09-22 12:19:19 +0100

    mandatory error-2 fix

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+
+// to reassign the value of age by 1 we need to use let instead of const
+
+console.log(age);
+
