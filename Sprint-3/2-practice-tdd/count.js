ea7e00c006847d6bf8e7925891aa3115031eca40
TzeMingHo <tzemingho@gmail.com>
2025-09-24 12:25:20 +0100

    In 2-practice-tdd, created a function to handle count characters

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,14 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  const arrayOfCharacters = stringOfCharacters.split("");
+  let count = 0;
+  for (let index = 0; index < arrayOfCharacters.length; index++) {
+    if (arrayOfCharacters[index] === findCharacter) {
+      count += 1;
+    }
+  }
+  console.log(arrayOfCharacters);
+  console.log(count);
+  return count;
 }
 
 module.exports = countChar;
