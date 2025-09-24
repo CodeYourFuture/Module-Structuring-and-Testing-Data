3abd4b564d50d4577dacc13923899bff5e8151aa
shreefAhmedM <livecoding.33@gmail.com>
2025-09-24 13:42:49 +0100

    updating time format

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -1,5 +1,6 @@
 function pad(num) {
   return num.toString().padStart(2, "0");
+
 }
 
 function formatTimeDisplay(seconds) {
@@ -7,7 +8,6 @@ function formatTimeDisplay(seconds) {
   const totalMinutes = (seconds - remainingSeconds) / 60;
   const remainingMinutes = totalMinutes % 60;
   const totalHours = (totalMinutes - remainingMinutes) / 60;
-
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 formatTimeDisplay(60);
@@ -18,18 +18,18 @@ formatTimeDisplay(60);
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here. one time
+// =============> 3 times 
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here .is 60
+// =============> .is 0 ,because totalHours = 0 at that point.
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here is 0
+// =============> is "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============>  num = 1 ,because last call to pad is for remainingMinutes, which is = 1
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============>  "01"  because pad(1) converts 1 to a string and pads it to two digits, resulting in "01"
