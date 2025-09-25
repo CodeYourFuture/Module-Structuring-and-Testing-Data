f1ac2fe738655b2a93e1db4fe8072897d2f02084
TzeMingHo <tzemingho@gmail.com>
2025-09-25 17:22:37 +0100

    answered part b to d in find.js

--- Sprint-3/3-stretch/find.js
+++ Sprint-3/3-stretch/find.js
@@ -26,5 +26,13 @@ console.log(find("code your future", "z"));
 //    Now, index is 1, and the while loop will check again, until the character is found or the index is no longer less than the length of the string.
 
 // b) What is the if statement used to check
+//    The if statement on line 5 checks if the character at current index of the string is equal to the target character (char).
+//    If they are equal, the function returns the current index.
+
 // c) Why is index++ being used?
+//    The index++ on line 8 is being used to update the index variable by incrementing it by 1.
+//    Given  the character is not found at the current index, we need to move to the next index.
+
 // d) What is the condition index < str.length used for?
+//    The condition index < str.length on line 4 is used to ensure that the while loop continues to execute as long as the index is less than the length of the string.
+//    More importantly, it prevents the while loop goes on forever into an infinite loop.
