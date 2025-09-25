88837ece72682750c5facfb0c04cc8eb0a016a62
AdnaanA <thetriplex247@gmail.com>
2025-09-25 09:47:33 +0100

    fixed error and answered questions

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,17 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// there are 5 function calls in this file, they are on lines 1, 2, 5, 6 and 9
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// the error was coming from line 5 there was a comma missing between the 2 quotations ("," "") by adding the comma the code was fixed and it worked as it should.
 
 // c) Identify all the lines that are variable reassignment statements
+// the variable reassignment statements are on lines 5 and 6
 
 // d) Identify all the lines that are variable declarations
+// the variable declaration statements are on lines 1, 2, 4, 7 and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+/* the expression is taking the variable carPrice and replacing all the commas in the string with nothing and then converting 
+the string into a number so that it can be used in calculations.*/
