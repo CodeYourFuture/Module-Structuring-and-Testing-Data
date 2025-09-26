4eb2d5b3fcd7509c00fe5febcc10a08cb04d160a
TzeMingHo <tzemingho@gmail.com>
2025-09-26 17:08:31 +0100

    created tests and function for credit card validation

--- Sprint-3/3-stretch/creditCard-validator.js
+++ Sprint-3/3-stretch/creditCard-validator.js
@@ -5,9 +5,33 @@ function creditCardValidator(cardNumber) {
   //check if the length of the sanitized input is 16
   if (sanitized.length !== 16) {
     return false;
-  } else {
-    return true;
   }
+  // check if the sanitized input contains only digits
+  if (!/^\d{16}$/.test(sanitized)) {
+    return false;
+  }
+
+  // check if there ara at least two different digits
+  const uniqueDigits = new Set(sanitized);
+  if (uniqueDigits.size < 2) {
+    return false;
+  }
+
+  // check if the last digit is even
+  const lastDigit = Number(sanitized[sanitized.length - 1]);
+  if (lastDigit % 2 !== 0) {
+    return false;
+  }
+
+  // check if the sum of all digits is greater than 16
+  const sum = sanitized
+    .split("")
+    .reduce((total, digit) => total + Number(digit), 0);
+  if (sum <= 16) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = creditCardValidator;
--- Sprint-3/3-stretch/creditCard-validator.test.js
+++ Sprint-3/3-stretch/creditCard-validator.test.js
@@ -5,4 +5,25 @@ describe("creditCardValidator", () => {
     expect(creditCardValidator("1234-5678-9012-3456")).toBe(true);
     expect(creditCardValidator("1234 5678 9012 3456")).toBe(true);
   });
+
+  test("should return false if the card number is not digit only", () => {
+    expect(creditCardValidator("1234-5678-9012-345a")).toBe(false);
+    expect(creditCardValidator("1234 5678 9012 3456")).toBe(true);
+  });
+
+  test("should return false if the card number does not contain at least two different digits", () => {
+    expect(creditCardValidator("1111-1111-1111-1111")).toBe(false);
+    expect(creditCardValidator("1111 2222 2222 2222")).toBe(true);
+  });
+
+  test("should return false if the card number does not end with an even digit", () => {
+    expect(creditCardValidator("1234-5678-9012-3457")).toBe(false);
+    expect(creditCardValidator("1234 5678 9012 3456")).toBe(true);
+  });
+
+  test("should return false if the sum of all the digits is not greater than 16", () => {
+    expect(creditCardValidator("0000-0000-0000-0000")).toBe(false);
+    expect(creditCardValidator("1111 1111 1111 1111")).toBe(false);
+    expect(creditCardValidator("1234 5678 9012 3456")).toBe(true);
+  });
 });
