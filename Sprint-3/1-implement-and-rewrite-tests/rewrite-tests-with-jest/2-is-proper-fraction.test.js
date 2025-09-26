b07437bd7ef27d4f2914c975f7c6b4fe189d9a51
AdnaanA <thetriplex247@gmail.com>
2025-09-26 10:39:26 +0100

    created testcases for proper-fractions, cases tested and passed.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,22 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 3)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return false for a negative fraction", () => {
+  expect(isProperFraction(-2, 3)).toEqual(false);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator", () => {
+  expect(isProperFraction(4, 4)).toEqual(false);
+});
+
+// We can run this test file using the command `npx jest 1-get-angle-type.test.js`
+// in the terminal. Making sure we are in the directory where this file is located.
+// If we have Jest installed globally, you can simply run `jest 1-get-angle-type.test.js`
+// instead. If you have added a test script to your package.json file, you can also run
+// `npm test 1-get-angle-type.test.js` to execute the tests.
