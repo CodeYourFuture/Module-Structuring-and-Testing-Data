c40ecef10cdbb4513db791ccea02accbe4ffa05a
AdnaanA <thetriplex247@gmail.com>
2025-09-27 12:36:06 +0100

    getordinalnumber test cases have been created and tested to pass all

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,22 @@
 function getOrdinalNumber(num) {
-  return "1st";
+
+  if (typeof num !== "number" || !Number.isInteger(num) || num <= 0) {
+    return "Invalid input";
+  }
+if (num % 100 >= 11 && num % 100 <= 13) {
+    return num + "th";
+  }
+  switch (num % 10) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
+
 }
 
 module.exports = getOrdinalNumber;
