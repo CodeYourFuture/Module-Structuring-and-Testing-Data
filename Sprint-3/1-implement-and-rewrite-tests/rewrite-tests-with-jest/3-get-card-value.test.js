9bc3e9dbcb2c8ed654cd508fbe3f345e1ae63060
shreefAhmedM <livecoding.33@gmail.com>
2025-09-27 22:17:16 +0100

    test: add Jest tests for getCardValue including error cases

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,22 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("Should return numeric value(8) for Number Cards (2-10)", () => {
+  const aceofSpades = getCardValue("8♠");
+  expect(aceofSpades).toEqual(8);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return the value 10,  a card with a rank of ('10', 'J', 'Q', or 'K')", () => {
+  const aceofSpades = getCardValue("K");
+  expect(aceofSpades).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return the value 11,for a card with a rank of 'A' ", () => {
+  const aceofSpades = getCardValue("A");
+  expect(aceofSpades).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error,with an invalid rank 'OL'", () => {
+  const aceofSpades = getCardValue("OL");
+  expect(aceofSpades).toBe("Invalid card rank");
+});
