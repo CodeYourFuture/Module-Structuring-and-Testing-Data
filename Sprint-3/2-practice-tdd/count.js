889f34ea7e3503e68b79180903c509c441aafe79
AdnaanA <thetriplex247@gmail.com>
2025-09-27 10:22:03 +0100

    created and tested jest-testcases

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,12 @@
-function countChar(stringOfCharacters, findCharacter) {
-  return 5
+function countChar(str, char) {
+  if (!char || char.length !== 1) return 0; // ensure char is a single character
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,44 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("should return 0 when character does not exist in the string", () => {
+  const str = "mnopqrst";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// we can add more test cases to make sure our function is working as expected
+// Scenario: Case Sensitivity
+// Given the input string str,
+// And a character char that exists in str but with different casing (e.g., 'A' in 'aAaAa'),
+// When the function is called with these inputs,
+// Then it should treat char as case-sensitive and only count occurrences that match the exact casing (e.g., 'A' appears two times in 'aAaAa').
+
+test("should treat character as case-sensitive", () => {
+  const str = "aAaAa";
+  const char = "A";
+  const count = countChar(str, char);
+  expect(count).toEqual(2);
+});
+
+// we can also add empty string test case
+// Scenario: Empty String
+// Given an empty input string str,
+// And any character char,
+// When the function is called with these inputs,
+// Then it should return 0, indicating that no occurrences of char can be found in an empty str.
+
+test("should return 0 for empty string", () => {
+  const str = "";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// We can run this test file using the command `npx jest count.test.js`
+// in the terminal. Making sure we are in the directory where this file is located.
+// If we have Jest installed globally, you can simply run `jest count.test.js`
+// instead. If you have added a test script to your package.json file, you can also run
+// `npm test count.test.js` to execute the tests.
