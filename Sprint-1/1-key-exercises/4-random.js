2d115ce677be1ec62e3b17b276b0328da1217695
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-09-28 20:00:02 +0100

    described what each method does and the order each expression is evaluated

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -4,6 +4,17 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
+    //num represents a variable with the a random number as its value
+
 // Try breaking down the expression and using documentation to explain what it means
+    //Math.random() returns a pseudo random number that's >=0 and <1
+    //Math.floor() rounds down to the nearest integer
+    
 // It will help to think about the order in which expressions are evaluated
+   //this first (maximum-minimum+1) then this Math.random()* then this Math.floor() then this +minimum
+
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+   //it returns a pseudo random number that's >=0 and <1
+
+
+console.log(num);
