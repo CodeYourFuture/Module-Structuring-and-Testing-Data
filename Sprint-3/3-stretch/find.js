deb0fe61376c604720310a3783e208e9b7f7c992
shreefAhmedM <livecoding.33@gmail.com>
2025-09-28 12:54:26 +0100

    update find.js files

--- Sprint-3/3-stretch/find.js
+++ Sprint-3/3-stretch/find.js
@@ -1,17 +1,17 @@
 function find(str, char) {
   let index = 0;
-
   while (index < str.length) {
     if (str[index] === char) {
       return index;
     }
     index++;
   }
+
   return -1;
 }
 
-console.log(find("code your future", "u"));
-console.log(find("code your future", "z"));
+// console.log(find("code your future", "u"));
+// console.log(find("code your future", "z"));
 
 // The while loop statement allows us to do iteration - the repetition of a certain number of tasks according to some condition
 // See the docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
@@ -20,6 +20,11 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+// It will keep track of our current position in the string  starting from 0 and increases by 1 on each loop iteration
+// intil character is found or the loop reache the end of the string
 // b) What is the if statement used to check
+// It is checking if the character at the current index in str is equal to char.
 // c) Why is index++ being used?
+//It is used to update the index during each iteration so the loop moves to the next character.
 // d) What is the condition index < str.length used for?
+// This ensure the loop keeps running until index reaches the length of the string, so we do not go past the last character.
