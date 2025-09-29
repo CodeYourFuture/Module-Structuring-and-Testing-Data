709bed60f1cf9281de83bf5efc0505f42487eb6c
Alaa-Tagi <alaa3litagi1994@gmail.com>
2025-09-29 23:11:39 +0100

    Predict, consider the error and update the code.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
+/* The old code:
 const cardNumber = 4533787178994213;
 const last4Digits = cardNumber.slice(-4);
+*/
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,16 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+/* My prediction: The code will give an error because the minus sign(-4) and slice works only with strings, not numbers.
+
+Consideration: The error is a TypeError: cardNumber.slice is not a function. mostly it was because I predicted that numbers do not have a slice method. About the minus sign, I have learned about positive and negative indexing in strings, now I can see the difference.
+
+
+To fix this, we need to convert cardNumber to a string before calling the slice method.
+*/
+
+// Updated code:
+const cardNumber = 4533787178994213;
+const last4DigitsCorrected = cardNumber.toString().slice(-4);
+console.log(last4DigitsCorrected);
