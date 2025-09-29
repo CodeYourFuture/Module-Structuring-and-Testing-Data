8c6bab4478b56404305784738c4cba03caebc7bb
Fares Bakhet <faresak47roo@gmail.com>
2025-09-29 20:50:07 +0100

    Create two variable dir/ext to show part of the FilePath, Comments were added too

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -11,13 +11,14 @@
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
-const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
+const base = filePath.slice(lastSlashIndex + 1); // from filePath it will show file.txt
+console.log(`The base part of ${filePath} is ${base}`); // it will print the base part of the filePath variable.
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex); // from FilePath it will show /Users/mitch/cyf/Module-JS1/week-1/interpret.
+console.log(`The dir part of ${filePath} is ${dir}`); // it will print the dir part of the filePath variable.
 
-// https://www.google.com/search?q=slice+mdn
+const ext = base.slice(base.lastIndexOf(".")); // from base it will show .txt
+console.log(`The ext part of ${filePath} is ${ext}`); // it will print the ext part of the filePath variable.
