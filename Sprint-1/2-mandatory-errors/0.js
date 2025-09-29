6efbcb4ecc0c028074c5e190566c91710de585f9
enjoy15 <narumoljoy@gmail.com>
2025-09-29 19:13:15 +0100

    0.js

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,3 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+By adding // at the beginning of each line to turn the text into comments.
