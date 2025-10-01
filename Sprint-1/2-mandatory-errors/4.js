df3c3a854da80a424ca3d5e615fe3ed822c815fa
Imran Mohamed <info@imranbmohamed.com>
2025-10-01 17:38:57 +0100

    completed task in Sprint-1/2-mandatory-errors/4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+//throws SyntaxError because variable name cannot start with a number
+const twelveHourClockTime = '08:53'
+const twentyFourHourClockTime = '20:53'
+
+console.log(twelveHourClockTime);
+console.log(twentyFourHourClockTime);
+
