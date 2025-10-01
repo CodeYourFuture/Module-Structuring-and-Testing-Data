d7d656b77a4d36bcda6af1cf76c05f8a0efe44c8
Imran Mohamed <info@imranbmohamed.com>
2025-10-01 17:53:04 +0100

    completed task in Sprint-1/2-mandatory-interpret/1-percentage-change.js

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -13,10 +13,23 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+//there are 5 function calls in this file
+//line 4 has 2 function calls - Number and replaceAll
+//line 5 has 2 function calls - Number and replaceAll
+// line 10 has 1 function call - console.log
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+//The error is coming from line 5 due to a missing comma in the replaceAll method. It should be replaceAll(",","")
 
 // c) Identify all the lines that are variable reassignment statements
 
+// lines 4 and 5
+
 // d) Identify all the lines that are variable declarations
 
+//lines 1, 2, 7 and 8 
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+//This expression is removing all the commas from the carPrice string and converting it into a number so that mathematical operations can be performed on it.
