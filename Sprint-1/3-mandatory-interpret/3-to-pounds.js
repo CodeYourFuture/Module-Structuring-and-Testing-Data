c45239d8a8ac23841956d88ca14a4cbddead848f
Imran Mohamed <info@imranbmohamed.com>
2025-10-01 18:25:01 +0100

    completed tasks in Sprint-1/2-mandatory-interpret/3-to-pounds.js

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,4 +1,4 @@
-const penceString = "399p";
+const penceString = "4354301p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
@@ -24,4 +24,15 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
+
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+//2. const penceStringWithoutTrailingP = penceString.substring(0,penceString.length-1) initialises a string variable that has a value of penceString without the last character
+
+//3. const paddedPenceString = pence~StringWithourTrailingP.padStart(3,'0') initialises a string variable that has the value of penceStringWithoutTrailingP but ensures the string is at least 3 characters long by adding 0's to the start if needed
+
+//4. const pounds = paddedPenceNumberString.substring(0,  paddedPenceNumberString.length - 2) initialises a string variable that has the value of paddedPenceNumberString without the last 2 characters
+
+//5. const pence = paddedPenceNumberString.subString(paddedPenceNumberString.length-2).padEnd(2,'0') initialises a string variale with the value of the last 2 characters of paddedPenceNumberString and ensures the string is at least 2 characters long by adding 0's to the end if needed
+
+//6. console.log(`£${pounds}.${pence}`) prints out the value of pounds and pence in £pounds.pence format by using string interpolation
