c9dbc7a1594d32281aaf344e24fe27dcd098a250
Fares Bakhet <faresak47roo@gmail.com>
2025-10-01 11:24:13 +0100

    Predict, Explain and Write new code

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here : There will be an error.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -7,7 +7,15 @@
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
-}
 
-// =============> write your explanation here
-// =============> write your new code here
+
+// =============> write your explanation here : Because str has been used(declared) twice. As an input(parameter) to the function(capitalise) and with let. We should used a different variable.
+
+// =============> write your new code here :
+/*
+function capitalise(str) {
+  let cap = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return cap;
+}
+console.log(capitalise("fares"));
+*/
