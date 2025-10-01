ac110859bc49d287ffb27d49c0eb65be3f42608b
Imran Mohamed <info@imranbmohamed.com>
2025-10-01 16:28:45 +0100

    completed task in  Sprint-1/2-mandatory-errors/1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,10 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+// const age = 33;
+// age = age + 1;
+
+// using let as opposed to const is appropriate here as we intend to reassign the value of age
+
+let age = 33
+age += 1
+console.log(age) //should log 34;
