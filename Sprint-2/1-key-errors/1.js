7f96fdf6f24027d50ea415339a87920c2d167a2a
Alaa-Tagi <alaa3litagi1994@gmail.com>
2025-10-02 11:20:56 +0100

    predict,explain and update the code

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,10 +1,12 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> write your prediction here : The variable decimalNumber has been declared.
 
 // Try playing computer with the example to work out what is going on
 
+// The old code :
+/*
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
@@ -13,8 +15,22 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+*/
 
-// =============> write your explanation here
-
+// =============> write your explanation here :
+/*
+-The variable decimalNumber has been declared within the function as a parameter then declared again using const.
+- The variable decimalNumber should be defined first.
+*/
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+//The correct code :
+const decimalNumber = 0.5;
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(decimalNumber));
