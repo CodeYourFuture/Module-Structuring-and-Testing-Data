f7173fc5b5bf092c11720aee9bdc3108ff140319
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-03 09:18:10 +0100

    replaced console.log with return inside the function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,18 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// the function hasn't got a return so the log will be undefined
 
-function multiply(a, b) {
-  console.log(a * b);
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// the function hasn't got a return so the log will be undefined
+
+// Finally, correct the code to fix the problem
+ function multiply(a, b) {
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
