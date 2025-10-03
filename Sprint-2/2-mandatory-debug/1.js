9bfd01ade26cae77743f66371dd3967fbe505055
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-03 09:24:13 +0100

    removed the semicolon after the "return" and moved "a+b" on the same line with"return" otherwise a semicolon would be automatically inserted

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  SyntaxError
 
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// the function doesn't return anything because there is ";" immediately after "return" and the mathematicall operation is on another line
+// Finally, correct the code to fix the problem
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
+  
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
