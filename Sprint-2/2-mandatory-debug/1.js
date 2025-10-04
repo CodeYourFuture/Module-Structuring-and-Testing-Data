853695fd2ef37a29b98a66d3b3312edf38863c14
Fares Bakhet <faresak47roo@gmail.com>
2025-10-04 20:05:09 +0100

    Fix the return error

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here : A syntax error for the semicolon after return.
 
 function sum(a, b) {
   return;
@@ -8,6 +8,11 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here : The semicolon after return end the statement. So there will be no value returned from the function.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
