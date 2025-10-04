73fd400bb56c84f8239a9ca39df469456ce274f4
Fares Bakhet <faresak47roo@gmail.com>
2025-10-04 21:22:50 +0100

    Upper snake case

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,9 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function snakeItUp(name) {
+  const upperCase = name.toUpperCase();
+  return upperCase.replaceAll(" ", "_");
+}
+console.log(snakeItUp("fares bakhet is my name"));
