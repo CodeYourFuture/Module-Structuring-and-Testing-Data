6732232bdf06c72350e09f5c64af6c52cd05672f
Della Bella <gisdellabella@uol.com.br>
2025-10-05 21:29:50 +0100

    practicing charAt() slice() Math.floor() Math.random() lastIndexOf() replaceAll()

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,19 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// 3 functions:
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// const percentageChange = (priceDifference / carPrice) * 100;
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+//Error was in line 5 missing a coman between the get to replace ""
 
 // c) Identify all the lines that are variable reassignment statements
+// lines 4, 5 
 
 // d) Identify all the lines that are variable declarations
+// lines 1 and 2 , 7 and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// it is converting the numbers prices to real numbers replace the commnas to empty string so takrs the commans out
