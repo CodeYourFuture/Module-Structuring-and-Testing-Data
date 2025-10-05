0b19fc5884f56e58ed0aef9c98ce2839ca5ab3a8
Abrsh100 <abrdbr2008@gmail.com>
2025-10-05 23:11:42 +0100

    Explaining about the = sign after the count

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Answer
+// Line 3; when we the right side first count in the right side is considerd as 0 so 0+1 will be 1.
+// But the = sign does not mean the mathimatical equal, its function is to take the result on the right and store it on the left.
+// Therfore if we write the same code in next line the result is not one but 2
