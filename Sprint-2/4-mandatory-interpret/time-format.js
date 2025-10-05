544cb8362e26ead956fed9ca02e83abd23ce3c99
Alaa-Tagi <alaa3litagi1994@gmail.com>
2025-10-05 16:39:08 +0100

    Update time-format.js

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -17,18 +17,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> write your answer here : three times in Line 11 with return.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here : 0, because totalHours is 0.
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here : "00", because num is converted to string and padded to 2 characters with "0".
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here : 1, because remainingSeconds is 1 on python tutor.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> write your answer here : "00", because num is converted to string and padded to 2 characters with "0". That's mean "1" becomes "01".
