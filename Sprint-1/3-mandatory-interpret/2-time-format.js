405be779ca126d6ef14d065acfdd1cd3fa78bc8d
AdnaanA <thetriplex247@gmail.com>
2025-10-06 11:17:11 +0100

    corrected the answers and updated code to add leading 0 when values are less than 10

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -6,7 +6,9 @@ const totalMinutes = (movieLength - remainingSeconds) / 60;
 const remainingMinutes = totalMinutes % 60;
 const totalHours = (totalMinutes - remainingMinutes) / 60;
 
-const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
+const result = `${String(totalHours).padStart(2, "0")}:${String(
+  remainingMinutes
+).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
 console.log(result);
 
 // For the piece of code above, read the code and then answer the following questions
@@ -15,7 +17,7 @@ console.log(result);
 // there are 6 variable declarations in this program. They are on lines 1, 3, 4, 6, 7 and 9
 
 // b) How many function calls are there?
-// there are 2 function calls in this program. They are on lines 9 and 10
+// there is 1 function calls in this program. And it is on line 10
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
@@ -27,7 +29,7 @@ seconds and then dividing that result by 60.*/
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 /* the variable result represents the total length of the movie in hours, minutes and seconds. A better name for this variable could 
-be movieDuration.*/
+be formattedmovieDuration.*/
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
 // yes this code will work for all values of movieLength as long as the value is a non-negative integer representing the length of a movie in seconds.
