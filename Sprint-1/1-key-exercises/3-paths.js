e26235605567431dd79e8816ebeddc042aa99868
Global Nmae <babatunde.yusuf@outlook.com>
2025-10-07 08:02:03 +0100

    created dir & ext variables + logged output to console

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,9 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex); // Creates a variable to store the dir part of the filePath variable
+const ext = base.slice(base.lastIndexOf(".")); // Create a variable to store the ext part of the variable
+console.log(`The dir part of ${filePath} is ${dir}`); // outputs "/Users/mitch/cyf/Module-JS1/week-1/interpret"
+console.log(`The ext part of ${filePath} is ${ext}`); // outputs ".txt"
 
 // https://www.google.com/search?q=slice+mdn
