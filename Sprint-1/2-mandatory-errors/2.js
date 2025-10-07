f3b8cf59fc6c2faa570a67a1971d29e9d506b5b0
shaghayeghfar <146011477+shaghayeghfar@users.noreply.github.com>
2025-10-07 18:22:52 +0100

    mandatory-error task done

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+// what's the error ? 
+//                  const was declared after using it , we have to declare a variable first and then use it.
 
-console.log(`I was born in ${cityOfBirth}`);
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+
+//identifiers cannot start with a number in Javascript
