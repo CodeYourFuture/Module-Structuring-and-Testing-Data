bbcddb9f553ce7cf611e442abd08399b60473b57
iswat <ibeetek@gmail.com>
2025-10-07 23:22:55 +0100

    Interpret the error message

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,11 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// Interpretation:
+// The error "SyntaxError: Unexpected identifier 'is'" means that JavaScript found the word "is", but it didn’t make sense in that position. 
+// It wasn’t valid code that JavaScript could understand.
+
+// Why the error occurred:
+// The error occurred because the sentence was written as plain text instead of a comment.
+// To fix it, the sentence should be written as a comment (by adding // at the beginning) so that JavaScript ignores it.
+
