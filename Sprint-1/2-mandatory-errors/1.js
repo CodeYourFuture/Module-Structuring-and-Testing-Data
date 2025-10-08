5b0c0bc5ed387bba69a55aa4e9d8495f3fe12202
iswat <ibeetek@gmail.com>
2025-10-08 09:47:25 +0100

    Interpret error message when a const variable is reassigned

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,7 @@
 
 const age = 33;
 age = age + 1;
+
+
+// The error message is "Assignment to constant variable", which means that a variable declared with the 'const' keyword
+//  cannot be reassigned to a new value after its initial declaration.
