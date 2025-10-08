2f25058f43b9432dea6b356110f85ac1547d5352
iswat <ibeetek@gmail.com>
2025-10-08 22:27:21 +0100

    Interpret the error message that occurs when a number starts a variable name

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+console.log(12HourClockTime , 24hourClockTime);
+
+// Answer
+// After running the code, it gave an error message "SyntaxError: Invalid or unexpected token". 
+// This error means that Javascript was unable to read the code because a variable name cannot start with a number
+// I can fix this code by removing the numbers from the start of the variable name
+
+const HourClockTime = "20:53";
+const hourClockTime = "08:53";
