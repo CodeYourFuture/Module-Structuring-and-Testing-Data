08353466076e5ba43f5335dcda2217b5bbb68679
iswat <ibeetek@gmail.com>
2025-10-08 14:36:59 +0100

    Interpret error message for when a variable is used before declaration

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,5 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+// The error message 'Cannot access 'cityOfBirth' before initialization' occured because the 'const' variable was used before it was declared. 
