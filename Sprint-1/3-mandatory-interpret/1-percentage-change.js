b9f2f51ec7de9a9e95af4e85fe38a74eeb9f9b8e
iswat <ibeetek@gmail.com>
2025-10-08 23:16:36 +0100

    Identify function calls, errors, variable declarations/reassignments, and explain Number(...replaceAll(...)) usage

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -1,8 +1,8 @@
 let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
-carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+carPrice = Number(carPrice.replaceAll("," ,""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ,""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,17 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// Answer: There are five function calls in this file. function was called on line 4 (twice), line 5 (twice) and line 10.
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// Answer:
+// After running the code, the error occurred because a comma was missing between the quotation marks on line 5. I fixed it by adding the missing comma.
 
 // c) Identify all the lines that are variable reassignment statements
+// Answer: Variable reassignment statements occurred on lines 4 and 5
 
 // d) Identify all the lines that are variable declarations
+// Answer: Variable declarations occurred on lines 1, 2, 7, and 8.
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// Answer: The .replaceAll() method runs first to find all the comma in the variable carPrice and replace them with nothing then the Number() functions  runs to convert the value to a number data type
