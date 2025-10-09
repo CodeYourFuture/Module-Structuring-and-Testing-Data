812cf8314b20b73a119d9f8b27f49a4250470ee5
Della Bella <gisdellabella@uol.com.br>
2025-10-09 07:57:30 +0100

    Fix : ****pass values into the function when you call it, not when you define it**** Parameters are NAMES for values, not the values themselve ***

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,16 +5,25 @@
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+//SyntaxError: Unexpected number
+
 
 // =============> explain this error message here
+// passing nunber 3 in the function without declarint it as a number to be square 
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    const squareNumber= (num * num);
+    return squareNumber ;
+}
 
+console.log (square(3));
+console.log (square(4));
