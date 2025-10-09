b5de08b92f56bc5424c222b51101f87db10599ef
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-10-09 19:31:45 +0100

    build up getAngleType case by case

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -11,8 +11,19 @@ function getAngleType(angle) {
   if (angle === 90) {
     return "Right angle";
   }
-   // Run the tests, work out what Case 2 is testing, and implement the required code here.
-   // Then keep going for the other cases, one at a time.
+  if (angle < 90 && angle > 0) {
+    return "Acute angle";
+  }
+  if (angle > 90 && angle < 180) {
+    return "Obtuse angle";
+  }
+  if (angle === 180) {
+    return "Straight angle";
+  }
+  if (angle > 180 && angle < 360) {
+    return "Reflex angle";
+  }
+  return "Invalid angle";
 }
 
 // The line below allows us to load the getAngleType function into tests in other files.
@@ -50,14 +61,33 @@ assertEquals(acute, "Acute angle");
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
 const obtuse = getAngleType(120);
-// ====> write your test here, and then add a line to pass the test in the function above
+assertEquals(obtuse, "Obtuse angle");
 
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
 // ====> write your test here, and then add a line to pass the test in the function above
-
+const straight = getAngleType(180);
+assertEquals(straight, "Straight angle");
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
 // ====> write your test here, and then add a line to pass the test in the function above
+const reflex = getAngleType(270);
+assertEquals(reflex, "Reflex angle");
+
+let angle = 120;
+console.log(angle + " degrees is a " + getAngleType(angle));
+angle = 45;
+console.log(angle + " degrees is a " + getAngleType(angle));
+angle = 90;
+console.log(angle + " degrees is a " + getAngleType(angle));
+angle = 180;
+console.log(angle + " degrees is a " + getAngleType(angle));
+angle = 270;
+console.log(angle + " degrees is a " + getAngleType(angle));
+angle = -10;
+console.log(angle + " degrees is a " + getAngleType(angle));
+angle = 400;
+console.log(angle + " degrees is a " + getAngleType(angle));
+// Case 6: Handle Invalid Angles:
