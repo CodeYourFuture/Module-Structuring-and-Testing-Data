0440a7fd5822abeb922c44e86d7414c400de4fa9
Della Bella <gisdellabella@uol.com.br>
2025-10-09 08:31:49 +0100

    Fix scope of the function

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,25 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> looking at it I think the sytntaks is worng return; a+b;
+
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> 
+//   I declare in the scope of function what a,b need to do return  
+// call the function with the template literal 
+// wich will give me the Text sentence with the result of any sum 
+
+// Finally, correct the code to fix the problem
+//  =============> 
 
 function sum(a, b) {
-  return;
-  a + b;
+   const newSum = a + b;
+   return newSum;
+ 
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
