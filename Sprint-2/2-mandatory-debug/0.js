b9391ff26145942d1e71389b95d9bc4bf365b848
Della Bella <gisdellabella@uol.com.br>
2025-10-09 08:23:04 +0100

    Add Return to scope of the Function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -8,7 +8,20 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
+//output= The result of multiplying 10 and 32 is undefined
+
 // =============> write your explanation here
+//The function runs console.log(10 * 32) print the result = 320 
+// after it runs the function dosen't have a return anything ang gives =  undefined.
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> to use the function that gives a result need to use RETURN not consol.log inside the function
+
+function multiply(a, b) {
+  const forAllMutiply = (a * b);
+  return forAllMutiply; //return can be use for diferents values/results
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//output= The result of multiplying 10 and 32 is 320
