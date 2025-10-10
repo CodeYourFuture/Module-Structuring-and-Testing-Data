8fd03183dc9a250962d318ea4c1b27573907fd21
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-10-10 17:50:55 +0100

    The time-format issue has been solved successfully

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,20 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// 6
 
 // b) How many function calls are there?
+//1 
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
+// Is return the remainder after dividing movieLength by 60
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+//calculate how long the move is in minutes.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+//MovieLengthH:M:S
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+//It works for positive integers
