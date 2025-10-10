81e5bf5dd3f01af15325961c89c8a64225986aa7
Global Nmae <babatunde.yusuf@outlook.com>
2025-10-10 18:53:24 +0100

    updated to correct code error

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+let age = 33; //changing const to let allows reassignment.
+age = age + 1; //age is now 34
+
+console.log(age); //This line was added just to test the expression. Returns 34.
