1f028f71d4ca06c3a6219d899f951f00c9f1018b
iswat <ibeetek@gmail.com>
2025-10-10 18:26:28 +0100

    Define a function to convert argument to time with "am" and add bassertions to compare current output with target output

--- prep/sprint-2/assertions-exercise.js
+++ prep/sprint-2/assertions-exercise.js
@@ -1,7 +1,25 @@
-const calculation = 10 + 32;
-const result = 40;
+// const calculation = 10 + 32;
+// const result = 40;
 // console.assert(calculation === result);
 
-function formatAs12HourClock() {}
-console.log(formatAs12HourClock())
-console.assert(formatAs12HourClock("08:00") === "08:00 am" , `current output: ${formatAs12HourClock("08:00")} , target output: 08:00 am`);
+// function formatAs12HourClock() {}
+// console.log(formatAs12HourClock())
+// console.assert(formatAs12HourClock("08:00") === "08:00 am" , `current output: ${formatAs12HourClock("08:00")} , target output: 08:00 am`);
+
+function formatAs12HourClock(time) {
+  return `${time} am`;
+}
+
+const currentOutput = formatAs12HourClock("08:00");
+const targetOutput = "08:00 am";
+console.assert(
+  currentOutput === targetOutput,
+  `current output: ${currentOutput}, target output: ${targetOutput}`
+);
+
+const currentOutput1 = formatAs12HourClock("23:00");
+const targetOutput1 = "11:00 pm";
+console.assert(
+  currentOutput === targetOutput,
+  `current output: ${currentOutput}, target output: ${targetOutput}`
+);
