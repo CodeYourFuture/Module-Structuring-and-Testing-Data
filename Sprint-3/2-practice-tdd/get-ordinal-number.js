e638e780bb0a70dbf08bd575268895a8d0495175
shreefAhmedM <livecoding.33@gmail.com>
2025-10-10 14:36:12 +0100

    refactoring and updating get-ordinal-nuber.js file

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,17 +1,20 @@
 function getOrdinalNumber(num) {
-  let last2Digit = num % 100;
-  let las1Digit = num % 10;
+  const last2Digit = num % 100;
+  const last1Digit = num % 10;
+
   if (last2Digit >= 11 && last2Digit <= 13) {
     return num + "th";
   }
-  if (las1Digit === 1) {
-    return num + "st";
-  } else if (las1Digit === 2) {
-    return num + "nd";
-  } else if (las1Digit === 3) {
-    return num + "rd";
-  } else {
-    return num + "th";
+
+  switch (last1Digit) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
   }
 }
 
