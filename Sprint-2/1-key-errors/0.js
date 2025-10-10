b619d9131b18944cbb902cc017083e9a607774f7
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-10 17:27:00 +0100

    Implement 12-hour clock formatting function and add test cases

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> this function get the argument and as string then first letter in capital and the rest of string in lower case;
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+// the problem is in line 8, because we can not use the "str" twice, currently once we used as parameter and again as variable,
 // =============> write your new code here
+
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+   
+}
+console.log(capitalise("hello world"));
