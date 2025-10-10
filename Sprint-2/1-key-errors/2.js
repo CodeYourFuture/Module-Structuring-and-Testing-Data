3dcaa8fe727c77eb175071e5b57334f3f0b7cb7c
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-10 18:12:39 +0100

    Fix syntax error in square function by using a variable as parameter

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,31 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// in javaScript function parameters should be variable names, but in line 10 we used a number "3" as parameter,
+// so the error will be "SyntaxError"
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+// function square(3) {
+//                 ^
+
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+
+// the problem is in line 10, because we can not use a number as parameter of the function, it should be variable name,
+// so the error is "SyntaxError: Unexpected number"
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
 
+
+console.log(square(3));
