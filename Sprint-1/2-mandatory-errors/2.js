73129858197a88a456280464710c166f1f201d0d
saff-coder <sophi.m.80@gmail.com>
2025-10-10 14:06:28 +0100

    All the tasks are complete

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+ //we have to declare the variable cityOfBirth with const or let before using it in the console.log statement
