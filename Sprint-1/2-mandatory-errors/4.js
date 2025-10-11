61a899904de8de81be83d0bf58867507e87a7510
asaniDev <asanidev9@gmail.com>
2025-10-11 16:04:02 +0100

    removed numbers from start of string name to end of string name

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const HourClockTime12 = "20:53"; //variable name cannot start with a number.
+const hourClockTime24 = "08:53";
+
+console.log(`12 hr clock ${HourClockTime12}, 24 hr clock ${hourClockTime24}`);
