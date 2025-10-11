81fb70c7182401532ce99d259221215b99c7f0a1
enjoy15 <narumoljoy@gmail.com>
2025-10-11 12:26:00 +0100

    Sprint2/01-key-errors

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,19 +2,27 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// PREDICTION: There will be a SyntaxError because 'decimalNumber' is declared twice - once as parameter, once as variable
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> write your explanation here
+// Cannot redeclare 'decimalNumber' with 'const' inside function - it's already the parameter name
+// Finally, correct the code to fix the problem
+// =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5));
