63a51e69b088de07e45428777838ea68c54abbd2
Della Bella <gisdellabella@uol.com.br>
2025-10-11 15:13:42 +0100

    Practicing Time

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -2,14 +2,15 @@ function pad(num) {
   return num.toString().padStart(2, "0");
 }
 
-function formatTimeDisplay(seconds) {
-  const remainingSeconds = seconds % 60;
-  const totalMinutes = (seconds - remainingSeconds) / 60;
-  const remainingMinutes = totalMinutes % 60;
-  const totalHours = (totalMinutes - remainingMinutes) / 60;
+function formatTimeDisplay(seconds) { // seconds = 61
+  const remainingSeconds = seconds % 60; // 1
+  const totalMinutes = (seconds - remainingSeconds) / 60; // 61 - 1) / 60 = 1
+  const remainingMinutes = totalMinutes % 60; // 1 / 60 = 1
+  const totalHours = (totalMinutes - remainingMinutes) / 60; // 0
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
+console.log(formatTimeDisplay(61));
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -17,18 +18,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+ //3 times 
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// 0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// 00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// 1
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// 01
