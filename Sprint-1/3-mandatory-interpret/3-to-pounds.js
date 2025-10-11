89e21bedd8d5602b98b2f43b7a6ef555bb00956d
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-10-11 16:07:35 +0100

    Solve the last task in madatory-interpret with my own words

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,20 +1,26 @@
 const penceString = "399p";
+ //initialises a string variable with the value "399p"
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 );
+// declare a new var and assign penceString value without last character (p)/
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+//declare a new variable and Make sure it is at least 3 digits long eg: 1 become 001.
+
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
+//declare a new variable and assign the paddedPenceNumberString value without last two digit.
 
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
-
+// pence= take the pound length and remove it and keep all rest digit and if the shorter than 2 digit add 0 until paddedPenceNumberString
+//  .substring(paddedPenceNumberString.length - 2) becomes 2 digits
 console.log(`£${pounds}.${pence}`);
 
 // This program takes a string representing a price in pence
