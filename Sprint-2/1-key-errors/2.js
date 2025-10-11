9d22332a6e80d45c3ee6ec8531bf334f0f1b937e
enjoy15 <narumoljoy@gmail.com>
2025-10-11 12:31:12 +0100

    Sprint2/2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,23 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// PREDICTION: There will be a SyntaxError because '3' is not a valid parameter name
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// The error message indicates that the function parameter is not valid because '3' is a number literal, not a variable name.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
-
+console.log(square(5)); // Test the function
