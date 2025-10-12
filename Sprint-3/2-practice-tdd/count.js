6ab27fc3699952eb7a8627d7ddf40e3eedb4897d
Fares Bakhet <faresak47roo@gmail.com>
2025-10-12 21:10:43 +0100

    Biuld a countChar Code

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,12 @@
-function countChar(stringOfCharacters, findCharacter) {
-  return 5
+function countChar(str, char) {
+  return str.split(char).length - 1;
 }
 
 module.exports = countChar;
+
+console.log(
+  countChar(
+    "The split() method of String values takes a pattern and divides this string into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.",
+    "a"
+  )
+);
