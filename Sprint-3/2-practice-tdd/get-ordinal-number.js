2eaa92cf3e50adbcf1e970adca6bafc3ca64d7db
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-10-12 21:51:24 +0100

    Editing getordinalnumber code

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,14 +1,16 @@
 function getOrdinalNumber(num) {
-  if (num === 1) {
-    return "1st";
-  }
-  if (num === 2) {
-    return "2nd";
-  }
-  if (num === 3) {
-    return "3rd";
-  }
-  return num + "th";  
+    num = num.toString();
+    if (num.slice(-2) === '11' || num.slice(-2) === '12' || num.slice(-2) === '13') {
+        return num + 'th';
+    } else if (num.slice(-1) === '1') {
+        return num + 'st';
+    } else if (num.slice(-1) === '2') {
+        return num + 'nd';
+    } else if (num.slice(-1) === '3') {
+        return num + 'rd';
+    } else {
+        return num + 'th';
+    } 
 }
 
 module.exports = getOrdinalNumber;
@@ -18,3 +20,4 @@ console.log(getOrdinalNumber(3));
 console.log(getOrdinalNumber(4));
 console.log(getOrdinalNumber(11));
 console.log(getOrdinalNumber(22));
+console.log(getOrdinalNumber(33));
