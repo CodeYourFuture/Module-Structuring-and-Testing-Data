eaad7eda02ffbb2671fbe4c58339ce8bb53638bc
ahmadehsas <ahmadehsas14345@gmail.com>
2025-10-12 22:43:57 +0100

    fixed the return statement.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -2,8 +2,8 @@
 //  =============> write your prediction here: The return function is not defined, so the function will not return the sum of a and b.
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
+  
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
