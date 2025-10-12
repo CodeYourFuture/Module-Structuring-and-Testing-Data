d440aaf8d137173a71e7402ba37a8ccd07b49553
Fares Bakhet <faresak47roo@gmail.com>
2025-10-12 18:35:14 +0100

    Passed isProperFraction test

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -1,6 +1,6 @@
 // This statement loads the isProperFraction function you wrote in the implement directory.
 // We will use the same function, but write tests for it using Jest in this file.
-const isProperFraction = require("../implement/2-is-proper-fraction");
+const isProperFraction = require("../implement/2-is-proper-fraction"); // The path
 
 test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
