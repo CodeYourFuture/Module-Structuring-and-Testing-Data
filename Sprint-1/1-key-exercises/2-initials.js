096874607fefc127c0f230859926bbed8d67120b
khalidbih <kain19m@hotmail.com>
2025-10-13 21:06:21 +0100

    Complete exercise 2, I checked it with VScode runner it worked

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -1,11 +1,19 @@
 let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
+firstName[0]
+middleName[0]
+lastName[0]
+let initials = (firstName[0] + middleName[0] + lastName[0]);
+console.log (initials);
+
+
+
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
