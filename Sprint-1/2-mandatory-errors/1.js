6ce8a2ec98462cc54a060fca542df5c4c39a3a5e
Georgina Rogers <zilinskyterogers@gmail.com>
2025-10-13 14:21:13 +0100

    constant cannot be reassigned

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,9 @@
 
 const age = 33;
 age = age + 1;
+// This will cause an error because age is a constant and cannot be reassigned
+console.log("I am " + age + " years old");
+// To fix the error, we can change const to let, which allows reassignment
+let age = 33;
+age = age + 1;
+console.log("I am " + age + " years old");  
