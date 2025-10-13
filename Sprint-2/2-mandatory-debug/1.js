4ca8651667236e6923114ba5201eca84b1c34946
Imran Mohamed <info@imranbmohamed.com>
2025-10-13 06:38:07 +0100

    completed tasks in 2-mandatory-debug/1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// When the code is run the console log will show undefined because the function returns before the sum is calculated
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+  // When a return statement is reached the function exits immediately. Therefore the line a + b is never executed and the function returns undefined.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
