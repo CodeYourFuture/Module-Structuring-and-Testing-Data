29a9fe7ed07f9e7859bbe16847039568033f0bd2
Georgina Rogers <zilinskyterogers@gmail.com>
2025-10-13 14:09:41 +0100

    variables cannot start with a number

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const ClockTime12Hour = "08:53";
+const ClockTime24Hour = "20:53";
+// Variables cannot start with a number
+// Can only contain letters, numbers, underscores and $
+// Variables wre written wrong way around
