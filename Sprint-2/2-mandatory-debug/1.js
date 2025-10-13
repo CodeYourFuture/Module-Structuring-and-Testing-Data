503eb4f8782f7287d3605d8860509ec387b998b0
Fares Bakhet <faresak47roo@gmail.com>
2025-10-13 21:21:07 +0100

    Re-answering the explation part

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here : A syntax error for the semicolon after return.
+//  =============> write your prediction here : There will be an error in return line.
 
 function sum(a, b) {
   return;
@@ -8,7 +8,7 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here : The semicolon after return end the statement. So there will be no value returned from the function.
+// =============> write your explanation here :  The return statement ends the function immediately. Since there is a line break after return, JavaScript automatically inserts a semicolon, so the function returns undefined and the expression a + b is never executed.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
