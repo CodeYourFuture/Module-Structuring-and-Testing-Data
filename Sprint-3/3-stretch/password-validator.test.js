e64db320cf4a152e5b191c995f137477f4cd742d
Fares Bakhet <faresak47roo@gmail.com>
2025-10-13 16:51:07 +0100

    Pass passwordValidate tests

--- Sprint-3/3-stretch/password-validator.test.js
+++ Sprint-3/3-stretch/password-validator.test.js
@@ -16,11 +16,59 @@ You must breakdown this problem in order to solve it. Find one test case first a
 */
 const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
-    // Arrange
-    const password = "12345";
-    // Act
-    const result = isValidPassword(password);
-    // Assert
-    expect(result).toEqual(true);
-}
-);
+  // Arrange
+  const password = "12345";
+  // Act
+  const result = isValidPassword(password);
+  // Assert
+  expect(result).toEqual(false);
+});
+
+// 2. Password is too short
+test("password must be at least 5 characters", () => {
+  const password = "Ab1!";
+  const result = isValidPassword(password);
+  expect(result).toEqual(false);
+});
+
+// 3. Password must contain at least one uppercase letter
+test("password must contain at least one uppercase letter", () => {
+  const password = "abc!1";
+  const result = isValidPassword(password);
+  expect(result).toEqual(false);
+});
+
+// 4. Password must contain at least one lowercase letter
+test("password must contain at least one lowercase letter", () => {
+  const password = "ABC!1";
+  const result = isValidPassword(password);
+  expect(result).toEqual(false);
+});
+
+// 5. Password must contain at least one number
+test("password must contain at least one number", () => {
+  const password = "Abc!@";
+  const result = isValidPassword(password);
+  expect(result).toEqual(false);
+});
+
+// 6. Password must contain at least one symbol (! # $ % . * &)
+test("password must contain at least one symbol", () => {
+  const password = "Abc12";
+  const result = isValidPassword(password);
+  expect(result).toEqual(false);
+});
+
+// 7. Password must not be a previously used password
+test("password must not be in the previous passwords array", () => {
+  const password = "Abc!1"; // this is in the passwords array inside the validator
+  const result = isValidPassword(password);
+  expect(result).toEqual(false);
+});
+
+// 8. Password is valid when all requirements are met
+test("password is valid when it meets all requirements", () => {
+  const password = "Abc!2";
+  const result = isValidPassword(password);
+  expect(result).toEqual(true);
+});
