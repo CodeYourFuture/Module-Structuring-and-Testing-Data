8499ca3aabbe5b0baad48339ddcf0e7ec1079107
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-10-13 20:44:47 +0100

    Editing passwordValidator code

--- Sprint-3/3-stretch/password-validator.js
+++ Sprint-3/3-stretch/password-validator.js
@@ -1,6 +1,30 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
-}
+const passwords = ["Amtf22#", "123$cdo"];
+function passwordValidator(password) {  
+    if (password.length < 5) {
+        return false;
+    }
+    
+    if (!/[A-Z]/.test(password)) {
+        return false;
+    }
+    
+    if (!/[a-z]/.test(password)) {
+        return false;
+    }
+    
+    if (!/[0-9]/.test(password)) {
+        return false;
+    }
+    if (!/[!#$%&? "]/.test(password)) {
+        return false;
+    }
 
+    if (passwords.includes(password)) { 
+        return false;
+    }
+    
+    
+    return true;
+}
 
 module.exports = passwordValidator;
