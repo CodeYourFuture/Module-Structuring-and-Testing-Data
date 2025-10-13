812128cc073aa700fd1bb36a7400a31251b0bb59
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-10-13 19:27:34 +0100

    write addition explanation for the prediction part

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,12 +1,14 @@
 // Predict and explain first...
 
-// =============> write your prediction here : There is no return statement for the parameters.
+// =============> write your prediction here :  This function doesn’t have a return statement. 
+// That means: even though it takes parameters, it won’t give any value back when called and the result will be undefined.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//function multiply(a, b) {
+//console.log(a * b);
+//}
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here : /* The function multiply does not return any value, it only logs the result to the console. It returns undefined. To fix this, we need to add a return statement in the multiply function. */
 
