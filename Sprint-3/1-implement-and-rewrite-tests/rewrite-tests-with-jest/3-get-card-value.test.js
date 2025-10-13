56625ec8ce659db6f29207d3771a315e1f135859
enjoy15 <narumoljoy@gmail.com>
2025-10-13 23:02:54 +0100

    3-get card value test with jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,28 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return numeric value for number cards (2-10)", () => {
+  const fiveofHearts = getCardValue("5♥");
+  expect(fiveofHearts).toEqual(5);
+});     
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards (J, Q, K)", () => {
+  const jackOfDiamonds = getCardValue("J♦");
+  const queenOfClubs = getCardValue("Q♣");
+  const kingOfHearts = getCardValue("K♥");
+  expect(jackOfDiamonds).toEqual(10);
+  expect(queenOfClubs).toEqual(10);
+  expect(kingOfHearts).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace (A)", () => {
+  const aceOfSpades = getCardValue("A♠");
+  expect(aceOfSpades).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should return undefined for invalid cards", () => {
+  const invalidCard = getCardValue("1♠");
+  expect(invalidCard).toEqual(undefined);
+});
