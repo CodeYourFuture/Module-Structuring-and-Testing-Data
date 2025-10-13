463d5315f4b4f3435a3c5bcb81a41672052c49ec
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-13 20:52:44 +0100

    added tests for handling number cards and face cards in getCardValue function

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,25 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 3 for 3 of Spades", () => {
+  const threeOfSpades = getCardValue("3♠");
+  expect(threeOfSpades).toEqual(3);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Spades", () => {
+  const jackOfSpades = getCardValue("J♠");
+  expect(jackOfSpades).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Spades", () => {
+  const aceofSpades = getCardValue("A♠");
+  expect(aceofSpades).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should return invalid card  for g of spades", () => {
+  const gOfSpades = getCardValue("g♠");
+  expect(gOfSpades).toEqual("Invalid card rank.");
+});
