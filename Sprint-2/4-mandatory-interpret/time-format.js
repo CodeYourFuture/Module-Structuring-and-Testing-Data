f6ed125b38a38d973de77c6d85f7f7bbbf21878b
ahmadehsas <ahmadehsas14345@gmail.com>
2025-10-13 22:42:21 +0100

    reviewed the answer of questions and fixed all the questions.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -10,6 +10,7 @@ function formatTimeDisplay(seconds) {
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
+console.log(formatTimeDisplay(61));
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -17,18 +18,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here: When formatTimeDisplay is called four times pad will be called, one for remainingSeconds, one for remainingMinutes, one for totalMinutes and one for totalHours.
+// =============> write your answer here: When formatTimeDisplay is called three times pad will be called, one for 'remainingSeconds', one for 'remainingMinutes', and one for 'totalHours'.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here: When pad is called for thee first time, num will be assigned the value of 1, which is the value of remainingSeconds.
+// =============> write your answer here: When pad is called for thee first time, num will be assigned the value of totalHours.
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here: The return value of pad when called for the first time will be `01` because it pads the number 1 with a leading zero to make it two digits.
+// =============> write your answer here: The return value of pad when called for the first time will be `pad(totalHours)` for example if the return of 'totalHours' is '1' we will add '0' to make it two digits('01').
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here: When pad is called for the last time, num will be assigned the value of 0, which is the value of totalHours.
+// =============> write your answer here: When pad is called for the last time, num will be assigned the value of 'remainingSecond'.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here: when pad is called for the last time, the return value will be `0` because it pads the number 0 with a leading zero to make it two digits.
+// =============> write your answer here: when pad is called for the last time, the return value will be `pad(remainingSecond)`.
