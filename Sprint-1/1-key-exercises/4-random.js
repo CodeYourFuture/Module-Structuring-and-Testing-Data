2e44be5292a6a01209c7e4dd7b7b427d3352616a
Georgina Rogers <zilinskyterogers@gmail.com>
2025-10-13 14:03:49 +0100

    random num between minimum and maximum generated

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,6 +2,11 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+// num is an integer between minimum and maximum, between 1 and 100 inclusive 
+// math.random() produces a decimal between 0 (inclusive) and 1 (exclusive)
+// math.floor() rounds down to the nearest integer
+// (maximum - minimum + 1)) + minimum chooses a number between minimum and maximum and adds 1 so its inclusive of maximum
+console.log(num);
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
