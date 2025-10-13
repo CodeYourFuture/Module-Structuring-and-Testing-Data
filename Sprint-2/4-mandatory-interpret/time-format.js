9cac48fff8d313b9c5c73f4e3f7fa6278ab8c218
Imran Mohamed <info@imranbmohamed.com>
2025-10-13 09:12:09 +0100

    completed tasks in 4-mandatory-interpret/time-format.js

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,21 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+//pad is called 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
+formatTimeDisplay(61);
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-
+// 0
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
-
+// '00'
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-
+// 1
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// '01'
+
