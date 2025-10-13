5ab6322f2f182e62d95b8e084f24263950904809
asaniDev <asanidev9@gmail.com>
2025-10-14 00:42:55 +0100

    removed line 10 and passed function to console

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,19 +2,21 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+//decimalNumber does not exist outside the scope of the convertToPercentage funtion so the log will throw an error saying it has not been declared.
 
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  //const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
 
 // =============> write your explanation here
-
+//decimalNumber is passed as a parameter when the function is called, line 10 tries to declare it again and assign it a new fixed value. we would need to remove that line so we can use the value passed in with the function call.
+//decimalNumber only exists inside the function scope so console.log cannot access it an error will be thrown. we need to instead pass the function to console.log.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
