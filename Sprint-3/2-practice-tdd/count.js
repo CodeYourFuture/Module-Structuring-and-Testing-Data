47e385f48cc7854c2df427a54c09cef9d9f05bdd
enjoy15 <narumoljoy@gmail.com>
2025-10-14 12:17:02 +0100

    2-practice-tdd/count test

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,15 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let count = 0;
+  
+  // Loop through each character in the string
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    // If the current character matches the character we're looking for
+    if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+  }
+  
+  return count;
 }
 
 module.exports = countChar;
