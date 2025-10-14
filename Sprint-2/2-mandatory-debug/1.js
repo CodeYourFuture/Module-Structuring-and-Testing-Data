90013dcd67549cce10bb5bf03842f58ad94b0e3f
asaniDev <asanidev9@gmail.com>
2025-10-14 22:14:44 +0100

    moved the return command to the line below it

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// the console will show The sum of 10 and 32 is undefined, this is because the return command is before the calculation so the return has no value to bring back.
 
+//function sum(a, b) {
+//  return;
+//  a + b;
+//}
+
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+//the console will show The sum of 10 and 32 is undefined, this is because the return command is before the calculation so the return has no value to bring back.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
