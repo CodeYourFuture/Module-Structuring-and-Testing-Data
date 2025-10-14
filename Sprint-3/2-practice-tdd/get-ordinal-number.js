00e6c6d2ce24c59c23e4e9d61316be4d7676539c
enjoy15 <narumoljoy@gmail.com>
2025-10-14 13:17:56 +0100

    2-practice-tdd/get ordinal number test

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,21 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  // Handle special cases for teens (11th, 12th, 13th)
+  if (num % 100 >= 11 && num % 100 <= 13) {
+    return num + "th";
+  }
+  
+  // Handle based on last digit
+  const lastDigit = num % 10;
+  
+  if (lastDigit === 1) {
+    return num + "st";
+  } else if (lastDigit === 2) {
+    return num + "nd";
+  } else if (lastDigit === 3) {
+    return num + "rd";
+  } else {
+    return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
