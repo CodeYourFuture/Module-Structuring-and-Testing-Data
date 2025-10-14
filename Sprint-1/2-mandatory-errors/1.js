2b12a3404ee02ea40506df2dc28be306c5ce4c3f
Ali <naru.a@yahoo.com>
2025-10-14 21:39:29 +0100

    Change age from constant to variable for reassignment and update comments for clarity

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33; // changed from constant to variable so that value can be reassigned
 age = age + 1;
+console.log(age); // should print 34 running testing 
+
+
+//after testing can confirm age variable is now 34.
