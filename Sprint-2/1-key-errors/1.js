c26d1a3eacb2b032faf471f22f44210c9be98440
Global Nmae <babatunde.yusuf@outlook.com>
2025-10-15 12:29:28 +0100

    refactor: fix syntax errors and improve function definitions in key error examples

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -4,7 +4,7 @@
 // =============> write your prediction here
 
 // Try playing computer with the example to work out what is going on
-/*
+
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
@@ -13,22 +13,8 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
-*/
 
 // =============> write your explanation here
-/*
-1. Undeclared variables are automatically declared when first used. As a parameter in the 'convertToPercentage' function,
-'decimalNumber' is (automatically) already declared. trying to declare it again with 'const' will cause a syntax error.
-2. Also, the 'console.log(decimalNumber);' line is outside the function, so 'decimalNumber' is not defined in that scope.
-3 decimalNumber = 0.5; overwrites the argument supplied, and so to make the function work as intended, this line should be removed, so the function does not ignore its input argument.
-*/
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
-function convertToPercentage(decimalNumber) {
-  const percentage = `${decimalNumber * 100}%`;
-
-  return percentage;
-}
-
-console.log(convertToPercentage(0.5));
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,25 +1,19 @@
+
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-/*
+
 function square(3) {
     return num * num;
 }
-*/
+
 // =============> write the error message here
-// 1-SyntaxError: Unexpected number.
+
 // =============> explain this error message here
-/*
-1-The parameter name '3' is not a valid identifier. Function parameters names must (be valid variable names & not literal values), i.e. start with a letter, underscore (_), or dollar sign ($), and cannot be a number.
-2-Also, the function uses 'num' which is not defined anywhere. It should use the parameter name instead.
-3-Finally, the function is not called anywhere, so it won't produce any output.
-*/
+
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-function square(num) {
-  return num * num;
-}
-console.log(square(3)); // This line is inserted only to test the function. It returns 9
+
