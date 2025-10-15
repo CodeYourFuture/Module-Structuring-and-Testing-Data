4ee2dff55ebbc539129c0069343fc702f053518d
Fithi Teklom <fthiteklom@gmail.com>
2025-10-15 15:19:47 +0100

    updated 4.js template

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,7 +3,12 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-// In this exercise, you will need to work out what num represents?
-// Try breaking down the expression and using documentation to explain what it means
-// It will help to think about the order in which expressions are evaluated
-// Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(num);
+
+// num is a random integer between minimum and maximum, including both endpoints.
+// Math.random generates random decimal numbers between 0 and 1.
+// Math.floor() rounds a number down to the nearest integer.Example 12.8 is rounded to 12.
+//(maximum - minimum + 1)= (100 - 1 + 1)= 100.... So the expression becomes (Math.random()*100)
+//In the output I can see random numbers between 1 and 100.
+
+
