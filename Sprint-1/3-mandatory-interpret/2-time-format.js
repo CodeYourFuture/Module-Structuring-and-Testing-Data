246a80cb3200b0dc2f19ace07985c7a0c9b9dd08
Fithi Teklom <fthiteklom@gmail.com>
2025-10-15 18:06:37 +0100

    updated js template

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,21 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// There are 6.
 
 // b) How many function calls are there?
+//1 function call.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+//Remainder assignment. Which means when 8784 seconds is divided by 60 it gives 24.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+//movieLength - remainingSeconds removes the leftover seconds that don’t make up a full minute.
+//Dividing that by 60 converts the remaining seconds into whole minutes.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+//It represents movie duration. A better name can be formattedMovie.
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// Mostly yes, but not good for fractional values, negative numbers and numbers less than 0, the leading 0 may not be displayed.
