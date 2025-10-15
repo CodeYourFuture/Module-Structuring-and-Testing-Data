f6f3ec344c8349ca4d9b8dab11aeef464ac329cc
Fithi Teklom <fthiteklom@gmail.com>
2025-10-15 15:30:52 +0100

    0.js updated template

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,3 +1,3 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
-//we can simply comment it out by adding two slashes(//) at the begining of line 1.
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+//we can simply comment it out by adding two slashes(//) at the begining of line 1 and line 2.
