d873260b5e5316c20cc0360089ef83bc8f095770
Abrsh100 <abrdbr2008@gmail.com>
2025-10-15 08:17:41 +0100

    Answering each question with in the file by puting // to separate from the code

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+// the 12 and 24 are changing position 
+const 24HourClockTime = "20:53";
+const 12hourClockTime = "08:53";
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,4 +1,4 @@
-const penceString = "399p";
+onst penceString = "399p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
@@ -25,3 +25,13 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. const penceStringWithoutTrailingP = penceString.substring( 0, penceString.length - 1);
+//    This remove the p and left the string with only 399
+// 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+//    this code make sure that the string has 3 character. since one pound is equal 100 pence.
+// 4. const pounds = paddedPenceNumberString.substring( 0, paddedPenceNumberString.length - 2);
+//    This code takes the number leaving the last 2 digits  "in this case it takes 3 and leave 99"
+// 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2)  .padEnd(2, "0");
+//    This code takes the last digit from the string and also make sure that it has only two digits this gives a "99" value.
+// 6. console.log(`£${pounds}.${pence}`);
+//    his gives a result with £ sign and separated by . " in this case it gives £3.99 "
