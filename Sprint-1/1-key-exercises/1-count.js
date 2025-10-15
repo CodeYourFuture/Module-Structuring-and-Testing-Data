458e1553b0a33c5bd5ff2d0499fa097c634f4d2c
Fithi Teklom <fthiteklom@gmail.com>
2025-10-15 13:05:34 +0100

    1-count.js updated template

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -3,4 +3,4 @@ let count = 0;
 count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// So line 3 is adding the value of the variable we created which is count and assigned it as zero by 1. Or we are updating count to be one more than it was before.
