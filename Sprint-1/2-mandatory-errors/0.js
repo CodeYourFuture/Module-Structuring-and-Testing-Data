4b28f8f2064a6496af0b40f7669b2ffaeb796484
Fithi Teklom <fthiteklom@gmail.com>
2025-10-15 15:22:49 +0100

    updated 0.js

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,3 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+//we can simply comment it out by adding two slashes(//) at the begining of line 1.
