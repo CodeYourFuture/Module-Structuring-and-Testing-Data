a8d716b4bb50d34db724780d95289f2e5f006dc2
iswat <ibeetek@gmail.com>
2025-10-16 13:15:01 +0100

    Add code predictions, explanations, and corrections
    - Predict the behavior of the existing code and provide explanations.
    - Include any error messages encountered during execution.
    - Provide the corrected code to fix the errors.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,29 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// When the code is being read or parsed by Node, it will throw a syntax error because it cannot interpret the number 3 inside the parentheses.
+// This is because a number (3) is not a valid parameter name in a function definition.
 
-function square(3) {
-    return num * num;
-}
+
+//function square(3) {
+  //  return num * num;
+//}
 
 // =============> write the error message here
-
+// SyntaxError: Unexpected number
 // =============> explain this error message here
+// This error message indicates that JavaScript only allows identifiers as parameters in a function definition.  
+// If a literal value (such as a number, string, or object) is used instead, a SyntaxError will be thrown.
+
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
+
+const actualOutput = square(3);
+console.log(actualOutput);
 
 
