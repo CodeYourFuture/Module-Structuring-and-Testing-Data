a59de403378b87913f4a66b33e863ee202071823
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-16 12:50:01 +0100

    Implement repeat function to handle string repetition with error handling for negative counts

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,11 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str,count) {
+  if(count<0){
+    return "Error: negative numbers are not valid";
+  }
+  return (str.repeat(count));
 }
 
 module.exports = repeat;
+
+
+
