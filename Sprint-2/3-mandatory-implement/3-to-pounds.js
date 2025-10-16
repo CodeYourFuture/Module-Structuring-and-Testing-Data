e0973162482356b438a581f4f986803ef7987470
asaniDev <asanidev9@gmail.com>
2025-10-16 14:44:44 +0100

    created a toPounds function to return a pounds and pence notation

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,48 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
+
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+
+  return `£${pounds}.${pence}`;
+}
+
+console.log(toPounds("399p"));
+
+const actualOutput = toPounds("4685p");
+const expectedOutput = "£46.85";
+
+console.assert(
+  actualOutput === expectedOutput,
+  `expected to get ${expectedOutput}, but got ${actualOutput}`
+);
+
+const actualOutput1 = toPounds("123456p");
+const expectedOutput1 = "£1234.56";
+
+console.assert(
+  actualOutput1 === expectedOutput1,
+  `expected to get ${expectedOutput1}, but got ${actualOutput1}`
+);
+
+const actualOutput2 = toPounds("4p");
+const expectedOutput2 = "£0.04";
+
+console.assert(
+  actualOutput2 === expectedOutput2,
+  `expected to get ${expectedOutput2}, but got ${actualOutput2}`
+);
