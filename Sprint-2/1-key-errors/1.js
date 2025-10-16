aca43982ccb071ba6fa9049f9f42b6996db69797
iswat <ibeetek@gmail.com>
2025-10-16 12:20:49 +0100

    Explain why the program throws an error, simulate execution to identify the issue, and add the corrected code.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,19 +2,52 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// Answer
+// An error will occur when the program runs because a variable cannot be redeclared. 
+// The parameter decimalNumber is already a declared variable, so it cannot be redeclared again.
 
 // Try playing computer with the example to work out what is going on
 
+//function convertToPercentage(decimalNumber) {
+  //const decimalNumber = 0.5;
+  //const percentage = `${decimalNumber * 100}%`;
+
+  //return percentage;
+//}
+
+//console.log(decimalNumber);
+
+// =============> write your explanation here
+// Answer
+//function convertToPercentage(decimalNumber) {
+  //const decimalNumber = 0.5;
+  //const percentage = `${decimalNumber * 100}%`;
+
+  //return percentage;
+//}
+
+// When the script starts, JavaScript defines the function convertToPercentage, but it doesn’t run it yet.
+// This means that nothing is executed inside the function; only the function itself is stored in memory.
+
+// console.log(decimalNumber);
+// At this point, JavaScript looks for a variable called decimalNumber in the current scope (the global scope), but none exists. 
+// We only declared decimalNumber as a parameter inside the function (which hasn’t been called yet).
+// Result: Because decimalNumber isn’t defined globally, JavaScript will throw an error
+// At this point, JavaScript looks for a variable called decimalNumber in the current scope (the global scope).
+// But none exists — we only declared decimalNumber as a parameter inside the function (which hasn’t been called yet).
+// Error message: "SyntaxError: Identifier 'decimalNumber' has already been declared"
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+decimalNumber = 0.5;
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
+const ActualOutput = convertToPercentage(15);
+console.log(ActualOutput);
 console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
