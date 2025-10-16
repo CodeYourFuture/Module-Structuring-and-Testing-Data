de7beb8567fb0eda35d58198069809aab1c1514a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-16 15:58:06 +0100

    added code to the getordinalfunction to cover all possibilities

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,26 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastDigit=Number(num.toString().slice(-1));
+  const lastTwoDigits=Number(num.toString().slice(-2));
+
+  if(typeof num !=="number"){
+    return NaN;
+  }
+
+  if(lastTwoDigits>=10 && lastTwoDigits<=13){
+    return `${num}th`;
+  }
+
+  if (lastDigit === 1) {
+    return `${num}st`;
+  } else if (lastDigit === 2) {
+    return `${num}nd`;
+  } else if (num === 3) {
+    return "3rd";
+  }
+  
+  return `${num}th`;
+
 }
 
 module.exports = getOrdinalNumber;
+
