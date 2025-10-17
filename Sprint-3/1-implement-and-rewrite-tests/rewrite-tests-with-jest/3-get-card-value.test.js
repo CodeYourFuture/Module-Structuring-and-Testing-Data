3a4df3a280e1c4e8367e99aa241f266e4436e095
Della Bella <gisdellabella@uol.com.br>
2025-10-17 22:40:52 +0100

    Cards Tests

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,24 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+
+test("should return number cards", () => {
+const fiveofHearts = getCardValue("5♥");
+expect(fiveofHearts).toEqual(5);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test ("should return Face Cards J, Q, K ", () =>{
+  const jackOfClubs = getCardValue("J♣"); 
+  expect(jackOfClubs).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test ("should return Ace", () =>{
+const aceofHearts = getCardValue("A♥");
+ expect(aceofHearts).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw 'Invalid card rank' for a rank of '1'", () => {
+  expect(() => getCardValue("1♦")).toThrow("Invalid card rank.");
+});
