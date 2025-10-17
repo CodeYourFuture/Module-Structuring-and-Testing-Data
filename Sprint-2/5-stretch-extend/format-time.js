0d5ad45ee9c0643b416a62d1cf1bf385b41ab13e
asaniDev <asanidev9@gmail.com>
2025-10-17 02:05:59 +0100

    wrote tests for any edge cases and fixed resulting bugs

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,8 +4,14 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
+  const minutes = time.slice(3);
+  console.log(minutes);
+  if (hours == 12) {
+    return `${hours}:${minutes} pm`;
+  }
+
   if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+    return `${hours - 12}:${minutes} pm`;
   }
   return `${time} am`;
 }
@@ -22,4 +28,18 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("23:30");
+const targetOutput3 = "11:30 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("12:01");
+const targetOutput4 = "12:01 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
 );
