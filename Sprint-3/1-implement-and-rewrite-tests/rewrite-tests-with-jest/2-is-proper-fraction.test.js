3e16cd7c4687736f18d260caa805683b4455c0e8
Della Bella <gisdellabella@uol.com.br>
2025-10-17 21:38:39 +0100

    Function Numerator & Denominator Tests

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,6 +8,28 @@ test("should return true for a proper fraction", () => {
 
 // Case 2: Identify Improper Fractions:
 
+test("it is a Improper Fraction",() =>{
+  expect(isProperFraction(3, 2)).toEqual(false);
+});
+
 // Case 3: Identify Negative Fractions:
+test("it is a Negative Fraction",() =>{
+  expect(isProperFraction(-2, 6)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+
+test("It is a equal numerator and denominador", () =>{
+  expect(isProperFraction( 2, 2)).toEqual(false);
+});
+
+
+
+// function isProperFraction(numerator, denominator) {
+//   if (numerator < denominator) {
+//     return true ;
+//   }
+//   else {
+//     return false;
+//   }
+// }
