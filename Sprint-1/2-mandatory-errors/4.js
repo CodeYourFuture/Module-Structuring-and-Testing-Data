d8109f2141596ecfe0ee8701540970c32f02f156
Ali <naru.a@yahoo.com>
2025-10-18 18:59:38 +0100

    Rename variables to avoid starting with numbers and update comments for clarity 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const Twelve_HourClockTime = "20:53";
+const Twenty_Four_hourClockTime = "08:53";
+
+//Variables cannot have numbers at the start of their name
+//the times are in the wrong format for their variable names
