6e76b0d9bf078297a9e8f9b5671bcaaeffc29e47
UbunMen <mahtem23@gmail.com>
2025-10-18 15:23:57 +0100

    Differentiated const and let

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,7 +2,10 @@
 
 //const age = 33;
 
-// we need to replace "const" with "let"
+// we need to replace "const" with "let", in javascript "const" is usd for variable
+// that will nor change / will be kept constant troughout the code, where is "let"
+// lets us to reassign variable later as we have done in the lines below the variabel age
+// gets updated by one... 
 
 let age = 33;
 age = age + 1;
