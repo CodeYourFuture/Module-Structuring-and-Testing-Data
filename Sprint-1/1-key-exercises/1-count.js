5dab8a68501256cadea71af1516e018933b0d5af
UbunMen <mahtem23@gmail.com>
2025-10-18 15:15:31 +0100

    Used template string to display initials

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -40,3 +40,16 @@ function drawStairs(n) {
 }
 
 console.log(drawStairs(10));
+
+/// Another example
+
+function drawStairs(n) {
+  let result = [];
+ 
+  for (let i = 0; i < n; i++) {
+    result[i] = `${' '.repeat(i)}I`;
+  }
+ 
+  return result.join('\n');
+}
+console.log(drawStairs(5));
