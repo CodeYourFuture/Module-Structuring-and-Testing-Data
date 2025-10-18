8fb5a3929ed24259a5dcb529f61a6820d1ee8045
Shayida <shayida914@gmail.com>
2025-10-18 15:16:21 +0100

    answered all the question and fixed the code.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -19,16 +19,73 @@ function formatTimeDisplay(seconds) {
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
 
-// Call formatTimeDisplay with an input of 61, now answer the following:
+Answer:  3
 
+return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
+
+pad is called three times: for totalHours, remainingMinutes, and remainingSeconds.
+
+So no matter what input we give to formatTimeDisplay, pad will always be called 3 times.
+
+// Call formatTimeDisplay with an input of 61, now answer the following:
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+
+Answer;
+
+The first call is pad(totalHours).
+
+const totalHours = (totalMinutes - remainingMinutes) / 60;
+
+The value assigned to num when pad is called for the first time is 0.
+Consequently, pad(0) will return '00', which ensures that the hour is formatted correctly in the final output string
+
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
 
+Answer; 1
+
+return num.toString().padStart(2, "0");
+
+num = 1
+
+num.toString() = "1"
+
+"1".padStart(2, "0") = "01"
+
+If you call pad(0), the return value will be '00'.
+
+If you call pad(1), the return value will be '01'.
+
+If you call pad(5), the return value will be '05'.
+
+If you call pad(10), the return value will be '10'
+
+
+
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+Answer; 1
+
+The last call is pad(remainingSeconds).
+
+When you call format_time_display(61):
+
+The calculations result in:
+
+total_hours = 0
+remaining_minutes = 1
+remaining_seconds = 1
+
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+Answwer; 01
+
+
+Third Call: pad(remaining_seconds) (where remaining_seconds = 1)
+
+Value of num for the last call: 1
+
+Return value: '01' 
--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -3,22 +3,36 @@
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
 function formatAs12HourClock(time) {
-  const hours = Number(time.slice(0, 2));
-  if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+  // Expect time in "HH:MM" format
+  let hours = Number(time.slice(0, 2));
+  let mins = time.slice(3);
+
+  if (hours === 0) {
+    return `12:${mins} am`;
   }
+
+  if (hours === 12) {
+    return `12:${mins} pm`;
+  }
+
+  if (hours > 12) {
+    let newHr = hours - 12;
+    // pad single digits like 1:00 -> 01:00
+    return `${String(newHr).padStart(2, "0")}:${mins} pm`;
+  }
+
   return `${time} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+let currentOutput = formatAs12HourClock("08:00");
+let targetOutput = "08:00 am";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+let currentOutput2 = formatAs12HourClock("23:00");
+let targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
