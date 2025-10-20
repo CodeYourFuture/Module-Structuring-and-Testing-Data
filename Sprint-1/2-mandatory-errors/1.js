c77d6f36d4d20b026658d5cf058e0647b0d0131a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-20 20:22:14 +0100

    fix: update comments to clarify the use of const and suggest using let for reassignment

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -4,3 +4,4 @@ const age = 33;
 age = age + 1;
 //the "age" variable has been declared with a const and already assigned a value and it can not 
 // be reassigned another value because of the "const"
+// "age" should be declared with "let"  to be able to change it's value
