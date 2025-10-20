6b61b4cefadd66739ac9e66ca37fdf5cc0271025
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-20 20:17:16 +0100

    fix: correct the slice start index for the dir part of the filePath

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,9 +17,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(1,lastSlashIndex);
+const dir = filePath.slice(0,lastSlashIndex);
 
 const extPart=base.lastIndexOf(".")
 const ext = base.slice(extPart);
 
 // https://www.google.com/search?q=slice+mdn
+
