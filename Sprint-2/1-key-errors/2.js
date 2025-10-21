bfedae08ae031d585fea2481de779093a2d2a2c0
Abrsh100 <abrdbr2008@gmail.com>
2025-10-21 08:20:12 +0100

    identifying error in file 1 and 2 and correct it

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,26 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+/* There is a problem in the function parameter. we can not use number in parameter.
+even though line 12 by it self has no coding problem but the num is not declared before.*/
 
+/*
 function square(3) {
     return num * num;
 }
+*/
 
 // =============> write the error message here
-
+/* unexpected number
 // =============> explain this error message here
+we can not use number in a parameter so 3 is not used as identifier  */
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
+console.log(square("3"))
 
 
--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,21 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+/*the function doesn't return any thing so there will be an error. */
 
-function multiply(a, b) {
+/*function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);*/
 
 // =============> write your explanation here
+/* As predicted the function does not return any thing. But the first call gives 320. and the second undefine. */
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
