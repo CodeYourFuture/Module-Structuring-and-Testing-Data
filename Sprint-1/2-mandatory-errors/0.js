d5dfe95fcfe298a4aa3a83bf4b86aafeacba401e
Zadri <zadriabdule@gmail.com>
2025-10-21 15:56:08 +0100

    Comment out instruction lines so they are not executed

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,3 @@
 This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+We don't want the computer to run these 2 lines - how can we solve this problem? 
+// Using // to make them comments should resolve the problem. The // tells JavaScript to skip that line completely
