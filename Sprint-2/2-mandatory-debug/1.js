95b56ec7ae54c3d3c6e95f7c6b9ad6d13f739538
Georgina Rogers <zilinskyterogers@gmail.com>
2025-10-21 15:36:05 +0100

    function return corrected

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  a + b should be written on the same line as return statement
+// This function will not return anything because of the semicolon, so when the function is call on line 10 it will be undefined.
 
 function sum(a, b) {
   return;
@@ -8,6 +9,11 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// Terminal output "The sum of 10 and 32 is undefined"
+// Console.log is calling a function that was not defined because the function body has an incorrect return statement.
+
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+function sum(a, b) {
+return a + b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
