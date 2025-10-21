74687a6020be02645528c2d88d7a50c8c7cd9a90
Payman IB <payman.ib.com>
2025-10-21 14:02:11 +0100

    1-percentage-change completed => questions are answered.

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -11,12 +11,33 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // Read the code and then answer the questions below
 
-// a) How many function calls are there in this file? Write down all the lines where a function call is made
+// a) How many function calls are there in this file? Write down all the lines where a function call is made 
+
+// lines 4,5,7,8
+// the following 4 lines are functions as they are manipulating the data.
+//carPrice = Number(carPrice.replaceAll(",", ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ,""));
+//const priceDifference = carPrice - priceAfterOneYear;
+//const percentageChange = (priceDifference / carPrice) * 100;
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
 
+// line 5
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," "")); the error is a missing "," in ...replaceAll("," , "")). with out a "," the " "" are not separated and computer read them together.
+
 // c) Identify all the lines that are variable reassignment statements
+
+// lines 4,5
+//carPrice = Number(carPrice.replaceAll(",", ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+
+//carPrice = Number(carPrice.replaceAll(",", ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+
 
 // d) Identify all the lines that are variable declarations
 
+// lines 1,2,7,8
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// it replace the "," with "" which means omitting the "," so we have whole number that computer can work with it.
