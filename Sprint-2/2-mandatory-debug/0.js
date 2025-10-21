516822ded1ec7796d4999e30dd9467588b349077
Georgina Rogers <zilinskyterogers@gmail.com>
2025-10-21 15:28:08 +0100

    function was not returned to global scope

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// console.log is being used inside the multiply function, which does not return any value.
+// This will result in undefined being printed in the template literal.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +9,15 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// Output in the terminal was: "320
+// The result of multiplying 10 and 32 is undefined"
+// Console.log within the function prints the product of the calculation, but the function does not return the value to the global scope.
+// Therefore within the template literal, the meaning of the multiply function call is undefined
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
