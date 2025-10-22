abcb28628a6e857521f7c5e28fb1e31d1a53d3a0
Imran Mohamed <info@imranbmohamed.com>
2025-10-22 23:35:02 +0100

    implemented getOrdinalNumber function and associated tests

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,25 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  switch (num % 100) {
+    case 11:
+    case 12:
+    case 13:
+      return num + "th";
+      break;
+  }
+  switch (num % 10) {
+    case 1:
+      return num + "st";
+      break;
+    case 2:
+      return num + "nd";
+      break;
+    case 3:
+      return num + "rd";
+      break;
+    default:
+      return num + "th";
+      break;
+  }
 }
 
 module.exports = getOrdinalNumber;
