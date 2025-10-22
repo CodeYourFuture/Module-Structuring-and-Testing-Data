0e18f0651f65e59fd23bc868ae154c6a0266f609
Imran Mohamed <info@imranbmohamed.com>
2025-10-22 20:35:12 +0100

    wrote tests for proper fraction function

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,30 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
-
+test("should return true for a negative proper fraction", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false when numerator equals denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
+
+// Case 5: Zero Numerator:
+test("should return true for zero numerator", () => {
+  expect(isProperFraction(0, 5)).toEqual(true);
+});
+
+// Case 6: Zero Denominator:
+test("should return false for zero denominator", () => {
+  expect(isProperFraction(4, 0)).toEqual(false);
+});
+
+// Case 7: Negative Denominator:
+test("should return true for negative denominator", () => {
+  expect(isProperFraction(3, -5)).toEqual(true);
+});
