2a7471ca92ee5508910d82045f50def2dbc2ed6f
shaghayeghfar <146011477+shaghayeghfar@users.noreply.github.com>
2025-10-22 20:00:25 +0100

    practice tdd task is done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -2,12 +2,36 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const getCardValue = require("../implement/3-get-card-value");
 
+// Case 1: Handle Ace
 test("should return 11 for Ace of Spades", () => {
   const aceofSpades = getCardValue("A♠");
   expect(aceofSpades).toEqual(11);
 });
 
-// Case 2: Handle Number Cards (2-10):
-// Case 3: Handle Face Cards (J, Q, K):
-// Case 4: Handle Ace (A):
-// Case 5: Handle Invalid Cards:
+// Case 2: Handle Number Cards (2-10)
+// When the card is "2" to "10", it should return the numeric value
+test("should return the correct value for number cards", () => {
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("2♦")).toEqual(2);
+  expect(getCardValue("10♣")).toEqual(10);
+});
+
+// Case 3: Handle Face Cards (J, Q, K)
+// When the card is "J", "Q", or "K", it should return 10
+test("should return 10 for face cards J, Q, K", () => {
+  expect(getCardValue("J♣")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♠")).toEqual(10);
+});
+
+// Case 4: Handle Ace (already handled in Case 1, but can check another suit)
+test("should return 11 for Ace of Hearts", () => {
+  expect(getCardValue("A♥")).toEqual(11);
+});
+
+// Case 5: Handle Invalid Cards
+// If the card rank is invalid, it should throw an error
+test("should throw error for invalid cards", () => {
+  expect(() => getCardValue("X♠")).toThrow("Invalid card rank.");
+  expect(() => getCardValue("11♣")).toThrow("Invalid card rank.");
+});
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -1,3 +1,4 @@
+// get-ordinal-number.test.js
 const getOrdinalNumber = require("./get-ordinal-number");
 // In this week's prep, we started implementing getOrdinalNumber
 
@@ -7,7 +8,27 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Case 1: Identify the ordinal number for 1
 // When the number is 1,
 // Then the function should return "1st"
-
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+});
+
+// Case 2: Identify the ordinal number for 2
+// When the number is 2,
+// Then the function should return "2nd"
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+// Case 3: Identify the ordinal number for 3
+// When the number is 3,
+// Then the function should return "3rd"
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+
+// Case 4: Identify the ordinal number for 4
+// When the number is 4,
+// Then the function should return "4th"
+test("should return '4th' for 4", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
 });
