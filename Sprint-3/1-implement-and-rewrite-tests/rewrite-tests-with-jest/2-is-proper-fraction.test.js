fec9455a159f93d69d51b4800b25b7ce019db426
iswat <ibeetek@gmail.com>
2025-10-22 15:00:13 +0100

    Write Jest tests to verify function behavior

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -2,12 +2,21 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
-test("should return true for a proper fraction", () => {
+test("should return true for a proper fractions", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for improper fractions", () => {
+  expect(isProperFraction(7, 3)).toEqual(false);
+})
 
 // Case 3: Identify Negative Fractions:
+test("should return false for negative fractions", () => {
+  expect(isProperFraction(-4, 9)).toEqual(true);
+})
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+})
