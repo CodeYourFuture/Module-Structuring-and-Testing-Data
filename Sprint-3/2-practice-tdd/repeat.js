93d9cb6604c2689694ea400e84f589d06373822c
iswat <ibeetek@gmail.com>
2025-10-23 22:47:05 +0100

    Add implementation for the function repeat

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,16 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(valueToRepeat, numOfTimes) {
+  let repeatedValue = "";
+  if (numOfTimes > 0) {
+    for (let i = 0; i < numOfTimes; i++) {
+      repeatedValue += valueToRepeat;
+    }
+  } else if (numOfTimes === 0) {
+    repeatedValue = "";
+  } 
+  else {
+    repeatedValue = "Negative number invalid";
+  }
+  return repeatedValue;
 }
 
 module.exports = repeat;
