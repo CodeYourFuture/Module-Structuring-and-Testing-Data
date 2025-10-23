1f72b490505ec927abc3a04ac5616d488d838b01
iswat <ibeetek@gmail.com>
2025-10-23 22:04:39 +0100

    Add the implementation for the function getOrdinalNumber

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,12 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (num === 1 || num === 21) {
+    return `${num}st`;
+  } else if (num === 11) {
+    return `${num}th`;
+  } else if (num === 2) {
+    return `${num}nd`;
+  }
 }
+
 
 module.exports = getOrdinalNumber;
