1da375f69baa6907102cd7a783163304bde5f399
HANISADAH <hanisadah81@gmail.com>
2025-10-23 23:24:47 +0100

    Answering th task 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// in this case, we will need to replace the const keyword with let keyword as const vasriable refuses to be reassigned.
+let age = 33;
 age = age + 1;
+
+console.log(age);
