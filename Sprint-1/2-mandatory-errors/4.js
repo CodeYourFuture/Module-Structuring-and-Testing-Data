f4e13bc29885fce250784f8e9bdb996c5f97486a
HANISADAH <hanisadah81@gmail.com>
2025-10-24 00:47:42 +0100

    Solving the code issue

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,16 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+// we have to convert the identifiers of the variables to valid ones by removing the 12 and 24 digits and to put them in camel case format.
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
+console.log(twelveHourClockTime);
+console.log(twentyFourHourClockTime);
+
+// The twelveHourClockTime and twentyFourHourClockTime variables should store the respective time formats
+// However, the code isn't working
+// Before running the code, make and explain a prediction about why the code won't work
+// Then run the code and see what error it gives.
+// Consider: Why does it give this error? Is this what I predicted? If not, what's different?
+// Then try updating the variable names to valid identifiers in order to get the correct value
