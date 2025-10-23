4102ecbf9322cb3d71a8c7e528ea3550858c45e6
HANISADAH <hanisadah81@gmail.com>
2025-10-23 23:07:17 +0100

    Updating the answer

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -5,17 +5,20 @@
 // "  /    home/user/dir / file  .txt "
 // └──────┴──────────────┴──────┴─────┘
 
+
+
+// all spaces in the "" line should be ignored. THey are purely for formatting.
+
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
-
-// Find the index of the last slash and the last dot
 const lastSlashIndex = filePath.lastIndexOf("/");
-const lastDotIndex = filePath.lastIndexOf(".");
+const base = filePath.slice(lastSlashIndex + 1);
+console.log (the base part of ${filePath} is ${base});
 
-// Extract each part
-const base = filePath.slice(lastSlashIndex + 1); // "file.txt"
-const dir = filePath.slice(0, lastSlashIndex);   // "/Users/mitch/cyf/Module-JS1/week-1/interpret"
-const ext = filePath.slice(lastDotIndex + 1);    // "txt"
+// Create a variable to store the dir part of the filePath variable.
+// Create a variable to store the ext part of the variable.
 
-console.log(`The dir part of ${filePath} is ${dir}`);
-console.log(`The base part of ${filePath} is ${base}`);
-console.log(`The ext part of ${filePath} is ${ext}`);
+const dir = filePath.slice(lastSlashIndex+1)
+const ext = filePath.slice(lastDotIndexOf(".") +1);   
+
+console.log (dir)
+// https://www.google.com/search?q=slice+mdn
