e7f48d3b6e37e6148c15558ba859867450d34e68
HANISADAH <hanisadah81@gmail.com>
2025-10-23 23:31:49 +0100

    Answering 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// we need to declare the variable cityOfBirth first then we can print it out.
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
