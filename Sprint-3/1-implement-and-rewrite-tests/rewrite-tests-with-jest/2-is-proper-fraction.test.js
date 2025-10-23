a3c8fc886400003969d6a928f566bed1624d38b7
Alaa-Tagi <alaa3litagi1994@gmail.com>
2025-10-23 20:53:00 +0100

    editing the code

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -5,7 +5,6 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
   
-
 });
 
 // Case 2: Identify Improper Fractions:
@@ -21,4 +20,7 @@ test("should return false for a negative fraction", () => {
 // Case 4: Identify Equal Numerator and Denominator:
 test("should return false for equal numerator and denominator", () => {
   expect(isProperFraction(3, 3)).toEqual(false);
+  expect(isProperFraction(-3, 3)).toEqual(false);
+  expect(isProperFraction(3, -3)).toEqual(false);
+  expect(isProperFraction(-3, -3)).toEqual(false);
 });
