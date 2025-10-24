8da53ea529c083f291d53e7095628f458c7d3884
UbunMen <mahtem23@gmail.com>
2025-10-24 23:49:23 +0100

    In-5-Strech-extended/format-time.js, tested different input groups and modified the code.

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -1,6 +1,7 @@
 // This is the latest solution to the problem from the prep.
 // Make sure to do the prep before you do the coursework
-// Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
+// Your task is to write tests for as many different groups of input data or edge cases as you can, 
+// and fix any bugs you find.
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
@@ -23,3 +24,63 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+const currentOutput4 = formatAs12HourClock("12:00");
+const targetOutput4 = "12:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("13:00");
+const targetOutput5 = "01:00 pm";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+
+);
+
+const currentOutput7 = formatAs12HourClock("25:00");
+const targetOutput7 = "01:00 am";
+console.assert(
+  currentOutput7 === targetOutput7,
+  `current output: ${currentOutput7}, target output: ${targetOutput7}`
+  
+)
+
+// console.assert(formatAs12HourClock("08:00") === "08:00 am"); 
+// console.assert(formatAs12HourClock("23:00") === "11:00 pm");
+// console.assert(formatAs12HourClock("22:00") === "10:00 pm");
+
+//  Modified Code:
+
+function formatAs12HourClock(time) {
+  let hours = Number(time.slice(0, 2));
+  const minutes = time.slice(3);
+
+  let suffix;
+  if (hours >= 12) {
+    suffix = "pm";
+  } else {
+    suffix = "am";
+  }
+
+  hours = hours % 12 || 12; // convert 0 to 12, 13 to 1
+  const formattedHours = hours.toString().padStart(2, "0");
+
+  return `${formattedHours}:${minutes} ${suffix}`;
+
+}
+
+const currentOutput6 = formatAs12HourClock("23:00");
+const targetOutput6 = "11:00 pm";
+console.assert(
+  currentOutput6 === targetOutput6,
+  `current output: ${currentOutput6}, target output: ${targetOutput6}`
+)
