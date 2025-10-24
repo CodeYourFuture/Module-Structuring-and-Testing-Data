a135b88401739c879e8e05520117b962b0e3fc47
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-24 18:50:48 +0000

    Implement countChar function and tests

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,11 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str,count) {
+  if (count=== 0){
+    return ""
+  }
+  if(count<0){
+    return "invalid"
+  }
+  return str.repeat(count);
 }
 
 module.exports = repeat;
