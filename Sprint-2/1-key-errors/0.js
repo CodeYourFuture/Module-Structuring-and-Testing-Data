4e992fc8a1f09f23ac0484ca2f6bc6345f50a475
UbunMen <mahtem23@gmail.com>
2025-10-24 15:55:47 +0100

    In 1-Key-errors/0.js Syntax Error corrected and code run correctly

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I guess we would'nt need to say let in line 8 because str is already decleared as an argument 
+// within the function capitalise(str).
+// 
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+//function capitalise(str) {
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return str;
+//}
+
+//capitalise("hello")
+// =============> A syntaxError occured as 'str' is already declared. 
+// =============> write your new code here
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise("hello"));
