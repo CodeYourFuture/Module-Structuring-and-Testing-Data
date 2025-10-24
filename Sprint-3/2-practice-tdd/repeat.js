8ff02d3704cef282e9f8370d1d328e8e6628735a
Alaa-Tagi <alaa3litagi1994@gmail.com>
2025-10-24 19:39:31 +0100

    Correct the code

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,17 +1,17 @@
-function repeat() {
-   const str = arguments[0];
-  const count = arguments[1];
+function repeat(str, count) {
   if (count < 0) {
     throw new Error("Count must be a non-negative integer");
   }
+
   let result = "";
   for (let i = 0; i < count; i++) {
     result += str;
   }
+
   return result;
 }
 
-
 module.exports = repeat;
-console.log(repeat("hello", 3));
+
+console.log(repeat("hello", 3)); 
 
