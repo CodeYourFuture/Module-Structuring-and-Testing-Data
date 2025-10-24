be4ee28351eee2e0bfec36f349034b71338e8a9b
UbunMen <mahtem23@gmail.com>
2025-10-24 16:25:25 +0100

    In 1-Key-errors/1.js Syntax Error identified, corrected and new code written

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,34 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+
+// =============> I think, the function convertToPercentage is not called at line 16, and 
+// there is no any importance of declaring cons decimalNumber as it is already declared.
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+//function convertToPercentage(decimalNumber) {
+//  const decimalNumber = 0.5;
+//  const percentage = `${decimalNumber * 100}%`;
+
+//  return percentage;
+//}
+
+//console.log(decimalNumber);
+
+// =============> A syntaxError appeared indicating 'decimalNumber' has already been declared. 
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+
+function convertToPercentage() {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage());
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+// The new code run smoothly and retuned "50%"
