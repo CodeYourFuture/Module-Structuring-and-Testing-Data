f3203bb8dee2aeabd5b6d48d662b53d3be80cfc4
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-24 16:58:30 +0000

    added jest test for3-get-card-value

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,24 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test ("should return a 5 for the card with rank of 5 like(5♥) ",()=>{
+  const fiveofHearts = getCardValue("5♥");
+  expect(fiveofHearts).toEqual(5);
+
+})
 // Case 3: Handle Face Cards (J, Q, K):
+test("shoild return 10 fore the faced cart like king(K♦) and queen (Q♣)")
+const kingCard =getCardValue("K♦");
+const queenCard= getCardValue("Q♣");
+expect(kingCard).toEqual(10);
+expect (queenCard).toEqual(10);
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Spades", () => {
+  const aceofSpades = getCardValue("A♣");
+  expect(aceofSpades).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test ("should return invalid card rank for invalid inputs like(z♣)",()=>{
+  const invalidCard= getCardValue("z♣");
+  expect(invalidCard).toEqual("Invalid card rank.");
+});
