4ef9f592fc0e539c64c5c27630284358fe9742ed
UbunMen <mahtem23@gmail.com>
2025-10-24 17:12:06 +0100

    In 2-Mandatory-debug/1js, the code has been fixed with slight modifcation

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,14 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The semi colon ; after retunr will results in syntax error. 
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> Unlike my expectation the code run without error as "The sum of 10 and 32 is undefined"
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+// we just need to delete the ;  after return and bring a+b side by side as retun a+b; 
