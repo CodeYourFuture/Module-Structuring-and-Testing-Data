a2ae864f00516fe354ed30684c2676d682a5c32d
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-24 11:10:26 +0100

    refactor countChar function for improved readability

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,7 +1,7 @@
 function countChar(stringOfCharacters, findCharacter) {
-  let char=0;
-  for(let i=0;i<stringOfCharacters.length;i++){
-    if(stringOfCharacters[i]===findCharacter){
+  let char = 0;
+  for(let i = 0;i < stringOfCharacters.length;i++){
+    if(stringOfCharacters[i] === findCharacter){
       char++;
     }
   }
