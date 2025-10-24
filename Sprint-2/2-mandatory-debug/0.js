ef668cc6dde032a176ba8fb41ea2fe39cdd37e91
UbunMen <mahtem23@gmail.com>
2025-10-24 16:59:37 +0100

    In 2-Mandatory-debug/0.js, predictedwhat the code would return and modified the code

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,22 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> In line 6 instead of console.log, I would expect retun a*b; the code will result in
+                // just printing a*b 
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> After running it returned 320 The result of multiplying 10 and 32 is undefined
+// Because the multiply parameters were not taken as paramaters within a retunr. 
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return  (a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
