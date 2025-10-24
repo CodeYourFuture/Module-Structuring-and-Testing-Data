45af57723898df16844af4268d93230609bff8c9
UbunMen <mahtem23@gmail.com>
2025-10-24 16:41:23 +0100

    In 1-Key-errors/2.js, predicted what the error would be, run the code, interpreted the error and wrote a new  and corrected  code.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,23 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> The num should be inside the function square (num) not 3 and square function should be
+                 // called later as square(3)
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//    return num * num;
+//}
 
-// =============> write the error message here
+// =============> function suqare(3) SyntaxError: Unexpected number
 
-// =============> explain this error message here
+// =============> 3 was not expected... instead a declaration or paramater like num was expected.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
+console.log(square(3))
 
