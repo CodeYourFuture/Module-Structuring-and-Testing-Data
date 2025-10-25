8beb07d1534eb7e3ccd276cff83ab6d1fc352a88
HANISADAH <hanisadah81@gmail.com>
2025-10-25 23:57:32 +0100

    Answering 3-to-pounds.js

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,13 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// to begin we can start with
+//1- const penceSring= "399p": initialises a string variable with the value "399p"
+//2- const penceStringWithoutTrailingP= a string value "399" is reassigned to penceStringWithoutTrailingP by removing the last character "p" from penceString using the substring method
+//3- const paddedPenceNumberString= declared new variable, padStart ensure any argument passed is at least 3 characters long by adding leading zeros if necessary
+//4- line 9 a new variable pence is declared and its value comes from the padded string by removing the last two characters which represent the pence.
+//5- line 14 const pence= declared variable and the variable is the result after extracting the last two characters from paddedPenceNumberString using substring method and padEnd method is used to ensure that the pence value is always two digits long by adding trailing zeros if necessary
+// from the pence string and pads it to two characters to have two digits number.
+//6- line 18: console.log (`£${pounds}.${pence}`): returns money amount combing pounds and pence along with the £ symbol
+//eg. £3.99
