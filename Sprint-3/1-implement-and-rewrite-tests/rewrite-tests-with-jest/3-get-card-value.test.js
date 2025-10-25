0e834a6de7329edf0df1568c0c3b44babe56c2a0
enjoy15 <narumoljoy@gmail.com>
2025-10-25 21:33:03 +0100

    Add more tests

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -33,3 +33,34 @@ test("should return undefined for invalid cards", () => {
   const invalidCard = getCardValue("1♠");
   expect(invalidCard).toEqual(undefined);
 });
+
+test("should return correct value for all number cards 2-10", () => {
+  expect(getCardValue("2♥")).toEqual(2);
+  expect(getCardValue("3♣")).toEqual(3);
+  expect(getCardValue("4♦")).toEqual(4);
+  expect(getCardValue("5♠")).toEqual(5);
+  expect(getCardValue("6♥")).toEqual(6);
+  expect(getCardValue("7♣")).toEqual(7);
+  expect(getCardValue("8♦")).toEqual(8);
+  expect(getCardValue("9♠")).toEqual(9);
+  expect(getCardValue("10♥")).toEqual(10);
+});
+
+test("should handle numeric-literal edge cases", () => {
+  expect(getCardValue("0x02♠")).toEqual(2); 
+  expect(getCardValue("2.1♠")).toBeUndefined();      
+  expect(getCardValue("0002♠")).toEqual(2);     
+});
+
+test("should return 10 for all face cards", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♣")).toEqual(10);
+});
+
+test("should return 11 for all Aces", () => {
+  expect(getCardValue("A♠")).toEqual(11);
+  expect(getCardValue("A♥")).toEqual(11);
+  expect(getCardValue("A♦")).toEqual(11);
+  expect(getCardValue("A♣")).toEqual(11);
+});
