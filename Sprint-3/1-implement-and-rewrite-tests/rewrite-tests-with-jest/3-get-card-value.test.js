958730d0419d99b6e35028e4224092d77611ee00
Abrsh100 <abrdbr2008@gmail.com>
2025-10-25 08:05:54 +0100

    testing code using jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,35 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 3 for 3 of Clubs", () => {
+  expect(getCardValue("3♣")).toEqual(3);
+});
+
+test("should return 7 for 7 of Spades", () => {
+  expect(getCardValue("7♠")).toEqual(7);
+});
+
+test("should return 9 for 9 of Diamonds", () => {
+  expect(getCardValue("9♦")).toEqual(9);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for Jack of Clubs", () => {
+  expect(getCardValue("J♦")).toEqual(10);
+});
+
+test("should return 10 for Queen of Spades", () => {
+  expect(getCardValue("Q♣")).toEqual(10);
+});
+
+test("should return 10 for King of Hearts", () => {
+  expect(getCardValue("K♠")).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Diamonds", () => {
+  expect(getCardValue("A♦")).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should return null for invalid card 'B'", () => {
+  expect(getCardValue("Z♠")).toBeNull();
+});
