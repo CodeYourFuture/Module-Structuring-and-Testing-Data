5e63f6863649fd5ba9d756b8a9eab2d5af39a649
Alaa-Tagi <alaa3litagi1994@gmail.com>
2025-10-26 15:20:44 +0000

    update the jest code

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,39 +8,25 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
-test("should return 7 for 7 of Hearts", () => {
-  const sevenOfHearts = getCardValue("7♥");
-  expect(sevenOfHearts).toEqual(7);
+test("should return correct value for numbered cards (2-10)", () => {
+  expect(getCardValue("5♥")).toBe(5);
+  expect(getCardValue("10♦")).toBe(10);
 });
 // Case 3: Handle Face Cards (J, Q, K):
-test("should return 10 for Jack of Diamonds", () => {
-  const jackOfDiamonds = getCardValue("J♦");
-  expect(jackOfDiamonds).toEqual(10);
+test("should return 'Invalid card rank' for invalid cards", () => {
+  ["1♣", "B♦", "Z♠"].forEach((card) => {
+    expect(getCardValue(card)).toBe("Invalid card rank");
+  });
 });
-
-//Case 4
-test("should return 10 for Queen of Clubs", () => {
-  const queenOfClubs = getCardValue("Q♣");
-  expect(queenOfClubs).toEqual(10);
-});
-
-test("should return 10 for King of Spades", () => {
-  const kingOfSpades = getCardValue("K♠");
-  expect(kingOfSpades).toEqual(10);
-});
-
 // Case 4: Handle Ace (A):
-test("should return 11 for Ace of Diamonds", () => {
-  const aceOfDiamonds = getCardValue("A♦");
-  expect(aceOfDiamonds).toEqual(11);
+test("should return 11 for Ace (A)", () => {
+  ["A♠", "A♥"].forEach((card) => {
+    expect(getCardValue(card)).toBe(11);
+  });
 });
 // Case 5: Handle Invalid Cards:
-test("should return null for invalid card '1X'", () => {
-  const invalidCard1 = getCardValue("1X");
-  expect(invalidCard1).toBe("Invalid card rank");
+test("should return 'Invalid card rank' for invalid cards", () => {
+  ["1♣", "B♦", "Z♠"].forEach((card) => {
+    expect(getCardValue(card)).toBe("Invalid card rank");
+  });
 });
-test("should return null for invalid card '11H'", () => {
-  const invalidCard2 = getCardValue("11H");
-  expect(invalidCard2).toBe("Invalid card rank");
-});
-
