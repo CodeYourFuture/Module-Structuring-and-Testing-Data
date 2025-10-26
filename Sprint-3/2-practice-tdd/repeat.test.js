a1e9f6058c6e8261c435443620219b0d877c5389
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-26 19:52:40 +0000

    add tests for repeat function to handle empty strings, strings with spaces, and long strings

--- Sprint-3/2-practice-tdd/repeat.test.js
+++ Sprint-3/2-practice-tdd/repeat.test.js
@@ -33,12 +33,12 @@ expect(repeatedStr).toEqual("hello")
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
 
-test("should return an empty string", ()=>{
-  const str = "hello";
-  const count = 0;
-  const repeatedStr = repeat(str, count);
-  expect(repeatedStr).toEqual("");
-})
+// test("should return an empty string", ()=>{
+//   const str = "hello";
+//   const count = 0;
+//   const repeatedStr = repeat(str, count);
+//   expect(repeatedStr).toEqual("");
+// })
 
 // case: Negative Count:
 // Given a target string str and a negative integer count,
@@ -51,3 +51,51 @@ test("should return error", ()=>{
   const repeatedStr = repeat(str, count);
   expect(repeatedStr).toEqual("Error: negative numbers are not valid");
 })
+
+// case: Handle empty string:
+// Given a target string str and a count equal to 2,
+// When the repeat function is called with these inputs,
+// Then it should return an empty string, ensuring that a count of 2 results in an empty output.
+
+test("should return an empty string", () => {
+  const str = "";
+  const count = 2;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("");
+});
+
+// case: Handle string with space:
+// Given a target string str and a count equal to 2,
+// When the repeat function is called with these inputs,
+// Then it should repeat the str count times and return a new string containing the repeated str values.
+
+test("should return an empty string", () => {
+  const str = "a b";
+  const count = 2;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("a ba b");
+});
+
+// case: Handle long string :
+// Given a target string str and a count equal to1,
+// When the repeat function is called with these inputs,
+// Then it should repeat the str count times and return a new string containing the repeated str values.
+
+test("should return an empty string", () => {
+  const str = "xvg56756yrhfghe5ujdfh45657tjrtg6yrthrty";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("xvg56756yrhfghe5ujdfh45657tjrtg6yrthrty");
+});
+
+// case: Handle single character string and high count :
+// Given a target string str and a count equal to 18,
+// When the repeat function is called with these inputs,
+// Then it should repeat the str count times and return a new string containing the repeated str values.
+
+test("should return an empty string", () => {
+  const str = "x";
+  const count = 18;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("xxxxxxxxxxxxxxxxxx");
+});
