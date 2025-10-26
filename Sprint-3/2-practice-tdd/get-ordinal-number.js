416689c0d99f655635e51084d3574502b3f79dcd
shaghayeghfar <146011477+shaghayeghfar@users.noreply.github.com>
2025-10-26 20:36:46 +0000

    Delete Sprint-3/2-practice-tdd/get-ordinal-number.js

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++
@@ -1,8 +0,0 @@
-function getOrdinalNumber(number) {
-  if (number === 1) return "1st";
-  if (number === 2) return "2nd";
-  if (number === 3) return "3rd";
-  return number + "th";
-}
-
-module.exports = getOrdinalNumber;
