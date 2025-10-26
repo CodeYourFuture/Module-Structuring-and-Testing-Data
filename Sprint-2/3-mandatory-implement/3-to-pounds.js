cf133333c8d658afa2d8b9e9bc6a93a18b13ee02
saff-coder <sophi.m.80@gmail.com>
2025-10-26 16:58:33 +0000

    changed the function now the output is a number

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -5,12 +5,17 @@
 
 // You should call this function a number of times to check it works for different inputs
 
+// I thought adding "lbs" need to be inside a string .
+// i converted the  string back to number
+
 function toPounds(kg) {
   const pounds = kg * 2.20462;
-  return `${pounds.toFixed(2)} lbs`; // returns a string
+  return Number(pounds.toFixed(2)); 
 }
+console.log(`${toPounds(5)} lbs`);  
 
-// Test cases 
-console.log(toPounds(1));   //the output "2.20 lbs"
-console.log(toPounds(5));   //the output is  "11.02 lbs"
-console.log(toPounds(10)); //the output is  "22.05 lbs"
+
+// more Test cases 
+console.log(`${toPounds(1)} lbs`); // 2.20 lbs 
+console.log(`${toPounds(5)} lbs`);  // 11.02 lbs
+console.log(`${toPounds(10)} lbs`);  // 22.05 lbs
