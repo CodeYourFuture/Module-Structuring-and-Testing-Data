6c0be541b462f8cc9e610e65329dc106742e8e5b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-26 20:01:39 +0000

    updated the function to handle empty strings

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,8 @@
 function countChar(stringOfCharacters, findCharacter) {
   let char = 0;
+  if (stringOfCharacters === 0){
+    return 0
+  }
   for(let i = 0;i <= stringOfCharacters.length - findCharacter.length;i++){ 
     if ( findCharacter.length === 0 || stringOfCharacters === 0){
       return 0;
