99f9720c29ede190336f1e4e901bb03d35ae70b2
Abrsh100 <abrdbr2008@gmail.com>
2025-10-27 07:32:12 +0000

    restoring sprint 1 folder from cyf main

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -13,37 +13,10 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
-// There are five functions calls.
-//carPrice.replaceAll(",", "")
-//Number(carPrice.replaceAll(",", ""))
-//priceAfterOneYear.replaceAll("," "")
-//Number(priceAfterOneYear.replaceAll("," ""))
-//console.log(`The percentage change is ${percentageChange}`)
-
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-
-// When I run the code the error is "syntaxError: missing ) after argument list.
-// The error comes from line 5
-// The error occurs due to the missing coma between the two strings in the replaceAll() call.
-// Add coma between the two strings "priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));"
 
 // c) Identify all the lines that are variable reassignment statements
 
-// there are to lines that state variable reassignment
-// carPrice = Number(carPrice.replaceAll(",", ""));
-// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
-
 // d) Identify all the lines that are variable declarations
 
-// There are fore variable declarations (line 1, line 2, line 7, line 8)
-// let carPrice = "10,000";
-// let priceAfterOneYear = "8,543";
-// const priceDifference = carPrice - priceAfterOneYear;
-// const percentageChange = (priceDifference / carPrice) * 100;
-
-
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
-
-// carprice at line 1 is a string in order to calculate for the next line of codes you have to change the string ti number.
-// carPrice.replaceAll(",","")). delete the coma in between the number (e.g from "10,000" to "10000")
-// number() change the string to number ("10000" to 10000)
--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,21 +12,14 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
-// There are 6 variable declaration. "line1, line3, line4, line6, line7, line9"
 
 // b) How many function calls are there?
-// There is only one function call in this program "console.log(result);"
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
-// % is called remainder. There fore movieLength % 60 represents the remainder of the movie length after dividing by 60.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-// totalMinutes are calculated by subtracting the remaining seconds from movie length and divided by 60 to get full minutes. this helps to get a number which divided by 60 with out reminder.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-// result represents the total length of the movie in the form of Hour: minute: second
-// moveDurationFormatted can be a better name to replace result. 
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-// Yes it works for all value. the only thing that is concerning is the validation of movieLength as the negative integer also gives value which is unrealistic. 
