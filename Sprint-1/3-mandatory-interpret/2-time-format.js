7bc75f1e84cde8249ebb0f78911771758e2b52a7
khalidbih <kain19m@hotmail.com>
2025-10-27 22:00:44 +0000

    I did the experiment for Question (f),for Question (E) I didn't give a name yet for the variable

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -13,13 +13,25 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
+// there is 7 variable declarations.
+
 // b) How many function calls are there?
+
+// there is 4  function calls.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+// it gives the number of seconds left after counting all full minutes.
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+
+// is the total number of complete minutes in the movie.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
+// represents the hours,minutes,seconds of the movie/no
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+// The code won’t work for all values of movieLength. If it’s negative, the result doesn’t make sense because time cannot be negative.
