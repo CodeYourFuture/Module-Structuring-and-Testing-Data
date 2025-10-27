83c98dcaa5107e235dad3b25aedfcf2a75af9a22
asaniDev <asanidev9@gmail.com>
2025-10-27 02:00:00 +0000

    added tests for the given scenarios and created a repeat function that takes in a string and count to create a new string

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,18 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str, count) {
+  let newStr = "";
+
+  if (count === 0) {
+    return newStr;
+  } else if (count === 1) {
+    return (newStr += str);
+  } else if (count > 1) {
+    for (let i = 0; i < count; i++) {
+      newStr += str;
+    }
+    return newStr;
+  } else {
+    return "Error invalid count used, please use integers from 0 upwards.";
+  }
 }
 
 module.exports = repeat;
