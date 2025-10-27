1c66099595196c801e244fdaffd33802942afc85
iswat <ibeetek@gmail.com>
2025-10-27 22:43:11 +0000

    Update countChar implementation to correctly count character occurrences

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,17 @@
 function countChar(stringOfCharacters, findCharacter) {
-  const characterOccurrence = stringOfCharacters.split(findCharacter).length - 1;
+  let characterOccurrence = "";
+  if (typeof stringOfCharacters === "string" && typeof findCharacter === "string") {
+    stringOfCharacters = stringOfCharacters.toLowerCase();
+    findCharacter = findCharacter.toLowerCase();
+    if (findCharacter.length === 1) {
+      characterOccurrence = stringOfCharacters.split(findCharacter).length - 1;
+    } else {
+      characterOccurrence = "invalid input: Input just one character";
+    }
+  } else {
+    characterOccurrence = "Invalid input: input should be a string";
+  }
+
   return characterOccurrence;
 }
 
