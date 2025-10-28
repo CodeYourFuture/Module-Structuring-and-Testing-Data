3289b6057dc3a3f6b128b1229a8f7271af778efa
shaghayeghfar <146011477+shaghayeghfar@users.noreply.github.com>
2025-10-28 13:40:41 +0000

    task 2 of spring 3 clean

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,8 @@
-function getOrdinalNumber(num) {
-  return "1st";
+function getOrdinalNumber(number) {
+  if (number === 1) return "1st";
+  if (number === 2) return "2nd";
+  if (number === 3) return "3rd";
+  return number + "th";
 }
 
 module.exports = getOrdinalNumber;
