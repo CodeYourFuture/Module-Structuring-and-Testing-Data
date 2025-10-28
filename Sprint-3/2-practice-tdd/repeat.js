4548cb54450efb445ebe7ba89cb1382220ad003c
iswat <ibeetek@gmail.com>
2025-10-28 12:17:02 +0000

    Update repeat function to handle positive, zero, negative, non-integer counts and repeat strings, numbers, booleans, null, undefined, arrays, and objects

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,14 +1,15 @@
 function repeat(valueToRepeat, numOfTimes) {
   let repeatedValue = "";
-  if (numOfTimes > 0) {
+  if (numOfTimes > 0 && Number.isInteger(numOfTimes)) {
     for (let i = 0; i < numOfTimes; i++) {
       repeatedValue += valueToRepeat;
     }
   } else if (numOfTimes === 0) {
     repeatedValue = "";
-  } 
-  else {
+  } else if(numOfTimes < 0) {
     repeatedValue = "Negative number invalid";
+  } else if(!Number.isInteger(numOfTimes)) {
+    repeatedValue = "Invalid count: count should be an integer"
   }
   return repeatedValue;
 }
