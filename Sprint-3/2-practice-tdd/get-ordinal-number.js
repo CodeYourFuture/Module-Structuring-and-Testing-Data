a6bbe867c4eba127c412b83b6dd5e4ddc9fef938
iswat <ibeetek@gmail.com>
2025-10-28 10:48:00 +0000

    Fix getOrdinalNumber to handle integers correctly and reject invalid inputs

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,12 +1,31 @@
 function getOrdinalNumber(num) {
-  if (num === 1 || num === 21) {
-    return `${num}st`;
-  } else if (num === 11) {
-    return `${num}th`;
-  } else if (num === 2) {
-    return `${num}nd`;
+  let ordinalNum;
+  if (Number.isInteger(num) === true) {
+    if (
+      Math.abs(num % 100) === 11 ||
+      Math.abs(num % 100) === 12 ||
+      Math.abs(num % 100) === 13 ||
+      Math.abs(num % 10) === 4 ||
+      Math.abs(num % 10) === 5 ||
+      Math.abs(num % 10) === 6 ||
+      Math.abs(num % 10) === 7 ||
+      Math.abs(num % 10) === 8 ||
+      Math.abs(num % 10) === 9 ||
+      Math.abs(num % 10) === 0
+    ) {
+      ordinalNum = `${num}th`;
+    } else if (Math.abs(num % 10) === 1) {
+      ordinalNum = `${num}st`;
+    } else if (Math.abs(num % 10) === 2) {
+      ordinalNum = `${num}nd`;
+    } else if (Math.abs(num % 10) === 3) {
+      ordinalNum = `${num}rd`;
+    } else {
+      ordinalNum = `${num}th`;
+    }
+  } else {
+    ordinalNum = "Invalid input: Input is an integer";
   }
+  return ordinalNum;
 }
-
-
 module.exports = getOrdinalNumber;
