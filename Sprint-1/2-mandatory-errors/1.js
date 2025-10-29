78c98898a3228ffb88c93ea8a533b96b36178523
Samuel Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-10-29 07:10:20 +0000

    fix(sprint-1): resolve TypeError by using let instead of const

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,11 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// const age = 33;
+// age = age + 1;
+
+// TypeError: Assignment to constant variable - `const` cannot be reassigned
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+// Solution:
+let age = 33;
 age = age + 1;
