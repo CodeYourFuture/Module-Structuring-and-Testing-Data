f3dab49214b3266d8209cc8db3fb0b3703f6a799
Samuel Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-10-29 07:14:51 +0000

    fix(sprint-2): resolve square SyntaxError by using valid parameter identifier

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,23 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// SyntaxError: Function parameters must be valid identifiers, not number literals
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// Parameters must be valid identifiers (start with letter/underscore/$), not numbers
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Unexpected_token
 
 // Finally, correct the code to fix the problem
-
 // =============> write your new code here
-
-
+function square(num) {
+  return num * num;
+}
