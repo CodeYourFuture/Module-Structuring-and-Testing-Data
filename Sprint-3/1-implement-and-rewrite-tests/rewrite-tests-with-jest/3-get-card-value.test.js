df7ccfba145e902b65b3316ebd71ebfe4bdfc441
iswat <ibeetek@gmail.com>
2025-10-29 14:44:23 +0000

    Add more Jest test cases for getCardValue to include 2 and 10

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -9,19 +9,23 @@ test("should return 11 for Ace of Spades", () => {
 
 // Case 2: Handle Number Cards (2-10):
 test("should return the numeric value for number cards from 2 to 10", () => {
-  const numberCard = getCardValue("7♥");
-  expect(numberCard).toEqual(7);
+  expect(getCardValue("7♥")).toEqual(7);
+  expect(getCardValue("2♥")).toEqual(2);
+  expect(getCardValue("10♥")).toEqual(10);
 })
+
 // Case 3: Handle Face Cards (J, Q, K):
 test("should return 10 for face cards", () => {
   const faceCard = getCardValue("J♥");
   expect(faceCard).toEqual(10);
 })
+
 // Case 4: Handle Ace (A):
 test("should return 11 for ace card", () => {
   const aceCard = getCardValue("A♥");
   expect (aceCard).toEqual(11);
 })
+
 // Case 5: Handle Invalid Cards:
 test("should return invalid card rank for random numbers", () => {
   const invalidRandomCard = getCardValue("85♠");
