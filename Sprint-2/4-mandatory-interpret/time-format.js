e1924c5ff8b7e2438575bf847117816615ed5160
Samuel Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-10-29 07:15:25 +0000

    docs(sprint-2): add concise answers for time formatting interpretation

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,22 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// 3 times: pad(totalHours), pad(remainingMinutes), pad(remainingSeconds)
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// 0 (totalHours for 61 seconds = 0 hours)
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// "00" (0.toString().padStart(2, "0"))
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// 1 (remainingSeconds for 61 seconds: 61 % 60 = 1)
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// "01" (1.toString().padStart(2, "0"))
