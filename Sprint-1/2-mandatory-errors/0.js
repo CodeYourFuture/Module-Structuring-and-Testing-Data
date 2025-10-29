d7626de937054b85ea99f6c429c155c421a78fe9
Samuel Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-10-29 07:10:20 +0000

    fix(sprint-1): resolve ReferenceError by declaring greeting outside block scope

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// Answer: Use comments (//) to prevent execution
+// SyntaxError: Unexpected identifier - plain text is not valid JavaScript
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#comments
