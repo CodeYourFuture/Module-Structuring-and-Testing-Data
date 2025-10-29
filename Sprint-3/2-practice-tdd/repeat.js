2beffba975d0afaa1fa22cf73fbfaffb4474acf8
Imran Mohamed <info@imranbmohamed.com>
2025-10-29 09:04:27 +0000

    Added edge case and invalid input tests and updated function to pass

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,6 +1,12 @@
 function repeat(str, count) {
-  if (count < 0) {
-    return "Count must be a non-negative integer"
+  if ( arguments.length !== 2) {
+    return "Function requires exactly 2 arguments";
+  }
+  if (typeof str !== "string") {
+    return "First argument must be a string";
+  }
+  if (!Number.isInteger(count) || count < 0) {
+    return "Second argument must be a non-negative integer";
   }
   return str.repeat(count);
 }
