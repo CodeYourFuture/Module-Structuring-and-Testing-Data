4d64b5e50e9490bac8c39b1b329a58a286eb6b65
Samuel Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-10-29 07:10:20 +0000

    fix(sprint-1): resolve SyntaxError by using unique variable names

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,12 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+// What's the error?
 
-console.log(`I was born in ${cityOfBirth}`);
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
+// ReferenceError: Cannot access 'cityOfBirth' before initialization - temporal dead zone
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const#temporal_dead_zone_tdz
+
+// Solution:
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
