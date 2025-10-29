f8a651069829bc354815cf90d9e245ee1285abe0
Payman IB <payman.ib.com>
2025-10-29 16:10:04 +0000

    Lines 26 & 43 updated.

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -23,7 +23,8 @@ console.log(`The percentage change is ${percentageChange}`);
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
 
 // line 5
-// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," "")); the error is a missing "," in ...replaceAll("," , "")). with out a "," the " "" are not separated and computer read them together.
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," "")); the error is a missing comma between the two arguments in the .replaceAll() function call. Without the comma,
+// the two string arguments "," and "" are not separated, so the computer reads them together as one argument.
 
 // c) Identify all the lines that are variable reassignment statements
 
@@ -40,4 +41,4 @@ console.log(`The percentage change is ${percentageChange}`);
 // lines 1,2,7,8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
-// it replace the "," with "" which means omitting the "," so we have whole number that computer can work with it.
+// It replace the "," with "" which means omitting the "," and add nothing so we have whole number form that computer can work with it.
