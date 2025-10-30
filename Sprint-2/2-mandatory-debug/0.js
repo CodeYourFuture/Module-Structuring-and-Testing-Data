ca2b7130b11a79d2bc685b34fbdb59203ef7be27
Fithi Teklom <fthiteklom@gmail.com>
2025-10-30 15:00:30 +0000

    0.js updated template

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// I predict there will be error because instead of console.log we should have used return.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +10,12 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// The same as I predicted.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return(a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
