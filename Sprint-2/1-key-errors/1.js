9bcb86b735e4879b755ac2548ee12631de0e2574
khalidbih <kain19m@hotmail.com>
2025-10-30 17:01:58 +0000

    I had to delete the broken code

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,21 +1,17 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
+
 // =============> write your prediction here 
+
 // the error occur because the decimalNumber has already been declared.
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
-
-console.log(decimalNumber);
 
 // =============> write your explanation here
+
 // I tried to run the code it gives me SyntaxError,that the decimalNumber has already been declared, we can see in the parameter already has name decimalNumber. 
 // thats why declaring it again inside the function, causes an error.
 
