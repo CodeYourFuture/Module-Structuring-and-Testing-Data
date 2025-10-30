7d83c03d6fc629b47c430a228a452e6875595ea4
kohanman <kohanman@googlemail.com>
2025-10-30 23:52:03 +0000

    Sprint 1 coursework 2.2 and 2.3

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = String(cardNumber).slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,8 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// i think the error will be that the slice method works for strings not numbers
+//this code gives TypeError: cardNumber.slice is not a function, this is probably because of the issue i predicted
+//but I don't know for sure base on the error code
+//i will change the cardnumber into a string using String() in the last4Digits expression
