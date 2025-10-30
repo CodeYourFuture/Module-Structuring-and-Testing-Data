468a49fd3515a166229629083b1f5c665bc5761a
Fithi Teklom <fthiteklom@gmail.com>
2025-10-30 15:06:35 +0000

    1.js updated pr template

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// there will be an error because return should not have semicolon which is the end of the declaration and a+b should be with it together.
 
 function sum(a, b) {
   return;
@@ -9,5 +10,11 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// it is undefined because it stopped reading at return;
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
