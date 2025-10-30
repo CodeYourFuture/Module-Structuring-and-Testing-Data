6a28411f5a237ad3cceb22da84941bacf9e8311c
Fithi Teklom <fthiteklom@gmail.com>
2025-10-30 14:42:13 +0000

    updated 2.js template

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,24 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// I guess an error will be thrown because a number can not be a parameter.
 
 function square(3) {
     return num * num;
 }
-
 // =============> write the error message here
+//SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// It does not know where the 3 came from. So instead of 3 it was supposed to be num.return num * num; does not work because num is not in the parameter.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
+
+console.log(square(3));
 
 
