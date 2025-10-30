492c04f4b08cad94d0fbc6763f58e715327ef785
Konvaly <konvaly@gmail.com>
2025-10-30 16:24:28 +0000

    Solved 4-random.js

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,22 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+console.log(`Random number between ${minimum} and ${maximum}: ${num}`);
+
+console.log(
+  "Random number between 0 and 1 after evaluating Math.random() is: ",
+  Math.random()
+);
+console.log(
+  "Math.random() * (maximum - minimum + 1) scales the range from 1 to 100 (100 isn't included) for the random number: ",
+  Math.random() * (maximum - minimum + 1)
+);
+console.log(
+  "Math.floor(Math.random() * (maximum - minimum + 1)) rounds down the random number: ",
+  Math.floor(Math.random() * (maximum - minimum + 1))
+);
+console.log(
+  "(Math.floor(Math.random() * (maximum - minimum + 1)) + minimum)  shifts the range to 1–100 (100 is included): ",
+  Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+);
