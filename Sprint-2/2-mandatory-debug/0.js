ccf9b9b13b77a831a88e7c9dac95efd82152c9f3
khalidbih <kain19m@hotmail.com>
2025-10-30 17:32:10 +0000

    Fixed multiply function to return correct output

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,17 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
-function multiply(a, b) {
-  console.log(a * b);
-}
-
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// in console.log we should just write (multiply(10,32) because writing in plain english will give us an error,
+// and it won't give the output.
 
 // =============> write your explanation here
+// the code was broken because the console.log was inside the function it not a function,
+//by using the return it gives us the correct output.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return (a * b)
+}
+
+console.log(multiply(10, 32));
