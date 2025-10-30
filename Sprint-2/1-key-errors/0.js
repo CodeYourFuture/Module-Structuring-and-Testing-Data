07075e1f51a97817b132e59fc7825a097e548ae2
khalidbih <kain19m@hotmail.com>
2025-10-30 15:39:07 +0000

    I fixed the error and wrote a new code, it runs perfectly

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//I think it will give an error because we have `str` twice in line 10 we should only have return without `str`  
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+
 
 // =============> write your explanation here
-// =============> write your new code here
+// The error happened because 'str' was declared twice: once as a parameter and once with 'let' inside the function.
+// Also, when calling the function, using a word without quotes caused a ReferenceError.
+// After removing the duplicate declaration and passing a string with quotes, the function works correctly,
+// =============> write your new code here 
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
+console.log(capitalise("Hard"));
