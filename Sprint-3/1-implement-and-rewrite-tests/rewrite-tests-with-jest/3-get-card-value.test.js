09eec6df44fe2f190a2fac8f8da1e95fe441ac97
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-31 18:33:22 +0000

    simplify getCardValue tests using categorical approach

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -1,31 +1,27 @@
-// This statement loads the getCardValue function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
 const getCardValue = require("../implement/3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
-  const aceofSpades = getCardValue("A♠");
-  expect(aceofSpades).toEqual(11);
+// Case 1: Handle Number Cards (2-10)
+test("should return the value of number cards (2-10)", () => {
+  expect(getCardValue("2♣")).toEqual(2);
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("10♦")).toEqual(10);
 });
 
-// Case 2: Handle Number Cards (2-10):
-test ("should return a 5 for the card with rank of 5 like(5♥) ",()=>{
-  const fiveofHearts = getCardValue("5♥");
-  expect(fiveofHearts).toEqual(5);
-
-})
-// Case 3: Handle Face Cards (J, Q, K):
-test("shoild return 10 fore the faced cart like king(K♦) and queen (Q♣)")
-const kingCard =getCardValue("K♦");
-const queenCard= getCardValue("Q♣");
-expect(kingCard).toEqual(10);
-expect (queenCard).toEqual(10);
-// Case 4: Handle Ace (A):
-test("should return 11 for Ace of Spades", () => {
-  const aceofSpades = getCardValue("A♣");
-  expect(aceofSpades).toEqual(11);
+// Case 2: Handle Face Cards (J, Q, K)
+test("should return 10 for face cards (J, Q, K)", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♣")).toEqual(10);
+  expect(getCardValue("K♦")).toEqual(10);
 });
-// Case 5: Handle Invalid Cards:
-test ("should return invalid card rank for invalid inputs like(z♣)",()=>{
-  const invalidCard= getCardValue("z♣");
-  expect(invalidCard).toEqual("Invalid card rank.");
+
+// Case 3: Handle Ace (A)
+test("should return 11 for Ace cards", () => {
+  expect(getCardValue("A♠")).toEqual(11);
+  expect(getCardValue("A♣")).toEqual(11);
+});
+
+// Case 4: Handle Invalid Cards
+test("should return 'Invalid card rank.' for invalid cards", () => {
+  expect(getCardValue("z♣")).toEqual("Invalid card rank.");
+  expect(getCardValue("11♠")).toEqual("Invalid card rank.");
 });
