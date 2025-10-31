ad0f006e052c74b97e23fd1eaecd6d6e7f7011bb
HANISADAH <hanisadah81@gmail.com>
2025-10-31 00:47:43 +0000

    Answering the task

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,32 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> I believe that the function converToPercentage is not mentioned in the console.log statement.
+// Declaring the const decimalNumber is unnecessary as it is already declared as a parameter within the function convertToPercentage.
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+// function convertToPercentage(decimalNumber) {
+  // const decimalNumber = 0.5;
+  // const percentage = `${decimalNumber * 100}%`;
+
+  // return percentage;
+}
+
+// console.log(decimalNumber);
+
+// =============> A syntaxError occurred saying decimalNumber is already declared.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+function convertToPercentage() {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage());
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+// the new code runs without error and outputs "50%"
