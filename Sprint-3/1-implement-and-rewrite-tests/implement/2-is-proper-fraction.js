7dd99dd1a4d55c4df1f306d6a55617a6acc61f19
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-31 19:42:08 +0000

    Fix isProperFraction to handle negative denominators correctly by adding Math.abs to input numbers

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -11,7 +11,7 @@ function isProperFraction(numerator, denominator) {
   if (denominator === 0) {
     return false;
   }
-  if (numerator < denominator && numerator > -denominator) {
+  if (Math.abs(numerator) < Math.abs(denominator)) {
     return true;
   } else {
     return false;
