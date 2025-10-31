c5a8edb76226138bb6e9868833df3f34e0f13993
HANISADAH <hanisadah81@gmail.com>
2025-10-31 23:38:09 +0000

    Answering the task

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,13 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> SyntaxError will occur due to putting a semicolon after return.
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> unlike the expectation that the code runs without error as 'the sum of 10 and 32 is undefined'.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// we needed just to remove the semicolon after return statement and to bring up the a+b expression side by side with return.
