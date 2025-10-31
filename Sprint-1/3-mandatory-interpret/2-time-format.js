349302502482e241793ad77b05a94f7cb41faa64
kohanman <kohanman@googlemail.com>
2025-10-31 00:42:21 +0000

    Sprint 1 coursework 3.2: change const to let

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = "0"; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -12,14 +12,20 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+//6
 
 // b) How many function calls are there?
+//1 - console.log()
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+//it gives you the remainder when movieLength is divided by 60
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+//it means it will take the movieLength in seconds, subtract remainingSeconds, and divide the answer by 60 to give the totalMinutes
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+//it is the length of time the movie has been running, remainingLength would be a better name
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+//tried really big, small, and multiples of 60 and found no issue
