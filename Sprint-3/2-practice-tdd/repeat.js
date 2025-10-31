7172fd2e29fbc48d7870536488418398fe2b3095
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-10-31 10:30:39 +0000

    I have implemented a function called repeat and writen a few different tests to check my function

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,10 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(myString, repeatNumber) {
+  if (repeatNumber < 0) return "Invalid Input must be a positive number";
+  let str = "";
+  for (let i = 0; i < repeatNumber; i++) {
+    str += myString;
+  }
+  return str;
 }
 
 module.exports = repeat;
--- Sprint-3/2-practice-tdd/repeat.test.js
+++ Sprint-3/2-practice-tdd/repeat.test.js
@@ -21,12 +21,31 @@ test("should repeat the string count times", () => {
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
 
+test("should return the string", () => {
+  const str = "CYF";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("CYF");
+});
+
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
 
+test("should return empty when the count is 0", () => {
+  const str = "CYF";
+  const count = 0;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("");
+});
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+test("should return empty when the count is 0", () => {
+  const str = "CYF";
+  const count = -2;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("Invalid Input must be a positive number");
+});
