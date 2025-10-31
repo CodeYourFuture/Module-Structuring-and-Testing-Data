0036b8df276423e9c949da51858d4950871d0275
Hadi Vahidi <hadivahidi20@gmail.com>
2025-10-31 17:36:37 +0000

    the get-ordinal-number test is done

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,17 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const twoLastDig = num % 100;
+  if (twoLastDig === 11 || twoLastDig === 12 || twoLastDig === 13) {
+    return num + "th";
+  }
+
+  const lastDig = num % 10;
+  if (lastDig === 1) return num + "st";
+  if (lastDig === 2) return num + "nd";
+  if (lastDig === 3) return num + "rd";
+  return num + "th";
 }
+
+
+
 
 module.exports = getOrdinalNumber;
