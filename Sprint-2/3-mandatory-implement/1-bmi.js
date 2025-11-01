f5b79bf4901e4cee8e849d1d5e5236589934133c
Payman IB <payman.ib.com>
2025-11-01 15:14:52 +0000

    Implement BMI calculation function and log result

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -16,4 +16,7 @@
 
 function calculateBMI(weight, height) {
     // return the BMI of someone based off their weight and height
+    const bmi = weight / (height * height);
+    return bmi.toFixed(1); 
 }
+console.log(`The BMI of a person weighing 70kg and 1.73m tall is ${calculateBMI(70, 1.73)}`);
