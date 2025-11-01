f91a49c97e3943ce59beeb81e4ee79046f9ad69f
HANISADAH <hanisadah81@gmail.com>
2025-11-01 22:02:54 +0000

    Answering the task

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,21 @@
+
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> In line 6, instead of console.log printing the result of multiply(10, 32), return a*b would be better
+//because the console.log would only print a*b.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+//function multiply(a, b) {
+  //console.log(a * b);
+//}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> After running the code, we got 'the result of multiplying 10 and 32 is undefined'
+// as the multiply parameters weren't considered as parameters inside the function multiply.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b; 
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
