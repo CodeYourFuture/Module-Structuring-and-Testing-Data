f08ec155572948edfbbad512b1666c2900df642e
Declan W <declanwilliams793@gmail.com>
2025-11-01 14:41:36 +0000

    implented tests for ordinal numbers 2nd and 3rd

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -11,3 +11,18 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
 });
+// Case 2: Identify the ordinal number for 2
+// When the number is 2,
+// The function should then return "2nd".
+
+test("Should return `2nd` for 2", () => {
+    expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+// Case 3: Identify the ordinal number for 3
+// When the number is 3,
+// The Function should the return "3rd"
+
+test("Should return `3rd` for 3", () => {
+    expect(getOrdinalNumber(3)).toEqual("3rd");
+});
