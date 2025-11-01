49307429a0881064ccf0619836a13b1b539feb47
UbunMen <mahtem23@gmail.com>
2025-11-01 17:42:48 +0000

    // Sprint-3 ewrite-tests-with-jest/2-is-proper-fraction.test.js. rewrote tests in jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,18 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 3)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative fraction", () => {
+  expect(isProperFraction(-2, 3)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
+
+// Sprint-3 ewrite-tests-with-jest/2-is-proper-fraction.test.js. rewrote tests in jest 
