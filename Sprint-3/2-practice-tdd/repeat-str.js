6a021e32c2d432a2a756d74b66558d44a967bdf7
Declan W <declanwilliams793@gmail.com>
2025-11-01 15:02:22 +0000

    implemented repeat function with error handling and edge cases with comments detailing.

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,19 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  // check if "count" is a negative number if so will throw new error
+  if (count < 0) {
+    throw new error("Count must be a positive number");
+
+    //check if the is equal to zero
+    if (count === 0){
+      return ""; // returns empty string if count is equal to zero
+    }
+      //check if the count is equals 1.
+  if (count === 1) {
+    //returns just the string as its not needed to be repeated
+    return str;
+  }
+  return str.repeat(count); // if the count is above two it repeat "count" number of times.
+}
 }
 
 module.exports = repeatStr;
