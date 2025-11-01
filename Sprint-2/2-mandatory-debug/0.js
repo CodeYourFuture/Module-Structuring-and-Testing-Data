7f385a8a74cedf23b485b76b68053b11ab0fcedb
kohanman <kohanman@googlemail.com>
2025-11-01 14:51:36 +0000

    Sprint 2 | Coursework  2.0

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,22 @@
 // Predict and explain first...
+//the function doesnt return anything so it will just display a*b (320)
+// and the message "the result of.....'undefined'"
 
 // =============> write your prediction here
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+//the function doesnt return anything it just performs console log using the same a and b variables
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
