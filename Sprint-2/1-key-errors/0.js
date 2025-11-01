e1136398e6e8f9234ebf76444c046db53f8393f9
Konvaly <konvaly@gmail.com>
2025-11-01 00:10:22 +0000

    Solved task 1-key-errors/0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,16 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// So, the error sounds "SyntaxError: Identifier 'str' has already been declared", it means that we can't declare the sane variable name twice
+// =============> write your new code here
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisedStr;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise("greetings")); // Output: "Greetings"
