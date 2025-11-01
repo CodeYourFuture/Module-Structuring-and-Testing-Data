22f8e4a652b07aca100ba9c717c33df2345ec486
Declan W <declanwilliams793@gmail.com>
2025-11-01 14:35:18 +0000

    implemented how special ordinal number cases like 11,12,13 are handled by making sure they always end in "th"

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,12 @@
 function getOrdinalNumber(num) {
-  return "1st";
+ const lastTwoDigits = num % 100; // gets the last two digits of the number because some like 11, 12, 13 are special cases.
+ const lastDigit= num % 10; // gets the last digit to decide if its going to be "St, Nd, Rd"
+
+ // handles special cases like "11,12,13" to always end in the "Th"
+ if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13){
+  return num + "St";
+}
+
 }
 
 module.exports = getOrdinalNumber;
