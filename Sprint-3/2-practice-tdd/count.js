a09a9927e926bf7a3cbee6e4bedc64b42bca64fc
Declan W <declanwilliams793@gmail.com>
2025-11-01 14:24:02 +0000

    refactor: implement character counting logic in countChar function

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,18 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+    // start a count of 0
+    let count = 0;
+
+    // check each of the characters in the string one by one.
+    for (let i = 0; i < stringOfCharacters.length; i++) {
+        // checks if the current characters matches the one were looking for in the string.
+        if (stringOfCharacters[i] === findCharacter)
+            // if it does, we increment the count by 1.
+            count = count + 1;
 }
+    
+    return count;
+}
+console.log(countChar("aaaaa", "a")); // 5
+console.log(countChar("hello", "l")); // 2
 
 module.exports = countChar;
