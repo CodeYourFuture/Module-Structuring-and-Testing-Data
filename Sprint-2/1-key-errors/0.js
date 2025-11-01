15dadf15bfa271701105278bd323bd4f6c24d82f
HANISADAH <hanisadah81@gmail.com>
2025-11-01 21:59:02 +0000

    Answering the task

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,22 @@
+
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I predict that we will not need to write let in line 8 as str is already declared
+// as an argument within the function captialise.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise (str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// capitalise ('hello');
+//==============> A syntaxError occurred as 'str' is being cleared.
+
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
-// =============> write your explanation here
-// =============> write your new code here
+console.log (capitalise ('hello'));
