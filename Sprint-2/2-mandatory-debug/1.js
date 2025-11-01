0b216d5993b529e2dbb3d8281189d8cc2eb383ef
Konvaly <konvaly@gmail.com>
2025-11-01 01:09:43 +0000

    Solved task 2-mandatory-debug/1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I guess that the output will be undefined because there is a semicolon after the "return" and this breaks the line of code, and after return statement nothing can be returned
 
 function sum(a, b) {
   return;
@@ -8,6 +8,8 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> The output is "The sum of 10 and 32 is undefined" as I predicted above
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> function sum(a, b) {
+//                   return a + b;
+//                 }
