a976af75be4306c47b8be370af164b05d0d12f6b
kohanman <kohanman@googlemail.com>
2025-11-01 14:19:40 +0000

    completed coursework sprint 2.0

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -5,9 +5,17 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  var str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+capitalise(hello);
 
 // =============> write your explanation here
+//cant overwrite the str variable with a new parameter, because it is let instead of var
 // =============> write your new code here
+
+function capitalise(str) {
+  var str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+capitalise("hello"");
