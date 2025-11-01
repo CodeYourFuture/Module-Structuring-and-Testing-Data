c68a37cd89a4c66b85632ee9bef3dc5787dd10c5
UbunMen <mahtem23@gmail.com>
2025-11-01 18:01:22 +0000

    Sprint-3 rewrite-tests-with-jest/2-get-card-value.test.js. rewrote tests using jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,23 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should number cards for (2-10)", () => {
+  const aceofSpades = getCardValue("5♠");
+  expect(aceofSpades).toEqual(5);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards", () => {
+  const aceofSpades = getCardValue("Q♠");
+  expect(aceofSpades).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+// test("should return 11 for Ace of Spades", () => {
+//   const aceofSpades = getCardValue("A♠");
+//   expect(aceofSpades).toEqual(11);
+// });
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid cards", () => {
+  expect(() => getCardValue("Z♠")).toThrow("Invalid Card");
+});
+// Sprint-3 rewrite-tests-with-jest/2-get-card-value.test.js. rewrote tests using jest 
