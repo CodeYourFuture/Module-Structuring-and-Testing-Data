876bf7bee3e7e26f921c21305b8f563c7f54a16c
Payman IB <payman.ib.com>
2025-11-01 14:11:44 +0000

    Fix variable shadowing in capitalise function and update console log; correct convertToPercentage function implementation

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,16 +1,15 @@
 // Predict and explain first...
 //  =============> write your prediction here
 // it gives us an error.
-
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 // function capitalise(str) {
- //  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  // return str;
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+// return str;
 // }
 
-// =============> write your explanation here : 
+// =============> write your explanation here :
 // as the variable in side the function has the same name as the function variable.
 // =============> write your new code here
 function capitalise(str) {
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,32 @@
 // Predict and explain first...
-
-// Why will an error occur when this program runs?
+// error on line 11
+// error on line 17
+// Why will an error occur when this program runs? yes
 // =============> write your prediction here
+// decimalNumber is declared before and can not be declared again.
+// console.log(decimalNumber) should be console.log(convertToPercentage)
+// Try playing computer with the example to work ot what is going on
 
-// Try playing computer with the example to work out what is going on
+// function convertToPercentage(decimalNumber) {
+// //  const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
 
+//   return percentage;
+// }
+
+//console.log(percentage);
+
+// =============> write your explanation here
+// decimalNumber is declared before and can not be declared again.
+// console.log(decimalNumber) should be console.log(convertToPercentage)
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  //const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5));
