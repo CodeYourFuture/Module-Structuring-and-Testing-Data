7ae8d3a46d7378669f11d8ecccef62726f6044b9
Declan W <declanwilliams793@gmail.com>
2025-11-01 13:01:49 +0000

    reorder code to declare cityOfBirth before usage in console.log to solve the error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,12 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
+// The error is that `cityOfBirth` is being used before it has been declared.
+// To fix this, we needed to declare `cityOfBirth` before using it in the console.log statement.
+
+// Here's the corrected code:
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
