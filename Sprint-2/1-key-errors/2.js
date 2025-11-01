58865b6d7f551ce380e0da2b506e95ac3a737104
kohanman <kohanman@googlemail.com>
2025-11-01 14:46:04 +0000

    Sprint 2 | Coursework  1.2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,25 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+//going to give an identifier expected error because the 3 should be num
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+//SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// function doesnt have an object to call if there is a number here
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+  return num * num;
+}
+console.log(square(3));
