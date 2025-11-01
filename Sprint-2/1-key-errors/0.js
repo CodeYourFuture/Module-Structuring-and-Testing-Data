2c6aae7c081b96b4712c4a1d1fd37870d871df0a
Payman IB <payman.ib.com>
2025-11-01 11:32:24 +0000

    1-key-errors,0.js, completed

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// it gives us an error.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+ //  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  // return str;
+// }
 
-// =============> write your explanation here
+// =============> write your explanation here : 
+// as the variable in side the function has the same name as the function variable.
 // =============> write your new code here
+function capitalise(str) {
+  let str1 = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str1;
+}
+console.log(capitalise("abcd"));
