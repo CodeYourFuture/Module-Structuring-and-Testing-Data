9aef62751c05469d61d07f3d39288afcad296c6e
kohanman <kohanman@googlemail.com>
2025-11-01 14:37:26 +0000

    Sprint 2 | Coursework  1.1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -5,16 +5,27 @@
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   let decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> write your explanation here
+//const decimalNumber cant be declared because it is already being set by the function,
+// so it gives error, that line can removed.
+// And the console log needs to call the function convertToPercentage(0.5) not decimalNumber
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5));
