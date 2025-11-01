3ae35f7a0531af328df1e5f3c0e6710bb924db05
khalidbih <kain19m@hotmail.com>
2025-11-01 14:36:10 +0000

    Updated variable names

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,15 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
-function sum(a, b) {
-  return;
-  a + b;
-}
-
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// The return statement is not in the right place, so the function doesn’t return the value.
 
 // =============> write your explanation here
+//  The return statement is stopping the function too early, so it doesn’t return a + b.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return (a+b);
+}
+
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -3,9 +3,31 @@
 // Predict the output of the following code:
 // =============> Write your prediction here
 
+// const num = 103;
+
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// the last digit of 42 is 3
+// the last digit of 105 is 3
+// the last digit of 806 is 3
+// Explain why the output is the way it is
+// =============> write your explanation here
+// we will always get number 3 because num is 103 in the last digit of it is 3.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
 const num = 103;
 
-function getLastDigit() {
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
@@ -13,12 +35,8 @@ console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// We need num in the function as a parameter.
