346b3775f9c26541af2fba08aa05f7e5ec9567ac
Declan W <declanwilliams793@gmail.com>
2025-11-01 12:47:41 +0000

    implement initials extraction and logs individual characters

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,7 +5,9 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+let initials = `${firstName[0]}${middleName[0]}${lastName[0]}`;
+console.log(initials); //CKJ
+// the above code is using bracket notation to access the first character of each string and then logs it to the console.
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
