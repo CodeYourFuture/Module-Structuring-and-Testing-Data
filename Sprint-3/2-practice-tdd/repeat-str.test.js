74552537b5d1dda6850f2a4bcf34409ed1707b29
Declan W <declanwilliams793@gmail.com>
2025-11-01 15:03:29 +0000

    test: add test case for returning original string when count is 1

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,6 +20,12 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
+test("should return the original string when count is 1", () => {
+    const str = "hello";
+    const count = 1;
+    const repeatedStr = repeat(str, count);
+    expect(repeatedStr).toEqual("hello");
+});
 
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
