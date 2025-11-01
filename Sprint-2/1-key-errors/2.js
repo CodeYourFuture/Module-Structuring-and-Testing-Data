c642719c28d0daaa113083af5aef4b8649d81682
HANISADAH <hanisadah81@gmail.com>
2025-11-01 22:00:41 +0000

    Answering the task

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,26 @@
 
+
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> The num parameter should be mentioned inside the function square instead of the number 3.
+// then it should be called as square (3) in the console.log statement.
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//return num * num;
+//}
 
-// =============> write the error message here
+// =============> function square(3) syntaxError: Unexpected number.
+// =============> 3 wasn't expected... instead a declaration like 'num' was expected.
 
-// =============> explain this error message here
+
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+  return num * num;
+}
 
-
+console.log(square(3));
