734265fed0b9c6f960f3eac443b5baeab11c2e55
christelleb <christelleboten@gmail.com>
2025-11-02 19:49:12 +0000

    answer 2 - initials

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -5,4 +5,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-// Line 3 is a statement that is is reassigning the value of count, to add 1 to the initial declaration which was 0.
+// Line 3 is a statement that is reassigning the value of count, to add 1 to the initial declaration which was 0.
