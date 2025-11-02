5f29b84e43bc0aa0d5a25c07c7388f74d5ac2ab1
Payman IB <payman.ib.com>
2025-11-02 14:48:43 +0000

    Refactor comments and formatting in time-format.js for clarity

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -4,12 +4,13 @@ function pad(num) {
 
 function formatTimeDisplay(seconds) {
   const remainingSeconds = seconds % 60;
-  const totalMinutes = (seconds - remainingSeconds) / 60;
-  const remainingMinutes = totalMinutes % 60;
+  const totalMinutes = (seconds - remainingSeconds) / 60; //1
+  const remainingMinutes = totalMinutes % 60; //1
   const totalHours = (totalMinutes - remainingMinutes) / 60;
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
+
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -18,17 +19,20 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+//  3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
+console.log(formatTimeDisplay(61));
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-
+// 0
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
-
+// "00"
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-
+// 1, because remainingSeconds is 1 when pad is called for the last time.
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// "01", because pad adds a leading zero to single digit numbers.
