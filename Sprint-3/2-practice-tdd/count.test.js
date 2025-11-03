26147072c308fdd972526c9523c91414568e43b7
saff-coder <sophi.m.80@gmail.com>
2025-11-03 22:21:14 +0000

    implement countChar test done with Different Scenario

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,19 +1,29 @@
 // implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
+
+
 // Given a string str and a single character char to search for,
 // When the countChar function is called with these inputs,
-// Then it should:
+// Scenario: Single Occurrence
 
+test("counts a single occurrence of a character", () => {
+    expect(countChar("hello", "h")).toEqual(1);
+    expect(countChar("world", "d")).toEqual(1);
+  });
 // Scenario: Multiple Occurrences
 // Given the input string str,
 // And a character char that may occur multiple times with overlaps within str (e.g., 'a' in 'aaaaa'),
 // When the function is called with these inputs,
 // Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
+test("counts multiple occurrences of a character", () => {
+    expect(countChar("banana", "a")).toEqual(3);
+    expect(countChar("mississippi", "s")).toEqual(4);
+  });
 
 test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
-  const char = "a";
-  const count = countChar(str, char);
+  const Char = "a";
+  const count = countChar(str,Char);
   expect(count).toEqual(5);
 });
 
@@ -22,3 +32,11 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+// Scenario: No Occurrences
+test("should return 0 if the character is not in the string", () => {
+  const str = "hello";
+  const char = "x"; // character not present in str
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
