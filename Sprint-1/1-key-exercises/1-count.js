e1447e3d41ddcfe0270c4cfa3ad7a73ad3054a0d
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-03 00:14:23 +0000

    Increase by 1 the variable count

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,10 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/**
+ * Line 3 increase the count variable by 1 then assign the result in right hand side to count variable
+ */
+ 
+console.log('Value of count variable: ', count)
+
