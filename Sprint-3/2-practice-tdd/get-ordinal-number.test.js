d6f14a8779d0888f468e67c9bc35d30cf82a3593
saff-coder <sophi.m.80@gmail.com>
2025-11-03 22:24:20 +0000

    test is done with different Numbers

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -11,3 +11,13 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
 });
+test("should return 'th' for 11, 12, 13", () => {
+  expect(getOrdinalNumber(11)).toBe("11th");
+  expect(getOrdinalNumber(12)).toBe("12th");
+  expect(getOrdinalNumber(13)).toBe("13th");
+});
+test("should return correct ordinal for numbers > 20", () => {
+  expect(getOrdinalNumber(21)).toBe("21st");
+  expect(getOrdinalNumber(22)).toBe("22nd");
+  expect(getOrdinalNumber(23)).toBe("23rd");    
+});
