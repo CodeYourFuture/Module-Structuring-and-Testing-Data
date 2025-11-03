d8fd1873339a7b47409d112e91c8c4c49b351dac
saff-coder <sophi.m.80@gmail.com>
2025-11-03 14:16:37 +0000

    completed the function now it should count how many char in a string

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,11 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+   let count = 0;
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+  }
+  return count; // this replaces 'return 5'
 }
 
 module.exports = countChar;
