4b1ab1f2ef195660926146e075a4a9d0537502af
zilinskyte <zilinskyterogers@gmail.com>
2025-11-03 12:16:21 +0000

    formatted with prettier

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,24 +4,27 @@
 // You should call this function a number of times to check it works for different inputs
 
 function toPounds(penceString) {
-penceString = String(penceString);// ensures input is treated as a string even if number is inputted
-// const penceStringWithoutTrailingP = penceString.substring(
-let penceStringWithoutTrailingP = penceString;
-if (penceString.endsWith("p")) {
-    penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
-}   // This prevents the last characters from being sliced off if there is no p.
+  penceString = String(penceString); // ensures input is treated as a string even if number is inputted
+  // const penceStringWithoutTrailingP = penceString.substring(
+  let penceStringWithoutTrailingP = penceString;
+  if (penceString.endsWith("p")) {
+    penceStringWithoutTrailingP = penceString.substring(
+      0,
+      penceString.length - 1
+    );
+  } // This prevents the last characters from being sliced off if there is no p.
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
 
-return`£${pounds}.${pence}`;
+  return `£${pounds}.${pence}`;
 }
 console.log(toPounds("481p"));
 console.log(toPounds("50p"));
@@ -29,6 +32,6 @@ console.log(toPounds("5p"));
 console.log(toPounds("0p"));
 console.log(toPounds("12345p"));
 console.log(toPounds("9")); // edge case: missing 'p' at the end, returns 0 rather than error because last character is sliced off
-// Can be fixed by changing line 8 
+// Can be fixed by changing line 8
 console.log(toPounds("abc")); // Works but returns nonsense value
 console.log(toPounds(123)); // Works with number input because of line 7 converting to string
