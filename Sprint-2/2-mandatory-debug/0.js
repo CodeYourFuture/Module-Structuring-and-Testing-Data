fc492bbdf84cdd44fa1be4ab5f143b228d5e2b66
Ali <naru.a@yahoo.com>
2025-11-03 15:36:10 +0000

    Fix multiply function to return the result and update predictions and explanations

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,9 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// I predict that the code will output the result of the multiplication, but the function multiply will not return a value.
+
+// this function should multiply two numbers and return the result
+
 
 function multiply(a, b) {
   console.log(a * b);
@@ -10,5 +13,13 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
+// The function multiply is using console.log to output the result of the multiplication, but it is not returning a value.
+// Therefore, the template literal will not be able to access the result of the multiplication.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
