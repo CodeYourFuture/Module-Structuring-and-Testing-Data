5c3f09f68cbe7065e332f7ae60370f6d068133cf
saff-coder <sophi.m.80@gmail.com>
2025-11-03 22:17:41 +0000

    Updated the CountChar function

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,11 +1,12 @@
-function countChar(stringOfCharacters, findCharacter) {
-   let count = 0;
-  for (let i = 0; i < stringOfCharacters.length; i++) {
-    if (stringOfCharacters[i] === findCharacter) {
-      count++;
+function countChar(str, char) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {//loop through each character in the string
+    if (str[i] === char) {//check if the character matches the target character
+      count = count + 1;
     }
   }
-  return count; // this replaces 'return 5'
-}
 
+  return count;//return the final count
+}
 module.exports = countChar;
