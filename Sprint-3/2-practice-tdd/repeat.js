c8c9bc13063259d4b071ecec4be7e32350646c8c
saff-coder <sophi.m.80@gmail.com>
2025-11-03 22:21:44 +0000

    function Updated

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,8 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(word, times) {
+  if (times < 0) {
+    throw new Error("Count must be a non-negative number");
+  }
+  return word.repeat(times);
 }
 
 module.exports = repeat;
