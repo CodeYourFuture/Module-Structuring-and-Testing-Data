177f0d72e0ba77f3d88fe64a30dde533a682f200
Fithi Teklom <fthiteklom@gmail.com>
2025-11-04 17:29:27 +0000

    updated pr template

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -8,6 +8,27 @@ test("should identify right angle (90°)", () => {
 
 // REPLACE the comments with the tests
 // make your test descriptions as clear and readable as possible
+//Case 2: Identify Acute Angles
+
+test("should identify acute angle (<90°)", () => {
+  expect(getAngleType(45)).toEqual("Acute angle");
+});
+
+// Case 3: Identify Obtuse Angles
+test("should identify obtuse angle (between 90° and 180°)", () => {
+  expect(getAngleType(120)).toEqual("Obtuse angle");
+});
+
+// Case 4: Identify Straight Angles
+test("should identify straight angle (180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
+
+// Case 5: Identify Reflex Angles
+test("should identify reflex angle (between 180° and 360°)", () => {
+  expect(getAngleType(270)).toEqual("Reflex angle");
+});
+
 
 // Case 2: Identify Acute Angles:
 // When the angle is less than 90 degrees,
