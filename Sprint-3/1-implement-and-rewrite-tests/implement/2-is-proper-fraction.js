632c967ff5b2454783c4f75f5598f9df7c5c81ae
ahmadehsas <ahmadehsas14345@gmail.com>
2025-11-05 18:24:00 +0000

    Updated my code.

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -8,13 +8,8 @@
 // write one test at a time, and make it pass, build your solution up methodically
 
 function isProperFraction(numerator, denominator) {
-  if (Math.abs(numerator) < Math.abs(denominator)) return true; // This version of code works correctly for proper and negative fractions.
-  if (Math.abs(numerator) >= Math.abs(denominator)) return false;
-  if (Math.abs(numerator) === Math.abs(denominator)) return false;
-
-  // if (numerator < denominator) {
-  //   return true;
-  // }
+  return Math.abs(numerator) < Math.abs(denominator);
+  
 
 }
 
@@ -67,5 +62,5 @@ console.log(isProperFraction(5, 2));
 console.log(isProperFraction(-4, 7));
 console.log(isProperFraction(-3, 3));
 console.log(isProperFraction(-2, -3)); // This is a proper fraction because the absolute value of numerator 2 and denominator is 3.
-// the value of  numerator is less than denominator.
+// // the value of  numerator is less than denominator.
 console.log(isProperFraction(-4, -7));
