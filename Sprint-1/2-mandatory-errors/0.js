92d1f33a5a7cc7053114f99851d48f61a0602c03
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-06 23:40:17 +0000

    Using comment to explain the information contain in the file, useful only for developers

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,29 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+/** If you have lines that are just instructions or notes for humans and you don’t want the computer to execute them, you can turn them into comments.
+
+In JavaScript, there are two ways to write comments:
+
+1. Single-line comment
+
+Use // at the start of a line.
+Everything after // is ignored by the computer.
+
+Example:
+
+// This line explains what the code does
+const num = 8;
+
+2. Multi-line comment
+
+Use \ /* ... *\ / to wrap several lines.
+
+Example:
+This section is just instructions
+The computer will ignore everything between these symbols
+
+const num = 5;
+
+*/
+
