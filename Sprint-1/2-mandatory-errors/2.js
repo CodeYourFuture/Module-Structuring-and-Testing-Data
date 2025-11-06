b9538b25f0343251f4dd1e6423185270c8caf24a
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-06 23:51:34 +0000

    Variable must be declared first before it can be used

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// this is a very common JavaScript error related to variable hoisting and the temporal dead zone.
+// Simply declare the variable before you use it
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
