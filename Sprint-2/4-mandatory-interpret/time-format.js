f425e8af83d6ef6d417d4eeffb6d0f64e98f847e
khalidbih <kain19m@hotmail.com>
2025-11-06 15:57:38 +0000

    This exercise complete

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,24 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// It will be called 3 times hours,minutes and Seconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// The first argument passed to pad (totalHours).
+// totalHours = 0
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// pad (totalHours) = pad(0) returns "00".
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The value assigned to num when pad called last time is num = 1 because that's the remainingSeconds which is 1 after
+// we 61 divided by 60 = 1.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The return value will be 01 because ToString gives "1" and padStart("2, 0") that adds 0 in the front 
