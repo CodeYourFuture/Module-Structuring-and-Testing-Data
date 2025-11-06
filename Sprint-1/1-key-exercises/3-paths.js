ad07e1618764c847a0c58c29dc15a3990bfbaf8a
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-06 23:07:20 +0000

    Directory and extension part of a file path

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -15,9 +15,14 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-// Create a variable to store the ext part of the variable
+const dir = filePath.slice(0, lastSlashIndex);
 
-const dir = ;
-const ext = ;
+// Create a variable to store the ext part of the variable
+const lastDotIndex = filePath.lastIndexOf(".");
+const ext = filePath.slice(lastDotIndex);
+
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
+
 
 // https://www.google.com/search?q=slice+mdn
