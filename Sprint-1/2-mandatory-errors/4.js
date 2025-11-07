57e97e9f029c016730df9ee710f38c083f73bdd6
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-07 00:12:09 +0000

    Change variable naming starting with letter instead of numbers

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,18 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+/**
+ * In javascript variable can't start with a number
+ * Can contain letters, digits (but not at the start), underscore _, and dollar signs $
+ *
+ * const 12HourClockTime = "20:53"; will cause a SyntaxError because variable names can't begin with a number. 
+ *
+ * To fix it by changing the name to start with a letter.
+ */
+
+const twelveHourClockTime = "08:53";
+const twentyFourClockTime = "20:53";
+
+console.log(twelveHourClockTime);
+console.log(twentyFourClockTime);
+
