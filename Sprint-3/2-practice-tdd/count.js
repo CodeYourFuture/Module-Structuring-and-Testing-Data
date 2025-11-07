4b39af73921e2e6fb9e11a7e4b96a275072ce181
UbunMen <mahtem23@gmail.com>
2025-11-07 17:44:11 +0000

    Added lines to check cases and return count of characters.

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,23 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  if (typeof stringOfCharacters!= "string") {
+    throw new Error("Input should be a string");
+  }
+   if (typeof findCharacter !== "string" || findCharacter.length != 1){
+    throw new Error ("Input must be a single character");
+  }
+
+  let count = 0;
+  for(let char of stringOfCharacters) {
+     if (char === findCharacter) {
+      count++;
+     }
+ }
+    return count;
+
 }
 
 module.exports = countChar;
+
+console.log(countChar("amazon", "a"));
+
+// Added lines to check cases and return count of characters.
