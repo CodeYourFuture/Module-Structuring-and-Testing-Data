6e2b1d7342470f72aa86356b7a94cec48c2edcf9
Zadri <zadriabdule@gmail.com>
2025-11-07 16:15:25 +0000

    Add fix code error

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,19 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// It throw a error - because of invalid parameter (can't use a number as a parameter name)
+//function square(3) {
+ //   return num * num;
+//}
 
-function square(3) {
+// =============> write the error message here
+// SyntaxError: Unexpected number
+// =============> explain this error message here
+// The "Unexpected number" error arises when a numeral is improperly positioned or used within JavaScript code.
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+function square(num){
     return num * num;
 }
 
-// =============> write the error message here
-
-// =============> explain this error message here
-
-// Finally, correct the code to fix the problem
-
-// =============> write your new code here
-
-
+console.log(square);
