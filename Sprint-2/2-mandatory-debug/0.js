2bf34878fd4d1dad860f470d91b9f206abfd8393
Zadri <zadriabdule@gmail.com>
2025-11-08 23:54:01 +0000

    Fix undefined return in sum() and explain cause (2-mandatory-debug/0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+// It will show a undefined error 
 function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}
 
 // =============> write your explanation here
-
+// The function printed the answer but did't return it, so when it was used in the template string, it came out as 'undefined.'
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+ function multiply(a, b) {
+      return a * b; // Once I put a * b on the same line as return, it worked
+}
+
+console.log(` The sum of 10 and 32 is ${sum(10, 32)}`);
+
