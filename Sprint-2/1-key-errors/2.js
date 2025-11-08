8a162e393efaa68d5640957aeb01caf39f80618a
Zadri <zadriabdule@gmail.com>
2025-11-08 22:35:53 +0000

    Fix and explain 'unexpected number' SyntaxError in square() (1-key-errors/2.js)

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -10,11 +10,16 @@ function square(3) {
 }
 
 // =============> write the error message here
-
+// SyntaxError: Unexpected number
 // =============> explain this error message here
-
+// The "Unexpected number"
 // Finally, correct the code to fix the problem
-
+// The "Unexpected number" error arises when a number is improperly positioned or used within JvaScript code
 // =============> write your new code here
+ function square(num){
+      return num * num;
+}
+
+console.log(square);
 
 
