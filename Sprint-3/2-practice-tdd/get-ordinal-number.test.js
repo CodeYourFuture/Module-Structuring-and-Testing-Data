034d8f802d3be6d701eeafb884f72575695a5715
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-11-08 14:32:34 +0000

    grouping the tests by behaviour

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -4,58 +4,36 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
 
-// Case 1: Identify the ordinal number for 1
-// When the number is 1,
-// Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
+
+test("append 'st' to numbers ending in 1, except those ending in 11", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(221)).toEqual("221st");
+  expect(getOrdinalNumber(191)).toEqual("191st");
 });
-// Case 2: Identify the ordinal number for 22
-// When the number is 22,
-// Then the function should return 22nd"
-test("should return '22nd' for 22", () => {
-  expect(getOrdinalNumber(22)).toEqual("22nd");
+test("append 'nd' to numbers ending in 2, except those ending in 12", () => {
+    expect( getOrdinalNumber(2) ).toEqual("2nd");
+    expect( getOrdinalNumber(22) ).toEqual("22nd");
+    expect( getOrdinalNumber(132) ).toEqual("132nd");
 });
-// Case 3: Identify the ordinal number for 73
-// When the number is 73,
-// Then the function should return 73rd"
-
-test("should return '73rd' for 73", () => {
-  expect(getOrdinalNumber(73)).toEqual("73rd");
+test("append 'rd' to numbers ending in 3, except those ending in 13", () => {
+    expect( getOrdinalNumber(3 )).toEqual("3rd");
+    expect( getOrdinalNumber(33 ) ).toEqual("33rd");
+    expect( getOrdinalNumber(133) ).toEqual("133rd");
 });
 
-// Case 4: Identify the ordinal number for 99
-// When the number is 99,
-// Then the function should return 99th"
 
-test("should return '99th' for 99", () => {
+test("append 'th'to numbers ending in 4-9 or ending with 11,12,13", () => {
   expect(getOrdinalNumber(99)).toEqual("99th");
+  expect(getOrdinalNumber(234)).toEqual("234th");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(112)).toEqual("112th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(313)).toEqual("313th");
 });
 
-// Case 5: Identify the ordinal number for number ends with 11,12, or 13
-//  When the number is 111,
-// Then the function should return 111th"
-//  When the number is 212,
-// Then the function should return 212th"
-//  When the number is 413,
-// Then the function should return 413th"
 
-test("should return '111th' for 111", () => {
-  expect(getOrdinalNumber(111)).toEqual("111th");
-});
-test("should return '212th' for 212", () => {
-  expect(getOrdinalNumber(212)).toEqual("212th");
-});
-test("should return '413th' for 413", () => {
-  expect(getOrdinalNumber(413)).toEqual("413th");
-});
-
-//case 6
-// when is the number is 0 or negative number
-test("should throw Invalid Input for negative numbers", () => {
+test("should throw Invalid Input for negative numbers or 0", () => {
   expect(getOrdinalNumber(-5)).toEqual("Invalid Input");
-});
-test("should throw Invalid Input for 0", () => {
-  expect(getOrdinalNumber(0)).toEqual("Invalid Input");
+    expect(getOrdinalNumber(0)).toEqual("Invalid Input");
 });
