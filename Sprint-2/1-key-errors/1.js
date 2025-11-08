f4e38d3d3bde27ad2a45c00bab758b797104b830
Zadri <zadriabdule@gmail.com>
2025-11-08 22:20:51 +0000

    Fix and explain redeclaration SyntaxError in convertToPercentage (1-key-errors/1.js)

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,6 +2,7 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// I predict that this program will throw a error message  - due to the variable decimalNumber being redeclared
 
 // Try playing computer with the example to work out what is going on
 
@@ -15,6 +16,22 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
+// Function covertToPercentage(decimalNumber) - it has a parameter named decimalNumber
+// Inside the function: const decimalNumber = 0.5; - this redeclares the same variable name that's already used for the parameter.
+// That causes a error: SyntaxError: Identifier 'decimalNumber' has already been declared - because you can't declare a const (or let)
+// with the same name as a parameter inside the same function scope.
+// JavaScript doesn't allow us to declare a new variable with the same name in the same scope, so it caused a error
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
+
+// Function decimalNumber = 0.5
+// It calculates 0.5 * 100 = 50
+// It returns "50%"
+// console logs 50%
