63dc46fabc99956bb8c2a80600771245ad3acfc0
kohanman <kohanman@googlemail.com>
2025-11-08 14:43:43 +0000

    Sprint 3 | Coursework 1.2

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -8,9 +8,9 @@
 // write one test at a time, and make it pass, build your solution up methodically
 
 function isProperFraction(numerator, denominator) {
-  if (numerator < denominator) {
+  if (Math.abs(numerator) < Math.abs(denominator)) {
     return true;
-  }
+  } else return false;
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
@@ -46,14 +46,19 @@ assertEquals(improperFraction, false);
 // target output: true
 // Explanation: The fraction -4/7 is a proper fraction because the absolute value of the numerator (4) is less than the denominator (7). The function should return true.
 const negativeFraction = isProperFraction(-4, 7);
-// ====> complete with your assertion
+assertEquals(negativeFraction, true);
 
 // Equal Numerator and Denominator check:
 // Input: numerator = 3, denominator = 3
 // target output: false
 // Explanation: The fraction 3/3 is not a proper fraction because the numerator is equal to the denominator. The function should return false.
 const equalFraction = isProperFraction(3, 3);
-// ====> complete with your assertion
+assertEquals(equalFraction, false);
 
 // Stretch:
 // What other scenarios could you test for?
+const doublenegativeFraction = isProperFraction(-3, -5);
+assertEquals(doublenegativeFraction, true);
+
+const zeronumeratorFraction = isProperFraction(0, 4);
+assertEquals(zeronumeratorFraction, true);
