484da5d2d9295c086f14c762ef711c3ed293ca59
kohanman <kohanman@googlemail.com>
2025-11-08 16:07:28 +0000

    Sprint 3 | Coursework 2 - Get Ordinal Number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,23 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (num % 100 >= 11 && num % 100 <= 13) {
+    return `${num}th`;
+  }
+  const lastDigit = num % 10;
+  let suffix = "th";
+
+  switch (lastDigit) {
+    case 1:
+      suffix = "st";
+      break;
+    case 2:
+      suffix = "nd";
+      break;
+    case 3:
+      suffix = "rd";
+      break;
+  }
+
+  return `${num}${suffix}`;
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -4,10 +4,30 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
 
-// Case 1: Identify the ordinal number for 1
-// When the number is 1,
-// Then the function should return "1st"
-
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+});
+
+test('should return "2nd" for 2', () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+
+test('should return "13th" for 13', () => {
+  expect(getOrdinalNumber(13)).toEqual("13th");
+});
+
+test("should return '41st' for 41", () => {
+  expect(getOrdinalNumber(41)).toEqual("41st");
+});
+
+test('should return "212th" for 212', () => {
+  expect(getOrdinalNumber(212)).toEqual("212th");
+});
+
+test("should return '221st' for 221", () => {
+  expect(getOrdinalNumber(221)).toEqual("221st");
 });
