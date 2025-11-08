7fc5c19c13613cf4f7215d59ec5526104bca295b
zilinskyte <zilinskyterogers@gmail.com>
2025-11-08 00:49:40 +0000

    ordinal number function written and cases tested

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,12 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (num === 1) return "1st";
+  if (num === 2) return "2nd";
+  if (num === 3) return "3rd";
+  if (num === 11) return "11th";
+  if (num === 21) return "21st";
+  if (num === 22) return "22nd";
+  if (num === 23) return "23rd";
+  return num + "th";
 }
 
 module.exports = getOrdinalNumber;
