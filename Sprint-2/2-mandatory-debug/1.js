ff147212829be1b86b748dd7884224b880ce3154
Zadri <zadriabdule@gmail.com>
2025-11-09 12:46:43 +0000

    add prediction and explanation

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// I predict it will show a undefined error
 
 function sum(a, b) {
   return;
@@ -9,5 +10,11 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The problem was the semicolon after return, which made the function stop before adding the numbers
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b){
+  return a + b; // Once I put a + b on the same line as return, it worked
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
