2f8d74ec01ce01331759726975587c78a86d3d93
Zadri <zadriabdule@gmail.com>
2025-11-09 19:45:35 +0000

    add and explain calculateBMI() function to compute BMI to one decimal place

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -16,4 +16,11 @@
 
 function calculateBMI(weight, height) {
     // return the BMI of someone based off their weight and height
+    const bmi = weight / (height * height);
+    return bmi.toFixed(1);
 }
+
+console.log(`Your BMI is ${calculateBMI(68, 1.75)}`);
+// How it works: function calculateBMI(weight, height)
+// Parameters: weight - number, person's weight in kilograms height - number, person's height in meters 
+// Returns: the body mass index (BMI), rounded to one decimal place
