54ca144c558ab08b5e60b7b5e738c73cece48b9b
Zadri <zadriabdule@gmail.com>
2025-11-09 12:33:10 +0000

    add changes to 2-mandatory-debug/0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -13,7 +13,7 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
  function multiply(a, b) {
-      return a * b; // Once I put a * b on the same line as return, it worked
+      return a * b; 
 }
 
 console.log(` The sum of 10 and 32 is ${sum(10, 32)}`);
