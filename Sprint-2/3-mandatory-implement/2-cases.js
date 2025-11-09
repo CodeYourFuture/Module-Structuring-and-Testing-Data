c52e0875d5031e59995ccde9b0f638f97c4f3cfd
Zadri <zadriabdule@gmail.com>
2025-11-09 19:57:41 +0000

    add using String.toUpperCase() to convert greeting to uppercase

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,6 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+const greeting = "Hello there."
+
+console.log(greeting.toUpperCase());
