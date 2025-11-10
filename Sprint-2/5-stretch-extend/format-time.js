083a9d288378aecf19368945570ba8fb312813b8
Zadri <zadriabdule@gmail.com>
2025-11-10 17:13:46 +0000

    add and test formatAs12HourClock function with passing assertions

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -2,7 +2,7 @@
 // Make sure to do the prep before you do the coursework
 // Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
 
-function formatAs12HourClock(time) {
+ /*function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
   if (hours > 12) {
     return `${hours - 12}:00 pm`;
@@ -22,4 +22,55 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+); */
+
+// The function ignores the minutes and did'not handle midnight (00:00) or noon (12:00) properly. 
+ // Better version:
+ function formatAs12HourClock(time) {
+  const hours = Number(time.slice(0, 2));
+  const minutes = time.slice(3, 5);
+
+  if (hours == 0) return `12:${minutes} am`; 
+  if (hours < 12) return `${time} am`;
+  if (hours === 12) return `${time} pm`;
+
+  return `${hours - 12}:${minutes} pm`;
+}
+const currentOutput1 = formatAs12HourClock("08:00");
+const targetOutput1 = "08:00 am";
+console.assert(
+  currentOutput1 === targetOutput1,
+  `current output: ${currentOutput1}, target output: ${targetOutput1}`
 );
+// Test failed: due to a typo
+const currentOutput2 = formatAs12HourClock("23:00");
+const targetOutput2 = "11:00 pm";
+console.assert(
+  currentOutput2 === targetOutput2,
+  `current output: ${currentOutput2}, target output: ${targetOutput2}`
+
+);
+
+const currentOutput3 = formatAs12HourClock("13:00");
+const targetOutput3 = "1:00 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3} target output: {targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("15:00");
+const targetOutput4 = "3:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4} target output: {targetOutput4}`
+);
+
+console.log(formatAs12HourClock("08:00"));
+console.log(formatAs12HourClock("23:00"));
+console.log(formatAs12HourClock("15:00"));
+console.assert(formatAs12HourClock("00:00") === "12:00 am");
+console.assert(formatAs12HourClock("12:00") === "12:00 pm");
+
+// The test passed, so my function gave the expected results.
+// All the console.assert() tests passed.
+// formatAs12HourClock() function works correctly for the inputs I tested.
