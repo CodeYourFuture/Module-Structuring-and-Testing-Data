044b702ca0878bdb3332100738e094004fa0cd5b
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-10 23:42:23 +0000

    Adjust the output adding the percentage symbol

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -7,7 +7,7 @@ priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
 
-console.log(`The percentage change is ${percentageChange}`);
+console.log(`The percentage change is ${percentageChange}%`);
 
 // Read the code and then answer the questions below
 
@@ -38,15 +38,16 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // d) Identify all the lines that are variable declarations
 
-// These are the lines where variables are first introduced:
+// These are the lines that are variables declarations:
 // let carPrice = "10,000";
 // let priceAfterOneYear = "8,543";
 // const priceDifference = carPrice - priceAfterOneYear;
 // const percentageChange = (priceDifference / carPrice) * 100;
 
-// e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// e) Describe what the expression Number(carPrice.replaceAll(",", "")) is doing - what is the purpose of this expression?
 
 // carPrice.replaceAll(",", "") - removes all commas from the string "10,000", then it becomes "10000"
 // Number("10000") - Converts the string "10000" into a numeric value 10000.
-// Purpose: To convert a formatted currency string (with commas) into a number that can be used for mathematical calculations.
+// Purpose: To convert a formatted currency expression (with commas) into a number that is appropriate for mathematical calculations.
+//
 
