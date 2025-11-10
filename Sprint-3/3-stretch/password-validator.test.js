bc06097a155e4830eebefc0aebc2761a87f45a2f
Fithi Teklom <fthiteklom@gmail.com>
2025-11-10 14:39:51 +0000

    password-validator.test.js updated

--- Sprint-3/3-stretch/password-validator.test.js
+++ Sprint-3/3-stretch/password-validator.test.js
@@ -14,6 +14,35 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+const previousPasswords = [];//Create an array to store previous passwords
+
+function isValidPassword(password) {
+  // Rule 1: At least 5 characters
+  if (password.length < 5) return false;
+
+  // Rule 2: At least one uppercase letter
+  if (!/[A-Z]/.test(password)) return false;
+
+  // Rule 3: At least one lowercase letter
+  if (!/[a-z]/.test(password)) return false;
+
+  // Rule 4: At least one number
+  if (!/[0-9]/.test(password)) return false;
+
+  // Rule 5: At least one special symbol ! # $ % . * &
+  if (!/[!#$%.*&]/.test(password)) return false;
+
+  // Rule 6: Must not be a previous password
+  if (previousPasswords.includes(password)) return false;
+
+  // If all checks pass, save password and return true
+  previousPasswords.push(password);
+  return true;
+}
+
+module.exports = isValidPassword;
+
+
 const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
     // Arrange
