5dc0b964feac01fb3c0513278f60e960888cf29b
Fithi Teklom <fthiteklom@gmail.com>
2025-11-10 17:37:40 +0000

    1.js updated

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -12,7 +12,7 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 // =============> write your explanation here
 // it is undefined because it stopped reading at return;
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> write your new code here.
 function sum(a, b) {
   return a + b;
 }
