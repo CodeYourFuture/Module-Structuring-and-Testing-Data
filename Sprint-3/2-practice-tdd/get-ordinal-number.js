a1a791ebeba3f03e0c94b925b5718736879b16dd
UbunMen <mahtem23@gmail.com>
2025-11-12 17:53:35 +0000

    Function implemented

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,25 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (!Number.isInteger(num) || num <= 0) {
+    throw new Error("Only positive integers are allowed");
+  }
+  // Handle Exception of 11, 12, 13
+  if (num % 100 >= 11 && num % 100 <= 13) {
+    return num + "th";
+  }
+  // Handle general last-digit cases
+  switch (num % 10) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
 
+
 module.exports = getOrdinalNumber;
+
+// Function implemented 
