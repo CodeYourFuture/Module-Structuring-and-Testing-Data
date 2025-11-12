826a780eef21f0413de0a46b7ae14131229c7397
UbunMen <mahtem23@gmail.com>
2025-11-12 17:10:03 +0000

    Added different cases and test using npx jest

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -18,7 +18,51 @@ test("should count multiple occurrences of a character", () => {
 });
 
 // Scenario: No Occurrences
-// Given the input string str,
-// And a character char that does not exist within the case-sensitive str,
-// When the function is called with these inputs,
-// Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should return 0 when the character is not in the string", () => {
+  const str = "hello";
+  const char = "x";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// Scenario: Case Sensitivity
+
+test("should be case-sensitive when counting characters", () => {
+  const str = "Banana";
+  expect(countChar(str, "a")).toEqual(3);
+  expect(countChar(str, "A")).toEqual(0);
+});
+
+// Scenario: Empty String
+
+test("should return 0 when the input string is empty", () => {
+  const str = "";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// Scenario : Special characters
+
+test("should count special characters like spaces or punctuation", () => {
+  const str1 = "a b a b";
+  const char1 = " ";
+  expect(countChar(str1, char1)).toEqual(2);
+
+  const str2 = "NO !!!";
+  const char2 = "!";
+  expect(countChar(str2, char2)).toEqual(3);
+});
+
+// Scenario: Invalid inputs
+
+test("should throw error for invalid string input", () => {
+  expect(() => countChar(123, "a")).toThrow("Input should be a string");
+});
+
+test("should throw error for invalid character input", () => {
+  expect(() => countChar("hello", "ab")).toThrow("Input must be a single character");
+});
+
+
+// Added different cases and test using npx jest  
