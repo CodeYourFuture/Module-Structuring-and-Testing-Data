9c6744595cb6cfe4dff0c1b1f6d2b4756c3e6ef6
zilinskyte <zilinskyterogers@gmail.com>
2025-11-12 17:12:19 +0000

    Update tests for isProperFraction to include negative fractions in validation

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -2,14 +2,19 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
-test("should return true for a proper fraction", () => {
+test("should return true for a proper fraction, including negatives", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
+  expect(isProperFraction(-2, 3)).toEqual(true);
   expect(isProperFraction(4, -5)).toEqual(true);
+  expect(isProperFraction(-4, -5)).toEqual(true);
 });
 
 // Case 2: Identify Improper Fractions:
-test("should return false for an improper fraction", () => {
-  expect(isProperFraction(5, 4)).toEqual(false);
+test("should return false for an improper fraction, including negatives", () => {
+  expect(isProperFraction(5, 4)).toEqual(false); //  numerator greater than denominator
+  expect(isProperFraction(5, -4)).toEqual(false); // negative denominator
+  expect(isProperFraction(-5, 4)).toEqual(false); // negative numerator
+  expect(isProperFraction(-5, -4)).toEqual(false); // both negative 
 });
 
 // Case 3: Identify Negative Fractions:
