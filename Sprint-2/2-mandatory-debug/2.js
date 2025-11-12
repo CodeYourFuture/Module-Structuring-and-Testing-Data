767c15620c4c2fd9a0d3637f18c5ee948467693c
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-11-12 12:50:44 +0100

    solution exercise 3 mandatory debug

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,8 +1,7 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
-
+// =============> the slice index can't be negative 
 const num = 103;
 
 function getLastDigit() {
@@ -14,11 +13,23 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============>
+// The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> the constant was declared out of the function 
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============> 
+
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
