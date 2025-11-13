4f483dde6f632a3b0c8e3148d3bca864174e87c0
Fithi Teklom <fthiteklom@gmail.com>
2025-11-13 14:57:10 +0000

    modified PR

---
+++ Sprint-3/3-stretch/validateCreditCard.test.js
@@ -0,0 +1,20 @@
+test("valid credit card numbers should return true", () => {
+  expect(validateCreditCard("9999777788880000")).toBe(true);
+  expect(validateCreditCard("6666666666661666")).toBe(true);
+});
+
+test("invalid cards with letters should return false", () => {
+  expect(validateCreditCard("a92332119c011112")).toBe(false);
+});
+
+test("invalid cards with all same digits should return false", () => {
+  expect(validateCreditCard("4444444444444444")).toBe(false);
+});
+
+test("invalid cards with sum less than 16 should return false", () => {
+  expect(validateCreditCard("1111111111111110")).toBe(false);
+});
+
+test("invalid cards with odd final digit should return false", () => {
+  expect(validateCreditCard("6666666666666661")).toBe(false);
+});
