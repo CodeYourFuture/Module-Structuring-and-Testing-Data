9dc5687cde4d563074ad3b634a8140197236a25f
UbunMen <mahtem23@gmail.com>
2025-11-13 00:52:16 +0000

    Added explanation to the questions.

--- Sprint-3/3-stretch/find.js
+++ Sprint-3/3-stretch/find.js
@@ -19,7 +19,23 @@ console.log(find("code your future", "z"));
 // Use the Python Visualiser to help you play computer with this example and observe how this code is executed
 // Pay particular attention to the following:
 
-// a) How the index variable updates during the call to find
+// a) How the index variable updates during the call to find a char
+
+   // As the code runs, index starts from zero and runs until the length of the string, 
+   // It starts from zero and in each loop it gets incremented until a char is found within the string.
+
 // b) What is the if statement used to check
+   
+   // It is used to check if the char is in the string. If it exists.. next step is to return the char.     
+
+
 // c) Why is index++ being used?
+  
+  // To check if the given char is present starting from 0 till st.length.
+
+
 // d) What is the condition index < str.length used for?
+
+   // As long as index is less than the length of the string, keep looping and finding the char.
+
+// Added explanation to the questions.
