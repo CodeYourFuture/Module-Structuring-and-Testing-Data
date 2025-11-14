6a9a7cd64fa179f0cd188217f545b462b1a37630
Ali <naru.a@yahoo.com>
2025-11-14 21:07:26 +0000

    Enhance isProperFraction tests with detailed descriptions for clarity

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -2,12 +2,27 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
-test("should return true for a proper fraction", () => {
+test("should return true for a proper fraction (numerator < denominator)", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
 });
 
 // Case 2: Identify Improper Fractions:
+// When the numerator is greater than or equal to the denominator,
+// Then the function should return false
+test("should return false for an improper fraction (numerator > denominator)", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+// When the numerator is negative but absolute value < denominator,
+// Then the function should return true
+test("should return true for a negative proper fraction (|numerator| < denominator)", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+// When the numerator is equal to the denominator,
+// Then the function should return false
+test("should return false when numerator equals denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
