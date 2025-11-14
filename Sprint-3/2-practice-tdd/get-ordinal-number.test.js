2d04b9c94f7fd22906ba58a93e7601eb7481b225
Abrsh100 <abrdbr2008@gmail.com>
2025-11-14 00:22:04 +0000

    adding test to get-ordinal-number.test.js

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -11,5 +11,29 @@ test("append 'st' to numbers ending in 1, except those ending in 11", () => {
     expect( getOrdinalNumber(1) ).toEqual("1st");
     expect( getOrdinalNumber(21) ).toEqual("21st");
     expect( getOrdinalNumber(221) ).toEqual("221st");
+    expect( getOrdinalNumber(11) ).toEqual("11th");
+});
+
+test("append 'nd' to numbers ending in 2, except those ending in 12 ", () => {
+    expect( getOrdinalNumber(2) ).toEqual("2nd");
+    expect( getOrdinalNumber(22) ).toEqual("22nd");
+    expect( getOrdinalNumber(132) ).toEqual("132nd");
+    expect( getOrdinalNumber(12) ).toEqual("12th");
+});
+
+test("append 'rd' to numbers ending in 3, except those ending in 13", () => {
+    expect( getOrdinalNumber(3) ).toEqual("3rd");
+    expect( getOrdinalNumber(33) ).toEqual("33rd");
+    expect( getOrdinalNumber(133) ).toEqual("133rd");
+    expect( getOrdinalNumber(13) ).toEqual("13th");
+});
+
+test("appends 'th' to numbers ending in 4 - 9, and 0 ", () => {
+    expect(getOrdinalNumber(4)).toBe("4th");
+    expect(getOrdinalNumber(15)).toBe("15th");
+    expect(getOrdinalNumber(36)).toBe("36th");
+    expect(getOrdinalNumber(57)).toBe("57th");
+    expect(getOrdinalNumber(78)).toBe("78th");
+    expect(getOrdinalNumber(10)).toBe("10th");
 });
 
