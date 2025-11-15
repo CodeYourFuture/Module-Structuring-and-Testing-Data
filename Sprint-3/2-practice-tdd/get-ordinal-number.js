096d45cfeaf27f83452d56cbc5ceba26c0a1e609
Declan Williams <declanwilliams793@gmail.com>
2025-11-15 12:17:56 +0000

    fix: correct ordinal suffix for special cases in getOrdinalNumber function

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -4,7 +4,7 @@ function getOrdinalNumber(num) {
 
  // handles special cases like "11,12,13" to always end in the "Th"
  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13){
-  return num + "St";
+  return num + "th";
 }
  // will return "St" if the number ends in 1.
   if (lastDigit === 1){
