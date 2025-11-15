de4bc3e0ebf25c4bee26dd3570ad848cacf2a39d
UbunMen <mahtem23@gmail.com>
2025-11-15 19:25:48 +0000

    Password-validator implemented and tested.

--- Sprint-3/3-stretch/password-validator.js
+++ Sprint-3/3-stretch/password-validator.js
@@ -1,5 +1,12 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+    // return password.length < 5 ? false : true 
+   
+   return (password.length < 5 &&
+        /[A-Za-z]/.test(password)&&
+        /[! # $ % .* &]/.test(password)&&
+        /[0-9]/.test(password)&&
+        !previousPasswords.includes(password) 
+   );
 }
 
 
--- Sprint-3/3-stretch/password-validator.test.js
+++ Sprint-3/3-stretch/password-validator.test.js
@@ -15,6 +15,7 @@ To be valid, a password must:
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
 const isValidPassword = require("./password-validator");
+
 test("password has at least 5 characters", () => {
     // Arrange
     const password = "12345";
@@ -24,3 +25,56 @@ test("password has at least 5 characters", () => {
     expect(result).toEqual(true);
 }
 );
+
+// 
+test("password has at least one English uppercase letter (A-Z)", () => {
+    // Arrange
+    const password = "A2345";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+//
+test("password has least one English lowercase letter (a-z)", () => {
+    // Arrange
+    const password = "Da2345";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+// 
+test("password has at least one number 0-9)", () => {
+    // Arrange
+    const password = "Cz!345";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+test("password has at least one non-alphanumeric symbols: !, #, $, %, ., *, &)", () => {
+    // Arrange
+    const password = "Cz!345";
+    // Act
+    const result = isValidPassword(password);
+    // Assert
+    expect(result).toEqual(true);
+}
+);
+
+// 
+test("password must not be a previous password", () => {
+  const previousPasswords = ["Mmd1!", "XyZ2$", "Test10%"];
+  const password = "Mmd1"; 
+  const result = isValidPassword(password, previousPasswords);
+  expect(result).toEqual(false);
+});
+
+// Password-validator implemented and tested.
