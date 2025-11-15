ded405ec9b5625276faed0eb38c944ff265e7b15
Payman IB <payman.ib.com>
2025-11-15 15:29:41 +0000

    Implement repeat function with error handling for negative counts and add corresponding tests

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,16 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str , count) {
+  if (count < 0) {
+    return "Count must be a non-negative integer";
+  }
+  if (count === 0) {
+    return "";
+  } 
+  for (let i = 0; i < count; i++) { 
+    return str.repeat(count);
+  } 
 }
+  console.log(repeat("hello", -2));
+  
+
 
 module.exports = repeat;
