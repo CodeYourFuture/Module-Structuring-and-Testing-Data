4ea61d39f0ec7f29905479b1e4a6e0fd1ec85576
Payman IB <payman.ib.com>
2025-11-15 13:39:26 +0000

    Add tests for getCardValue function to cover number cards, face cards, Ace, and invalid ranks

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -1,5 +1,6 @@
 // This statement loads the getCardValue function you wrote in the implement directory.
 // We will use the same function, but write tests for it using Jest in this file.
+// const { test } = require("picomatch");
 const getCardValue = require("../implement/3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
@@ -8,6 +9,27 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return numeric value for number cards (2-10)", () => {
+  const fiveofHearts = getCardValue("5♥");
+  expect(fiveofHearts).toEqual(5);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards (J, Q, K) and 10", () => {
+  expect(getCardValue("10♦")).toEqual(10);
+  expect(getCardValue("J♣")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♠")).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace (A)", () => {
+  expect(getCardValue("A♦")).toEqual(11);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+  expect(() => {
+    getCardValue("1♠");
+  }).toThrow("Invalid card rank.");
+  expect(() => {
+    getCardValue("B♥");
+  }).toThrow("Invalid card rank.");
+});
