6106da7630fd31e2fe79aaa9ad42342017171cf4
Ali <naru.a@yahoo.com>
2025-11-16 15:08:20 +0000

    Refactor random number generation to use constants MIN and MAX, and enhance comments for clarity.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -1,34 +1,11 @@
-const minimum = 1;
-const maximum = 100;
+// MIN and MAX are constants that define the lower and upper bounds for our random number range.
+const MIN = 1;
+const MAX = 10;
 
-const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+// Math.random() returns a decimal value in the interval [0, 1)
+// Multiplying by (MAX - MIN + 1) scales the interval to [0, MAX - MIN + 1)
+// Applying Math.floor() converts the value to an integer within [s0, MAX - MIN]
+// Finally, adding MIN shifts the interval to [MIN, MAX], which is our desired result.
 
-// In this exercise, you will need to work out what num represents?
-
-//After running this exercise a few times I can see that 'num' is a random number between 1 and 100
-
+const num = MIN + Math.floor(Math.random() * (MAX - MIN + 1));
 console.log(num);
-
-// Try breaking down the expression and using documentation to explain what it means
-
-// at the top we can see our minimum and maximum values which are fixed therefore Constants,
-// we then have our variable num which is assigned a value using the Math object and its methods floor and random
-// Math.random() generates a random decimal number between 0 (inclusive) and 1 (exclusive)
-// We then multiply this random number by the range of our desired numbers which is (maximum - minimum + 1)
-
-// It will help to think about the order in which expressions are evaluated
-
-//the same as math, multiplication is done before addition and subtraction
-// so we first calculate (maximum - minimum + 1) which is (100 - 1 + 1) = 100
-// then we multiply the random decimal number by 100 which gives us a number between 0 and 100 (but not including 100)
-// then we apply Math.floor() to round down to the nearest whole number, giving us a number between 0 and 99
-// finally we add the minimum value (1) to shift the range up, resulting in a final value between 1 and 100 (inclusive)
-
-// So in summary, 'num' is a random integer between 1 and 100, inclusive of both endpoints.
-
-
-// Try logging the value of num and running the program several times to build an idea of what the program is doing
-
-//you can see how random function generates different numbers each time the program is run
-//and how the range is always between 1 and 100
-
