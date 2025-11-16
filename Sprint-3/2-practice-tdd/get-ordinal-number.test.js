6993bb1559c6e3b72247aad5c1aff13be5aed8a5
UbunMen <mahtem23@gmail.com>
2025-11-16 16:22:20 +0000

    Modified the test into groups

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -8,39 +8,57 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-// Case 1: 1 → Standard ordinals
+// Case 1: Numbers ending in 1 except 11
 
-test("should return correct ordinals for 1, 2, 3, 4, 21, 22, 23, 24", () => {
-  expect(getOrdinalNumber(1)).toEqual("1st");
-  expect(getOrdinalNumber(2)).toEqual("2nd");
-  expect(getOrdinalNumber(3)).toEqual("3rd");
-  expect(getOrdinalNumber(4)).toEqual("4th");
-  expect(getOrdinalNumber(21)).toEqual("21st");
-  expect(getOrdinalNumber(22)).toEqual("22nd");
-  expect(getOrdinalNumber(23)).toEqual("23rd");
-  expect(getOrdinalNumber(24)).toEqual("24th");
+test("should return 'st' for numbers ending in 1 except 11", () => {
+  const values = [1, 21, 31, 101, 1001];
+  values.forEach(num => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}st`);
+  });
 });
 
-// Case 2: Exceptions 11, 12, 13
-test("should handle exceptions 11, 12, 13 correctly", () => {
-  expect(getOrdinalNumber(11)).toEqual("11th");
-  expect(getOrdinalNumber(12)).toEqual("12th");
-  expect(getOrdinalNumber(13)).toEqual("13th");
-  expect(getOrdinalNumber(111)).toEqual("111th"); 
+// Case 2: Numbers ending in 2 except 12
+
+test("should return 'nd' for numbers ending in 2 except 12", () => {
+  const values = [2, 22, 32, 102, 1002];
+  values.forEach(num => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}nd`);
+  });
 });
 
-// Case 3: Large numbers
-test("should handle larger numbers correctly", () => {
-  expect(getOrdinalNumber(101)).toEqual("101st");
-  expect(getOrdinalNumber(112)).toEqual("112th"); 
-  expect(getOrdinalNumber(123)).toEqual("123rd");
+// Case 3: Numbers ending in 3 except 13
+
+test("should return 'rd' for numbers ending in 3 except 13", () => {
+  const values = [3, 23, 33, 103, 1003];
+  values.forEach(num => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}rd`);
+  });
 });
 
-// Case 4: Invalid cases / inputs 
+// Case 4: Exceptions 11, 12 & 13 
+
+test("should return 'th' for 11, 12, 13 regardless of other digits", () => {
+  const values = [11, 12, 13, 111, 212, 513];
+  values.forEach(num => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}th`);
+  });
+});
+
+// Case 5: Numbers ending in 0 and (4-9)
+
+test("should return 'th' for numbers ending in 0 or 4–9", () => {
+  const values = [4, 5, 6, 7, 8, 9, 10, 20, 25, 100, 104, 1009];
+  values.forEach(num => {
+  expect(getOrdinalNumber(num)).toEqual(`${num}th`);
+  });
+});
+
+// Case 6: Invalid cases / inputs 
 test("should throw error for zero, negative or non-integer inputs", () => {
   expect(() => getOrdinalNumber(0)).toThrow("Only positive integers are allowed");
   expect(() => getOrdinalNumber(-5)).toThrow("Only positive integers are allowed");
   expect(() => getOrdinalNumber(2.5)).toThrow("Only positive integers are allowed");
 });
 
-// Added possible and invalid cases and tested using npx jest  
+// Added possible and invalid cases and tested using npx jest 
+// Modified the test into groups  
