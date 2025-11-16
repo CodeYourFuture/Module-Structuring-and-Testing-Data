a33085b30964b7aa76c3e1be3418de248381efdb
Ali <naru.a@yahoo.com>
2025-11-16 14:59:43 +0000

    Restore original file contents

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,28 +1,13 @@
 // Predict and explain first...
-//  =============> write your prediction here:
-// I think their will be an error because of the str tag
-
-
-
-
+//  =============> write your prediction here
 
 // call the function capitalise with a string input
-console.log(capitalise("hello")); // should print "Hello"
-
-
 // interpret the error message and figure out why an error is occurring
 
+function capitalise(str) {
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
 
-//function capitalise(str) {
-  //let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  //return str;
-//}
-
-// =============> write your explanation here:
-// str is being used as both the function parameter and a variable inside the function, causing a conflict.
-
-
+// =============> write your explanation here
 // =============> write your new code here
-
-function capitalise(inputStr) {
-  return `${inputStr[0].toUpperCase()}${inputStr.slice(1)}`;}
