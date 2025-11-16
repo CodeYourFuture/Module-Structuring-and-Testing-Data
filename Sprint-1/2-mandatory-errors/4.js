b401592b6f34885b8fae519164f6aa26b30b7320
Ali <naru.a@yahoo.com>
2025-11-16 15:03:44 +0000

    Rename variables to follow camelCase convention in 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,5 +1,6 @@
-const Twelve_HourClockTime = "20:53";
-const Twenty_Four_hourClockTime = "08:53";
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
 
 //Variables cannot have numbers at the start of their name
 //the times are in the wrong format for their variable names
