5233e20d42cf938e537c89ea8dbf263a0a7b9efe
Imran Mohamed <info@imranbmohamed.com>
2025-11-18 15:38:10 +0000

    refactor countChar and repeat functions to improve argument validation and error handling

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,22 +1,28 @@
 function countChar(stringOfCharacters, findCharacter) {
+
   if (stringOfCharacters === undefined || findCharacter === undefined) {
     throw new Error(
       "Function requires exactly two arguments: a string and a character to find."
     );
   }
+
   if (typeof stringOfCharacters !== 'string'){
     throw new Error("First argument must be a string.");
   }
+
   if (typeof findCharacter !== "string") {
     throw new Error("Second argument must be a string.");
   }
+
   if (findCharacter.length !== 1) {
     throw new Error("Character to find must be a single character.");
   } 
+
   if (!stringOfCharacters.length) {
     return 0;
-  }  
-  return Array.from(stringOfCharacters).filter(char => char === findCharacter).length;
+  }
+
+  return [...stringOfCharacters].filter(char => char === findCharacter).length;
 }
 
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,14 +1,18 @@
 function repeat(str, count) {
+
   if ( arguments.length !== 2) {
     throw new Error(`Function requires exactly two arguments: a string and a count. Received ${arguments.length} arguments`);
     
   }
+
   if (typeof str !== "string") {
     throw new Error("First argument must be a string. Received type " + typeof str);
   }
+
   if (!Number.isInteger(count) || count < 0) {
     throw new Error("Second argument must be a non-negative integer. Received " + count  );
   }
+  
   return str.repeat(count);
 }
 
