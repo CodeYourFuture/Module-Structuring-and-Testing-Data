f04aba11a1ce65966429afc17e2faf5316f51fd4
asaniDev <asanidev9@gmail.com>
2025-11-19 18:27:42 +0000

    made the function more generic to cover all numbers and added more tests to check that different numbers work

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,14 +1,33 @@
 function getOrdinalNumber(num) {
-  if (num === 1) {
-    return "1st";
-  } else if (num === 2) {
-    return "2nd";
-  } else if (num === 3) {
-    console.log(num);
-    return "3rd";
-  } else if (num > 3 || num < 21) {
-    return `${num}th`;
+  const lastDigit = num.toString()[num.toString().length - 1];
+
+  if (lastDigit === "1") {
+    if (num === 11) {
+      return `${num}th`;
+    }
+
+    return `${num}st`;
+  } else if (lastDigit === "2") {
+    if (num.toString().length > 1) {
+      const last2Digits = num.toString().slice(-2);
+
+      if (last2Digits === "12") {
+        return `${num}th`;
+      }
+    }
+    return `${num}nd`;
+  } else if (lastDigit === "3") {
+    if (num.toString().length > 1) {
+      const last2Digits = num.toString().slice(-2);
+
+      if (last2Digits === "13") {
+        return `${num}th`;
+      }
+    }
+    return `${num}rd`;
   }
+
+  return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -8,18 +8,34 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
+test("should append 'st' to numbers with 1 at the end except for those ending with 11", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(101)).toEqual("101st");
+  expect(getOrdinalNumber(151)).toEqual("151st");
+  expect(getOrdinalNumber(2061)).toEqual("2061st");
 });
 
-test("should return '2nd' for 2", () => {
+test("should append 'nd' to numbers with 2 at the end except for those ending with 12", () => {
   expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(342)).toEqual("342nd");
+  expect(getOrdinalNumber(592)).toEqual("592nd");
+  expect(getOrdinalNumber(1972)).toEqual("1972nd");
 });
 
-test("should return '3rd' for 3", () => {
+test("should append 'rd' to numbers with 3 at the end except for those ending with 13", () => {
   expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(33)).toEqual("33rd");
+  expect(getOrdinalNumber(353)).toEqual("353rd");
+  expect(getOrdinalNumber(93)).toEqual("93rd");
+  expect(getOrdinalNumber(783)).toEqual("783rd");
 });
 
-test("should return any number between 4 and 20 with a suffix of 'th' at end of number", () => {
+test("should append 'th' to all other numbers which do not end in 1,2,3,11,12 or 13", () => {
   expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(212)).toEqual("212th");
+  expect(getOrdinalNumber(113)).toEqual("113th");
+  expect(getOrdinalNumber(17)).toEqual("17th");
 });
