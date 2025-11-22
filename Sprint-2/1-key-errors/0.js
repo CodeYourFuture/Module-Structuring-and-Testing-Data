9f2702f8090fb9135d2995a26369bdfdb52ad9a2
Payman IB <payman.ib.com>
2025-11-22 14:33:15 +0000

    removed sprint 2 from this branch

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+// I gives us a syntax error as str declared before.
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+// return str;
+// }
 
-// =============> write your explanation here
-// =============> write your new code here;
+// =============> write your explanation here :
+// as the variable in side the function has the same name as the function variable.
+// =============> write your new code here
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
+console.log(capitalise("abcd"));
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// there is an error on line 5 and 6  
+//function sum(a, b) {
+// //return;
+//  a + b;
+// }
 
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+//the return statement is not returning any value. a+b mentioned after return.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
