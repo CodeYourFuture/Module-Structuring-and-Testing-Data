78f21df9609f05843fdff84ddcf9c5ff9809eb06
asaniDev <asanidev9@gmail.com>
2025-11-22 14:40:27 +0000

    revert changes to sprint 2 folder

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,23 +1,13 @@
 // Predict and explain first...
 //  =============> write your prediction here
-//we should have an error showing that str already exists so we cannot declare it again inside the function.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-//function capitalise(str) {
-//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-//  return str;
-//}
-
-//console.log(capitalise("cat"));
-
-// =============> write your explanation here
-//str has been passed down as a parameter so we cannot declare it again. we would need to declare a different variable name.
-// =============> write your new code here
 function capitalise(str) {
-  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return capitalisedStr;
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
 }
 
-console.log(capitalise("cat"));
+// =============> write your explanation here
+// =============> write your new code here
