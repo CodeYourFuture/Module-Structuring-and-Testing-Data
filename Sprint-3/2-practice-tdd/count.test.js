213865e13a116347e3d8759d2f674aad8c09e5cb
Abayie <jonathanboahene@gmail.com>
2025-11-24 00:02:15 +0000

    practice tdd count

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -10,6 +10,7 @@ const countChar = require("./count");
 // When the function is called with these inputs,
 // Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
 
+
 test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
   const char = "a";
@@ -22,3 +23,17 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("should return 0 when character does not exist in the string", () => {
+  const str = "hello world";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+
+// Add case sensitivity tests
+test("should be case-sensitive", () => {
+  expect(countChar("AaAa", "A")).toEqual(2);
+  expect(countChar("AaAa", "a")).toEqual(2);
+});
