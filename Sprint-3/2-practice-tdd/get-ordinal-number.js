992821e2cae1639bf061d48184ebc987c3e5b246
Abayie <jonathanboahene@gmail.com>
2025-11-24 00:13:05 +0000

    practice tdd get ordinal number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,18 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastTwo = num % 100;
+  const last = num % 10;
+
+  // Special exceptions: 11, 12, 13
+  if (lastTwo === 11 || lastTwo === 12 || lastTwo === 13) {
+    return `${num}th`;
+  }
+
+  // Regular cases
+  if (last === 1) return `${num}st`;
+  if (last === 2) return `${num}nd`;
+  if (last === 3) return `${num}rd`;
+
+  return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
