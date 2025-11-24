b90afe291a67928934ac79c418a14b39383e2d35
Abayie <jonathanboahene@gmail.com>
2025-11-24 00:35:01 +0000

    practice tdd repeat

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,13 +1,36 @@
-function repeat(str, count) {
-  if (count < 0) {
-    throw new Error("Count must be non-negative");
-  }
-
-  let result = "";
-  for (let i = 0; i < count; i++) {
-    result += str;
-  }
-  return result;
+function repeat() {
+  if (arguments[1] < 0) throw new Error("Count must be non-negative");
+  return arguments[0].repeat(arguments[1]);
 }
+//other implementation
+// function repeat(str, count) {
+//   if (count < 0) {
+//     throw new Error("Count must be non-negative");
+//   }
 
-module.exports = repeat;
+//   let result = "";
+//   for (let i = 0; i < count; i++) {
+//     result += str;
+//   }
+//   return result;
+// }
+
+// module.exports = repeat;
+
+// function repeat() {
+//   const str = arguments[0];
+//   const count = arguments[1];
+
+//   if (count < 0) {
+//     throw new Error("Count must be non-negative");
+//   }
+
+//   let output = "";
+//   for (let i = 0; i < count; i++) {
+//     output += str;
+//   }
+
+//   return output;
+// }
+
+// module.exports = repeat;
