4c7828ee3825d957744f683a57cb1da1691d1fac
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-01 14:31:36 +0000

    complete 2-is-proper-fraction with jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,18 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative fraction", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+// const equalFraction = isProperFraction(3, 3);
+// assertEquals(equalFraction, false);
+test("should return false when numerator and denominator are equal", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
