0110eec5610080453e93e0310c95ce6b6b4a2d9b
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-01 14:46:38 +0000

    complete 3-get-card-value with jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -3,11 +3,47 @@
 const getCardValue = require("../implement/3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-  const aceofSpades = getCardValue("A♠");
-  expect(aceofSpades).toEqual(11);
+  const aceOfSpades = getCardValue("A♠");
+  expect(aceOfSpades).toEqual(11);
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return same number for number cards", () => {
+  const cards = [
+    ["2♥", 2],
+    ["3♣", 3],
+    ["4♦", 4],
+    ["5♠", 5],
+    ["6♥", 6],
+    ["7♣", 7],
+    ["8♦", 8],
+    ["9♠", 9],
+    ["10♥", 10],
+  ];
+
+  for (const [card, expectedValue] of cards) {
+    expect(getCardValue(card)).toEqual(expectedValue);
+  }
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
-// Case 4: Handle Ace (A):
+test("should return 10 for face cards", () => {
+  const cards = ["J♦", "K♥", "Q♠"];
+
+  for (const card of cards) {
+    expect(getCardValue(card)).toEqual(10);
+  }
+});
+
+// // Case 4: Handle Ace (A):
+test("should return 11 for Ace of Diamonds", () => {
+  const aceOfDiamonds = getCardValue("A♦");
+  expect(aceOfDiamonds).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should throw error for invalid card rank", () => {
+  expect(() => {
+    getCardValue("A");
+  }).toThrow("Invalid card rank.");
+});
