cb9b03ece58d9d4de7e209fa42b44db3553396b7
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-01 16:28:44 +0000

    implement getOrdinalNumber function and add tests

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,21 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const remainderTen = num % 10;
+  const remainderHundred = num % 100;
+
+  if (remainderHundred >= 11 && remainderHundred <= 13) {
+    return `${num}th`;
+  }
+
+  switch (remainderTen) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
+  }
 }
 
 module.exports = getOrdinalNumber;
