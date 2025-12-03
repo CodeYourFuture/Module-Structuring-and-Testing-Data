8bcfee08b72e401ea576a301767b7da22048a17c
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-03 20:27:54 +0000

    refactor(tests): improve structure of ordinal number tests

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -1,41 +1,34 @@
 const getOrdinalNumber = require("./get-ordinal-number");
-// In this week's prep, we started implementing getOrdinalNumber
 
-// continue testing and implementing getOrdinalNumber for additional cases
-// Write your tests using Jest - remember to run your tests often for continual feedback
-
-// Case 1: Identify the ordinal number for 1
-// When the number is 1,
-// Then the function should return "1st"
-
-test("should return '1st' for 1", () => {
+// Test for numbers ending in 1, except those ending in 11
+test("append 'st' to numbers ending in 1, except those ending in 11", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(31)).toEqual("31st");
 });
 
-test("should return '2nd' for 2", () => {
+// Test for numbers ending in 2, except those ending in 12
+test("append 'nd' to numbers ending in 2, except those ending in 12", () => {
   expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(132)).toEqual("132nd");
 });
 
-test("should return '3rd' for 3", () => {
+// Test for numbers ending in 3, except those ending in 13
+test("append 'rd' to numbers ending in 3, except those ending in 13", () => {
   expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(133)).toEqual("133rd");
 });
 
-test("should return '4th' for 4", () => {
+// Test for numbers ending in 0, 4, 5, 6, 7, 8, 9, and those ending in 11, 12, 13
+test("append 'th' to numbers ending in 0, 4, 5, 6, 7, 8, 9, and those ending in 11, 12, 13", () => {
   expect(getOrdinalNumber(4)).toEqual("4th");
-});
-
-test("should return '11th' for 11", () => {
   expect(getOrdinalNumber(11)).toEqual("11th");
-});
-
-test("should return '12th' for 12", () => {
   expect(getOrdinalNumber(12)).toEqual("12th");
-});
-
-test("should return '13th' for 13", () => {
   expect(getOrdinalNumber(13)).toEqual("13th");
-});
-
-test("should return '111th' for 111", () => {
+  expect(getOrdinalNumber(14)).toEqual("14th");
+  expect(getOrdinalNumber(20)).toEqual("20th");
+  expect(getOrdinalNumber(100)).toEqual("100th");
   expect(getOrdinalNumber(111)).toEqual("111th");
 });
