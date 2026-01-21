b1ecc00a867eed03e8c1dfa0842c68e837eed108
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-21 21:23:00 +0000

    Fix typo in priceAfterOneYear conversion and update comments for clarity

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,21 +2,36 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ,""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
 
 console.log(`The percentage change is ${percentageChange}`);
 
+
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+    // there are 5 function calls :
+    // carPrice = Number(carPrice.replaceAll(",", ""));
+    // priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+    // console.log(`The percentage change is ${percentageChange}`);
+
+
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+    //the error appears on line 5 because of a missing coma between the arguments, fixed by adding a coma
 
 // c) Identify all the lines that are variable reassignment statements
+    // carPrice = Number(carPrice.replaceAll(",", ""));
+    // priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // d) Identify all the lines that are variable declarations
+    // let carPrice = "10,000";
+    // let priceAfterOneYear = "8,543";
+    // const priceDifference = carPrice - priceAfterOneYear;
+    // const percentageChange = (priceDifference / carPrice) * 100;
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+    //it get's rid of the coma and turns the string in to a number
