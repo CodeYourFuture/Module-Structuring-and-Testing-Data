f7799bba4f89900b6a526c62aa2480c1e4314dd3
Karla G. <k.grajalesluna@gmail.com>
2026-01-22 22:53:03 +0000

    feat: extract root, filename and extension using slice and lastIndexOf

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -12,7 +12,23 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
-console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+const lastDot = filePath.lastIndexOf(".");
+console.log(`The indexDot is ${lastDot}`);
+
+const ext = filePath.slice(lastDot);
+
+const fileName = filePath.slice(lastSlashIndex + 1, lastDot);
+const dir = filePath.slice(1, lastSlashIndex + 1);
+
+
+
+console.log(`The dir is 👉 ${dir}`);
+console.log(`The nameFile is 👉 ${fileName}`);
+console.log(`The extension is 👉 ${ext}`);
+
+
+
