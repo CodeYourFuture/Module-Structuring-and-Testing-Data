377dde2c69bffa0450d5be3da26ce374dc824c93
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-22 16:34:31 +0000

    fixed the function to return the sum of the 2 parameters

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here:  the log will be The sum of 10 and 32 is undefined
 
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here: because of the ";" immediately after return the function doesn't get to a + b; and returns undefined
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
