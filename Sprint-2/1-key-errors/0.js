bcb05251587adc2ff9d72d532482809b9a78c144
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-22 16:03:10 +0000

    fixed variable naming issue

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here : we'll get an error because a parameter can not be declared as a variable also, inside the function
+
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// =============> write your explanation here: Identifier 'str' has already been declared
+// =============> write your new code here:
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+
