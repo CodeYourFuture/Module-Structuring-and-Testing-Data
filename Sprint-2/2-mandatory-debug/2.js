0f130076f09ec1b7da5335020ed547b73cbca716
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-01-24 21:33:23 +0000

    Completed exercise 2 fully

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,23 +2,39 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// The function will always return '3' because it uses the global variable num instead of the input parameter.
 
 const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
+// Explain why the output is the way it is
+// =============> write your explanation here
+// The function getLastDigit does not use its input parameter. Instead it always converts the global
+// variable num, which is 103 to a string and returns its last character which is '3'.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
