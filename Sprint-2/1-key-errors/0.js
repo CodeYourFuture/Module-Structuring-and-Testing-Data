7e9bb599b678932bd80cea30b9394acc6b8ad71d
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-01-24 21:15:47 +0000

    Completed exercise 1

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,21 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// console.log(capitalise("lowercase"));
 
 // =============> write your explanation here
+// The error states str has already been declared. This is because str is the parameter and within the
+// function it is trying to declare a new variable with the same name using let.
+
 // =============> write your new code here
+function capitalise(str) {
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
+}
+
+console.log(capitalise("lowercase"));
