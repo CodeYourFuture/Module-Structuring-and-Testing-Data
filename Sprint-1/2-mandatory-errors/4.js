924305606a32677c99a7aac86a227661ff7411e9
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-01-25 18:10:32 +0000

    Completed section 2

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+// Error is because variable names cannot start with a number.
+
+const hour12ClockTime = "20:53";
+const hour24ClockTime = "08:53";
+console.log(hour12ClockTime);
+console.log(hour24ClockTime);
