80311093d4c158cbc7332a0e232b972989b9de77
Mohsen Zamani <mohsenzamanist@gmail.com>
2026-01-28 13:48:24 +0000

    Complete sprint-3-stretch

--- Sprint-3/4-stretch/password-validator.js
+++ Sprint-3/4-stretch/password-validator.js
@@ -1,6 +1,12 @@
+const previousPasswords = ["5B43n21"];
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
-}
+  if (password.length < 5) return false;
+  if (previousPasswords.includes(password)) return false;
+  previousPasswords.push(password);
 
+  const rules = [/[A-Z]/, /[a-z]/, /[0-9]/, /[!#$%.*&]/];
+
+  return rules.every((rule) => rule.test(password));
+}
 
 module.exports = passwordValidator;
