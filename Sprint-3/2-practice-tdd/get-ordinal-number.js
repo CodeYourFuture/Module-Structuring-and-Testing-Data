f1bb8edebb35acce8b5516bc565e82cb22c69790
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-01-28 00:44:14 +0000

    Completed all exercises in 2-practice-tdd directory

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,15 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  let stringNum = num.toString();
+  let lastDigit = stringNum[stringNum.length - 1];
+  if (lastDigit === "1" && num % 100 !== 11) {
+    return stringNum + "st";
+  } else if (lastDigit === "2" && num % 100 !== 12) {
+    return stringNum + "nd";
+  } else if (lastDigit === "3" && num % 100 !== 13) {
+    return stringNum + "rd";
+  } else {
+    return stringNum + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,12 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count === 0) {
+    return "";
+  } else if (count > 0) {
+    return str.repeat(count);
+  } else {
+    throw new Error("Count must be a non-negative integer");
+  }
+  return "";
 }
 
 module.exports = repeatStr;
