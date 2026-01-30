c4aa0c2d0a5b0137e207cf57d4923f1e528f2f79
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-01-30 22:24:56 +0000

    Added step by step breakdown of random number generation

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -1,9 +1,20 @@
 const minimum = 1;
 const maximum = 100;
 
-const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum; // Generates a random whole number between minimum and maximum
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Generate a random decimal number between 0 and 1
+const decimal = Math.random();
+// Calculate the range size
+const range = maximum - minimum + 1;
+// Scale the decimal to the range
+const scaled = decimal * range;
+// Round down to get a whole number
+const floored = Math.floor(scaled);
+// Shift the number up to start at the minimum
+const result = floored + minimum;
