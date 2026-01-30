0ae5f4b568265282d98375ddf342a0f4e44ef683
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-30 10:46:13 +0000

    updated getAngleType to check for acute angle

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -11,6 +11,9 @@ function getAngleType(angle) {
   if (angle === 90) {
     return "Right angle";
   }
+  if(angle<90){
+    return "Acute angle"
+  }
    // Run the tests, work out what Case 2 is testing, and implement the required code here.
    // Then keep going for the other cases, one at a time.
 }
