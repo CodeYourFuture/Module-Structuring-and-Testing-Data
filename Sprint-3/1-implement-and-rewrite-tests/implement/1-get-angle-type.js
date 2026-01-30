dc90d4261461d7ab8503cdbc165c4909be165292
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-30 10:49:14 +0000

    wrote test to check for obtuse angle and implemented the function to pass the test

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -14,6 +14,9 @@ function getAngleType(angle) {
   if(angle<90){
     return "Acute angle"
   }
+  if(angle>90 && angle<180){
+    return "Obtuse angle"
+  }
    // Run the tests, work out what Case 2 is testing, and implement the required code here.
    // Then keep going for the other cases, one at a time.
 }
@@ -53,6 +56,7 @@ assertEquals(acute, "Acute angle");
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
 const obtuse = getAngleType(120);
+assertEquals(obtuse,"Obtuse angle")
 // ====> write your test here, and then add a line to pass the test in the function above
 
 // Case 4: Identify Straight Angles:
