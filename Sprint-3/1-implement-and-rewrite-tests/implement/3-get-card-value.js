9f3ed4fc1ef08c95d3cbea56cf888626776ffba5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-31 08:03:09 +0000

    assigned rang with the value of card[0]

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -8,9 +8,11 @@
 // write one test at a time, and make it pass, build your solution up methodically
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
 function getCardValue(card) {
-  if (rank === "A") {
+  let rank=card[0]
+  if (rank=== "A") {
     return 11;
   }
+  
 }
 
 // The line below allows us to load the getCardValue function into tests in other files.
@@ -31,14 +33,15 @@ function assertEquals(actualOutput, targetOutput) {
 // Given a card string in the format "A♠" (representing a card in blackjack - the last character will always be an emoji for a suit, and all characters before will be a number 2-10, or one letter of J, Q, K, A),
 // When the function getCardValue is called with this card string as input,
 // Then it should return the numerical card value
-const aceofSpades = getCardValue("A♠");
-assertEquals(aceofSpades, 11);
+const aceOfSpades = getCardValue("A♠");
+assertEquals(aceOfSpades, 11);
 
 // Handle Number Cards (2-10):
 // Given a card with a rank between "2" and "9",
 // When the function is called with such a card,
 // Then it should return the numeric value corresponding to the rank (e.g., "5" should return 5).
-const fiveofHearts = getCardValue("5♥");
+const fiveOfHearts = getCardValue("5♥");
+assertEquals(fiveOfHearts,5)
 // ====> write your test here, and then add a line to pass the test in the function above
 
 // Handle Face Cards (J, Q, K):
