0492fb431272408c0e9cb0f02649b5d416e6ba78
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-01 15:56:36 +0000

    Add additional test cases for isProperFraction function to cover improper fractions, negative fractions, and equal numerator and denominator scenarios

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,19 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for improper fraction",()=>{
+  expect(isProperFraction(3,2)).toEqual(false)
+})
 
 // Case 3: Identify Negative Fractions:
+test("should return true when the absolute value of the numerator is smaller than the absolute value of the denominator",()=>{
+  expect(isProperFraction(-2,9)).toEqual(true);
+  expect(isProperFraction(2,-9)).toEqual(true)
+  expect(isProperFraction(-2,-9)).toEqual(true)
+})
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator ",()=>{
+  expect(isProperFraction(1,1)).toEqual(false)
+  expect(isProperFraction(-1, 1)).toEqual(false);
+})
