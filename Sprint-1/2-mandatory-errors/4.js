1631f4ed9b9b68da1b87cce940ece19639937481
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-02 04:39:11 +0000

    Fixed invalid variable names in 4.js and replaced smart quotes with valid JavaScript strings

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// Prediction: The code will fail because variable names cannot start with a number.
+// JavaScript identifiers must begin with a letter, $, or _. Starting with a 12 or 24 is invalid.
+// The quotes used are also smart quotes which will cause a syntax error.
+
+// Corrected code:
+
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
