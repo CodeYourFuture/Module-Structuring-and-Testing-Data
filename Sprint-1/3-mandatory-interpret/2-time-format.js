376454ff9095a1c4b18613845a889212e69eacbb
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-02 16:54:11 +0000

    Completed tasks a-f with explanations and test analysis

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,33 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// movieLength, remainingSeconds, totalMinutes, remainingMinutes, totalHours, result
+// Answer: Each uses const, so each counts as a declaration. There are 6 variable declarations in total.
 
 // b) How many function calls are there?
+// console.log(result)
+// Answer: There is 1 function call in this program. Everything else is arithmetic, not a function call.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+// According to the documentation, the remainder operator (%) returns the remainder after dividing one number by another.
+// In this case, movieLength % 60 divides movieLength (which is in seconds) by 60, 
+// returns the leftover seconds that don't fit into a full minute.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// This line removes the leftover seconds (remainingSeconds) and converts the remaining whole seconds into minutes. 
+// So it gives you the total number of whole minutes in the movie.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+// The result variable represents the final formatted time of the movie in hours, minutes and seconds.
+// hours:minutes:seconds format. A better name could be formattedTime or movieDuration.
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// If I set movieLength to 8784, the output will be "2:26:24", which is correct, 
+// so this is a normal case where the code works as expected.
+
+// If I set movieLength to 0, the output will be "0:0:0", which is also correct, 
+// shows it can handle edge cases even though it is an unusual value.
+
+// If I set movieLength to -200, the output will be "-1:-3:-20", which is not a valid time format, 
+// shows that the logic breaks because the code does not protect against invalid negative inputs.
