afc243013be0ebc40802c8fc205fe1689c5df1f8
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-02-04 12:10:10 +0000

    Solved the mandatory errors

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+// a variable name's first letter cannot start with a number
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
