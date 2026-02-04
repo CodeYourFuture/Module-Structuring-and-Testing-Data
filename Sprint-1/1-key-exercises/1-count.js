de40d9d693bee3dc698502cbd98e5286f938b6ba
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-02-04 19:48:46 +0000

    complete count.js exercise

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 reassigns the value of the variable count. For this, the assignment operator = is used. The new value of count will be an expression of count + 1.
