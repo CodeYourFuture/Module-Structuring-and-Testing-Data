9c357138fa55956c2be5d7d97d90c22031540a1e
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-06 16:45:56 +0200

    Solution: mandatory-interpret, 3-to-pounds

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -23,5 +23,23 @@ console.log(`£${pounds}.${pence}`);
 // You need to do a step-by-step breakdown of each line in this program
 // Try and describe the purpose / rationale behind each step
 
-// To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// Step 1: initialize the string variable penceString with the value "399p"
+// const penceString = "399p";
+
+// Step 2: Remove the "p" from the end of "399p" and store the value to penceStringWithoutTrailingP
+// const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+
+// Step 3: The padStart method is used to ensure that the paddedPenceNumberString string variable has at least 3
+// characters, padding it with "0" at the start if necessary. Since paddedPenceNumberString is already 3 characters
+// long, no padding is added.
+// const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+// Step 4: The substring method is used to extract the pounds part of the price and store it in the variable pounds.
+// const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+
+// Step 5: The substring method is used to extract the pence part of the price and store it in the variable pence.
+// It uses the padEnd method to ensure that the pence string has at least 2 characters, padding it with "0" at the end if necessary.
+// const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+
+// Step 6: The console.log method is used to output the final price in pounds and pence format.
+// console.log(`£${pounds}.${pence}`);
