96f937931f0a911c6254abec335a9f39aeb303da
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 15:51:59 +0000

    Committing solution for EXT

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -12,13 +12,15 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
+const lastDotIndex = filePath.lastIndexOf(".");
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
 const dir = filePath.slice(0, lastSlashIndex);
-//const ext = ;
+const ext = filePath.slice(lastDotIndex + 1);
 
 console.log("Dir:", dir);
+console.log("Ext:", ext);
 // https://www.google.com/search?q=slice+mdn
