dc4671ced9911baed179657efb0cf622272d7d19
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-07 21:17:35 +0200

    Sprint 3 Coursework: Implement and rewrite tests 2

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -1,24 +1,11 @@
-// Implement a function isProperFraction,
-// when given two numbers, a numerator and a denominator, it should return true if
-// the given numbers form a proper fraction, and false otherwise.
-
-// Assumption: The parameters are valid numbers (not NaN or Infinity).
-
-// Note: If you are unfamiliar with proper fractions, please look up its mathematical definition.
-
-// Acceptance criteria:
-// After you have implemented the function, write tests to cover all the cases, and
-// execute the code to ensure all tests pass.
-
 function isProperFraction(numerator, denominator) {
-  // TODO: Implement this function
+  return Math.abs(numerator) < Math.abs(denominator);
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
 // This will be useful in the "rewrite tests with jest" step.
 module.exports = isProperFraction;
 
-// Here's our helper again
 function assertEquals(actualOutput, targetOutput) {
   console.assert(
     actualOutput === targetOutput,
@@ -26,8 +13,14 @@ function assertEquals(actualOutput, targetOutput) {
   );
 }
 
-// TODO: Write tests to cover all cases.
-// What combinations of numerators and denominators should you test?
-
-// Example: 1/2 is a proper fraction
+// Positive proper fraction test
 assertEquals(isProperFraction(1, 2), true);
+
+// Improper proper fraction test
+assertEquals(isProperFraction(3, 2), false);
+
+// Negative proper fraction test
+assertEquals(isProperFraction(-1, 2), true);
+
+// Zero numerator test
+assertEquals(isProperFraction(0, 2), true);
