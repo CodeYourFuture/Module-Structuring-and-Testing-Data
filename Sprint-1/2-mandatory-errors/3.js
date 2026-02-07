5a6944423d1f20c07bc671bb0b356642d406d7cd
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 17:01:22 +0000

    Updating the solution with the correct code

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,6 +1,7 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
 
+console.log(last4Digits);
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
@@ -8,3 +9,4 @@ const last4Digits = cardNumber.slice(-4);
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 // Prediction: The code may not work due to the const variable being an int. The slice method usually works on strings. 
+// Updating the const last4Digits with .toString() allowed the const cardNumber to convert from int to string and therefore the solution works as expected.
