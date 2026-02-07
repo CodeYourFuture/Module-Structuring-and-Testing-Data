0e510601ab00b9c02a1f55039290bf8094b217d7
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 16:38:54 +0000

    Adding the explination of the solution for this activity

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,7 +2,7 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-console.log(Math.random());
+console.log(num);
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
@@ -10,3 +10,5 @@ console.log(Math.random());
 // Explaning the solution: console.log(num); returns a number.
 // Upon investigation Math.Floor - returns and whole int that is the less than or equal to its numeric argument. 
 // Math.randon - returns a random float/int
+// Now putting my understanding together - I understand that the const num when logged using console.log returns a value in this order 
+// A number at random is chosen and is multiplied with the maximum value (of that number) - the minimum value is then added to one (+ 1) and then added to the minimum of the total. 
