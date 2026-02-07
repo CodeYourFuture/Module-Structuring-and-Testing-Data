abee58956af17c9d000945b8ba6ddf531d43a1b3
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 16:33:38 +0000

    Experimenting with Math.Floor by logging the output of a random float to the terminal

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,9 +2,10 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-console.log(num);
+console.log(Math.floor(0.5));
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 // Explaning the solution: console.log(num); returns a number.
+// Upon investigation Math.Floor - returns and whole int that is the less than or equal to its numeric argument. 
