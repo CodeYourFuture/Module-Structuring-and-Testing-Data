ed9ee26db2c029a8c470c8011644c8e8adf5d667
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-07 18:04:12 +0200

    Sprint 3 Coursework: Implement and rewrite tests 1

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -1,21 +1,17 @@
-// Implement a function getAngleType
-//
-// When given an angle in degrees, it should return a string indicating the type of angle:
-// - "Acute angle" for angles greater than 0° and less than 90°
-// - "Right angle" for exactly 90°
-// - "Obtuse angle" for angles greater than 90° and less than 180°
-// - "Straight angle" for exactly 180°
-// - "Reflex angle" for angles greater than 180° and less than 360°
-// - "Invalid angle" for angles outside the valid range.
-
-// Assumption: The parameter is a valid number. (You do not need to handle non-numeric inputs.)
-
-// Acceptance criteria:
-// After you have implemented the function, write tests to cover all the cases, and
-// execute the code to ensure all tests pass.
-
 function getAngleType(angle) {
-  // TODO: Implement this function
+  if (angle > 0 && angle < 90) {
+    return "Acute angle";
+  } else if (angle === 90) {
+    return "Right angle";
+  } else if (angle > 90 && angle < 180) {
+    return "Obtuse angle";
+  } else if (angle === 180) {
+    return "Straight angle";
+  } else if (angle > 180 && angle < 360) {
+    return "Reflex angle";
+  } else {
+    return "Invalid angle";
+  }
 }
 
 // The line below allows us to load the getAngleType function into tests in other files.
@@ -31,7 +27,42 @@ function assertEquals(actualOutput, targetOutput) {
   );
 }
 
-// TODO: Write tests to cover all cases, including boundary and invalid cases.
-// Example: Identify Right Angles
-const right = getAngleType(90);
+// Acute Angles Test
+let acute = getAngleType(45);
+assertEquals(acute, "Acute angle");
+
+// Right Angles Test
+let right = getAngleType(90);
 assertEquals(right, "Right angle");
+
+// Obtuse Angles Test
+let obtuse = getAngleType(120);
+assertEquals(obtuse, "Obtuse angle");
+
+// Straight Angles Test
+let straight = getAngleType(180);
+assertEquals(straight, "Straight angle");
+
+// Reflex Angles Test
+let reflex = getAngleType(200);
+assertEquals(reflex, "Reflex angle");
+
+// Invalid Angles Test
+let invalid = getAngleType(0);
+assertEquals(invalid, "Invalid angle");
+
+// Invalid Angles Test
+invalid = getAngleType(360);
+assertEquals(invalid, "Invalid angle");
+
+// Invalid Angles Test
+invalid = getAngleType(500);
+assertEquals(invalid, "Invalid angle");
+
+// Invalid Angles Test
+invalid = getAngleType(-200);
+assertEquals(invalid, "Invalid angle");
+
+// Test for float inputs
+acute = getAngleType(34.333);
+assertEquals(acute, "Acute angle");
