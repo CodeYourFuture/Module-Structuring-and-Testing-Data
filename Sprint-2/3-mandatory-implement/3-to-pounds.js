bc3c74c05e09ae6775df1c9da34c01dd8fc87477
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-07 15:12:38 +0200

    Sprint 2 Coursework: Mandatory Implement 3

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -1,6 +1,29 @@
-// In Sprint-1, there is a program written in interpret/to-pounds.js
+function toPounds(str) {
+  const price = (parseInt(str.replace("p", ""), 10) / 100).toFixed(2);
 
-// You will need to take this code and turn it into a reusable block of code.
-// You will need to declare a function called toPounds with an appropriately named parameter.
+  return `£${price}`;
+}
 
-// You should call this function a number of times to check it works for different inputs
+let returnedPrice = toPounds("399p");
+let actualPrice = "£3.99";
+
+console.assert(
+  returnedPrice === actualPrice,
+  `returned price: ${returnedPrice}, actual price: ${actualPrice}`
+);
+
+returnedPrice = toPounds("1p");
+actualPrice = "£0.01";
+
+console.assert(
+  returnedPrice === actualPrice,
+  `returned price: ${returnedPrice}, actual price: ${actualPrice}`
+);
+
+returnedPrice = toPounds("102342342342p");
+actualPrice = "£1023423423.42";
+
+console.assert(
+  returnedPrice === actualPrice,
+  `returned price: ${returnedPrice}, actual price: ${actualPrice}`
+);
