82eeb813b0e6e43f06fb4e17449d0a19f3e8cf7d
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-07 11:58:35 +0000

    4-random done

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,8 +2,10 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+console.log(num)
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+//results: 4,23,91,46 the program is giving you a random number while math.random give you a random decimal number math.floor rounds the total number up
