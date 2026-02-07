df44244c0875478527738d2b1bffe6d71e7568ca
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 16:47:30 +0000

    Adding in the explaination of the solution

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -3,3 +3,6 @@
 let age = Math.floor(Math.random());
 age = age + 1;
 console.log(age);
+// Explaning the solution: Assigning age to a variable that is not constant - then utalising Math.Floor and Math.Random to assign age a whole random number.
+// I then allow age to be added to 1
+// I then log to the console to verify the age. 
