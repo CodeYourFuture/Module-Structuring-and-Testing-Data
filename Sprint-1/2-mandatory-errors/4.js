ff43f7de866e9f92e1fa8ae77e018952c2694c3e
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-07 13:43:18 +0000

    1-interpret done

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+//answer
+
+const HourClockTime24 = "20:53";
+const hourClockTime12 = "08:53";
+
+// you can't start a parameter with the number, also the values did not match logically to the parameter
