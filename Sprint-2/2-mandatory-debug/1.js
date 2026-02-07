ac6d7dfc3dcf48407af300a259d448dc7bd8d069
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-07 04:33:23 +0200

    Sprint 2 Coursework: Mandatory Debug 1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,27 @@
-// Predict and explain first...
-//  =============> write your prediction here
+//  =============> Prediction <=============
+// The script will run but not as intended. The sum function has a valid
+// return, though it is simply null. Since a + b calculation comes after
+// the return, it isn't run as the program exits the loop once the return
+// call has been made. I therefore expect the following to be printed on
+// the terminal:
+// The sum of 10 and 32 is undefined
+
+//  =============> Actual Results <=============
+// The sum of 10 and 32 is undefined
+
+//  =============> Explanation <=============
+// The return was made before a and b could be added.
+
+//  =============> Corrected Script <=============
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+const actualResult = sum(10, 32);
+const expectedResult = 42;
 
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+console.assert(
+  actualResult === expectedResult,
+  `actual result: ${actualResult}, expected result: ${expectedResult}`
+);
