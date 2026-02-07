5c23704758605ea6dd34e002e8d96eb8fa86b27c
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 15:16:21 +0000

    Committing an attempt to figure out the issue

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -10,14 +10,16 @@
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
-const lastSlashIndex = filePath.lastIndexOf("/");
+//const lastSlashIndex = filePath.lastIndexOf("/");
+const firstSlashIndex = filePath.firstIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 //console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-//const dir = ;
-const ext = filePath.slice(lastSlashIndex + 5);
-console.log(ext);
+const dir = filePath.slice(firstSlashIndex + 1);
+console.log(dir);
+//const ext = filePath.slice(lastSlashIndex + 5);
+//console.log(ext);
 // https://www.google.com/search?q=slice+mdn
