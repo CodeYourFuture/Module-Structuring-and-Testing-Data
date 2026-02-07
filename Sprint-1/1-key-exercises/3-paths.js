2f85b848c19a5a5aef0aa5158c588b979d65a6e4
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-07 15:26:41 +0000

    Committing solution for dir pathway

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -10,16 +10,15 @@
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
-//const lastSlashIndex = filePath.lastIndexOf("/");
-const firstSlashIndex = filePath.firstIndexOf("/");
+const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
-//console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(firstSlashIndex + 1);
-console.log(dir);
-//const ext = filePath.slice(lastSlashIndex + 5);
-//console.log(ext);
+const dir = filePath.slice(0, lastSlashIndex);
+//const ext = ;
+
+console.log("Dir:", dir);
 // https://www.google.com/search?q=slice+mdn
