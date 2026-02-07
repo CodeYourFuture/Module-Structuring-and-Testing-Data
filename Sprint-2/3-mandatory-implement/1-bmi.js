f1d48771e228d9270dd0fa62e72ea1176d14b50b
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-07 13:54:05 +0200

    Sprint 2 Coursework: Mandatory Implement 1

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -1,19 +1,13 @@
-// Below are the steps for how BMI is calculated
-
-// The BMI calculation divides an adult's weight in kilograms (kg) by their height in metres (m) squared.
-
-// For example, if you weigh 70kg (around 11 stone) and are 1.73m (around 5 feet 8 inches) tall, you work out your BMI by:
-
-// squaring your height: 1.73 x 1.73 = 2.99
-// dividing 70 by 2.99 = 23.41
-// Your result will be displayed to 1 decimal place, for example 23.4.
-
-// You will need to implement a function that calculates the BMI of someone based off their weight and height
-
-// Given someone's weight in kg and height in metres
-// Then when we call this function with the weight and height
-// It should return their Body Mass Index to 1 decimal place
-
 function calculateBMI(weight, height) {
-    // return the BMI of someone based off their weight and height
+  const bmi = parseFloat((weight / height ** 2).toFixed(1));
+
+  return bmi;
 }
+
+const calculatedBMI = calculateBMI(70, 1.73);
+const actualBMI = 23.4;
+
+console.assert(
+  calculatedBMI === actualBMI,
+  `calculated BMI: ${calculatedBMI}, actual BMI: ${actualBMI}`
+);
