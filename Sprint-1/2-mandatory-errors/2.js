da67e6838544ad8bacf5fa534776460d0d1cdf5c
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-02-08 20:38:41 +0000

    Changed corrected code into a comment

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -7,7 +7,7 @@ const cityOfBirth = "Bolton";
 // The error is that the variable cityOfBirth isn't declared and assigned a value.
 // To fix the error, we need to declare and assign a value to cityOfBirth before using it in the console.log statement.
 
-// The corrected code is below:
+// The correct code is below:
 
-const cityOfBirth = "Bolton";
-console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+// console.log(`I was born in ${cityOfBirth}`);
