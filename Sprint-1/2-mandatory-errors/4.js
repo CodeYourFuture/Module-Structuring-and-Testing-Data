fb5cc26231747dee8eba2940f40d0182704b6541
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-08 16:05:42 +0000

    Adding the explination of the solution

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -3,3 +3,4 @@ const twentyFourHourClockTime = "08:53";
 
 console.log(twelveHourClockTime);
 console.log(twentyFourHourClockTime);
+//Explaining the solution: The issue is that the syntax to assign a const var needs to be a string.
