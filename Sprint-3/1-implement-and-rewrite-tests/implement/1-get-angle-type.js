a0b25ccaf1a8db3267b3ccae62aa824f14215614
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-09 00:27:41 +0200

    Refactor: Sprint 3 Coursework: Implement and rewrite tests 3

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -1,25 +1,18 @@
 function getAngleType(angle) {
-  if (angle > 0 && angle < 90) {
-    return "Acute angle";
-  } else if (angle === 90) {
-    return "Right angle";
-  } else if (angle > 90 && angle < 180) {
-    return "Obtuse angle";
-  } else if (angle === 180) {
-    return "Straight angle";
-  } else if (angle > 180 && angle < 360) {
-    return "Reflex angle";
-  } else {
-    return "Invalid angle";
-  }
+  // Ensure non-numbers are treated as an invalid angles
+  if (typeof angle != "number") return "Invalid angle";
+
+  if (angle > 0 && angle < 90) return "Acute angle";
+  if (angle === 90) return "Right angle";
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
+  if (angle > 180 && angle < 360) return "Reflex angle";
+
+  return "Invalid angle";
 }
 
-// The line below allows us to load the getAngleType function into tests in other files.
-// This will be useful in the "rewrite tests with jest" step.
 module.exports = getAngleType;
 
-// This helper function is written to make our assertions easier to read.
-// If the actual output matches the target output, the test will pass
 function assertEquals(actualOutput, targetOutput) {
   console.assert(
     actualOutput === targetOutput,
