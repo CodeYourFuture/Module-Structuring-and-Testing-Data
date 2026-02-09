ab2653229ce8454768100ba2015d27ffa5f03b1f
Laura C <laura.csaki.uk@gmail.com>
2026-02-09 23:29:27 +0000

    Fix invalid variable names starting with numbers

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// The error occurs because variable names cannot start with numbers
+// JavaScript identifiers must begin with a letter, $ or _
+
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
