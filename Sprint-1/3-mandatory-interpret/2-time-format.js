b43117dec7ab4f0dc596f0fd3a7f28614cd55c4d
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-09 11:22:23 +0000

    Committing the ans to e)

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -35,5 +35,7 @@ console.log(result);
 // This expression converts the total movie length from seconds into whole minutes, excluding any leftover second. 
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+// The variable result represents the formatted movie duration into hours, minutres and seconds. 
+// A better variable name may be: movieDuration
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
