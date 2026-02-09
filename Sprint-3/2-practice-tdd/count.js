6b58b5634839a9bad736af7d0407661934c06f7d
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-09 09:16:17 +0000

    implemented function to check how many times a character appears in a string

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,11 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let charCount = 0;
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      charCount += 1;
+    }
+  }
+  return charCount;
 }
 
 module.exports = countChar;
