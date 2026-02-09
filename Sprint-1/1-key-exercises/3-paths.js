4260fdac0dfc43566970f9bd9265cd5e4b8cf7b3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-09 22:21:19 +0000

    updated code to store the ext part

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -19,8 +19,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
+
 const dir = filePath.slice(0,lastSlashIndex);
-const ext = filePath.slice(lastDotIndex+1);
+const ext = filePath.slice(filePath.lastIndexOf(".") + 1);
+
 
 
 // https://www.google.com/search?q=slice+mdn
