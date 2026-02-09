129958ca08757f8f0687fd281859aaae5b54f10e
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-09 09:15:34 +0000

    wrote test to check if the character exists within the string

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -4,7 +4,7 @@ const countChar = require("./count");
 // When the countChar function is called with these inputs,
 // Then it should:
 
-// Scenario: Multiple Occurrences
+// Scenario: Multiple Occurrences 
 // Given the input string str,
 // And a character char that may occur multiple times with overlaps within str (e.g., 'a' in 'aaaaa'),
 // When the function is called with these inputs,
@@ -22,3 +22,9 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should return 0 if the character does not occur",()=>{
+  const str1="ads1"
+  const char1="l"
+  const count1=countChar(str1,char1)
+  expect(count1).toEqual(0)
+})
