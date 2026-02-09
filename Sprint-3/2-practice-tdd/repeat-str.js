c46fdb085904fa13b2faa0030b62c7ccd3a387a5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-09 09:40:14 +0000

    updated function to handle 0 and negative numbers

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,8 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Only positive integers accepted");
+  }
+  return str.repeat(count);
 }
 
 module.exports = repeatStr;
