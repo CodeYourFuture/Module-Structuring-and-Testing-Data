373185ad6c24627c9a59f5193c1d30fc4cfc36ef
Pretty Taruvinga <pretty548@gmail.com>
2026-02-09 16:01:41 +0200

    answered questions 2 and 3

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -20,4 +20,6 @@ console.log(`The base part of ${filePath} is ${base}`);
 const dir = filePath.slice(0, lastSlashIndex);
 const ext = filePath.slice(filePath.lastIndexOf(".") + 1);
 
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
 // https://www.google.com/search?q=slice+mdn
