39253b5513909f41e82e321e9718c7e6ac57fd90
Laura C <laura.csaki.uk@gmail.com>
2026-02-09 23:21:43 +0000

    I fixed template string and variable order error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+// The error is caused by using the variable before it is declared
+// and by using single quotes instead of backticks for a template string
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
