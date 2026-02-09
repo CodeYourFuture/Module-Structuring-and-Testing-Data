24c4800083ea993147fc526d5d7be2b51b41e99b
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-02-09 20:17:13 +0000

    Step-by-step breakdown of each line in this program

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,20 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1); :
+// Removes the "p" character so only the numeric part of the price remains.
+
+// 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"):
+// Ensures the number is at least 3 digits long by adding leading zeros when necessary, which helps with consistent formatting.
+
+// 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2); :
+// Extracts everything except the last two digits to form the pounds portion of the price.
+
+// 5. const pence = paddedPenceNumberString .substring(paddedPenceNumberString.length - 2) .padEnd(2, "0"); :
+// Takes the final two digits of the padded string to form the pence portion.
+
+// 6. console.log(`£${pounds}.${pence}`); :
+// Combines the pounds and pence into a standard currency format and prints it to the console, "£3.99"
+// The program takes a string representing a price in pence (e.g., "399p")
+// and converts it to a string representing the price in pounds (e.g., "£3.99").
