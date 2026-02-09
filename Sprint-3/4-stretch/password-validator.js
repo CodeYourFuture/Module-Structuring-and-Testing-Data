8bc912e31ad8d155789fb296ae0485e433949d42
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-09 20:03:16 +0000

    implemented password validation function

--- Sprint-3/4-stretch/password-validator.js
+++ Sprint-3/4-stretch/password-validator.js
@@ -1,6 +1,9 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+  if (password.match(/[A-Z]/) && password.match(/[a-z]/) &&
+  password.match(/[0-9]/) &&  password.match(/[!#$%.*&]/) && 
+  password.length >= 5) return true;
+ 
+  return false;
 }
-
 
 module.exports = passwordValidator;
