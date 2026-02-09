599ea5273e9c55774611f40cbf9582e36dd9bb23
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-09 14:43:09 +0000

    correcting typos and making the explanation clearer

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -28,7 +28,7 @@ console.log(`£${pounds}.${pence}`);
 
 // 2. const penceStringWithoutTrailingP = penceString.substring( 0, penceString.length - 1);
 // penceString.length -1: points to the index just before the last character. The substring(0, ect) takes everything from the start up to the last character (but not including the last character)
-// The result removes th trailing "p" so the output becomes "399p" --> "399"
+// The result removes the trailing "p" so the output becomes "399p" --> "399"
 
 // 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");: 
 // Ensures the pence int has at least 3 characters by adding leading zeros if required.
@@ -36,12 +36,12 @@ console.log(`£${pounds}.${pence}`);
 
 // 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
 // This takes everything except the last two digits
-// The last two digits represent the pence, so whatever is left over fro the calculation is the pounds.
+// The last two digits represent the pence, so whatever is left over from the calculation is the pounds.
 
 // 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
-// The substring(paddedPenceNumberString.length - 2) takes the last two characters as the pence part "399" and converts it into "99"
+// The substring(paddedPenceNumberString.length - 2) takes the last two characters of the padded string (e.g. "399") and extacts the pence portion "99"
 // The .padEnd(2, "0") ensures the pence part is always 2 digits by adding trailing zeros if needed.
-// In most cases there are already two digits, however part of the code protects against edge cases such as "3" becoming "30" which would be an inaccurate outcome.
+// In most cases there are already two digits, however part of the code protects against outputs such as "3" becoming "30" which would be an inaccurate outcome.
 
 // 6. console.log(`£${pounds}.${pence}`);
 // Prints the final formatted price in pounds and pence. An example would be "399p" would print as "£3.99
