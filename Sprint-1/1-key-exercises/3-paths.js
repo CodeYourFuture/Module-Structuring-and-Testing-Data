19051a8548e25fee02f700e82f5640837f7fb718
Laura C <laura.csaki.uk@gmail.com>
2026-02-09 22:30:06 +0000

    Replace placeholder with dir and ext extraction

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -19,6 +19,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 const dir = filePath.slice(0, lastSlashIndex);
 const ext = base.slice(base.lastIndexOf(".") + 1);
+
 console.log(dir);
 console.log(ext);
 
