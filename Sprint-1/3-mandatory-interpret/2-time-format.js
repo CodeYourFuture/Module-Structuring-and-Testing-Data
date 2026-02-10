0ed56e52161727ca1207fb633d6ffc0aa9875e29
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-10 01:55:26 +0000

    Complete time-format code interpretation exercise

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -9,6 +9,18 @@ const totalHours = (totalMinutes - remainingMinutes) / 60;
 const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 console.log(result);
 
+//Answers:
+//a) There are 6 variable declarations in this program. They are on lines 1,3,4,6,7, and 9.
+//b) There is 1 function call in this program. It is on line 9.
+//c) The expression movieLength % 60 represents the remainder when movieLength is divided by 60.
+//d) The expression convert the total movie length into minutes, ignoring the leftover seconds.
+//e) The variable result represents the total length of the movie in hours, minutes, and seconds. 
+// a better name is movieLength 
+//f) This code work perfectly for positive numbers> However, it doesn't work for negative or very large numbers, and decimals.
+
+
+
+
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
