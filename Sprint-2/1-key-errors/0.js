ffebd9e98b1a50b07c07245f17032009a673326d
Martin <majamc@outlook.com>
2026-02-10 14:48:48 +0000

    sprint 2/1 compleat

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,23 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// you will get an error becuase srt has alredy been declared
+
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+  return str;*/
+
 
 // =============> write your explanation here
+//let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  //    ^
+
+//SyntaxError: Identifier 'str' has already been declared
 // =============> write your new code here
+function capitalise(str) {
+  let strTwo = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -2,19 +2,29 @@
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
-
+// there is nothing con nectimng the 3 to the variable num
 // =============> write your prediction of the error here
 
-function square(3) {
+/*function square(3) {
     return num * num;
 }
-
+*/
 // =============> write the error message here
+//function square(3) {
+   //             ^
+
+//SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// it was expecting a variable not a number
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
 
+function square(num) {
+    num = 3;
+    return num * num;
+}
+console.log(square());
