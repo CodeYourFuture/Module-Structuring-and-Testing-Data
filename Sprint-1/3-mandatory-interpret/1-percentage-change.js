98f4de994b2eb6b95d37f0db153dbfd43611c295
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-10 01:22:59 +0000

    Fix missing comma in replaceAll

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,12 +2,20 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
 
 console.log(`The percentage change is ${percentageChange}`);
+
+//a) There are 4 function calls in this file. They are on lines 1, 2, 4, and 5.
+//b) The error is in line 4 and 5. The error is occurring because the replaceAll function is being called on a string, 
+// but the string has not been converted to a number.
+//c) The variable reassignment statements are in lines 4 and 5
+//d) The variable declarations are in lines 1,2,7, and 8.
+//e) The expression Number(carPrice.replaceAll(",","")) is converting the string carPrice into a number
+// by removing the commas and then converting the resulting string into a number. from "10,000" to "10000" to 10000
 
 // Read the code and then answer the questions below
 
