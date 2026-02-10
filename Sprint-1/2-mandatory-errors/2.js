e6623612f98d08d7f20e6d53f222817c754fc1cf
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-10 00:09:48 +0000

    fixed the input error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
+let cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
+//The error was because the variable was declared after the consol.log
