20c57d3a64f7637f1929fafc0ddf53a026b64405
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-02-10 17:31:53 +0000

    copmpleted_2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// missed "return". undefined output. 
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +10,12 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+//return — returns a value from a function to the outside 
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
