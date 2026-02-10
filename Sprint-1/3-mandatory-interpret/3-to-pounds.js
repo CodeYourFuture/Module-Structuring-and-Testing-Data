9e74f96431bb695fa9d910ed2109eec5df1f2e73
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-10 02:20:37 +0000

    Enhance comments for clarity in pence to uk format pounds conversion

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,21 +1,25 @@
 const penceString = "399p";
-
+// it creates a string representing a price in pence,the p at the end indicates the unit.
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 );
-
+// it removes the trailing "p" from the penceString, leaving only the numeric part of the string.
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+//Insure the numeric string has at least 3 characters, padding it with 0 if needed.
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
-
+// Takes all digits except the last two to represent the pounds part of the price.
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
-
+// Takes the last two digits to represent the pence part of the price, ensuring it has two characters.
 console.log(`£${pounds}.${pence}`);
+// it logs the final price in pounds and pence format : "£3.99"
+
+
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
