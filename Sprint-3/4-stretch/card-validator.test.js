244ef86670a0be5e6bd955522102867f357ac67f
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-10 09:19:36 +0000

    tested for card number length

--- Sprint-3/4-stretch/card-validator.test.js
+++ Sprint-3/4-stretch/card-validator.test.js
@@ -1,1 +1,13 @@
+const validateNumber = require("./card-validator")
 const isValidNumber=require("./card-validator")
+
+
+test("number should be 16 digits long",()=>{
+    expect(validateNumber(1029384756820563)).toEqual(true)
+})
+test("number should be 16 digits long", () => {
+  expect(validateNumber(10293847568205)).toEqual(false);
+});
+test("number should be 16 digits long", () => {
+  expect(validateNumber(1029384756820512345)).toEqual(false);
+});
