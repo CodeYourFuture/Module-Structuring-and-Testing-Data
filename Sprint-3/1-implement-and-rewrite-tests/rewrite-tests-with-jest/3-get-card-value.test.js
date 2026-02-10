c156ae9855dbe274e97bf175640a507ba18a2c66
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-10 21:57:17 +0200

    Test: get card value testing

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -1,20 +1,42 @@
-// This statement loads the getCardValue function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
 const getCardValue = require("../implement/3-get-card-value");
-
-// TODO: Write tests in Jest syntax to cover all possible outcomes.
+const suits = ["♠", "♥", "♦", "♣"];
 
 // Case 1: Ace (A)
-test(`Should return 11 when given an ace card`, () => {
-  expect(getCardValue("A♠")).toEqual(11);
-});
+for (const suit of suits) {
+  const ace = `A${suit}`;
+  test(`Should return 11 when given an ace card ${ace}`, () => {
+    expect(getCardValue(ace)).toEqual(11);
+  });
+}
 
-// Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-//   Face Cards (J, Q, K)
-//   Invalid Cards
+// Case 2: Face Cards (J, Q, K)
+const faceCards = ["J", "Q", "K"];
+for (const rank of faceCards) {
+  for (const suit of suits) {
+    const cardFace = `${rank}${suit}`;
+    test(`Should return 10 when given a face card ${cardFace}`, () => {
+      expect(getCardValue(cardFace)).toEqual(10);
+    });
+  }
+}
 
-// To learn how to test whether a function throws an error as expected in Jest,
-// please refer to the Jest documentation:
-// https://jestjs.io/docs/expect#tothrowerror
+// Case 3: Number Cards (2-10)
+const cardsNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const rank of cardsNumbers) {
+  for (const suit of suits) {
+    const cardFace = `${rank}${suit}`;
+    test(`Should return ${rank} when given a card ${cardFace}`, () => {
+      expect(getCardValue(cardFace)).toEqual(rank);
+    });
+  }
+}
 
+// Case 4: Invalid Cards
+const invalidCards = ["invalid", 7, "", "AA♠", "10♠♦"];
+for (const invalidCard of invalidCards) {
+  test(`Should throw an error when given an invalid card ${invalidCard}`, () => {
+    expect(() => {
+      getCardValue(`${invalidCard}`);
+    }).toThrow();
+  });
+}
