04e0b3fc99eeeb0bebea0106a72969bc435e75dd
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-10 12:52:01 +0000

    Implement card number validation logic

--- Sprint-3/4-stretch/card-validator.js
+++ Sprint-3/4-stretch/card-validator.js
@@ -1,4 +1,16 @@
-function validateNumber(){
+function validateNumber(number) {
+  let arrNumber = [...number.toString()];
 
+  
+
+  return arrNumber.length === 16 &&
+    arrNumber.every((x) => x >= "0" && x <= "9") &&
+    new Set(arrNumber).size > 1 &&
+    arrNumber[arrNumber.length-1]%2===0 &&
+    arrNumber.reduce((acc,cur)=>+acc+(+cur),0)>16
+    ? true
+    : false;
 }
-module.exports=validateNumber
+console.log(validateNumber(11111111111112));
+
+module.exports = validateNumber;
