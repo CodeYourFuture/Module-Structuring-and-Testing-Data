3c7e3ca0c6103e54aa9088ace6341d8d973ee1e3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-10 12:52:09 +0000

    Refactor card number validation function for improved readability

--- Sprint-3/4-stretch/card-validator.js
+++ Sprint-3/4-stretch/card-validator.js
@@ -1,16 +1,13 @@
 function validateNumber(number) {
   let arrNumber = [...number.toString()];
 
-  
-
   return arrNumber.length === 16 &&
     arrNumber.every((x) => x >= "0" && x <= "9") &&
     new Set(arrNumber).size > 1 &&
-    arrNumber[arrNumber.length-1]%2===0 &&
-    arrNumber.reduce((acc,cur)=>+acc+(+cur),0)>16
+    arrNumber[arrNumber.length - 1] % 2 === 0 &&
+    arrNumber.reduce((acc, cur) => +acc + +cur, 0) > 16
     ? true
     : false;
 }
-console.log(validateNumber(11111111111112));
 
 module.exports = validateNumber;
