52d7af471c3e5f121f8eec8b5055ced1da7c0067
Laura C <laura.csaki.uk@gmail.com>
2026-02-11 15:54:21 +0000

    resolve Unexpected number SyntaxError in square function (2.js)

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,16 +5,38 @@
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+// I predict this will throw a SyntaxError because
+// a number (3) is used as a function parameter,
+// which is not allowed in JavaScript.
+
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
 
+// SyntaxError: Unexpected number
+
 // =============> explain this error message here
+
+// This throws a SyntaxError: Unexpected number.
+// Function parameters must be valid variable names.
+// The number 3 is not a valid identifier, so JavaScript
+// cannot parse the function definition.
+
+// Also, the function uses 'num' inside the body,
+// but 'num' is not defined anywhere.
+
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+
+function square(num) {
+    return num * num;
+}
+
+console.log(square(3));
+
 
 
