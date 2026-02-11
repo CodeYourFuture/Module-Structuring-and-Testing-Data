bb852dd9bc61513e470ba5088a81656e5061c71d
RomanSanaye <sanaye143@gmail.com>
2026-02-11 17:59:15 +0000

    Mandatory interprets are done

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,13 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-
+    // line: 4 and line 5 and it is replaceAll().
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-
+    // error is coming from line 5. The error is because of not putting comma between the inputs of replaceAll function. 
+    // To fix the problem just add a comma between the inputs of replaceAll function.
 // c) Identify all the lines that are variable reassignment statements
-
+    // Line 4 and 5.
 // d) Identify all the lines that are variable declarations
-
+    // we have variable declarations at lines 1, 2, 7 and 8.
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+    // the purpose of that expression is to turn into number the string and replace all commas  with an empty space.
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,21 +1,16 @@
 const penceString = "399p";
 
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+const penceStringWithoutTrailingP = 
+penceString.substring(0, penceString.length - 1); // => "399";
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); // => "399";
+const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2); // => "3"
 
 const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+  .substring(paddedPenceNumberString.length - 2) // => "99"
+  .padEnd(2, "0"); 
 
-console.log(`£${pounds}.${pence}`);
+console.log(`£${pounds}.${pence}`); // => "3.99"
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
@@ -24,4 +19,12 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 1. const penceString = "399p": initializes a string variable with the value "399p"
+// 2. const penceStringWithoutTrailingP uses the substring()method to remove the last character.
+// 3. const paddedPenceNumberString tries to add "0" at the beginning, but the length is already 3; so it does nothing.
+// Its worthy to talk about padStart() method. we use this to add a character at the beginning of a string.
+// 4. const pound uses the substring() method on paddedPenceNumberString variable and remove its last two characters.Its value ll be "3".
+// 5. const pence uses substring() on paddedPenceNumberString variable and take its length and then subtract it with 2 ...
+// which will remain like this .substring(1), so it take the index 1 till the last which is only one more index that its character is 9 ...
+// so the final value ll be "99". then uses padEnd( 2, "0") that can not add "0" at the end, as we set the length "2".
+// 6. Lastly, we print the value of our pound and pence variable. which is "3.99".
