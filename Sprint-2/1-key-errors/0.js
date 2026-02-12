cb6e13e417ac1f77050eb104133c4c7c1a4b9b04
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-12 10:55:26 +0000

    predicted the error and generated new code

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> the code gives an error because there is a variable declaration with the same name as the parameter of the function.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+
+// =============> write your explanation here: The error is occurring because the variable newStr is declared inside the function capitalise, 
+// but it is being accessed outside the function.
+
+// =============> write your new code here: 
+//  function capitalise(str) {
+//  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return newStr;
+//  }
+//  let newStr = capitalise("hello");
+//  console.log(newStr);
