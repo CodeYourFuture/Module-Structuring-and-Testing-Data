31b4ae8c6f274391a328ec6bbeffb109d8e124e9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-12 12:39:26 +0000

    changed function for more efficiency

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -15,6 +15,7 @@
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 function upperSnakeCase(str){
-    return str.toUpperCase().split(" ").join("_")
+  return str.toUpperCase().replaceAll(" ","-")
 }
+
 
