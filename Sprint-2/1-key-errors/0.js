7bd2bfd305fc17c6cd695876067dbdd697774d48
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-12 20:18:26 +0000

    replacing the code to prevent merge issues

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -6,6 +6,10 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+function capitalise(str) {
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
 
 //console.log(capitalise("hello"));
 // =============> write your explanation here 
