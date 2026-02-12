f711afc27df4839365fdb19c91cb8dc4dd1b1536
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-12 20:45:24 +0000

    fix: improved 1-count.js comments with 'increment' term + MDN links

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,9 +1,7 @@
 let count = 0;
 
-count = count + 1;
+count = count + 1;  // increment: adds 1 and assigns the new value back
 
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
-
-// The = operator assigns a new value to 'count'. It takes the current value of 'count'.
-// adds 1 to it, and then stores that new value back into the 'count' variable.
+// MDN Increment operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// MDN Assignment operators: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Assignment_operators
+// Alternatives: count += 1 or count++
