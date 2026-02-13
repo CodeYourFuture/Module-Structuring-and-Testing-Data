7902ea5adf4d7fc47016803a1d46d6e5456906d4
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-13 22:53:58 +0000

    commented why 2.js has its error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,7 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+
+// The 'ReferenceError' occurs because the code tries to use the variable cityOfBirth before it has been defined.
+// JavaScript reads from top to bottom, so the variable must be declared BEFORE it is called in the console.log.
