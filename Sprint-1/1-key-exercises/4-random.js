0e92b5a0de52f9c2690d4ee04f75c276af7bd506
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-13 22:33:58 +0000

    gave my answer for "4-random" & tested it

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,11 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+// This code generates a random whole number between 1 and 100.
+// It starts with a random decimal, multiplies it to get the right range, and chops off the decimals to make it a whole number.
+// Then it adds the minimum to make sure the count starts at 1.
+
+
+console.log("The magic number is: " + num);
