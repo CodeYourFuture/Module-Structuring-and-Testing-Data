bd9746aa56625a01efd1deadd60e4d8fc760aa64
Martin <majamc@outlook.com>
2026-02-13 17:18:58 +0000

    second one done

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -2,13 +2,23 @@
 
 // =============> write your prediction here
 
+// the fuction is not returning anything, so the result of multiplying 10 and 32 will be undefined how ever the function 
+// is still multiplying the two numbers and printing the result to the console
+
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// // =============> write your explanation here
+
+// // Finally, correct the code to fix the problem
+// //  =============> write your new code here
 function multiply(a, b) {
-  console.log(a * b);
+  const result = a * b;
+  console.log(result);
+  return result;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//there is nothing being returned so the function will be undefined
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
-function sum(a, b) {
-  return;
-  a + b;
-}
-
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
