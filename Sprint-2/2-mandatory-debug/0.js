9304ca2ccdc643724c25c8d98d7e29ae26def039
RomanSanaye <sanaye143@gmail.com>
2026-02-13 13:29:19 +0000

    Mandatory debugs are done

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> This function will not execute any operation, as it does not return anything.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// After running the code, I found that the function works partially, it prints lines 6 and as we called the function on line 9. but line 9 has an error to be fixed. As it does not print what we need.
+// also : In JavaScript, if a function doesn’t explicitly return something, it automatically returns. this is why we have got the first input ok and the second input Undefined.
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> write your new code here;
+function multiply(a, b) {
+  return (a * b);
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,17 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> This function will not return the sum of a + b as the operation is located on the next line or return. so we ll have a syntax error.
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// =============> after running the code I got undefined on the output as our function does not return anything. because after return keyword our function do not operate the codes.
 
-// =============> write your explanation here
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
