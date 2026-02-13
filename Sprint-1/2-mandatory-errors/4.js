bf43f7aa3f2635459a1f0c73da4addfae4f2d65a
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-13 23:17:46 +0000

    I commented why 4.js would have an error and changed it so it works, also adding a console.log to check it is all working

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const hourClockTime12 = "20:53";
+const hourClockTime24 = "08:53";
+
+// In JavaScript, variable names aren't allowed to start with a number.
+// The computer gets confused because it thinks 12 is a value, not a name.
+// To fix it, we have to start the name with a letter, like clockTime12Hour or just twelveHourClockTime.
+
+console.log(hourClockTime12 + " | " + hourClockTime24);
