5f4ae713724182c0e47afe419545db0a037357d7
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-13 08:12:51 +0000

    Committing the actions for 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,24 @@
-// Predict and explain first...
-//  =============> write your prediction here
+  // Predict and explain first...
+  //  =============> write your prediction here
+  // I predict that the function will not return undefined.
+  // This is due to the fact that the return statement is on its own line, therefore trying to do the addition will not work as the code is not read.
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+    function sum(a, b) {
+      return;
+      a + b;
+    }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+    console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+  // =============> write your explanation here
+  // The return statement is written on its own line, therefore JS automatically inserts a semicolon after the return.
+  // This means that the function exits immediately and returns undefined.
+  // The expression a + b; is not executed.
+
+  // Finally, correct the code to fix the problem
+  //  =============> write your new code here
+  function sum(a, b) {
+    return a + b;
+  }
+
+  console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
