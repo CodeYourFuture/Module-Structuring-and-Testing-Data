69007d989572a08f434070680aa99d0ecbbba021
RomanSanaye <sanaye143@gmail.com>
2026-02-13 11:36:44 +0000

    Sprint-2 Key-errors fixed and explained

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,25 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here 
+  // `My prediction` => The capitalise function tries to capitalize the first character of the string and concat it with the rest. This function may not run as it has error.
 
 // call the function capitalise with a string input
-// interpret the error message and figure out why an error is occurring
+  // `I called the capitalise function and found out there is a syntax error, we have already declared our 'str' variable in our function parameter, so its not a good idea to declare the same variable name as it ll throw an error.`
 
-function capitalise(str) {
+// interpret the error message and figure out why an error is occurring
+  // `The error occurs because we have declared the same (str) variable two times`. We can not declare two variables with the same name.
+
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+console.log(capitalise("Roman"));*/
 
 // =============> write your explanation here
+  // `to make this function run we should make we should declare two different variables in our function
+  // .one as a parameter of the func and assign it with the above value. the second option is to return directly without creating a variable.`
 // =============> write your new code here
+function capitalized(str2){
+  let capitalizedStr = `${str2[0].toUpperCase()}${str2.slice(1)}`;
+  return capitalizedStr;
+}
+console.log(capitalized("hi dear"));
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,27 @@
 
 // Predict and explain first BEFORE you run any code...
+// ====> As my understanding this function will throw and error as we have have not called the function with the argument 'num' .
 
-// this function should square any number but instead we're going to get an error
+// this function should square any number but instead we're going to get an error.
+// ====> it will not square any number but 3 when we call it with the argument 'num' inside.
 
-// =============> write your prediction of the error here
 
-function square(3) {
+/*function square(3) {
     return num * num;
-}
+}*/
 
 // =============> write the error message here
+// the error message is: SyntaxError: Unexpected number.
 
 // =============> explain this error message here
+// after running I understood that we can not put a value in function parameter but a variable name. to make it short, in JavaScript we put variable in parameter and value as argument when calling the function.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
+console.log(square(9));
 
 
