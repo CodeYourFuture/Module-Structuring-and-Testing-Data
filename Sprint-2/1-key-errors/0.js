4bf8b874fe45a7e5424aea60345cf1acc394e77e
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-02-13 20:48:23 +0000

    I explained why the code will throw an error and fixed the code

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The code will throw a SyntaxError
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+// Original code with error:
+/* function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
-}
+} */
 
 // =============> write your explanation here
+// The error occurs because because the variable 'str' is being redeclared inside the function.
+// The parameter 'str' is already defined, and trying to declare it again with 'let' causes a conflict.
+
 // =============> write your new code here
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
