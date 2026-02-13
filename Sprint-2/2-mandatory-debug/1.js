a79905f38d1e5ff5c6e6ca1d88bfcbc41a477a4a
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-13 13:06:39 +0000

    Fix sum function to correctly return the sum of two numbers

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,17 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here: The function will not return the sum of a and b because there is a syntax error.
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here : return should be on the same line as a + b.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum (a,b){
+  return a+b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
