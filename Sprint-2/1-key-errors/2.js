dab62aa50cadcff488064f8af103716fe6999f34
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-13 12:46:15 +0000

    modified the function to correctly square a number

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,23 @@
 
-// Predict and explain first BEFORE you run any code...
+// Predict and explain first BEFORE you run any code: the function is supposed to return the square of the number.
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here: Maybe because we used a number instead of a variable inside the function declaration.
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//    return num * num;
+// }
 
-// =============> write the error message here
+// =============> write the error message here : syntax error: Unexpected number (3)
 
-// =============> explain this error message here
+// =============> explain this error message here: we shouldn't use a number as a parametre name of the function.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
+function square(num) {
+    return num*num;
+}
+console.log(square(3));
 
