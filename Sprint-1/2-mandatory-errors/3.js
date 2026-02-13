1a08bb2e6cc7bd30aec6abe79f4efd006826a179
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-13 23:10:21 +0000

    I have explained why 3.js has an error and have updated it to be in working order

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,9 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+
+// I predict an error will happen because cardNumber is a number and not a string and slice can not be used for pure numbers.
+// We can fix it by turning the number into a string by using .toString() before we slice it.
+
+console.log("The last four digits are: " + last4Digits);
