bcb55b07f4beab7fb4943b15a8334ec4eeb06218
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-14 12:43:32 +0200

    Count: explained counter incrementation

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-// Answer: Line 3 reassigns the value of count by increasing it by 1. It therefore increases the value of count from 0 to 1
+// Answer: The "=" reassigns the value of count by incrementing  it by 1 through the "+" operator.
