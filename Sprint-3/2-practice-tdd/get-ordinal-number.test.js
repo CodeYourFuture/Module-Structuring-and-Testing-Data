19a965cd4ff240c2d640d003c9182dbc2795817d
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-14 10:33:33 +0200

    Ordinal number solution

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,11 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+
+test("should append 'th' for numbers ending with 11", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(700011)).toEqual("700011th");
+});
+
+test();
