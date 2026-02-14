c7bd6f92f0d644e8d641e0a10a13ec4995c7fb05
Fayaz <fayaz.sadat551@yahoo.com>
2026-02-14 11:39:21 +0000

    answered the sprint 1 count work

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// line 3 is updating the value of count by adding 1 to its current value and then assigning that new value back to count.
+// the assignemt operator (=) is used to assign the result of the expression (count + 1) back to the variable count, effectively updating its value.
