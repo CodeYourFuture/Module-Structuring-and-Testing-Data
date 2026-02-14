90f27bbd225cec40103dfe73a99c340272748729
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-02-14 16:26:59 +0000

    complete 1-count

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,12 @@
 let count = 0;
 
-count = count + 1;
+// count = count + 1;
 
+// count += 1;
+
+count++;
+
+console.log(count);
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// In line 3 they are reassign the value for count by adding count+1 and = here called assigning operator
