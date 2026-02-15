3b4e99094afa4a4a5ce1912544094fdc30e0bb92
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 23:05:03 +0000

    completed 2-time-format.js with comments

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,37 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// *Answer
+// There are 6.
 
 // b) How many function calls are there?
+// *Answer 
+// There is 1; console.log()
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
+//*Answer
+// This expression returns the remainder of seconds, after dividing the movieLength by 60, 
+// using the modulus operator it returns 24 seconds
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// *Answer
+// It is converting the seconds to minutes and rounding it to the last whole minute.
 
-// e) What do you think the variable result represents? Can you think of a better name for this variable?
+// e) What do you think the variable result represents? 
+//*Answer
+// The result is the duration of the film in hours, minutes and seconds - 2:26:24.
 
-// f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// Can you think of a better name for this variable?
+// *Answer
+//  movieDuration
+
+// f) Try experimenting with different values of movieLength. 
+// Will this code work for all values of movieLength? Explain your answer,
+// *Answer
+// I changed the value to;
+// 1200 returned 0:20:00
+// 92383.7 returned 25:39:43.69999999999709
+// -8784 returned -2:-26:-24
+// it worked with all numbers including decimals and negative numbers.
