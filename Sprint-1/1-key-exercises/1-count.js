98cd32095af7887362e38a6234a926edac1eeeca
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-15 20:21:58 +0000

    Moved solution to the bottom

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,8 +2,10 @@ let count = 0;
 
 count = count + 1;
 
-/* The line 3 is increasing the variable count by 1 and assigning the result to count variable on the left hand  * side.   
- */
-
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/* Solution of the problem:
+ * The line 3 is increasing the variable count by 1 and assigning the result to count variable on the left hand  * side.   
+ */
+
