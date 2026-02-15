d3add85129e4e3fe993f769cbf8c283159a07d71
FarancisGH <etonkiefrancisdore@gmail.com>
2026-02-15 18:38:55 +0000

    Changes done on Sprint-1/1-key-exercise 1-4.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+//This means that increase the count number by 1 (by adding 1 to the initial number) and maintain the count value after adding 1. E.g 0+1 =1, so the new count now will be 1. 
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,19 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+//ANSWERS:
+// Num represents a random whole number(integers).  In which in this case it can be between 1 to 100.
+//Now let me break it down;
+
+//This const minimum = 1; and this const maximum = 100; represent the range from which i want my random numbers to come from.
+
+//This command Math.random() generates a random decimal number from 0(being inclusive) to 1(but not inclusing 1) e.g 0.23, 0.87, 0.001 or 0.999 but it will never generate 1. 
+// This command (maximum - minimum + 1) is a calculation of the Maximum and Minimum number i.e, 100-1+1=100
+// This command Math.floor helps to remove any decimal point and round numbers down to the nearest whole number. e.g from 45.9 to 45
+// This command +Minimum helps to add the minimum number in our range inorder to increase the numbers to 100(incusive)
+
+console.log (num)
+
+//After logging and running num, I had these series of numbers: 12, 66, 36,64,9,91....
