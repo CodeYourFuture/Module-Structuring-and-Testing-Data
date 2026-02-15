8af030affb262156ceb7ce4cf56b922ef0190fb6
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 19:41:30 +0000

    completed 1.js with comments

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,10 @@
 
 const age = 33;
 age = age + 1;
+
+//Answer:
+//we cannot re-assign variables with "const". 
+// We have use "let". let allows the variable to be re-assigned.
+
+let age = 33;
+age = 33 + 1;
