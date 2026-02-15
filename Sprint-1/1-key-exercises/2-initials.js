db7592c5ad721675f0dae9359e2873e8e7a3cf10
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 16:18:19 +0000

    answer for initials.js with comments

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,7 +5,14 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials =`${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(0)}`;
+//Answer:
+//Solution using charAt() a Javascript string method that returns a character's position, using template literal 
+// and toUpperCase to ensure it is capitalized.
+let initials = `${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+
+
+//alternative solution using string indexing, template literal and toUppercase(to make sure it is capitalized)
+//let initials - `${firstName[0]}${middleName[0]}${lastName[0]}`.toUpperCase());
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
