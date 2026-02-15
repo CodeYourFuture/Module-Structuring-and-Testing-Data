2589dc77ddab51e2bf519443a2645c161fa59d1d
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-02-15 08:58:05 +0000

    good

---
+++ prep/test.js
@@ -0,0 +1,10 @@
+
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+const output1 = convertToPercentage(0.5);
+const output2 = convertToPercentage(0.231);
+console.log(output1);
+console.log(output2);
