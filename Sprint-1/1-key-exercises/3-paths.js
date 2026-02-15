a66e3fe8ece232d037b3acd548e87f6bb8344424
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 18:19:25 +0000

    completed paths.js with added comments

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,19 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+//Answer:
+//**where the file lives^^ 
+// It gives me everything from Index 0 to 
+// the last slash just before the filename.
+const dir = filePath.slice(0, lastSlashIndex); 
+console.log(dir);
+
+//Answer:
+//**what type of file**
+//Uses the the lastIndexOf method to look for the dot.
+//the dot is now index 0 so we start from index 1 onwards 
+// to get the file type.
+const ext = filePath.slice(filePath.lastIndexOf(".") + 1);
+console.log(ext);
 
 // https://www.google.com/search?q=slice+mdn
