a9a03908094dc78e99e3224630297c27f40d3264
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-15 22:58:16 +0000

    2.js committed

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+/** console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+*/
+
+const cityOfBirth = "Bolton"; // the declaration must be assign before to be able to call it
+console.log(`I was born in ${cityOfBirth}`);
+
