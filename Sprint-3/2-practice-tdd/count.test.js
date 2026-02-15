e918829329e485695890f271f9766e69ac5e303c
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-15 18:22:18 +0200

    Test for multiple occurences of a character

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,20 +1,12 @@
 // implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
-// Given a string `str` and a single character `char` to search for,
-// When the countChar function is called with these inputs,
-// Then it should:
 
-// Scenario: Multiple Occurrences
-// Given the input string `str`,
-// And a character `char` that occurs one or more times in `str` (e.g., 'a' in 'aaaaa'),
-// When the function is called with these inputs,
-// Then it should correctly count occurrences of `char`.
-
+// Scenario 1: Multiple Occurrences
 test("should count multiple occurrences of a character", () => {
-  const str = "aaaaa";
-  const char = "a";
-  const count = countChar(str, char);
-  expect(count).toEqual(5);
+  expect(countChar("aaaaa", "a")).toEqual(5);
+  expect(countChar("sea saw", "s")).toEqual(2);
+  expect(countChar("Arrested Development", "d")).toEqual(2);
+  expect(countChar("* Star * TV *", "*")).toEqual(3);
 });
 
 // Scenario: No Occurrences
