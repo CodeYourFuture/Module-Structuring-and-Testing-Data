1bf725a9511dfac666a83e8a9def1e93a7414989
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-15 22:50:34 +0000

    0.js file committed

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,19 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+	// There are 3 ways to do that.
+	
+	// 1. Using double slash at beginning of each line
+        // This is just an instruction for the first activity - but it is just for human consumption
+        // We don't want the computer to run these 2 lines - how can we solve this problem?
+
+	// 2. Using single slash and asterisk at beginner and asterisk and slash at end with this line inside
+	/**
+	 *
+	 * This is just an instruction for the first activity - but it is just for human consumption
+	 * We don't want the computer to run these 2 lines - how can we solve this problem?
+	 */
+  	
+	// 3. Using backtick at beginner and end of these two lines.
+	`This is just an instruction for the first activity - but it is just for human consumption
+	We don't want the computer to run these 2 lines - how can we solve this problem?`
