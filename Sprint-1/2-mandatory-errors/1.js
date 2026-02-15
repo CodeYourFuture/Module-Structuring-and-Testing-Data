818acf94431bd73332878b8d6d9f5d477c92b8ac
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 20:20:37 +0000

    completed 3.js with comments also commented out question in 1.js and 2.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,10 +1,11 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+//const age = 33;
+//age = age + 1;
 
 //Answer:
 //we cannot re-assign variables with "const". 
 // We have use "let". let allows the variable to be re-assigned.
 let age = 33;
 age = age + 1;
+console.log(age);
--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,12 +1,12 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
+//console.log(`I was born in ${cityOfBirth}`);
+//const cityOfBirth = "Bolton";
 
 //*Answer
 // The console.log is before the variable and it cannot see it. 
-// The console log needs to be blow the variable to compute the data.
+// The console log needs to be below the variable to compute the data.
 
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
