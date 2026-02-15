b7acb737dc22fff5413973a240af20ce0bc4dd1e
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 19:33:44 +0000

    completed 0.js with comments

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,8 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+/*This is just an instruction for the first activity - but it is just for human consumption
+We don't want the computer to run these 2 lines - how can we solve this problem?*/
+
+//*Answer
+// We comment it out,
+// We can do this by using a two forward slashes ("//")if it is on one line
+//or we use one forward slash with an asterisk at the start("/*") and an asterisk 
+// and a forward slash at the end ("*/") for block commenting
