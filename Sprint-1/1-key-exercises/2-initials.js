ed76a69b8c600020c5844b182d3428441755f34b
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 18:23:30 +0000

    added console.log to  initials.js

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -9,7 +9,7 @@ let lastName = "Johnson";
 //Solution using charAt() a Javascript string method that returns a character's position, using template literal 
 // and toUpperCase to ensure it is capitalized.
 let initials = `${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
-
+console.log(initials);
 
 //alternative solution using string indexing, template literal and toUppercase(to make sure it is capitalized)
 //let initials - `${firstName[0]}${middleName[0]}${lastName[0]}`.toUpperCase());
