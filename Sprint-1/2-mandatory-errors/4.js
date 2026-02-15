e05bbcecc4336f6516d26de23e4831fc888385da
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-15 20:59:19 +0000

    completed 4.js with comments

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,11 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+//What's wrong with this code?
+//*Answer
+//The variable is not named in line with the Javascript naming convention. 
+// When declaring a variable it must not start with a number, space or reserved word.
+// to fix this, we can rename them correctly.
+
+const twelve_HourClockTime = "20:53";
+const twentyFour_HourClockTime = "08:53";
