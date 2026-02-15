4a7201ff23798f6b69f8a6928853fb43f17be170
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-15 23:28:54 +0000

    4.js committeed

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+/**
+ * I've changed the variable names to tweelveHourClockTime and twentyFourHourClockTime since JavaScript doesn't allow variable names starting with numbers.
+ */
+
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
