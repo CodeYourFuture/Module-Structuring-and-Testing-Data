2daa64934f067eae9d5c901c8f0a0441c3e5b10a
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-16 20:03:11 +0000

    Committing the answer to a) for 4-mandatory-interpret

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,6 +18,7 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// Pad will be called three times (for hours, minutes and seconds)
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
