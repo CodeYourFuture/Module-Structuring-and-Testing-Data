f50ee6752254c291d1adec8a5df0098c4bef4aa6
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-02-17 19:09:18 +0000

    Fixed the code and explained it

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// It will return undefined.
 
+/*
 function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+*/
 
 // =============> write your explanation here
+// The function sum returns nothing because the return statement is followed by a semicolon
+// and the expression `a + b` is not returned.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
