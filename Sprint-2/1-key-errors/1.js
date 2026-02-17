73086b905a297eb73c827d9eb8ddb86447281a80
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-02-17 18:39:45 +0000

    I predicted and explained the errors and then fixed it

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,11 +1,11 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-
 // =============> write your prediction here
+// The error will occur because the variable 'decimalNumber' is being redeclared.
 
 // Try playing computer with the example to work out what is going on
-
+/*
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
@@ -14,8 +14,19 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+*/
 
 // =============> write your explanation here
+// The parameter decimalNumber is already declared as part of the function.
+// When we try to declare it again inside the function, it causes a syntax error because
+// we cannot redeclare a variable that is already declared in the same scope.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
