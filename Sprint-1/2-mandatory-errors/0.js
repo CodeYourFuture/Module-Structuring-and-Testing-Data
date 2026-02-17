a273daad0399a7f3d1c54451d1443d854f738cb6
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-17 23:28:28 +0000

    converted instruction lines into comments

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//We solve the problem by commenting out the lines -
+// this means that the computer will ignore them when it runs the program
