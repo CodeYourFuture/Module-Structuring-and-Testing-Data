66634e1faa9f65a7aab5c8e4263d84f733e01981
RomanSanaye <sanaye143@gmail.com>
2026-02-17 18:06:53 +0000

    Sprint-2 coursework: all folders reviewed, predictions, explanations, debugged

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,26 @@
 // Predict and explain first...
+// =====> This function tries to convert a decimal number to percentage, but I think it will not run as we have declared same variable name two times.
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> It will throw an error as we have declared decimalNumber variable in our function parameter and again we declared this decimalNumber variable inside our function.
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+/*function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
+console.log(decimalNumber);*/
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
+// =============> I tried to run this, it throw an error like: 'decimalNumber' has already been declared. to fix this we should remove the decimalNumber variable which we have inside our function.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  console.log(`Your Decimal number will be ${percentage}.`);
+  return percentage;
+}
+convertToPercentage(0.9);
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,24 +1,34 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// =============> In my knowledge only the output from line 6 will be printed correctly. As for line 12, 13, and 14 it will throw error or will print undefined as we don't declare any variable for them but only value.
 
-const num = 103;
+// const num = 103;
 
-function getLastDigit() {
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> the output after running the code is: ---The last digit of 42 is 3  --- The last digit of 105 is 3  --- The last digit of 806 is 3;
+// my prediction was half correct as it worked only with line 6 but it did not throw error or undefined. here not syntax error but logical error.
+
+// Explain why the output is the way it is
+// =============> Its because JavaScript only calls the function 3 times, it does not read or ignore the value inside as they are not declared.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
-
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
-
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// ===> it was not working with line 12, 13, and 14 because we had not declared the variable inside the function parameter, to fix the issue is to declare the num variable inside the parameter of the function. and remove the const variable of num above the function or line 6.
