5fdb20e8f0878584aa60304a103c032a4800913a
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-02-17 18:53:05 +0000

    I explained why there is an error and then fixed it

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,25 @@
-
 // Predict and explain first BEFORE you run any code...
+// It will throw a syntax error because 3 is not a valid.
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
+/*
 function square(3) {
     return num * num;
 }
-
+*/
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// The error occurs because function parameters must be valid identifiers (i.e., variable names), not literal numbers.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+  return num * num;
+}
 
-
+console.log(square(3));
