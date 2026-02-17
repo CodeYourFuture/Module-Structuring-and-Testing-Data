fca74e94ab71582da7097e49001c2c3fe7ee42a1
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-17 22:57:49 +0000

    assigned values to variables dir and ext (3-paths.js)
    Utilized prettier to format code in (1-count.js; 2-initials.js; 3-paths.js)

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -7,6 +7,6 @@ console.log(count);
 // Describe what line 3 is doing, in particular focus on what = is doing
 
 // Line 3 is reassigning the value of the count variable to the result of the expression count + 1.
-// The = operator is the assignment operator, which assigns the value on the right-hand side 
-// (the result of count + 1) to the variable on the left-hand side (count). 
+// The = operator is the assignment operator, which assigns the value on the right-hand side
+// (the result of count + 1) to the variable on the left-hand side (count).
 // This increments the value of count by 1.
