6364ddd817fbe39ed5df853b680858534aa1cb43
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-17 22:02:41 +0200

    Count.js function

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,30 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  if (
+    typeof stringOfCharacters != "string" ||
+    typeof findCharacter != "string"
+  ) {
+    throw new Error("Please enter a valid string");
+  }
+
+  if (findCharacter.length != 1) {
+    throw new Error(
+      "Please enter a single character for findCharacter, e.g. 't"
+    );
+  }
+
+  if (stringOfCharacters.length === 0) {
+    throw new Error(
+      "Please enter a non-empty string, e.g. 'A series of unfortunate events'"
+    );
+  }
+
+  let charCount = 0;
+  let lowerCaseChar = findCharacter.toLowerCase();
+  for (char of stringOfCharacters.toLowerCase()) {
+    if (lowerCaseChar === char) charCount++;
+  }
+
+  return charCount;
 }
 
 module.exports = countChar;
