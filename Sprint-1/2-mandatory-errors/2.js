934aeec568819dc01abdb6ce345dca2c3f9f548e
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-17 23:37:31 +0000

    place cityOfBirth variable and assigned value ahead of console log statement to fix error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,14 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+//ReferenceError: Cannot access 'cityOfBirth' before initialization
+
+// This is happening because we are trying to use the variable cityOfBirth
+// before it has been declared and assigned a value.
+
+// Variables declared with let and const can't be accessed before they are initialized.
+
+// To fix this error, we need to declare and assign a value to cityOfBirth before we try to log it to the console.
