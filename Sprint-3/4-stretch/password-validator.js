55f0cccf09ced0ce6980fc79f92536b89735d634
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-18 18:28:38 +0000

    enhanced passwordValidator to check against previously used passwords

--- Sprint-3/4-stretch/password-validator.js
+++ Sprint-3/4-stretch/password-validator.js
@@ -1,12 +1,14 @@
-function passwordValidator(password) {
+function passwordValidator(password,oldPasswords=[]) {
   if (
     /[A-Z]/.test(password) &&
     /[a-z]/.test(password) &&
     /[0-9]/.test(password) &&
     /[!#$%.*&]/.test(password) &&
-    password.length >= 5
+    password.length >= 5 &&
+    !oldPasswords.includes(password)
   )
     return true;
+    
 
   return false;
 }
