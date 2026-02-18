d97b50e78f755f8c874ce6bb937301affa24cfdd
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 11:12:17 +0000

    complete exercise mandatory-errors/4

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+const clockTime12Hour = "08:53";
+const clockTime24Hour = "20:53";
+
+// I see two errors here:
+// 1 - In Javascript, variable names cannot start with numbers.
+// 2 - The variable names are both descriptive, which is correct, but their values seem to have been swapped with each other: the 24h clock should be 20:53 and the 12h clock 08:53
