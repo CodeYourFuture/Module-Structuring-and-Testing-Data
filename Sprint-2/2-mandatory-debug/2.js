a0f9e215798d00fda595395c27fc804b908a59c3
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 16:47:33 +0000

    complete 2-mandatory-debug/2

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,23 +2,39 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// getLastDigit is missing a parameter, meaning it only has access to the const num = 103 declared before. So I'm expecting it to return either an error or 3 for each call.
 
-const num = 103;
+// const num = 103;
 
-function getLastDigit() {
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
+// Explain why the output is the way it is
+// =============> write your explanation here
+// As I was saying, the function declaration has no parameters, so the only num variable that it can access is the one above it, const num = 103.
+// Because of this, even if we call the function with different arguments, we are not actually passing them as parameters, so the result will always be the last digit of 103.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
