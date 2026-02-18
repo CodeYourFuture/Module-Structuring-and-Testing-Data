16c32592f7ef3edf827d279031f75d801c9a4ed5
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 10:47:27 +0000

    add explanation to mandatory-errors/1

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,7 @@
 
 let age = 33;
 age = age + 1;
+
+// const are variables which value cannot change
+// let is a variable which value can be updated
+// to fix the error I modified the variable declaration and used let instead of const
