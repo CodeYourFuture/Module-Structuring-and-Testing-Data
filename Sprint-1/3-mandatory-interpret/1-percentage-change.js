15e22b24f286a2d112d1eaa0186f2d17f8503d78
Laura C <laura.csaki.uk@gmail.com>
2026-02-18 12:28:15 +0000

    Fix explanation for syntax error in part b

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -16,18 +16,21 @@ console.log(`The percentage change is ${percentageChange}`);
 // Line 5: priceAfterOneYear.replaceAll(",", "")
 // Line 5: Number(...)
 // Line 10: console.log(...)
-// Answer: 5 function calls
+// My answer: 5 function calls
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-// The error comes from the console.log line because it uses single quotes with ${percentageChange}
-// ${...} only works inside backticks `...` (template strings)
-// Fix: use backticks: console.log(`The percentage change is ${percentageChange}`);
+// The error comes from line 5:
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// I think there is a syntax error because a comma is missing between the two arguments of replaceAll.
+// JavaScript expects: replaceAll(",", "")
+// To fix the problem, add the missing comma:
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // c) Identify all the lines that are variable reassignment statements
-// Reassignment means we change an existing variable's value (no let/const on the line)
+// Reassignment means I change an existing variable's value (no let/const on the line)
 // Line 4: carPrice = Number(carPrice.replaceAll(",", ""));
 // Line 5: priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
-// Answer: I think lines 4 and 5
+// My answer- I think lines 4 and 5
 
 // d) Identify all the lines that are variable declarations
 // Declarations use let or const
@@ -35,10 +38,9 @@ console.log(`The percentage change is ${percentageChange}`);
 // Line 2: let priceAfterOneYear = "8,543";
 // Line 7: const priceDifference = carPrice - priceAfterOneYear;
 // Line 8: const percentageChange = (priceDifference / carPrice) * 100;
-// Answer: lines 1, 2, 7 and 8
+// Answer- lines 1, 2, 7 and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",", "")) is doing - what is the purpose of this expression?
 // replaceAll(",", "") removes commas from the string (e.g. "10,000" becomes "10000")
 // Number(...) converts the cleaned string into a real number so we can do maths with it
 // Purpose: turn "10,000" (text) into 10000 (number)
-
