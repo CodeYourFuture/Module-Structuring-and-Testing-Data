6215410e59de643128579e92fa99a4158675bd01
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 17:05:46 +0000

    complete 3-mandatory-implement/2-cases

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,11 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(string) {
+  return string.toUpperCase().replaceAll(" ", "_");
+}
+
+console.log(toUpperSnakeCase("jurassic park"));
+console.log(toUpperSnakeCase("on the road"));
+console.log(toUpperSnakeCase("parmigiano reggiano"));
