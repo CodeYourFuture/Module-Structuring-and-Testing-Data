7a00c6a07e25744d7fd423d7e5902f4da8f67bb3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-18 10:01:37 +0000

    fix: correct variable name for Ace of Spades test in getCardValue tests

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -3,8 +3,8 @@
 const getCardValue = require("../implement/3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-  const aceofSpades = getCardValue("A♠");
-  expect(aceofSpades).toEqual(11);
+  const aceOfSpades = getCardValue("A♠");
+  expect(aceOfSpades).toEqual(11);
 });
 
 // Case 2: Handle Number Cards (2-10):
