b125371a53fb21d7397f32ab995f9d2753abbf6e
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 15:39:16 +0000

    complete 1-key-errors/0

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
-
+//  =============> The function is trying to return a string with the only the first character capitalised
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   console.log(str);
+//   return str;
+// }
+
+// =============> The code is trying to declare a new variable called str, but this name is already in use by the function parameter so it cannot be declared a second time.
+// =============> As I am not using the value anywhere else, I chose to not create a new variable
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+capitalise("amaranth");
+capitalise("leonardo");
