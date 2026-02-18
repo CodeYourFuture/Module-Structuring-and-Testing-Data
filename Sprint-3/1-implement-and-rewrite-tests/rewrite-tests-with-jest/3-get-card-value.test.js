288a17dd32a8723bd2c658b65dfaaf13057ac688
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-18 10:08:07 +0200

    Refactor: group similar test cases under a single category to reduce console printouts

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -2,41 +2,40 @@ const getCardValue = require("../implement/3-get-card-value");
 const suits = ["♠", "♥", "♦", "♣"];
 
 // Case 1: Ace (A)
-for (const suit of suits) {
-  const ace = `A${suit}`;
-  test(`Should return 11 when given an ace card ${ace}`, () => {
+test(`Should return 11 when given an ace card`, () => {
+  for (const suit of suits) {
+    ace = `A${suit}`;
     expect(getCardValue(ace)).toEqual(11);
-  });
-}
+  }
+});
 
 // Case 2: Face Cards (J, Q, K)
-const faceCards = ["J", "Q", "K"];
-for (const rank of faceCards) {
+test(`Should return 10 when given a face card`, () => {
   for (const suit of suits) {
-    const cardFace = `${rank}${suit}`;
-    test(`Should return 10 when given a face card ${cardFace}`, () => {
-      expect(getCardValue(cardFace)).toEqual(10);
-    });
+    const faceCards = ["J", "Q", "K"];
+    for (const faceCard of faceCards) {
+      expect(getCardValue(`${faceCard}${suit}`)).toEqual(10);
+    }
   }
-}
+});
 
 // Case 3: Number Cards (2-10)
-const cardsNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (const rank of cardsNumbers) {
-  for (const suit of suits) {
-    const cardFace = `${rank}${suit}`;
-    test(`Should return ${rank} when given a card ${cardFace}`, () => {
+test(`Should return the numerical value of number cards`, () => {
+  const cardsNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (const rank of cardsNumbers) {
+    for (const suit of suits) {
+      const cardFace = `${rank}${suit}`;
       expect(getCardValue(cardFace)).toEqual(rank);
-    });
+    }
   }
-}
+});
 
 // Case 4: Invalid Cards
-const invalidCards = ["invalid", 7, "", "AA♠", "10♠♦"];
-for (const invalidCard of invalidCards) {
-  test(`Should throw an error when given an invalid card ${invalidCard}`, () => {
+test(`Should throw an error when given an invalid card`, () => {
+  const invalidCards = ["invalid", 7, "", "AA♠", "10♠♦"];
+  for (const invalidCard of invalidCards) {
     expect(() => {
-      getCardValue(`${invalidCard}`);
+      getCardValue(invalidCard);
     }).toThrow();
-  });
-}
+  }
+});
