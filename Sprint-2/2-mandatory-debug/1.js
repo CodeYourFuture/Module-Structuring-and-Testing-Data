a03f4b7443394b52036ed3ce7831424fa66d6db2
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 16:37:31 +0000

    complete 2-mandatory-debug/1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,23 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// We will get either an undefined or an error, because a + b is not on the same line as return
+
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+
+// return marks the end of a function, telling the computer to exit and go back to the global scope. Because the operation a + b is not on the same line as return, the computer won't be able to read it, so the function is returning nothing.
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
