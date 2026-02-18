483e8fdf5a547b8d8cb5754458b7da50f35247af
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-18 23:34:06 +0200

    Refactor test for readbility

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,47 +1,64 @@
-// implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
 
 // Scenario 1: Multiple Occurrences
-test("should count multiple occurrences of a character", () => {
-  expect(countChar("aaaaa", "a")).toEqual(5);
-  expect(countChar("sea saw", "s")).toEqual(2);
-  expect(countChar("Arrested Development", "d")).toEqual(2);
-  expect(countChar("* Star * TV *", "*")).toEqual(3);
+test("Should count multiple occurrences of a character", () => {
+  const multipleOccurrences = [
+    { a: "aaaaa", b: "a", c: 5 },
+    { a: "sea saw", b: "s", c: 2 },
+    { a: "Arrested Development", b: "d", c: 2 },
+    { a: "* Star * TV *", b: "*", c: 3 },
+  ];
+
+  for (const { a, b, c } of multipleOccurrences) {
+    expect(countChar(a, b)).toEqual(c);
+  }
 });
 
 // Scenario 2: Single Occurrence
-test("should count single occurrence of a character", () => {
-  expect(countChar("a", "a")).toEqual(1);
-  expect(countChar("Star Light", "i")).toEqual(1);
-  expect(countChar("!@#$%^&*()", "#")).toEqual(1);
-  expect(countChar("onomatopoeia", "t")).toEqual(1);
+test("Should count single occurrence of a character", () => {
+  const singleOccurrences = [
+    { a: "a", b: "a" },
+    { a: "Star Light", b: "i" },
+    { a: "!@#$%^&*()", b: "#" },
+    { a: "Why fly?", b: " " },
+  ];
+
+  for (const { a, b } of singleOccurrences) {
+    expect(countChar(a, b)).toEqual(1);
+  }
 });
 
 // Scenario 3: No Occurrence
-test("should count single occurrence of a character", () => {
-  expect(countChar("The Rookie", "S")).toEqual(0);
-  expect(countChar("Angela Bassett", "r")).toEqual(0);
-  expect(countChar("Angela Bassett", "r")).toEqual(0);
-  expect(countChar("Better Off Ted", "9")).toEqual(0);
+test("Should return 0 for no occurrence of a character", () => {
+  const noOccurrences = [
+    { a: "The Rookie", b: "S" },
+    { a: "Angela Bassett", b: "r" },
+    { a: "Santa Clarita Diet", b: "*" },
+    { a: "Better Off Ted", b: "9" },
+  ];
+
+  for (const { a, b } of noOccurrences) {
+    expect(countChar(a, b)).toEqual(0);
+  }
 });
 
 // Scenario 4: Invalid Entries
-const invalidEntries = [
-  { a: "Courage, the Cowardly Dog", b: "dog" },
-  { a: "Sheep in the Big City", b: 1 },
-  { a: -205, b: "a" },
-  { a: -205, b: 55.5 },
-  { a: true, b: "f" },
-  { a: undefined, b: "f" },
-  { a: null, b: true },
-  { a: false, b: true },
-  { a: [false], b: {} },
-];
+test("Should throw an error when given an invalid input", () => {
+  const invalidEntries = [
+    { a: "Courage, the Cowardly Dog", b: "dog" },
+    { a: "Sheep in the Big City", b: 1 },
+    { a: -205, b: "a" },
+    { a: -205, b: 55.5 },
+    { a: true, b: "f" },
+    { a: undefined, b: "f" },
+    { a: null, b: true },
+    { a: false, b: true },
+    { a: [false], b: {} },
+  ];
 
-describe.each(invalidEntries)("countChar($a, $b)", ({ a, b }) => {
-  test(`Should throw an error when given an invalid input (${a}, ${b})`, () => {
+  for (const { a, b } of invalidEntries) {
     expect(() => {
       countChar(a, b);
     }).toThrow();
-  });
+  }
 });
