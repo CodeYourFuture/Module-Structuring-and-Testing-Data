0d9eb1dd4d71adfc0d815a51491c47a5568fbc7a
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 10:42:35 +0000

    add explanation to mandatory-errors/0

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
 // This is just an instruction for the first activity - but it is just for human consumption
 // We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// Explanation
+// The computer will try to run anything that is not a comment. To make comments we can add // at the beginning of a line (ctrl + /)
+// for multiple line comments we use  /* text */
