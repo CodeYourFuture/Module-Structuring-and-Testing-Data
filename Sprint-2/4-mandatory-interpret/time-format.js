81cb5ed27ea8a0bb29cdf05b8e982d045fef6c9a
XiaoQuark <kris.oldrini@gmail.com>
2026-02-18 17:30:26 +0000

    complete 4-mandatory-interpret/time-format

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,24 +11,32 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(61));
+console.log(formatTimeDisplay(6671));
+console.log(formatTimeDisplay(832));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> "0"
+// The first time, pad() is called with totalHours, which value is "0"
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> "00"
+// pad() adds a "0" at the start of the string to make it 2 characters long
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> "1"
+// The last time, pad() is called with remainingSeconds, which value is "1"
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> 01
+// pad adds a "0" before "1" to make it 2 characters long
