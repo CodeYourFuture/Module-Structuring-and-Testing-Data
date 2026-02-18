26a311926fbb667d8aeec3403e7c5684e37e94c1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-18 10:00:28 +0000

    added tests for improper fractions, negative proper fractions, and equal numerator and denominator cases in isProperFraction function

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for improper fraction",()=>{
+  expect(isProperFraction(3,2)).toEqual(false)
+})
 
 // Case 3: Identify Negative Fractions:
+test("should return true for negative proper fraction",()=>{
+  expect(isProperFraction(-2,7)).toEqual(true)
+})
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal nominator and denominator",()=>{
+  expect(isProperFraction(2,2)).toEqual(false)
+})
