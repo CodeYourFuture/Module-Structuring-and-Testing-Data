d675e7323f359bb9c8c08168bcfc3b423d4334f8
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-20 01:00:23 +0200

    Testing for repeats

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -1,32 +1,78 @@
-// Implement a function repeatStr
 const repeatStr = require("./repeat-str");
-// Given a target string `str` and a positive integer `count`,
-// When the repeatStr function is called with these inputs,
-// Then it should:
 
-// Case: handle multiple repetitions:
-// Given a target string `str` and a positive integer `count` greater than 1,
-// When the repeatStr function is called with these inputs,
-// Then it should return a string that contains the original `str` repeated `count` times.
-
+// Case 1: handle count of 1
 test("should repeat the string count times", () => {
-  const str = "hello";
-  const count = 3;
-  const repeatedStr = repeatStr(str, count);
-  expect(repeatedStr).toEqual("hellohellohello");
+  const repeats = [
+    { a: "hello", b: 1, c: "hello" },
+    { a: "Silo", b: 1, c: "Silo" },
+    { a: "Ugly Betty", b: 1, c: "Ugly Betty" },
+    { a: "ByteOil", b: 1, c: "ByteOil" },
+  ];
+
+  for (const { a, b, c } of repeats) {
+    expect(repeatStr(a, b)).toEqual(c);
+  }
 });
 
-// Case: handle count of 1:
-// Given a target string `str` and a `count` equal to 1,
-// When the repeatStr function is called with these inputs,
-// Then it should return the original `str` without repetition.
+// Case 2: handle count greater than 1
+test("should return the original string repeated", () => {
+  const repeats = [
+    { a: "hello", b: 3, c: "hellohellohello" },
+    { a: "CYF ", b: 5, c: "CYF CYF CYF CYF CYF " },
+    {
+      a: "My Year of Meat",
+      b: 2,
+      c: "My Year of MeatMy Year of Meat",
+    },
+    { a: "Y2K", b: 10, c: "Y2KY2KY2KY2KY2KY2KY2KY2KY2KY2K" },
+  ];
 
-// Case: Handle count of 0:
-// Given a target string `str` and a `count` equal to 0,
-// When the repeatStr function is called with these inputs,
-// Then it should return an empty string.
+  for (const { a, b, c } of repeats) {
+    expect(repeatStr(a, b)).toEqual(c);
+  }
+});
 
-// Case: Handle negative count:
-// Given a target string `str` and a negative integer `count`,
-// When the repeatStr function is called with these inputs,
-// Then it should throw an error, as negative counts are not valid.
+// Case 3: Handle count of 0
+test("should return an empty string when count is 0", () => {
+  const repeats = [
+    { a: "The Office", b: 0, c: "" },
+    { a: "Angela Martin", b: 0, c: "" },
+    { a: "Michael Scott", b: 0, c: "" },
+    { a: "Kevin Malone", b: 0, c: "" },
+  ];
+
+  for (const { a, b, c } of repeats) {
+    expect(repeatStr(a, b)).toEqual(c);
+  }
+});
+
+// Case 4: Handle negative count
+test("should return an empty string when count is negative", () => {
+  const repeats = [
+    { a: "Osuofia in London", b: -1 },
+    { a: "Nkem Owoh", b: -5 },
+    { a: "Mara Derwent", b: -10 },
+    { a: "Cynthia Okereke", b: -100 },
+  ];
+
+  for (const { a, b } of repeats) {
+    expect(() => repeatStr(a, b)).toThrow();
+  }
+});
+
+// Case 5: Handle non-string input
+test("should throw an error when input is not a string", () => {
+  const repeats = [
+    { a: 123, b: 3 },
+    { a: "123", b: "3" },
+    { a: "true", b: true },
+    { a: null, b: 1 },
+    { a: "undefined", b: undefined },
+    { a: {}, b: 4 },
+    { a: [], b: 0 },
+  ];
+
+  for (const { a, b } of repeats) {
+    expect(() => repeatStr(a, b)).toThrow();
+  }
+});
