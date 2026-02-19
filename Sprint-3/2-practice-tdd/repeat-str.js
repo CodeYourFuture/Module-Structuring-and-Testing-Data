31a753f93deaac1a0568a97d88b317ca9d3ae572
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-20 01:17:42 +0200

    Implement repeat str function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,13 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (typeof str !== "string")
+    throw new Error("Please use a string i.e. 'Hello World'");
+
+  if (!Number.isInteger(count))
+    throw new Error("Please set count to an integer");
+
+  if (count < 0) throw new Error("Please use a count of 0 or greater");
+
+  return str.repeat(count);
 }
 
 module.exports = repeatStr;
