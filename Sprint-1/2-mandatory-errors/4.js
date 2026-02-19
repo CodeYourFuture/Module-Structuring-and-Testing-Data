9923e051992d78cf584b4f1f0d36414b1646b60f
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-19 12:26:15 +0000

    Rname constant variable to start with letter instead of number.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,11 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const Hour12ClockTime = "20:53";
+const hour24ClockTime = "08:53";
+
+console.log(Hour12ClockTime); //"20;53"
+console.log(hour24ClockTime); // "08:53"
+
+// SyntaxError: Invalid or unexpected token.
+// variable names cannot start with number.
+
+// renaming our variable would be the right thing.
+
