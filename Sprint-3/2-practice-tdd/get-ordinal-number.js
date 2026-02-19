c5f8e47323fff06c6da4304d98b1be0aa8359dba
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-19 16:53:31 +0200

    Implement get-ordinal-numbers

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,28 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (typeof num != "number" || !Number.isInteger(num)) {
+    throw new Error("Please enter an integer");
+  }
+
+  const integerNumber = parseInt(num);
+  const positiveNumber = Math.abs(integerNumber);
+
+  let suffix;
+
+  switch (true) {
+    case positiveNumber % 10 == 1 && positiveNumber % 100 != 11:
+      suffix = "st";
+      break;
+    case positiveNumber % 10 == 2 && positiveNumber % 100 != 12:
+      suffix = "nd";
+      break;
+    case positiveNumber % 10 == 3 && positiveNumber % 100 != 13:
+      suffix = "rd";
+      break;
+    default:
+      suffix = "th";
+  }
+
+  return `${integerNumber}${suffix}`;
 }
 
 module.exports = getOrdinalNumber;
