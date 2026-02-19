19fe9ba7942a735aa9de257fcf2c143cd786fe62
Oussama Mouggal <oussama.mouggal@gmail.com>
2026-02-19 23:47:49 +0000

    fixed 1.key exercise mistakes

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,6 +2,6 @@ let count = 0;
 
 count = count + 1;
 
-Line 3 is an assignment statement that updates the value of the count variable. 
+Line 3 is an assignment statement that updates the value of the count variable. It's called incrementing the variable
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,6 +3,5 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 console.log(num)
-
-//In this exercise num is a variable that stores a random integer between 1 and 100.
+//In this exercise num is a variable that stores a random integer in the range in [1,100]
 
