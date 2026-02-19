145e443a25ffd0bfbab4a148d49664641060572b
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-19 11:30:51 +0000

    Identify the type error and ensure the type error is fixed.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,17 @@
 
 const age = 33;
 age = age + 1;
+
+//TypeError: Assignment to constant variable.
+// here we already have a variable constant age=33, now we are trying to reassign the variable constant.
+
+// to enable this work, we can say;
+let age = 33;
+age = age + 1;
+
+console.log(age); //34 
+//this will be a way to fix error.
+
+
+
+
