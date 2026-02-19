1e4ccf1bc5765b46aa538572bba70ef7774e1758
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-19 08:54:23 +0000

    Expressed the functions with documentation explanation  and include the console.log results in five multiple loggings.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,21 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+
+//Math.random() returns a random number/ decimal number between 0 (inclusive),  and 1 (exclusive).
+
+//Math.random() always returns a number lower than 1.
+
+// * (maximum - minimum + 1) multiplying by this is to give a range of possible values.
+
+// adding the minimum at the end is to give a range value between the minimum and maximum inclusive.
+
+// Math.floor usually rounds the number down  to the nearest integer. 
+
+console.log (num); // first print is 24
+console.log (num); // second print 3
+console.log (num); // third print is 17
+console.log (num); // fourth print is 70
+console.log (num); // fifth print is 7
