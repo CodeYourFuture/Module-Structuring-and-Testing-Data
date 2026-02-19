39b4b88448828f4976592c30fd416776f1d64f16
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-19 23:44:00 +0000

    1.js committed

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,25 @@
 // Predict and explain first...
 //  =============> write your prediction here
+/**
+ * It returns an "undefined"
+ */
 
-function sum(a, b) {
+/** function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+*/
 
 // =============> write your explanation here
+ /** The arguments "a + b" should be on the same line as the return so the sum returns that value instead of "undefined"
+  */
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
