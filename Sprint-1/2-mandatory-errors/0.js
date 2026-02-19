4610def2f3c22115649db22a05ae385aca9832fb
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-19 11:12:37 +0000

    Correct identified errors.

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,9 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem.
+
+//SyntaxError: Unexpected identifier 'is' that was the error after i ran node.
+// I think javascript is trying run them word for word and found is in an unfamiliar place.
+// could also be that its trying to run is as a code which is not correct.
+
+/* To make this correct we can add a double line(//) before the statement to make it a single line comment,
+or we can add a line and asterisk  before and after to make it a double line  comment */ 
