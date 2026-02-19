884ddbe7bd5aca925c869c7043497612096b782f
abduhasen <abdumussa1363@gmail.com>
2026-02-19 13:42:01 +0000

    I described the line of code in the given code format

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// line 3 is reassigning the value of the variable, = sign is used to reassign the count to initial value of 0 + the additional value of 1
