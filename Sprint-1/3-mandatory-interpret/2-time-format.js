9fd849ac0f452178e7e8d503b3f34021ce080446
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-19 13:54:48 +0000

    Ensure my answers reflex in this file.

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = -8784; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -23,3 +23,20 @@ console.log(result);
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+
+
+// a) 6 variables declarations.
+
+// b) 1 function call.
+
+/* c) The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.
+In this case it will give the remainder of 8748 % 60 */
+
+// d) It simply wants to get the length of the movie in minutes. subtract the remaining seconds from the movie length and convert to minutes.
+
+// e) It represents the movie durations in hours, minutes and seconds. (movie duration).
+
+/*f) I have tried three values and it worked. It will work perfectly for larger and smaller numbers.
+However, with negative values it gives negative figures.*/ 
+
