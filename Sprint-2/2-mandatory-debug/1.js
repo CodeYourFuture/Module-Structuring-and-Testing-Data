fc53da3a5d8d632ca0f4ad0321a90243a853cf37
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-20 21:45:07 +0000

    Solved debug 1js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> Return being by it self and just ending with a ; is definitely just going to return nothing.
 
 function sum(a, b) {
   return;
@@ -8,6 +8,12 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> Return is not returning the a + b because it is cut off by the ; and on a different line.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
