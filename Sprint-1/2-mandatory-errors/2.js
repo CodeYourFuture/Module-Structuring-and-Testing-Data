8ab1e36b302c09d52e9f3838b719bb48453f6efe
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-20 15:34:11 +0000

    swapped line order

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,8 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
 
 
 // The 'ReferenceError' occurs because the code tries to use the variable cityOfBirth before it has been defined.
