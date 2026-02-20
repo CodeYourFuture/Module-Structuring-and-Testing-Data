83005387019fe53f52703289f64c127a00b2bf82
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-20 15:33:13 +0000

    changed const to let

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,8 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
 
+console.log(age)
 
 // The error "Assignment to constant variable" occurs because const is used to declare a constant value that cannot be changed.
 // Since the code tries to reassign the value on line 4 (age = age + 1), the program crashes.
