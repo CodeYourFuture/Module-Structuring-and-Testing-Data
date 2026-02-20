20fcc146571f9a9c0635d7ef46ed032088768b44
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-20 20:06:22 +0000

    exercise-1 committed

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,3 +1,5 @@
+/**
+ *
 // Find the instances of unreachable and redundant code - remove them!
 // The sayHello function should continue to work for any reasonable input it's given.
 
@@ -15,3 +17,22 @@ testName = "Aman";
 const greetingMessage = sayHello(greeting, testName);
 
 console.log(greetingMessage); // 'hello, Aman!'
+*
+*/
+
+// Find the instances of unreachable and redundant code - remove them!
+// The sayHello function should continue to work for any reasonable input it's given.
+
+let testName = "Jerry";
+const greeting = "hello";
+
+function sayHello(greeting, name) {
+  return `${greeting}, ${name}!`;
+}
+
+testName = "Aman";
+
+const greetingMessage = sayHello(greeting, testName);
+
+console.log(greetingMessage); // 'hello, Aman!'
+
