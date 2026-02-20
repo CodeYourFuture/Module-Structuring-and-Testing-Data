94e656352900b28b82261457ea9455a7e081d4b2
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-20 20:04:27 +0000

    get-ordinal-number.js committed

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,36 @@
+/**
 function getOrdinalNumber(num) {
   return "1st";
 }
 
 module.exports = getOrdinalNumber;
+*
+*/
+
+function getOrdinalNumber(num) {
+  // Handle negative numbers by preserving the sign
+  const absoluteNum = Math.abs(num);
+  const sign = num < 0 ? "-" : "";
+
+  // Special cases for 11, 12, 13
+  const lastTwoDigits = absoluteNum % 100;
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return `${sign}${absoluteNum}th`;
+  }
+
+  // Check the last digit for other cases
+  const lastDigit = absoluteNum % 10;
+  switch (lastDigit) {
+    case 1:
+      return `${sign}${absoluteNum}st`;
+    case 2:
+      return `${sign}${absoluteNum}nd`;
+    case 3:
+      return `${sign}${absoluteNum}rd`;
+    default:
+      return `${sign}${absoluteNum}th`;
+  }
+}
+
+module.exports = getOrdinalNumber;
+
