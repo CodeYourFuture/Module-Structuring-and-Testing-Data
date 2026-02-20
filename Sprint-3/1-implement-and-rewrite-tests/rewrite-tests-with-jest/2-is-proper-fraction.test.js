d4ace42fc4d9663b9542eb7a9bc71dc25b59bba4
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-20 03:53:13 +0000

    Rewrote tests for 2-is-proper-fraction.test.js using Jest syntax

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,7 +4,47 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
+// Special case: denominator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
+});
+
+// Special case: numerator is zero
+test(`should return true when numerator is zero`, () => {
+  expect(isProperFraction(0, 4)).toEqual(true);
+});
+
+// Special case: numerator equals denominator
+test(`should return false when numerator equals denominator`, () => {
+  expect(isProperFraction(7, 7)).toEqual(false);
+});
+
+// Special case: numerator is smaller than denominator
+test(`should return true when numerator is smaller than denominator`, () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+});
+
+// Special case: positive proper fraction
+test(`should return true for positive proper fraction`, () => {
+  expect(isProperFraction(3, 5)).toEqual(true);
+});
+
+// Special case: numerator is larger than denominator
+test(`should return false when numerator is larger than denominator`, () => {
+  expect(isProperFraction(8, 3)).toEqual(false);
+});
+
+// Special case: negative denominator
+test(`should return false when denominator is negative`, () => {
+  expect(isProperFraction(2, -6)).toEqual(false);
+});
+
+// Special case: negative numerator
+test(`should return false when numerator is negative`, () => {
+  expect(isProperFraction(-1, 4)).toEqual(false);
+});
+
+// Special case: small positive proper fraction
+test(`should return true for small positive proper fraction`, () => {
+  expect(isProperFraction(1, 100)).toEqual(true);
 });
