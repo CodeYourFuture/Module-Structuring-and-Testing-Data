64e0ddd083e0f0548c2850f995cdc9c46cadf27d
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-20 20:04:47 +0000

    get-ordinal-number.test.js committed

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -1,3 +1,4 @@
+/**
 const getOrdinalNumber = require("./get-ordinal-number");
 // In this week's prep, we started implementing getOrdinalNumber.
 
@@ -18,3 +19,66 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+*
+*/
+
+const getOrdinalNumber = require("./get-ordinal-number");
+
+test("should append 'st' for numbers ending with 1, except those ending with 11", () => {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(131)).toEqual("131st");
+});
+
+test("should append 'nd' for numbers ending with 2, except those ending with 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(132)).toEqual("132nd");
+});
+
+test("should append 'rd' for numbers ending with 3, except those ending with 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(133)).toEqual("133rd");
+});
+
+test("should append 'th' for numbers ending with 11, 12, or 13", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(111)).toEqual("111th");
+  expect(getOrdinalNumber(112)).toEqual("112th");
+  expect(getOrdinalNumber(113)).toEqual("113th");
+});
+
+test("should append 'th' for all other numbers", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(5)).toEqual("5th");
+  expect(getOrdinalNumber(6)).toEqual("6th");
+  expect(getOrdinalNumber(7)).toEqual("7th");
+  expect(getOrdinalNumber(8)).toEqual("8th");
+  expect(getOrdinalNumber(9)).toEqual("9th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(14)).toEqual("14th");
+  expect(getOrdinalNumber(20)).toEqual("20th");
+  expect(getOrdinalNumber(24)).toEqual("24th");
+  expect(getOrdinalNumber(30)).toEqual("30th");
+  expect(getOrdinalNumber(100)).toEqual("100th");
+  expect(getOrdinalNumber(114)).toEqual("114th");
+});
+
+test("should handle negative numbers correctly", () => {
+  expect(getOrdinalNumber(-1)).toEqual("-1st");
+  expect(getOrdinalNumber(-2)).toEqual("-2nd");
+  expect(getOrdinalNumber(-3)).toEqual("-3rd");
+  expect(getOrdinalNumber(-4)).toEqual("-4th");
+  expect(getOrdinalNumber(-11)).toEqual("-11th");
+  expect(getOrdinalNumber(-12)).toEqual("-12th");
+  expect(getOrdinalNumber(-13)).toEqual("-13th");
+  expect(getOrdinalNumber(-21)).toEqual("-21st");
+});
+
+test("should handle zero correctly", () => {
+  expect(getOrdinalNumber(0)).toEqual("0th");
+});
+
