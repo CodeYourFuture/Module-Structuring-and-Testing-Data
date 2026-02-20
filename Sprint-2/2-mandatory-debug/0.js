fa7e8c7b76d029bef60a1f6afe732af51c2028b4
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-20 21:36:50 +0000

    Solved debug 0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> Why are there 2 console.logs? I am not sure how to explain but I don't think it will work because of that.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,14 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> Well nothing is being returned so the outside console.log is just printing undefined. I will change the first console.log into a const and return that for the function.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  const result = (a * b);
+  return result;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
