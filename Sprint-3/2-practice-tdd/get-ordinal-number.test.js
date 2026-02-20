64d94f87f614fe75b61d3d3a332d0793051ceff6
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-20 22:53:00 +0000

    Adding a few other instances to cover the requirements correctly for test case5

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -44,6 +44,8 @@ test("should append 'th' for numbers ending with 0 or 4-9", () => {
 // Case 5: Special cases 11, 12, 13, 14, (always "th")
 test("should append 'th' for numbers ending with 11, 12, or 13", () => {
   expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
   expect(getOrdinalNumber(12)).toEqual("12th");
   expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(14)).toEqual("14th");
 });
