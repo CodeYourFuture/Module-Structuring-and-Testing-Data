ed774497058319ee823ebe2e42106a05b2b385f5
Pretty Taruvinga <pretty548@gmail.com>
2026-02-20 19:50:58 +0200

    predicted, explained and fixed the code

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+// I predict that the error will occur because the function 'sum' does not return any value, so when we try to use it inside the template literal, it will return 'undefined'.
+//  This will result in the output being "The sum of 10 and 32 is undefined" instead of the expected sum of 10 and 32.
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +10,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The error occurs because the function 'sum' has a return statement that does not return any value, which means it returns 'undefined' by default. 
+// When we try to use the result of 'sum(10, 32)' inside the template literal, it evaluates to 'undefined', which is not the expected output.
+//  To fix this error, we need to change the return statement in the 'sum' function to return the sum of 'a' and 'b'.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,7 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
-
+// I predict that the output will be "The last digit of 42 is 3", "The last digit of 105 is 3", and "The last digit of 806 is 3" because the function 'getLastDigit' is using the variable 'num' which is set to 103, so it will always return the last digit of 103, which is 3, regardless of the input passed to the function.
 const num = 103;
 
 function getLastDigit() {
@@ -15,10 +15,25 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+// The output is "The last digit of 42 is 3", "The last digit of 105 is 3", and "The last digit of 806 is 3". This matches my prediction because the function 'getLastDigit' is using the variable 'num' which is set to 103, so it will always return the last digit of 103, which is 3, regardless of the input passed to the function.
 // Explain why the output is the way it is
 // =============> write your explanation here
+// The output is the way it is because the function 'getLastDigit' is not using the parameter 'number' that is passed to it.
+//  Instead, it is using the variable 'num' which is set to 103.
+//  Since 'num' is not changing based on the input, the function will always return the last digit of 103, which is 3, regardless of the input passed to the function. To fix this issue, we need to change the function to use the parameter 'number' instead of the variable 'num'.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+const num = 103;
+
+function getLastDigit(number) {
+  return number.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// GetLastDigit seems not to work properly because the variable num is never passed into the function.
+// The fix is to call getLastDigit(num) so the function uses that value.
