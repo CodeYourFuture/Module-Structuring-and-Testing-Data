50f1a251bfa79ed0aa742c523a94feadaa396bf6
Johnny <30743018+JohnnyBoyV@users.noreply.github.com>
2026-02-20 17:06:07 +0000

    converted string back to a number

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.toString().slice(-4);
+const last4Digits = Number(cardNumber.toString().slice(-4));
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -13,3 +13,4 @@ const last4Digits = cardNumber.toString().slice(-4);
 // We can fix it by turning the number into a string by using .toString() before we slice it.
 
 console.log("The last four digits are: " + last4Digits);
+console.log("This is a " + typeof last4Digits)
