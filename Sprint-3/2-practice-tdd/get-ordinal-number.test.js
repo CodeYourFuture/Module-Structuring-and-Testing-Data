fae571e4487c7f5f7456423bccc78411d49111c6
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-20 23:48:21 +0000

    TDD: Completed get-ordinal-number.test.js with 5 grouped test cases

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,47 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+
+// Case 2: Numbers ending with 2 (except 12)
+// Given the number ends with 2 but not 12,
+// When getOrdinalNumber is called,
+// Then it should append "nd".
+test("should append 'nd' for numbers ending with 2 except those ending with 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(102)).toEqual("102nd");
+});
+
+// Case 3: Numbers ending with 3 (except 13)
+// Given the number ends with 3 but not 13,
+// When getOrdinalNumber is called,
+// Then it should append "rd".
+test("should append 'rd' for numbers ending with 3 except those ending with 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(103)).toEqual("103rd");
+});
+
+// Case 4: Numbers ending with 4-9, 0, or 11-19
+// Given the number ends with 4-9, 0, ot 11-19,
+// When getOrdinalNumber is called,
+// Then it should append "th".
+test("should append 'th' for numbers ending with 4-9, 0, or 11-19", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(5)).toEqual("5th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(20)).toEqual("20th");
+});
+
+// Case 5: Large numbers
+// Give a very large positive number,
+// When getOrdinalNumber is called,
+// Then it should still append the correct suffix based on the last digits.
+test("should handle large numbers correctly", () => {
+  expect(getOrdinalNumber(101)).toEqual("101st");
+  expect(getOrdinalNumber(112)).toEqual("112th");
+  expect(getOrdinalNumber(1000)).toEqual("1000th");
+});
