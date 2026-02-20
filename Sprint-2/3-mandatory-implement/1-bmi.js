2e6073a4bc4013a7555b019fa3affe2f2d8d33c2
Pretty Taruvinga <pretty548@gmail.com>
2026-02-20 20:39:22 +0200

    1, calculated the BMI 2. Turned the code 3. Declared the function

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -15,5 +15,6 @@
 // It should return their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
-    // return the BMI of someone based off their weight and height
+    const bmi = weight / (height * height);
+    return Math.round(bmi * 10) / 10;
 }
