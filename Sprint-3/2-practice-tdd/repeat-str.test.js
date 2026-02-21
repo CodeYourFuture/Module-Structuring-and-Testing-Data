29320b5d07056771c4a2000c2f93372995b10b48
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-21 00:16:50 +0000

    TDD: Completed repeat-str.test.js with jest test cases

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,13 +20,37 @@ test("should repeat the string count times", () => {
 // Given a target string `str` and a `count` equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
+test("should return the original string when count is 1", () => {
+  expect(repeatStr("hello", 1)).toEqual("hello");
+  expect(repeatStr("a", 1)).toEqual("a");
+  expect(repeatStr("", 1)).toEqual("");
+});
 
 // Case: Handle count of 0:
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
+test("should return empty string when count is 0", () => {
+  expect(repeatStr("hello", 0)).toEqual("");
+  expect(repeatStr("anything", 0)).toEqual("");
+  expect(repeatStr("", 0)).toEqual("");
+});
 
 // Case: Handle negative count:
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
+test("should throw error when count is negative", () => {
+  expect(() => repeatStr("hello", -1)).toThrow();
+  expect(() => repeatStr("test", -5)).toThrow();
+  expect(() => repeatStr("", -3)).toThrow();
+});
+
+// Case: Handle positive count:
+// Given an empty string `str`and a positive integer `count`,
+// When the repeatStr function is called,
+// Then it should return an empty string
+test("should return empty string when input string is empty", () => {
+  expect(repeatStr("", 5)).toEqual("");
+  expect(repeatStr("", 1)).toEqual("");
+});
