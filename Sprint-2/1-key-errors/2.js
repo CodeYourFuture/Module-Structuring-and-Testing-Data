40ac0ad142769e8db1c0e930e8cbb92e97efb4c7
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-21 17:46:36 +0000

    Refactor square function example to correct parameter name and update error explanations

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,25 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> the code will throw a syntax error because the parameter name '3' is not a valid identifier in JavaScript.
+//                  The error message will likely indicate that there is an unexpected number or that the parameter name is invalid.
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//    return num * num;
+//}
 
-// =============> write the error message here
+// =============> C:\Users\alexo\CYF\Module-Structuring-and-Testing-Data\Module-Structuring-and-Testing-Data\Sprint-2\
+//                1-key-errors\2.js:9 function square(3) {SyntaxError: Unexpected number
 
-// =============> explain this error message here
+// =============> We get an unexpected number error because the parameter name '3' is not a valid identifier in JavaScript.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+  return num * num;
+}
 
+console.log(square(4));
