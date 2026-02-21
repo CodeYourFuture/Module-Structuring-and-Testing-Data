bf6d02134fd0588f5f47d60987c0becbd2c8d9ad
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-21 16:01:10 +0000

    Update comments to clarify reference error and correct function implementation in convertToPercentage

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,30 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> It will likely be a reference error because the variable decimalNumber is not defined in the global scope
+//                and is only declared within the function convertToPercentage. When we try to log decimalNumber outside of the function,
+//                it will not be accessible and will throw an error indicating that decimalNumber is not defined.
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+//function convertToPercentage(decimalNumber) {
+//const decimalNumber = 0.5;
+//const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//return percentage;
+//}
 
-console.log(decimalNumber);
+//console.log(decimalNumber);
 
 // =============> write your explanation here
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(a) {
+  const decimalNumber = 0.5;
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+console.log(convertToPercentage(0.5));
