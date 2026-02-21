6640d9efd6266673feb5a15f1370eb02a127fd40
Pretty Taruvinga <pretty548@gmail.com>
2026-02-21 13:03:59 +0200

    completing the answers to the questions

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,20 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
-
+// Pad will be called 3 times when formatTimeDisplay is called, once for each of the total hours, remaining minutes, and remaining seconds.
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-
+// When pad is called for the first time, the value assigned to num is 0, which is the total hours calculated from the input of 61 seconds. This is because 61 seconds is equal to 1 minute and 1 second, which means there are 0 total hours in that time duration.
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// The return value of pad when called for the first time is "00", because 0 padded to 2 digits is "00".
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// When pad is called for the last time, the value assigned to num is 1, which is the remaining seconds calculated from the input of 61 seconds. This is because 61 seconds is equal to 1 minute and 1 second, so there is 1 second remaining after accounting for the full minutes.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The return value assigned to num when pad is called for the last time in this program is "01", because 1 padded to 2 digits is "01".
