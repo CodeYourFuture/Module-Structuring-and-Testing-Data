3bec4b0a629c6fe4e97dcf876a40f980cbf87eaf
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-22 20:28:53 +0000

    Remove Sprint-1 1-count.js from this branch

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,10 +2,5 @@ let count = 0;
 
 count = count + 1;
 
-console.log(count);
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-// The '=' assignment operator. It assigns the value on the right hand side to the variable on the left.
-// The variable 'count' is initially assigned the value of 0.
-// And the result is assigned back to count.
-// This mean 'count; is updated each time this line runs.
