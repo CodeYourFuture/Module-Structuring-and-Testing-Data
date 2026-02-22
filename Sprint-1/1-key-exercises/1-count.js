7f1a52d6c27b1013ee698e150d9ad3785580ef88
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-02-22 20:51:47 +0000

    described how "=" operates, with an example.
    Corrected the increment operation for the count variable.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,7 @@
 let count = 0;
 
-count = count + 1;
+count = count + 1; 
+// line 3 is assigning the variable "count" a new value of "count +1", then storing it back into "count".
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
