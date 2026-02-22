1b2d58892afb9a73c03d7427217474ab8bac9065
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-23 00:38:27 +0300

    Added explanation comments for random number generation logic

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,6 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//Math.random() returns a random number between 0 and 1 but not including 1. By multiplying by (maximum-minimum +1) we get a random number between 0 and 100. 
+// The answer could however be in decimal point, thereofor by using Math.floor() we round down to the nearest whole number, but that would give us a range between 0 and 99. Adding minimum (1) to the result gives us a range between 1 and 100 inclusive.
