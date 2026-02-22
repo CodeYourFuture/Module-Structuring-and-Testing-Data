d50f377b453f7536ed0d584949bb25fc7f3cccb6
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-02-22 21:59:00 +0000

    Erased two lines of code to prevent execution.

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// I have erased the above two lines, to prevent the computer from running them. 
