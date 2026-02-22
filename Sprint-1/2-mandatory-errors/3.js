ced4d8ccc931570c1862fdc6d11d3b0ea2692eab
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-02-22 22:42:51 +0000

    added an extra function to turn the card number into a string
    Changed cardNumber to a string before slicing to get last 4 digits.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,13 @@
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.slice(-4);
+
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = String(cardNumber).slice(-4);
+
+//My prediction was the long card number isn't a string so js will assume it's a variable
+//Error given: cardNumber.slice is not a function
+//added String function
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
