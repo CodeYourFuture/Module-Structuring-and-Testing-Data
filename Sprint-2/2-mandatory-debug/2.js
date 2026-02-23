85274162f0f964e7aabe784c85a95bf7a99defb6
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-24 00:59:49 +0300

    Updated comments to clarify the behavior of getLastDigit function and corrected it to accept various inputs.

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// =============> num has already been declared as 103, therefore, no other number can be used. 
 
 const num = 103;
 
@@ -14,11 +14,18 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> The last digit is always 3 because no other inputs are allowed.
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> No other inputs are allowed as num will always be defined as 103.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
