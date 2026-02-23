b574e153d26a217cf381945ab3c35521f3e777cc
XiaoQuark <kris.oldrini@gmail.com>
2026-02-23 08:09:49 +0000

    fix logic for selecting dir part

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -1,11 +1,12 @@
 // The diagram below shows the different names for parts of a file path on a Unix operating system
 
 // ????????????????????????????????????
-// ?          dir        ?    base    ?
-// ????????              ??????????????
-// ? root ?              ? name ? ext ?
+// ┌─────────────────────┬────────────┐
+// │          dir        │    base    │
+// ├──────┬              ├──────┬─────┤
+// │ root │              │ name │ ext │
 // "  /    home/user/dir / file  .txt "
-// ????????????????????????????????????
+// └──────┴──────────────┴──────┴─────┘
 
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
@@ -17,7 +18,9 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(filePath.indexOf("/") + 1, lastSlashIndex);
+const dir = filePath.slice(filePath.indexOf("/"), lastSlashIndex);
 const ext = base.slice(base.lastIndexOf("."));
+
+console.log(dir);
 
 // https://www.google.com/search?q=slice+mdn
