d20c0a3eaaf516a4dfebc330b778e3ebbed5d2be
boladjebsoft <djebbour1985@gmail.com>
2026-02-23 22:23:56 +0000

    describing what line 3 is doing (reassignment)
    Updated comment to clarify the assignment operation.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -3,4 +3,4 @@ let count = 0;
 count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// the third line used the the assignment operator '=' to reassign the variable count to the initial value which is zero incremented by one  resulting to the value 1
