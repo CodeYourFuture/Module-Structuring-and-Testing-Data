51aad730281cc66958d9d9e0b4e9d9848d89ac46
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-24 00:49:39 +0300

    Updated comments and added return statement in multiply function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> there is no return statement, therefore, you cannot use the result of the function in a console log. The function will run but it will return undefined.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,12 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> you need to add a return statement in order to use the result and so it can be displayed on the console. 
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> 
+  function multiply(a, b) {
+    return a * b;
+  }
+  console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
