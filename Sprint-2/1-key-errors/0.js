7241ee3642dedd5873ff9abf7f6e6f101a41d5fc
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-23 14:54:52 +0000

    completed 0.js answered remaining questions and corrected the code to make it work

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-//Answer
+//  =============> write your prediction here
+//  1.*Answer
 //  We have declared the "str" twice.It is already declared 
 //  in this function parameter, hence causing the conflict. 
 //  Although "let" allows re-assignment of variables, the issue is redeclaration, 
@@ -7,24 +8,38 @@
 
 
 // call the function capitalise with a string input
-//Answer
-/// home/justice/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-2/1-key-errors/tempCodeRunnerFile.js:2
+// 2.*Answer
+//  It throws an error detailed below;
+//  / home/justice/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-2/1-key-errors/tempCodeRunnerFile.js:2
 //  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
 //      ^
-
-//SyntaxError: Identifier 'str' has already been declared
+//  SyntaxError: Identifier 'str' has already been declared.
 
 
 // interpret the error message and figure out why an error is occurring
-//Answer
+//  3.*Answer
+//  The SyntaxError - Something is invalid that is not following 
+// Javascript rules.
+//  The identifier - a name used in the code has a problem and it 
+// lets m know which one in this case "str". It also let's me know
+// what the problem is in this case "str" has already been declared.
 
-
-
-
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+//Fix this code:
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+//I have re-assigned the "str" variable and it now runs with no errors.
+// I also checked with console.log to see the output.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise("justice"));
+
+
