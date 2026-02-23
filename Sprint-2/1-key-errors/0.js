6b40bf3e1bf16ae2b76ae0e4b3e47b004a43ec9d
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-23 14:12:07 +0000

    0.js answered prediction and logged error returned

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,8 +1,25 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//Answer
+//  We have declared the "str" twice.It is already declared 
+//  in this function parameter, hence causing the conflict. 
+//  Although "let" allows re-assignment of variables, the issue is redeclaration, 
+//  not reassignment.
+
 
 // call the function capitalise with a string input
+//Answer
+/// home/justice/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-2/1-key-errors/tempCodeRunnerFile.js:2
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//      ^
+
+//SyntaxError: Identifier 'str' has already been declared
+
+
 // interpret the error message and figure out why an error is occurring
+//Answer
+
+
+
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
