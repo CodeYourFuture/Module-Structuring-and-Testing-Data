62dc90f0d4fb9e09ad1aea8648d5ba4da1da5cfd
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-02-23 18:44:27 +0000

    Fixed redeclaration error in capitalise function
    Updated variable name to avoid redeclaration error.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here: We'll get an error message because "str" paramater has been declared already,
+//using it again inside the function will clash due to redaclaration. 
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+//function capitalise(str) {
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return str;
+//}
+
+// =============> write your explanation here: Identifier 'str' has already been declared, 
+//redaclaration in the same scope throws up the error message 
+// =============> write your new code here: 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+//Above code works because we have used another variable "newStr", avoiding redeclaration.
+
