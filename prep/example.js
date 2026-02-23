577235d63a7dcd9a466a5b8d04fe38c7b45f4f31
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-23 19:58:58 +0000

    decribed what line 3 is doing in count.js excercise

---
+++ prep/example.js
@@ -0,0 +1,12 @@
+// This are declarations
+const firstName = "Jane"; 
+const lastName = "Doe"; 
+const yearOfBirth = 1971;
+let currentYear =2026;
+
+//This are statements
+currentYear++;
+
+const introduction = `Hi, my name is ${firstName} ${lastName}, I am ${currentYear - yearOfBirth} years old.`; //saving return values
+console.log(introduction);
+
