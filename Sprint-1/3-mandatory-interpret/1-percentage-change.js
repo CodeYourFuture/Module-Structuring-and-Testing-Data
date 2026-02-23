fca8c46530431b97d6a0c220fcc105d000c2bc9f
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-02-23 08:22:45 +0000

    Fix syntax error and added comments to answer the questions.
    Fixed syntax error by adding a missing comma in replaceAll function calls.

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -1,8 +1,8 @@
 let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
-carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+carPrice = Number(carPrice.replaceAll(",",""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",",""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -13,10 +13,27 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+//There are three function calls: replaceAll() - used twice, number() - used twice, console.log() - used once.  
+// carPrice = Number(carPrice.replaceAll("," , ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," , ""));
+// console.log(`The percentage change is ${percentageChange}`);
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+//There was a syntax error on line five, missing comma. Fixed by adding one.
 
 // c) Identify all the lines that are variable reassignment statements
 
+//carPrice = Number(carPrice.replaceAll("," , ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," , ""));
+
 // d) Identify all the lines that are variable declarations
 
+//let carPrice = "10,000";
+//let priceAfterOneYear = "8,543";
+//const priceDifference = carPrice - priceAfterOneYear;
+//const percentageChange = (priceDifference / carPrice) * 100;
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+// It's removing the commas, since javascript will recognise 10,000 as a string not a number.
