70a5ed9ffdc682c23cfd987d3ad562173d3bb5e8
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-24 00:22:59 +0300

    Fixed variable redeclaration in convertToPercentage function and update comments for clarity

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> The variable decimalNumber has been declared twice and will retun an error.
 
 // Try playing computer with the example to work out what is going on
 
@@ -14,7 +14,14 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
+// =============> The variable decimalNumber has been declared twice and returned an error saying it had already been declared. There is also a console log error as it is trying to use a variable that is inside the function and hasn't been declared outside. 
+
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  console.log(percentage);
+  return percentage;
+}
+//We can also use console log inside the function. 
