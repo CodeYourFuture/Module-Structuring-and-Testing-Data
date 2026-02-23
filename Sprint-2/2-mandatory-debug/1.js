dc6c2ef6ae335ae1f4e82ef32789647ba39f0dc0
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-24 00:52:29 +0300

    Fixed return statement placement.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> return statement is on a new line, therefore there will be an error. 
 
 function sum(a, b) {
   return;
@@ -8,6 +8,11 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> return statement should be on the same line. 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b){
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
