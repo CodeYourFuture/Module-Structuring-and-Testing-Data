f5555b2b008a5d9c45ea51bf6cd19da4d568e21e
XiaoQuark <kris.oldrini@gmail.com>
2026-02-23 11:01:46 +0000

    write jest tests for getAngleType function

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -10,11 +10,42 @@ test(`should return "Acute angle" when (0 < angle < 90)`, () => {
   // Test various acute angles, including boundary cases
   expect(getAngleType(1)).toEqual("Acute angle");
   expect(getAngleType(45)).toEqual("Acute angle");
-  expect(getAngleType(89)).toEqual("Acute angle");
+  expect(getAngleType(89.436)).toEqual("Acute angle");
 });
 
 // Case 2: Right angle
+test(`should return "Right angle" when (angle = 90)`, () => {
+  expect(getAngleType(90)).toEqual("Right angle");
+});
+
 // Case 3: Obtuse angles
+test(`should return "Obtuse angle" when (90 < angle < 180)`, () => {
+  // Test various acute angles, including boundary cases
+  expect(getAngleType(92)).toEqual("Obtuse angle");
+  expect(getAngleType(111.1)).toEqual("Obtuse angle");
+  expect(getAngleType(179)).toEqual("Obtuse angle");
+});
+
 // Case 4: Straight angle
+test(`should return "Straight angle" when (angle = 180)`, () => {
+  // Test various acute angles, including boundary cases
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
+
 // Case 5: Reflex angles
+test(`should return "Reflex angle" when (180 < angle < 360)`, () => {
+  // Test various acute angles, including boundary cases
+  expect(getAngleType(181)).toEqual("Reflex angle");
+  expect(getAngleType(294)).toEqual("Reflex angle");
+  expect(getAngleType(357.34)).toEqual("Reflex angle");
+});
+
 // Case 6: Invalid angles
+test(`should return "Invalid angle" when (angle <= 0 || angle >= 360)`, () => {
+  // Test various acute angles, including boundary cases
+  expect(getAngleType(0)).toEqual("Invalid angle");
+  expect(getAngleType(-90)).toEqual("Invalid angle");
+  expect(getAngleType(360)).toEqual("Invalid angle");
+  expect(getAngleType(411.65)).toEqual("Invalid angle");
+  expect(getAngleType(256578)).toEqual("Invalid angle");
+});
