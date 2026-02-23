30a2809c93d5230a7553bd0ee8332d9700bf1647
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-24 00:35:52 +0300

    Fixed function declaration and update error explanations in square function

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,21 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> the error message will be about 'num' not being declared. 
 
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
+// =============> Uncaught SyntaxError: Illegal return statement. 
 
-// =============> explain this error message here
+// =============> The error message is because this num is not a valid variable as it has been undeclared. 
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
 
