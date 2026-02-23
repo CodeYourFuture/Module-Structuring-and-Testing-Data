7ade54a2b5d87c99d6e3f9539f09315d35ef76db
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-23 20:57:21 +0000

    2-mandatory-debug/0.js Fixed multiply function to return value and update comments for clarity

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,21 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> The funtion does not return a value, so when we try to log the result of the function call,
+// it will return undefined and log "The result of multiplying 10 and 32 is undefined" to the console.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+//function multiply(a, b) {
+//  console.log(a * b);
+//}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> 320 The result of multiplying 10 and 32 is undefined
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
