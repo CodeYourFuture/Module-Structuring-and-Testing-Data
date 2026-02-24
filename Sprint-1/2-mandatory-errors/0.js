97ad7703f120ae303e785fc25a23d7de1d8ab601
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-24 17:44:23 +0000

    commented out the 2 lines in 0.js

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+/* comment out any code you do not want the machine to run by add two forward slash to a single line comment 
+a single slash and a multiplication sign to both ends of a double line comment like this.
+*/
