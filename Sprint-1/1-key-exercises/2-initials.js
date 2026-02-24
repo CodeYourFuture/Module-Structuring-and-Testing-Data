3428f4199b5a0b2de0dbed37f5838cec30142b28
boladjebsoft <djebbour1985@gmail.com>
2026-02-24 23:21:05 +0000

    producing the string "CKJ"
    Updated the initials variable to dynamically generate initials from first, middle, and last names.

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,7 +5,11 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+let initials = firstName.charAt(0)+middleName.charAt(0)+lastName.charAt(0);
+// or
+let initials = firstName[0] + middleName[0] + lastName[0];
+// or but rarely used and not recommended because it might cause bugs
+let initials = firstName[0].concat(middleName[0], lastName[0]);
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
