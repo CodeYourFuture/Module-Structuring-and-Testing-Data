8b1f048fc6f08508592376b38cd880fbf254d857
Darkidd77 <m.almahdi77@yahoo.com>
2026-02-24 01:21:42 +0000

    Refactor comments in mandatory error examples for clarity and instruction

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,13 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// const age = 33;
+// age = age + 1;
+
+// this code will throw an error because age is declared as a constant and we cannot reassign a new value to a constant variable
+
+// To fix this error, we can change the declaration of age from const to let
+
+let age = 33;
 age = age + 1;
+
+// Now the code will work without throwing an error, and the value of age will be updated to 34.
