0c3a7bf9a7d30c1e4253bd8f4411b5c76970df30
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-24 20:21:26 +0000

    interpreted TypeError in 1.js file

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,7 @@
 
 const age = 33;
 age = age + 1;
+
+//TypeError: Assignment to constant variable.
+//what went wrong
+//The program is trying to reassign a constant variable. In javascript a constant once assigned cannot be reassigned another value 
