5468f4391664f6af680ed0ba088f9cebf02532a2
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-24 21:21:44 +0000

    interpreted ReferenceError in 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,7 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+//ReferenceError: Cannot access 'cityOfBirth' before initialization
+/* A lexical variable was accessed before it was initialized. This happens within any scope (global, module, function, or block) when variables declared with let or const are accessed before the place where they are declared has been executed
+*/
