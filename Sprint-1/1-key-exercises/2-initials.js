bd37553dcf466891d74a2d02ac9f345d24ed04e5
Darkidd77 <m.almahdi77@yahoo.com>
2026-02-24 00:38:34 +0000

    Implement initials extraction from first, middle, and last names

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,7 +5,12 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+let initials = firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0); 
+// I understood that charAt(0) gets the first character of the string. also I can concatenate strings with the + operator 
+// I can also use bracket notation/ Index access to get the first character of the string like this: firstName[0] + middleName[0] + lastName[0]
+
+console.log(initials);
+
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
