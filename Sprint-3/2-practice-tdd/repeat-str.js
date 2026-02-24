3dcd12be14b7a81f794955d57b511b788ea9f43a
Raihan Sharif <raihansharif92@gmail.com>
2026-02-24 11:09:25 +0000

    repeat-str tests

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,18 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("invalid input: negative number");
+  }
+  if (count === 1) {
+    return str;
+  }
+
+  if (count === 0) {
+    return "";
+  }
+
+  if (count > 1) {
+    return str.repeat(count);
+  }
 }
 
 module.exports = repeatStr;
