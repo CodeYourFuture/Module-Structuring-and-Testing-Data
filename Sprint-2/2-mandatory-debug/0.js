ca5d3866f4485b303bcffc479183a6d9ceec58dd
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-24 22:18:58 +0000

    completed 2-mandatory-debug-0.js updated code to work and added answers

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,30 @@
 // Predict and explain first...
-
+// The first console.log prints 320
+//It throws an error at console.log.
 // =============> write your prediction here
 
-function multiply(a, b) {
-  console.log(a * b);
-}
-
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//    function multiply(a, b) {
+//      console.log(a * b);
+//    }
+//    console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// It return the following output:
+// 320
+// The result of multiplying 10 and 32 is undefined
+// I am not sure why?
+
+//the computer reads the values in the second console.log and
+//the first console.log only prints the value, it does not store it.
+//the second console.log outputs the template literals and undefined
+// as it has no value to reach.
+
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+  function multiply(a, b) {
+    return (a * b);
+  }
+
+  console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+  //prints: The result of multiplying 10 and 32 is 320
