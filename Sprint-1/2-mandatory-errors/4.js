e781b958466983c2a687fbb42e63167da844bb66
Darkidd77 <m.almahdi77@yahoo.com>
2026-02-24 01:32:14 +0000

    Clarify variable naming rules in comments and enhance example output for better understanding

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+
+
+console.log(12HourClockTime);
+console.log(24hourClockTime);
+
+// it throws a SyntaxError: Invalid or unexpected token because variable names cannot start with a number.
+// The error is that we are trying to assign a string value to a variable that starts with a number, which is not allowed in JavaScript.
+// Variable names cannot start with a number.
+
