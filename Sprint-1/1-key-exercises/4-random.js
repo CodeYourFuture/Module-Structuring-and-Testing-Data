46a401bc6bd42e996e9b14f02f89b7fde34e7112
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-24 17:22:34 +0000

    documented the evaluation of the code provided in 4-random.js file

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,15 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+/*
+num stores a random integer between minimum (1) and maximum (100), inclusive.
+
+Breakdown:
+1. Math.random() generates a decimal number between 0 (inclusive) and 1 (exclusive).
+2. Multiplying by (maximum - minimum + 1) scales the range to 0–100.
+3. Math.floor() removes the decimal part, producing integers from 0–99.
+4. Adding minimum shifts the range to 1–100.
+*/
+console.log(num);
