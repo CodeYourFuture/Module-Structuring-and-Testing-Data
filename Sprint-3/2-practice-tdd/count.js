a0605d6fcc880bb6f55b04948d0e0f502c55d25d
XiaoQuark <kris.oldrini@gmail.com>
2026-02-24 11:27:09 +0000

    refactor 2-practice-tdd/count task

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,8 +1,6 @@
 function countChar(stringOfCharacters, findCharacter) {
   const charRegex = new RegExp(findCharacter, "g");
-  console.log(charRegex, typeof charRegex, "regex");
   const charArr = [...stringOfCharacters.matchAll(charRegex)];
-  console.log(charArr, "array");
   return charArr.length;
 }
 
