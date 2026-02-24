36ad414286aa5eb8ff8a85446487492ef1a77a37
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-24 22:21:02 +0000

    interpreted TypeError of Number conversion in 3.js file

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,6 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +8,10 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//TypeError: cardNumber.slice is not a function
+//The Number type value stored in cardNumber does not a slice method
+//slice methods only works with arrays, strings or objects that has a callable property named slice
+
+//Solution: Convert the number value to string
+//Options: use a String Wrapper  or toString method
