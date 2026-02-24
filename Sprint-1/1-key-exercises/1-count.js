644e205065d9db8851c0cea33e3f3637aa7618ef
Darkidd77 <m.almahdi77@yahoo.com>
2026-02-24 00:02:02 +0000

    Exercise 1 - Incrementing

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+//  = It assign the value of the variable count added to 1, it's called Incrementing 
+// Also Line 3, could be written as such   count++; according to doc
