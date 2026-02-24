830f9e678b31935b734c6f033748fd93f3c63a45
Raihan Sharif <raihansharif92@gmail.com>
2026-02-24 10:57:41 +0000

    ordinal numbers tests

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,19 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  // 11 is a special case
+  if (num === 11) {
+    return "11th";
+  }
+  const lastDigit = num % 10;
+  switch (lastDigit) {
+    case 1:
+      return String(num) + "st";
+    case 2:
+      return String(num) + "nd";
+    case 3:
+      return String(num) + "rd";
+    default:
+      return String(num) + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
