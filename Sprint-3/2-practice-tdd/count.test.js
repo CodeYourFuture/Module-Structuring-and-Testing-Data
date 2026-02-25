18ef7563dc387ea24cc5e732cd118f37646d4241
Pretty Taruvinga <pretty548@gmail.com>
2026-02-25 12:46:40 +0200

    test: add unit tests for countChar function

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -3,6 +3,10 @@ const countChar = require("./count");
 // Given a string `str` and a single character `char` to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
+// - Iterate through each character in `str`.
+// - Compare each character to `char`.
+// - Increment a counter each time a match is found.
+// - Return the final count of occurrences of `char` in `str`.
 
 // Scenario: Multiple Occurrences
 // Given the input string `str`,
@@ -22,3 +26,17 @@ test("should count multiple occurrences of a character", () => {
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+test("should return 0 when the character does not occur in the string", () => {
+  const str = "hello world";
+  const char = "x";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// Scenario: Case Sensitivity
+test("should be case sensitive", () => {
+  const str = "Hello World";
+  const char = "H";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
