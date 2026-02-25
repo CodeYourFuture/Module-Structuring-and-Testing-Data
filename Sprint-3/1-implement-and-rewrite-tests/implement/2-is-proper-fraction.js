4466a9b4cc6bd1b6cb0205683040f44c6a374366
Shuheda <shuheda.hatun@gmail.com>
2026-02-25 17:19:48 +0000

    Update 2-is-proper-fraction.js
    Defined proper fractions and implemented isProperFraction function.

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -5,13 +5,27 @@
 // Assumption: The parameters are valid numbers (not NaN or Infinity).
 
 // Note: If you are unfamiliar with proper fractions, please look up its mathematical definition.
+// A proper fraction means: The top number (numerator) is smaller than the bottom number (denominator).
+// e.g. 1/2, 3/5. 5/2 would not be a proper fraction.
+// Numerator < Denominator
 
 // Acceptance criteria:
 // After you have implemented the function, write tests to cover all the cases, and
 // execute the code to ensure all tests pass.
 
-function isProperFraction(numerator, denominator) {
   // TODO: Implement this function
+function isProperFraction(numerator, denominator) {
+  // A fraction cannot have denominator 0
+  if (denominator === 0) {
+    return false;
+  }
+
+  // A proper fraction means numerator is smaller than denominator
+  if (numerator < denominator) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
@@ -28,6 +42,8 @@ function assertEquals(actualOutput, targetOutput) {
 
 // TODO: Write tests to cover all cases.
 // What combinations of numerators and denominators should you test?
+// I should test proper fractions, equal numbers, improper fractions, zero numerator, 
+// zero denominator and negative numbers.
 
 // Example: 1/2 is a proper fraction
 assertEquals(isProperFraction(1, 2), true);
