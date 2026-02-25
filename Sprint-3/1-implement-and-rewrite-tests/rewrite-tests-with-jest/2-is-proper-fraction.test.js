6dc114e7a739f92e116e0b26de979edbe81aa4f1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-25 22:22:27 +0000

    refactor: improve formatting and consistency of tests in isProperFraction test suite

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,22 +4,22 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
-test(`should return false when denominator is zero`, () => {
+// Special case: denominator is zero
+test("should return false when denominator is zero", () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
 
 // Case 2: Identify Improper Fractions:
-test("should return false for improper fraction",()=>{
-  expect(isProperFraction(3,2)).toEqual(false)
-})
+test("should return false for improper fraction", () => {
+  expect(isProperFraction(3, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
-test("should return true for negative proper fraction",()=>{
-  expect(isProperFraction(-2,7)).toEqual(true)
-})
+test("should return true for negative proper fraction", () => {
+  expect(isProperFraction(-2, 7)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
-test("should return false for equal nominator and denominator",()=>{
-  expect(isProperFraction(2,2)).toEqual(false)
-})
+test("should return false for equal nominator and denominator", () => {
+  expect(isProperFraction(2, 2)).toEqual(false);
+});
