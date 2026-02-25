9515acb2a48d50aac5d0ed520557ebbbb657045a
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-25 16:22:59 +0000

    Effect the changes to 1.js .

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,16 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  There will be an error because there is going to be nothing to define after return. =============> write your prediction here
 
-function sum(a, b) {
-  return;
-  a + b;
-}
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+
+
+//Finally, correct the code to fix the problem
+ function sum(a, b) {
+  return a+b;
+  
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); 
+//=============> write your new code here
