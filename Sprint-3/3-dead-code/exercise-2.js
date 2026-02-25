4a3e85152a499c792e4df49cf7d8d89b175819ad
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-25 18:31:27 +0000

    exercise 2

--- Sprint-3/3-dead-code/exercise-2.js
+++ Sprint-3/3-dead-code/exercise-2.js
@@ -1,13 +1,16 @@
 // Remove the unused code that does not contribute to the final console log
 // The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
 
+
 const pets = ["parrot", "hamster", "horse", "dog", "hamster", "cat", "hamster"];
-const capitalisedPets = pets.map((pet) => pet.toUpperCase());
+// const capitalisedPets = pets.map((pet) => pet.toUpperCase()); - redundant
 const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
 
-function logPets(petsArr) {
-  petsArr.forEach((pet) => console.log(pet));
-}
+
+// function logPets(petsArr) {
+//   petsArr.forEach((pet) => console.log(pet));
+// } - redundant
+
 
 function countAndCapitalisePets(petsArr) {
   const petCount = {};
@@ -23,6 +26,9 @@ function countAndCapitalisePets(petsArr) {
   return petCount;
 }
 
+
 const countedPetsStartingWithH = countAndCapitalisePets(petsStartingWithH);
 
+
 console.log(countedPetsStartingWithH); // { 'HAMSTER': 3, 'HORSE': 1 } <- Final console log
+
