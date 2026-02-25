af481768bb66e3740dd8941427761eaa0a7985d8
boladjebsoft <djebbour1985@gmail.com>
2026-02-25 23:02:00 +0000

    Rename clock time variables to valid identifiers
    Updated variable names to avoid starting with digits.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const _12HourClockTime = "20:53"; // identifier can not begin with a number
+// or
+const $12HourClockTime = "20:53"; // or any change that makes the identifier not stating with digit
+const _24hourClockTime = "08:53"; // identifier can not begin with a number
+// or
+const $24hourClockTime = "08:53"; // or any change that makes the identifier not stating with digit
