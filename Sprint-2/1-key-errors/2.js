fcd1dc10700b95ba704daa2d50390f5a1294c4d5
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-25 18:11:12 +0000

    key 2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,20 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
+//we can't declare number as a variable 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-
+//Uncaught SyntaxError SyntaxError: Unexpected number
 // =============> explain this error message here
-
+//we can't declare number as a variable ?
 // Finally, correct the code to fix the problem
-
+function square(num) {
+    return num * num;
+}
+console.log(square(2))
 // =============> write your new code here
 
 
