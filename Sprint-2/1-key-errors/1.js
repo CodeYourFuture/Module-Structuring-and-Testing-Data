d70c833438662b9cfb53b96b8543ac68162afa7c
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-02-25 18:09:23 +0000

    key error 1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,7 +2,7 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
+//2 errors: 1. The parameter decimalNumber is already declared 2. it should show error or undefined as console.log is calling variable not the function
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
@@ -15,6 +15,13 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
-
+// Identifier 'decimalNumber' has already been declared - 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
