21914bb407a895b19e2de778edb292aead6b1778
boladjebsoft <djebbour1985@gmail.com>
2026-02-25 22:18:35 +0000

    Correct variable declaration for cityOfBirth
    Fix variable declaration order to avoid reference error.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// the error is that the variable 'cityOfBirth' is declared after the statement it is used in
