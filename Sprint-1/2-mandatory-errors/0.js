44271ba7b060f6ef262c88fae0a083fd97ae2547
boladjebsoft <djebbour1985@gmail.com>
2026-02-25 22:06:55 +0000

    Convert instructions to comments

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,3 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+// to make the computer not running these two line we use inline comments by adding two slashes at the beginning of each line
