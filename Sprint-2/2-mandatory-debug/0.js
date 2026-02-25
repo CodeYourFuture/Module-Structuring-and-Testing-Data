9ba2becf8782a3cc1f4ddf1dc7ebf78ca08229fb
tasleemadedokun <adedokuntasleem@gmail.com>
2026-02-25 16:12:43 +0000

    Implement changes.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,19 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// I am predicting there will be an error. =============> write your prediction here
 
-function multiply(a, b) {
-  console.log(a * b);
-}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+
+
+// Although it is going to print the value to be 320, it won't return back the value as 320.=============> write your explanation here
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+ 
+
+function multiply(a, b) {
+  return (a * b);
+}
+ 
+console.log(`${multiply(10, 32)}`)
+//=============> write your new code here
