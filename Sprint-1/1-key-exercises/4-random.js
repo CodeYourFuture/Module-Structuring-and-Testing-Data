2f3f08d109929057e3b48615d9a6f22a994b4736
boladjebsoft <djebbour1985@gmail.com>
2026-02-25 18:52:47 +0000

    building an idea of what a program is doing
    Added comments to explain the purpose and behavior of the num variable and the random number generation process.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,14 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// the num represents a value with limited interval and that's why
+// the num assigned to a value that's in the dor of an expression
+// the math object is invoked to perform some mathematical tasks
+// the random method is invoked to get a random number between 0 and 1
+// the random number is multiplied by 100 
+// the floor method is invoked to round down the result to its nearest integer
+//  added one to the new result
+// the final result of the whole expression is what the variable num assigned to
+/* after applying the program several times, I got the idea that the program is generating 
+a value to the num variable thats always between minimum an maximum*/  
