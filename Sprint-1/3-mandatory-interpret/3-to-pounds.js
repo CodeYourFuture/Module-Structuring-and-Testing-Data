6b7a4ed26a0c3bdab6b69cbb42caee434e979dd1
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-26 21:51:29 +0000

    evalutated each line of code in 3-to-pounds.js file

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -6,6 +6,7 @@ const penceStringWithoutTrailingP = penceString.substring(
 );
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
@@ -25,6 +26,9 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 /* 1. const penceString = "399p": initializes a string variable with the value "399p"
-   2. const penceStringWithoutTrailingP = This stores the substring of penceString without a trailing p, so "399p" becomes "399"
-   3. const paddedPenceNumberString= 
+   2. const penceStringWithoutTrailingP = This stores the substring of penceString by extracting all characters except the final p. It takes the substring from index 0 up to (but not including) the last character p, so "399p" becomes "399". This removes the unit symbol so only the numeric portion remains.
+   3. const paddedPenceNumberString= This stores the value of penceStringWithoutTrailingP, padded to a total length of 3 characters by adding leading zeros where necessary. This guarantees there are always enough digits to separate pounds and pence correctly.
+   4. const pounds= Stores the substring of paddedPenceNumberString from index 0 to the last 2 index. These leading digits represent the pound portion of the amount.
+   5. const pence=  Extracts the final two digits of the padded string to represent the pence portion. padEnd(2, "0") ensures that the pence value always contains exactly two digits.
+   6. console.log(`£${pounds}.${pence}`); logs the value of pounds and pence into a formatted currency string using template literals
 */
