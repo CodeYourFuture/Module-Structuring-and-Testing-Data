2af6c11e737bc2698345e105d3525c231b626a8b
Pretty Taruvinga <pretty548@gmail.com>
2026-02-26 17:27:32 +0200

    test: add tests for repeatStr

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,22 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(n, str) {
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatStr;
+const repeatStr = require("./repeat-str");
+
+test("repeats a string n times", () => {
+  expect(repeatStr(3, "hello")).toBe("hellohellohello");
+});
+
+test("repeats a different string", () => {
+  expect(repeatStr(2, "abc")).toBe("abcabc");
+});
+
+test("returns empty string when n is 0", () => {
+  expect(repeatStr(0, "hi")).toBe("");
+});
