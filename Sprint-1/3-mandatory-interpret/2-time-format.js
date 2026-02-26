7d1a29ec45a5a40553dfb44b157d36060b864977
marthak1 <marthaogunbiyi196@gmail.com>
2026-02-26 17:58:09 +0000

    interpreted the program in 2-time-formation.js file

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,25 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// 6
 
 // b) How many function calls are there?
+// 1
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+//Remainder operator.
+//The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. 
+// It always takes the sign of the dividend.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// The value stored in totalMinute is calculating the number of minutes that have elapsed in the movie by subtracting the remaining time from the total duration and converting seconds to minutes.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+//The variable result represents a formatted time string in the format HH:MM:SS. 
+// A better name would be "movieDurationDisplay", as these names more clearly describe the purpose and content of the variable.
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// No. Not for all possible values of movieLength, some EdgeCases are: 
+//1. when movieLength is of negative value, the maths produces negative time and its illogical for a movie duration
+//2. if movieLength is not a number, its produces "NAN" and this breaks mathematically
