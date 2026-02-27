b72751f496440c69e9275a957d26afe2af82de00
boladjebsoft <djebbour1985@gmail.com>
2026-02-27 15:22:01 +0000

    describing the purpose behind each step of the code
    Added comments to explain the purpose of each step in the code.

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,8 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// line 3: calling substring method to remove the 'p' letter and keep only digits in the string
+// line 8: using padstart method to make the minimum length of the string to 3 characters by adding one zero or two to the left.
+// line 9: using substring method to take only the integer part
+// line 14: using substring method to take only the decimal part and padend method to make the minimum length of pence to 2 characters by adding zero to the right
+// line 18: calling console function to print the integer part that represents the pound and the decimal part that represents the pence seprated by point
