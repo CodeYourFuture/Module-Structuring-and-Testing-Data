9c9c8946ca759cb999042b8bd1ace4ce83ce78bc
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-27 23:34:21 +0000

    1-count.js committed

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/* The line 3 is increasing the variable count by 1 and assigning the result to count variable on the left hand  * side.
+ */
