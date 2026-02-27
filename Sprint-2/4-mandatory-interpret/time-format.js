77f2ef2f14bd153402cafee56db4da5f7342908d
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-27 11:41:12 +0000

    2-madatory-interpret 2-time-format.js completed formatted

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -20,28 +20,28 @@ console.log(formatTimeDisplay(61));
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
-//It will be called 3 times. 1 for hours, 1 for minutes, 1 for seconds.
-// return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
+//  It will be called 3 times. 1 for hours, 1 for minutes, 1 for seconds.
+//  return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 
-// Call formatTimeDisplay with an input of 61, now answer the following:
-// [Running] node "/home/justice/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-2/4-mandatory-interpret/time-format.js"
-//  00:01:01
+//  Call formatTimeDisplay with an input of 61, now answer the following:
+//  [Running] node "/home/justice/Documents/CYF/Module-Structuring-and-Testing-Data/Sprint-2/4-mandatory-interpret/time-format.js"
+//   00:01:01
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-// The value is 0.
+//  The value is 0.
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
-// The return value is a string "00".
+//  The return value is a string "00".
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-// The value is 1. When the input is 61, It is evaluating number of remaining seconds, 
-// which equals 1.
-// it is evaluating from left to right.
+//  The value is 1. When the input is 61, It is evaluating number of remaining seconds, 
+//  which equals 1.
+//  it is evaluating from left to right.
 // 
 //  e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-// The return value is a string "01" because also because .padStart has been used to ensure that the 
-// value displays as a minimum of 2 digits in this case if less than 2 digits add a leading "0".
+//  The return value is a string "01" because also because .padStart has been used to ensure that the 
+//  value displays as a minimum of 2 digits in this case if less than 2 digits add a leading "0".
