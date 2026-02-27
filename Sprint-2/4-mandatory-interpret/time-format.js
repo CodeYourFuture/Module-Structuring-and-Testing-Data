2eaa8513fb8cec5b09b0494900b689fc2f02304b
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-02-27 16:04:23 +0000

    solved the mandatory interpret

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,24 +11,28 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(60))
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> the first time pad was called with the totalHours
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> "00"
+
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> "1"
+// the last time, pad() is called with the remaining seconds which is value "1"
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> "01"
+// When it is call for the last time it adds 0 before 1 to make it 2 characters long
