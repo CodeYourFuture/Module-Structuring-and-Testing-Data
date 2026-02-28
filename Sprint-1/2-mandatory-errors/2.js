d0a524159be8dbae578dd1da15ad705a73a182fd
Mona-Eltantawy <karamrafat@gmail.com>
2026-02-28 11:24:34 +0000

    in random erros .2 I explained why the code doesn't work and changed the const varible place to be before the console function line.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,6 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
 Because the const varibale is decalred after it is used in the console={.log} statement and it should be decalred befor to be executed. 
