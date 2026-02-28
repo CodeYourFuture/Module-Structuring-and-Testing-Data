1942553db802679a216a27e0df7fbd93f1c62b18
ofonimeedak <ofonimeedak@gmail.com>
2026-02-28 01:18:13 +0000

    fixed time format

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,20 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
-
+// Pad is called once.
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-
+ // value of num=undefined
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// it returns a function object
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+//value is 0
+//the frame side when pad is called the last time num is assigned a value of 0
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
