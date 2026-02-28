da4cf4c040fde350dc787d7fb996df5e4b904245
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-28 00:06:46 +0000

    0.js committed

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+/**
+ * The // tells JavaScript to ignore everything after it on that line, so the computer will not try to execute these instructions as code, but it remains visible for humans to read and understand the program.
+ */
