d2f6fa0035772b0cec7b62f2e5f62e98cc1449c4
Carlos Abreu <carlos.abreu@zoho.com>
2026-02-28 00:17:21 +0000

    1.js committed

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,16 @@
+/**
 // trying to create an age variable and then reassign the value by 1
 
 const age = 33;
+age = age + 1;
+*/
+
+/**
+ * There's a problem with your code. It's trying to reassign a value to a const variable, which is not allowed in JavaScript as const variables cannot be changed after they're declared.
+ * To fix this, you should use let instead:
+*/
+
+// trying to create an age variable and then reassign the value by 1 using the keywork 'let' instead of 'const'
+
+let age = 33;
 age = age + 1;
