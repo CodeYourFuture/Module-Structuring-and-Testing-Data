471439c560380f2fc714df307a8bc8f78e6eecde
Darkidd77 <m.almahdi77@yahoo.com>
2026-02-28 13:55:21 +0000

    Fix function implementations to return correct values in multiply, sum, and getLastDigit functions

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,24 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// I predict that there will be error because the function will not return anything 
+// and when we try to log the result of the function call to the console, it will return undefined.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// The function multiply logs in the result of a * b to the console but does not return anything, 
+// so when we try to log the result of the function call to the console, it will return undefined.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,8 +2,36 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// II predict that the output will be "The last digit of 42 is 3", "The last digit of 105 is 3", 
+// and "The last digit of 806 is 3" because the function getLastDigit is using the variable num
+//  which is assigned the value of 103 using const in global scope, 
+// so it will always return the last digit of 103 which is 3.
 
-const num = 103;
+// const num = 103;
+
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+// it's similar to what I predicted.
+
+// Explain why the output is the way it is
+// =============> write your explanation here
+// The output is the way it is because the function getLastDigit is using the variable num which is assigned 
+// the value of 103 using const in global scope, and the function definition didn't have an argument to receive the number.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
 
 function getLastDigit() {
   return num.toString().slice(-1);
@@ -13,12 +41,3 @@ console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
-
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
