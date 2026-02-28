2467b5fa66e0b6593f3683903b00e3c7a7865416
mervereis <mervereis78@gmail.com>
2026-02-28 22:41:24 +0000

    predicted errors explained and fixed

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// //My prediction` => The capitalise function tries to capitalize the first character of the string and concat it with the rest. This function may not run as it has error.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -9,5 +9,10 @@ function capitalise(str) {
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// call the function with a string argument
+capitalise("hello world");
+// the error is occurring because we are trying to declare a variable with the same name as the function parameter
+
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 
+
+
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// The error will occur because the variable 'decimalNumber' is being redeclared.
 
 // Try playing computer with the example to work out what is going on
 
@@ -14,7 +16,14 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
+// the error is occurring because we are trying to declare a variable with the same name as the function parameter, which is not allowed in JavaScript.
 
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+
+ function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
