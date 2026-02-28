9493010522b06d308981984f564490bd560b5bfd
Divine <divinemankrado1@hotmail.com>
2026-02-28 13:04:20 +0000

    Finished mandatory interpret 1

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -12,11 +12,18 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// There are 4 function calls on lines 4 to 5.
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// Error occurs on line 5 because the second argument to replaceAll is missing quotes.
+// Fix: Change `replaceAll("," ""))` to `replaceAll(",", ""))`
 
 // c) Identify all the lines that are variable reassignment statements
+// Line 4: carPrice = Number(carPrice.replaceAll(",", ""));
+// Line 5: priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // d) Identify all the lines that are variable declarations
+// Line 1 and line 2.
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// The expression removes commas from the string carPrice and converts it to a number.
