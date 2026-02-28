21b30d739d9c85d2f271fea3eab72451e27a17b0
mervereis <mervereis78@gmail.com>
2026-02-28 22:44:19 +0000

    Implement BMI calculation, solved sneak case and pounds functions

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,11 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+function toPounds(penceAmount) {
+  const padded = penceAmount.padStart(3, "0");
+  const pounds = padded.substring(0, padded.length - 2);
+  const pence = padded.substring(padded.length - 2);
+  return `£${pounds}.${pence}p`;
+}
+
+console.log(toPounds("399"));
