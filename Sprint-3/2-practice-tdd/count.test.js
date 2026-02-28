406890d68c902d5a22c21cedacaee532338fc624
Alrzini2025 <jomaalrzini@gmail.com>
2026-02-28 15:11:11 +0000

    removing wrong file

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -17,7 +17,7 @@ test("should count multiple occurrences of a character", () => {
   expect(count).toEqual(5);
 });
 
-// Scenario: No Occurrences in file 
+// Scenario: No Occurrences
 // Given the input string `str`,
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
