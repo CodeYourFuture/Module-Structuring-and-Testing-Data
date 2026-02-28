489671d4e9342f81d2a3b2aed407bb6755025f61
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-02-28 12:11:55 +0000

    Refactor comments in time-format.js to clarify function behavior and provide answers to questions

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,24 +11,27 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(10002));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> pad will be called 3 times, once for each of the hours, minutes and seconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> num is assigned the value of 0, because 61 seconds is equal to 1 minute and 1 second, so the total hours is 0.
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> The return value is "00", because pad pads the number 0 to 2 digits with leading zeros.
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> num is assigned the value of 1, because 61 seconds is equal to 1 minute and 1 second,
+//                so the remaining seconds is 1.
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> The return value is "01", because pad pads the number 1 to 2 digits with leading zeros.
