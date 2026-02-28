3dce6cdce41cdd1412096677a4dfcdc6cc038e9e
AngelaMcLeary <angela.mcleary@outlook.com>
2026-02-28 10:45:47 +0000

    2-practice-tdd count.js updated code

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -7,6 +7,24 @@ function countChar(stringOfCharacters, findCharacter) {
   }
   return count;
 }
-console.log(countChar("please work for me", "e" ))
+//console.log(countChar("please work for me", "e" )) //output is: 3
 
-module.exports = countChar;
+// module.exports = countChar;
+
+// function assertEquals(actualOutput, targetOutput) {
+//   console.assert(
+//     actualOutput === targetOutput,
+//     `Expected ${actualOutput} to equal ${targetOutput}`
+//   );
+// }
+
+// assertEquals(countChar("aaaaa", "a" ), 5);
+// assertEquals(countChar("venue", "b" ), 0);
+// assertEquals(countChar("", "x" ), 0); 
+
+// try {
+//   countChar(12);
+
+//   // This line will not be reached if an error is thrown as expected
+//   console.error("Error was not thrown for invalid string");
+// } catch (error) {}
