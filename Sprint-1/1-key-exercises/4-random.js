f3afc492320154b55467c51acd1c33d8360be045
Abrsh100 <abrdbr2008@gmail.com>
2026-02-28 10:26:40 +0000

    answering acording to the questions

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -8,7 +8,9 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 
-// Answer
+
+
+Answer
 
 // 1. 
 // The num in this scenario represent any integer between 1 and 100
