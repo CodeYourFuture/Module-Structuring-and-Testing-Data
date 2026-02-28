24ab09f1fe4e79159de992450e1041a02f272c0e
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-02-28 19:16:34 +0000

    test: add 2♠ test case per reviewer question + verify returns 2

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -15,10 +15,11 @@ test(`Should return 11 when given an ace card`, () => {
 //   Invalid Cards
 
 // Case 2: Number Cards (2-10)
-test(`Should return correct value for single digit number card`, () => {
+test(`Should return correct value for number cards (2-10)`, () => {
   expect(getCardValue("9♠")).toEqual(9);
   expect(getCardValue("5♦")).toEqual(5);
   expect(getCardValue("10♣")).toEqual(10);
+  expect(getCardValue("2♠")).toEqual(2);
 });
 
 // Case 3: Face cards (J, Q, K)
