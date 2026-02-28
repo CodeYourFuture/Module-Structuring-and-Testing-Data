c4c1e6fe41ad7529f02f673bcef0ef6071260767
Mona-Eltantawy <karamrafat@gmail.com>
2026-02-28 11:33:20 +0000

    In random errors .4 I explained why the code didn't work and that we need to changw the card number to string if we want to use .slice( ) function.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -9,8 +9,8 @@ console.log(last4Digits);
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
-I exepected first that the .slice negative value (-4) will count from the beganning and drob of the first four Number. 
+I exepected first that the .slice negative value (-4) will count from the beganning and drob of the first four Numbers. 
 When I run the code the error was that the cardNumber is not a function, 
 so I thought that I might need to turn the code into a function, 
-but when I checked an AI toolbarit showed that .slice is a methode that is used for string and array ,
- so the card number should turned into a string by addint .tostring after the card number and then use the .slice method to get the last 4 last4Digits.
+but when I checked an AI toolbar it showed that .slice is a methode that is used for string and array ,
+ so the card number should turned into a string by add .tostring after the card number and then use the .slice method to get the last 4 last4Digits.
