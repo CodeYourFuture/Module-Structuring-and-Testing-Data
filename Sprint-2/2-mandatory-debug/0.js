17b209916bfc23103a139ae92aeeb005e2a18e08
mervereis <mervereis78@gmail.com>
2026-02-28 22:42:17 +0000

    fixed return statements and added parameter to function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// The error will occur because the function 'multiply' does not return a value, so when we try to use it will return 'undefined'.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +10,12 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// The error occurs because the 'multiply' function does not have a return statement, so it returns 'undefined' by default.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,6 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+//The output will be 3 in each expression.
 
 const num = 103;
 
@@ -15,10 +16,28 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+// The output is:
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
 // Explain why the output is the way it is
 // =============> write your explanation here
+// The function `getLastDigit` is not taking any arguments, so it always returns the last digit which is 103.
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+// The function `getLastDigit` is not working properly because it was using `num`, 
+// instead of the parameter passed to the function. By changing the function to accept a parameter `num`,
+// it now correctly returns the last digit of the number passed to it..
