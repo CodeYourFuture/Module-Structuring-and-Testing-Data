458871f9fe35f6fb6d70ddba9d61aa111f1fbb18
dcostaprakash <starfinhub@gmail.com>
2026-03-01 23:00:17 +0000

    Made the changes as per the feedback.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -8,7 +8,7 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Function Math.random() generates a decimal number between 0 to 1 which is 0 to 0.9999
 // Funcion (maximum-minimum + 1). Therefore it is 100 - 1 + 1 = 100
 // The * sign will multiply the random number with 100 which becomes 0 to 99.99
-// The function Math.floor will round the new number to the nearest whole number. Hence it will be 0 to 99
+// The function Math.floor will unconditionally round it down. Hence it will be 0 to 99
 // + minimum will add 1 to the new rounded up number Hence the old rance 0 - 99 will now become 1 to 100
 // It will help to think about the order in which expressions are evaluated
 // Done - Try logging the value of num and running the program several times to build an idea of what the program is doing
--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -21,8 +21,8 @@ console.log(result);
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 // % 60 is a remainder operator which in this case divides the total by 60 and shares the remainder (seconds / minutes)
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-// The expression assigned to total minutes is the difference of the total movie time in seconds less the remainder of the seconds divided by 60 which gives the total length of the movie in minutes
+// It gives the total number of minutes in the movie, ignoring any leftover seconds.
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 // The variable result represents the time of the movie in Hours Mintutes and seconds. We could use time instead of result
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-// Yes. This code will work for all values of movie length. I tried with 8400 seconds and 8405 seconds
+// Yes. This code will work for all values of movie length. Decimal numbers or string, NAN, undefined or Null could break the code.
