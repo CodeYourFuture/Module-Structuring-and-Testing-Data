7b87fbc5bbd98c0be4b11fc25882e06c08f409be
boladjebsoft <djebbour1985@gmail.com>
2026-03-01 22:27:55 +0000

    Update 2.js

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,10 +1,7 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> 
-// The last digit of 42 is 3
-// The last digit of 105 is 3
-// The last digit of 806 is 3
+// =============> Write your prediction here
 
 const num = 103;
 
@@ -17,20 +14,11 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============>// The last digit of 42 is 3
-// The last digit of 105 is 3
-// The last digit of 806 is 3
+// =============> write the output here
 // Explain why the output is the way it is
-// =============> the variable num is assigned to the value 103 in the global scope and not assigned to any value in the function scope
+// =============> write your explanation here
 // Finally, correct the code to fix the problem
 // =============> write your new code here
-function getLastDigit(num) { // we add the identier num as parameter to the function getLastDigit
-  return num.toString().slice(-1);
-}
-
-console.log(`The last digit of 42 is ${getLastDigit(42)}`);
-console.log(`The last digit of 105 is ${getLastDigit(105)}`);
-console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
