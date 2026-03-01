7d09f40e315ffe84147e63ce9dc0f5ba7cba979e
boladjebsoft <djebbour1985@gmail.com>
2026-03-01 22:20:30 +0000

    Remove upperSnakeCase function restore the file to the main version
    Removed the upperSnakeCase function implementation and example call.

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,9 +14,3 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
-function upperSnakeCase(wordsSet) {
-  let wsSnakeCase = wordsSet.replaceAll(' ','_');
-  let wsupperSnakeCase = wsSnakeCase.toUpperCase();
-  return console.log(wsupperSnakeCase);
-}
-upperSnakeCase('i do not know'); // evaluates to: "I_DO_NOT_KNOW"
