0280acb9636aa1c8986d448501e136fcb53ac1ce
mervereis <mervereis78@gmail.com>
2026-03-01 23:04:26 +0000

    Refactor getOrdinalNumber function and enhance test coverage for ordinal suffixes

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -13,8 +13,35 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Case 1: Numbers ending with 1 (but not 11)
 // When the number ends with 1, except those ending with 11,
 // Then the function should return a string by appending "st" to the number.
-test("should append 'st' for numbers ending with 1, except those ending with 11", () => {
+test("should append 'st' for numbers ending with 1, except 11", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
   expect(getOrdinalNumber(21)).toEqual("21st");
-  expect(getOrdinalNumber(131)).toEqual("131st");
+  expect(getOrdinalNumber(31)).toEqual("31st");
+  expect(getOrdinalNumber(101)).toEqual("101st");
 });
+
+test("should append 'nd' for numbers ending with 2, except 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(82)).toEqual("82nd");
+});
+
+test("should append 'rd' for numbers ending with 3, except 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(33)).toEqual("33rd");
+  expect(getOrdinalNumber(93)).toEqual("93rd");
+});
+
+test("should append 'th' for the exceptions 11, 12, and 13", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(111)).toEqual("111th");
+});
+
+test("should append 'th' for all other numbers", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(35)).toEqual("35th");
+  expect(getOrdinalNumber(99)).toEqual("99th");
+  expect(getOrdinalNumber(100)).toEqual("100th");
