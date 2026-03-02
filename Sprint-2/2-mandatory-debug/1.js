ad1ca7f006cf83371f2971db688bb3d6901dc382
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-03-02 08:16:08 +0000

    Fixed the function to return the sum of 2 parameters properly
    Corrected the sum function to return the sum of two numbers instead of undefined.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here: js is a sequential language, as in reads code line by line, so i predict nothing will happen after 'return' is processed and 
+//some sort of an error message will appear. 
+
+//function sum(a, b) {
+// return;
+//  a + b;
+//}
+
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here: 'undefined' error appeared. Fixed the code by adding the addition on the same line as return.
+
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here:
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
