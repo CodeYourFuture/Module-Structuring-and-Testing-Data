fc3d35e3019d7fe7ea25ac83ed82e0005c7b1261
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 03:51:49 +0000

    changed const to let and commented why

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,5 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+//const age = 33; needs to be let or the value cannot be reassigned
+let age = 33;
 age = age + 1;
