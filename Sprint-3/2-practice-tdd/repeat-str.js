e56c42cf72b09d9404f4f2ad9c07bed7b693e316
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-03-02 16:41:08 +0000

    repeatStr function code completed

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,9 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count >= 0) {
+    return str.repeat(count);
+  } else {
+    throw new Error("");
+  }
 }
 
 module.exports = repeatStr;
