80c54914981643707225373848dbd5e36d898c23
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 16:44:45 +0000

    removed const and added parameter to function

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// =============> the function has no parameter so will use the 
+// global value of num for num.toString which means it will always 
+// return 3 which is the last digit of 103
 
 const num = 103;
 
@@ -14,11 +16,28 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
+
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> the function has no parameter so will use the 
+// global value of num for num.toString
+
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============> 
+// function getLastDigit(num) {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// removed  const num = 103; and added num as a parameter to the function 
+// getLastDigit so that it can take in the value of num when the function is 
+// called and return the last digit of that number instead of always using 
+// the global value of num which was 103.
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
