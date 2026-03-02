1b7863b3d4c0138e2c1a5422b218a021b19a3583
Carlos Abreu <carlos.abreu@zoho.com>
2026-03-02 22:59:20 +0000

    2.jscommitted

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -10,13 +10,16 @@ const cityOfBirth = "Bolton";
 /**
  * The error is that you're trying to use the variable cityOfBirth in the template literal before it has been declared and initialized.
 
-In JavaScript, you cannot access a const variable before its declaration. This creates a Temporal Dead Zone (TDZ) error.
+In JavaScript, you cannot access a const variable before its declaration. This creates what it's callled in JavaScript a Temporal Dead Zone (TDZ) error.
 
 Fix: Move the console.log statement after the variable declaration:
 */
 
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+
+// The error is:
+// ReferenceError: Cannot access 'cityOfBirth' before initialization
 
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
