d24a40fbf8f0deac240afc77fec30003cce53d5c
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-03-02 15:00:50 +0000

    get-card-value.test.js completed

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -18,3 +18,26 @@ test(`Should return 11 when given an ace card`, () => {
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
 
+// Number Cards (2-10)
+test(`should return the number (numeric value)`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("10♥")).toEqual(10);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("4♣")).toEqual(4);
+  expect(getCardValue("5♦")).toEqual(5);
+})
+
+// Face Cards (J, Q, K)
+test(`should return 10 for face cards ("J", "Q", "K")`, () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♣")).toEqual(10);
+  expect(getCardValue("K♥")).toEqual(10);
+})
+
+// Invalid Cards
+test(`should throw for invalid cards`, () => {
+  expect(() => getCardValue("1♠")).toThrow();
+  expect(() => getCardValue("11")).toThrow();
+  expect(() => getCardValue("♣10")).toThrow();
+  expect(() => getCardValue("♣")).toThrow();
+})
