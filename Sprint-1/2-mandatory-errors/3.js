cfe1b0cc419be2fafefdcc48c59b489108aa7288
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 04:11:02 +0000

    changed to function and commented with alternative changes

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,15 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+
+//cardNumber is a number and slice does not work with numbers only strings
+// to fix this we can convert the number to a string and then use slice 
+// to get the last 4 characters of the string, which will be the last 4 digits of the original number
+// const last4DigitsString = cardNumber.toString().slice(-4);
+// or make it into a function that takes a number as an argument and returns the last 4 digits as a string
+
+function last4Digits(cardNumber) 
+  return `${cardNumber}`.slice(-4);    
+
+console.log(last4Digits(cardNumber)); 
