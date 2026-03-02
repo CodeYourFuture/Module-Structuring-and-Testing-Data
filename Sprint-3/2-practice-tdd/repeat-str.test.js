e4845c4586efe0bb17c91331c1747365f8bcc8c2
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-03-02 16:40:47 +0000

    repeat-str.test.js completed

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,13 +20,33 @@ test("should repeat the string count times", () => {
 // Given a target string `str` and a `count` equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
+test("should return the original `str` without repetition", () => {
+  const str = "action";
+  const count = 1;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("action");
+})
 
 // Case: Handle count of 0:
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
+test("should return an empty string when count == 0", () => {
+  const str = "hardwork";
+  const count = 0;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("");
+})
 
 // Case: Handle negative count:
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
+test("should throw an error", () => {
+  const str = "responsibility";
+  const count = -5;
+  // const repeatedStr = repeatStr(str, count);
+  // In order for it to work, 'the fuction' itself should directly be passed to 'expect'.
+  // Moreover, the function should be wrapped in a function call so jest can run it
+  expect(() => repeatStr(str, count)).toThrow();
+})
