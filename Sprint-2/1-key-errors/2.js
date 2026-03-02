02c8367a275522b201c5863353fe7a622b9864e9
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-03-02 07:43:06 +0000

    Fix square function parameter and return statement
    Corrected the square function to use a valid parameter name and return the square of the input.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,30 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here: error one, 3 cannot be a parameter name, parameters must be variable names, not values. 
+// Error two, num is not defined. There is no variable named num, it would need to match the name inside the function. 
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//    return num * num;
+//}
 
-// =============> write the error message here
+// =============> write the error message here:
 
-// =============> explain this error message here
+//Uncaught SyntaxError: Unexpected number
+
+//Uncaught SyntaxError: Illegal return statement
+
+// =============> explain this error message here:
+
+// first error message came up because JS found a number instead of a variable for the function.
+// second error message came up because in JS return has to be inside of a function. 
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+// =============> write your new code here:
+
+function square(num) {
+    return num * num;
+}
 
 
