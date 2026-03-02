b7fb44c8693096a36f5ee044d0616679a0c49480
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-03-02 14:56:05 +0000

    Add calculateBMI function to compute BMI
    Implement BMI calculation and log result.

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -14,6 +14,12 @@
 // Then when we call this function with the weight and height
 // It should return their Body Mass Index to 1 decimal place
 
-function calculateBMI(weight, height) {
+ function calculateBMI(weight, height) {
+    const bmi = weight / (height * height);
+    return Number(bmi.toFixed(1));
+}
+
+console.log(calculateBMI(88, 1.76));
+    
     // return the BMI of someone based off their weight and height
 }
