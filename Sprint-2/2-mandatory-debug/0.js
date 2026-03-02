a10df18fdd45bbbbace7a110350bc8b851663629
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-03-02 07:57:58 +0000

    erased first console.log and replaced it with 'return' to properly log the result.
    Changed console.log to return in multiply function to fix undefined error.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,23 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> write your prediction here: there will be an error, due to console.log being used incorrectly. Console.log is for displaying but there is no return command to
+//clarify what to send to console.log
+
+//function multiply(a, b) {
+//  console.log(a * b);
+//}
+
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here: I have run the code on node.js and got 'undefined' as an error message. I shall add 'return' instead of the first 'console.log'
+//so that the result can be displayed properly in the second console.log.
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here:
 
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
