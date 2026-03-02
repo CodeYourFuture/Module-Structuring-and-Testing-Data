f5b1cd8d7cc365abdc124c97f7f34148bf190ad7
Divine <divinemankrado1@hotmail.com>
2026-03-02 22:14:49 +0000

    Finished mandatory debug 1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here: The function does not return any value. The a + b should be before the semi-colon and after the return. It will print undefined instead of 42.
 
 function sum(a, b) {
   return;
@@ -8,6 +8,9 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here: It is a logic error as it prints out an unexpected answer, undefined.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// function sum(a, b) {
+//   return a + b;
+//}
