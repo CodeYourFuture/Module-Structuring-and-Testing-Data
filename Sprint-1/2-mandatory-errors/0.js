e49b1fc9297150d3203a587144f36ceb35151447
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 03:48:53 +0000

    making the lines into comments

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,3 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+//putting two forward slashes at the start of the line makes it a comment, which means the computer will ignore it when running the program
