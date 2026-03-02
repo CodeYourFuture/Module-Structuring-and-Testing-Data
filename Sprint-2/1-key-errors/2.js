b2869988cf7c107112b015fe5c1d0377268dbe44
Divine <divinemankrado1@hotmail.com>
2026-03-02 22:08:41 +0000

    Finished Key error 2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,19 @@
 
 // Predict and explain first BEFORE you run any code...
-
 // this function should square any number but instead we're going to get an error
-
-// =============> write your prediction of the error here
+// =============> The error will be a syntax error. A number is not a valid variable name - variable names cannot start with a number.
 
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
+// =============> write the error message here: SyntaxError: Unexpected number. The error is because the variable name cannot start with a number.
 
-// =============> explain this error message here
+// =============> explain this error message here: You cannot begin a variable name with a number
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
-
-
+// =============> write your new code here:
+// function square(num) {
+//   return num * num;
+// }
