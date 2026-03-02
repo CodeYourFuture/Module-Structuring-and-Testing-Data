803168d141256ee6c22ea39eca85b755983a134e
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 20:01:42 +0000

    function to convert pence to pounds and pence

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -1,24 +1,17 @@
-// In Sprint-1, there is a program written in interpret/to-pounds.js
+function getPenceString(penceString){
 
-// You will need to take this code and turn it into a reusable block of code.
-// You will need to declare a function called toPounds with an appropriately named parameter.
-
-// You should call this function a number of times to check it works for different inputs
-//const penceString = "399p";
-
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
-
+const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+    //removes p
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+    //puts zeros in front of number if less than 3 digits
+const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
+    //removes last 2 digits to get pounds
+const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+    //gets last 2 digits to get pence, if less than 2 digits adds zeros to end of string but shouldnt be less than 3
+    //due to padStart above
+return `£${pounds}.${pence}`;
+    // returns string with pounds and pence in correct format
+}
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
-
-console.log(`£${pounds}.${pence}`);
+console.log(getPenceString("399p"));
+    // tested with 123p 1200p 24589p 89p 9p 0p
