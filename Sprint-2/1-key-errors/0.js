0f63b879826e892f1f78c09e0cd8a51d47b48b05
Divine <divinemankrado1@hotmail.com>
2026-03-02 21:59:33 +0000

    I have finished KeyErrors 0

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,18 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The error will be that the variable str is being redefined within the function. It was already defined when it was passed as an arguement.
 
 // call the function capitalise with a string input
+capitalise("hello world");
 // interpret the error message and figure out why an error is occurring
+// Error message: SyntaxError: Identifier 'str' has already been declared. The error is because the variable str has already been declared.
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> I've given the variable a new name, newStr, to avoid redclaring the variable.
+// =============> function capitalise(str) {
+//  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return newStr;
+//}
