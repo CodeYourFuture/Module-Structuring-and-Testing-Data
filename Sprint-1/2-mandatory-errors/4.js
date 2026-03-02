5795f102dfa02722d04796a9394afd33d53906db
Carlos Abreu <carlos.abreu@zoho.com>
2026-03-02 23:04:12 +0000

    4.js committed

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
+/**
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+*/
+
+/**
+ * In JavaScript, variable names cannot start with a number. They must begin with a letter, underscore (_), or dollar sign ($).
+ */
+
+// Here the correct version
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
