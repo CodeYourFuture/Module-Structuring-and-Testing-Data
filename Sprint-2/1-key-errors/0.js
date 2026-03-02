ca7fa61638e4622b192101b5ce9056eba6e75faa
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 11:14:50 +0000

    removed let from the code

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// I think this function is meant to make the first letter of a string into uppercase then return the string
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+//function capitalise(str) {
+ // let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+ // return str;
+//}
+
+// I got the error "SyntaxError: Identifier 'str' has already been declared" 
+// This is because str had been declared in line 7 and let str in line 8 is also trying to declare str
+
+// to fix this, I just removed the word let in line 8. This gave a value to str without trying to declare it again
+// I ran the new code and there were no errors
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
-// =============> write your explanation here
-// =============> write your new code here
