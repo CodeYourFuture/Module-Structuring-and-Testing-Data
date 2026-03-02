599d6dbb22c828778ff0e70916f2167fffe5d328
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-03-02 14:16:30 +0000

    is-proper-fraction.test.js completed

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,3 +8,31 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
+
+test(`should return true when (numerator < denominator)`, () => {
+  expect(isProperFraction(0, 1)).toEqual(true);
+})
+
+test(`should return true when (numerator < denominator)`, () => {
+  expect(isProperFraction(-6, -3)).toEqual(true);
+})
+
+test(`should return true when (numerator < denominator)`, () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+})
+
+test(`should return false when (numerator > denominator)`, () => {
+  expect(isProperFraction(2, 1)).toEqual(false);
+})
+
+test(`should return false when (numerator == denominator)`, () => {
+  expect(isProperFraction(0, 0)).toEqual(false);
+})
+
+test(`should return false when (numerator > denominator)`, () => {
+  expect(isProperFraction(-1, -2)).toEqual(false);
+})
+
+test(`should return false when (numerator > denominator)`, () => {
+  expect(isProperFraction(0, -1)).toEqual(false);
+})
