7d5122257de0563685ed4d7af123bc12f8d4f6eb
Darkidd77 <m.almahdi77@yahoo.com>
2026-03-02 04:21:59 +0000

    Add console log and comments to clarify pad function usage in formatTimeDisplay

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,6 +11,8 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(61));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
@@ -18,17 +20,24 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// pad will be called 3 times, once for totalHours, once for remainingMinutes and once for remainingSeconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// The value assigned to num when pad is called for the first time is 0 (totalHours).
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// The return value of pad when it is called for the first time is "00" 
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The value assigned to num when pad is called for the last time in this program is 1 (remainingSeconds).
+// This is because when we call formatTimeDisplay(61), the remainingSeconds is calculated as 61 % 60 which gives us 1
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// The return value assigned to num when pad is called for the last time in this program is "01".
+// This is because pad(1) returns "01" since 1 is padded to 2 digits with leading zeros.
