578f8167c8116b2243e369c4f90f73a926f94a78
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-02 03:54:36 +0000

    changed order and commented

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+//console.log(`I was born in ${cityOfBirth}`);
+//const cityOfBirth = "Bolton";
+// these are in the wrong order.  Cannot access the value of cityOfBirth before it has been declared.  Need to declare the variable first and then log it to the console.
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
