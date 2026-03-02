6f0c4c813705543fb0ef136a94878fecb8d59f48
Darkidd77 <m.almahdi77@yahoo.com>
2026-03-02 03:19:51 +0000

    Implement toUpperSnakeCase function to convert strings to UPPER_SNAKE_CASE

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,12 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(str) {
+    // return the string in UPPER_SNAKE_CASE
+    
+return str.replaceAll(" ", "_").toUpperCase();
+// I used replaceAll to replace all spaces with underscores and then used toUpperCase to convert the string to uppercase.
+}
+
+console.log(toUpperSnakeCase("hello there"));
