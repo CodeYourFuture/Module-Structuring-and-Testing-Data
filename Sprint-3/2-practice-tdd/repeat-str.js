77bc6bedac78f57aa2fc55d01921e7c2a3dddb5c
Pretty Taruvinga <pretty548@gmail.com>
2026-03-02 13:35:53 +0200

    refactor: simplify repeatStr using String.prototype.repeat

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -8,15 +8,31 @@ function repeatStr(n, str) {
 
 module.exports = repeatStr;
 // const repeatStr = require("./repeat-str");
-
+// Given a target string `str` and a positive integer `count`,
+// When the repeatStr function is called with these inputs,
+// Then it should:
 // test("repeats a string n times", () => {
 //   expect(repeatStr(3, "hello")).toBe("hellohellohello");
 // });
-
+const repeatStr = require("./repeat-str");
+test("repeats a string n times", () => {
+  expect(repeatStr(3, "hello")).toBe("hellohellohello");
+});
 // test("repeats a different string", () => {
 //   expect(repeatStr(2, "abc")).toBe("abcabc");
 // });
-
+test("repeats a different string", () => {
+  expect(repeatStr(2, "abc")).toBe("abcabc");
+});
 // test("returns empty string when n is 0", () => {
 //   expect(repeatStr(0, "hi")).toBe("");
 // });
+test("returns empty string when n is 0", () => {
+  expect(repeatStr(0, "hi")).toBe("");
+});
+// test("returns empty string when n is 0", () => {
+//   expect(repeatStr(0, "hi")).toBe("");
+// });
+test("returns empty string when n is 0", () => {
+  expect(repeatStr(0, "hi")).toBe("");
+});
