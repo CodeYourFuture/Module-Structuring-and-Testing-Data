364b18dec8aaa26dbf11c6740304f46e7044baa4
AngelaMcLeary <angela.mcleary@outlook.com>
2026-03-02 22:34:01 +0000

    1-key-errors 0.js updated code to return value without declaring the variable

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -37,8 +37,7 @@
 // =============> write your new code here
 
 function capitalise(str) {
-  str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
 }
 console.log(capitalise("justice"));
 
