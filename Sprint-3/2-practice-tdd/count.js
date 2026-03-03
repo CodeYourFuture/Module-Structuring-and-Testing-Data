c473ad8d4a3ab65bb427f5118a0b0307a595fea5
Mouawia Elkhalifa <moawia115@hotmail.com>
2026-03-03 09:30:51 +0000

    fix: apply prettier formatting

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,9 +1,11 @@
 function countChar(stringOfCharacters, findCharacter) {
-  let count =0;
-  for(let i=0; i<stringOfCharacters.length; i++){
-  if(stringOfCharacters[i] === findCharacter){
-    count=count+1;}}
-    return count;
+  let count = 0;
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      count = count + 1;
+    }
+  }
+  return count;
 }
-console.log(countChar("banana","a"))//3
+
 module.exports = countChar;
