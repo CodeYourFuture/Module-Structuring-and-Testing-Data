40f4d05fd3f6b770875714cf680150360187fc0d
miriamjorna <hoimir@gmail.com>
2026-03-03 23:43:42 +0000

    Sprint-2, 2-mandatory-debug, 1.js fixed

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// the semicolon after "return" will hamper the return statement because it 
+// will end the statement before it has been defined what it needs to return.
+
 
 function sum(a, b) {
   return;
@@ -9,5 +12,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// as above
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
