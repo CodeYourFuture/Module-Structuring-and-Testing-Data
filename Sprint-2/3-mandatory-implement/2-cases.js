e27fa7b78152d3786a06d9bb6fad15b8126a3e71
Carlos Abreu <carlos.abreu@zoho.com>
2026-03-03 17:31:51 +0000

    2-cases.js committed

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,22 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function convertToUpperSnakeCase(inputString) {
+    // Split the string into words using spaces as separators
+    const words = inputString.split(' ');
+
+    // Convert each word to uppercase using map() function
+    const upperWords = words.map(word => word.toUpperCase());
+
+    // Join the uppercase words with underscores
+    const upperSnakeString = upperWords.join('_');
+
+    return upperSnakeString;
+}
+
+// Test the function
+console.log(convertToUpperSnakeCase("hello there")); // HELLO_THERE
+console.log(convertToUpperSnakeCase("lord of the rings")); // LORD_OF_THE_RINGS
+console.log(convertToUpperSnakeCase("UPPER SNAKE CASE")); // UPPER_SNAKE_CASE
+
