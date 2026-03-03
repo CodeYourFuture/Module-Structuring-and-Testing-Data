0a5221c28039607870df4911712156af24955b09
miriamjorna <hoimir@gmail.com>
2026-03-03 22:39:09 +0000

    Working on Coursework Sprint 2: 1-key-errors, first problem

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> This will not run because the variable str has already been used
+// as a parameter. This will cause a syntax error 
+
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+ let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+ return str;
+ }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> The first mistake this renders is indeed in line 14 that 
+// "Identifier 'str' has already been declared" 
+// =============> 
+// 
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
