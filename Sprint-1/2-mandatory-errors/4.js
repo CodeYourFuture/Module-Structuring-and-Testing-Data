90e26259babc7c874243152eec8296892ed9c3db
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-03 09:20:00 +0000

    made variable names lower case first letter

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,4 +1,4 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
 // variable names cannot begin with a number
-// can change to ClockTime12Hour and ClockTime24Hour or something similar to make it valid
+// can change to clockTime12Hour and clockTime24Hour or something similar to make it valid
