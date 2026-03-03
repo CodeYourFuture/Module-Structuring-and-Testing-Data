244451e8f4cb289935cffa35eac47746c78a1ad3
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-03 09:10:09 +0000

    completed exercises

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -1,6 +1,7 @@
 // This is the latest solution to the problem from the prep.
 // Make sure to do the prep before you do the coursework
-// Your task is to write tests for as many different groups of input data or edge cases as you can, and fix any bugs you find.
+// Your task is to write tests for as many different groups of input data or edge cases as you can, 
+// and fix any bugs you find.
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
@@ -23,3 +24,5 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+console.log formatAs12HourClock("14:00"); // 2pm
