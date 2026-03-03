440f0f15baf40b42b73a820801fbf8b687954430
miriamjorna <hoimir@gmail.com>
2026-03-03 23:24:16 +0000

    Sprint-2, 1-key-errors, 2.js done

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,26 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// num has not been defined and the parameter is not used in the function;
+// it has a number instead
+
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
+
 
 // =============> explain this error message here
+// The error message is saying that it was not expecting a number in the function
+// declaration. This is because the parameter should be a variable name, not a number 
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
+function square(num) {
+    return num * num;
+}
 
