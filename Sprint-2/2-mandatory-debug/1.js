89c0e114e07732ba2ffd67c37923b154836ad2e3
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-04 21:29:01 +0000

    explained that th function sum will not return any value because the return statement comes befor the value a +b and changed it to the correct order.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// 
 
 function sum(a, b) {
   return;
@@ -9,5 +10,13 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+when I run the program it give this output:
+The sum of 10 and 32 is undefined
+
+the output is undefiened because the function sum does not return any CSSMathValue. to fix this error we need to change the return statement to return a + b instead of just return. 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
