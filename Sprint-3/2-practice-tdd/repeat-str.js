208f354dec56c437d35ff56bbb75b95f54ae8073
dcostaprakash <starfinhub@gmail.com>
2026-03-04 23:12:45 +0000

    Glasgow | 26-ITP-JAN | Prakash Dcosta | Sprint 3 | Practice TDD Self checklist
    - [X] I have titled my PR with Region | Cohort | FirstName LastName | Sprint | Assignment Title
    - [X] My changes meet the requirements of the task
    - [X] I have tested my changes
    - [X] My changes follow the [style guide](https://curriculum.codeyourfuture.io/guides/reviewing/style-guide/)
    
    ## Changelist
    
    Briefly explain your PR.
    Have made changes to the file as per the coursework

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,14 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Count cannot be negative");
+  }
+
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+
+  return result;
 }
 
 module.exports = repeatStr;
