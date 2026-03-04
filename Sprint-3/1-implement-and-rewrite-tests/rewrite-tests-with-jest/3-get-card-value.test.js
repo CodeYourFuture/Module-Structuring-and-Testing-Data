3bba9c30139dadaccd85e961ff0baa1d61f81b70
AngelaMcLeary <angela.mcleary@outlook.com>
2026-03-04 20:03:13 +0000

    updated 3-get-card-value-test.js with grouped testing and loops on some tests also formatted code

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -4,52 +4,97 @@ const getCardValue = require("../implement/3-get-card-value");
 
 // TODO: Write tests in Jest syntax to cover all possible outcomes.
 
-// Case 1: Ace (A)
-test(`Should return 11 when given an ace card`, () => {
-  expect(getCardValue("A♠")).toEqual(11)});
+// Case 1: Ace (A) - works
+// test(`Should return 11 when given an ace card`, () => {
+//   expect(getCardValue("A♠")).toEqual(11);
+// });
+
 // Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-test(`Should return a number given 9 card` , ()=> {
-  expect(getCardValue("9♠")).toEqual(9)});
-//   Face Cards (J, Q, K)
-test(`Should return 10 when given a Queen card`, () => {
-  expect(getCardValue("Q♥")).toEqual(10);
-});
-test(`Should return 10 when given a King card`, () => {
-  expect(getCardValue("K♦")).toEqual(10);
+//   Number Cards (2-10) - works
+// test(`Should return a number given (2-10) card`, () => {
+//   expect(getCardValue("2♠")).toEqual(2);
+//   expect(getCardValue("3♥")).toEqual(3);
+//   expect(getCardValue("4♦")).toEqual(4);
+//   expect(getCardValue("5♣")).toEqual(5);
+//   expect(getCardValue("6♠")).toEqual(6);
+//   expect(getCardValue("7♥")).toEqual(7);
+//   expect(getCardValue("8♦")).toEqual(8);
+//   expect(getCardValue("9♣")).toEqual(9);
+//   expect(getCardValue("10♠")).toEqual(10);
+// });
+
+//   Face Cards (J, Q, K) - works
+// test(`Should return 10 when given a Queen, King and Jack cards`, () => {
+//   expect(getCardValue("Q♥")).toEqual(10);
+//   expect(getCardValue("K♦")).toEqual(10);
+//   expect(getCardValue("J♣")).toEqual(10);
+// });
+
+//   Invalid Cards - works
+// test('Should throw "Invalid card" for "invalid"', () => {
+//   expect(() => getCardValue("invalid")).toThrow("Invalid card");
+//   expect(() => getCardValue("789")).toThrow("Invalid card");
+//   expect(() => getCardValue("")).toThrow("Invalid card");
+//   expect(() => getCardValue("♠♥♦♣")).toThrow("Invalid card");
+//   expect(() => getCardValue("5$")).toThrow("Invalid card");
+//   expect(() => getCardValue("£50")).toThrow("Invalid card");
+// });
+
+// **Invalid Cards - this only checks the array of invalid input. I have to think about how to
+// use the loop here in the reverse order, what is valid or else return invalid!**
+// test('should throw "Invalid card" for invalid inputs', () => {
+//   const invalidCards = ["invalid", "789", "", "♠♥♦♣", "5$", "£50"];
+
+//   for (const card of invalidCards) {
+//     expect(() => getCardValue(card)).toThrow("Invalid card");
+//   }
+// });
+
+// Case 1: Ace (A)
+test("should return a value of 11 when given Ace cards", () => {
+  const validSuits = ["♠", "♥", "♦", "♣"];
+
+  for (let i = 0; i < validSuits.length; i++) {
+    const card = "A" + validSuits[i];
+    expect(getCardValue(card)).toEqual(11);
+  }
 });
 
-test(`Should return 10 when given a Jack card`, () => {
-  expect(getCardValue("J♣")).toEqual(10);
+// Suggestion: Group the remaining test data into these categories:
+// Case 2: Number Cards (2-10)
+test("should return the value of number cards (2-10)", () => {
+  const validSuits = ["♠", "♥", "♦", "♣"];
+  for (let rank = 2; rank <= 10; rank++) {
+    for (let suit of validSuits) {
+      const card = `${rank}${suit}`;
+      expect(getCardValue(card)).toEqual(rank);
+    }
+  }
 });
 
-//   Invalid Cards
+// Case 3: Face Cards (J, Q, K)
+test("should return a value of 10 for cards (J, Q, K)", () => {
+  const validRanks = ["J", "Q", "K"];
+  const validSuits = ["♠", "♥", "♦", "♣"];
+
+  for (let rank of validRanks) {
+    for (let suit of validSuits) {
+      const card = `${rank}${suit}`;
+      expect(getCardValue(card)).toEqual(10);
+    }
+  }
+});
+
+// Case 4: Invalid Cards
 test('Should throw "Invalid card" for "invalid"', () => {
   expect(() => getCardValue("invalid")).toThrow("Invalid card");
-});
-
-test('Should throw "Invalid card" for input "789"', () => {
   expect(() => getCardValue("789")).toThrow("Invalid card");
-});
-
-test('Should throw "Invalid card" for input ""', () => {
   expect(() => getCardValue("")).toThrow("Invalid card");
-});
-
-test('Should throw "Invalid card" for input "♠♥♦♣"', () => {
   expect(() => getCardValue("♠♥♦♣")).toThrow("Invalid card");
-});
-
-test('Should throw "Invalid card" for input "5$"', () => {
   expect(() => getCardValue("5$")).toThrow("Invalid card");
-});
-
-test('Should throw "Invalid card" for input "£50"', () => {
   expect(() => getCardValue("£50")).toThrow("Invalid card");
 });
-
 
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
-
