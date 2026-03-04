b61cf2162c400f6cc0a07eb43954e915073877d1
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-04 19:53:03 +0000

    predicted the output and explained the error in 0.js file

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,26 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The function when called was supposed to capitalise the first letter of a string by calling the first character of the string and then transforming 
+// to uppercase character and adding it back to the string, but because the variable "str" had already been declared it going to throw a syntaxerror
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
-
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+// console.log(capitalise("what is your name?"))
 // =============> write your explanation here
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//       ^
+
+// SyntaxError: Identifier 'str' has already been declared
+// As predicted,when the function is called, it does not compile and throws a syntax error because the identifier "str" had already been declared as a parameter of the function. This violate the rules of JavaScript
+// Variable can be re-assigned using the "let" keyword but cannot be redeclared
 // =============> write your new code here
+function capitalise(str) {
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisedStr;
+}
+console.log(capitalise("tell me about yourself"))
