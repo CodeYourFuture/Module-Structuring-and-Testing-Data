206dc6822662495b1a28f6dfbc777b291ea34faa
Richard Frimpong <richardfrimpongalx@gmail.com>
2026-03-04 04:10:02 +0000

    Implement ordinal number suffix rules

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,26 @@
-function getOrdinalNumber(num) {
-  return "1st";
+function getOrdinalNumber(number) {
+  const lastTwoDigits = number % 100;
+
+  // Special cases: 11, 12, 13 always use "th"
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return `${number}th`;
+  }
+
+  const lastDigit = number % 10;
+
+  if (lastDigit === 1) {
+    return `${number}st`;
+  }
+
+  if (lastDigit === 2) {
+    return `${number}nd`;
+  }
+
+  if (lastDigit === 3) {
+    return `${number}rd`;
+  }
+
+  return `${number}th`;
 }
 
 module.exports = getOrdinalNumber;
