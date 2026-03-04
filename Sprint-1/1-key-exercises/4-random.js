c23bfd0d02b7b22e15ac80042f725e3ca661edf8
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-04 08:38:14 +0000

    change comment about math.random

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -12,7 +12,7 @@ console.log(num);
 
 //num is a random number between 1 and 100 including 1 and 100 [1,100]
 //Math.floor rounds the decimal number to nearest lower number
-// Math.random() generates a random decimal number between 0 and 1 including 0 and 1 [0,1]
+// Math.random() generates a random decimal number between 0 and 1 including 0 but not including 1 [0,1)
 // when max – min need to add +1 or not all the numbers will be 
 // included for instance max=5 min=1 5-1=4 but there are 
 // 5 numbers, 1, 2, 3, 4,5 so need to +1
