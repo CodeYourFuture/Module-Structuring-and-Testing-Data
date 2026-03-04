abe79e9adef999cd18e44f401789c4b23bef437c
miriamjorna <hoimir@gmail.com>
2026-03-04 00:11:44 +0000

    Sprint-2, 2-mandatory-debug, 2.js fixed

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,6 +2,11 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// The "const" is hampering the function getLastDigit because the return 
+// function will always calculate with the value of the const and ignore any 
+// other input
+
+
 
 const num = 103;
 
@@ -15,10 +20,26 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+//The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
 // Explain why the output is the way it is
 // =============> write your explanation here
+// The function getLastDigit is not working properly because it is using the variable 
+// num, which is defined outside the function with the value of 103. It will always 
+// return the last digit of 103 instead of the given parameter in the template literal
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
