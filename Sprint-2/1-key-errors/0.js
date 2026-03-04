f18f2afb8c51a49571d0f3423c6980c1334d2ff1
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-04 20:41:16 +0000

    in key errors 1.js explained the predicated errors and explained the syntax error occurs when the program runs and write the new code that doesn't redeclare the decimalNumber in a const variable in inside the function

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,14 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> 
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
+
 
 // =============> write your explanation here
 // =============> write your new code here
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,8 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+=============> write your prediction here: the error will occur because the variable decimslNumber gevine a value of 0.5
+ with a const declaration and this will make an error if the brograme given a diffrent value for the decimalNumber.
 
 // Try playing computer with the example to work out what is going on
 
@@ -14,7 +15,18 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
+// =============> write your explanation here 
+when the program run it give a SyntaxError: Identifier 'decimalNumber' has already been declared. This is because the variable decimalNumber is declared twice in the function
+convertToPercentage, and inside the function with the constant varible CSSStyleDeclaration. also using the function name decimalNumber with the console.log function will give an error because the function name should be
+to fix this error we can remove the const variable declaration of decimalNumber from the function and use the function name
+  'ConvertToPercentage' to recall the function and pass the value of the decimalNumber.CSSStyleDeclaration
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
