84a56addb5ad9ee09ef8e8581fd9b3c21a49b258
Carlos Abreu <carlos.abreu@zoho.com>
2026-03-04 21:49:42 +0000

    refactored and cleaned dead code

--- Sprint-3/3-dead-code/exercise-2.js
+++ Sprint-3/3-dead-code/exercise-2.js
@@ -1,3 +1,6 @@
+/**
+ * Original file:
+ *
 // Remove the unused code that does not contribute to the final console log
 // The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
 
@@ -26,3 +29,31 @@ function countAndCapitalisePets(petsArr) {
 const countedPetsStartingWithH = countAndCapitalisePets(petsStartingWithH);
 
 console.log(countedPetsStartingWithH); // { 'HAMSTER': 3, 'HORSE': 1 } <- Final console log
+*
+* End of file
+*/
+
+// Remove the unused code that does not contribute to the final console log
+// The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
+
+const pets = ["parrot", "hamster", "horse", "dog", "hamster", "cat", "hamster"];
+const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
+
+function countAndCapitalisePets(petsArr) {
+  const petCount = {};
+
+  petsArr.forEach((pet) => {
+    const capitalisedPet = pet.toUpperCase();
+    if (petCount[capitalisedPet]) {
+      petCount[capitalisedPet] += 1;
+    } else {
+      petCount[capitalisedPet] = 1;
+    }
+  });
+  return petCount;
+}
+
+const countedPetsStartingWithH = countAndCapitalisePets(petsStartingWithH);
+
+console.log(countedPetsStartingWithH); // { 'HAMSTER': 3, 'HORSE': 1 } <- Final console log
+
