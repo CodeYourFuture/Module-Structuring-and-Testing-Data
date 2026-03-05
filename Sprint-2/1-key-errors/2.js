4c0e0ebafdbb1af7ee13d325066c6ab23da1bacd
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-05 22:05:47 +0000

    predict, explain and code

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,33 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> I think that return can only return a value not do a calculation 
+// so this will return an error because it is trying to do a calculation instead of returning a value
 
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
 
-// =============> explain this error message here
+function square(3) {
+    return num * num;
+}
+
+// =============> SyntaxError: Unexpected number
+
+// =============> this is because variables and parameters in functions cannot start with a number or be a number
+// however, return can be used for calculations so my prediction was wrong
+
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+// =============> 
+// function square (num) {
+//   return num * num;
+// }
+// console.log(square(5));
+
+// ran console.log to ensure it works
+
 
 
