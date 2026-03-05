45e94c75788484392b32d32c09d7caa7e8e2982a
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-05 19:25:46 +0000

    exlained the output of the function that takes no parameter in 2.js

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,23 +2,37 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// The function will always return 3
 
+// const num = 103;
+
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// Explain why the output is the way it is
+// =============> write your explanation here
+//getLastDigit takes no parameters.
+//Inside the function, will always return the last digit of the global variable num, which is "3". 
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+// This program should tell the user the last digit of each number.
+// Explain why getLastDigit is not working properly - correct the problem
+//Every call ignores the number that is passed in the argument and just returns "3"
 const num = 103;
 
-function getLastDigit() {
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
-
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
