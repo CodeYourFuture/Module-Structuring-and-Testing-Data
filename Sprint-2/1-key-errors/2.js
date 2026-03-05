c14ac01c154372ac680b056e5d991ad32131b8e6
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-05 07:58:38 +0000

    evaluated the code and explained errors in 2.js file

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,28 @@
 
 // Predict and explain first BEFORE you run any code...
-
 // this function should square any number but instead we're going to get an error
+//
 
 // =============> write your prediction of the error here
-
-function square(3) {
-    return num * num;
-}
+// // This code would throw a syntaxerror "missing valid function parameter".
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+// function square(3) {
+//                 ^
 
+// SyntaxError: Unexpected number
 // =============> explain this error message here
+// The JavaScript exception "missing formal parameter" occurs when your function declaration is missing valid parameters.
+// In the declaration of a function, the parameters must be identifiers, not any value like numbers, strings, or objects.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+    return num * num;
+}
+console.log(square(10));
