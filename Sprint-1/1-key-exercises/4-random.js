dc4066cd602c1dc4af7b42fec657567aa4eb1153
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-03-05 16:12:04 +0000

    rewriting the comments to be precise according to the programing specification

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -9,8 +9,8 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 
 // math.floor will make the number as a whole and remove any decimals or more likely to round the the nearest whole number
-// math.random will generate a random number from 0 to 1 but not 1 
+// math.random will generate a random number between (0,1) inclusivly
 // (maximum - minimum + 1) provide a range of generated random number 
-// num is a random whole number between 1 and 100 
+// num is a random whole number (1,100) inclusivly 
 
 console.log(num);
