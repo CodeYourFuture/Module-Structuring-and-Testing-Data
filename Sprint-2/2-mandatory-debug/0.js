df1dd85b8c67134e63f4eb3ed56ef199af0d5e47
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-05 08:11:48 +0000

    evaluated the program in 0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// This function only logs the evaluation and would compile as undefined when called
 
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+// The function does not have a return statement, it only logs the evaluation inside the function, but calling the function without a return statement would not return the evaluation. it would return undefined
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
