a8778acd75d04d7619479d3033df4c8aa980a473
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-05 18:55:29 +0000

    explained return statement in 1.js file

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The function would return undefined
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+// When a return statement is used in a function body, the execution of the function is stopped, therefore, evaluation a+b cannot be reached.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
