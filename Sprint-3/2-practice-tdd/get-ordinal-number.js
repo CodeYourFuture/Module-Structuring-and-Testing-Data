94b050b8dd91ca55ada74c99f3836eaeb338d1cd
Divine <divinemankrado1@hotmail.com>
2026-03-06 17:05:14 +0000

    Finished get ordinal numberFinished get-ordinal number and the test

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,22 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const str = num.toString();
+  const lastTwo = str.slice(-2);
+  const lastDigit = str.slice(-1);
+
+  let suffix;
+  if (lastTwo === '11' || lastTwo === '12' || lastTwo === '13') {
+    suffix = 'th';
+  } else if (lastDigit === '1') {
+    suffix = 'st';
+  } else if (lastDigit === '2') {
+    suffix = 'nd';
+  } else if (lastDigit === '3') {
+    suffix = 'rd';
+  } else {
+    suffix = 'th';
+  }
+
+  return str + suffix;
 }
 
 module.exports = getOrdinalNumber;
