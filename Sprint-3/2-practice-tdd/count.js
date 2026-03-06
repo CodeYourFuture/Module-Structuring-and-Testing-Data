0141efbdaf63726981fd93b7fbd13dbe4fbdbc23
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-03-06 09:31:40 +0000

    (count.js) Implement character counting logic in countChar function and (count.test.js) add test for zero occurrences

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,17 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  // 1. Assign a variable to keep track of the count
+  let count = 0;
+
+  // 2. Loop through all chars in the string
+  for (let char = 0; char < stringOfCharacters.length; char++) {
+    // 3. Check if the current char matches the one we are looking for
+    if (stringOfCharacters[char] === findCharacter) {
+      count++; // If it's a match, add 1 to our tally
+    }
+  }
+
+  // 4. Return the final tally after the loop finishes
+  return count;
 }
 
 module.exports = countChar;
