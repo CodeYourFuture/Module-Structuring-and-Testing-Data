f047e5961920589386749916936fcb59aec496ec
Maryanne Mosonik <chmosonik@gmail.com>
2026-03-07 02:06:33 +0300

    Added the word integer

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,6 +1,6 @@
-function repeatStr(str,count) {
+function repeatStr(str, count) {
   if (count < 0) {
-    throw new Error("Count must be a non-negative");
+    throw new Error("Count must be a non-negative integer");
   }
   return str.repeat(count);
 }
