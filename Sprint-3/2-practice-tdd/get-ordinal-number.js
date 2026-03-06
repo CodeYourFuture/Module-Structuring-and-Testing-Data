4a9765be8c7dbf97f7a251eaeba27181e160a390
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-06 15:10:01 +0000

    Implement the function and test.

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,26 @@
+
 function getOrdinalNumber(num) {
-  return "1st";
+  const absNum = Math.abs(num); // handle negative numbers
+  const lastTwo = absNum % 100; // get last two digits
+  const lastDigit = absNum % 10; // get last digit
+
+  if (lastTwo >= 11 && lastTwo <= 13) {
+    return num + "th"; // exceptions: 11,12,13
+  }
+
+  switch (lastDigit) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
+console.log(getOrdinalNumber(12)); //printed 12th.
+console.log(getOrdinalNumber(123)); // printed 123rd.
+console.log(getOrdinalNumber(52));  // printed 52nd.
 
 module.exports = getOrdinalNumber;
