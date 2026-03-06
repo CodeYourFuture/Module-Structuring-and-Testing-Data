a69ca5da55b3c2d7ff00f40c90ed9d7997506965
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-06 12:54:53 +0000

    Effect Changes to the count.js and count.test.js

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,15 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let count = 0;
+
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countChar;
+console.log(countChar("aaaaa", "a"));//print 5
+console.log(countChar("aaaaa", "z"));//print 0
