69ae220b1e666c3f767aa3fcd1a6c80955a97bb0
boladjebsoft <djebbour1985@gmail.com>
2026-03-07 19:23:24 +0000

    formatting using prettier

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -6,15 +6,22 @@
 // You should call this function a number of times to check it works for different inputs
 
 function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
 
-const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
 
-const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
-
-return console.log(`£${pounds}.${pence}`);
+  return console.log(`£${pounds}.${pence}`);
 }
-toPounds('399p') // £3.99
-toPounds('1095p') // £10.95
+toPounds("399p"); // £3.99
+toPounds("1095p"); // £10.95
