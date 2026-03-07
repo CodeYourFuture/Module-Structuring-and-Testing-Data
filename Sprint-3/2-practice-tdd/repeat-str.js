d4aba95f7b778c063682d798637de8a6ff6c9b5b
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-03-07 09:37:35 +0000

    Implement repeatStr function to handle string repetition based on input count

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,25 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(stringToRepeat, numberOfRepetitions) {
+  // Handle count of 0
+  if (numberOfRepetitions === 0) {
+    return "";
+  }
+
+  // Handle count of 1
+  if (numberOfRepetitions === 1) {
+    return stringToRepeat;
+  }
+
+  // Handle multiple repetitions (> than 1)
+  if (numberOfRepetitions > 1) {
+    let finalString = "";
+
+    // loop for repeating the string and concatenating it to the final string
+    for (let repetition = 0; repetition < numberOfRepetitions; repetition++) {
+      finalString = finalString + stringToRepeat;
+    }
+
+    return finalString;
+  }
 }
 
 module.exports = repeatStr;
