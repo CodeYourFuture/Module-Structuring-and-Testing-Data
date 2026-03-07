ecbbbef7a71079fdfaf0aa0f534b9ab0a391f5a4
boladjebsoft <djebbour1985@gmail.com>
2026-03-07 19:22:42 +0000

    formatting using prettier

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -15,8 +15,8 @@
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 function upperSnakeCase(wordsSet) {
-  let wsSnakeCase = wordsSet.replaceAll(' ','_');
+  let wsSnakeCase = wordsSet.replaceAll(" ", "_");
   let wsupperSnakeCase = wsSnakeCase.toUpperCase();
   return console.log(wsupperSnakeCase);
 }
-upperSnakeCase('i do not know'); // evaluates to: "I_DO_NOT_KNOW"
+upperSnakeCase("i do not know"); // evaluates to: "I_DO_NOT_KNOW"
