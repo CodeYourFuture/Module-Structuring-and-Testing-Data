98d12151f9b37f0ba59008b919785fee7293432e
Zadri <zadriabdule@gmail.com>
2026-03-07 01:10:42 +0000

    Sprint 3: implement solutions and rewrite tests with Jest

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -7,11 +7,19 @@
 // complete the rest of the tests and cases
 // write one test at a time, and make it pass, build your solution up methodically
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
-function getCardValue(card) {
+function getCardValue(card){
+  const rank = card.slice(0, -1);
   if (rank === "A") {
     return 11;
   }
+  if (rank === "10" || rank === "J" || rank === "Q" || rank === "K") {
+    return 10;
+  }
+
+throw new Error ("Invalid card rank"); {
+
 }
+
 
 // The line below allows us to load the getCardValue function into tests in other files.
 // This will be useful in the "rewrite tests with jest" step.
@@ -39,19 +47,25 @@ assertEquals(aceofSpades, 11);
 // When the function is called with such a card,
 // Then it should return the numeric value corresponding to the rank (e.g., "5" should return 5).
 const fiveofHearts = getCardValue("5♥");
+assertEquals(fiveofHearts, 5);
 // ====> write your test here, and then add a line to pass the test in the function above
 
 // Handle Face Cards (J, Q, K):
 // Given a card with a rank of "10," "J," "Q," or "K",
 // When the function is called with such a card,
 // Then it should return the value 10, as these cards are worth 10 points each in blackjack.
+const king = getCardValue("K♣");
+assertEquals(king, 10);
 
 // Handle Ace (A):
 // Given a card with a rank of "A",
 // When the function is called with an Ace,
 // Then it should, by default, assume the Ace is worth 11 points, which is a common rule in blackjack.
-
-// Handle Invalid Cards:
-// Given a card with an invalid rank (neither a number nor a recognized face card),
-// When the function is called with such a card,
-// Then it should throw an error indicating "Invalid card rank."
+const ace = getCardValue("A♠");
+assertEquals(ace, 11);
+try {
+  getCardValue("Z♠");
+} catch (error) {
+  assertEquals(error.message, "Invaild card rank");
+}
+}
