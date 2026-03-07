b3b93d6eb577d38611a4d2751769f7996b4446ab
boladjebsoft <djebbour1985@gmail.com>
2026-03-07 16:22:14 +0000

    formatting using prettier

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,4 @@
 // trying to create an age variable and then reassign the value by 1
 
-let age = 33; // we should use the keyword 'let' to declare the variable 'age' to allow reassignment later 
+let age = 33; // we should use the keyword 'let' to declare the variable 'age' to allow reassignment later
 age = age + 1;
