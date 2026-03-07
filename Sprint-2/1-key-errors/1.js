b194762fbbc5dbcfdb239e2a934995cf31c26fac
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-07 19:07:24 +0000

    removed the lcocal variable within the function scope to pass values as argument

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -21,10 +21,8 @@
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
-
   return percentage;
 }
 
-console.log(convertToPercentage());
+console.log(convertToPercentage(0.5));
