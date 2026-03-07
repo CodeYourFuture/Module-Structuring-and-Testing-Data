c4c751f330a450df8d2dc1b43b1e3fc21b9e6db0
boladjebsoft <djebbour1985@gmail.com>
2026-03-07 16:53:58 +0000

    formatting using prettier and removing suggestion

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,6 +1,3 @@
 const _12HourClockTime = "20:53"; // identifier can not begin with a number
-// or
-const $12HourClockTime = "20:53"; // or any change that makes the identifier not stating with digit
+
 const _24hourClockTime = "08:53"; // identifier can not begin with a number
-// or
-const $24hourClockTime = "08:53"; // or any change that makes the identifier not stating with digit
