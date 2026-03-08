9027d5dd66e81628ad0a018dc86e296d8de29d8b
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-08 08:21:17 +0000

    I prediacted that the error aill cbe because of the double declaration of the str identifier and this what happened when i tested the programe. I suggested to change the let variable name into an new name 'newStr' and this fixed the issue.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 //  =============> 
-
+I predicte that the error will be that the variable str is already declared in the function. 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return newStr;
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
 }
 
 
-// =============> write your explanation here
+// =============> write your explanation here 
+when I run the program it give an error message that the identifier str has already been declared. this is because the variable str is declared twice in the function capitalise and in the variable let str.
+to fix this we can change the variable name of the let str to newStr and this will fix the issue. 
 // =============> write your new code here
+function capitalise(str) { 
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
+}
