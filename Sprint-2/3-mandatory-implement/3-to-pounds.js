45d6ec829e7664b385d326c0ba03240aa06b41ba
KK Tech <dyxj6n786p@privaterelay.appleid.com>
2026-03-08 13:12:18 +0000

    improved my work regarding to the feedback

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -5,9 +5,11 @@
 
 // You should call this function a number of times to check it works for different inputs
 
-function toPounds(pence) {
-    return "£" + (pence / 100).toFixed(2);
+function toPounds(penceString) {
+  const penceNumber = Number(penceString.slice(0, -1));
+  return "£" + (penceNumber / 100).toFixed(2);
 }
-console.log(toPounds(399));
-console.log(toPounds(250));
-console.log(toPounds(100));
+
+console.log(toPounds("399p"));
+console.log(toPounds("250p"));
+console.log(toPounds("100p"));
