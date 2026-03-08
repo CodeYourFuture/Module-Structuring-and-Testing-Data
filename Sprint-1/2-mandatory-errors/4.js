9d78d99846e8d05f41cbc586763e1aabb8d3dedb
boladjebsoft <djebbour1985@gmail.com>
2026-03-08 16:32:11 +0000

    seggesting to begin a variable name with a lowercase letter.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,3 +1,6 @@
 const _12HourClockTime = "20:53"; // identifier can not begin with a number
-
+//or
+// const twelveHourClockTime = "20:53"; // this would be a valid identifier
 const _24hourClockTime = "08:53"; // identifier can not begin with a number
+// or
+// const twentyFourHourClockTime = "08:53"; // this would be a valid identifier
