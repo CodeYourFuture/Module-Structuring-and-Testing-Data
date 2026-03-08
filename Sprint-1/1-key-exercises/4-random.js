7a2b695847b598917459f24a3c847de05bbe33fa
boladjebsoft <djebbour1985@gmail.com>
2026-03-08 15:49:05 +0000

    demonstrating precisely what's included and what's excluded.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -11,10 +11,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // the num represents a value with limited interval and that's why
 // the num assigned to a value that's in the dor of an expression
 // the math object is invoked to perform some mathematical tasks
-// the random method is invoked to get a random number between 0 and 1
+// the random method is invoked to get a random number between 0 (inclusive) and 1 (exclusive)
 // the random number is multiplied by 100
 // the floor method is invoked to round down the result to its nearest integer
-//  added one to the new result
+// added one to the new result
 // the final result of the whole expression is what the variable num assigned to
 /* after applying the program several times, I got the idea that the program is generating 
-a value to the num variable thats always between minimum an maximum*/
+a value to the num variable thats always between minimum wich is number 1 (included) and maximum wich is number 100 (included) */
