375754a1b32b91bd958efdea5cecf9ada6f03776
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-08 08:45:32 +0000

    Added the term 'increment to the description.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -5,7 +5,7 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 
-line 3 is updating the value of the count variable by adding 1 to its current value.
+line 3 is and incerment operation that increasing the value of the count variable by adding 1 to its current value.
  The = opperator is an assignment opperator that assigns the result of the expression on the right (count+1) to the varibale on the left (count). 
  so, if count was 0 befor line 3 is excuted it will be 1 after line 3 is excuted.
   And if it was 1 it will be 2 after line 3 is excuted. 
