dd973e61dcf9cb7a2784c58086de5116e5eb7e9c
boladjebsoft <djebbour1985@gmail.com>
2026-03-08 23:44:36 +0000

    Revert "Fix multiply function to return result and log output"
    This reverts commit 5809aeec54cfeae9d2cd763b7f590d7566076987.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> the function when called will throw an error
+// =============> write your prediction here
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,19 +8,7 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> the function multiply has no return statement
+// =============> write your explanation here
 
 // Finally, correct the code to fix the problem
-//  =============> 
-function multiply(a, b) {
-  return (a * b); // we change the console.log statement with the return statement
-}
-
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// an extra notice: the purpose of creating a function is to reuse it when needed 
-// I've noticed that the parameters are already assigned to values 10 and 32, so I changed the code to the following:
-function multiply(a, b) {
-  return console.log(`The result of multiplying ` + a +` and ` + b +` is ` + (a * b));
-}
-multiply(10, 32);
+//  =============> write your new code here
