ea6c7f20271f01f6dc21fa03334550c7c477863e
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-09 12:48:46 +0000

    minor amendment

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,13 +1,12 @@
 function countChar(stringOfCharacters, findCharacter) {
-  
   let count = 0;
-  
+
   for (let char of stringOfCharacters) {
     if (char === findCharacter) {
       count++;
-      }
     }
-      return count;
+  }
+  return count;
 }
 
 module.exports = countChar;
