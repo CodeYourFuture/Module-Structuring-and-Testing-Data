69ec0e4db3fba3250ff7f096cc0a5b3a580d3d32
Maryanne Mosonik <chmosonik@gmail.com>
2026-03-09 15:45:34 +0300

    Added code to filter pets starting with H

--- Sprint-3/3-dead-code/exercise-2.js
+++ Sprint-3/3-dead-code/exercise-2.js
@@ -2,10 +2,7 @@
 // The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
 
 const pets = ["parrot", "hamster", "horse", "dog", "hamster", "cat", "hamster"];
-
-
-
-
+const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
 
 function countAndCapitalisePets(petsArr) {
   const petCount = {};
