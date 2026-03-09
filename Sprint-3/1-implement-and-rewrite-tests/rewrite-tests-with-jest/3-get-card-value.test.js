e3943d2348f7eeae5dd4446e7f5ab2477ed6da94
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-09 11:40:33 +0000

    tested cards with jest and updated comments

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -1,20 +1,54 @@
 // This statement loads the getCardValue function you wrote in the implement directory.
 // We will use the same function, but write tests for it using Jest in this file.
+const { createTestScheduler } = require("jest");
 const getCardValue = require("../implement/3-get-card-value");
-
-// TODO: Write tests in Jest syntax to cover all possible outcomes.
 
 // Case 1: Ace (A)
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
 
-// Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-//   Face Cards (J, Q, K)
-//   Invalid Cards
+// Case 2: Face Cards (J, Q, K)
+test(`Should return 10 when given a Jack card`, () => {
+  expect(getCardValue("J♥")).toEqual(10);
+});
 
-// To learn how to test whether a function throws an error as expected in Jest,
-// please refer to the Jest documentation:
-// https://jestjs.io/docs/expect#tothrowerror
+test(`Should return 10 when given a Queen card`, () => {
+  expect(getCardValue("Q♦")).toEqual(10);
+});
 
+test(`Should return 10 when given a King card`, () => {
+  expect(getCardValue("K♣")).toEqual(10);
+});
+
+// Case 3: Number Cards (2-10)
+test(`Should return 2 when given a 2 card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+});
+
+test(`Should return 10 when given a 10 card`, () => {
+  expect(getCardValue("10♥")).toEqual(10);
+});
+
+// Case 4: Invalid Cards
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("♠J")).toThrow();
+});
+
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("invalid")).toThrow();
+});
+
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("12♠")).toThrow();
+});
+
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("1")).toThrow();
+});
+
+// when I tested with test(`Should throw an error when given an invalid card`, () => {
+// expect(() => getCardValue("22")).toThrow();  it did not throw an error because 22 passes the
+// test of first number between and 9 and being 2 characters long,
+// but it is not a valid card because the second character is not a valid suite.
+// The second character is not checked.
