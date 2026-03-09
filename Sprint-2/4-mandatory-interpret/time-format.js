a68f54624fded1f70cbc1ba53343fdd26245cf0e
boladjebsoft <djebbour1985@gmail.com>
2026-03-09 00:50:26 +0000

    Revert "Clarify pad function behavior in comments"
    This reverts commit 56b74d5f822563c2bf7600d712a49a7ecc154b99.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -17,27 +17,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> pad will be called 3 times
+// =============> write your answer here
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> the value assigned to num is 0
+// =============> write your answer here
 
 // c) What is the return value of pad is called for the first time?
-// =============> the return value of pad is called for the first time is "00"
+// =============> write your answer here
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> the value assigned to num is 1
-// because the last time pad is called for the remainingSeconds holding the value 1
-// return pad(remainingSeconds)
-// function pad(1){
+// =============> write your answer here
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> the return value of pad is called for the last time is "01"
-// because the argument passed to the pad parameter is the value of the remainingSeconds wich is 1
-// function pad(1){
-// return 1.toString().padStart(2, "0");
-// return "1".pad(2,"0");
-// return "01"
-//}
+// =============> write your answer here
