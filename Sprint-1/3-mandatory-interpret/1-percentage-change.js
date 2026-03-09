9e6f84627da5e30ba1f75409ca4f429fdb616890
Alex-Os-Dev-Lab <Alexokorefe@hotmail.co.uk>
2026-03-09 08:07:29 +0000

    Correct function call count in comments
    Updated the comment to reflect the correct number of function calls and their locations.

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -13,7 +13,7 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
-//There are 4 function calls in this file.
+//There are 5 function/method calls in this file. Lines 4, 5 and 10.
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
 
