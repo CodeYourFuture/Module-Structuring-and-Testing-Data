a0791acff5258b2a8f9b2c256191496d4577b2de
boladjebsoft <djebbour1985@gmail.com>
2026-03-09 00:29:31 +0000

    Reapply "Update 0.js"
    This reverts commit 64c81746f7a34641b72387a5233446cf45cfeb9f.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -13,6 +13,10 @@ function capitalise(str) {
 // but it creates a new one.
 // =============> my new code: 
 function capitalise(str) {
+<<<<<<< HEAD
   result = `${str[0].toUpperCase()}${str.slice(1)}`;
+=======
+  let result = `${str[0].toUpperCase()}${str.slice(1)}`;
+>>>>>>> parent of 3bb9ddf (Update 0.js)
   return resault;
 }
