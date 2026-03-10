3217cc5624c1211ebd565e17906cad30e0921ec6
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-10 11:33:08 +0000

    tests and function to repeat string

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,13 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Count must be positive");
+  }
+
+  if (count === 0) {
+    return " ";
+  }
+
+  return str.repeat(count);
 }
 
 module.exports = repeatStr;
