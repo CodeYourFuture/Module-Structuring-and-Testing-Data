9e13ea1e6ceba17d541e1ee982193ce0a2c487e0
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-03-10 09:57:37 +0000

    fixed a typo in the test description and made all the descriptions consise

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -13,13 +13,13 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Case 1: Numbers ending with 1 (but not 11)
 // When the number ends with 1, except those ending with 11,
 // Then the function should return a string by appending "st" to the number.
-test("should append 'st' for numbers ending with 1, except those ending with 11", () => {
+test("appends 'st' for numbers ending in 1 except 11", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
 
-test("should append 'nd' for numbers ending with 2, except those ending with 12", () => {
+test("appends 'nd' for numbers ending in 2 except 12", () => {
   expect(getOrdinalNumber(2)).toEqual("2nd");
   expect(getOrdinalNumber(22)).toEqual("22nd");
   expect(getOrdinalNumber(92)).toEqual("92nd");
@@ -27,14 +27,14 @@ test("should append 'nd' for numbers ending with 2, except those ending with 12"
   expect(getOrdinalNumber(1142)).toEqual("1142nd");
 })
 
-test("should append 'rd' for numbers ending with 3, except those ending with 13", () => {
+test("appends 'rd' for numbers ending in 3 except 13", () => {
   expect(getOrdinalNumber(3)).toEqual("3rd");
   expect(getOrdinalNumber(23)).toEqual("23rd");
   expect(getOrdinalNumber(63)).toEqual("63rd");
   expect(getOrdinalNumber(533)).toEqual("533rd");
 })
 
-test("should append 'th' for numbers ending to any numbers expect (1, 2, 3)", () => {
+test("appends 'th' when number does not end in 1, 2, or 3", () => {
   expect(getOrdinalNumber(4)).toEqual("4th");
   expect(getOrdinalNumber(10)).toEqual("10th");
   expect(getOrdinalNumber(14)).toEqual("14th");
@@ -44,7 +44,7 @@ test("should append 'th' for numbers ending to any numbers expect (1, 2, 3)", ()
   expect(getOrdinalNumber(670)).toEqual("670th");
 })
 
-test("should append 'th' for numbers ending in (11, 12, 13)", () => {
+test("appends 'th' for numbers ending in 11, 12, or 13", () => {
   expect(getOrdinalNumber(11)).toEqual("11th");
   expect(getOrdinalNumber(12)).toEqual("12th");
   expect(getOrdinalNumber(13)).toEqual("13th");
