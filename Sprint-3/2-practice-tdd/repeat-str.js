205610dc9e4bf1676a40fdbaec8f022756ec098d
miriamjorna <hoimir@gmail.com>
2026-03-10 23:24:11 +0000

    test+code repeat-str.js

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,12 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Negative counts are not valid");
+  }
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatStr;
