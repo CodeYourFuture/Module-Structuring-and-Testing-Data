388b82e4dfaa1680e4d2a390426c3e5a18668425
mervereis <mervereis78@gmail.com>
2026-03-10 12:44:33 +0000

    Add additional test cases for countChar function to cover case sensitivity

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -29,3 +29,38 @@ test("should return 0 when character does not exist in the string", () => {
   const count = countChar(str, char);
   expect(count).toEqual(0);
 });
+
+test("should be case sensitive and only count exact character matches", () => {
+  const str = "AaAaAa";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+test("should be case sensitive - uppercase A should not match lowercase a", () => {
+  const str = "AaAaAa";
+  const char = "A";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+test("should count occurrences of numeric characters", () => {
+  const str = "1a2b3c1d2e1f";
+  const char = "1";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+test("should count occurrences of special characters", () => {
+  const str = "hello!world!test!";
+  const char = "!";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+test("should count occurrences of spaces", () => {
+  const str = "hello world this is a test";
+  const char = " ";
+  const count = countChar(str, char);
+  expect(count).toEqual(5);
+});
