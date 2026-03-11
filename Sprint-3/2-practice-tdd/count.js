c9d6085c3dc91b505bd4603f960a28c75e27fd9d
Jacknguyen4438 <jacknguyen4438@gmail.com>
2026-03-11 02:33:51 +0000

    Complete the first part of the second branch

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,33 @@
-function countChar(stringOfCharacters, findCharacter) {
-  return 5
+function countChar(fullString,findCharacters) {
+  let count = 0;
+  // both of these if statement is for input validation if the input not a string.
+  if( typeof findCharacters !== "string" || typeof fullString !== "string"){
+    throw new Error("Invalid input");
+  };
+  
+  if (findCharacters.length < 1) {
+    throw new Error("findCharacters must be a single character");
+  }
+
+  for (let i = 0; i < fullString.length; i++){
+    if(fullString[i] === findCharacters){
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countChar;
+
+
+function assertTest(testInput,testCheck){
+ console.assert(
+  testInput === testCheck,
+  `Expect ${testInput} equal to ${testCheck}`
+ );
+};
+
+assertTest(countChar("whale fat hat cat","a"),4)
+assertTest(countChar("I need to lean more and know more","e"),5)
+assertTest(countChar("the city centre currently have a carnival","c"),4)
+assertTest(countChar("the cruise ship in in transit to south America","s"),4)
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,6 @@ test("should count multiple occurrences of a character", () => {
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+test( "should return count of zero occurrences of a character", () =>{
+   expect(countChar("go home and study","c")).toEqual(0)
+});
