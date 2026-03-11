6f10d482a4f5baff1ca82f16bcfd23c0d6385e7e
Divine <divinemankrado1@hotmail.com>
2026-03-11 14:38:36 +0000

    Resolved merge conflict in time-format.js

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -11,9 +11,6 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
-// You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
-// to help you answer these questions
-
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
@@ -24,11 +21,11 @@ function formatTimeDisplay(seconds) {
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> 0
 
-// c) What is the return value of pad is called for the first time?
+// c) What is the return value of pad when it is called for the first time?
 // =============> "00"
 
-// d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+// d) What is the value assigned to num when pad is called for the last time in this program? Explain your answer
 // =============> 1. The last pad call is pad(remainingSeconds), and with 61 seconds input, remainingSeconds = 61 % 60 = 1
 
-// e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// e) What is the return value of pad when it is called for the last time in this program? Explain your answer
 // =============> "01". pad(1) calls toString() to get "1", then padStart(2, "0") pads it to "01"
