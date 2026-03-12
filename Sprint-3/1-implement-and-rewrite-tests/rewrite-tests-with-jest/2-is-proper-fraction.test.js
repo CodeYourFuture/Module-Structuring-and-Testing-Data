f1de0d9723d8bb9aea3ddc20657ca4ea687c3234
KK Tech <dyxj6n786p@privaterelay.appleid.com>
2026-03-12 23:33:33 +0000

    Completed rewrite-tests-with-jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,7 +4,28 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
-test(`should return false when denominator is zero`, () => {
+// Case 1: Proper fractions (numerator < denominator)
+test("should return true when numerator is less than denominator", () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+  expect(isProperFraction(3, 4)).toEqual(true);
+  expect(isProperFraction(0, 5)).toEqual(true);
+});
+
+// Case 2: Improper fractions (numerator >= denominator)
+test("should return false when numerator is greater than or equal to denominator", () => {
+  expect(isProperFraction(2, 1)).toEqual(false);
+  expect(isProperFraction(4, 3)).toEqual(false);
+  expect(isProperFraction(5, 5)).toEqual(false);
+});
+
+// Case 3: Negative numbers
+test("should handle negative numbers", () => {
+  expect(isProperFraction(-1, 2)).toEqual(true);
+  expect(isProperFraction(1, -2)).toEqual(true);
+  expect(isProperFraction(-3, -4)).toEqual(true);
+});
+
+// Case 4: Denominator is zero
+test("should return false when denominator is zero", () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -5,10 +5,33 @@ const getCardValue = require("../implement/3-get-card-value");
 // TODO: Write tests in Jest syntax to cover all possible outcomes.
 
 // Case 1: Ace (A)
-test(`Should return 11 when given an ace card`, () => {
+test("Should return 11 when given an ace card", () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
 
+// Case 2: Face cards (J, Q, K)
+test("Should return 10 when given a face card", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♠")).toEqual(10);
+  expect(getCardValue("K♠")).toEqual(10);
+});
+
+// Case 3: Number cards (2-10)
+test("Should return the numeric value when given a number card", () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("5♠")).toEqual(5);
+  expect(getCardValue("10♠")).toEqual(10);
+});
+
+// Case 4: Invalid cards
+test("Should throw an error when given an invalid card", () => {
+  expect(() => getCardValue("1♠")).toThrow("Invalid card");
+  expect(() => getCardValue("11♠")).toThrow("Invalid card");
+  expect(() => getCardValue("B♠")).toThrow("Invalid card");
+  expect(() => getCardValue("A")).toThrow("Invalid card");
+  expect(() => getCardValue("")).toThrow("Invalid card");
+  expect(() => getCardValue("InvalidCard")).toThrow("Invalid card");
+});
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
 //   Face Cards (J, Q, K)
