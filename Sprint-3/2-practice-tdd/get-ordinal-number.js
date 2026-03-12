2256f87c8b60f4da09e2746d17cfef770ae82102
miriamjorna <hoimir@gmail.com>
2026-03-12 13:49:13 +0000

    In get-ordinal-number: if statement replaced by switch statement

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,23 +1,25 @@
 function getOrdinalNumber(num) {
-   if (num === 0) {
+  if (num === 0) {
     return "0";
   }
+
   const lastTwoDigits = num % 100;
   const lastDigit = num % 10;
 
-  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
-    return `${num}th`;
+  switch (true) {
+    case lastTwoDigits === 11:
+    case lastTwoDigits === 12:
+    case lastTwoDigits === 13:
+      return `${num}th`;
+    case lastDigit === 1:
+      return `${num}st`;
+    case lastDigit === 2:
+      return `${num}nd`;
+    case lastDigit === 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
   }
-  if (lastDigit === 1) {
-    return `${num}st`;
-  }
-  if (lastDigit === 2) {
-    return `${num}nd`;
-  }
-  if (lastDigit === 3) {
-    return `${num}rd`;
-  }
-  return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
