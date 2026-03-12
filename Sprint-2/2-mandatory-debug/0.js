0f84719684ad77b2dfbcc1b6bb36dcb72593f80c
boladjebsoft <djebbour1985@gmail.com>
2026-03-12 01:07:25 +0000

    deleted the console.log statement from the function and added a return statement

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -9,7 +9,7 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> the function multiply has no return statement
-
+// when we call the function multiply(10, 32) it will throw this message: The result of multiplying 10 and 32 is undefined
 // Finally, correct the code to fix the problem
 //  =============>
 function multiply(a, b) {
@@ -17,12 +17,3 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// an extra notice: the purpose of creating a function is to reuse it when needed
-// I've noticed that the parameters are already assigned to values 10 and 32, so I changed the code to the following:
-function multiply(a, b) {
-  return console.log(
-    `The result of multiplying ` + a + ` and ` + b + ` is ` + a * b
-  );
-}
-multiply(10, 32);
