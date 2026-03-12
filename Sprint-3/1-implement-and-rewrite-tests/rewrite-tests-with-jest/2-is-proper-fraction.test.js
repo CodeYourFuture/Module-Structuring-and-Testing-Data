ec69eea2f55a37d417d6a24908f68aba0934d20b
abduhasen <abdumussa1363@gmail.com>
2026-03-12 10:24:45 +0000

    adding  `abs(...) ` notation to indicate the comparison is made on the absolute values of the numerator and the denominator.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -15,12 +15,12 @@ test("should return true when numerator is 0", () => {
   expect(isProperFraction(0, 3)).toEqual(true);
   expect(isProperFraction(0, -2)).toEqual(true);
 });
-test("should return true when numerator > denominator", () => {
+test("should return true when abs(numerator) > abs(denominator)", () => {
   expect(isProperFraction(-12, -19)).toEqual(true);
   expect(isProperFraction(0, 6)).toEqual(true);
   expect(isProperFraction(17, -71)).toEqual(true);
 });
-test("should return false when numerator < denominator", () => {
+test("should return false when abs(numerator) < abs(denominator)", () => {
   expect(isProperFraction(-180, -109)).toEqual(false);
   expect(isProperFraction(27, 5)).toEqual(false);
   expect(isProperFraction(-29, 17)).toEqual(false);
