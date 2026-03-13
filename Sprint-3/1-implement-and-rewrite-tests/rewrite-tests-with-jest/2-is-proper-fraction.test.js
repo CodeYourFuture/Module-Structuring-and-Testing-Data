853bea5d8cb0ec0ac080d830e80b8451b4673374
RomanSanaye <sanaye143@gmail.com>
2026-03-13 16:11:22 +0000

    jest test description now matches with the function

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,27 +4,30 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
+test("returns true when |numerator| < |denominator|", () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+});
+
+test("returns true when numerator is zero", () => {
+  expect(isProperFraction(0, 5)).toEqual(true);
+});
+
+test("returns false when |numerator| > |denominator|", () => {
+  expect(isProperFraction(3, 2)).toEqual(false);
+});
+
+test("returns false when |numerator| = |denominator|", () => {
+  expect(isProperFraction(5, 5)).toEqual(false);
+});
+
 test("returns false when denominator is zero", () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
 
-test("returns false when numerator is zero", () => {
-  expect(isProperFraction(0, 1)).toEqual(false);
+test("returns true when denominator is negative but |numerator| < |denominator|", () => {
+  expect(isProperFraction(2, -4)).toEqual(true);
 });
 
-test("returns false when numerator > denominator", () => {
-  expect(isProperFraction(2, 1)).toEqual(false);
-});
-
-test("returns false when numerator is negative", () => {
-  expect(isProperFraction(-2, 2)).toEqual(false);
-});
-
-test("returns false when denominator is negative", () => {
-  expect(isProperFraction(2, -2)).toEqual(false);
-});
-
-test("returns false when both numerator and denominator are negative", () => {
-  expect(isProperFraction(-3, -4)).toEqual(false);
+test("returns true when both numbers are negative but |numerator| < |denominator|", () => {
+  expect(isProperFraction(-3, -4)).toEqual(true);
 });
