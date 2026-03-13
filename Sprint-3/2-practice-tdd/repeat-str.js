f980882cae6fbe1da27f57a4c4d523b3db2b8984
Pretty Taruvinga <pretty548@gmail.com>
2026-03-13 14:02:11 +0200

    fixed the function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,4 +1,7 @@
 function repeatStr(n, str) {
+  if (!Number.isInteger(n) || n < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
   let result = "";
   for (let i = 0; i < n; i++) {
     result += str;
@@ -7,32 +10,3 @@ function repeatStr(n, str) {
 }
 
 module.exports = repeatStr;
-// const repeatStr = require("./repeat-str");
-// Given a target string `str` and a positive integer `count`,
-// When the repeatStr function is called with these inputs,
-// Then it should:
-// test("repeats a string n times", () => {
-//   expect(repeatStr(3, "hello")).toBe("hellohellohello");
-// });
-const repeatStr = require("./repeat-str");
-test("repeats a string n times", () => {
-  expect(repeatStr(3, "hello")).toBe("hellohellohello");
-});
-// test("repeats a different string", () => {
-//   expect(repeatStr(2, "abc")).toBe("abcabc");
-// });
-test("repeats a different string", () => {
-  expect(repeatStr(2, "abc")).toBe("abcabc");
-});
-// test("returns empty string when n is 0", () => {
-//   expect(repeatStr(0, "hi")).toBe("");
-// });
-test("returns empty string when n is 0", () => {
-  expect(repeatStr(0, "hi")).toBe("");
-});
-// test("returns empty string when n is 0", () => {
-//   expect(repeatStr(0, "hi")).toBe("");
-// });
-test("returns empty string when n is 0", () => {
-  expect(repeatStr(0, "hi")).toBe("");
-});
