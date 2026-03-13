6d18f442f7e25c933356b72a7a065396e5e8839f
Fattouma Ouannassi <fatt13967@gmail.com>
2026-03-13 11:08:06 +0000

    Update count.js

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,20 +1,13 @@
-// function countChar(stringOfCharacters, findCharacter) {
-//   let count = 0;
-//   for (let i = 0; i < stringOfCharacters.length; i++) {
-//     if (stringOfCharacters[i] === findCharacter) {
-//       count++;
-//     }
-//   }
+// countChar
+function countChar(stringOfCharacters, findCharacter) {
+  let count = 0;
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+  }
+  return count;
+}
 
-//   return count;
-// }
-// module.exports = countChar;
-let numbers=[1,2,3,4,5,6,1,2,3,4,5,6]
-let idxArray=[];
-numbers.find((el,ix,numbers)=>{
-    if (el===3){
-      
-  idxArray.push(ix);}
-})
-console.log(idxArray);
+module.exports = countChar;
 
