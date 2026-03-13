d961cef23974be91d099027f7f4d300095c71d42
Darkidd77 <m.almahdi77@yahoo.com>
2026-03-13 04:44:45 +0000

    Implement getOrdinalNumber function and add comprehensive tests for ordinal suffixes

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,13 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, num) {
+  if (num < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
+
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatStr;
