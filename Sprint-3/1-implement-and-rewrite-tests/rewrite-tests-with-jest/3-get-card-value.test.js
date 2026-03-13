7eab738c6df788545a63e6609ca05b0d9dfde775
boladjebsoft <djebbour1985@gmail.com>
2026-03-13 03:28:05 +0000

    Write tests in Jest syntax to cover all possible outcomes.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,13 +8,87 @@ const getCardValue = require("../implement/3-get-card-value");
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
+test(`Should return 11 when given an ace card`, () => {
+  expect(getCardValue("A♥")).toEqual(11);
+});
+test(`Should return 11 when given an ace card`, () => {
+  expect(getCardValue("A♦")).toEqual(11);
+});
+test(`Should return 11 when given an ace card`, () => {
+  expect(getCardValue("A♣")).toEqual(11);
+});
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
+test(`Should return 2 when given a 2 card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+});
+test(`Should return 3 when given a 3 card`, () => {
+  expect(getCardValue("3♥")).toEqual(3);
+});
+test(`Should return 4 when given a 4 card`, () => {
+  expect(getCardValue("4♦")).toEqual(4);
+});
+test(`Should return 5 when given a 5 card`, () => {
+  expect(getCardValue("5♣")).toEqual(5);
+});
+test(`Should return 6 when given a 6 card`, () => {
+  expect(getCardValue("6♥")).toEqual(6);
+});
+test(`Should return 7 when given a 7 card`, () => {
+  expect(getCardValue("7♠")).toEqual(7);
+});
+test(`Should return 8 when given a 8 card`, () => {
+  expect(getCardValue("8♦")).toEqual(8);
+});
+test(`Should return 9 when given a 9 card`, () => {
+  expect(getCardValue("9♠")).toEqual(9);
+});
+test(`Should return 10 when given a 10 card`, () => {
+  expect(getCardValue("10♣")).toEqual(10);
+});
+
 //   Face Cards (J, Q, K)
+test(`Should return 10 when given a J card`, () => {
+  expect(getCardValue("J♠")).toEqual(10);
+});
+test(`Should return 10 when given a Q card`, () => {
+  expect(getCardValue("Q♦")).toEqual(10);
+});
+test(`Should return 10 when given a K card`, () => {
+  expect(getCardValue("K♥")).toEqual(10);
+});
 //   Invalid Cards
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("1♠")).toThrow();
+});
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("2❦")).toThrow();
+});
+
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("11♥")).toThrow();
+});
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("B♦")).toThrow();
+});
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("Z♣")).toThrow();
+});
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("♣2")).toThrow();
+});
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("s")).toThrow();
+});
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("♣♥♠♦")).toThrow();
+});
+
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("")).toThrow();
+});
 
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
-
