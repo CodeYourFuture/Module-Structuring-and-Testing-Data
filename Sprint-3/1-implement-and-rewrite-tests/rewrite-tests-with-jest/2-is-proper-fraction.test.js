19b1d130faeceddd7acb08283e52d5c02d6b44dc
abduhasen <abdumussa1363@gmail.com>
2026-03-13 10:06:57 +0000

    adding description

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -9,6 +9,7 @@ test("should Throw new error for abs(denominator) == 0", () => {
   expect(() => isProperFraction(0, 0)).toThrow("Denominator cannot be zero");
   expect(() => isProperFraction(7, 0)).toThrow("Denominator cannot be zero");
 });
+// It should return true  when the numerator is 0
 test("should return true when abs(numerator) == 0", () => {
   expect(isProperFraction(0, -1)).toEqual(true);
   expect(isProperFraction(0, 3)).toEqual(true);
