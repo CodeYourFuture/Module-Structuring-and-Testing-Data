4b7064626f024fab8fb30aadcddd995fe43f4518
boladjebsoft <djebbour1985@gmail.com>
2026-03-14 00:52:57 +0000

    implement the function getOrdinalNumber

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,16 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (num % 10 === 1 && num % 100 !== 11) {
+    // we also can use if (num[-1]===1 && num.slice(-2)!==11)
+    return num + "st";
+  } else if (num % 10 === 2 && num % 100 !== 12) {
+    // we also can use if (num[-1]===2 && num.slice(-2)!==12)
+    return num + "nd";
+  } else if (num % 10 === 3 && num % 100 !== 13) {
+    // we also can use if (num[-1]===3 && num.slice(-2)!==13)
+    return num + "rd";
+  } else {
+    return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
