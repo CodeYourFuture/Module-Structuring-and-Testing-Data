34ae0f23f4ee883b47d5133f30a2a8c4e2a6a673
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-03-14 12:34:45 +0000

    Fixed the logic bug( a misplced parenthesis

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -11,27 +11,27 @@ test(`Should return 11 when given an ace card`, () => {
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
-test(`Should return the correct value for number cards (2-10)`), () => {
+test(`Should return the correct value for number cards (2-10)`, () => {
   expect(getCardValue("2♠")).toEqual(2);
   expect(getCardValue("5♠")).toEqual(5);
   expect(getCardValue("10♠")).toEqual(10);
-}
+})
 //   Face Cards (J, Q, K)
-test(`Should return 10 when given a face card (J, Q, K)`), () => {
+test(`Should return 10 when given a face card (J, Q, K)`, () => {
   expect(getCardValue("J♠")).toEqual(10);
   expect(getCardValue("Q♠")).toEqual(10);
   expect(getCardValue("K♠")).toEqual(10);
-}
+})
 //   Invalid Card suits
-test(`Should throw an error when given an invalid card suit`), () => {
+test(`Should throw an error when given an invalid card suit`, () => {
   expect(() => getCardValue("3-")).toThrow("Invalid card rank");
-  expect(() => getCardValue("5X")).toThrow("Invalid card suit");
+  expect(() => getCardValue("5X")).toThrow("Invalid card suit")});
 
 //  Invalid Card ranks
-test(`Should throw an error when given an invalid card rank`), () => {
+test(`Should throw an error when given an invalid card rank`, () => {
   expect(() => getCardValue("1♠")).toThrow("Invalid card rank");
   expect(() => getCardValue("11♠")).toThrow("Invalid card rank");
-}
+})
 
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
