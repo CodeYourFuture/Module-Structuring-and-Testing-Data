7661229ffa28adb06db5c9e5f225c2b869694938
boladjebsoft <djebbour1985@gmail.com>
2026-03-14 01:18:42 +0000

    implement repeatStr function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,21 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count === 0) {
+    return "";
+  } else if (count === 1) {
+    return str;
+  } else if (count > 1) {
+    return str.repeat(count);
+  } else {
+    throw new Error("Count cannot be a negative number");
+  }
 }
+// I can make the 3 valid cases in 1 case with the repeat method as folows:
+// function repeatStr(str, count) {
+//   if (count >= 0) {
+//     return str.repeat(count);
+//   } else {
+//     throw new Error("Count cannot be a negative number");
+//   }
+// }
 
 module.exports = repeatStr;
