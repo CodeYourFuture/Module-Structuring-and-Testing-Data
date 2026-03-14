e4de3c6d1a5ac465cc36b3ee58969e201a2acf00
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-14 17:50:10 +0000

    added curly bracket

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,11 +1,12 @@
 function countChar(stringOfCharacters, findCharacter) {
-
-let count = 0;
-for (let char of stringOfCharacters) {
-  if (char === findCharacter) {
-    count++;
+  let count = 0;
+  for (let char of stringOfCharacters) {
+    if (char === findCharacter) {
+      count++;
+    }
   }
+
+  return count;
 }
-return count;
 
 module.exports = countChar;
