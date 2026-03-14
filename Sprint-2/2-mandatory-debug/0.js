9b3eb4b687e97668b07b25732f9a67a512c7724f
Seti Mussa <Mussasti@gmail.com>
2026-03-14 13:31:13 +0000

    Applied the feedback

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -4,7 +4,6 @@
 // The program will print 320 first.
 // Then it will " The result of mutiplying 10 and 32 is 320".
 
-
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -14,14 +13,14 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 // =============> write your explanation here
 // The function multiply is used console.log(a * b) to show the result
 // however, it does not resturn the value .
-// when the function is used inside the sentence with ${multiply(10, 32)} 
+// when the function is used inside the sentence with ${multiply(10, 32)}
 // JavaScript expects the function to give back a value.
 //Becuse the function does not return anything, the result becomes undefined.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
-function multiply(a,b){
-  return a*b;
-  }
-  
-  console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)} `);
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)} `);
