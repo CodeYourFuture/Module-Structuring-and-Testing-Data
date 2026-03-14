01e4e00d75d28eeab32567d1397f921ef0d81d96
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-14 16:32:02 +0000

    added tests and renamed variable

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,18 +1,18 @@
 function getOrdinalNumber(num) {
   // Check if input is a number
-  if (typeof num !== "number" || isNaN(num)) {
+  if (typeof num !== "number" || isNaN(num)) || !Number.isInteger(num) || !Number.isFinite(num)) {
     throw new Error("Input must be a valid number");
   }
 
-  const j = num % 10; // checks what last number is
-  const k = num % 100; // checks what last two numbers are, needed to check for 11
+  const lastnum = num % 10; // checks what last number is
+  const last2num = % 100; // checks what last two numbers are, needed to check for 11
 
-  if (k === 11 || k === 12 || k === 13) {
+  if (last2num === 11 || last2num === 12 || last2num === 13) {
     return num + "th";
   }
-  if (j === 1) return num + "st";
-  if (j === 2) return num + "nd";
-  if (j === 3) return num + "rd";
+  if (lastnum === 1) return num + "st";
+  if (lastnum === 2) return num + "nd";
+  if (lastnum === 3) return num + "rd";
 
   return num + "th";
 }
