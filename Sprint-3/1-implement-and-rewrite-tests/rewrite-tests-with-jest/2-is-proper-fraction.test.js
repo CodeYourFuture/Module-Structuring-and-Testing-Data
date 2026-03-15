5f7a512f8f94ecaab55de5d5b265fb80fedbf688
miriamjorna <hoimirextra@gmail.com>
2026-03-15 23:10:31 +0000

    all tasks done, ready for PR

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,4 +7,29 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 // Special case: numerator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
+  expect(isProperFraction(-1, 0)).toEqual(false);
+  expect(isProperFraction(0, 0)).toEqual(false);  
 });
+
+test(`should return true when the absolute value of the numerator is less than the absolute value of the denominator`, () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+  expect(isProperFraction(-1, 2)).toEqual(true);
+  expect(isProperFraction(1, -2)).toEqual(true);
+  expect(isProperFraction(-1, -2)).toEqual(true);
+}); 
+
+test(`should return false when the absolute value of the numerator is greater than or equal to the absolute value of the denominator`, () => {
+  expect(isProperFraction(2, 1)).toEqual(false);
+  expect(isProperFraction(-2, 1)).toEqual(false);
+  expect(isProperFraction(2, -1)).toEqual(false);
+  expect(isProperFraction(-2, -1)).toEqual(false);
+  expect(isProperFraction(5, 0)).toEqual(false);
+  expect(isProperFraction(-5, 0)).toEqual(false);
+});
+
+test(`should return true when the numerator is zero and the denominator is non-zero`, () => {
+  expect(isProperFraction(0, 5)).toEqual(true);
+  expect(isProperFraction(0, -5)).toEqual(true);
+}
+);  
+
--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -11,10 +11,26 @@ test(`Should return 11 when given an ace card`, () => {
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
+test(`Should return the correct value for number cards`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("5♠")).toEqual(5);
+  expect(getCardValue("10♠")).toEqual(10);
+});
+
 //   Face Cards (J, Q, K)
+test(`Should return 10 for face cards`, () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♠")).toEqual(10);
+  expect(getCardValue("K♠")).toEqual(10);
+});
+
 //   Invalid Cards
+test(`Should throw an error for invalid card strings`, () => {
+  expect(() => getCardValue("invalid")).toThrow("Invalid card format");
+  expect(() => getCardValue("11♠")).toThrow("Invalid card format");
+  expect(() => getCardValue("A♤")).toThrow("Invalid card format");
+  expect(() => getCardValue(123)).toThrow("Card must be a string");
+});
 
-// To learn how to test whether a function throws an error as expected in Jest,
-// please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
-
+    
