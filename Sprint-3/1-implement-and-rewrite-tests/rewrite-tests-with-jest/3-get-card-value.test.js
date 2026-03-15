d4660145fc30efd3aa540fb2203d92517bb636a6
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-15 08:48:00 +0000

    I Wrote tests in Jest syntax to cover all possible outcomes for the getCardValue function.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -9,6 +9,32 @@ test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
 
+
+// Case 2: Number Cards (2–10)
+test("Should return the numeric value for number cards", () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("10♣")).toEqual(10);
+});
+
+// Case 3: Face Cards (J, Q, K)
+test("Should return 10 for face cards", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♦")).toEqual(10);
+});
+
+// Case 4: Invalid Cards
+test("Should throw an error for invalid cards", () => {
+  expect(() => getCardValue("1♠")).toThrowError();
+  expect(() => getCardValue("B♣")).toThrowError();
+  expect(() => getCardValue("10?")).toThrowError();
+  expect(() => getCardValue("invalid")).toThrowError();
+});
+
+
+
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
 //   Face Cards (J, Q, K)
