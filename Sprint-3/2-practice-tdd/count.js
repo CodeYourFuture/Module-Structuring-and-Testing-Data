17f285abda3c81a4bf06660aab85e4227c217eed
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-15 09:25:41 +0000

    implemented the function countChar that counts the number of times a character occurs in a string

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,7 @@
-function countChar(stringOfCharacters, findCharacter) {
-  return 5
+function countChar(str, char) {
+  
+  return str.split(char).length -1; 
 }
 
+console.log(countChar('lol', 'a'));
 module.exports = countChar;
