e94715041799fbedba0be857d135a74565ed2ecc
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-15 19:20:21 +0000

    removed redundant checks and added check for negative values

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,11 +1,11 @@
 function getOrdinalNumber(num) {
   // Check if input is a number
-  if (typeof num !== "number" || isNaN(num)) || !Number.isInteger(num) || !Number.isFinite(num)) {
+  if (!Number.isInteger(num) || !Number.isFinite(num) || num <= 0) {
     throw new Error("Input must be a valid number");
   }
 
   const lastnum = num % 10; // checks what last number is
-  const last2num = % 100; // checks what last two numbers are, needed to check for 11
+  const last2num = num % 100; // checks what last two numbers are, needed to check for 11
 
   if (last2num === 11 || last2num === 12 || last2num === 13) {
     return num + "th";
