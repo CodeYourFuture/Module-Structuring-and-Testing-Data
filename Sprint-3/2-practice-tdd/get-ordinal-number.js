f55e9b5fd033d0c1842d2b2631121540c8bb434d
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-15 19:33:02 +0000

    made variables into camelCase and shortened the code for checking 11, 12,13

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -4,15 +4,15 @@ function getOrdinalNumber(num) {
     throw new Error("Input must be a valid number");
   }
 
-  const lastnum = num % 10; // checks what last number is
-  const last2num = num % 100; // checks what last two numbers are, needed to check for 11
+  const lastNum = num % 10; // checks what last number is
+  const last2Num = num % 100; // checks what last two numbers are, needed to check for 11
 
-  if (last2num === 11 || last2num === 12 || last2num === 13) {
+  if (last2Num >= 11 && last2Num <= 13) {
     return num + "th";
   }
-  if (lastnum === 1) return num + "st";
-  if (lastnum === 2) return num + "nd";
-  if (lastnum === 3) return num + "rd";
+  if (lastNum === 1) return num + "st";
+  if (lastNum === 2) return num + "nd";
+  if (lastNum === 3) return num + "rd";
 
   return num + "th";
 }
