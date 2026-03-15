665ebcab02fc3e5cdeded614685177ad37306b4b
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-15 23:32:54 +0000

    I implimented a function to get ordinal number for different ordinal cases nd, th, st, and rd.

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,17 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+
+// Case 2 : Ordinal number ending with 2
+test ("should append 'nd`for numbers ending with 2, except those ending with 12" , ()=> {
+  expect(getOrdinalNumber(2).toEqual("2nd"));
+ expect(getOrdinalNumber(22).toEqual("22nd"));
+ expect(getOrdinalNumber(142).toEqual("142nd"));
+});
+// Case 3: Ordinal numbers ending with 1 (but not 13)
+test("should append 'rd' for numbers ending with 3, except those ending with 13" , ()=> {
+  expect(getOrdinalNumber(3).toEqual("2rd"));
+ expect(getOrdinalNumber(23).toEqual("23rd"));
+ expect(getOrdinalNumber(143).toEqual("143rd"));
+});
+
