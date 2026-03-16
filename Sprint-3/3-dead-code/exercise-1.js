7d24e378de28c85f60bbade491e55f4b2f57b0fe
abduhasen <abdumussa1363@gmail.com>
2026-03-16 13:55:59 +0000

    fixing dead code based on mentor comment

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,10 +1,15 @@
 // Find the instances of unreachable and redundant code - remove them!
 // The sayHello function should continue to work for any reasonable input it's given.
 
-let testName = "Chris";
 const greeting = "hello";
 
 function sayHello(greeting, name) {
+  const greetingStr = greeting + ", " + name + "!";
   return `${greeting}, ${name}!`;
 }
-console.log(sayHello(greeting, testName));
+
+testName = "Aman";
+
+const greetingMessage = sayHello(greeting, testName);
+
+console.log(greetingMessage); // 'hello, Aman!'
