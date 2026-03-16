02245dbfc29bffd4d0d5f798e4a0f1c196d3b2a7
boladjebsoft <djebbour1985@gmail.com>
2026-03-16 17:28:07 +0000

    renamed variables

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,13 +1,13 @@
 function getOrdinalNumber(num) {
-  let remainder1 = num % 10;
-  let remainder2 = num % 100;
-  if (remainder1 === 1 && remainder2 !== 11) {
+  let lastDigit = num % 10;
+  let lastTwoDigits = num % 100;
+  if (lastDigit === 1 && lastTwoDigits !== 11) {
     // we also can use if (num[-1]===1 && num.slice(-2)!==11)
     return num + "st";
-  } else if (remainder1 === 2 && remainder2 !== 12) {
+  } else if (lastDigit === 2 && lastTwoDigits !== 12) {
     // we also can use if (num[-1]===2 && num.slice(-2)!==12)
     return num + "nd";
-  } else if (remainder1 === 3 && remainder2 !== 13) {
+  } else if (lastDigit === 3 && lastTwoDigits !== 13) {
     // we also can use if (num[-1]===3 && num.slice(-2)!==13)
     return num + "rd";
   } else {
