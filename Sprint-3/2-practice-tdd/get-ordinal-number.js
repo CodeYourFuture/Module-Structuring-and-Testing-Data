0e0c2f529b7b5eca20d30987bf3afac8ba79d34d
boladjebsoft <djebbour1985@gmail.com>
2026-03-16 15:55:03 +0000

    created variables to store the values of repeatedly used expressions

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,11 +1,13 @@
 function getOrdinalNumber(num) {
-  if (num % 10 === 1 && num % 100 !== 11) {
+  let remainder1 = num % 10;
+  let remainder2 = num % 100;
+  if (remainder1 === 1 && remainder2 !== 11) {
     // we also can use if (num[-1]===1 && num.slice(-2)!==11)
     return num + "st";
-  } else if (num % 10 === 2 && num % 100 !== 12) {
+  } else if (remainder1 === 2 && remainder2 !== 12) {
     // we also can use if (num[-1]===2 && num.slice(-2)!==12)
     return num + "nd";
-  } else if (num % 10 === 3 && num % 100 !== 13) {
+  } else if (remainder1 === 3 && remainder2 !== 13) {
     // we also can use if (num[-1]===3 && num.slice(-2)!==13)
     return num + "rd";
   } else {
