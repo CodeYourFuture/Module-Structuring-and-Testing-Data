b55668e835d0e45bb6ee0113ada4237067c89f33
Carlos Abreu <carlos.abreu@zoho.com>
2026-03-16 22:57:20 +0000

    Removed the olde code

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,26 +1,3 @@
-/**
- * Original code:
- *
-// Find the instances of unreachable and redundant code - remove them!
-// The sayHello function should continue to work for any reasonable input it's given.
-
-let testName = "Jerry";
-const greeting = "hello";
-
-function sayHello(greeting, name) {
-  const greetingStr = greeting + ", " + name + "!";
-  return `${greeting}, ${name}!`;
-  console.log(greetingStr);
-}
-
-testName = "Aman";
-
-const greetingMessage = sayHello(greeting, testName);
-
-console.log(greetingMessage); // 'hello, Aman!'
-*
-* End of code
-*/
 
 // The sayHello function should continue to work for any reasonable input it's given.
 
