73e0c619a4b5fee029142519fc908db525c50463
boladjebsoft <djebbour1985@gmail.com>
2026-03-16 17:36:21 +0000

    fixed the comparison between a sting and a number to between two strings

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -2,13 +2,13 @@ function getOrdinalNumber(num) {
   let lastDigit = num % 10;
   let lastTwoDigits = num % 100;
   if (lastDigit === 1 && lastTwoDigits !== 11) {
-    // we also can use if (num[-1]===1 && num.slice(-2)!==11)
+    // we also can use if (num[-1]==="1" && num.slice(-2)!=="11")
     return num + "st";
   } else if (lastDigit === 2 && lastTwoDigits !== 12) {
-    // we also can use if (num[-1]===2 && num.slice(-2)!==12)
+    // we also can use if (num[-1]==="2" && num.slice(-2)!=="12")
     return num + "nd";
   } else if (lastDigit === 3 && lastTwoDigits !== 13) {
-    // we also can use if (num[-1]===3 && num.slice(-2)!==13)
+    // we also can use if (num[-1]==="3" && num.slice(-2)!=="13")
     return num + "rd";
   } else {
     return num + "th";
