c8ef645c4e05eafbb48d725e5e05413e8bc834fd
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-17 12:52:46 +0000

    I removed the console.log (greetingStr) code because it is unreachable code. - deleted the const greetingStr = greeting + ", " + name + "!"; because it is a redundant code that repeated already int the return function.

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,3 +1,4 @@
+
 // Find the instances of unreachable and redundant code - remove them!
 // The sayHello function should continue to work for any reasonable input it's given.
 
@@ -5,9 +6,7 @@ let testName = "Jerry";
 const greeting = "hello";
 
 function sayHello(greeting, name) {
-  const greetingStr = greeting + ", " + name + "!";
   return `${greeting}, ${name}!`;
-  console.log(greetingStr);
 }
 
 testName = "Aman";
