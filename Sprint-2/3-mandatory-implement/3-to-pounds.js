dee92a3bc99e7efb89fe508f98f4185f39593de8
Seti Mussa <Mussasti@gmail.com>
2026-03-18 23:26:48 +0000

    Fixed the issues

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -5,29 +5,12 @@
 
 // You should call this function a number of times to check it works for different inputs
 
-const toPounds = (kg) => +(kg * 2.20462).toFixed(2);
-
-const penceToPounds = (p) => `£${(p / 100).toFixed(2)}`;
-const toUpperSnakeCase = (s) => s.toUpperCase().trim().split(" ").join("_");
-const multiply = (a, b) => a * b;
+const penceToPounds= (p) => {
+  const value = parseInt(p.replace("p", ""));
+  return `£${(value / 100).toFixed(2)}`;
+};
 
 // Tests
-console.log(toPounds(10)); // 22.05
+console.log(penceToPounds("399p")); // "£3.99"
 
-console.log(penceToPounds(150)); // "£1.50"
 
-console.log(toUpperSnakeCase("hello there"));
-// "HELLO_THERE"
-console.log(multiply(10, 32)); // 320
-
-const convertToUpperSnakeCase = (str) =>
-  str.toUpperCase().trim().split(" ").join("_");
-
-console.log(convertToUpperSnakeCase("hello there")); // "HELLO_THERE"
-
-// Four functions are implemented because each one performs a specific task.
-// This makes the code easier to read, reuse, and maintain.
-//
-// The penceToPounds function was updated to handle a pence string like "399p".
-// It removes the "p", converts the value to a number, and divides by 100
-// so the function correctly returns "£3.99".s
