32ba32524d395df11270f4613b3a86facb9da4d7
Jacknguyen4438 <jacknguyen4438@gmail.com>
2026-03-23 22:21:16 +0000

    Make change to fit with dir part

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,11 +17,9 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
- 
-let dirDirectory = filePath.slice(filePath.indexOf("/"),filePath.lastIndexOf("."));
+let dirDirectory = filePath.slice(filePath.indexOf("Users"),filePath.lastIndexOf("/"));
 let extDirectory = filePath.slice(filePath.lastIndexOf("."));
-let baseDirectory = filePath.slice(filePath.lastIndexOf("/"));
 console.log(`The dir part of the file is ${dirDirectory}.`);
 console.log(`The ext part of the file is ${extDirectory}.`);
-console.log(`The base part of the file is ${baseDirectory}.`);
+
 // https://www.google.com/search?q=slice+mdn
