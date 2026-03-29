bfe3ee48e496691cf0c535325fe2075048c76186
Zadri <zadriabdule@gmail.com>
2026-03-29 17:05:52 +0100

    Fix syntax error in count.test.js

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,6 +1,6 @@
 // implement a function countChar that counts the number of times a character occurs in a string
-const countChar = require("./count");{
-}
+const countChar = require("./count");
+
 // Given a string `str` and a single character `char` to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
