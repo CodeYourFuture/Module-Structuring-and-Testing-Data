1dbf4e370f5be6407f155c8b4f3e6d088f0ac502
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-29 17:04:39 +0100

    implement a function to get the ordinal number for different ordinal cases.

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,11 @@
-function getOrdinalNumber(num) {
-  return "1st";
+function getOrdinalNumber(n) {
+  if (n % 100 >= 11 && n % 100 <= 13) return n + "th";
+
+  if (n % 10 === 1) return n + "st";
+  if (n % 10 === 2) return n + "nd";
+  if (n % 10 === 3) return n + "rd";
+
+  return n + "th";
 }
 
 module.exports = getOrdinalNumber;
