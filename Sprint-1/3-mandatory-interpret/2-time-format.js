a685e57eb1f9b7d17d8a06c428e3ceea9f23d407
Ayodeji_Ayorinde26 <Godleadco@hotmail.com>
2026-04-05 17:39:30 +0100

    Update 2-time-format.js

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,16 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
-
+//ANSWER: There were six variable declarations
 // b) How many function calls are there?
-
+//ANSWER: There were  no function calls except for the log() function
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
-
+//ANSWER: The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-
+//ANSWER: Line 4 with find the remainder whenmovieLength is divided by 60 and assigns the remainder to remainingSeconds (24)
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-
+//ANSWER: The variable result represents the total lenth of the movie which shows the total hours, minutes and seconds remain in string format.
+// A better name will be movieTotalTime.
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+//ANSWER: The code semed to  work for all values of movieLength.
