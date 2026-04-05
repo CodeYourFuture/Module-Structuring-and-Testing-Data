34492eb16f684e6e9b58b31b4d7eeee11988573f
Ayodeji_Ayorinde26 <Godleadco@hotmail.com>
2026-04-05 16:28:42 +0100

    Update 3-paths.js

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.substring(0, 44);
+console.log(`The dir part of the filePath variable is  ${dir}`);
+const lastDotIndex = filePath.lastIndexOf(".");
+const ext = filePath.slice(lastDotIndex + 1);
+console.log(`The ext of ${filePath} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
