1de07dc1e82e3d78f65eeea3c08039278fcab341
Ayodeji_Ayorinde26 <Godleadco@hotmail.com>
2026-04-05 16:32:17 +0100

    Update 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,10 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+//ANSWER:
+// The 'cityOfBirth' was not assigned before the console.log statement
+// Solution will be to switch the lines as shown below:
+
+
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
