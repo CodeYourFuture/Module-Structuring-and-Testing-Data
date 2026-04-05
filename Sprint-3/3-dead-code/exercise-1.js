47f4b0acd486e7c7e5f9063344c9859e19f896eb
Ayodeji_Ayorinde26 <Godleadco@hotmail.com>
2026-04-05 13:58:26 +0100

    Update exercise-1.js

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -5,7 +5,6 @@ let testName = "Jerry";
 const greeting = "hello";
 
 function sayHello(greeting, name) {
-  const greetingStr = greeting + ", " + name + "!";
   return `${greeting}, ${name}!`;
   console.log(greetingStr);
 }
