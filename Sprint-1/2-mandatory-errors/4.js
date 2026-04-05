d8a4d08a913263ccfced3ac29685f0cc9515898d
Ayodeji_Ayorinde26 <Godleadco@hotmail.com>
2026-04-05 16:58:43 +0100

    Update 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+// SUggested Solutions:
+const ClockTime12Hour = "20:53";
+const ClockTime24hour = "08:53";
