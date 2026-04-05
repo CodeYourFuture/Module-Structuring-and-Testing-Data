ab26b8ebe52a6c77292028e63dd8e7c01941c132
Ayodeji_Ayorinde26 <Godleadco@hotmail.com>
2026-04-05 16:31:28 +0100

    Update 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,9 @@
 
 const age = 33;
 age = age + 1;
+// ANSWER
+// In the solution above, 'age' is a constant variable becuse the keyword 'const' was used with it hence the value cannot change.
+
+// Solution:
+let age = 33;
+let age = age + 1;
