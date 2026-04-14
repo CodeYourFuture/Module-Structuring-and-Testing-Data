e947f0f5d47e093677ee35b5eefb2fa7bbfebea0
Mona-Eltantawy <karamrafat@gmail.com>
2026-04-14 23:09:38 +0100

    implement count function and wrote diffrent test cases.

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,14 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
-}
+  if (typeof stringOfCharacters !== "string") return 0;
 
+  let count = 0;
+
+  for (let char of stringOfCharacters) {
+    if (char === findCharacter) {
+      count++;
+    }
+  }
+
+  return count;
+}
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,39 @@ test("should count multiple occurrences of a character", () => {
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+test("should return 0 when character does not exist in string", () => {
+  const str = "hello";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// Scenario: Case Sensitivity
+test("should be case sensitive", () => {
+  expect(countChar("AaAa", "a")).toBe(2);
+});
+
+// Scenario: Empty String
+test("should return 0 for an empty string", () => {
+  expect(countChar("", "a")).toBe(0);
+});
+
+// Scenario: Special Characters
+test("should count special characters", () => {
+  expect(countChar("!@#$%^&*()!", "!")).toBe(2);
+});
+
+// Scenario: Non-String Inputs
+test("should handle non-string inputs gracefully", () => {
+  expect(countChar(12345, "1")).toBe(0);
+  expect(countChar(null, "a")).toBe(0);
+  expect(countChar(undefined, "a")).toBe(0);
+});
+test ("should count numeric characters in a string", () => {
+
+  expect(countChar("12345", "1")).toBe(1);
+});
+// Scenario: Space Character
+test("should count spaces correctly", () => {
+  expect(countChar("a a a", " ")).toBe(2);
+});
