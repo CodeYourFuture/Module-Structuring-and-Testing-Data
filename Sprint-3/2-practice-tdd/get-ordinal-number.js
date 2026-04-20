08cb4ea54d88b3bf39b8f5cd0756109c9752c16f
Ayodeji Ayorinde <godleadco@hotmail.com>
2026-04-20 12:44:32 +0100

    Unused code removed

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,31 +1,4 @@
 
-/*
-function getOrdinalNumber(num) {
-  const s = num.toString();
-  const lastDigit = num % 10;
-  const lastTwoDigits = num % 100;
-
-  // Rule for 11th, 12th, 13th (The "Teens" exception)
-  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
-    return s + "th";
-    //return "1st";
-
-    // Standard rules based on the last digit
-    switch (lastDigit) {
-      case 1:
-        return s + "st";
-      case 2:
-        return s + "nd";
-      case 3:
-        return s + "rd";
-      default:
-        return s + "th";
-    }
-  }
-}
-*/
-
-
 function getOrdinalNumber(num) {
   const s = num.toString();
   const lastDigit = num % 10;
--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -3,10 +3,9 @@ function repeatStr(str, count) {
     throw new Error("Count must be a non-negative integer");
   }
 
-  // If count is 0, .repeat(0) naturally returns an empty string "".
-  // If count is 1, .repeat(1) naturally returns the original string.
+
   return str.repeat(count);
-  //return "hellohellohello";
+
 }
 
 module.exports = repeatStr;
