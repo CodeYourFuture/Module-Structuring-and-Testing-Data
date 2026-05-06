fc9b43bc46ea5d5b2434215cc2bab8cf8a7699aa
Raihan Sharif <raihansharif92@gmail.com>
2026-05-06 02:23:36 +0100

    mandatory-debug done

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,28 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// logging the result of the function will always be undefined, since it doesn't return anything.
+// if the inputs are two numbers, should console log the right number, e.g. (2, 2) -> 4 (2.5, 2.5) -> 6.25
+// otherwise, it might produce incorrect results
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(2, "asdf")}`);
 
 // =============> write your explanation here
+// Javascript tries to cast non-number values to numbers before multiplying. So, "3" to 3 for example.
+// if the value can't be converted to a number, it might cause unpredictable or incorrect outputs.
+// e.g. Boolean true is converted to numeric 1, so true * 2 becomes 1 * 2 = 2.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  if (Number.isFinite(a) && Number.isFinite(b)) {
+    return a * b;
+  }
+  throw new TypeError("Arguments must be finite numbers");
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,23 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// the output of console log should be 'The sub of 10 and 32 is undefined'
 
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+// since the return statement is the first thing in the function body, and it returns nothing.
+// the return value of the function is undefined.
+// so in the console log, it will coerce undefined to string, and print that.
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
