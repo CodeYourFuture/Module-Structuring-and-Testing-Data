3f831045298cd8832db6e4badc9b1c190aa03504
Raihan Sharif <raihansharif92@gmail.com>
2026-05-10 14:24:54 +0100

    use proper variable naming style

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,4 +1,4 @@
-const TwelveHourClockTime = "20:53";
-const TwentyFourHourClockTime = "08:53";
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
 
 // variable names can't start with a number
