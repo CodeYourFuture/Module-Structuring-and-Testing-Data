365b0564052641bd1dff81112eb2f031c1a47d90
Raihan Sharif <raihansharif92@gmail.com>
2026-05-10 14:24:10 +0100

    include leading slash in path dir

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,8 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(1, lastSlashIndex);
+const dir = filePath.slice(lastSlashIndex);
 const ext = base.split(".")[1];
 
+console.log(dir);
 // https://www.google.com/search?q=slice+mdn
