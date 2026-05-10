8186e0b6b5a1e3ad3b303728973ddf337d5e0d1a
Raihan Sharif <raihansharif92@gmail.com>
2026-05-10 14:23:47 +0100

    explictly explain increment

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -6,4 +6,4 @@ count = count + 1;
 // Describe what line 3 is doing, in particular focus on what = is doing
 
 // It is taking the value of the variable count at line 1, and then adding 1 to it.
-// Then it is assigning that value to the variable count.
+// Then it is assigning that value to the variable count. This is called incrementing.
