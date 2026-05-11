51c7a8886026c196c8dff29cb3077c8923b5c1ec
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-11 15:40:30 +0100

    added solution to 3-to-pounds.js

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,48 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+console.log(penceStringWithoutTrailingP)
+
+// 3. const penceStringWithoutTrailingP = penceString.substring(
+// initialises a string variable with value "399" by using function substring on penceString
+
+// 4.   0,
+// first argument for substring, taking part of the string starting from the first letter
+
+// 5.   penceString.length - 1
+// second argument for substring, taking the string up until the 3rd letter
+// done by taking the whole string length (.length) of 4 and removing 1 (this removes the 'p')
+
+// 6. );
+// closes the arguments for the function
+
+console.log(paddedPenceNumberString)
+
+// 8. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+// Ensures the string is at least 3 in length, and if not adds zeros to the front
+// This is to ensure later calculations don't return negative values
+
+// 9-12. const pounds = paddedPenceNumberString.substring(
+//  0,
+//  paddedPenceNumberString.length - 2
+// );
+// Performing same function as lines 3 to 6, taking the padded string and removing the last two digits
+// these are the digits representing pence, leaving a string that represents pounds and intialising
+// that as a pounds variable of 3
+
+// 14. const pence = paddedPenceNumberString
+// intialising new pence variable
+
+// 15.  .substring(paddedPenceNumberString.length - 2)
+// performing substring function on the padded string this time for pence by starting the substring
+// from the penultimate (-2) character of the padded string length
+
+
+// 16. .padEnd(2, "0");
+// ensures the pence string is at least 2 characters long, adding a zero to the start if not
+// closing this function returns 99 to the variable pence
+
+// 18. console.log(`£${pounds}.${pence}`);
+// takes the pounds and pence variables and prints them to the log as a string with a decimal divider and added pound sign
+// at the start: £3.99
