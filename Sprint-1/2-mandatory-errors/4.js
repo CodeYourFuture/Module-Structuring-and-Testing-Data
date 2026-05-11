3db266c58358ca9cfc9bc23d6b0d5bf5a3670799
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-11 14:28:13 +0100

    added solution to 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+// The error: SyntaxError: Invalid or unexpected token
+// This occurs as a variable cannot start with a number (it can include a number)
+// Renaming to something like ClockTime12hour would solve this
