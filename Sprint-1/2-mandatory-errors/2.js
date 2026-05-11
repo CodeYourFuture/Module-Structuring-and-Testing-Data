5308baa165e89e0048cd0462f7d885db5826789a
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-11 14:13:05 +0100

    solution to 2.js added

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,8 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+// error message: ReferenceError: Cannot access 'cityOfBirth' before initialization
+// this is occurring as the computer runs the code from top to bottom
+// the variable cityOfBirth is assigned in line 5 but is trying to be run in line 4
+// this could be fixed by swapping the lines around
