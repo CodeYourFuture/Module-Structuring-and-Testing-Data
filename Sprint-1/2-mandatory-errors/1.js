adbf4207f17b2ef8122141856d1f67ec79430a61
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-11 14:10:38 +0100

    solution to 1.js added

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,8 @@
 
 const age = 33;
 age = age + 1;
+
+// The error is TypeError: Assignment to constant variable.
+// This is occurring as a constant variable cannot be reassigned
+// line 4 is trying to reassign the constant variable age
+// This could be navigated by using let instead of const, or assigning a new variable name for age + 1
