8006ddfe370ffc332142ca621e0d441e830ac2e2
Raihan Sharif <raihansharif92@gmail.com>
2026-05-14 10:02:05 +0100

    fix isProperFraction test suite

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -27,9 +27,6 @@ function assertEquals(actualOutput, targetOutput) {
   );
 }
 
-// TODO: Write tests to cover all cases.
-// What combinations of numerators and denominators should you test?
-
 // Example: 1/2 is a proper fraction
 assertEquals(isProperFraction(1, 2), true);
 assertEquals(isProperFraction(2, 1), false);
--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -2,48 +2,31 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
-// TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
-
-// Special case: numerator is zero
 test(`should return false when denominator is zero`, () => {
-  expect(isProperFraction(1, 0)).toEqual(false);
-});
-// This statement loads the isProperFraction function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
-const isProperFraction = require("../implement/2-is-proper-fraction");
-
-// TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
-
-// Special case: numerator is zero
-test(`should return false when denominator is zero`, () => {
-  expect(isProperFraction(1, 0)).toEqual(false);
-  expect(isProperFraction(0, 0)).toEqual(false);
   expect(isProperFraction(-1, 0)).toEqual(false);
+  expect(isProperFraction(0, 0)).toEqual(false);
+  expect(isProperFraction(1, 0)).toEqual(false);
 });
 
-test(`should return true when numerator and denominator both positive and numerator < denominator`, () => {
-  expect(isProperFraction(1, 2)).toEqual(true);
-  expect(isProperFraction(4, 5)).toEqual(true);
-  expect(isProperFraction(2, 3)).toEqual(true);
+test(`should return true when numerator is zero`, () => {
+  expect(isProperFraction(0, -1)).toEqual(true);
+  expect(isProperFraction(0, 100)).toEqual(true);
 });
 
-test(`should return true when numerator and denominator both negative and numerator < denominator`, () => {
-  expect(isProperFraction(-3, -2)).toEqual(true);
+test(`should return false when |numerator| > |denominator|`, () => {
+  expect(isProperFraction(-3, -2)).toEqual(false);
+  expect(isProperFraction(3, 2)).toEqual(false);
+  expect(isProperFraction(-3, 2)).toEqual(false);
 });
 
-test(`should return true when numerator is negative and numerator < denominator`, () => {
-  expect(isProperFraction(-3, 2)).toEqual(true);
-});
-
-test(`should return false when numerator > denominator`, () => {
-  expect(isProperFraction(2, 1)).toEqual(false);
-});
-
-test(`should return false when numerator > denominator, negative nums`, () => {
-  expect(isProperFraction(-1, -2)).toEqual(false);
-});
-
-test(`should return false when numerator == denominator`, () => {
+test(`should return false when |numerator| == |denominator|`, () => {
   expect(isProperFraction(-2, -2)).toEqual(false);
   expect(isProperFraction(2, 2)).toEqual(false);
+  expect(isProperFraction(-2, 2)).toEqual(false);
+});
+
+test(`should return true when |numerator| < |denominator|`, () => {
+  expect(isProperFraction(2, 3)).toEqual(true);
+  expect(isProperFraction(-3, -4)).toEqual(true);
+  expect(isProperFraction(-2, 3)).toEqual(true);
 });
