70ac1d042b61154a72f840f3ad6fb22c69036d29
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-15 15:12:44 +0100

    updated == to ===

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -5,16 +5,16 @@ function getOrdinalNumber(num) {
 
   const lastTwoDigits = num.toString().slice(-2);
   const lastDigit = lastTwoDigits.slice(-1);
-  if (lastTwoDigits == 11 || lastTwoDigits == 12 || lastTwoDigits == 13) {
+  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
     return `${num}th`;
   }
-  if (lastDigit == 1) {
+  if (lastDigit === 1) {
     return `${num}st`;
   }
-  if (lastDigit == 2) {
+  if (lastDigit === 2) {
     return `${num}nd`;
   }
-  if (lastDigit == 3) {
+  if (lastDigit === 3) {
     return `${num}rd`;
   } else return `${num}th`;
 }
