33428cf363777852ec7cc6476447cbd5d9e09621
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-15 16:42:22 +0100

    added update to key errors 1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -27,10 +27,7 @@ console.log(decimalNumber);
 // =============> write your new code here
 
 function convertToPercentage(decimalNumber) {
-  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
