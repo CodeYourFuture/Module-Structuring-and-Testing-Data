90055d660d5349810b3c5434675ea341589322a8
Vitalii Kmit <vitalikkmita@gmail.com>
2026-05-18 21:39:32 +0100

    Work on 1-key-exercises

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex + 1);
+const ext = base.split(".")[1];
+
+console.log(`The dir is ${dir}`);
+console.log(`The ext is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
