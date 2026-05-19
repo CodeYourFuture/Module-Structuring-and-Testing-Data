0392c8edd5abd730189e52933c55d37197eff650
Remi Marcelle <remibraithwaite@hotmail.co.uk>
2026-05-19 18:02:25 +0100

    explained what line 3 does, = operator

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,10 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Line 3 is updating the value of the count variable. The = operator is an 
+// assignment operator, which assigns the value on the right side 
+// (count + 1) to the variable on the left side (count). In this case, 
+// it takes the current value of count (which is 0), adds 1 to it, and then 
+// assigns the result (1) back to count. So after line 3 executes, count will 
+// have a new value of 1.
