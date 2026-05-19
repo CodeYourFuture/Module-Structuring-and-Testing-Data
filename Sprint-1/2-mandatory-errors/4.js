031eb398b367bcbd6804057b8883a467356d0108
Remi Marcelle <remibraithwaite@hotmail.co.uk>
2026-05-19 22:27:56 +0100

    fix 4.js -> renamed variables to start with letters

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,7 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const twelveHourClockTime = "8:53pm";
+const twentyFourHourClockTime = "20:53";
+
+// Found out that variable names cannot start with a number in JavaScript. 
+// To fix this, we can rename the variables to start with a letter or an 
+// underscore. 
+// In this instance I spelled out the numbers as words.
