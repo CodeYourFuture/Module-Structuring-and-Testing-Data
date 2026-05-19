62c8f2f7b4084acd538e784a56f4f1a6c2a9d0df
Remi Marcelle <remibraithwaite@hotmail.co.uk>
2026-05-19 22:09:23 +0100

    fix 3.js -> convert cardNumber to string before sliced, prediction and exp. added

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,17 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// Prediction: I think the code won't work because cardNumber is a number and the
+//  slice method is for strings. So, it will give an error that slice is not a function.
+
+// Error: TypeError: cardNumber.slice is not a function
+// It was what I predicted. This is because it occurs because cardNumber is a number,
+// and numbers do not have the slice method. To fix this, we can convert cardNumber 
+// to a string before using the slice method.
+
+// Fix: I converted cardNumber to a string using the toString() method, and then
+// I used the slice method to get the last 4 digits. The updated code is above.
+
+console.log(last4Digits);
+
