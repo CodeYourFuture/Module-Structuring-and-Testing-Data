cce7c2453f2173043b28907ef308cdf53b607e06
Remi Marcelle <remibraithwaite@hotmail.co.uk>
2026-05-21 16:17:17 +0100

    jest and testing

---
+++ prep/example.js
@@ -0,0 +1,8 @@
+const yearOfBirth = 1990; // declaration
+let currentYear = 2023; // declaration
+
+currentYear++; // statement
+`I am ${currentYear - yearOfBirth} years old`; // statement
+
+console.log(`I am ${currentYear - yearOfBirth} years old`); // statement
+console.log("hello there")
