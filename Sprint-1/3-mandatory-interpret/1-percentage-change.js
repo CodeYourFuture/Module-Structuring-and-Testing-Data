91da5ffa22e538c513f4ed82ac1a936fd05f719c
Jorvan White <jorvanwhite@gmail.com>
2026-06-10 16:31:57 +0100

    added answers to the questions in the file 1-percentage-change.js

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,16 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+//There are 5 function calls on lines 4,5,7,8 and 10
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+//Line 5 Column 60 was missing a ',' to separate the fuction.
 
 // c) Identify all the lines that are variable reassignment statements
+//LInes 4 and 5 are variable reassignment statements.
 
 // d) Identify all the lines that are variable declarations
+//Lines 1 and 2 are variable declarations.
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+//This is removing the commas to convert the string into a number for equations
