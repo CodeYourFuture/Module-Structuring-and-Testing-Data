1fd3943907ed2d2b76760e20ba75bebe46412731
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-10 22:59:05 +0100

    Store initials from first characters of names
    Updated the initials variable to concatenate the first characters of first, middle, and last names.

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -1,11 +1,16 @@
+// Declare a variable called initials that stores the first character of each string.
+// This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+
 let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
 
-// Declare a variable called initials that stores the first character of each string.
-// This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+let initials = firstName[0] + middleName[0] + lastName[0];
 
-let initials = ``;
+console.log(initials);
 
-// https://www.google.com/search?q=get+first+character+of+string+mdn
+//I have run this code on chrome console and the answer was CKJ
+
+
+
 
