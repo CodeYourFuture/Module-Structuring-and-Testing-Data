e88e5ebd3740eabe591c3a82d20d61e683c0600d
Jorvan White <jorvanwhite@gmail.com>
2026-06-10 12:47:13 +0100

    Updated 2-initials.js to extract the first character  of each name and concatenate them to form the initials.

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -4,8 +4,13 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+const firstInitial = firstName .charAt(0);
+const middleInitial = middleName .charAt(0);
+const lastInitial = lastName .charAt(0);        
 
-let initials = ``;
+let initials = firstInitial + middleInitial + lastInitial;
+
+console.log(initials);
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
