81d701ec28ebc16ec4506aef0a5928072cec7860
Jorvan White <jorvanwhite@gmail.com>
2026-06-10 16:12:29 +0100

    Made changes to 3.js to fix the error. The card number should be a string,  not a number, to use the slice method correctly.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,4 +1,4 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
@@ -10,3 +10,5 @@ const last4Digits = cardNumber.slice(-4);
 
 //Prediction: I believe the code won't work because the slice method is incorrect. The slice method is used for strings.
 //Error: TypeError: cardNumber.slice is not a function
+//Needed to add "" to make it a string.
+
