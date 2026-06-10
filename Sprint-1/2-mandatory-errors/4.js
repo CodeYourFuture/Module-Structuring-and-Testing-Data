8fa926fba576e0f870e9387e90e68e01396487c2
Jorvan White <jorvanwhite@gmail.com>
2026-06-10 16:22:51 +0100

    changed numbers to words in 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const twelveHourClockTime = "8:53pm";
+const twentyfourHourClockTime = "20:53";
+
+//changed '12' to 'twelve' and '24' to 'twentyfour' in order to fix the error.
