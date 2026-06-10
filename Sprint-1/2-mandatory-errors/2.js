67a252f1d8f59b96532536a2606a2383d5d26297
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-11 00:33:02 +0100

    Fix variable declaration order in 2.js
    Declare the variable 'cityOfBirth' before using it in console.log.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,8 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+//We must declare the variable first then use it, because JS reads from top to bottom.
+
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
