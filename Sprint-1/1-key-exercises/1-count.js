398d3ff1a14d336402451c805ddf21975080101c
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-10 22:28:53 +0100

    Add comments to clarify count variable operations
    Added comments to explain variable declaration and increment operation.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+// The assignment operator (=) stores the result of count + 1 in the variable count. i.e. incrementing the count by 1. 
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+
