f9c599e9eccc01a48c48051f82a809a21e956d29
dona-shehu <shehu.donna@gmail.com>
2026-06-11 20:29:06 +0100

    Explained how to write variable names

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const HourClockTime = "8:53pm";
+const hourClockTime = "20:53";
+
+// A variable name can not start with a number.
+
+// A variable name can :
+// Stat with a letter (a-z, A-Z)
+// Start with _ or $
+// Contain numbers after the first character.
