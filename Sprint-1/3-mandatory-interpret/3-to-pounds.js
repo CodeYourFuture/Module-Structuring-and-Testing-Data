c5117e1556afb942d040608c301e3d16fb870612
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-11 11:56:36 +0100

    Enhance 3-to-pounds.js with detailed comments
    Added comments to explain each step of the conversion process from pence to pounds.

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,21 +1,34 @@
-const penceString = "399p";
+const penceString = "399p"; // This line shows price in pence with a letter "p" attached at the end.
 
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+
+const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+
+// Above line of code removes the "p", using "penceString.substring" function by removing the last character with "-1",
+//out of a four digit string "399p".
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
+// We are using "padstart" method to ensure the string contains three characters at least, 
+//and if necessary to add "0" to the front.
+
+
+const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
+
+// We are calculating the pounds const variable by taking away the last two digits from "paddedPenceNumberString" const.
+
+const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
 
+//First we extracting the last two digits with "(paddedPenceNumberString.length - 2)", then making ensure the pence part
+//has two digits always with "padEnd(2, "0")". Basically calculating the const pence variable. 
+
 console.log(`£${pounds}.${pence}`);
+
+//Uses a template literal to combine the pounds and pence values into a currency format and prints the result to the console.
+
+
+
+
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
