3701ef46ced340002e3d090f929e52b42ef82503
dona-shehu <shehu.donna@gmail.com>
2026-06-11 20:27:14 +0100

    Explained the code runs to to bottom.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// The program get executed from top to bottom.
+// cityOfBirth was declared after console.log , so the string prints out without the cityOfBirth value.
