1f56171f3fffe38cfd1e2ad01bc89918999139ba
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-11 01:16:04 +0100

    Document syntax error in variable naming
    Added comments explaining the syntax error related to variable naming.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
 const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53";
+
+/* 
+I have predicted the code won't run due to the fact that "8:53pm" should be "08:53"
+Got this error "Uncaught SyntaxError: Invalid or unexpected token", meaning it is a syntax error. 
+I have searched it up to find that variables can't start with numbers because it makes it easier 
+for JS to read code by removing a mathematical ambiguity.
+*/
