fe54c7f01ca4666b44126847e877b2b66af416c6
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-11 11:06:31 +0100

    Enhance comments for clarity on function calls and errors
    Added comments to clarify function calls, errors, variable reassignment, and declarations.

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -13,10 +13,31 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+/*
+There are three function call names, but two functions have been made twice inside another, adding up to five function calls being made overall. 
+1. carPrice.replaceAll(",", "")
+2. Number(carPrice.replaceAll(",", ""))  this expression contain two, one inside the other
+3. priceAfterOneYear.replaceAll(",", "")
+4. Number(priceAfterOneYear.replaceAll(",", "")) this expression contains two as well 
+5. console.log(`The percentage change is ${percentageChange}`)
+*/
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+//I have run the code and got a syntax error on line 5, due to two arguments not being seperated by a comma.
 
 // c) Identify all the lines that are variable reassignment statements
 
+//carPrice = Number(carPrice.replaceAll(",", ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+
 // d) Identify all the lines that are variable declarations
 
+//let carPrice = "10,000";
+//let priceAfterOneYear = "8,543";
+//const priceDifference = carPrice - priceAfterOneYear;
+//const percentageChange = (priceDifference / carPrice) * 100;
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+//Removing all the comma for calculation then convert the string into a number.
