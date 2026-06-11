0ca8dade657a35497ef7a65ec62b1c0268a5cfe1
dona-shehu <shehu.donna@gmail.com>
2026-06-11 20:29:40 +0100

    Explained how to use comments.

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+/* This is just an instruction for the first activity - but it is just for human consumption
+We don't want the computer to run these 2 lines - how can we solve this problem? */
+
+// If  we do not want the computer to run these lines  witch are just for human consumption we 
+// make them comments with  "//"
+/* Or for multiline comments we use  */
