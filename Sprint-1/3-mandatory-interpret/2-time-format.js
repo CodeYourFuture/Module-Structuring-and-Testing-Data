4c935f3b70f65c8fc30dd84d7fa33025090359d0
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-11 11:31:23 +0100

    Enhance comments for clarity on time format logic
    Added comments to explain variable declarations, function calls, and expressions related to movie length formatting.

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -13,13 +13,26 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
+// Six, every line that starts with "const" keyword.
+
 // b) How many function calls are there?
+
+// One, line 10.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+// It calculates how many seconds are left over.
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+
+// It first removes the leftover seconds from the total movie length, then converts the remaining seconds into minutes.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
+// It represents a formatted time string and since it's a movie, i would change "result" variable to "movieDuration".
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+// The code wouldn't work with decimal numbers "because movieLength" is a whole number, also negative numbers won't work
+//because movies can't be less than zero.
