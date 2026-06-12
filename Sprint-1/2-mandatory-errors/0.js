13e1b500b874d5c0a61a30511c6c83c541d87b5b
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-06-12 17:28:40 +0100

    Convert instructions to comments in 0.js

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,7 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+
+/*
+This caused a SyntaxError because plain English text is not valid JavaScript.
+Converting the lines to comments prevents Node from trying to execute them.
+*/
