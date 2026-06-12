2c05e7e2ace8b63058b1a0a1dd6585775f1b53be
vmoratti <vmoratti@gmail.com>
2026-06-12 13:22:16 +0100

    solve the slicing proble,create dir variabe and ext variable

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -16,8 +16,12 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
-
-const dir = ;
-const ext = ;
+const dir = filePath.slice(lastSlashIndex - 9, lastSlashIndex);//dir variable gets to "dir part of
+//  the path by using last instance of "/" as a reference and counting backwards 9 digits to get the beginning of the path
+// and uses last instance of "/" as the end of the path, which is "interpret"
+const ext = filePath.slice(lastSlashIndex + 5);// ext variable gets the last instance of "/" as a reference and
+//  counts 5 indexes forward to get the "ext" part of the path which is ".txt"
+console.log(`The dir part of the filePath ${filePath} variable is "${dir}".`);
+console.log(`The ext part of the filePath ${filePath} variable is "${ext}".`);
 
 // https://www.google.com/search?q=slice+mdn
