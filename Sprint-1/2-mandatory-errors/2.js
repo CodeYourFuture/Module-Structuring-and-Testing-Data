7efd29443e3df2c13b29b9f36206e13e83b8e7e3
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-06-12 19:02:09 +0100

    Fix variable initialization order in 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,15 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+
+
+/*
+The error occurs because cityOfBirth is accessed before it is initialized.
+
+Although the variable is registered during hoisting, it is placed in the Temporal Dead Zone,
+which prevents it from being used until the declaration line is executed.
+
+This results in a ReferenceError: Cannot access 'cityOfBirth' before initialization.
+*/
