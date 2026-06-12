805a500605c1b44de384fc7e1d8a8dc986e3b7cb
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-12 17:09:28 +0100

    fix error and add comment in 2-mandatory-errors/4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const _12HourClockTime = "8:53pm";
+console.log(_12HourClockTime);
+const _24hourClockTime = "20:53";
+console.log(_24hourClockTime);
+
+// Martin comment - a variable cannot begin with a number.  I have converted both to start with an underscore
