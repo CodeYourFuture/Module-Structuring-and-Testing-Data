72167f91b610da72a7b6a7de55fa4605f97b4eba
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-12 16:40:36 +0100

    update 2-mandatory-errors/1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+
+// Martin comment - const cannot be reassigned.  I have changed the declaration to let which allows the value of age to be reassigned
