477607c9d1a894e876e98ae060d0d30e13dec325
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-12 16:44:38 +0100

    fix error in 2-mandatory-errors/2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// Martin response - cityOfBirth was being logged before it was declared.  I have moved the variable initialisation above the console.log line and this now works as expected.
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
