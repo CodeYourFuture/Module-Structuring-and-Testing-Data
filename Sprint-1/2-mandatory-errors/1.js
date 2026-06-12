b1e65d555ac716a04e393b297246cd32e710f1d7
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-06-12 17:42:44 +0100

    Fix reassignment error in 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,16 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+
+let age = 33;
 age = age + 1;
+
+/*
+The original error occurred because age was declared using const.
+
+Variables declared with const cannot be reassigned.
+
+The statement age = age + 1 tried to assign a new value to age,
+which caused a TypeError: Assignment to constant variable.
+
+Changing const to let allows the value to be updated.
+*/
