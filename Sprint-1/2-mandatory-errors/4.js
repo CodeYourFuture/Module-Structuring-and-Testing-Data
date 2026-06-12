cd191706a2cd75b15f513c0f2a7efee6d1b54a72
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-06-12 20:22:19 +0100

    Fix invalid variable names in 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const twelveHourClockTime = "8:53pm";
+const twentyFourHourClockTime = "20:53";
+
+
+/*
+The error occurs because variable names cannot start with a number.
+
+The variable name 12HourClockTime begins with 12, which is not valid JavaScript syntax,
+causing a SyntaxError.
+
+Renaming the variables so they start with letters fixes the issue.
+*/
