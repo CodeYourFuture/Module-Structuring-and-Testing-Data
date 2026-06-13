dc1750192176da98fcff4a1d942b4034dbe3ed0e
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-13 03:30:24 +0100

    Implemented the function to get the initials as output without writing out the initials

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -1,11 +1,19 @@
+// Declare a variable called initials that stores the first character of each string.
+// This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+
 let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
 
-// Declare a variable called initials that stores the first character of each string.
-// This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+function getInitials() {
+  return `${firstName[0]}${middleName[0]}${lastName[0]}`;
+}
 
-// https://www.google.com/search?q=get+first+character+of+string+mdn
+console.log(getInitials()); 
+
+// Export the initials for testing instead of returning at top-level
+module.exports = getInitials;
+
+
 
