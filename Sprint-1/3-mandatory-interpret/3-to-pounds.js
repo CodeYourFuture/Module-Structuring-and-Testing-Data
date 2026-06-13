9d06c8b5106c8314c7ad16a8a079a3908d35259b
Monsur Abdulrahman <monsuromotunde.abdulrahman@gmail.com>
2026-06-13 20:44:13 +0100

    Refactor comments for clarity and consistency in 3-to-pounds.js

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,31 +25,22 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+
 // 2. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1): 
 // creates a new string variable that removes the last character "p" from the original string
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 // 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"): 
 // pads the string with leading zeros to ensure it has at least 3 characters
 
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+
 // 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2): 
 // extracts the substring representing the pounds by taking all characters except the last two
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+
 // 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0"): 
 // extracts the last two characters representing the pence and pads it with trailing zeros if necessary
 
-console.log(`£${pounds}.${pence}`);
+
 // 6. console.log(`£${pounds}.${pence}`): outputs the final formatted price in pounds and pence to the console  
 
 
