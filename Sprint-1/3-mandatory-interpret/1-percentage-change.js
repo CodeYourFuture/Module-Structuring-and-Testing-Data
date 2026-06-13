dad5f5b11d96df94cfdb4fbb6fbf39102fe2707e
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-13 07:58:37 +0100

    fix error and add comments to 3-mandatory-interpret/1-percentage-change.js

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -13,10 +13,19 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+// Martin answer - functions calls are on lines - 4, 5 with the use of the Number function
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+// Martin answer - There is a syntax error on line 5, comma missing between current item and replacement item in replaceAll method
 
 // c) Identify all the lines that are variable reassignment statements
 
+// Martin answer - lines 4 and 5
+
 // d) Identify all the lines that are variable declarations
+// Martin answer - lines 1, 2, 7 and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+// Martin answer - the purpose of this expression is to use the replace all method to replace the comma in the carPrice price expression with a blank so that the expression reads "10000".  This is converted to a number using the number function converting the string "10000" to a number 10000
