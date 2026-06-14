be8683372da30d747ec91cf04dae1ccafa1f2541
Niangh Ciang <cciang462@gmail.com>
2026-06-14 10:38:06 +0100

    Define variable before using it to fix ReferenceError

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+//The error is that cityOfBirth is used before it is declared. The console.log runs before the variable exists.
