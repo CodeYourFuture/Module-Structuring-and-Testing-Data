1a0e054ede9e0789a9851869dc10aa7755ae522e
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-14 19:44:07 +0100

    add explanation for count implement in Sprint 1 > 1-key-exercises > 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// the value stored in count is 0 to start with
+// With the code in line 3 we add one to the value that is stored in count (RHS) to make it 1.
+// this is then reassign to count, so the updated  value that is stored in count is 1 now.
