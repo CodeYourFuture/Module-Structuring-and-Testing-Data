e767704414e038cdb2d63f54c3d62a6e8aadeaa4
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-14 19:59:03 +0100

    Fix variable clash and update function usage
    Removed clashing variable and corrected function call.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,12 +1,17 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> just like in the previous exercise, you can't have two variables 
+//with the same name in one scope. Second error is you can't call on "decimalNumber"
+//from outside the function. Since that variable only lives inside the function.
 
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
+//a parameter (decimalNumber) is created
   const decimalNumber = 0.5;
+//this is where the first error appears, as now there are two clashing variables 
+//under the same name. Code stops working.  
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
@@ -14,7 +19,25 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
+// =============> my prediction was correct. 
 
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============> 
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
+
+//removed the clashing variable and placed the function call inside the 
+//console log.
+
+
+
+
+
+
+
+
