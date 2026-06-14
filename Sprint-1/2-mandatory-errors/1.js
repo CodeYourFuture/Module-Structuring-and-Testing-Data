81bacc5d6fa9e1141ae50fedf61553f1569de542
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-14 21:25:26 +0100

    Explain assignment to constant variable error in Sprint-1 > 2-mandatory-errors > 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,10 @@
 
 const age = 33;
 age = age + 1;
+
+//js:4
+// age = age + 1;
+//     ^
+
+// TypeError: Assignment to constant variable.
+// a number was declared using const (which cannot change) but here on line 4 (js:4) we tried reassigning it.
