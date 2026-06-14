f041f22ebb326cd3fc19bd5f97675ac5fd24c3c8
Niangh Ciang <cciang462@gmail.com>
2026-06-14 16:49:47 +0100

    Explain purpose of each step in pence price formatting code

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,20 +1,26 @@
+//initialises a string variable with the value "399p"
 const penceString = "399p";
 
+//Removes the last character "p" from "399p", leaving just "399".
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 );
 
+//Ensures the pence value has at least 3 digits by adding leading zeros if needed.
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+//Takes all but the last two digits as the pounds part
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
 
+//Takes the last two digits as the pence part and keeps it at 2 digits
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
 
+//Prints the final price in pounds format
 console.log(`£${pounds}.${pence}`);
 
 // This program takes a string representing a price in pence
