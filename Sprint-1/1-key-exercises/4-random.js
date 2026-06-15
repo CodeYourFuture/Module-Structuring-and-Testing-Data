cb97a289773d80b48a9f7bbd96a5075b83ee5f11
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-15 18:56:36 +0200

    add explanation and breaking down random.js

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,13 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Print the random number to the console
+console.log(num);
+
+//num is a random whole number between 1 and 100.
+
+//Math.random() creates a random decimal.
+//Multiply by the range size.
+// Math.floor() rounds down.
+// Add minimum so it starts at 1.
