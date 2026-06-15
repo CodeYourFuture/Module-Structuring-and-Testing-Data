cecb6bab100371173948724d411c55df155b8671
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-15 23:49:46 +0100

    added a "for" statement, for random number generation to log multiple values in a loop

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -1,7 +1,10 @@
 const minimum = 1;
 const maximum = 100;
 
-const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+for (let i = 0; i < 5; i++) {
+  const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  console.log(num);
+}
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
