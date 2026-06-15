8b6c551f9ff268a6c569996c949115c6afb4eaec
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-15 20:14:20 +0200

    debugging errors

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -20,3 +20,30 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+//answers
+
+/* a) there are there three
+     carPrice.replaceAll(",", "")
+     Number(carPrice.replaceAll(",", ""))
+     Number(priceAfterOneYear.replaceAll(",", ""))*/
+
+/* b) priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", "")); 
+The issue is  a syntax mistake in the brackets/quotes, and JavaScript throws a SyntaxError before running.
+
+the fix would be priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+
+*/
+
+/* c)carPrice = Number(carPrice.replaceAll(",", ""));
+     priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));*/
+
+/* d) let carPrice = "10,000";
+let priceAfterOneYear = "8,543";
+const priceDifference = carPrice - priceAfterOneYear;
+const percentageChange = (priceDifference / carPrice) * 100;*/
+
+/* e)carPrice is a string:
+    replaceAll(",", "") removes commas:
+    Number(...) converts string to a number:
+    Convert a formatted string number into a real numeric value so calculations can work.*/
