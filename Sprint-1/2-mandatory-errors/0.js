bbfef51320010ca4d9ce915b0f0bb0f3dec62019
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-15 23:54:58 +0100

    Added comments to explain code execution prevention and commenting methods

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+
+To prevent the computer from executing these lines of code, you can comment them out, 
+you can use `//` for single-line comments or `/* */` for multi-line comments, like i demonstated above.
