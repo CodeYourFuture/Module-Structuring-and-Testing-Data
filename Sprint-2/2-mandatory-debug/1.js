a6357b6e163d9b42ecc84baf408449a77a607496
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-15 20:33:46 +0100

    Fix sum function to return the correct sum
    Removed semicolon to ensure the sum function returns the correct value.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
-//  =============> write your prediction here
-
+//  =============> due to the semicolon on line 5 , the function will not progress further. In the console line template literal becomes 
+//`The sum of 10 and 32 is ${undefined}`
 function sum(a, b) {
   return;
   a + b;
@@ -8,6 +8,13 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> my prediction was correct, i have removed the semicolon and put the expression on line 6 with the return function on line 5.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
