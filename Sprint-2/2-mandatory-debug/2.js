96ce70ffa786afa24b6e976b2b65b7ad84b2958e
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-15 20:57:01 +0100

    Fix getLastDigit function to accept input parameter
    Updated getLastDigit function to accept a parameter and return the last digit of the input number. Adjusted console.log calls to use the function correctly.

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// =============> there is no paramater for getLastDigit to work with but a fixed value of 103 outside the function, so the console will show 3 everytime. 
 
 const num = 103;
 
@@ -14,11 +14,31 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> 
+VM447:7 The last digit of 42 is 3
+VM447:8 The last digit of 105 is 3
+VM447:9 The last digit of 806 is 3
+
+undefined
+
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> because when the console.log calls for the function there are no parameters in it, so 42 gets ignored. 
+//However 103 is used because it is part of the return function under num keyword. Therefore causing the error for all the console lines.
+
+
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============> 
+const num = 103;
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now, with getLastDigit having a num variable, function will be able to assign different digits when console calls on it. 
+
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
