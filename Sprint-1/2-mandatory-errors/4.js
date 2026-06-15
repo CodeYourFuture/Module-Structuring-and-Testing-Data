e5269604c0d97bc29f7f22541d0ab2f1970ea513
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-15 19:49:29 +0200

    fix invalid variable name

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const twelveHourClockTime = "8:53pm";
+const twentyFourHourClockTime = "20:53";
+
+//the code will fail because variable names cannot start with a number
