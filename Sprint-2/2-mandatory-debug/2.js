93486c9bf2e00b1e9d69b1478a1613531f4593f7
Arthur <>
2026-06-15 22:23:39 +0100

    global frame and variable

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,12 @@
-// Predict and explain first...
 
-// Predict the output of the following code:
-// =============> Write your prediction here
+The const should not be put into the global frame. 
+if it is set in the global frame,  this will result in a single result:3, 
+no matter which variables you put in later. 
+
+The variable insides should have num to match the function expression. 
+
+
+My prediction of the result will be all in 3.
 
 const num = 103;
 
@@ -13,12 +18,22 @@ console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
+
+yes the output is all in 3, because the reason are above .
+
+
+
+... ... ... ... ... ... ... ... ... ... ... ... ... ... ...
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+
+After we delete the global frame and put in the variable, we see
+the result now shows three different digit number.
