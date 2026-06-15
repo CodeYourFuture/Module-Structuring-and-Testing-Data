1df05b543747ff100a313d6dc86a0f0e5a9d9f93
Arthur <>
2026-06-15 13:30:32 +0100

    Key errors sprint 2  0.js 1-key-errors 0.js have spotted

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,25 @@
-// Predict and explain first...
-//  =============> write your prediction here
 
-// call the function capitalise with a string input
-// interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  let str = '${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+It said syntaxError because identifier 'str' has already been declared 
+because we should use backtick`` to wrap the template literal.The first one of the bracket is a
+single quote, not a backtick.
+
+  Also, since str has already been declared as a parameter, we should not use it to define as variable.
+
+
+
+
+
+function capitalise(str) {
+
+  let result = `${str[0].toUpperCase() + str.slice(1)}`;
+  return result;
+}
+
+console.log(capitalise("str"));
+
