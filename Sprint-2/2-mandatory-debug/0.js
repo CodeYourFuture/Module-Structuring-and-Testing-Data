d8cf3adb458da35b844c9730761ebf93e02e77da
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-06-15 20:24:22 +0100

    Fix multiply function to return the result
    Updated the multiply function to return the product instead of just logging it. This allows the result to be used in the template literal.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> this code needs a return function to insert 320 into the string  
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,24 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> this code creates a function called multiply on  line 5, line 6 is ignored for now because it's part of the function definition.
+//Then JS read the code further from top to bottom until line 9, where it sees the values attached to the parameters and runs the function on 5 now, with new values. 
+//lines 6 then becomes console.log(320). Function ends here. 
+
+//
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> 
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+/* 
+
+this exercise is about understanding the differemce between console and return , in essense. 
+With return, the value can be inserted into the template literal.
+With console.log, the value is only displayed and then discarded.
+
+*/
