7fd77103ff83cb8e7f8840bdbe848df8b6e0c7fd
dona-shehu <shehu.donna@gmail.com>
2026-06-16 23:35:59 +0100

    capitalize function, str declaration

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction ==> This function it is going to capitalize the first character in a string.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+const helloString = "hello"
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let newStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return newStr;
 }
 
+
 // =============> write your explanation here
+//We receive an error message because the str was already been declared as parameter in the function.
+
 // =============> write your new code here
+console.log(capitalise(helloString));
