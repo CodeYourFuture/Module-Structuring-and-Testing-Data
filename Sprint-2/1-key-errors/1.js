88d76b490fba92b8e514e1d718092238624bbc1a
dona-shehu <shehu.donna@gmail.com>
2026-06-16 23:53:25 +0100

    Declaration and scope

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,19 +2,23 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// When trying to console.log(decimalNumber) we will occur an error  bcs the decimalNumber 
+// was declared inside the function scop but console.log() is outside the function.
+
 
 // Try playing computer with the example to work out what is going on
-
+ 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));
 
 // =============> write your explanation here
+// Also decimalNumber was declared as parameter in function.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
