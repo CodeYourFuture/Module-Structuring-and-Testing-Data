987c2ce8ad752c874a124886309dca743553b882
B3-Bisrat <bisratttt1919@gmail.com>
2026-06-17 07:16:57 +0100

    1-key-exercises are done.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,8 @@ let count = 0;
 
 count = count + 1;
 
+console.log(count);
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 adds 1 to the current value of count. The = sign means we save the new value back into count. Since count started at 0, count + 1 makes it 1
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -4,6 +4,13 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
+// num creates a random whole number between 1-100 (inclusive).
 // Try breaking down the expression and using documentation to explain what it means
+//maximum-minimum+1 gives us the number of possible values (100-1+1 = 100)
+//Math.random() gives us a random decimal number between 0 (inclusive) and 1 (exclusive)
+//Multiplying Math.random() by 100 gives us a random decimal number between 0 and 100 (exclusive)
+//Math.floor() rounds down the random decimal number to the nearest whole number.
+//Adding minimum to the result gives us a random whole number between minimum and maximum (inclusive)
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(num);
