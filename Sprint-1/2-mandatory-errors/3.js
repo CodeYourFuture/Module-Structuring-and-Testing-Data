78253f11328fe07b0b946d7cdf86099973ed5bec
d-odumosu <dt.odumosu@gmail.com>
2026-06-17 23:29:53 +0100

    fixed the error in 3.js and the code now runs

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,11 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.slice(-4);
+
+
+// to fix the code, we'll convert the number to a string because .slice does not work on number data type
+let cardNumber = 4533787178994213;
+cardNumber = cardNumber.toString()
+let last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +13,10 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//Answer
+// We are not logging to the console
+// The slice method should be a positive number to represent the index position.
+
+console.log(last4Digits)
+//TypeError: cardNumber.slice is not a function
