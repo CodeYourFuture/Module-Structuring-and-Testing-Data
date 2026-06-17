e6d78a0cc4595c2aa18de9b5e37b3298ea95a327
d-odumosu <dt.odumosu@gmail.com>
2026-06-17 22:23:44 +0100

    completed task in 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+/* we cannot reassign a variable that was declared using the const keyword so we will change it to the let keyword
+ which allows us to reassign variables*/
+
+let age = 33;
 age = age + 1;
+console.log(age)
