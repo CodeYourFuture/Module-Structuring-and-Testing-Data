6f1ae810af957b18211238de6f64290e11d3a346
russom <russom.g.meskel@gmail.cpm>
2026-06-17 21:52:57 +0100

    commenting out none code texts

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
+/*This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+*/
+
+// We can use forward slash and asterisk for multi-line or block comments as used above line 1 to 3. Or we can use double forward slashes for single line comments as used on this 5.
