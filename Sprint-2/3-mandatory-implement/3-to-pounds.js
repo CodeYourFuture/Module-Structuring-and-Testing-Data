095f72d2e3d91de1dc086aed5e489c1c31c5e0d2
dona-shehu <shehu.donna@gmail.com>
2026-06-17 20:31:08 +0100

    Removed comments

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -6,24 +6,6 @@
 // You should call this function a number of times to check it works for different inputs
 
 
-// const penceString = "399p";
-
-// const penceStringWithoutTrailingP = penceString.substring(
-//   0,
-//   penceString.length - 1
-// );
-
-// const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-// const pounds = paddedPenceNumberString.substring(
-//   0,
-//   paddedPenceNumberString.length - 2
-// );
-
-// const pence = paddedPenceNumberString
-//   .substring(paddedPenceNumberString.length - 2)
-//   .padEnd(2, "0");
-
-// console.log(`£${pounds}.${pence}`);
 
 function convertPenceToPounds(penceString){
 
@@ -34,8 +16,10 @@ function convertPenceToPounds(penceString){
     const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
 
     const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+
     return `£${pounds}.${pence}`;
 }
+
 console.log(convertPenceToPounds("299p"));
 console.log(convertPenceToPounds("499p"));
 console.log(convertPenceToPounds("25p"));
