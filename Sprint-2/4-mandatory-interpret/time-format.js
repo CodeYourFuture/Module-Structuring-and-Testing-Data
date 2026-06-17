4aa639f0f1a1d678f33cb50ca99a28099f0e6e9c
dona-shehu <shehu.donna@gmail.com>
2026-06-17 22:53:01 +0100

    Time format pad()

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -14,6 +14,7 @@ function formatTimeDisplay(seconds) {
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
+console.log(formatTimeDisplay(61))
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -21,18 +22,19 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> The function pad() is called 3 times.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> When pad is called for the first time the value assigned to  num is 
+// totalHours 
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> The return value is 00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> The value assigned to num when pad is being called for 3 time is remainingSeconds.
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> The return value of pad when  called the last time is 01.
