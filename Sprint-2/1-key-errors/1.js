91a716a975fb1ef9a1cfb89bc6d80f60f4bcda85
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-17 10:21:26 +0100

    fix error inside 1-key-errors/js

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,19 +2,27 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// Martin response - an error will occur because the variable decimalNumber is called without being declared in the global scope.  There is also a secondary error in that the parameter for convertToPercentage function is decimalNumber which is re-declared inside the function
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//   return percentage;
+// }
 
-console.log(decimalNumber);
+// console.log(decimalNumber);
 
 // =============> write your explanation here
+// Martin response - the cause of the error is the parameter for convertToPercentage function is decimalNumber which is re-declared inside the function
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  return `${decimalNumber * 100}%`;
+}
+
+console.log(convertToPercentage(0.5));
