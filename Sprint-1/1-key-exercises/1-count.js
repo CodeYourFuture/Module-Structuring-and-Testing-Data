1f8897790b8437d0b5a608eeabf061852a224b95
d-odumosu <dt.odumosu@gmail.com>
2026-06-17 19:49:52 +0100

    answered question in 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//line 3: we are re-assigning count using the assignment operator (=). The " = " is used in JavaScript to assing a value
+// to a variable and in this case  we are re-assinging count to equal count + 1.
