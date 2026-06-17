8e4be200ece3378edbc6ca9ee53143bb6f5345c4
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-17 13:08:12 +0100

    stored initial character of each name

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -7,5 +7,8 @@ let lastName = "Johnson";
 
 let initials = ``;
 
-// https://www.google.com/search?q=get+first+character+of+string+mdn
+let initials = `${firstName.charAt(0)} ${middleName.charAt(0)} ${lastName.charAt(0)}`;
 
+console.log(initials);
+
+// https://www.google.com/search?q=get+first+character+of+string+mdn
