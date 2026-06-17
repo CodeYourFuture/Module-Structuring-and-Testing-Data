d06f9a329d76f9c1c5c8beb2b1fc0596a3fe9707
dona-shehu <shehu.donna@gmail.com>
2026-06-17 19:38:54 +0100

    Add return a * b

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,25 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// This function does not return nothing.
+
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+// It just console.log(a * b);
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 
 function multiply(a, b) {
-  console.log(a * b);
+
+  return a * b;
+
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
