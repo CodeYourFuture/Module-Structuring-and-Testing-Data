482d8eeea9638f5f7fdd700a971fe8843041c361
russom <russom.g.meskel@gmail.cpm>
2026-06-17 20:29:11 +0100

    A dir and ext variables created  to store parts of the file path

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -13,11 +13,16 @@ const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
+// The base part of /Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt is file.txt
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex); // Keeps everything except the last slash just before week-1/interpret.
+const ext = filePath.slice(lastSlashIndex + 1).split(".")[1]; // Keeps everything after the last slash and splits it into an array of strings at the dot.
+console.log(`The dir part of ${filePath} is ${dir}`); 
+console.log(`The ext part of ${filePath} is ${ext}`);
+// The dir part of /Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt is /Users/mitch/cyf/Module-JS1/week-1/interpret
+// The ext part of /Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt is txt
 
 // https://www.google.com/search?q=slice+mdn
