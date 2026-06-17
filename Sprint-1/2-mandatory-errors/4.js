df2d21411e027607b281d29ccd7ce46becf4bdd4
d-odumosu <dt.odumosu@gmail.com>
2026-06-17 23:33:50 +0100

    fixed the variable naming in 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const _12HourClockTime = "8:53pm";
+const $24hourClockTime = "20:53";
+
+//when starting a variable name with a number we have to precede with either of _ or $
