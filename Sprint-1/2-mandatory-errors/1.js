b9db880e21f22853c3e3597c68480c5488751584
russom <russom.g.meskel@gmail.cpm>
2026-06-17 22:03:44 +0100

    An age variable created and  reassigned the value by 1

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
 const age = 33;
-age = age + 1;
+//age = age + 1; The variable age is declared as a constant using the const keyword, which means its value cannot be changed after it is assigned. 
+const newAge = age + 1; // A new variable called newAge is assigned.This way, we can still calculate the new age without modifying the original constant variable.
+
