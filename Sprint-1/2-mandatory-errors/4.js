56fa8437ae01f9d13f4919743a2878eddc6bb743
russom <russom.g.meskel@gmail.cpm>
2026-06-18 20:43:13 +0100

    Variable names changed to start with letters instead of numbers.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
 const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53";
+
+// In js variable names cannot start with a number. The variable names are changed to start with a letter instead of a number as below.
+const twelveHourClockTime = "8:53pm";
+const twentyFourHourClockTime = "20:53";
