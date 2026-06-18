5c345ec3849345693d18d00da57a3fc8c3007d20
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-18 12:09:32 +0100

    Explained problem

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,4 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//  We comment them using slash(//) where human can read and computer can't read because it is not a code or translated into code// 
