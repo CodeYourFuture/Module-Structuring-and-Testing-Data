78c195666791a7241a73e5379c989897150eb3a2
russom <russom.g.meskel@gmail.cpm>
2026-06-18 20:13:26 +0100

    Print the string fixed by defining city of birth variable first and print afterwards

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -2,9 +2,9 @@
 // what's the error ?
 
 console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton"; // The variable cityOfBirth is declared after it is used in the console.log statement. 
+const cityOfBirth = "Bolton"; // The variable cityOfBirth is declared after it is used in the console.log statement.
 
-// The order of the code should be as below. 
+// The order of the code should be as below.
 
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
