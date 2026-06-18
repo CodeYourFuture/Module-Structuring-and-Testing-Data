c4d5c881a12db1f1afe65fa47325dbef0053cfec
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-18 11:44:02 +0100

    Explained order of operation about math

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,11 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// console.log(num);
+
+// 1) Num represent a randomly  generated whole  number between maximum and minimum //
+
+// 2) Firstly, Math.random create a random number between 0 to 1 then using a maximum and minimum,which gives us range.
+//  Multiplying them create a random number inside that range.  Math.floor remove decimal parts and give us whole number
+//  finally we add minimum number ( + minimum),which start from  a minimum value. Therefore num represent a whole number.
