3b6570f8c2439e51e144fb7c6d759b8fc4b9ac73
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-19 10:24:26 +0100

    fix error and explain code execution

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +20,9 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+//  1) five function call there including log function .Lines 4 has two function and lines 5 has two function   .
+// 2 ) problem was line number 5 where comma was missing.
+//  3) reassignment statement lines are 4 and 5.
+//  4) declaration lines are 1,2,7 and 8
+// 5) line number 4, first it is removing commas using replaceAll then converting string to number for calculation.
