8b6387740a2f817ba6d638fc77675746ff124771
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-06-19 23:36:56 +0100

    Fix return statement order in 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,28 @@
-// Predict and explain first...
-//  =============> write your prediction here
+// Predict and explain :
+// The function will return undefined.
+// Therefore, the output will be:
+// "The sum of 10 and 32 is undefined"
 
-function sum(a, b) {
+//----------------------------------------------------------------------------------------------------------
+
+// Original code :
+/*function sum(a, b) {
   return;
   a + b;
 }
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
 
+//----------------------------------------------------------------------------------------------------------
+
+// My explanation :
+// The return statement ends the function immediately.
+// Because return is on its own line, the expression a + b is never executed.
+// As a result, the function returns undefined.
+
+//----------------------------------------------------------------------------------------------------------
+
+//new code :
+function sum(a, b) {
+  return a + b;
+}
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
