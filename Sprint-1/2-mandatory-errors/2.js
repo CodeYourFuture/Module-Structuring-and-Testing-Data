1f1249d11ab7f15b730ea93431a1b3eb008e4751
Tobias <amaechinatobias@gmail.com>
2026-06-20 10:06:42 +0100

    the Varible was being access from the TDZ

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
+
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+/* The error is that the variable `cityOfBirth` is being used before it is declared and assigned a value. In JavaScript, variables declared with `const` (or `let`) are not hoisted in the same way as `var`, so you cannot access them before their declaration. To fix this, you should declare and assign the variable before using it in the `console.log` statement:*/
+//The variable were being accessed from the Temporal Dead Zone (TDZ) before it was declared and assigned a value. In JavaScript, variables declared with `const` (or `let`) are not hoisted in the same way as `var`, so you cannot access them before their declaration. To fix this, you should declare and assign the variable before using it in the `console.log` statement:
+
+
