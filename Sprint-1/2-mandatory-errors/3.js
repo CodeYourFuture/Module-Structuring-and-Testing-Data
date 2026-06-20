26c75aaf824764416e2d4dea8d5e9e7954de5a9d
Tobias <amaechinatobias@gmail.com>
2026-06-20 10:35:58 +0100

    I predicted that it wouldn't work as slice method is used on a number instead of string

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,4 +1,5 @@
 const cardNumber = 4533787178994213;
+ 
 const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
@@ -7,3 +8,6 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+// I suspect the code doesn't work because the slice method is being called on a number, but slice is a method for strings. Therefore, I predict that the code will throw an error saying that slice is not a function for numbers.
+
+
