f98987f2eb5970513656f1342d0f740489028024
Niangh Ciang <cciang462@gmail.com>
2026-06-20 13:01:43 +0100

    Fixed square function by using a valid parameter name

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,25 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+//I think the problem in this code is the parameter inside the function. A number cannot be used as a parameter name.
 
-function square(3) {
+/*function square(3) {
     return num * num;
-}
+}*/
 
 // =============> write the error message here
+//SyntaxError: Unexpected number
 
 // =============> explain this error message here
+//The error message means that number cannot be used as a parameter name.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
-
+function square(num) {
+  return num * num;
+}
+console.log(square(2));
+console.log(square(3));
