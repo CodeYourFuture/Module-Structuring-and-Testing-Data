9dad574a442fab085481d56b6daa1d910bcbcbbd
Tobias <amaechinatobias@gmail.com>
2026-06-20 10:08:00 +0100

    the varible has been hoisted and initialled before accessing

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -7,4 +7,6 @@ const cityOfBirth = "Bolton";
 /* The error is that the variable `cityOfBirth` is being used before it is declared and assigned a value. In JavaScript, variables declared with `const` (or `let`) are not hoisted in the same way as `var`, so you cannot access them before their declaration. To fix this, you should declare and assign the variable before using it in the `console.log` statement:*/
 //The variable were being accessed from the Temporal Dead Zone (TDZ) before it was declared and assigned a value. In JavaScript, variables declared with `const` (or `let`) are not hoisted in the same way as `var`, so you cannot access them before their declaration. To fix this, you should declare and assign the variable before using it in the `console.log` statement:
 
+const cityOfBirth = "Bolton";
 
+console.log(`I was born in ${cityOfBirth}`);
