44be7d7162d866b62a4fdc0a1e369ab98d49af58
shafiekwalker7861 <westerncapetowndesigner@gmail.com>
2026-06-20 14:45:08 +0200

    Completed mandatory excercise 1

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,21 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// There are 5 function calls in this file.
+// Line 4 calls replaceAll() and Number().
+// Line 5 calls replaceAll() and Number().
+// Line 10 calls console.log().
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// The error was on line 5. The replaceAll() function call was missing the correct closing bracket/parenthesis.
+// I fixed it by making the line: priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // c) Identify all the lines that are variable reassignment statements
+// Lines 4 and 5 are variable reassignment statements because its declared on lines 1 and 2.
+
 
 // d) Identify all the lines that are variable declarations
+// line 1 and 2 are variable declarations because they are declared using the let keyword.
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+//// Removes commas and converts the string to a number.
