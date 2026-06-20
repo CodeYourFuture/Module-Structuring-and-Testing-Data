2cdf11c18e62a38c2ca2ecc2930a928fca696111
Zadri Abdule <zadriabdule@gmail.com>
2026-06-20 23:34:03 +0100

    implemented repeatStr and added Jest tests

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,16 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (typeof str !== "string") {
+    throw new TypeError("First argument must be a string");
+  }
+  if (!Number.isInteger(count) || count < 0) {
+    throw new TypeError("Count must be a non-negative integer");
+  }
+
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatStr;
