31950b1d1848fd12ecfb613ecdd8bf8ddd47267e
Arthur <>
2026-06-20 21:29:06 +0100

    What does num represent ?

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,7 +3,18 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+Math.floor is a function to make sure it is a whole number.
+
+The fuction will randomly choose a decimal number from 0 - 1. 
+After that will do the Math(100-1+1) and multiply the decimal value and add 1.
+
+
+
+
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+
