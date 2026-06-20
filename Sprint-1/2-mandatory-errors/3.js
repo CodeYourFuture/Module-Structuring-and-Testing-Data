7a6e15e5a54620f306824665b9b17d29c1ac3df3
Tobias <amaechinatobias@gmail.com>
2026-06-20 10:41:30 +0100

    I printed the type of data type variable cardNumber is on the console and it reads number, I have to comment out  slice method line of code

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,6 +1,7 @@
 const cardNumber = 4533787178994213;
+console.log(typeof cardNumber);
  
-const last4Digits = cardNumber.slice(-4);
+//const last4Digits = cardNumber.toString().slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -9,5 +10,7 @@ const last4Digits = cardNumber.slice(-4);
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 // I suspect the code doesn't work because the slice method is being called on a number, but slice is a method for strings. Therefore, I predict that the code will throw an error saying that slice is not a function for numbers.
+
+// i console the typeof cardNumber  and it returns number
 
 
