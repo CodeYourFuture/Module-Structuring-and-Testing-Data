3d31d1bc6baa6ecdf3ea5a3eb0a7472e492bdb88
shafiekwalker7861 <westerncapetowndesigner@gmail.com>
2026-06-21 16:44:48 +0200

    completed sprint-2/mandatory-interpret

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -21,18 +21,19 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// pad() is called 3 times.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> 00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> 1
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> The return value of pad when it is called for the last time is "01".
+//  This is because the value of num is 1, and we are padding it with zeros to make it 2 characters long.
