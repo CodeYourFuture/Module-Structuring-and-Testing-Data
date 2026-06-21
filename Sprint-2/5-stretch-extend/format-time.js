70fd1a03355e6558a2ffa3ef7edf978aa2274797
shafiekwalker7861 <westerncapetowndesigner@gmail.com>
2026-06-21 16:58:04 +0200

    Completed sprint-2/5 stretch-extend

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,9 +4,19 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
+
+  if (hours === 0) {
+    return `12:00 am`;
+  }
+
+  if (hours === 12) {
+    return `12:00 pm`;
+  }
+
   if (hours > 12) {
     return `${hours - 12}:00 pm`;
   }
+
   return `${time} am`;
 }
 
@@ -22,4 +32,19 @@ const targetOutput2 = "11:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+
+const currentOutput3 = formatAs12HourClock("12:00");
+const targetOutput3 = "12:00 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("00:00");
+const targetOutput4 = "12:00 am";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
 );
