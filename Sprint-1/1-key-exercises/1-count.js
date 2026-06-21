abb16d76a1c0e79a14885b240f57cfce90f26fe5
Dagim-Daniel <dagimdan19@gmail.com>
2026-06-21 23:39:28 +0100

    i just  answer 1 -key -exercise using  multiple line comment . i also created a branch coursework/sprint-1.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,13 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+/*Line 3 is updating the value of the variable `count`. 
+The `=` operator is an assignment operator, 
+which means it takes the value on the right side 
+(in this case, `count + 1`) 
+and assigns it to the variable on the left side (`count`). 
+So, it takes the current value of `count`, adds 1 to it, 
+and then stores that new value back into `count`. 
+in general, Line 3 is incrementing the value of `count` by 1.
+*/
+
