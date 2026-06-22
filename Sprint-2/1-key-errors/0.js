16055c33d66fbddedb286ee1e219d16205651394
B3-Bisrat <bisratttt1919@gmail.com>
2026-06-22 22:32:52 +0100

    1-key-errors are done.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// I predict the code will give an error because inside the function we create a variable called str again.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -10,4 +11,8 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+// The error occurs because we are trying to declare a variable named str inside the function, but it is already declared as a parameter. This creates a conflict and results in a syntax error.
 // =============> write your new code here
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
