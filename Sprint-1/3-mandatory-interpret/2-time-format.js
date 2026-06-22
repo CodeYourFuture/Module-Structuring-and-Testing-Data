996180e4a8925dfac76752963f7be3ca2a95ca27
Asanda <asandagwala@gmail.com>
2026-06-22 17:09:26 +0200

    time-format.js file update

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 10652; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -23,3 +23,18 @@ console.log(result);
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+// Answers 
+
+// a) 6
+
+// b) One function call is on this code - console.log() in line 10.
+
+// c) The remainder (%) operator returns the remainder left over when one operand is divided by a second 
+// operand. It always takes the sign of the dividend.
+
+// d) In line 4 the totalHours is calculated by subtracting the remainingMinutes from the totalMinutes and then dividing that answer by 60
+
+// e) It is a string representing the time movie time in hours, minutes and seconds
+
+// f) yes, because the variables for calculating the results use a formulas that will work with any number that is assigned to the movieLength variable
