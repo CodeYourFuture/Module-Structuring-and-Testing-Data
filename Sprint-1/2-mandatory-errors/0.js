c994a4e2def64db274a7c5966d40be1400853d0a
Asanda <asandagwala@gmail.com>
2026-06-22 14:40:33 +0200

    mandatory errors correction

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//  We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// This is a a syntax error. This text is supposed to be commented out in order for us to not run into this error
+//  we should add // so that the computer doesn't try to run this text as code .
+  
--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+
+// the variable age has been declared as constant in order to fix this age will
+// need to be declared using let so that it's values can be reassigned
