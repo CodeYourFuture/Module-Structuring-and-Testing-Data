8f010c7944ada4b35517ffeb467501cad0474517
Ndumiso Mkhasibe <ndmkhjhb025@student.wethinkcode.co.za>
2026-06-22 12:29:57 +0200

    count explaination

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,8 @@ let count = 0;
 
 count = count + 1;
 
+// line basically modifies the variable count by updating and adding 1 to it. So basically count is now equal to the current count + 1. In this instance count
+// count = 1
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
