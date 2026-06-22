6be81b5401b873077c583507c0db1556ac8bcb09
Asanda <asandagwala@gmail.com>
2026-06-22 15:57:03 +0200

    percentage-change.js file update

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,8 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// console.log(carPrice)
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +21,17 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+
+// a) There are 2 function calls in this code. replaceAll() in line 4 and 5 and console.log() in line 10
+
+// b) There is a SyntaxError on line 5. The replaceAll() function takes 2 arguments 
+// and this is missing a comma between the 2 arguments 
+
+//c) line 4 & 5
+
+// d) line 1,2 7 & 8
+
+// e) It is replaces the comma "," in carPrice and removes any spaces so 
+// everything becomes one string
