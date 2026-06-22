c97a1ab20137157ec215f0cfbce48e680f57f53e
John Robinson <robinsonantiquex@gmail.com>
2026-06-22 22:23:50 +0100

    Refactor count.js to clarify variable update and implement initials extraction in initials.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,8 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Answer: Line 3 is updating the value of the variable 'count' by adding 1 to its current value.
+// The = operator is used to update / assign a new value to the variable 'count'.
+// It takes the current value of 'count', adds 1 to it, and then assigns that new value back to 'count'.
+// 
