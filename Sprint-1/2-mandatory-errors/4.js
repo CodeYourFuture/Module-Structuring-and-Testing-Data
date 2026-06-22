3d1f1b7850e2ac2daca073725323b1b585c5b8a2
Yonatan Teklemariam <jante26uk@gmail.com>
2026-06-22 10:53:44 +0100

    Remove sprint 1 and 3 files from sprint 2 branch

--- Sprint-1/2-mandatory-errors/4.js
+++
@@ -1,2 +0,0 @@
-const 12HourClockTime = "8:53pm"; // this will throw an error because the variable name started with a number where it shouldn't be.
-const 24hourClockTime = "20:53"; // the same happens here as well.
