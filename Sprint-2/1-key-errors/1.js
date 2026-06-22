df6c2f80d47f11fa85445b9b3ddeafcf4bd3560b
Yonatan Teklemariam <jante26uk@gmail.com>
2026-06-22 11:44:31 +0100

    Correct variable scope and console log usage
    Fixed variable declaration issue in convertToPercentage function and updated console log.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,19 @@
 // Predict and explain first...
-
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> The error will appear because we are trying to log the variable decimalNumber in the function. We have to call the function at this point. Besides to that, the decimalNumber is again declared inside the function where it shouldn't be.
+
 
 // Try playing computer with the example to work out what is going on
+
 
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
+
   return percentage;
 }
 
+
 console.log(decimalNumber);
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
