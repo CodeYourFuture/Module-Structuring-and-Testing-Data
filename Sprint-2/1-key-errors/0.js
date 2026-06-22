ff84a769e23749f80cd14762895655df3462c2c1
Yonatan Teklemariam <jante26uk@gmail.com>
2026-06-22 11:43:35 +0100

    Fix redeclaration error in capitalise function
    Renamed variable 'str' to 'result' to avoid redeclaration error.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,23 @@
 // Predict and explain first...
-//  =============> write your prediction here
-
+//  =============> I presume the str is repeatedly declared, first as a parameter and again as a variable.
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+
+// =============> str is already declared in the parameter and shouldn't be declared inside the function. The error says it's been declared already for this reason. Therefore we need to rename the str inside the function.
+/* ==========> function capitalise(str) {
+let result = `${str[0].toUpperCase()}${str.slice(1)};
+return result;
+}
+OR
+function capitalise(str) {
+return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
+*/
+
