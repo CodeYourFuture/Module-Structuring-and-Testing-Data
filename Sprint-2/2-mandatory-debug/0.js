3e834b447aa968317394f30448e1fd9175ab8a68
Asanda <asandagwala@gmail.com>
2026-06-23 20:20:18 +0200

    updated 0.js file

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+// This function is not returning anything
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -9,6 +9,13 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
-
+// This function is not returning anything. It does multiply the assigned values of a,b but it does not return the String with the 
+// as required on line 9
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a,b){
+  return(a*b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
