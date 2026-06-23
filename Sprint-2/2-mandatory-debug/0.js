99a897ce13a7b016234c2d28d31c95f59ac18d6f
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-24 00:02:49 +0200

    debug 1

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+/*The code will print a correct multiplication inside the function*/
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +10,13 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+/*The result shows undefined because the function prints the answer with console.log instead of returning it, so nothing is passed back into the template string.*/
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
