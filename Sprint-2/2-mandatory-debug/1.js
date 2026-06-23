3a6e5407874281c4e80c8ce10666cbdfcf60daf1
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-23 23:26:54 +0200

    Clean Sprint 2 (no Sprint 1)

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+//The sum of 10 and 32 is undefined
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +9,14 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+/*The function returns undefined because the return; statement ends the function immediately, so the calculation a + b never runs or gets returned.*/
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
