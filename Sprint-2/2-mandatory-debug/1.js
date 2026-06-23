ccc134bf7947e9533514b8ad8af9781ccb197924
Asanda <asandagwala@gmail.com>
2026-06-23 20:23:36 +0200

    updated 0.js file on mandatory debug folder

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+// this function does not return anything since the return statements has a semicolon before the command of what 
+// it has to actually return
 function sum(a, b) {
   return;
   a + b;
@@ -11,3 +12,8 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a,b){
+  return (a+b);
+
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
