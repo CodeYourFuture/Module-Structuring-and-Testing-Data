b823c2736756a71c32c10bdcd64aed3a6ea6c1c4
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-23 21:23:58 +0200

    interpret code

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -22,17 +22,23 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// a) 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// b) 0
 
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
+// c) "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// d) 1, because the last time pad is called it is for the remaining seconds which is 1 second in this case
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+
+// e) "01", because the last time pad is called it is for the remaining seconds which is 1 second in this case, and pad adds a leading zero to make it two digits
