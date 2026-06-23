3780aff480788c1d009ad15dffc937e8723eddb8
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-23 08:03:07 +0100

    Add additional test and check it passes to 1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -36,5 +36,6 @@ assertEquals(isProperFraction(1, 2), true);
 assertEquals(isProperFraction(3, 1), false);
 assertEquals(isProperFraction(2, 2), false);
 assertEquals(isProperFraction(5, 0), false);
+assertEquals(isProperFraction(-1, 0), false);
 assertEquals(isProperFraction(-2, 4), true);
 assertEquals(isProperFraction(4, -2), false);
