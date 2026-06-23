7e23fc35fa6b2b1b6e0071e2a6b962818c6e59a6
lintsang <lintsang.uk@yahoo.com>
2026-06-23 18:09:59 +0100

    recorrect 2-mandatory-errors/1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -6,3 +6,4 @@ age = age + 1;
 //TypeError: Assignment to constant variable.
 //This error is caused by the wrong reassignment of variable 'age'. It is a constant that cannot be reassigned.
 //So the error prompted when the second line code try to reassign the variable value.
+//In this case, I would use let age instead of const age to assign age and set its value.
--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I predict this code will create a function which can capitalise the first letter of a string and keep the second letters to last one the same. This string is stored and return to the function call.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -9,5 +9,8 @@ function capitalise(str) {
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> The error message shows "SyntaxError: Identifier 'str' has already been declared". And it indicated the variable name 'str' at line 8, I think it triggered a syntax error when try to declare 'str' in the function while it is already being defined as a function parameter. So I changed it to 'strStored' in the function and it works.
+// =============> function capitalise(str) {
+//  let strStored = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return strStored;
+//}
