30efd9d3787e718ba078113e9186e49935b467d9
Asanda <asandagwala@gmail.com>
2026-06-23 20:11:14 +0200

    updated 2.js file

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,6 +1,7 @@
 
 // Predict and explain first BEFORE you run any code...
-
+// It will probably not run because the value 2 is a fixed integer, your parameters / arguments get values 
+// assigned to them so you can't really reassign 2.
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
@@ -10,11 +11,14 @@ function square(3) {
 }
 
 // =============> write the error message here
-
+// SyntaxError: Unexpected number
 // =============> explain this error message here
-
+// This is a syntax error the number 2 cannot be used as a parameter since it cannot be reassigned.
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
+function square(num){
+    return num*num;
+}
+// console.log(square(3))
 
