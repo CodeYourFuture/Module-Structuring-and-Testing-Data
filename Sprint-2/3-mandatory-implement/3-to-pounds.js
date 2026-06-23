645b665e8c606c364a04fab4196f387a41b26d3c
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-23 21:16:43 +0200

    feat: convert toPounds script into reusable function and add test cases

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,18 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+  const withoutP = penceString.slice(0, -1);
+
+  const padded = withoutP.padStart(3, "0");
+
+  const pounds = padded.slice(0, -2);
+  const pence = padded.slice(-2);
+
+  return `£${pounds}.${pence}`;
+}
+
+console.log(toPounds("399p"));
+console.log(toPounds("45p"));
+console.log(toPounds("5p"));
