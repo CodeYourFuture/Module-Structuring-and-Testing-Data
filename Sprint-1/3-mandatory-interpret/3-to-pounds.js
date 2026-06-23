7deb9ec073bb863376e5cbbd7923a1a0855b44d1
Asanda <asandagwala@gmail.com>
2026-06-23 11:45:19 +0200

    pound.js update

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -20,6 +20,13 @@ const pence = paddedPenceNumberString
 // console.log(pence)
 console.log(`£${pounds}.${pence}`);
 
+// This program takes a string representing a price in pence
+// The program then builds up a string representing the price in pounds
+
+// You need to do a step-by-step breakdown of each line in this program
+// Try and describe the purpose / rationale behind each step
+
+// To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
 // 2. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length-1): this 
 // variable creates a new string that cuts the penceSting to 399,so it basically creates a new
