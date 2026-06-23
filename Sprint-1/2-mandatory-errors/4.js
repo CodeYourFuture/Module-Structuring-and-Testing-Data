5ccc3b7b4510054e8e8882bb45b515f0a9906fe4
vmoratti <vmoratti@gmail.com>
2026-06-23 11:55:20 +0100

    Fix variable names to comply with naming conventions
    Renamed variables to start with a letter to avoid SyntaxError.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,3 +1,10 @@
 const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53"; 
 //what is the task in this exercise?
+
+// *****************Answer***********
+//Variable names must srart with a letter and here they start with a number
+//triggering SyntaxError
+//Therefore the they should look like the code below.
+const twelveHourClockTime = "8:53pm";
+const twentyFourHourClockTime = "20:53";
