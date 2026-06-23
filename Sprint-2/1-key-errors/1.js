a6ba68a27b7330612a3c30c7594800f6a8689740
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-23 19:06:14 +0200

    fix: correct convertToPercentage function

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,5 +1,4 @@
 // Predict and explain first...
-
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
@@ -16,5 +15,15 @@ console.log(decimalNumber);
 
 // =============> write your explanation here
 
+/*An error will occur when the program runs because decimalNumber is declared twice. The function already receives decimalNumber as a parameter, but then const decimalNumber = 0.5 tries to create another variable with the same name inside the same scope. JavaScript does not allow redeclaring variables with const.
+Another error will happen at console.log(decimalNumber) because decimalNumber only exists inside the function and cannot be accessed outside of it.*/
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
