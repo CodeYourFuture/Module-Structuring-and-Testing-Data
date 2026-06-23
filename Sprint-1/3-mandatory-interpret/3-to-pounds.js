2721e61301f385747462d5ae99c3d9b0f2f975b6
John Robinson <robinsonantiquex@gmail.com>
2026-06-23 09:14:01 +0100

    Enhance comments in 3-to-pounds.js for detailed step-by-step explanation

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,11 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. Line 3: uses the substring method to remove the "p" from the end of the string
+// 3. Line 8: the variable paddedPenceNumberString is used to ensure that the string has at least 3 characters,
+// if not it will add a zero to the start of the string
+// 4. Line 9 - 11: the variable pounds uses the substring method in the same way as line 3 but this time removes the last two characters
+// the result is now stored in the const 'pounds' to be used in the final output
+// 5. Line 14 - 16: the variable pence also uses the substring method to extract the last two characters from paddedPenceNumberString
+// the result is now stored in the const 'pence' to be used in the final output, a '0' is added to the end of the string if there is only one character
+// 6. Line 18: the console.log function prints our output - the values stored in the consts 'pounds' and 'pence'.
