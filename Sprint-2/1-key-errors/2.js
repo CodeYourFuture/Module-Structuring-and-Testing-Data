72f37f53f95f8ca77e2088879911bd7801186a78
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-24 15:59:43 +0100

    resolve and explain syntax error in Sprint-2 > 1-key-errors > 2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,21 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> we should put num in the input of the function, instead of 3
 
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
+// =============> 
+//Uncaught SyntaxError SyntaxError: Unexpected number
 
 // =============> explain this error message here
-
+//3  is unexpected. when we define a function  we want to give it parameter(s), not a specific number. it is when the function is called that we want to give it specific values to pass in. 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
+function square(num) {
+    return num * num;
+}
 
