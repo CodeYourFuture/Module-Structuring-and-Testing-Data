b9e9f5c978b8c7c9d331007df7b0eab09a54c538
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-24 14:58:21 +0100

    resolve syntax error in 1-key-errors 0.js and comment

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,16 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// The issue might be that the input to the function is str but also there is a variable declaration str inside it
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+function capitalise("hello") {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// Uncaught SyntaxError: Identifier 'str' has already been declared - meaning str should not have been used inside the function or the parameter of the function
+function capitalise(str) {
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
