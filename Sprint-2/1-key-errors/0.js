5343b19a8a43cd0708ef55f576f17b15c0aab3d5
lintsang <lintsang.uk@yahoo.com>
2026-06-24 07:04:13 +0100

    Make sure only Sprint 1 files is touched

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> I predict this code will create a function which can capitalise the first letter of a string and keep the second letters to last one the same. This string is stored and return to the function call.
+//  =============> write your prediction here
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -9,8 +9,5 @@ function capitalise(str) {
   return str;
 }
 
-// =============> The error message shows "SyntaxError: Identifier 'str' has already been declared". And it indicated the variable name 'str' at line 8, I think it triggered a syntax error when try to declare 'str' in the function while it is already being defined as a function parameter. So I changed it to 'strStored' in the function and it works.
-// =============> function capitalise(str) {
-//  let strStored = `${str[0].toUpperCase()}${str.slice(1)}`;
-//  return strStored;
-//}
+// =============> write your explanation here
+// =============> write your new code here
