598d02c0857e42d697b7ca7a8dcb25c0343b6f7b
russom <russom.g.meskel@gmail.cpm>
2026-06-24 22:35:34 +0100

    Error explained and corrected.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -3,6 +3,9 @@
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
+// decimalNumber is declared twice and passed a value inside the function.
+// The function hasn't been called properly
+
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
@@ -16,5 +19,17 @@ console.log(decimalNumber);
 
 // =============> write your explanation here
 
+// The error occurred because decimalNumber is declared twice. No need to create the percentage variable too.
+// Instead the expression is returned directly without declaring a new variable.
+// The function should be called with a value passed in as an argument and the result should be stored in a variable to be logged to the console.
+// convertedPercentage is declared to store the result of the function call and then logged to the console.
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  return`${decimalNumber * 100}%`;
+}
+
+const convertedPercentage = convertToPercentage(0.5);
+console.log(convertedPercentage);
