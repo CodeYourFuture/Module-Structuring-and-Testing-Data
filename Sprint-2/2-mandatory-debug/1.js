0c1f1ad5df8b129ac190ad2c03326dc08a2b8a8f
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-24 04:01:13 +0100

    Fix formatting in explanation comment for sum function

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -12,8 +12,8 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
 
-// Explanation: The return keyword is used and the functions is not executed because the expression is on a different line 
-  which means the computer would not run the expression alongside the return keyword.
+// Explanation: The return keyword is used and the functions is not executed because the expression is on a different line
+//which means the computer would not run the expression alongside the return keyword.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
