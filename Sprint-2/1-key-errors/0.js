71235b3fa457a8b728467c3aa6e32304563a99a4
vmoratti <vmoratti@gmail.com>
2026-06-24 11:55:10 +0100

    solve the code error message

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
-// call the function capitalise with a string input
+// Well, I think the variable "str" is being declared twice, so the code will
+//  through an error message.
+//
+//call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
@@ -10,4 +12,12 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+//The variable "str" has been used as a parameter for the capitalise function
+//and also been declared again inside the function. I think if "let" is removed
+//the code will work.
 // =============> write your new code here
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise("sorted"));
