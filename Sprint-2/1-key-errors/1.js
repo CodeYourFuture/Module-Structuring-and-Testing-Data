06429030fb3fa2c361c04f5721233149a092b700
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-24 03:17:39 +0100

    Fix syntax errors in convertToPercentage and square functions, and update predictions and explanations

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -3,18 +3,31 @@
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
+//Prediction: The program will fail with a SyntaxError because 'decimalNumber' is redeclared.
+
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  //const decimalNumber = 0.5; <--- this line causes the syntaxError.
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+//console.log(decimalNumber); <--- this line causes the ReferenceError.
 
 // =============> write your explanation here
+//1. Inside the function: Just like the previous example, the parameter `decimalNumber` acts as a local variable. Trying to redeclare it with `const decimalNumber = 0.5;` causes a crash.
+// Additionally, hardcoding 0.5 defeats the purpose of having a parameter at all.
+
+// 2. Outside the function: The variable `decimalNumber` is "scoped" to the function. It doesn't exist in the outside world. When `console.log(decimalNumber)` runs globally, JavaScript doesn't know what it is,
+// causing a ReferenceError. You need to call the function and pass the number as an argument instead.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+console.log(convertToPercentage(0.5));
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,22 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
+// Prediction: The code would likely return a syntax error because the parameter in the () is '3' and not 'num'.
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
 
+// A defined function in a () must be variable names, not values.
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+    return num * num;       
+}
+console.log(square(3));
