6ca562542d41a1eab7166652e65e99b4d5d75978
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-24 15:50:36 +0100

    resolve syntax and reference errors in Sprint-2 > 1-key-errors > 1.js

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,12 +1,12 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> same as before, decimalNumber in the function para, but also as a constant inside the function. also we are trying to log decimalNumber, but thats the input, or inside the function the new variable which would result in a scope issue, since this does not exist outside of the function
 
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  const decimal = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
@@ -14,7 +14,14 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
-
+// =============> Uncaught SyntaxError: Identifier 'decimalNumber' has already been declared - so the same issue as the previous
+//also after changing the decimalNumber to decimal inside the function: Uncaught ReferenceError ReferenceError: decimalNumber is not defined -->in console log we should ask for the function
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============>
+function convertToPercentage() {
+  const decimalNumber = 0.5;
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+console.log(convertToPercentage());
