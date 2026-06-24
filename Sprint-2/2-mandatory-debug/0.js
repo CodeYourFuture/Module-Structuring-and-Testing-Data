aaa500c65c72b75aeaab6b3a41deb85ebed1f9ce
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-24 04:00:38 +0100

    Add predictions and explanations for multiply and sum functions, and correct their implementations

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// 320
 
 function multiply(a, b) {
   console.log(a * b);
@@ -10,5 +11,14 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
+/*Explanation: The `multiply` function calculates `a * b` and immediately prints it to 
+  the console, but it doesn't use the `return` keyword to hand the value back.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+//The sum would return as undefined.
+
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +11,15 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+// Explanation: The return keyword is used and the functions is not executed because the expression is on a different line 
+  which means the computer would not run the expression alongside the return keyword.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
