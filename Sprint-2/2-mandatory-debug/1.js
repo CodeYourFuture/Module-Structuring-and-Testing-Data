3772da26995c01861ec7f0fa9dc5e6f07572aa7d
russom <russom.g.meskel@gmail.cpm>
2026-06-25 21:13:13 +0100

    Error explained and fixed by removing semicolon after return statement.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// This will throw an error because there is a semicolon after return statement.
+
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +11,17 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+//The sum of 10 and 32 is undefined
+
+// The above error message is displayed when tested with node.
+// The cause of the error message is the semicolon that is placed after the return statement.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
