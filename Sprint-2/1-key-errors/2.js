6de1b67c263ae5668bf0d75915c01893b69b482a
russom <russom.g.meskel@gmail.cpm>
2026-06-25 20:53:08 +0100

    square function error explained and corrected.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,16 +5,31 @@
 
 // =============> write your prediction of the error here
 
+//  num is not declared as a result undefined error will be displayed. 
+
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
 
+// /Users/Russom/Desktop/x.js:1
+//function square(3) {
+//SyntaxError: Unexpected number
+
 // =============> explain this error message here
+
+// The error message indicates that 3 cannot be passed as a parameter in the function declaration.
+// The parameter should be a variable name not a number.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
 
+function square(num) {
+	return num * num;
+}
+console.log(square(3));
+
+// The output of the function will be 9 as 3 * 3 = 9.
