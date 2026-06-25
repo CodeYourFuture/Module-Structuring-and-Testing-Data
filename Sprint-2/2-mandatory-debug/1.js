de2b780d9813093c16b5b491ae9c500e9b3556ff
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-25 18:58:29 +0100

    debug 1.js in Sprint 2 > 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> return is not followed by anything (directly), and a+b is after the ";" after return. in a function nothing gets executed after return
 
 function sum(a, b) {
   return;
@@ -8,6 +8,11 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> return value is undefined, so in the console log sentence the result (return value) is substituted with "undefined" instead of the actual sum
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
