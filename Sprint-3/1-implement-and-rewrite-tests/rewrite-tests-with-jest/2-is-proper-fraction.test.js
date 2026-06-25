163c1d0e29f16fe10bdaf1b899cf7b839d27e9af
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-25 10:33:44 +0100

    Write jest tests for 2-is-proper-fraction.test.js

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,3 +8,13 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
+
+test(`should return false when (numerator >= denominator)`, () => {
+  expect(isProperFraction(2, 1)).toEqual(false);
+  expect(isProperFraction(2, 2)).toEqual(false);
+});
+
+test(`should treat numerator and denominator as absolute values when determining proper fractions`, () => {
+  expect(isProperFraction(-2, 3)).toEqual(true);
+  expect(isProperFraction(-7, 2)).toEqual(false);
+});
