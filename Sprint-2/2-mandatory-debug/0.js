9cad53a0c09f0099151ddb9a03b6defc2d62f90c
Edina Kurdi <edina.kurdi@gmail.com>
2026-06-25 18:54:18 +0100

    debug 0.js in Sprint 2 > 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> the function is not returning anything, it just logs to the console.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,14 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here:
+// 320
+// The result of multiplying 10 and 32 is undefined
+// it prints the result of 10*32 since we called the function in our last console log, then it prints the sentence, but the sentence would include the return value of the function, but that is undefined, as the function did not return any value.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
