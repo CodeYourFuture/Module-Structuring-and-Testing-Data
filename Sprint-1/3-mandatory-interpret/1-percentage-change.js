a96b47edea6a7784209709b94b8a9e8fd07d6391
Tobias <amaechinatobias@gmail.com>
2026-06-25 17:48:30 +0100

    gave the right programming term for the term that belongs in the blank

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -39,3 +39,4 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
 /* The expression Number(carPrice.replaceAll(",","")) is converting the string value of carPrice, which contains a comma, into a number. The replaceAll() method is used to remove all commas from the string, and then the Number() function is used to convert the resulting string into a number. This allows for mathematical operations to be performed on the value of carPrice without any issues caused by the presence of commas in the string.*/
+// a comma is missing between the two arguments in the replaceAll() method, which is causing a syntax error. The correct syntax should be replaceAll(",", "")., the programming term that  belongs in the blank is call Arguments as  they are  actual values passed when the function is called:
