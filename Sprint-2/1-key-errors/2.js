4b743c8a159d10bbeccdbcd0d73d12a2ac11d02d
vmoratti <vmoratti@gmail.com>
2026-06-25 11:45:02 +0100

    fix code, explain fix

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,24 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
-
-function square(3) {
+//=========Prediction=============
+// I think there will not be en error till we call the function.
+/*function square(num) {
     return num * num;
 }
-
+console.log(square(5))*/
 // =============> write the error message here
-
+// Unexpected number
 // =============> explain this error message here
-
+// I think it
 // Finally, correct the code to fix the problem
-
+// ===========correction=========
+// to correct the code we need to introduce parameter as a variable, rather than a number.
+// that way any number can be passed when function is called.
 // =============> write your new code here
-
-
+function square(num) {
+  return num * num;
+}
+console.log(square(5));
