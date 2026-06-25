de0d8144263fa26191d5d29d1c92193bbd2559c3
Enice-Codes <mutandaeunice@94gmail.com>
2026-06-26 00:27:57 +0200

    completed key errors excercise 2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,27 @@
 
 // Predict and explain first BEFORE you run any code...
+// i predict that the code will fail because js expects a variable name ,not a number and 3 is a number.
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// Uncaught SyntaxError: Unexpected number
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+//Uncaught SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// function parameters must be identifiers (variable names) such num ,x numbers can not be used like variable names 
+// and so the engine will be unable to  read the code and cause synx errorS
 
-// Finally, correct the code to fix the problem
+// Finall correct the code to fix the problem
 
 // =============> write your new code here
-
+function square (num){
+    return num*null;
+}
 
