5d0b10b8bbc1897cac9c92fefce344248283c38d
Tobias <amaechinatobias@gmail.com>
2026-06-26 18:37:06 +0100

    gave an explanation as to why the console.log method produced undefined

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -7,6 +7,7 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here.// The code will print undefined because the function sum does not return any value. Instead, it has a return statement with no value, which means it will return undefined by default. The second line of the function, a + b, is never executed because the return statement ends the function execution before it can be reached.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
