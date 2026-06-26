695a57e2e52be7f7a2a3b0cd4484a78ce94ea366
Tomislav Dukez <tomdu3@ymail.com>
2026-06-26 16:19:11 +0100

    add task 3 - fix slice number

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = Number(String(cardNumber).slice(-4));
+
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,8 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// Slice method can be used on strings and arrays, not on numbers.
+// The error message is telling that slice is not a function, but this happens because number don't have a slice method.
+// I have used String(cardNumber) to convert the number to a string, so that the slice method can be used on it.
+// Then the 4 digit string is converted back to a number with Number(string) to match the expected type.
