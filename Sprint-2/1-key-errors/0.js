bf0aa4984f4f6bfe6008d3c66d092e4db7164f3d
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-26 11:04:25 +0100

    Fix syntax error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,21 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+////////  error code /////
+
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// fix code //
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let string = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return string;
 }
+console.log(capitalise("hello js error"));
 
 // =============> write your explanation here
 // =============> write your new code here
+
+// The Error was syntax error as "str" has already been declared and we can't declare it again as variable name. Now I declare a new variable name  and assign value.
