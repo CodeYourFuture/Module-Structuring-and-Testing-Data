f09ff4b659ea9adb07fd96bb391f4bed567537f2
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-26 18:33:50 +0100

    fix syntax error and define erro

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -3,16 +3,18 @@
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
+// There will be two errors, First syntax errors as decimalNumber has already been declared and tried to redeclare it again inside function.
+// second without defining decimalNumber try to use decimalNumber in log.
+
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.9));
 
 // =============> write your explanation here
 
