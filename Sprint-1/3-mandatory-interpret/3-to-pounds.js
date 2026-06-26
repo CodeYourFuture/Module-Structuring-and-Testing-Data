aef0c0751c0d745c8aee50e5808c76da54874d25
Maryam Janjua <maryamjanjua65@gmail.com>
2026-06-26 11:21:11 +0100

    Did Mandatory Intepret tasks on 3 files of sprint 1

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -30,21 +30,24 @@ const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 ) : calculate  the length and then subtract 1,length is 4-1=3 , and extract substring 0 to 3 i.e 399
-/*
+
 line 8.Padstart puts zero in beginning until it meets the desire length. The length is 3 and string length is already 3, so it remained unchanged.
 paddedPenceNumberString = "399"
+
 line 9-12. 
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 ); paddedPenceNumberString.length - 2: 3-2=1 -> paddedPenceNumberString.substring(0,1)-> pounds="3"
-line 14-16.
+l
+ine 14-16.
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
 (paddedPenceNumberString.length - 2)-> 3-2 = 1 -> substring(1).padEnd(2,"0")-> 99, the length is already two.
 pence ="99"
 
+line 18.
 console.log(`£${pounds}.${pence}`);
 £3.99
 */
