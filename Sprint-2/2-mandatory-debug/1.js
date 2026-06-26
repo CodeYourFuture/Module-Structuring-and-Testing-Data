82ad98f8e21b39e973d7189fea776ef0a21304b8
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-26 20:06:44 +0100

    fixed sum function to return correct value

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+//  It will show undefined
+
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +11,11 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// the reason is that I did not write anything inside return that's why return can't find anything to return
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
