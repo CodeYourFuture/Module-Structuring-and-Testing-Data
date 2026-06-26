2bca2a93309f22fe1b4e1af8947a82afaff8a364
Tobias <amaechinatobias@gmail.com>
2026-06-26 17:15:16 +0100

    commented out the original program so I can write a new working code

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -5,14 +5,14 @@
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+/*function convertToPercentage(decimalNumber) {
    decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(decimalNumber); */
 
 // =============> write your explanation /* the first error SyntaxError pointed at line 9 as to where the error occurred  and explained that the identifier-decimalNumber has already been declared. The second error ReferenceError pointed at line 15 as to where the error occurred and explained that the identifier-decimalNumber is not defined in the global scope. */
 /* /home/tobi/CYF/Module-Structuring-and-Testing-Data/Sprint-2/1-key-errors/1.js:9
