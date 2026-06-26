ca8a1e2413c7c171d80ad8bbd266d754828e8177
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-26 08:41:22 +0100

    Comment out original code and predictions for clarity in error explanation

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -8,15 +8,15 @@ const last4Digits = cardNumber.slice(-4);
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
-PREDICTION:
+//PREDICTION:
 //the code wont work because the card number isnt in ("") and the computer wont be able to apply the .slice due to this.
 
-THE ERROR: 
+//THE ERROR:
 //cardNumber.slice is not a function
 
 //YES! the error is what i predicted.
 
-FIX:
+//FIX:
 const cardnumber = "4533787178994213";
 const last4Digits = cardnumber.slice(-4);
-console.log(last4Digits); 
+console.log(last4Digits);
