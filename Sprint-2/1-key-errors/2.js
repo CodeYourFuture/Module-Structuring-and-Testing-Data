56258790c1a0fd0181375ee07a9ab983c9e762d7
Tobias <amaechinatobias@gmail.com>
2026-06-26 17:48:51 +0100

    commented out the original code

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,17 +1,17 @@
 
-// Predict and explain first BEFORE you run any code...
+// Predict and explain first BEFORE you run any code...we are going to get a SyntaxError because the function parameter is a number and not a variable name. The function parameter should be a variable name that can be used inside the function body.
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here // SyntaxError: Unexpected number. 
 
-function square(3) {
+/*function square(3) {
     return num * num;
-}
+}*/
 
-// =============> write the error message here
+// =============> write the error message here /* /home/tobi/CYF/Module-Structuring-and-Testing-Data/Sprint-2/1-key-errors/2.js:8 function square(3) { SyntaxError: Unexpected number*/
 
-// =============> explain this error message here
+// =============> explain this error message here/* this error is saying that  something is wrong with the function parameter because it is a number and not a variable name. The function parameter should be a variable name that can be used inside the function body. the SyntaxError denote that the rule is violated, and pointed out that  the number is unexpected at line 8 */
 
 // Finally, correct the code to fix the problem
 
