609ccf649bdd54ab3ed231fe4f30e78e211d97cc
Tobias <amaechinatobias@gmail.com>
2026-06-26 19:10:56 +0100

    Wrote the new code to fix the scope issue with the global variable

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -20,5 +20,15 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);*/
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
+
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
