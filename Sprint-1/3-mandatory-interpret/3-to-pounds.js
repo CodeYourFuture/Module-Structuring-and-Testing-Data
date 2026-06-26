09bd32f1e7683b03b9e39c8d548f50bf0fa6c0c5
Sayeed Hussain <hussainsahid980@gmail.com>
2026-06-26 22:19:38 +0100

    added function

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,21 +1,28 @@
-const penceString = "399p";
+// const penceString = "399p";
 
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
 
-console.log(`£${pounds}.${pence}`);
+  let poundPence = `£${pounds}.${pence}`;
+
+  return poundPence;
+}
+console.log(toPounds("223p"));
+
+// console.log(`£${pounds}.${pence}`);
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
