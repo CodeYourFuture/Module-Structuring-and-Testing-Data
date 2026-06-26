c6e39912eb59684afa2155329a911d6f3f0da68d
Tobias <amaechinatobias@gmail.com>
2026-06-26 18:18:15 +0100

    gave the explanation as to why the fist console.log inside the function printed a value and the one ouside that  called the fuction using template literal produce undefine

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -7,7 +7,7 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> write your explanation here. /* The function multiply is called inside the console.log statement, but it does not return any value. Instead, it only logs the result of multiplying a and b to the console. Therefore, when the console.log statement is executed, it will print "The result of multiplying 10 and 32 is undefined" because the multiply function does not return anything. The first console.log inside the multiply function will print 320 to the console, but the second console.log will print undefined. */
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
