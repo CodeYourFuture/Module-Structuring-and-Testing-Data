637daa2cdfe6536c089196d3539640a24775c305
Tobias <amaechinatobias@gmail.com>
2026-06-26 18:20:13 +0100

    commented out the original code to allow re-writing a new one

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,11 +1,11 @@
 // Predict and explain first...
 
 // =============> write your prediction here// The code will print 320 and undefine  secondly.
-function multiply(a, b) {
+/*function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);*/
 
 // =============> write your explanation here. /* The function multiply is called inside the console.log statement, but it does not return any value. Instead, it only logs the result of multiplying a and b to the console. Therefore, when the console.log statement is executed, it will print "The result of multiplying 10 and 32 is undefined" because the multiply function does not return anything. The first console.log inside the multiply function will print 320 to the console, but the second console.log will print undefined. */
 
