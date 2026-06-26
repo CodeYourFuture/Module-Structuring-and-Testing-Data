80f3a4ff5431f69cc8b581cbf387353bfd998028
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-06-26 12:36:17 +0100

    Update comments in formatTimeDisplay function to provide detailed explanations for pad function calls

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -21,18 +21,21 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> It would be called 3 times, that is for hours, minutes and seconds.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> The answer would be '0' because the first time pad is called, relates to the hours remaining.
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> It would return '00' because the value of num 0 is converted to a string "0", since its length is less than two the pad function adds a "0" to the front to output "00".
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> The answer is 1.
+//explanation: the last time pad is  called is for the remaining seconds, which would return 1 because we passed 61 into the program, calculated as 61 % 60.
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> Answer "01".
+// Explaination: The return value of pad when it is called for the last time in this program is '01'. This is because the value of num is 1 (the remaining seconds after calculating 61 % 60).
+// Since the length of the string representation of 1 is less than 2, the pad function adds a "0" to the front, resulting in "01".
