782cf84942cad98e7c189774875832b137a2c526
Tobias <amaechinatobias@gmail.com>
2026-06-26 16:58:52 +0100

    Predicted why an error will occur when the programm is run

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> write your prediction here. /* Error will occur when this program is run because a variable already declared inside the function parameter , was redeclared inside the function body. Also the console.log function is calling the variable decimalNumber which is not defined in the global scope. */
 
 // Try playing computer with the example to work out what is going on
 
