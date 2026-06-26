1e18211e3350342f1b48e95c5c97cecaf966ab1c
Tobias <amaechinatobias@gmail.com>
2026-06-26 18:41:36 +0100

    commented out the original code to aid re-writing a new one

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,11 +1,11 @@
 // Predict and explain first...
 //  =============> write your prediction here // will print undefined.
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
 
 // =============> write your explanation here.// The code will print undefined because the function sum does not return any value. Instead, it has a return statement with no value, because the semicolon ended the return statement  which means it will return undefined by default. The second line of the function, a + b, is never executed because the return statement ends the function execution before it can be reached.
 
