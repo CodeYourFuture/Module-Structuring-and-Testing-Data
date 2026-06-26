2b580830036d0f07d43ce5ea36444482cf02c90a
Arthur <>
2026-06-26 15:18:00 +0100

    Go back to original sprint 2

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,20 +1,7 @@
-function repeatStr(str, count) {
+function repeatStr() {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  let  repeatedStr= "";
-  
-  for(i=0; i<count; i++){
-   repeatedStr= repeatedStr + str; 
-  };
-  return repeatedStr;}
-
-
-
+  return "hellohellohello";
+}
 
 module.exports = repeatStr;
-
-
-
-
-
-
