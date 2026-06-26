14b44dc8b5085d601a8b39f0ecffc1ed3cd55c3e
Tobias <amaechinatobias@gmail.com>
2026-06-26 19:04:40 +0100

    Explained why the output 3 for the three different Arguements

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -16,7 +16,7 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 // Now run the code and compare the output to your prediction
 // =============> write the output here// The out put is 3 for the three console.log calls as predicted 
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> write your explanation here. /* The reason is because the function getLastDigit is not using the argument passed to it, instead it is using the global variable num which is set to 103. Therefore, the last digit of 103 is 3 and that is what will be printed for all three console.log statements.*/
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
