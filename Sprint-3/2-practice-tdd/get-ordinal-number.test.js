7ff81d8125061c8cb6bf04697f4315379725b0a0
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-27 14:09:30 +0200

    ordinal number test

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,38 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+
+//fixed and completed version
+
+const getOrdinalNumber = require("./get-ordinal-number");
+
+// st cases (except 11)
+test("should return 'st' for numbers ending in 1 except 11", () => {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(31)).toEqual("31st");
+});
+
+// nd cases (except 12)
+test("should return 'nd' for numbers ending in 2 except 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(32)).toEqual("32nd");
+});
+
+// rd cases (except 13)
+test("should return 'rd' for numbers ending in 3 except 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(33)).toEqual("33rd");
+});
+
+// th cases (everything else)
+test("should return 'th' for numbers ending in 4-9 and special teens (11-13)", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(14)).toEqual("14th");
+  expect(getOrdinalNumber(100)).toEqual("100th");
+});
