41d7543ef667500c0e27ce2f40a96e58855c85d7
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-06-27 13:49:40 +0100

    variable declearation

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,13 @@
 let count = 0;
 
 count = count + 1;
+console.log(count);
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//Line 3 is updating the value of the variable `count`.
+//The `=` operator is an assignment operator,
+//which means it takes the value on the right side (in this case, `count + 1`,
+//which evaluates to 1) and assigns it to the variable on the left side (`count`).
+//So after this line executes, the value of `count` will be updated from 0 to 1.
