a1026246ac74132bb308021b947dea4a755c705c
Dipa Sarker <dipasarker696@gmail.com>
2026-06-27 16:15:40 +0100

    run the program several times to observe the results

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,8 +2,20 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+console.log(num);
 
 // In this exercise, you will need to work out what num represents?
+// num stores the result of the expression after calculating it.
 // Try breaking down the expression and using documentation to explain what it means
-// It will help to think about the order in which expressions are evaluated
-// Try logging the value of num and running the program several times to build an idea of what the program is doing
+// There is a range of numbers from 1 to 100. In the expression, 
+// (maximum - minimum + 1) calculates the size of the possible integers, which is 100. 
+// The Math.random() function generates a random decimal number in the range 0 ≤ x < 1.
+// This random decimal is then multiplied by the range size to scale it to a
+// value between 0 and 100. The result is a floating-point number in this interval.
+// Next, Math.floor() is used to round the number down to the largest integer less than
+// or equal to the calculated value, producing an integer between 0 and 99.
+// Finally, by adding minimum, the range is shifted from 0–99 to 1–100, giving a random
+// integer within the required range.
+// I have run 5 times the program and this produces different values each time because
+// Math.random() generates a new random decimal number every time. 
+// Since the final value of num depends on this function, the result changes on each execution.
