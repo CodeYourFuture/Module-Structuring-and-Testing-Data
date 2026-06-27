9cfc4d249c9f1ed6e39c21c98f3349a4cf075a69
Dipa Sarker <dipasarker696@gmail.com>
2026-06-27 16:04:52 +0100

    add explanation for line 3

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -3,4 +3,6 @@ let count = 0;
 count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// The = sign means 'assignment' operator
+// In line 3, at first the calculation will be done at R.H.S, 
+// with the initial count value then the calculated value will be stored at the L.H.S count 
