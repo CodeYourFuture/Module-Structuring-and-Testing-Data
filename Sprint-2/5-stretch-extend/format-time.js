87642d5618f306f4f7302049de5fe080b856df28
shafiekwalker7861 <westerncapetowndesigner@gmail.com>
2026-06-27 17:15:13 +0200

    Fix 12-hour clock edge cases and add tests

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -4,47 +4,53 @@
 
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
+  const minutes = time.slice(3);
 
   if (hours === 0) {
-    return `12:00 am`;
+    return `12:${minutes} am`;
   }
 
   if (hours === 12) {
-    return `12:00 pm`;
+    return `12:${minutes} pm`;
   }
 
   if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+    return `${hours - 12}:${minutes} pm`;
   }
 
-  return `${time} am`;
+    return `${hours}:${minutes} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+const currentOutput = formatAs12HourClock("00:34");
+const targetOutput = "12:34 am";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput2 = formatAs12HourClock("23:00");
-const targetOutput2 = "11:00 pm";
+const currentOutput2 = formatAs12HourClock("12:34");
+const targetOutput2 = "12:34 pm";
 console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
 
 
-const currentOutput3 = formatAs12HourClock("12:00");
-const targetOutput3 = "12:00 pm";
+const currentOutput3 = formatAs12HourClock("01:01");
+const targetOutput3 = "1:01 am";
 console.assert(
   currentOutput3 === targetOutput3,
   `current output: ${currentOutput3}, target output: ${targetOutput3}`
 );
 
-const currentOutput4 = formatAs12HourClock("00:00");
-const targetOutput4 = "12:00 am";
+const currentOutput4 = formatAs12HourClock("13:01");
+const targetOutput4 = "1:01 pm";
 console.assert(
   currentOutput4 === targetOutput4,
   `current output: ${currentOutput4}, target output: ${targetOutput4}`
 );
+
+/*console.log (formatAs12HourClock("00:34"));
+console.log (formatAs12HourClock("12:34"));
+console.log (formatAs12HourClock("01:01"));
+console.log (formatAs12HourClock("13:01"));*/
