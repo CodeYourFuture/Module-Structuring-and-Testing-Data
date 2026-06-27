1b3966011c5684484497aa9280a3091fadd425b3
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-06-27 14:00:25 +0100

    identification of wrong variable

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
 const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53";
+
+// The error in the code is that the variable `12HourClockTime` starts with a number, 
+// which is not allowed in JavaScript variable names. 
+// Variable names must start with a letter, underscore (_), or dollar sign ($).
+// To fix this error, we can rename the variable to start with a letter, 
+// such as `twelveHourClockTime` or `hour12ClockTime`.
+const twelveHourClockTime = "8:53pm";
+const hour24ClockTime = "20:53";
+// you can ether put the number at thr end of the variable name or spell it out in words, 
+// but you cannot start a variable name with a number.
