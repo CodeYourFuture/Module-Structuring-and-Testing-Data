6842ec5dd132c985472f2d5e16be4c5effc60fc0
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-06-27 13:58:41 +0100

    correcting error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,11 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+// The error in the code is that the variable `cityOfBirth` is being used before it is declared and assigned a value.
+// In JavaScript, variables declared with `const` (or `let`) are not hoisted in the same way as `var`,
+// meaning they cannot be accessed before their declaration.
+// To fix this error, we need to declare and assign a value to `cityOfBirth` before using it in the `console.log` statement.
+// Here's the corrected code:
+//const cityOfBirth = "Bolton";
+//console.log(`I was born in ${cityOfBirth}`);
