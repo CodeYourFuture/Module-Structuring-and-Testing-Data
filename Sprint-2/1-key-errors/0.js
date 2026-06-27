376a6b01cc4999bf60a0b87527c15cde540554dd
JorvanW <your.email@example.com>
2026-06-27 11:47:24 +0100

    added answers to 0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The fuction intends to capitalize the first letter of the input string
+//  from th left starting with the first character (str[0]).
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -9,5 +10,7 @@ function capitalise(str) {
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> str has already been declared as a parameter function, so it cannot be redeclared within the function body. This will cause a syntax error. To fix this, we should use a different variable name for the new string we are creating.
+// =============> function capitalise(str) {
+//  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return str;
