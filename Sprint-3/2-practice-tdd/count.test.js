ac872ce422aebec9f99a6bd6c670658d341a4dbb
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-27 14:03:24 +0200

    count.test

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -23,4 +23,26 @@ test("should count multiple occurrences of a character", () => {
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
 
-//
+//completed version
+
+const countChar = require("./countChar");
+
+// Scenario: Multiple Occurrences
+test("should count multiple occurrences of a character", () => {
+  const str = "aaaaa";
+  const char = "a";
+
+  const count = countChar(str, char);
+
+  expect(count).toEqual(5);
+});
+
+// Scenario: No Occurrences
+test("should return 0 when character is not found", () => {
+  const str = "hello";
+  const char = "z";
+
+  const count = countChar(str, char);
+
+  expect(count).toEqual(0);
+});
