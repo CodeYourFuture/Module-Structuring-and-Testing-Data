b068348912a283d592496fc479216dae7a1d91fb
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-06-27 13:40:35 +0100

    for name arrangment

---
+++ name.js
@@ -0,0 +1,7 @@
+let firstName = "Creola";
+let middleName = "Katherine";
+let lastName = "Johnson";
+
+let initials = ``;
+initials = `${firstName[0]}${middleName[0]}${lastName[0]}`;
+console.log(initials);
