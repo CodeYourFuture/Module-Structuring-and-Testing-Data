8a5a9f0c687f0c8f231cabaf52beb21402ba38f6
Yonatan Teklemariam <jante26uk@gmail.com>
2026-06-27 15:42:39 +0100

    fix; revert to original

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -5,10 +5,13 @@ let testName = "Jerry";
 const greeting = "hello";
 
 function sayHello(greeting, name) {
-  // const greetingStr = greeting + ", " + name + "!"; this line will also be removed because the we're trying to return something else, not the greetingStr and apparently it is a redundant code
+  const greetingStr = greeting + ", " + name + "!";
   return `${greeting}, ${name}!`;
-  // console.log(greetingStr); this line will obviously be omitted as it is unreachable code because it is written after return. In fact, anything after return is not reachable and should be get rid of
+  console.log(greetingStr);
 }
 
+testName = "Aman";
+
 const greetingMessage = sayHello(greeting, testName);
-console.log(greetingMessage);
+
+console.log(greetingMessage); // 'hello, Aman!'
