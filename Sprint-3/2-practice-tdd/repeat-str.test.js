2d01d60d4c56e2ecb6236d1795ffb3f3d90854d8
Yonatan Teklemariam <jante26uk@gmail.com>
2026-06-27 12:50:26 +0100

    written jest tests for all the three functions and output tests as expected

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -11,22 +11,39 @@ const repeatStr = require("./repeat-str");
 
 test("should repeat the string count times", () => {
   const str = "hello";
-  const count = 3;
+  const count = -5;
   const repeatedStr = repeatStr(str, count);
   expect(repeatedStr).toEqual("hellohellohello");
 });
 
 // Case: handle count of 1:
+test("should repeat the string count of 1", () => {
+  const str = "fella";
+  const count = 1;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("fella");
+});
+
 // Given a target string `str` and a `count` equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
 
 // Case: Handle count of 0:
+test("should repeat the string count of 0", () => {
+  const str = "fella";
+  const count = 0;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("");
+});
+
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
 
 // Case: Handle negative count:
+test(`should throw an error for negative count`, () => {
+  expect(() => repeatedStr("fella", -2)).toThrowError();
+});
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
