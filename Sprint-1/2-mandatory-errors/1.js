a0b416f55e6ed2873f1d9173ea2350378d361158
Dipa Sarker <dipasarker696@gmail.com>
2026-06-27 16:20:07 +0100

    fixed error in the problem

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
-// trying to create an age variable and then reassign the value by 1
 
 const age = 33;
 age = age + 1;
+
+// There is a constant variable age, which is fixed and it's value cannot be reassigned.
+// If we want to change the value of age, we have to use let instead of const.
