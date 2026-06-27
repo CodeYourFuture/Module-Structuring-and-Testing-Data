b54e16e14d98d052453d1c389d04504d414c88d9
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-27 14:00:49 +0200

    count.js

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,21 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  return 5;
+}
+
+module.exports = countChar;
+
+// fixed version of the function
+
+function countChar(stringOfCharacters, findCharacter) {
+  let count = 0;
+
+  for (let char of stringOfCharacters) {
+    if (char === findCharacter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countChar;
