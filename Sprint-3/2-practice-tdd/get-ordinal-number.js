8f2c7fab427b0368d7647514c00c182691b6fa8c
Liyema Mfengwana <67449042+solohuns@users.noreply.github.com>
2026-06-27 14:05:03 +0200

    ordinal-numbers

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -3,3 +3,22 @@ function getOrdinalNumber(num) {
 }
 
 module.exports = getOrdinalNumber;
+
+//correct version of the function
+
+function getOrdinalNumber(num) {
+  const lastDigit = num % 10;
+  const lastTwoDigits = num % 100;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return num + "th";
+  }
+
+  if (lastDigit === 1) return num + "st";
+  if (lastDigit === 2) return num + "nd";
+  if (lastDigit === 3) return num + "rd";
+
+  return num + "th";
+}
+
+module.exports = getOrdinalNumber;
