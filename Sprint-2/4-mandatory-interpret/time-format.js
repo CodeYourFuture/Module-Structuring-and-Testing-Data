4fd9db65e4ec6ab474d592175148cb9b56aae57d
Asanda <asandagwala@gmail.com>
2026-06-27 13:59:03 +0200

    time-format file update

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -15,24 +15,34 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(61))
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// pad will be called 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// The value assigned to num when pad is called for the first 
+// time is 0 (the value of totalHours). 
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// The return value of pad when it is called for the first time is "00" 
+// because 0 is padded with a leading zero.
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+//The value assigned to num when pad is called for the 
+// last time is 1 (the value of remainingSeconds). 
+// This is because the return statement is executed from left to right, so the order of pad calls is: totalHours, remainingMinutes, and finally remainingSeconds.
+
+
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+//The return value of pad when it is called for the last time is "01" because 1 is padded with a leading zero.
+
+// The final output of formatTimeDisplay(61) will be "00:01:01".
