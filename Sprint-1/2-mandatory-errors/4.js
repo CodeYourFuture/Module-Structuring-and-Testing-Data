aa03f47e23666f04b7d9eb7b6754cbd634b89759
ChinweP <exponentialresults1973@gmail.com>
2026-06-28 18:20:31 +0100

    Explain why error occurred

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
 const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53";
+
+//JavaScript throws a SyntaxError: Invalid or unexpected token because it does not allow variables to start with a number.
+// The variable 12HourClockTime starts with a number 12 making the entire identifier invalid therefore JavaScript could not interpret the variable as valid syntax.
