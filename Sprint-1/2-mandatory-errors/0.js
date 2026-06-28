f74950f83a6f88894b11532c875239b16fd24e6d
ChinweP <exponentialresults1973@gmail.com>
2026-06-28 14:01:20 +0100

    Comment out non-code instruction lines to prevent runtime execution

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,4 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//We can comment it out to stop the computer from executing it by using either // for single-line comment or /* */ for multi-line comment.
