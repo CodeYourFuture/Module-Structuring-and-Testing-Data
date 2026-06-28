41b59bdae63fa0ec9bc11e70ce90148b3ba305cd
ChinweP <exponentialresults1973@gmail.com>
2026-06-28 16:47:56 +0100

    Explain TypeError caused by reassigning a constant variable.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,7 @@
 
 const age = 33;
 age = age + 1;
+
+/*This threw the error TypeError: Assignment to constant variable because the line
+ age = age + 1 attempts to change the value of age after a constant variable is created.
+ Constant variables cannot be reassigned after initial value is set.*/
