e97765db1dd9636785ae6ec2d742d796c9ef847a
Tobias <amaechinatobias@gmail.com>
2026-06-29 19:59:31 +0100

    Wrote test for the upper limit acute angle

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -41,3 +41,5 @@ const acute = getAngleType(1);// Test just above zero
 assertEquals(acute, "Acute angle");
 const acute2 = getAngleType(45); // Test acute angle just at the middle
 assertEquals(acute2, "Acute angle");
+const acute3 = getAngleType(89.9); // Test just above zero
+assertEquals(acute3, "Acute angle");
