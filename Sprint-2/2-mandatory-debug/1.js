11253932d6cc03e3e2ed9a870999bacd1aea3269
d-odumosu <dt.odumosu@gmail.com>
2026-06-29 21:18:05 +0100

    completed 2-mandatory-debug/1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// we have added a semicolon between the return statement and what we want to return, the semicolon signifies the end
+// of a statement, so a + b will not be returned
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+//
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
+// =============> write your explanation here
+// I have removed the semicolon right after the return statement
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
