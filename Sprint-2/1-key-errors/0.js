418195fa6a5cf67865d3df4e215738d66ff79d76
d-odumosu <dt.odumosu@gmail.com>
2026-06-29 18:54:27 +0100

    answered all questions in 0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// the function will throw a syntax error because we are redeclaring 'str'. we can reassign - without using the let keyword.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+// i will remove the let keyword, which is only used for declaration and assignment, when redeclaring we do not use the let keyword
 // =============> write your new code here
+function capitalise(str){
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str
+}
