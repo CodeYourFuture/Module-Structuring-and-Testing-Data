9e0a4cf13cbe8b36a062e1bb1f4aef91e3c97fcf
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-29 11:35:31 +0100

    Complete countChar function to make all jest tests pass in 2-practice-tdd/count.js

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,10 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  if (stringOfCharacters.length === 0 || findCharacter.length === 0) return 0;
+  let count = 0;
+  for (const char of stringOfCharacters) {
+    if (char === findCharacter) count++;
+  }
+  return count;
 }
 
 module.exports = countChar;
