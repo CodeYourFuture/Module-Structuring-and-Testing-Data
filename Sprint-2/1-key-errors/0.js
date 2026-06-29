79b23419270e32066e67f80e00c7e7d33c6c663c
Tomislav Dukez <tomdu3@ymail.com>
2026-06-29 15:41:22 +0100

    add key-error task 0

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,21 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+// the error message details: SyntaxError: Identifier 'str' has already been declared
+// In function definition the variable str is already declared, therefore in the function body
+// a new declaration of the same variable is redundant.
+// when a function call is made, variable str gets the value from the argument of the call.
+// In order to fix the issue, we can easily just return the value as it is given in line 8.
 // =============> write your new code here
+
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
+
+console.log(capitalise("hello, world! tom's here."));
