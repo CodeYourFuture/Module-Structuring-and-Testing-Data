3ba86a11d39959563cd052e5da60d8c054324de6
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-06-29 19:43:45 +0100

    correction for multiplication function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,7 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// This code will actually run without throwing a red crash error, 
+// but it will print something very strange. the result of multiplying 10 and 32 is undefined.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +9,14 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// console.log() inside the function: Inside the multiply function, you used console.log(a * b). 
+// This instantly prints 320 to the screen, but it does not give that number back to the code that called it.
+//The Missing return: In JavaScript, if a function does not explicitly use the word return, it automatically hands back undefined.
+//The undefined string: Because multiply(10, 32) returns undefined, your outer console.log plugs undefined into the sentence, resulting in: "The result of multiplying 10 and 32 is undefined".
 
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+// Finally, correct the code to fix the problem  
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(10, 32));
