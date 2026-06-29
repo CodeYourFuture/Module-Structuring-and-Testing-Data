bf5c3320dad52e8b6d4cb5a2e8ab1fc23d1c27df
JorvanW <your.email@example.com>
2026-06-29 10:58:56 +0100

    added answer to 0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> Code is attempting to multiple a & b but there is no return value
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,11 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> because there is no return value, the outcome stays as undefined while giving the answer
+// to fix this, change "console.log" in the second row to "return"
+//
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+function multiply(a, b) {
+  return (a * b);
+}
