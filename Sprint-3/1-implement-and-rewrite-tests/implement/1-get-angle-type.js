4bc146eaa45050573ff29d2fb06452de45de1a9f
dona-shehu <shehu.donna@gmail.com>
2026-06-30 20:59:33 +0100

    implement, 1-get-angle-type.

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -16,6 +16,22 @@
 
 function getAngleType(angle) {
   // TODO: Implement this function
+  if(  angle > 0 && angle < 90){
+    return "Acute angle";
+  }
+  else if  (angle === 90){
+    return  "Right angle";
+  }
+  else if ( angle > 90 && angle < 180){
+    return "Obtuse angle";
+  }
+  else if(angle === 180){
+    return "Straight angle";
+  }
+  else if( angle > 180  && angle < 360){
+    return "Reflex angle";
+  }
+  else return "Invalid angle";
 }
 
 // The line below allows us to load the getAngleType function into tests in other files.
@@ -33,5 +49,32 @@ function assertEquals(actualOutput, targetOutput) {
 
 // TODO: Write tests to cover all cases, including boundary and invalid cases.
 // Example: Identify Right Angles
+
+const acute = getAngleType(45);
+assertEquals(acute, "Acute angle");
+
+
 const right = getAngleType(90);
 assertEquals(right, "Right angle");
+
+
+const obtuse = getAngleType(120);
+assertEquals(obtuse, "Obtuse angle");
+
+
+const straight = getAngleType(180);
+assertEquals(straight, "Straight angle");
+
+
+const reflex = getAngleType(200);
+assertEquals(reflex, "Reflex angle");
+
+
+const  invalid0 = getAngleType(0);
+assertEquals(invalid0, "Invalid angle");
+
+const invalid360 = getAngleType(360);
+assertEquals(invalid360, "Invalid angle");
+
+
+
