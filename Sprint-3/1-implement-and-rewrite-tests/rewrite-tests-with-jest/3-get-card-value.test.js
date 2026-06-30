d469d9423d139903226425f220ff71c112819260
Dipa Sarker <dipasarker696@gmail.com>
2026-06-30 17:13:15 +0100

    rewrite get-card-value with jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -1,20 +1,50 @@
-// This statement loads the getCardValue function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
 const getCardValue = require("../implement/3-get-card-value");
-
-// TODO: Write tests in Jest syntax to cover all possible outcomes.
-
-// Case 1: Ace (A)
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
-
-// Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-//   Face Cards (J, Q, K)
-//   Invalid Cards
-
-// To learn how to test whether a function throws an error as expected in Jest,
-// please refer to the Jest documentation:
-// https://jestjs.io/docs/expect#tothrowerror
-
+describe ("Number Cards" , () => {
+ test(`Should return 5 for 5♥`, () => {
+  expect(getCardValue("5♥")).toEqual(5);
+ });
+ test(`Should return 10 for 10♥`, () => {
+  expect(getCardValue("10♥")).toEqual(10);
+ });
+});
+describe ("Face Cards" , () => {
+ test(`Should return 10 for J♣`, () => {
+  expect(getCardValue("J♣")).toEqual(10);
+ });
+ test(`Should return 10 for Q♦`, () => {
+  expect(getCardValue("Q♦")).toEqual(10);
+ });
+ test(`Should return 10 for K♦`, () => {
+  expect(getCardValue("K♦")).toEqual(10);
+ });
+});
+ describe("Invalid Cards", () => {
+  test("throws error for invalid card", () => {
+   expect(() => {
+    getCardValue("10");
+  }).toThrow();
+ });
+  test("throws error for invalid card", () => {
+   expect(() => {
+    getCardValue("A");
+  }).toThrow();
+ });
+  test("throws error for invalid card", () => {
+   expect(() => {
+    getCardValue("10x");
+  }).toThrow();
+ });
+  test("throws error for invalid card", () => {
+   expect(() => {
+    getCardValue("JK");
+  }).toThrow();
+ });
+  test("throws error for invalid card", () => {
+   expect(() => {
+    getCardValue("Qxx");
+  }).toThrow();
+ });
+});
