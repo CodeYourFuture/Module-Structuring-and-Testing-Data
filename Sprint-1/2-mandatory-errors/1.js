d43c9faa264711ce41895d3cf464f892db752913
vmoratti <vmoratti@gmail.com>
2026-06-30 21:45:16 +0100

    Delete Sprint-1/2-mandatory-errors/1.js

--- Sprint-1/2-mandatory-errors/1.js
+++
@@ -1,5 +0,0 @@
-// trying to create an age variable and then reassign the value by 1
-
-let age = 33; // by changing the way we declare the variable from "const" to "let" we allow
-//  reassignment of the value of the variable
-age = age + 1;
