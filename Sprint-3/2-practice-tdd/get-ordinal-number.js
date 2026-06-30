a2a025bed4a1f0f7cfbfa4bc97a4254dffdb02d8
Martin Mwaka <martinmwaka@hotmail.com>
2026-06-30 10:05:51 +0100

    Complete function in get-ordinal-number.js

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,17 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  let strNum;
+  const lastNum = num % 10;
+  const lastTwoNums = num % 100;
+  if (lastNum === 1 && lastTwoNums !== 11) {
+    strNum = `${String(num)}st`;
+  } else if (lastNum === 2 && lastTwoNums !== 12) {
+    strNum = `${String(num)}nd`;
+  } else if (lastNum === 3 && lastTwoNums !== 13) {
+    strNum = `${String(num)}rd`;
+  } else {
+    strNum = `${String(num)}th`;
+  }
+  return strNum;
 }
 
 module.exports = getOrdinalNumber;
