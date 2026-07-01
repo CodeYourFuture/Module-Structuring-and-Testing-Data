730a4ddad854714226c914236d2fd55f84f0b073
Tobias <amaechinatobias@gmail.com>
2026-07-01 19:41:36 +0100

    wrote a test for proper fraction with denominator -ve and numerator +ve

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -40,3 +40,5 @@ assertEquals(isProperFraction(0, 5), true);
 assertEquals(isProperFraction(5, 0), false);
 // Write a test for -1/2, which is a proper fraction
 assertEquals(isProperFraction(-1, 2), true);
+// Write a test for 1/-2, which is a proper fraction
+assertEquals(isProperFraction(1, -2), true);
