c1afcc21c1a104229ae332204c323b4be23d3b5c
Dipa Sarker <dipasarker696@gmail.com>
2026-07-01 21:43:37 +0100

    implemented functions and tests for repeated string

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -1,13 +1,4 @@
-// Implement a function repeatStr
 const repeatStr = require("./repeat-str");
-// Given a target string `str` and a positive integer `count`,
-// When the repeatStr function is called with these inputs,
-// Then it should:
-
-// Case: handle multiple repetitions:
-// Given a target string `str` and a positive integer `count` greater than 1,
-// When the repeatStr function is called with these inputs,
-// Then it should return a string that contains the original `str` repeated `count` times.
 
 test("should repeat the string count times", () => {
   const str = "hello";
@@ -16,22 +7,12 @@ test("should repeat the string count times", () => {
   expect(repeatedStr).toEqual("hellohellohello");
 });
 
-// Case: handle count of 1:
-// Given a target string `str` and a `count` equal to 1,
-// When the repeatStr function is called with these inputs,
-// Then it should return the original `str` without repetition.
-
 test("should return the original string without repetition", () => {
   const str = "hello";
   const count = 1;
   const repeatedStr = repeatStr(str, count);
   expect(repeatedStr).toEqual("hello");
 });
-
-// Case: Handle count of 0:
-// Given a target string `str` and a `count` equal to 0,
-// When the repeatStr function is called with these inputs,
-// Then it should return an empty string.
 
 test("should return empty string", () => {
   const str = "hello";
@@ -40,14 +21,11 @@ test("should return empty string", () => {
   expect(repeatedStr).toEqual("");
 });
 
-// Case: Handle negative count:
-// Given a target string `str` and a negative integer `count`,
-// When the repeatStr function is called with these inputs,
-// Then it should throw an error, as negative counts are not valid.
-
 test("should throw an error for negative counts", () => {
   const str = "hello";
   const count = -1;
-  
-  expect(() => {repeatStr(str, count)}).toThrow("Negative counts are invalid");
+
+  expect(() => {
+    repeatStr(str, count);
+  }).toThrow("Negative counts are invalid");
 });
