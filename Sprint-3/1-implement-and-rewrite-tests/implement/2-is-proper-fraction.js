ce304926c4f293077c5469499b04a7258c9243c0
Tobias <amaechinatobias@gmail.com>
2026-07-01 19:29:00 +0100

    Implement the statement to execute the logic

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -12,6 +12,7 @@
 
 function isProperFraction(numerator, denominator) {
   // TODO: Implement this function
+  return Math.abs(numerator) < Math.abs(denominator) && denominator !== 0;
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
