29a304ec90f5f2cf2138d756214afdc1c0536709
Abid Akhtar <abidakhtar2023@gmail.com>
2026-07-01 23:05:30 +0100

    Variable declaration explained

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+
+// In line 3 JS takes the current value of count which is 0, adds 1 to it and then stores the result back into count variable on the left.
