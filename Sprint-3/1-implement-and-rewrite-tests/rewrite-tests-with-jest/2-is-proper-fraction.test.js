5c305646f32a8809f754f71d1dacc640c52fd86c
Tomislav Dukez <tomdu3@ymail.com>
2026-07-01 17:11:31 +0100

    test: add jest tests for isProperFraction

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,4 +7,24 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 // Special case: numerator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
+  expect(isProperFraction(2, 0)).toEqual(false);
+  
+});
+
+// Case 2: Denominator is greater than numerator
+test(`should return true when denominator is greater than numerator`, () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+  expect(isProperFraction(2, 4)).toEqual(true);
+  expect(isProperFraction(3, 6)).toEqual(true);
+  expect(isProperFraction(4, 8)).toEqual(true);
+  expect(isProperFraction(5, 10)).toEqual(true);
+});
+
+// Case 3: Denominator is less than numerator
+test(`should return false when denominator is less than numerator`, () => {
+  expect(isProperFraction(2, 1)).toEqual(false);
+  expect(isProperFraction(4, 2)).toEqual(false);
+  expect(isProperFraction(6, 3)).toEqual(false);
+  expect(isProperFraction(8, 4)).toEqual(false);
+  expect(isProperFraction(10, 5)).toEqual(false);
 });
