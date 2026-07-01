08e2b27675972a9d825d577d4809cb0083209e16
Dipa Sarker <dipasarker696@gmail.com>
2026-07-01 21:35:11 +0100

    implemented functions for get-ordinal-number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -14,23 +14,6 @@ function getOrdinalNumber(num) {
     return `${num}` + "th";
   }
 }
-console.log(getOrdinalNumber(1));
-console.log(getOrdinalNumber(21));
-console.log(getOrdinalNumber(131));
-console.log(getOrdinalNumber(2));
-console.log(getOrdinalNumber(22));
-console.log(getOrdinalNumber(432));
-console.log(getOrdinalNumber(3));
-console.log(getOrdinalNumber(33));
-console.log(getOrdinalNumber(303));
-console.log(getOrdinalNumber(4));
-console.log(getOrdinalNumber(66));
-console.log(getOrdinalNumber(100));
-console.log(getOrdinalNumber(11));
-console.log(getOrdinalNumber(12));
-console.log(getOrdinalNumber(13));
-console.log(getOrdinalNumber(111));
-console.log(getOrdinalNumber(212));
-console.log(getOrdinalNumber(313));
+
 
 module.exports = getOrdinalNumber;
