d38e418896a35acfec256e965f892e78a1d2477c
Dipa Sarker <dipasarker696@gmail.com>
2026-07-01 21:39:49 +0100

    implemented functions and tests for repeated string

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,13 +1,10 @@
 function repeatStr(str, count) {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-
   if (count < 0) {
     throw Error("Negative counts are invalid");
   }
-  let result = ""; 
+  let result = "";
   for (let i = 0; i < count; i++) {
-    result += str; //result = result+str
+    result += str;
   }
   return result;
 }
