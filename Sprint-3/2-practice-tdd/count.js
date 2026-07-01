0c39f01fc4c7e590837de7e45906c7dfe82f1807
Dipa Sarker <dipasarker696@gmail.com>
2026-07-01 02:35:39 +0100

    implemented countChar func and tests scenarios

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,16 @@
-function countChar(stringOfCharacters, findCharacter) {
-  return 5
+function countChar(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
 }
+const str = "aaaaa";
+const char = "a";
+const count = countChar(str, char);
+console.log(countChar("aaaaa", "a"));
+console.log(countChar("aaaaa", "b"));
 
 module.exports = countChar;
