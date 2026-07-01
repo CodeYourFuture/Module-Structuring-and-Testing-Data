7b66caea505597eba564a38f05c3fa1699301186
Tobias <amaechinatobias@gmail.com>
2026-07-01 19:36:05 +0100

    Wrote a test for Improper fraction with denomintor having zero value

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -36,3 +36,5 @@ assertEquals(isProperFraction(1, 2), true);
 assertEquals(isProperFraction(2, 1), false);
 // Write a test for 0/5, which is a proper fraction
 assertEquals(isProperFraction(0, 5), true);
+// Write a test for 5/0, which is not a proper fraction (denominator cannot be zero)
+assertEquals(isProperFraction(5, 0), false);
