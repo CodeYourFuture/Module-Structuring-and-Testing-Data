390b39059a978fb6e279c928551c93741c6e273a
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-07-02 09:45:43 +0100

    Rewrite tests for getCardValue using Jest
    Updated test cases for getCardValue function to use Jest syntax, covering all card types including Aces, number cards, face cards, and invalid cards.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -1,20 +1,32 @@
-// This statement loads the getCardValue function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
-const getCardValue = require("../implement/3-get-card-value");
-
-// TODO: Write tests in Jest syntax to cover all possible outcomes.
 
 // Case 1: Ace (A)
-test(`Should return 11 when given an ace card`, () => {
+test("should return 11 when given an ace card", () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
 
-// Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-//   Face Cards (J, Q, K)
-//   Invalid Cards
+// Case 2: Number cards (2-10)
+test("should return the numeric value for number cards", () => {
+  expect(getCardValue("2♥")).toEqual(2);
+  expect(getCardValue("5♣")).toEqual(5);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("10♠")).toEqual(10);
+});
 
-// To learn how to test whether a function throws an error as expected in Jest,
-// please refer to the Jest documentation:
-// https://jestjs.io/docs/expect#tothrowerror
+// Case 3: Face cards (J, Q, K)
+test("should return 10 for face cards", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♦")).toEqual(10);
+});
+
+// Case 4: Invalid cards
+test("should throw an error for invalid cards", () => {
+  expect(() => getCardValue("invalid")).toThrow();
+  expect(() => getCardValue("1♠")).toThrow();
+  expect(() => getCardValue("11♠")).toThrow();
+  expect(() => getCardValue("A")).toThrow();
+  expect(() => getCardValue("♠")).toThrow();
+  expect(() => getCardValue("KH")).toThrow();
+  expect(() => getCardValue("")).toThrow();
+});
 
