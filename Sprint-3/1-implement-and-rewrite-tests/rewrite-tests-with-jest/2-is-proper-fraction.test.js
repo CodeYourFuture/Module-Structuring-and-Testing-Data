4ab6f5cf804e139edae82d7d2fc7b11437b4f11e
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-07-02 09:44:02 +0100

    Implement Jest tests for isProperFraction function
    Added comprehensive Jest tests for the isProperFraction function, covering special cases, positive and negative proper and improper fractions.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -1,10 +1,36 @@
-// This statement loads the isProperFraction function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
-const isProperFraction = require("../implement/2-is-proper-fraction");
-
-// TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
+// Special case: denominator is zero
+test("should return false when denominator is zero", () => {
+  expect(isProperFraction(1, 0)).toEqual(false);
+});
 
 // Special case: numerator is zero
-test(`should return false when denominator is zero`, () => {
-  expect(isProperFraction(1, 0)).toEqual(false);
+test("should return true when numerator is zero and denominator is not zero", () => {
+  expect(isProperFraction(0, 5)).toEqual(true);
+  expect(isProperFraction(0, -5)).toEqual(true);
+});
+
+// Positive proper fractions
+test("should return true for positive proper fractions", () => {
+  expect(isProperFraction(1, 2)).toEqual(true);
+  expect(isProperFraction(2, 5)).toEqual(true);
+});
+
+// Positive improper fractions
+test("should return false for positive improper fractions", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+  expect(isProperFraction(2, 2)).toEqual(false);
+});
+
+// Negative proper fractions
+test("should return true for negative proper fractions", () => {
+  expect(isProperFraction(-1, 2)).toEqual(true);
+  expect(isProperFraction(1, -2)).toEqual(true);
+  expect(isProperFraction(-2, -5)).toEqual(true);
+});
+
+// Negative improper fractions
+test("should return false for negative improper fractions", () => {
+  expect(isProperFraction(-5, 2)).toEqual(false);
+  expect(isProperFraction(5, -2)).toEqual(false);
+  expect(isProperFraction(-5, -2)).toEqual(false);
 });
