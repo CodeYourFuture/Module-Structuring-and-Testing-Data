83bf403e8a5e1eff139f9844385f2ed67a2d3712
Tomislav Dukez <tomdu3@ymail.com>
2026-07-03 09:37:06 +0100

    fix remove dead code

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,13 +1,11 @@
 // Find the instances of unreachable and redundant code - remove them!
 // The sayHello function should continue to work for any reasonable input it's given.
 
-let testName; // = "Jerry"; -> we can initialize the variable without giving it a value because the testName is being reassigned later in the code. The value "Jerry" never appears in a function call.
+let testName;
 const greeting = "hello";
 
 function sayHello(greeting, name) {
-  // const greetingStr = greeting + ", " + name + "!";  -> this line is not necessary because the returned string accomplishes the same thing, and the greetingStr is never being used. So it is redundant code and can be removed.
   return `${greeting}, ${name}!`;
-  // console.log(greetingStr);  -> this line is never being executed because it comes after the return statement, so it never runs and can be removed.
 }
 
 testName = "Aman";
--- Sprint-3/3-dead-code/exercise-2.js
+++ Sprint-3/3-dead-code/exercise-2.js
@@ -2,13 +2,8 @@
 // The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
 
 const pets = ["parrot", "hamster", "horse", "dog", "hamster", "cat", "hamster"];
-// const capitalisedPets = pets.map((pet) => pet.toUpperCase());  -> this line is not necessary because the same functionality is implemented inside the function countAndCapitalisePets.
-// Also, the variable capitalisedPets in not being used in the global scope
-const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
 
-// function logPets(petsArr) {
-//   petsArr.forEach((pet) => console.log(pet));
-// }  -> This function is not being used/called anywhere in the program, it is redundant code and should be removed.
+const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
 
 function countAndCapitalisePets(petsArr) {
   const petCount = {};
