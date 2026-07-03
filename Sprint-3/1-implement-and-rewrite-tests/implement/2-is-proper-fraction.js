70485c642fb8b568b5982de3ad75c5f0e060f5a4
Yonatan Teklemariam <jante26uk@gmail.com>
2026-07-03 20:51:42 +0100

    Correct syntax in isProperFraction function
    Fix syntax error in isProperFraction function.

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -24,6 +24,7 @@ numerator = Number(numerator);
 denominator = Number(denominator);
 if (isNaN(numerator) || isNaN(denominator) {
 return false;})
+*/
 // The line below allows us to load the isProperFraction function into tests in other files.
 // This will be useful in the "rewrite tests with jest" step.
 module.exports = isProperFraction;
