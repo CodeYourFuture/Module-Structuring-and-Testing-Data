d38452c78ee3e1a74e538512b28931f9b084351a
abduhasen <abdumussa1363@gmail.com>
2026-07-03 13:41:42 +0100

    fixing code using mentor feed back

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -10,7 +10,8 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here: firstly the process start accepting that it is a function and takes
 //                                             the parameter to hold the place then it go through the function but by the 5th line
-//                                             the function return and the value will come out undefined
+//                                             the function return and the value will come out undefined,which means java script doesn't
+//                                             have step to continue so it is returning with out a value.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here: function sum(a, b) {
 //                                              return a + b;
--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -15,7 +15,7 @@
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 
-function toCapitalizedSnakeCase(str) {
+function toUpperSnakeCase(str) {
   const toCapitalized = str.toUpperCase().replace(/ /g, "_");
   return toCapitalized;
 }
