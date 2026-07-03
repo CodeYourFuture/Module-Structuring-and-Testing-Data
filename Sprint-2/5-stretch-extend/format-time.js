550232a2c131ee2679586b68a1f9a20ca206e019
B3-Bisrat <bisratttt1919@gmail.com>
2026-07-03 22:42:19 +0100

    5-stretch-extend is done.

--- Sprint-2/5-stretch-extend/format-time.js
+++ Sprint-2/5-stretch-extend/format-time.js
@@ -23,3 +23,24 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+const currentOutput3 = formatAs12HourClock("00:00");
+const targetOutput3 = "12:00 am";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("12:00");
+const targetOutput4 = "12:00 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+console.log(formatAs12HourClock("08:00"));
+console.log(formatAs12HourClock("23:00"));
+console.log(formatAs12HourClock("00:00"));
+console.log(formatAs12HourClock("12:00"));
+
+//The function did not handle the edge cases 00:00 and 12:00.
