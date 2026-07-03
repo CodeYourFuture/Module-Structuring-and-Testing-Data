a86278a40567ae524b898390108d7e15999265df
B3-Bisrat <bisratttt1919@gmail.com>
2026-07-03 22:18:43 +0100

    2-mandatory-debug are done.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+// I predict the code will give an error because the sum function does not return a value.
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +9,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The function sum(a, b) has a return statement that does not return the result of a + b.
+// Instead, it returns undefined because the return statement is followed by a semicolon, which ends the statement before the addition operation is executed.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,6 +2,7 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+//The function has no parameter, so it cannot use input values like 42, 105, 806
 
 const num = 103;
 
@@ -15,10 +16,21 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+// The out will be the last digit of 103 for all three console.log statements, because the function getLastDigit() is using the variable num which is set to 103, instead of using the input values passed to it.
 // Explain why the output is the way it is
 // =============> write your explanation here
+//The function is not using the value passed into it.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// If a function should work with different values → it must have parameters.
+// Otherwise it will always use the same fixed value
