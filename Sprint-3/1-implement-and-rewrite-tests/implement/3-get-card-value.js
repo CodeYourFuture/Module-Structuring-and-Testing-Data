4e86a323bf66a8a3cd5c060970af2237b70f43ca
Tobias <amaechinatobias@gmail.com>
2026-07-03 19:50:34 +0100

    Wrote a test to check if the rank contains K and output 10

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -66,6 +66,7 @@ assertEquals(getCardValue("A♠"), 11);
 assertEquals(getCardValue("10♦"), 10);
 assertEquals(getCardValue("J♣"), 10);
 assertEquals(getCardValue("Q♥"), 10);
+assertEquals(getCardValue("K♦"), 10);
 
 // Handling invalid cards
 try {
