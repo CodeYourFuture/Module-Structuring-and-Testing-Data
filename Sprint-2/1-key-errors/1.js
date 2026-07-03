9de35ad9cf2a38f6f5a4bdc740799881d8ea5a28
ChinweP <exponentialresults1973@gmail.com>
2026-07-03 20:35:30 +0100

    Add error  prediction, explanation and  corrected code

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,6 +2,9 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+/* I predict that it will throw a SyntaxError because decimalNumber is declared twice inside the same function, 
+and JvaScript does not allow redeclaring a variable with const in the same scope. 
+It will also throw a ReferenceError because decimalNumber is logged outside the function, where it is not defined. */
 
 // Try playing computer with the example to work out what is going on
 
@@ -15,6 +18,16 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
+/* The function receives decimalNumber as a parameter, so that variable already exists in the function scope. Declaring const decimalNumber = 0.5; 
+inside the function attempts to create a second variable with the same name, which causes a SyntaxError. Additionally, the console.log(decimalNumber) is outside the function, so JavaScript cannot
+find a variable called decimalNumber in that scope, which causes a ReferenceError. */ 
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
+
