778229ebf6737b4b171d42a645fd8bab81d65856
HM-127BTY <mills.hugh@yahoo.com>
2026-07-03 14:52:58 +0100

    mandatory errors 3.js added " to cardNumber to make into string, added console.log to make last4Digits visible.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,6 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
+console.log (last4Digits)
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +8,5 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+// I firsth thought the -4 would not work as it starts on 0
+// I then recalled the code wont work as the cardNumber is a number and not a string, it needs to be a string for the .slice to work.
