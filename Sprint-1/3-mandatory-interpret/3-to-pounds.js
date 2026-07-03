1540cdc61b291b08b6f33298abe85c59e3efa76a
HM-127BTY <mills.hugh@yahoo.com>
2026-07-03 18:03:17 +0100

    3-mandatory-interpret  3-to-pounds.js - added the answers to the question.

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,9 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 3-6. It is taking the vaule from pencestring and using substring to return part of the string, by -1 so from the end resualting in "399"
+// 8. It is now adding "0" infront of vaule if the vaule is under 3 characters in size.
+// 9-12. Using the substring function to reduce the size of the vaule to just 2 characters.
+// 14-16. Taking the value from paddedPenceNumberString and this time with substring, only showing the last 2 characters in teh value.
+      //  Then with using .padEnd it ensures that 2 characters are shown with 0 being there is nothing is there.
+// 18. Showing the resualt of the code starting with a "£" then the value of "pounds" then a "." and lastly value of "pence"
