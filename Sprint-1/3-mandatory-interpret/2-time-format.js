4c9c27418ad2573851fc11f17112c97bc312318a
Enice-Codes <mutandaeunice@94gmail.com>
2026-07-05 01:06:37 +0200

    Renamed ,Corrected code

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,13 +1,12 @@
 const movieLength = 8784; // length of movie in seconds
-
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
 
 const remainingMinutes = totalMinutes % 60;
 const totalHours = (totalMinutes - remainingMinutes) / 60;
 
-const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
-
+const result = `${2,359}:${2,398}:${146,4}`;
+console.log(movieLengthTime(result));       
 
 
 // For the piece of code above, read the code and then answer the following questions
@@ -24,9 +23,10 @@ const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
 // it means that the movie hour  is being calculated. totalminutes minus remainingseconds divided by 60.
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-// the variable result represents the total length of the movie in hours and minutes.
-//Better name is MOvieTime.
+// the variable result represents the total length it takes to watch the movie in hours and minutes.
+//Better name is MovieLengthTime.
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-// Yes, this code will work for all values because it is using the modulus operator to calculate the remaining seconds and is using division to calculate the total hours. 
+// Yes, this code will work for different values of movieLength because it calculates the total hours, minutes and seconds based on the length of the movie in seconds. 
 // The code will correctly handle any length of movie in seconds and convert it to the appropriate format.
+
 
