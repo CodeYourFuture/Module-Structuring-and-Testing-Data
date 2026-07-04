ce6155550a04e4868df8ddbb06ca860c397c532a
Yonatan Teklemariam <jante26uk@gmail.com>
2026-07-04 12:58:49 +0100

    corrected errors in tests and commited the changes

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,19 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return String.repeat(count);
+// Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+// The goal is to re-implement that function, not to use it.
+function repeatStr(str, count) {
+  if (typeof str !== "string" || typeof count !== "number" || count < 0) {
+    throw new Error(
+      "Invalid input: str must be a string and count must be a non-negative integer"
+    );
+  }
+
+  let result = "";
+
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+
+  return result;
 }
 
 module.exports = repeatStr;
