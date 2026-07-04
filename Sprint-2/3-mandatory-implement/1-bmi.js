195189aa7cd5b896c67e360ebd03c4e5114f9f90
JorvanW <your.email@example.com>
2026-07-04 15:38:10 +0100

    completed. 1-bmi

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -17,3 +17,10 @@
 function calculateBMI(weight, height) {
     // return the BMI of someone based off their weight and height
 }
+
+function calculateBMI(weight, height) {
+    const bmi = weight / (height * height);
+    return parseFloat(bmi.toFixed(1));
+}
+
+console.log(calculateBMI(70, 1.73));
