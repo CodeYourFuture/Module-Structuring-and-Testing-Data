091f62b921e99cfd6282e52fa41e27c3ba4123eb
Yonatan Teklemariam <jante26uk@gmail.com>
2026-07-04 03:14:41 +0100

    Fix function name from repeatedStr to repeatStr

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -42,7 +42,7 @@ test("should repeat the string count of 0", () => {
 
 // Case: Handle negative count:
 test(`should throw an error for negative count`, () => {
-  expect(() => repeatedStr("fella", -2)).toThrowError();
+  expect(() => repeatStr("fella", -2)).toThrowError();
 });
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
