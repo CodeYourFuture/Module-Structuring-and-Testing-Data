65daf85b94afa4ce9d12a743a9e9a648000029be
Tobias <amaechinatobias@gmail.com>
2026-07-05 02:55:56 +0100

    removed the template string to make the return value a number instead of a string

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -15,7 +15,7 @@
 // It should return their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
-    return` $ ${(weight / (height * height)).toFixed(1)}`;
+    return (weight / (height * height)).toFixed(1);
     // return the BMI of someone based off their weight and height
 }
 console.log(calculateBMI(70, 1.73)); // should return 23.4  
