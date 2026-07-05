0a536d0370c16bc99db44ebb7d0c9d178701f0b5
cywong <zenobiatony@gmail.com>
2026-07-05 16:26:27 +0100

    Add explanation for random integer generation
    [ChunYanWong] Added comment explaining num as a random integer.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,5 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// [ChunYanWong] num is the random integer number generated in the range of 1 and 100 inclusively 
