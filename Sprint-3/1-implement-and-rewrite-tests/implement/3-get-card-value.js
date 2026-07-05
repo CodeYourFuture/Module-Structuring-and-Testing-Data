7c5d0b404d1b25c885047f40ad5fa987ce19f1d8
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-05 21:06:30 +0100

    implement getCardValue() function and add tests

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -22,7 +22,26 @@
 // execute the code to ensure all tests pass.
 
 function getCardValue(card) {
-  // TODO: Implement this function
+  const suits = ["♠", "♥", "♦", "♣"];
+  if (card[0] === "A" && suits.includes(card[1]) && card.length === 2)
+    return 11;
+  if (
+    (card[0] === "J" || card[0] === "Q" || card[0] === "K") &&
+    suits.includes(card[1]) &&
+    card.length === 2
+  )
+    return 10;
+  if (
+    ["2", "3", "4", "5", "6", "7", "8", "9"].includes(card[0]) &&
+    suits.includes(card[1]) &&
+    card.length === 2
+  )
+    return Number(card[0]);
+  if (card.slice(0, 2) == "10" && suits.includes(card[2]) && card.length === 3)
+    return 10;
+  throw new Error(
+    'Invalid input: Expected rank followed by a suit symbol. For example: "A♠", "2♥", "10♥", "J♣", "Q♦", "K♦"'
+  );
 }
 
 // The line below allows us to load the getCardValue function into tests in other files.
@@ -40,6 +59,11 @@ function assertEquals(actualOutput, targetOutput) {
 // TODO: Write tests to cover all outcomes, including throwing errors for invalid cards.
 // Examples:
 assertEquals(getCardValue("9♠"), 9);
+assertEquals(getCardValue("Q♦"), 10);
+assertEquals(getCardValue("K♣"), 10);
+assertEquals(getCardValue("10♥"), 10);
+assertEquals(getCardValue("2♦"), 2);
+assertEquals(getCardValue("7♦"), 7);
 
 // Handling invalid cards
 try {
@@ -52,3 +76,62 @@ try {
 }
 
 // What other invalid card cases can you think of?
+// for instance "1♦" where it starts with 1 but its not 10
+// where it includes the correct answer but there are added characters maybe space(s) like "9♠ "
+//where its the opposite way : "♠9"
+//where it starts with a valid number but has otherwise more digits eg: "9♠♠" or "99♠"
+//where there is a different symbol from the 4 given such as ❤️
+
+try {
+  getCardValue("1♥");
+
+  // This line will not be reached if an error is thrown as expected
+  console.error("Error was not thrown for invalid card 😢");
+} catch (e) {
+  console.log("Error thrown for invalid card 🎉");
+}
+
+try {
+  getCardValue("9♠ ");
+
+  // This line will not be reached if an error is thrown as expected
+  console.error("Error was not thrown for invalid card 😢");
+} catch (e) {
+  console.log("Error thrown for invalid card 🎉");
+}
+
+try {
+  getCardValue("♠9");
+
+  // This line will not be reached if an error is thrown as expected
+  console.error("Error was not thrown for invalid card 😢");
+} catch (e) {
+  console.log("Error thrown for invalid card 🎉");
+}
+
+try {
+  getCardValue("99♠");
+
+  // This line will not be reached if an error is thrown as expected
+  console.error("Error was not thrown for invalid card 😢");
+} catch (e) {
+  console.log("Error thrown for invalid card 🎉");
+}
+
+try {
+  getCardValue("9♠♠");
+
+  // This line will not be reached if an error is thrown as expected
+  console.error("Error was not thrown for invalid card 😢");
+} catch (e) {
+  console.log("Error thrown for invalid card 🎉");
+}
+
+try {
+  getCardValue("9❤️");
+
+  // This line will not be reached if an error is thrown as expected
+  console.error("Error was not thrown for invalid card 😢");
+} catch (e) {
+  console.log("Error thrown for invalid card 🎉");
+}
