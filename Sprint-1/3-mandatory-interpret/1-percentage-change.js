a16ebf15444933ff5110dc650e6236c326cc4867
Dipa Sarker <dipasarker696@gmail.com>
2026-07-05 12:31:07 +0100

    corrected variable declarations & reassignment

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -25,13 +25,15 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // c) Identify all the lines that are variable reassignment statements
 // There are 2 variable reassignment statements. 
-// const priceDifference = carPrice - priceAfterOneYear;
-// const percentageChange = (priceDifference / carPrice) * 100;
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // d) Identify all the lines that are variable declarations
 // There are 4 variable declarations lines. 
 // let carPrice = "10,000";
 // let priceAfterOneYear = "8,543";
+// const priceDifference = carPrice - priceAfterOneYear;
+// const percentageChange = (priceDifference / carPrice) * 100;
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
 // Firstly this expression replace all the commas from the variable carPrice = "10,000" to "10000" and then it converts 
