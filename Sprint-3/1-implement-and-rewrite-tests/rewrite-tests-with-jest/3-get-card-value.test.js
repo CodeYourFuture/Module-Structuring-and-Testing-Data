0d211421bbe242cc94805b274293478219b71d01
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-05 22:29:13 +0100

    rewrite cards test with jest(exercise 3)

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -7,14 +7,43 @@ const getCardValue = require("../implement/3-get-card-value");
 // Case 1: Ace (A)
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
+  expect(getCardValue("A♥")).toEqual(11);
+  expect(getCardValue("A♦")).toEqual(11);
+  expect(getCardValue("A♣")).toEqual(11);
 });
 
 // Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-//   Face Cards (J, Q, K)
+//   Case 2: Number Cards (2-10)
+test(`Should return the number when given a number card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("3♦")).toEqual(3);
+  expect(getCardValue("4♣")).toEqual(4);
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("6♠")).toEqual(6);
+  expect(getCardValue("7♣")).toEqual(7);
+  expect(getCardValue("8♠")).toEqual(8);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("10♥")).toEqual(10);
+});
+
+//   Case 3: Face Cards (J, Q, K)
+test(`Should return 10 when given a face card`, () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♣")).toEqual(10);
+  expect(getCardValue("K♥")).toEqual(10);
+});
+
 //   Invalid Cards
+test(`Should return an error when given a invalid input`, () => {
+  expect(() => getCardValue("J♠ ")).toThrow();
+  expect(() => getCardValue("Q")).toThrow();
+  expect(() => getCardValue("9♠ ")).toThrow();
+  expect(() => getCardValue("♠9")).toThrow();
+  expect(() => getCardValue("♠99")).toThrow();
+  expect(() => getCardValue("99♠")).toThrow();
+  expect(() => getCardValue("9❤️")).toThrow();
+});
 
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
-
