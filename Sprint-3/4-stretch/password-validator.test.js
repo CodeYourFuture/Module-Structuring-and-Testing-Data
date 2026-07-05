012c657a53b56f380fae77522893b20765b89ac0
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-05 08:39:03 +0100

    Write tests for 4-stretch/password-validator.test.js

--- Sprint-3/4-stretch/password-validator.test.js
+++ Sprint-3/4-stretch/password-validator.test.js
@@ -16,11 +16,38 @@ You must breakdown this problem in order to solve it. Find one test case first a
 */
 const isValidPassword = require("./password-validator");
 test("password has at least 5 characters", () => {
-    // Arrange
-    const password = "12345";
-    // Act
-    const result = isValidPassword(password);
-    // Assert
-    expect(result).toEqual(true);
-}
-);
+  // Arrange
+  const password = "12345";
+  // Act
+  const result = isValidPassword(password);
+  // Assert
+  expect(result).toEqual(true);
+});
+
+test("returns true for a valid password", () => {
+  expect(isValidPassword("Pen5!")).toBe(true);
+});
+
+test("returns false for a short password", () => {
+  expect(isValidPassword("Pen!")).toBe(false);
+});
+
+test("returns false if there is no uppercase English letter", () => {
+  expect(isValidPassword("pen5!")).toBe(false);
+});
+
+test("returns false if there is no lowercase English letter", () => {
+  expect(isValidPassword("PEN5!")).toBe(false);
+});
+
+test("returns false if there is no number", () => {
+  expect(isValidPassword("PENf!")).toBe(false);
+});
+
+test("returns false if there is no symbol", () => {
+  expect(isValidPassword("Pen5a")).toBe(false);
+});
+
+test("returns false if password is a previous password", () => {
+  expect(isValidPassword("HeLlo5.")).toBe(false);
+});
