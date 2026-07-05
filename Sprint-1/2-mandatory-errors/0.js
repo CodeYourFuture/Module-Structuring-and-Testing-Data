6184479ee9ca9c2214ac1bf1c0fb8410be9e8f4a
Dipa Sarker <dipasarker696@gmail.com>
2026-07-05 11:46:59 +0100

    write single line comment correctly

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,5 +1,5 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
 
-// We can solve this problem by writing those two lines as comments using //, 
-// because JavaScript ignores commented lines and they will not be executed by the computer.
+// We can solve this problem by writing those two lines as comments using // 
+// Because JavaScript ignores commented lines and they will not be executed by the computer. //
