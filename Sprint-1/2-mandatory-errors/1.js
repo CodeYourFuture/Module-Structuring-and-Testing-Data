94d7e29304b24b32e909fa63fb68d86f74ed6f6c
Dipa Sarker <dipasarker696@gmail.com>
2026-07-05 11:49:26 +0100

    wrote single comment correctly

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,5 +2,5 @@
 const age = 33;
 age = age + 1;
 
-// There is a constant variable age, which is fixed and it's value cannot be reassigned.
-// If we want to change the value of age, we have to use let instead of const.
+// There is a constant variable age, which is fixed and it's value cannot be reassigned.//
+// If we want to change the value of age, we have to use let instead of const. //
