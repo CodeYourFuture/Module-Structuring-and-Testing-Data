4d1bf8c552f5c44365ed0bf3c8912ef101ad0726
Dagim-Daniel <dagimdan19@gmail.com>
2026-07-06 20:02:41 +0100

    Sprint 3 implement and rewrite-test with jest has been done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -11,9 +11,34 @@ test(`Should return 11 when given an ace card`, () => {
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
+test(`Should return 10 when given a number card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("3♦")).toEqual(3);
+  expect(getCardValue("4♣")).toEqual(4);
+  expect(getCardValue("5♠")).toEqual(5);
+  expect(getCardValue("6♦")).toEqual(6);
+  expect(getCardValue("7♣")).toEqual(7);
+  expect(getCardValue("8♠")).toEqual(8);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("10♣")).toEqual(10);
+});
 //   Face Cards (J, Q, K)
-//   Invalid Cards
+test(`Should return 10 when given a number card`, () => {
+  expect(getCardValue("K♠")).toEqual(10);
+  expect(getCardValue("q♦")).toEqual(10);
+  expect(getCardValue("J♣")).toEqual(10);
+});
 
+//   Invalid Cards
+test(`Should return Invalid Cards when given invalid inputs`, () => {
+  expect(() => getCardValue("")).toThrow();
+  expect(() => getCardValue("11")).toThrow();
+  expect(() => getCardValue("♠10")).toThrow();
+  expect(() => getCardValue("invalid")).toThrow();
+  expect(() => getCardValue("♠1")).toThrow();
+  expect(() => getCardValue("ab")).toThrow();
+  
+});
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
