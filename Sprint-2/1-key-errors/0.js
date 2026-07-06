066a39eeada70a2a358c681b61c37fd78c68c867
cywong <zenobiatony@gmail.com>
2026-07-06 15:57:52 +0100

    Prevent uncaught type error for empty string
    Commented out code to prevent uncaught type error for empty strings.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -12,3 +12,6 @@ function capitalise(str) {
 // =============> write your explanation here
 // =============> write your new code here
 // [ChunYanWong]The uncaught type error will occur if the string is empty
+// if (str.length > 0) {
+//    let str = ...
+// 
