3538f2895ba6f559d38141cf8c63473cb5e3e692
Dagim-Daniel <dagimdan19@gmail.com>
2026-07-06 18:35:18 +0100

    Revert "sprint 3  implement and rewrite  - i  modified the implement part of get-card-value add line 37 to 40 and also done with the rewrite tests with jest tasks."
    This reverts commit c822d538910bba2a0e5b47084792104ec62703c8.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -3,27 +3,8 @@
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
-test(`should return True when denominator is positive and  less than numerator`, () => {
-  expect(isProperFraction(1, 2)).toEqual(true);
-  expect(isProperFraction(-1, 2)).toEqual(true );
-  expect(isProperFraction(20, 21)).toEqual(true );
-  expect(isProperFraction(60, 90)).toEqual(true );
-});
-
-test(`should return false when denominator greater than numerator`, () => {
-  expect(isProperFraction(10, 1)).toEqual(false);
-  expect(isProperFraction(11, 1)).toEqual(false);
-});
-
-test(`should return false when denominator and numerators are Equal`, () => {
-  expect(isProperFraction(1, 1)).toEqual(false);
-  expect(isProperFraction(-1,-1)).toEqual(false);
-  expect(isProperFraction(-1, 1)).toEqual(false);
-});
 
 // Special case: numerator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
-  expect(isProperFraction(-1, 0)).toEqual(false);
-  expect(isProperFraction(0, 0)).toEqual(false);
 });
