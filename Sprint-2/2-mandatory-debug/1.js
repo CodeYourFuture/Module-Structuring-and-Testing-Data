9a6d44e7b95ac1d501edbde142de7766f0a5563b
cywong <zenobiatony@gmail.com>
2026-07-06 16:11:19 +0100

    Fix sum function to return correct sum
    Fixed the sum function to return the correct result by including the addition operation in the return statement.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -8,6 +8,12 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
+// [ChunYanWong] 
+// The sum of 10 and 32 is undefined
+
 // =============> write your explanation here
+// [ChunYanWong] a+b should be included in the return statement
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// [ChunYanWong] return (a+b);
