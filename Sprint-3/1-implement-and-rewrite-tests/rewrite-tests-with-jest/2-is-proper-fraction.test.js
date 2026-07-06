5cec9c4161e3d013790933ba0107f3a613c4b68c
dona-shehu <shehu.donna@gmail.com>
2026-07-06 17:42:21 +0100

    Add test in Jest syntax

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,7 +4,43 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
+// Special case: denominator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
+});
+
+
+// Special case: numerator is zero
+test(`should return false when numerator is zero`, () => {
+  expect(isProperFraction(0, 1)).toEqual(false);
+});
+
+
+// Special case: numerator is equal to denominators is a proper fraction
+test(`should return false when numerator is equal to denominator`, () => {
+  expect(isProperFraction(2, 2)).toEqual(false);
+});
+
+
+// Special case: numerator is negative is a proper fraction
+test(`should return false when numerator is negative value`, () => {
+  expect(isProperFraction(-1, 2)).toEqual(false);
+});
+
+
+// Special case: denominator is negative is a proper fraction
+test(`should return false denominator  is negative value `, () => {
+  expect(isProperFraction(1, -2)).toEqual(false);
+});
+
+
+ // Special case: numerator and denominator are both negative values is a proper fraction//
+ test(`should return true when numerator ans denominator are both negative values `, () => {
+  expect(isProperFraction(-5, -6)).toEqual(true);
+});
+
+
+// Special case: negative numerator with smaller value then negative dominator is a proper fraction
+test(`should return false when negative numerator is smaller than negative denominator `, () => {
+  expect(isProperFraction(-6, -5)).toEqual(false);
 });
