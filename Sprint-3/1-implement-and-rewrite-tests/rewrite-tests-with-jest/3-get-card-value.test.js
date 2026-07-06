893490932a480ce6bd6d49ebf96b084b76d5c873
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-06 18:43:08 +0100

    completes sprint 3 task 1 exercise 3 pt 2

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -6,14 +6,40 @@ const getCardValue = require("../implement/3-get-card-value");
 
 // Case 1: Ace (A)
 test(`Should return 11 when given an ace card`, () => {
+  expect(getCardValue("A♣")).toEqual(11);
   expect(getCardValue("A♠")).toEqual(11);
+  expect(getCardValue("A♥")).toEqual(11);
+  expect(getCardValue("A♦")).toEqual(11);
 });
 
-// Suggestion: Group the remaining test data into these categories:
-//   Number Cards (2-10)
-//   Face Cards (J, Q, K)
-//   Invalid Cards
+// Case 2: Number Cards (2-10)
+test(`Should return a value equal to the number on the card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("9♦")).toEqual(9);
+  expect(getCardValue("10♣")).toEqual(10);
+});
 
+// Case 3: Face Cards (J, Q, K)
+test(`Should return 10 when given a face card`, () => {
+  expect(getCardValue("K♥")).toEqual(10);
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♦")).toEqual(10);
+});
+
+// Case 4: Invalid Cards
+test(`All invalid cards should throw an Invalid error`, () => {
+  expect(() => getCardValue("9")).toThrow(/Invalid/); // missing suit
+  expect(() => getCardValue("♥")).toThrow(/Invalid/); // missing rank
+  expect(() => getCardValue("1♠")).toThrow(/Invalid/); // invalid rank
+  expect(() => getCardValue("11♠")).toThrow(/Invalid/); // rank too high
+  expect(() => getCardValue("9X")).toThrow(/Invalid/); // invalid suit
+  expect(() => getCardValue("♠9")).toThrow(/Invalid/); // wrong order
+  expect(() => getCardValue("9 ♠")).toThrow(/Invalid/); // space inside
+  expect(() => getCardValue("9♠ ")).toThrow(/Invalid/); // space after
+  expect(() => getCardValue("")).toThrow(/Invalid/); // empty string
+  expect(() => getCardValue("j♠")).toThrow(/Invalid/); // lowercase
+});
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
