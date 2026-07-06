1fb3330e7c4e3c70c47d33a7afccc8368fc16485
cywong <zenobiatony@gmail.com>
2026-07-06 16:08:32 +0100

    Fix multiply function to return correct value
    Added return statement to the multiply function to ensure it returns the product of the two numbers.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -8,7 +8,15 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
+// [ChunYanWong] 
+//320
+//The result of multiplying 10 and 32 is undefined
+
 // =============> write your explanation here
+// [ChunYanWong] The program can be executed but no return statement in the function and hence no value is returned.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// [ChunYanWong]
+// console.log(a * b);
+// return (a * b)
