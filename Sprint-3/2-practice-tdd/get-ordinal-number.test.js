ddd628dc8178660424553382d948451086f60708
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-07-07 04:04:25 +0100

    Add tests for getOrdinalNumber function: cover cases for 2, 3, and other numbers

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,32 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+// Case 2: Numbers ending with 2 (but not 12)
+// When the number ends with 2, except those ending with 12,
+// Then the function should return a string by appending "nd" to the number.
+test("should append 'nd' for numbers ending with 2, except those ending with 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(102)).toEqual("102nd");
+});
+
+// Case 3: Numbers ending with 3 (but not 13)
+// When the number ends with 3, except those ending with 13,
+// Then the function should return a string by appending "rd" to the number.
+test("should append 'rd' for numbers ending with 3, except those ending with 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(103)).toEqual("103rd");
+});
+
+// Case 4: All other numbers, including 11, 12, and 13
+// When the number doesn't fall into the above categories,
+// Then the function should return a string by appending "th" to the number.
+test("should append 'th' for all other numbers, including 11, 12, and 13", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(111)).toEqual("111th");
+  expect(getOrdinalNumber(0)).toEqual("0th");
+});
