e8cf53206be9923f952f187face6cd8f03303790
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-07 09:20:22 +0100

    count functions

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,17 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let count = 0;
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countChar;
+
+// for mordern approach, we can use the following code
+// function countChar(stringOfCharacters, findCharacter) {
+//   return stringOfCharacters.split(findCharacter).length - 1;
+// }
+// module.exports = countChar;
