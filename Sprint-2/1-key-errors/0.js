a91b538226497720d81e57f30c81469347fc3ad4
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-07 17:29:31 +0100

    fixed function error in 0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,13 +4,15 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise("hello") {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise("hello") {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // Uncaught SyntaxError: Identifier 'str' has already been declared - meaning str should not have been used inside the function or the parameter of the function
 function capitalise(str) {
   let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return capitalisedStr;
 }
+
+console.log(capitalise("hello"));
