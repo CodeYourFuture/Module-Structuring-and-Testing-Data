02f49e7e1b8c73ffa8d3c270a61e8cc3d516712f
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-07 09:22:51 +0100

    my get original number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,28 @@
+//function getOrdinalNumber(num) {
+//return "1st";
+//}
+
+//module.exports = getOrdinalNumber;
 function getOrdinalNumber(num) {
-  return "1st";
+  // Handle the teen exception rule first (11th, 12th, 13th)
+  // Any number ending in 11, 12, or 13 gets a "th" suffix
+  const lastTwoDigits = num % 100;
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return num + "th";
+  }
+
+  // Otherwise, look closely at the very last digit
+  const lastDigit = num % 10;
+  switch (lastDigit) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
