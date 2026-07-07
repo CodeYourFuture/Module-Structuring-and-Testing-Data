6a5a66cbad3a9a89b077df1ae9ea107ea815e8ab
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-07-07 04:48:28 +0100

    Implement repeatStr function to re-create string repetition logic without using String.prototype.repeat

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,16 @@
 function repeatStr() {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
-}
+  function repeatStr(str, count) {
+    if (count < 0) {
+      throw new Error("Count must be a non-negative integer");
+    }
+    let result = "";
+    for (let i = 0; i < count; i++) {
+      result += str;
+    }
+    return result;
+  }
 
-module.exports = repeatStr;
+  module.exports = repeatStr;
+}
