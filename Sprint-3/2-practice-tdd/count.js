c67170cec6ede8077c975c44f6430a55d75af8f0
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-07 21:15:37 +0100

    Update solution for Sprint-3/2-practice-tdd.js

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,9 +1,4 @@
 function countChar(stringOfCharacters, findCharacter) {
-  // return 0 if either the string or character is empty
-  const hasNoInput =
-    stringOfCharacters.length === 0 || findCharacter.length === 0;
-  if (hasNoInput) return 0;
-
   let count = 0;
   for (const char of stringOfCharacters) {
     if (char === findCharacter) count++;
