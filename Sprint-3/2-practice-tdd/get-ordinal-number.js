602f4d67756f6a7a06308d5f5b6d137ad29dc6c1
Tomislav Dukez <tomdu3@ymail.com>
2026-07-08 11:06:05 +0100

    fix formatting

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -4,13 +4,17 @@ function getOrdinalNumber(num) {
     throw new Error("Invalid input: number must be a non-negative integer");
   }
 
-if (num % 100 >= 11 && num % 100 <= 13) {  // check for numbers ending with 11, 12, or 13
+  if (num % 100 >= 11 && num % 100 <= 13) {
+    // check for numbers ending with 11, 12, or 13
     return num + "th";
-  } else if (num % 10 === 1) {  // check for numbers ending with 1 (but not 11)
+  } else if (num % 10 === 1) {
+    // check for numbers ending with 1 (but not 11)
     return num + "st";
-  } else if (num % 10 === 2) {  // check for numbers ending with 2 (but not 12)
+  } else if (num % 10 === 2) {
+    // check for numbers ending with 2 (but not 12)
     return num + "nd";
-  } else if (num % 10 === 3) {  // check for numbers ending with 3 (but not 13)
+  } else if (num % 10 === 3) {
+    // check for numbers ending with 3 (but not 13)
     return num + "rd";
   }
   // For all other numbers, append "th"
