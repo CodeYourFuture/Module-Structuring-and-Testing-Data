d86c3ed16ee7ac11f3f4610c86482bf6342d7914
Tomislav Dukez <tomdu3@ymail.com>
2026-07-08 11:01:00 +0100

    fix remove redundant condition

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,7 +1,4 @@
 function countChar(stringOfCharacters, findCharacter) {
-  if (!stringOfCharacters.includes(findCharacter)) {
-    return 0;
-  }
   return stringOfCharacters.split("").filter((char) => char === findCharacter)
     .length;
 }
