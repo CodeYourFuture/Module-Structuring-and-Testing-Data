c1a8dc68fb87896b6bcba380fe334cf622cc5abf
Dipa Sarker <dipasarker696@gmail.com>
2026-07-09 22:11:37 +0100

    correctly wrote single line comment

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,9 +1,9 @@
-// Currently trying to print the string "I was born in Bolton" but it isn't working... //
-// what's the error ? //
+// Currently trying to print the string "I was born in Bolton" but it isn't working... 
+// what's the error ? 
 
 const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
 
 
-// The variable const cityOfBirth = "Bolton"; needs to be declared at first. //
-// As it is not declared, JavaScript cannot find out the variable cityofBirth in console.log. //
+// The variable const cityOfBirth = "Bolton"; needs to be declared at first. 
+// As it is not declared, JavaScript cannot find out the variable cityofBirth in console.log. 
