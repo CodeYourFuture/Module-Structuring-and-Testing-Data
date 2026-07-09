215f710d1b16d80909ef435f9f47ffa24f066d32
HM-127BTY <mills.hugh@yahoo.com>
2026-07-09 18:03:36 +0100

    Sprint 2 - 1 key errors - 0.js - Answered questions.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+//I know that its going to say str has allready been declared.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +12,14 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+/*
+  I think its because of been called multiple times, at "let str" after "funtion capitalise(str)"
+  changing "let str" to "let capitalised" should help the issue.
+*/
 // =============> write your new code here
+/*
+function capitalise(str) {
+  let capitalised = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalised;
+}
+*/
