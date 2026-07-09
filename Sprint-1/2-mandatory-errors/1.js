772f314161ffa176f3b86dc5b41f1768e0828149
Dipa Sarker <dipasarker696@gmail.com>
2026-07-09 22:08:16 +0100

    replace const with let and write single line comment correctly

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,6 +1,6 @@
 
-const age = 33;
+let age = 33;
 age = age + 1;
 
-// There is a constant variable age, which is fixed and it's value cannot be reassigned.//
-// If we want to change the value of age, we have to use let instead of const. //
+// There is a constant variable age, which is fixed and it's value cannot be reassigned.
+// If we want to change the value of age, we have to use let instead of const. 
