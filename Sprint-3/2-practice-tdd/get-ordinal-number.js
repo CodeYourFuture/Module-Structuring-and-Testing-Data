7991a9b5ae9245f010d3772b4259ba5e6490f8bd
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-10 17:59:47 +0100

    getting ordinal num

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -3,3 +3,12 @@ function getOrdinalNumber(num) {
 }
 
 module.exports = getOrdinalNumber;
+// this is a code i came up with to get the ordinal number of a given number. 
+// I will now write tests to check if it works correctly.
+
+function getOrdinalNumber(n) {
+  const pr = new Intl.PluralRules("en-US", { type: "ordinal" });
+  const suffixes = { one: "st", two: "nd", few: "rd", other: "th" };
+  const rule = pr.select(n);
+  return `${n}${suffixes[rule]}`;
+}
