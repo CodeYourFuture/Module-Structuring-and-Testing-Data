59f9fe219d8797267df09052a4236a1460f9db19
cywong <zenobiatony@gmail.com>
2026-07-10 18:25:28 +0100

    Implement character counting in countChar function

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,12 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+    let count = 0;
+    let char = "";
+    for (let char of stringOfCharacters) {
+        if (char === findCharacter) {
+            count++;
+        }
+    }
+    return count;
 }
 
 module.exports = countChar;
