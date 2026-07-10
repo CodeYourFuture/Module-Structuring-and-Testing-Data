16cb2955d08bbf2f9a8c5cecae4fc25a756aaad7
HM-127BTY <mills.hugh@yahoo.com>
2026-07-10 11:56:14 +0100

    Sprint 2 - 2 Mandatory debug - 1.js added answer to questions asked.

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// return undefined for ${sum(10, 32)}
 
 function sum(a, b) {
   return;
@@ -9,5 +10,13 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// in the function return is split from a + b by ; 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+/*
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+*/
