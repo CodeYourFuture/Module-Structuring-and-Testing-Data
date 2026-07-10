120157ae9491e5fdf0fd207494b8259bd00e4c07
HM-127BTY <mills.hugh@yahoo.com>
2026-07-10 14:03:27 +0100

    Sprint 2 - 3 Mandatory implement - 3-to-pounds.js Added the simple code for converting to pounds,  using kilograms.

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,8 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+function toPounds(weight) {
+    return (weight * 2.204);
+}
+
+console.log(toPounds(70))
