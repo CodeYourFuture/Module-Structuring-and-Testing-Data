6f56e8d4ac9bf62c8f599cd0ccb795e5a16e873a
cywong <zenobiatony@gmail.com>
2026-07-10 18:26:59 +0100

    Add ordinal number logic to getOrdinalNumber function
    Implement logic to return ordinal numbers based on input.

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,17 @@
 function getOrdinalNumber(num) {
-  return "1st";
+    const lastTwoDigits = num % 100;
+
+    // Handle special cases: 11th, 12th, 13th
+    if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+        return `${num}th`;
+    }
+
+    // Normal rules
+    if (lastDigit === 1) return `${num}st`;
+    if (lastDigit === 2) return `${num}nd`;
+    if (lastDigit === 3) return `${num}rd`;
+
+    return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
