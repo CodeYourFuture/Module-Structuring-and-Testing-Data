c1996f95dbe02902c3929749569b526437466e3b
cywong <zenobiatony@gmail.com>
2026-07-10 18:32:10 +0100

    Update test assertions to use toEqual

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -20,26 +20,26 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
 });
 
 test("handles numbers ending in 2 (nd)", () => {
-    expect(getOrdinalNumber(2)).toBe("2nd");
-    expect(getOrdinalNumber(22)).toBe("22nd");
-    expect(getOrdinalNumber(102)).toBe("102nd");
+    expect(getOrdinalNumber(2)).toEqual("2nd");
+    expect(getOrdinalNumber(22)).toEqual("22nd");
+    expect(getOrdinalNumber(102)).toEqual("102nd");
 });
 
 test("handles numbers ending in 3 (rd)", () => {
-    expect(getOrdinalNumber(3)).toBe("3rd");
-    expect(getOrdinalNumber(23)).toBe("23rd");
-    expect(getOrdinalNumber(103)).toBe("103rd");
+    expect(getOrdinalNumber(3)).toEqual("3rd");
+    expect(getOrdinalNumber(23)).toEqual("23rd");
+    expect(getOrdinalNumber(103)).toEqual("103rd");
 });
 
 test("handles numbers ending in 11, 12, 13 (th exceptions)", () => {
-    expect(getOrdinalNumber(11)).toBe("11th");
-    expect(getOrdinalNumber(12)).toBe("12th");
-    expect(getOrdinalNumber(13)).toBe("13th");
-    expect(getOrdinalNumber(111)).toBe("111th");
+    expect(getOrdinalNumber(11)).toEqual("11th");
+    expect(getOrdinalNumber(12)).toEqual("12th");
+    expect(getOrdinalNumber(13)).toEqual("13th");
+    expect(getOrdinalNumber(111)).toEqual("111th");
 });
 
 test("handles general th cases", () => {
-    expect(getOrdinalNumber(4)).toBe("4th");
-    expect(getOrdinalNumber(20)).toBe("20th");
-    expect(getOrdinalNumber(100)).toBe("100th");
+    expect(getOrdinalNumber(4)).toEqual("4th");
+    expect(getOrdinalNumber(20)).toEqual("20th");
+    expect(getOrdinalNumber(100)).toEqual("100th");
 });
