b337899941ab037d24be36d22ddc6c6475c4f9ad
HM-127BTY <mills.hugh@yahoo.com>
2026-07-10 12:48:01 +0100

    Sprint 2 - 3 Mandatory implement - 1-bmi.js Added code for the bmi calculator,

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -15,5 +15,10 @@
 // It should return their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
-    // return the BMI of someone based off their weight and height
+    let squaredHeight = (height * height);
+    let baseBmi = (weight / squaredHeight);
+    return bmi = Math.round(baseBmi * 10) / 10;
 }
+console.log (`BMI is ${calculateBMI(70,1.73)}`);
+
+    // return the BMI of someone based off their weight and height
