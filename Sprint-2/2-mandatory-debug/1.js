21024447fc9bb18cb55ff1b475d2845e687a9b5e
Monsur Abdulrahman <monsuromotunde.abdulrahman@gmail.com>
2026-07-10 11:59:13 +0100

    Fix sum function to return correct sum of two numbers

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The output of the code will be: undefined.
 
 function sum(a, b) {
   return;
@@ -9,5 +10,14 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The function sum takes two arguments, a and b, and has a return statement that
+// does not return any value. The line a + b is never executed because it comes after
+// the return statement. Therefore, when we call sum(10, 32) inside the template literal,
+// it will return undefined. As a result, the output of the console.log statement will be:
+// "The sum of 10 and 32 is undefined".
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
