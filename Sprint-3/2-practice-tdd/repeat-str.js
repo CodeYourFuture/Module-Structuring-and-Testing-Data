e28b498ee838591158d2f9d574ca3449febb3255
cywong <zenobiatony@gmail.com>
2026-07-10 18:28:15 +0100

    Implement repeatStr function with error handling
    Implement repeatStr function to repeat a string a given number of times without using String.prototype.repeat. Handle negative count by throwing an error.

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,17 @@
 function repeatStr() {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+
+    for (let i = 0; i < count; i++) {
+        result += str;
+    }
+
+    if (count >= 0) {
+        return result;
+    }
+    else {
+        throw new Error("Count cannot be negative");
+    }
 }
 
 module.exports = repeatStr;
