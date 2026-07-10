d79d235e4d7036a76a2e3fd9ce234db8e235a4b3
HM-127BTY <mills.hugh@yahoo.com>
2026-07-10 15:51:09 +0100

    Sprint 2 - 4 mandatory interpret - time-format.js Answered questions required.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -21,18 +21,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> 61 
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> "61"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> 1, when pad is last called its at ${pad(remainingSeconds)} where the value left is 1
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> 1 at const remainingSexonds = seconds % 60 gives 1
