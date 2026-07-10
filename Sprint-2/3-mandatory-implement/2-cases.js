4a5d86a150c8e9e7749fe0e8189efb22d4a29913
Monsur Abdulrahman <monsuromotunde.abdulrahman@gmail.com>
2026-07-10 13:14:31 +0100

    Implement function to convert string to UPPER_SNAKE_CASE

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -11,6 +11,12 @@
 
 // Another example: "lord of the rings" should be "LORD_OF_THE_RINGS"
 
+
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(str) {
+    // return the string in UPPER_SNAKE_CASE
+    return str.toUpperCase().replace(/ /g, '_');
+}       
