3fbf92e0557b42b293c50352d1ac73d940cabdca
dona-shehu <shehu.donna@gmail.com>
2026-07-11 16:06:27 +0100

    Implement the function countChar  Sprint-3/2-practice-tdd/count.js

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  //count the occurrences of the char in the string
+  // Make it case insensitive
+  let countChar = 0;
+  for (const char of stringOfCharacters) {
+    if (char.toLowerCase() === findCharacter.toLowerCase()) {
+      countChar++;
+    }
+  }
+  return countChar;
 }
 
 module.exports = countChar;
