78c7248ce456bd8de83a05684016aa35a89570d9
dona-shehu <shehu.donna@gmail.com>
2026-07-11 22:14:28 +0100

    Implemented repeatSttr(), Sprint-3/2-practice-tdd/repeat-str.js

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,16 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+// Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+// The goal is to re-implement that function, not to use it.
+
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Count must not be negative");
+  }
+  let newString = "";
+  for (let i = 0; i < count; i++) {
+    newString += str;
+  }
+
+  return newString;
 }
 
 module.exports = repeatStr;
