3c2d06c032b149ccb6727934e661e55199f3b902
Tomislav Dukez <tomdu3@ymail.com>
2026-07-11 16:04:54 +0100

    fix remove additonal test

--- Sprint-3/4-stretch/password-validator.test.js
+++ Sprint-3/4-stretch/password-validator.test.js
@@ -96,15 +96,6 @@ test("password has at least one non-alphanumeric symbol: (!, #, $, %, ., *, &)",
   expect(result).toEqual(true);
 });
 
-test("password has at least one non-alphanumeric symbol: (!, #, $, %, ., *, &)", () => {
-  // Arrange
-  const password = "12aA$12";
-  // Act
-  const result = isValidPassword(password);
-  // Assert
-  expect(result).toEqual(true);
-});
-
 test("must not be any previous password in the passwords array.", () => {
   // Arrange
   const password = "pas$W0rd";
