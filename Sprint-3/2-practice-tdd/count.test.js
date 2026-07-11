80396406c488907cc418d25ce84ff3c728dbe625
dona-shehu <shehu.donna@gmail.com>
2026-07-11 16:09:42 +0100

    written tests for Sprint-3/2-practice-tdd/count.test.js

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,38 @@ test("should count multiple occurrences of a character", () => {
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+
+test("should return 0 if the character in not in the given string", () => {
+  const str = "aaaaa";
+  const char = "b";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+test("should return 1 if the is 1 occurrence of the char in the string", () => {
+  const str = "abc";
+  const char = "c";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
+
+test("should return 0 if there is a empty string ", () => {
+  const str = "";
+  const char = "c";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+test("should return 0 if character is empty", () => {
+  const str = "abcdefg";
+  const char = "";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+test("should be case insensitive ", () => {
+  const str = "Successful";
+  const char = "s";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
