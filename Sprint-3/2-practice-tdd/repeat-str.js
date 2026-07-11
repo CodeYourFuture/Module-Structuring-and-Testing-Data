73f8459e58b751db50296aebd47ae9bae245fc4e
Vitalii Kmit <vitalikkmita@gmail.com>
2026-07-11 16:02:20 +0100

    Implement repeat-str

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,17 @@
-function repeatStr() {
+function repeatStr(str, count) {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+  if (count < 0) {
+    throw new Error("Expected a positive integer");
+  }
+
+  let out = "";
+
+  for (let i = 0; i < count; i++) {
+    out = out + str;
+  }
+
+  return out;
 }
 
 module.exports = repeatStr;
