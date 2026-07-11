1f547457af4e63f2e9dd54144ece3e38c6f2e348
Sayeed Hussain <hussainsahid980@gmail.com>
2026-07-11 13:31:53 +0100

    fix original files

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,5 +4,3 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-
-//  Line 3 increases the number by one and reassigns new  value to count, which stored 0.///
