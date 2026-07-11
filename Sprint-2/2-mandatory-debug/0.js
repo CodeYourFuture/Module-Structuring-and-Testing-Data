ed78bdd422263d15bc0d629ed97d604ef786fe39
fromonda <fr.monda@gmail.com>
2026-07-11 16:52:41 +0100

    Replace console.log with return

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// The result of multiplying 10 and 32 is expected to be undefined because the multiply() function does not return the result.
+// It only prints it using console.log().
+
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +10,12 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// After running the code, the result of multiplying 10 and 32 is undefined indeed.
+// The problem occurs because the function has no return statement to send a value back to where the function was called.
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  function multiply(a, b) {
+//  return a * b;
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
