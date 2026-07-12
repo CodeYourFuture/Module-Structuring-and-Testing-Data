eb5da76e8759231b3caf1bdb5490613b34727cb3
Tobias <amaechinatobias@gmail.com>
2026-07-12 22:00:16 +0100

    implement a function to pass the dynmaict repeat of function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,12 @@
-function repeatStr() {
+function repeatStr(str,count) {
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+  return result;
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
-}
 
+}
+console.log(repeatStr("hello", 3)); // Output: "hellohellohello"
 module.exports = repeatStr;
