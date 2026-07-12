815729eaecbb24a2cb614a30f260fd24357fbcbf
Maryam Janjua <maryamjanjua65@gmail.com>
2026-07-12 15:43:13 +0100

    Fixed Erros for 2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,23 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+//We cannot use number as a parameter. It should be any variable.
 
-function square(3) {
+/*function square(3) {
     return num * num;
-}
-
+}*/
 // =============> write the error message here
-
+//The error message is, 'Unexpected number'. 
 // =============> explain this error message here
-
+// it says unexpected in terms, javaScript doesn't expect number/value as a parameter.
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
 
+function square(num){
+    return num*num;
+}
+
+const result = square(3);
+console.log(result);
