5481fb455ed6e72fb4c6cf1dc29209404d58b230
Tobias <amaechinatobias@gmail.com>
2026-07-12 20:58:34 +0100

    wrote a test case  for number ending in 12 and 13

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -26,5 +26,11 @@ test("should append 'th' for numbers ending with 11", () => {
   expect(getOrdinalNumber(11)).toEqual("11th");
   expect(getOrdinalNumber(111)).toEqual("111th");
   expect(getOrdinalNumber(211)).toEqual("211th");
+  expect(getOrdinalNumber(312)).toEqual("311th");
 });
+
+// Case 3: Numbers ending with 2 (but not 12)
+
+
+
 
