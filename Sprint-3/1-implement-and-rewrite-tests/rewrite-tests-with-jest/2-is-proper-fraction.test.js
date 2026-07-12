c278f255912b842902b50baf6e517923d8244531
vmoratti <vmoratti@gmail.com>
2026-07-12 14:28:58 +0100

    add test for zero numerator

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,7 +4,12 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
+// Special case: denominator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
+});
+
+// Special case: numerator is zero
+test(`should return true when numerator is zero `, () => {
+  expect(isProperFraction(0, 2)).toEqual(true);
 });
