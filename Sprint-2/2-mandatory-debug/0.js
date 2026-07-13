5e3518c5a4bf70e0db873cb9409a4813dafa1a50
Maryam Janjua <maryamjanjua65@gmail.com>
2026-07-13 12:10:34 +0100

    Fixed Return statment

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,21 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
-function multiply(a, b) {
+// In this example function is being called but as there's no return statement so it will return undefined.
+// and a *b multiplication. and the line on line 10
+/*function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
+*/
 // =============> write your explanation here
-
+// I fixed it by adding a return statement and create another variable outside of the function that stores,
+//value of function.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return (a*b);
+}
+const result = multiply(10,32);
+console.log(`The result of multiplying 10 and 32 is ${result}`);
