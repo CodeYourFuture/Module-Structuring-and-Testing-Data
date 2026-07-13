91836abdbbb8f671ae2c05b5f08f6100a3f44dea
Abid Akhtar <Abidakhtar2023@gmail.com>
2026-07-14 00:47:49 +0100

    Complete time format and to pounds exercises

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -24,4 +24,40 @@ console.log(result);
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
 
+/*  
 
+a)variables declarations
+
+1. There are 6 variable declarations 
+
+b) Function calls
+
+There is only 1 function call
+
+console.log(result);
+
+c)Using documentation, explain what the expression movieLength % 60 represents
+
+According to the JavaScript documentation, the remainder (%) operator returns the remainder left over after one number is divided by another. 
+
+So movieLength % 60  means Divide movieLength by 60 and return the remainder.
+
+d) Interpret line 4. What does the expression assigned to totalMinutes mean?
+
+It means remove the leftover seconds from the total movie length, then divide by 60 to calculate the total number of whole minutes.
+
+
+e) What do you think the variable result represents? Can you think of a better name?
+
+It stores the movie length formatted as hours:minutes:seconds
+
+A better variable name could be formattedTime
+
+
+f) Try experimenting with different values of movieLength.
+
+It works correctly for positive whole numbers (integers) representing seconds. However, there are some cases where it doesn't produce ideal results like:
+
+const movieLength = -100;
+
+*/
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,4 +1,4 @@
-const penceString = "399p";
+const penceString = "380p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
@@ -25,3 +25,54 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+/* 
+
+Step 1:
+we create a variable called penceString and store the text "339p".
+
+Step 2:
+
+a) . (dot) lets us use something that belongs to the string.
+b) length is a property that tells us how many characters are in the string. There are 4 characters so returns 4.
+c) penceString.length - 1 which means 4 - 1 = 3 We want to remove the last character ("p"), so we stop before it.
+d) penceString.substring(0, 3) substring(start, end) start = where to begin end = where to stop (not including that position) So: substring(0, 3)
+
+Start at character 0 and stop before character 3. Why? We don't want the "p" anymore because we only need the number.
+
+Step 3: 
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); 
+
+It means If this string is shorter than length, add the character to the beginning until it reaches that length. Our value already has 3 characters so nothing changes.
+
+Step 4:
+
+const pounds = paddedPenceNumberString.substring(
+  0, 
+  paddedPenceNumberString.length - 2);
+
+a) paddedPenceNumberString.length returns 3 then 3 - 2 is 1. 
+
+Why? because the last 2 digits are always the pence and everything before them is the pounds.
+
+Step 5:
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+  a) substring(length - 2) the length is 3 so substring(1) means Start at index 1 and keep going to the end.
+
+  b) .padEnd(2, "0")  which means if the string is shorter than 2 characters, add "0" to the end.
+
+
+Step 6:
+
+  console.log(`£${pounds}.${pence}`);
+  
+Inside it is a template literal, which uses backticks (`): and `£${pounds}.${pence}` means insert the value of this variable here. 
+
+If pounds = "3" pence = "99" then javascript prints 3.99
+
+
+*/
