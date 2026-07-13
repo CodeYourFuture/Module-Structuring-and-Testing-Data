68d126607371925774a75135bf9b1474cb606098
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-07-13 12:45:07 +0100

    Refactor isProperFraction function for improved readability

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -11,10 +11,7 @@
 // execute the code to ensure all tests pass.
 
 function isProperFraction(numerator, denominator) {
-  if (denominator === 0) {
-    return false;
-  }
-  return Math.abs(numerator) < Math.abs(denominator);
+  return denominator !== 0 && Math.abs(numerator) < Math.abs(denominator);
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
