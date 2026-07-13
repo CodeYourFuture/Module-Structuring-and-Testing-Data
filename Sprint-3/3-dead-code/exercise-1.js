d5d63e8db2032d6bdbf80e58194538cf61ef430e
bodoszidi <szidibee@gmail.com>
2026-07-13 19:54:23 +0100

    completed exercise and commented out the redundant lines

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -5,9 +5,11 @@ let testName = "Jerry";
 const greeting = "hello";
 
 function sayHello(greeting, name) {
-  const greetingStr = greeting + ", " + name + "!";
+  //const greetingStr = greeting + ", " + name + "!"; 
+  //  - removing this line as this is redundant code and duplicated inside the return 
   return `${greeting}, ${name}!`;
-  console.log(greetingStr);
+  //console.log(greetingStr); 
+  // - removing this line because after return the console.log will not execute
 }
 
 testName = "Aman";
