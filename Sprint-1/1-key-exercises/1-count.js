b11e4157f80f65b8557e5afdcd962bd2ec2a6e11
TaliaK <talia_k@live.co.uk>
2026-07-14 15:40:35 +0100

    answered the first question in exercise 1

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,4 @@ let count = 0;
 
 count = count + 1;
 
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// The expression on the right is evaluated. The = operator assigns the result to the variable on the left, updating the value stored in count. So it took the value of count which was 0 and added 1 to equal 1 and assigned that value to the variable of count.
