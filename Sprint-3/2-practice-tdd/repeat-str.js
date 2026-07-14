82538915648f10748bf616bc94b55d0a2826b1b0
russom <russom.g.meskel@gmail.cpm>
2026-07-14 14:18:16 +0100

    repeatStr function created to check count times is str repeated.

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,15 @@
-function repeatStr() {
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Error");
+  }
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+  //The goal is to re-implement that function, not to use it.
+  let repeatedStr = "";
+
+  for (let i = 0; i < count; i++) {
+    repeatedStr += str;
+  }
+  return repeatedStr;
 }
 
 module.exports = repeatStr;
