d50ef0ba3c52e0d36931962db144f0b1de920bf3
Maryam Janjua <maryamjanjua65@gmail.com>
2026-07-15 17:17:14 +0100

    Function to convert into pounds

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,21 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+function ConversionPenceToPounds(penceString){
+    const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+    paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    pounds = paddedPenceNumberString.substring( 0,paddedPenceNumberString.length - 2);
+    paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    pounds = paddedPenceNumberString.substring( 0,paddedPenceNumberString.length - 2);
+    const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+    const result = `£${pounds}.${pence}`;
+    return result;
+}
+const pencestring1 = ConversionPenceToPounds("663p");
+const pencestring2 = ConversionPenceToPounds("98p");
+const pencestring3 = ConversionPenceToPounds("986p");
+console.log(pencestring1);
+console.log(pencestring2);
+console.log(pencestring3);
