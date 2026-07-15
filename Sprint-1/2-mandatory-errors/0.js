17cbf5533bded8c8d5e9269457b51495088f7d86
christelleb <christelleboten@gmail.com>
2026-07-15 13:55:29 +0100

    worked on mandatory errors 0 and 1

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,4 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//  We use two forward slashes to comment things we do not want the computer to read. It is for human reading only. 
