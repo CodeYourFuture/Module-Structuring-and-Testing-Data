464f1eacb703d63017ebbc47a694026435654364
christelleb <christelleboten@gmail.com>
2026-07-15 14:04:10 +0100

    MAndatory error 4

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const HourClockTime = "8:53pm";
+const hourClockTime = "20:53";
+
+console.log(HourClockTime, hourClockTime);
+// When declaring variables, we do not use numbers to name the variable. 
