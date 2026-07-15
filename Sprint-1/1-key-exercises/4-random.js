3c3188f47ab5259558d1806c93b5eda899995d25
mandip sanger <mandipsanger1@gmail.com>
2026-07-15 17:06:01 +0100

    add randoms

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,8 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+// num represents a random integer between the minimum and maximum values (inclusive). 
+// The expression Math.random() generates a random decimal number between 0 (inclusive) and 1 (exclusive). 
+// By multiplying this value by (maximum - minimum + 1), we scale it to the desired range. 
+// The Math.floor() function then rounds this value down to the nearest whole number, ensuring we get an integer. 
+// Finally, we add the minimum value to shift the range to start from the minimum instead of 0.
