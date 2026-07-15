6720ac57d971adb570e48c8a104550334395d295
bodoszidi <szidibee@gmail.com>
2026-07-15 22:09:23 +0100

    added explanations for while loop questions

--- Sprint-3/4-stretch/find.js
+++ Sprint-3/4-stretch/find.js
@@ -20,6 +20,10 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+//--the index variable will start at 0, and this will increase by +1 each time the loop runs by using index++ to loop around the string, checking each index inside the string one by one
 // b) What is the if statement used to check
+//--the if statement checks the current str index if it's equal to char, to find the character we are searching for
 // c) Why is index++ being used?
+//--the index++ increases the index by 1 each time so the function can check every character in the string one by one.
 // d) What is the condition index < str.length used for?
+//--this condition will checks the index has not reached the end of the string. Once the index is equal to the string length, the loop stops
