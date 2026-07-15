7cd725aa2748ea6972e086ac864112f80289c416
mandip sanger <mandipsanger1@gmail.com>
2026-07-15 17:07:43 +0100

    add errors task4

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
 const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53";
+
+// 12 Hour clock time is a string that represents the time in 12-hour format, while 24hourClockTime is a string that represents the time in 24-hour format.
+// To convert 12HourClockTime to 24-hour format, we can use the following steps:
+// 1. Split the string into hours and minutes using the split() method.
+// 2. Check if the time is in the "pm" period. If it is, add 12 to the hours (unless it's 12pm).
+// 3. If the time is in the "am" period and the hour is 12, set the hour to 0.
+// 4. Format the hours and minutes into a string in 24-hour format.
