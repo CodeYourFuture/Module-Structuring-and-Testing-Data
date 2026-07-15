103bc6947656984847c632dcfd7dc8cbce3d86f0
mandip sanger <mandipsanger1@gmail.com>
2026-07-15 17:07:06 +0100

    add errors task2

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+const cityOfBirth = "Bolton";
 
 console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
+
+// return has not been defined before it is used in the console.log statement. 
+// To fix this, you should declare and assign a value to the variable cityOfBirth before using it in the console.log statement.
+// To do this, you can move the line const cityOfBirth = "Bolton"; above the console.log statement. 
+// This way, the variable will be defined before it is used, and the code will work as expected.
