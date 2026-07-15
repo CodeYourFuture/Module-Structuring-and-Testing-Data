f76ff589e0c562892ec2a86b4762f77e43b07eb4
Dagim-Daniel <dagimdan19@gmail.com>
2026-07-15 16:24:11 +0100

    another way of doing getOrdinalNumber task. in a clear way

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,8 +1,19 @@
 function getOrdinalNumber(num) {
-  const s = ["th", "st", "nd", "rd"];
-    const lastDigits = num % 100;
-    // Uses the index 1, 2, or 3 for st/nd/rd, defaults to 0 (th)
-    return num + (s[(lastDigits - 20) % 10] || s[lastDigits] || s[0]);
+  const lastTwoDigits = num % 100;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return num + "th";
+  }
+  switch (num % 10) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
