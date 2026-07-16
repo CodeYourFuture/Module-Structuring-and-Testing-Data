4140d5a1fefd78c07c3040455760f622318f7f7c
mandip sanger <mandipsanger1@gmail.com>
2026-07-16 17:17:27 +0100

    fix square errors

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,13 +1,12 @@
-
-// Predict and explain first BEFORE you run any code...
+// SyntaxError: Unexpected token '3'
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//    return num * num;
+//}
 
 // =============> write the error message here
 
@@ -16,5 +15,8 @@ function square(3) {
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+  return num * num;
+}
 
-
+console.log(square(3));
