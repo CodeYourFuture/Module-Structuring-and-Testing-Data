9e5a3cf85acbe3cd9714d5e2084cc93125d7a4ab
Dipa Sarker <dipasarker696@gmail.com>
2026-07-16 21:30:09 +0100

    refactored get ordinal number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,19 +1,15 @@
 function getOrdinalNumber(num) {
-  let lastTwoDigits = num % 100;
-  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
-    return `${num}` + "th";
-  }
-  let lastDigit = num % 10;
-  if (lastDigit === 1) {
-    return `${num}` + "st";
-  } else if (lastDigit === 2) {
-    return `${num}` + "nd";
-  } else if (lastDigit === 3) {
-    return `${num}` + "rd";
+  const lastDigit = num % 10;
+  const lastTwoDigits = num % 100;
+  if (lastDigit === 1 && lastTwoDigits !== 11) {
+    return `${num}st`;
+  } else if (lastDigit === 2 && lastTwoDigits !== 12) {
+    return `${num}nd`;
+  } else if (lastDigit === 3 && lastTwoDigits !== 13) {
+    return `${num}rd`;
   } else {
-    return `${num}` + "th";
+    return `${num}th`;
   }
 }
-
 
 module.exports = getOrdinalNumber;
