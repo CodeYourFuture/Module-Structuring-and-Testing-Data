8edf045883f0edd7a05e794de63432728f9c702f
bodoszidi <szidibee@gmail.com>
2026-07-16 19:55:05 +0100

    added jest cases to ensure the functions is works correctly

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,31 @@ test("should count multiple occurrences of a character", () => {
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+
+test("should count multiple occurrences of a character", () => {
+  const str = "banana";
+  const char = "n";
+  const count = countChar(str, char);
+  expect(count).toEqual(2);
+});
+
+test("should count multiple occurrences of a character", () => {
+  const str = "appleTree";
+  const char = "e";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+test("should count multiple occurrences of a character", () => {
+  const str = "strawberry";
+  const char = "r";
+  const count = countChar(str, char);
+  expect(count).toEqual(3);
+});
+
+test("should count multiple occurrences of a character", () => {
+  const str = "ghost";
+  const char = "o";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
