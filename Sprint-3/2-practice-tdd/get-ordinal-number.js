d5357cde0f914204b2051f9f5a31af1e83de4eee
vmoratti <vmoratti@gmail.com>
2026-07-16 19:48:12 +0100

    correct function

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,14 +1,18 @@
 function getOrdinalNumber(num) {
-  if (num % 100 === 11) {  
-      return `${num}th` // 11 is a special case that should return "th" instead of "st". % 100 will return 11 for any number ending in 11, such as 111, 211, etc.
+  if (num % 100 === 11) {
+      return `${num}th`;
+  } else if (num % 100 === 12) {
+      return `${num}th`;
+  } else if (num % 100 === 13) {
+      return `${num}th`;
   } else if (num % 10 === 1) {
-      return `${num}st`; // If the number ends with 1, except those ending with 11, return "st".
+      return `${num}st`;
   } else if (num % 10 === 2) {
       return `${num}nd`
   } else if (num % 10 === 3) {
-      return `${num}rd` // If the number ends with 2, return "nd". If the number ends with 3, return "rd".
+      return `${num}rd`
   } else {
-      return `${num}th` // For all other cases, return "th".
+      return `${num}th`
   }
 }
 
@@ -19,7 +23,11 @@ console.log(getOrdinalNumber(2));
 console.log(getOrdinalNumber(3));
 console.log(getOrdinalNumber(4));
 console.log(getOrdinalNumber(11));
+console.log(getOrdinalNumber(12));
+console.log(getOrdinalNumber(13));
 console.log(getOrdinalNumber(21));
 console.log(getOrdinalNumber(52));
 console.log(getOrdinalNumber(63));
 console.log(getOrdinalNumber(111))
+
+
