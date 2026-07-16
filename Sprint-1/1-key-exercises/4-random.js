ae3b1cd72af38124590c47fc2b5e3fcaaa013f51
TaliaK <talia_k@live.co.uk>
2026-07-16 19:32:39 +0100

    amended answer to 4-random.js to be more detailed

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,4 +3,7 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-// num generates a random number between 1 and 100
+// minimum and maximum store the lowest and highest numbers that can be generated. 
+// Math.random() generates a random decimal between 0 and 1. This value is multiplied by (maximum - minimum + 1) to produce a random number within the required range.
+// Math.floor() rounds the result down to the nearest whole number producing a number from 0 to 99. 
+// Minimum is then added, shifting the range to 1–100. This ensures that every whole number between 1 and 100 has an equal chance of being generated.
