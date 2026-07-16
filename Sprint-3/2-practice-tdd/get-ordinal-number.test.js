88ecb07c2fe2f6887355d442edbc2e413fe9ae88
vmoratti <vmoratti@gmail.com>
2026-07-16 19:59:26 +0100

    add tests for numbers ending on 2, and on 3

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,15 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+test(`should return 'nd' for numbers ending with 2`, () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(122)).toEqual("122nd");
+})
+
+test(`should add 'rd' for numbers ending with 3`, () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(123)).toEqual("123rd");
+})
+
