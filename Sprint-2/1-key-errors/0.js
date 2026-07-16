2c2076a8ba3268c6e98f89ae45c535217e7089c9
mandip sanger <mandipsanger1@gmail.com>
2026-07-16 17:05:29 +0100

    fix error percentages

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,26 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> SyntaxError: Identifier 'str' has already been declared
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+//function capitalise(str) {
+// let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//return str;
+//}
+
+// =============> str has already been declared in the function. let can not redeclare a variable with the same name in the same scope. parameter, so we cannot declare it again with let. This will cause a syntax error. To fix this, we should remove the let keyword when assigning the new value to str.
+// =============> write new code here
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  let capitalised = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalised;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise("hello"));
+
+// or function capitalise(str) {
+//return `${str[0].toUpperCase()}${str.slice(1)}`;
+//}
+
+//console.log(capitalise("hello"));
