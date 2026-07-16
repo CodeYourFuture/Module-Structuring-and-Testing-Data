e0d2999f1878b2e3399cb24c1b2afb0e09709557
Monsur Abdulrahman <monsuromotunde.abdulrahman@gmail.com>
2026-07-17 00:00:51 +0100

    Add Jest tests for getCardValue function covering aces, face cards, number cards, and invalid inputs

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -4,10 +4,43 @@ const getCardValue = require("../implement/3-get-card-value");
 
 // TODO: Write tests in Jest syntax to cover all possible outcomes.
 
+
 // Case 1: Ace (A)
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
+  expect(getCardValue("A♥")).toEqual(11);
+  expect(getCardValue("A♦")).toEqual(11);
+  expect(getCardValue("A♣")).toEqual(11);
 });
+
+// Case 2: Face cards (J, Q, K)
+test(`Should return 10 when given a face card`, () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♦")).toEqual(10);
+});
+
+// Case 3: Number cards (2-10)
+test(`Should return the numeric value when given a number card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("3♥")).toEqual(3);
+  expect(getCardValue("4♦")).toEqual(4);
+  expect(getCardValue("5♣")).toEqual(5);
+  expect(getCardValue("6♠")).toEqual(6);
+  expect(getCardValue("7♥")).toEqual(7);
+  expect(getCardValue("8♦")).toEqual(8);
+  expect(getCardValue("9♣")).toEqual(9);
+  expect(getCardValue("10♠")).toEqual(10);
+});
+
+// Case 4: Invalid cards
+test(`Should throw an error when given an invalid card`, () => {
+  expect(() => getCardValue("invalid")).toThrow("Invalid card");
+  expect(() => getCardValue("1♠")).toThrow("Invalid card");
+  expect(() => getCardValue("11♥")).toThrow("Invalid card");
+  expect(() => getCardValue("A♦")).not.toThrow(); // Valid ace card
+  expect(() => getCardValue("J♣")).not.toThrow(); // Valid face card
+}); 
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
