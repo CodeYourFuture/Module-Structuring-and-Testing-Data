f0b6faa7f183876796752b895fd1bee6c92f798f
bodoszidi <szidibee@gmail.com>
2026-07-16 19:34:44 +0100

    added jest test cases

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -9,6 +9,29 @@ test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
 
+test(`Should return 10 when given a Joker card`, () => {
+  expect(getCardValue("J♥")).toEqual(10);
+});
+
+test(`Should return 2 when given the 2 card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+});
+
+test(`Should return 10 when given a King card`, () => {
+  expect(getCardValue("K♠")).toEqual(10);
+});
+
+test(`Should return 5 when given the 5 card`, () => {
+  expect(getCardValue("5♠")).toEqual(5);
+});
+
+test(`Should return 8 when gine the 8 card`, () => {
+  expect(getCardValue("8♠")).toEqual(8);
+});
+
+test(`Should return 10 when given the Queen card`, () => {
+  expect(getCardValue("Q♠")).toEqual(10);
+});
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
 //   Face Cards (J, Q, K)
@@ -16,5 +39,9 @@ test(`Should return 11 when given an ace card`, () => {
 
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
-// https://jestjs.io/docs/expect#tothrowerror
-
+// https://jestjs.io/docs/exåpect#tothrowerror
+test(`Should not return any card should throw an error message`, () => {
+  expect(() => {
+    getCardValue("QQ♥");
+  }).toThrow();
+});
