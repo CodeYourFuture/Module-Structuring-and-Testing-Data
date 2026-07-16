7715334f346ebbfe462668ae810a1a576d79bc51
vmoratti <vmoratti@gmail.com>
2026-07-16 21:00:37 +0100

    change conditions in the function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,12 +1,17 @@
 function repeatStr(str, num) {
     var result = "";
     var i;
-    for (i = 0; i < num; i++) {
+    if (num < 0) {
+        throw new Error("negative numbers are not valid")
+    } else if (num === 0) {
+        result = ""
+    } else {
+        for (i = 0; i < num; i++) {
         result += str;
-    }
+    }}
     return result;
 }
     
-console.log(repeatStr("hello", 3))
+console.log(repeatStr("hello", 44))
 
 module.exports = repeatStr;
