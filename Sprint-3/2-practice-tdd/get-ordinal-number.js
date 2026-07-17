c3429056dd97bfb8952e76957a5386a70a957b41
vmoratti <vmoratti@gmail.com>
2026-07-17 17:38:13 +0100

    rewrite code

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,32 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (num % 100 === 11) {
+      return `${num}th`;
+  } else if (num % 100 === 12) {
+      return `${num}th`;
+  } else if (num % 100 === 13) {
+      return `${num}th`;
+  } else if (num % 10 === 1) {
+      return `${num}st`;
+  } else if (num % 10 === 2) {
+      return `${num}nd`
+  } else if (num % 10 === 3) {
+      return `${num}rd`
+  } else {
+      return `${num}th`
+  }
 }
 
 module.exports = getOrdinalNumber;
+
+console.log(getOrdinalNumber(1));
+console.log(getOrdinalNumber(2));
+console.log(getOrdinalNumber(3));
+console.log(getOrdinalNumber(4));
+console.log(getOrdinalNumber(11));
+console.log(getOrdinalNumber(12));
+console.log(getOrdinalNumber(13));
+console.log(getOrdinalNumber(21));
+console.log(getOrdinalNumber(52));
+console.log(getOrdinalNumber(63));
+console.log(getOrdinalNumber(111))
+
