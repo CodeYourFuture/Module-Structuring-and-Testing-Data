dad46f3e6c0b31c2b65dd2495be295ca32c2da06
vmoratti <vmoratti@gmail.com>
2026-07-17 16:58:42 +0100

    rewrite code completely and tests

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -16,7 +16,33 @@
 
 function getAngleType(angle) {
   // TODO: Implement this function
+   // first converting conditions into variables
+  const acuteAngle = (angle > 0 && angle < 90);
+  const rightAngle = (angle === 90);
+  const obtuseAngle = (angle > 90 && angle < 180);
+  const straightAngle = (angle === 180);
+  const reflexAngle = (angle > 180 && angle < 360);
+  const zeroAngle = (angle === 0);
+  
+  
+  if (rightAngle) {
+    // checking for conditions
+      return  "Right angle";
+  } else if (zeroAngle) {
+      return "Invalid angle";
+  }else if (obtuseAngle) {
+      return "Obtuse angle";
+  } else if (straightAngle) {
+      return "Straight angle";
+  } else if (reflexAngle) {
+      return "Reflex angle";
+  } else if (acuteAngle) {
+      return "Acute angle";
+  } else if (angle === zeroAngle || angle != acuteAngle || angle != rightAngle || angle != obtuseAngle || angle != straightAngle || angle != reflexAngle) {
+        return "Invalid angle"; 
+  }
 }
+
 
 // The line below allows us to load the getAngleType function into tests in other files.
 // This will be useful in the "rewrite tests with jest" step.
@@ -35,3 +61,24 @@ function assertEquals(actualOutput, targetOutput) {
 // Example: Identify Right Angles
 const right = getAngleType(90);
 assertEquals(right, "Right angle");
+
+const acute = getAngleType(45);
+assertEquals(acute, "Acute angle");
+
+const obtuse = getAngleType(120);
+assertEquals(obtuse, "Obtuse angle");
+
+const straight = getAngleType(180);
+assertEquals(straight, "Straight angle");
+
+const reflex = getAngleType(270);
+assertEquals(reflex, "Reflex angle");
+
+const invalidAngle1 = getAngleType(360);
+assertEquals(invalidAngle1, "Invalid angle");
+
+const invalidAngle2 = getAngleType(0);
+assertEquals(invalidAngle2, "Invalid angle");
+
+const invalidAngle3 = getAngleType(-1);
+assertEquals(invalidAngle3, "Invalid angle");
