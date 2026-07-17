52c9e2e9526d265bc18bed52434982fbe3b607a6
vmoratti <vmoratti@gmail.com>
2026-07-17 13:31:15 +0100

    Add implementation instructions for repeatStr
    Added implementation notes for repeatStr function.

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,4 +1,7 @@
 function repeatStr(str, num) {
+// Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+  // The goal is to re-implement that function, not to use it.
+  //return "hellohellohello";
   var result = "";
   var i;
   if (num < 0) {
