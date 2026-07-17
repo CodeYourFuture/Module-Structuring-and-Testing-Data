c951858ed6407b1015530e4e16389c07bba9ec7c
vmoratti <vmoratti@gmail.com>
2026-07-17 13:20:21 +0100

    Update tests for isProperFraction function
    Added a test case for zero numerator and removed duplicate test case.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,15 +4,17 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
+// Special case: numerator is zero
+test(`should return true when numerator is zero `, () => {
+  expect(isProperFraction(0, 2)).toEqual(true);
+});
+
 // Special case: denominator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
 
-// Special case: numerator is zero
-test(`should return true when numerator is zero `, () => {
-  expect(isProperFraction(0, 2)).toEqual(true);
-});
+
 
 // numerator and denominator are both zero
 test(`should return false when numerator and denominator are both zero`, () => {
