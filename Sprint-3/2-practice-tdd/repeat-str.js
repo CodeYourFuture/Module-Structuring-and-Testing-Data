bf23461c88959b23d6805e52aad2084fd81b8fe5
vmoratti <vmoratti@gmail.com>
2026-07-17 13:36:15 +0100

    Update comment to clarify implementation requirements

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,5 @@
 function repeatStr(str, num) {
- // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
   //return "hellohellohello";
   var result = "";
