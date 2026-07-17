822c364aa8b464f9adc73d2e378eb2ffebf95fae
vmoratti <vmoratti@gmail.com>
2026-07-17 17:41:38 +0100

    rewrite code

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,20 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+function repeatStr(str, num) {
+  var result = "";
+  var i;
+  if (num < 0) {
+    throw new Error("negative numbers are not valid");
+  } else if (num === 0) {
+    result = "";
+  } else {
+    for (i = 0; i < num; i++) {
+      result += str;
+    }
+  }
+  return result;
 }
+
+//console.log(repeatStr("hello", -1));
+//console.log(repeatStr("hello", 0));
+//console.log(repeatStr("hello", 5))
 
 module.exports = repeatStr;
