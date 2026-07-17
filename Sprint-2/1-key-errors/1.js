3d919010b90afe6f1838ebc8d80fd850b451df94
d-odumosu <dt.odumosu@gmail.com>
2026-07-17 20:18:36 +0100

    fixed my explanation of the code on the use of const and let

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,9 +2,7 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-/* - The function will throw a syntax error because we have redeclared the parameter variable "decimalNumber", and we have
- also used const for declaring percentage, percentage will change when we call the function with different arguments so it should
- be declared using let.
+/* - The function will throw a syntax error because we have redeclared the parameter variable "decimalNumber".
  - we are console logging the variable decimalNumber outside the function - the variable lives only inside the function
  */
 
@@ -21,10 +19,13 @@
 
 // =============> write your explanation here
 /*I will remove line 14 - a redeclaration of the parameter variable, we will not reassign it, we will get the decimal
- number by calling the function with an argument and we will console.log the function call*/
+ number by calling the function with an argument, and we will console.log the function call*/
 // Finally, correct the code to fix the problem
 // =============> write your new code here
-function convertToPercentage(decimalNumber){
-  return `${decimalNumber * 100}%`;
+function convertToPercentage(decimalNumber) {
+    decimalNumber = 0.5;
+    const percentage = `${decimalNumber * 100}%`;
+    return percentage;
 }
+
 console.log(convertToPercentage(0.5))
