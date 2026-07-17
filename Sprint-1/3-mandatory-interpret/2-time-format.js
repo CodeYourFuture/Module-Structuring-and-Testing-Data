9ec9ff578a67a151237309192591c2cc62466f95
Maryam Janjua <maryamjanjua65@gmail.com>
2026-07-17 02:53:49 +0100

    Updatted movie Duration Formatiing

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,12 +1,29 @@
 //const movieLength = 8784; // length of movie in seconds
-const movieLength = 9893; 
-const remainingSeconds = movieLength % 60;
+//const movieLength = 9893; 
+//const movieLength = 223;
+//const movieLength = 50;
+//const movieLength = 345;
+//const movieLength = 600;
+//const movieLength = 400;
+//const movieLength = 90;
+//const movieLength = 189;
+function MovieFormatting(num){
+    if (num < 10){
+      var result =  0 + num.toString();
+    }
+    else{
+        return num.toString();
+    }
+    return result;
+}
+
+const movieLength = 800;
+const remainingSeconds = movieLength % 60; 
 const totalMinutes = (movieLength - remainingSeconds) / 60;
 
 const remainingMinutes = totalMinutes % 60;
 const totalHours = (totalMinutes - remainingMinutes) / 60;
-
-const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
+const result = `${MovieFormatting(totalHours)}:${MovieFormatting(remainingMinutes)}:${MovieFormatting(remainingSeconds)}`;
 console.log(result);
 
 // For the piece of code above, read the code and then answer the following questions
@@ -26,7 +43,10 @@ console.log(result);
 Firstly it will evaluate bracket i.e (movieLength - remainingSeconds) and then divide the value by 60.
 */
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-// The result represents the how long the movie is, in hours, minutes and seconds. It can named as MovieDuration.
+// The result represents the how long the movie is, in hours, minutes and seconds. It can named as "Duration".
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-// Yes, I changed the value of movieLength and it worked. The movieLength is the variable used in calculating other 
-// values and to evaluate the total length of movie.
+// Yes, the code works for different values of movieLength and does not produce any errors. However, it does not always format 
+// the output correctly. If the hours, minutes, or seconds are less than 10(e.g 5, 7), they are displayed as a single 
+// digit instead of two digits. The standard way to display a movie duration is 00:00:00, so I created a function that
+// adds a leading 0 when a value is less than 10. Otherwise, it returns the value as a string.
+// After creating a function, I test my code with multiple values of movieLength and it passed all the test cases.
