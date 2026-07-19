a3b92600682c8dbd4a01f26f48b4de3cc25a0950
d-odumosu <dt.odumosu@gmail.com>
2026-07-19 19:34:33 +0100

    completed jest testing on count test and implemented the function

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,18 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
-}
+  if(stringOfCharacters === "" || findCharacter === ""){
+    throw new Error("string an char are not given")
+  }
 
+  const str = stringOfCharacters.split("")
+  let count = 0;
+  for(const char of str){
+    if(char === findCharacter){
+      count++
+    }
+  }
+    return count
+
+}
+countChar("aeioyuyuuiiiiu", "i")
 module.exports = countChar;
+//push char into a new array and count the lenght on new array return the lenght
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -19,6 +19,14 @@ test("should count multiple occurrences of a character", () => {
 
 // Scenario: No Occurrences
 // Given the input string `str`,
+
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+
+test("should return 0 when no occurrence of the char", () => {
+  const str = "aeiou";
+  const char = "b";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+})
