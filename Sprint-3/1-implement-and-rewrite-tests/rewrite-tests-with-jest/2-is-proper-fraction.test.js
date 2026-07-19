09fe227d8b7431b1dd4b54e583845fed63595b9c
d-odumosu <dt.odumosu@gmail.com>
2026-07-19 15:23:54 +0100

    all jest tests pass in 2-is-proper-fraction.js

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -2,9 +2,32 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
-// TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
 // Special case: numerator is zero
 test(`should return false when denominator is zero`, () => {
-  expect(isProperFraction(1, 0)).toEqual(false);
+  expect(isProperFraction(1,2 )).toEqual(true);
+  expect(isProperFraction(2,1)).toEqual(false);
+  expect(isProperFraction(5,5)).toEqual(false);
+  expect(isProperFraction(0,5)).toEqual(true);
+  expect(isProperFraction(5,0)).toEqual(false);
+  expect(isProperFraction(0,0)).toEqual(false);
+  expect(isProperFraction(-1,2)).toEqual(true);
+  expect(isProperFraction(1,-2)).toEqual(false);
+  expect(isProperFraction(-2,-1)).toEqual(true);
+  expect(isProperFraction(-1,-2)).toEqual(false);
+  expect(isProperFraction(0.5,1)).toEqual(true);
+  expect(isProperFraction(1.5,1)).toEqual(false);
+  expect(isProperFraction(Infinity,2)).toEqual(false);
+  expect(isProperFraction(2,Infinity)).toEqual(false);
+  expect(isProperFraction(NaN,2)).toEqual(false)
+  expect(isProperFraction(2,NaN)).toEqual(false);
+  expect(isProperFraction(999999999, 1000000000)).toEqual(true)
+
+
+
+
+
+
+
+
 });
