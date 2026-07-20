3dfbd3711178c0b37dc811a3b2d4d37a9e977520
cywong <zenobiatony@gmail.com>
2026-07-20 10:28:22 +0100

    Fix variable declaration for character iteration

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,6 +1,6 @@
 function countChar(stringOfCharacters, findCharacter) {
     let count = 0;
-    let char = "";
+
     for (let char of stringOfCharacters) {
         if (char === findCharacter) {
             count++;
