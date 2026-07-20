8706d73f3175ef8b3f00105f4b2504e0997f2bc6
Dipa Sarker <dipasarker696@gmail.com>
2026-07-20 11:28:12 +0100

    fixed if and else if to make the function more cleaner

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -3,13 +3,14 @@ function getOrdinalNumber(num) {
   const lastTwoDigits = num % 100;
   if (lastDigit === 1 && lastTwoDigits !== 11) {
     return `${num}st`;
-  } else if (lastDigit === 2 && lastTwoDigits !== 12) {
-    return `${num}nd`;
-  } else if (lastDigit === 3 && lastTwoDigits !== 13) {
-    return `${num}rd`;
-  } else {
-    return `${num}th`;
   }
+  if (lastDigit === 2 && lastTwoDigits !== 12) {
+    return `${num}nd`;
+  }
+  if (lastDigit === 3 && lastTwoDigits !== 13) {
+    return `${num}rd`;
+  }
+  return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
