db19ecb63bdc32859d1851f2f5e7994c952ed786
Tobias <amaechinatobias@gmail.com>
2026-07-20 20:08:03 +0100

    update the code to valide the input first thereby failing fast

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,11 +1,13 @@
 function repeatStr(str,count) {
+  // validate input first
+  if (!Number.isInteger(count) || count < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
   let result = "";
   for (let i = 0; i < count; i++) {
     result += str;
   }
-  if (count < 0) {
-    throw new Error("Count must be a non-negative integer");
-  }
+  
   return result;
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
