766e9b5827e60c56af8ff8737359562d1ccf8c16
Maryam Janjua <maryamjanjua65@gmail.com>
2026-07-20 20:56:41 +0100

    Update function for 3-to-pounds.js

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,21 +4,29 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
-function ConversionPenceToPounds(penceString){
+function toPounds(penceString){
     const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
-    paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-    pounds = paddedPenceNumberString.substring( 0,paddedPenceNumberString.length - 2);
-    paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-    pounds = paddedPenceNumberString.substring( 0,paddedPenceNumberString.length - 2);
+    const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    const pounds = paddedPenceNumberString.substring( 0,paddedPenceNumberString.length - 2);
     const pence = paddedPenceNumberString
     .substring(paddedPenceNumberString.length - 2)
     .padEnd(2, "0");
     const result = `£${pounds}.${pence}`;
     return result;
 }
-const pencestring1 = ConversionPenceToPounds("663p");
-const pencestring2 = ConversionPenceToPounds("98p");
-const pencestring3 = ConversionPenceToPounds("986p");
-console.log(pencestring1);
-console.log(pencestring2);
-console.log(pencestring3);
+const penceString1 = toPounds("663p");
+const penceString2 = toPounds("98p");
+const penceString3 = toPounds("986p");
+const penceString4 = toPounds("9p");
+const penceString5 = toPounds("15p");
+const penceString6 = toPounds("100p");
+const penceString7 = toPounds("1p");
+
+
+console.log(penceString1);
+console.log(penceString2);
+console.log(penceString3);
+console.log(penceString4);
+console.log(penceString5);
+console.log(penceString6);
+console.log(penceString7);
