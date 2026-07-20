b04e9e7baf828ed5f5b13c4f92fee1f8c494e19d
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2026-07-20 10:25:58 +0100

    Implement passwordValidator with all 6 rules and add tests for each rule

--- Sprint-3/4-stretch/password-validator.js
+++ Sprint-3/4-stretch/password-validator.js
@@ -1,6 +1,13 @@
+const previousPasswords = ["Pass1!", "Hello1#", "Secure1$"];
+
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+  if (password.length < 5) return false;
+  if (!/[A-Z]/.test(password)) return false;
+  if (!/[a-z]/.test(password)) return false;
+  if (!/[0-9]/.test(password)) return false;
+  if (!/[!#$%.\*&]/.test(password)) return false;
+  if (previousPasswords.includes(password)) return false;
+  return true;
 }
 
-
-module.exports = passwordValidator;
+module.exports = { passwordValidator, previousPasswords };
