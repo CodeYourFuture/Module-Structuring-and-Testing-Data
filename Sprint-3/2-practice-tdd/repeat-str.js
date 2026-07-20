248d2a4d18a5fff87a3d4d7b8cfce6fb098ead43
d-odumosu <dt.odumosu@gmail.com>
2026-07-20 20:01:46 +0100

    completed jest testing o and implemented the function as required

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,24 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
-}
+function repeatStr(word, wordCounter) {
+    // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+    // The goal is to re-implement that function, not to use it.
+    let count = 0;
+    let repeatedString = []
 
+    if (wordCounter < 0) { // rejects negative numbers
+        throw new Error("Enter a positive number")
+    }
+    if(wordCounter === 0){
+        return ""
+    }
+    if(wordCounter === 1){
+        return word
+    }
+    if(wordCounter > 1){
+        while(count < wordCounter){
+            count++
+            repeatedString.push(word)
+        }
+    }
+return repeatedString.join("")
+}
 module.exports = repeatStr;
--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,13 +20,32 @@ test("should repeat the string count times", () => {
 // Given a target string `str` and a `count` equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
+test("should repeat the string 1 time", () =>{
+  const str = "name";
+  const count = 1;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("name")
+})
 
 // Case: Handle count of 0:
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
+test("should return an empty string", () =>{
+  const str = "name";
+  const count = 0;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("")
+})
 
 // Case: Handle negative count:
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
+// Case: Handle negative count:
+// Given a target string `str` and a negative integer `count`,
+// When the repeatStr function is called with these inputs,
+// Then it should throw an error, as negative counts are not valid.
+test("throws on negative number", () => {
+  expect(() => repeatStr("dami",-5)).toThrow("Enter a positive number");
+});
