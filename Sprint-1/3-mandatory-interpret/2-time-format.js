8a8a3f71ce2d8bec91485079ab34cc7fb2df1eb4
Enice-Codes <mutandaeunice@94gmail.com>
2026-07-20 20:07:38 +0200

    modified some answers

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,12 +1,12 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 3609; // length of movie in seconds
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
 
 const remainingMinutes = totalMinutes % 60;
 const totalHours = (totalMinutes - remainingMinutes) / 60;
 
-const result = `${2,359}:${2,398}:${146,4}`;
-console.log(movieLengthTime(result));       
+const formattedMovieTime = `${totalHours}:${String(remainingMinutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+console.log(formattedMovieTime);    
 
 
 // For the piece of code above, read the code and then answer the following questions
@@ -24,7 +24,7 @@ console.log(movieLengthTime(result));
 // it means that the movie hour  is being calculated. totalminutes minus remainingseconds divided by 60.
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 // the variable result represents the total length it takes to watch the movie in hours and minutes.
-//Better name is MovieLengthTime.
+//Better name is formattedMovieTime.
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
 // Yes, this code will work for different values of movieLength because it calculates the total hours, minutes and seconds based on the length of the movie in seconds. 
 // The code will correctly handle any length of movie in seconds and convert it to the appropriate format.
