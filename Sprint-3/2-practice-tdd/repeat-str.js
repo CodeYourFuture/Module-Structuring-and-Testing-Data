38c0d903df63c7b5fd39c653adc1790a49c37468
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-20 17:18:08 +0100

    implement repeat string function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,44 @@
-function repeatStr() {
+function repeatStr(str, count) {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+
+  //handle negative count
+  if (count < 0) {
+    throw new Error("Count cannot be negative");
+  }
+
+  //with valid count input
+  let output = "";
+
+  for (let i = 0; i < count; i++) {
+    output = output + str;
+  }
+
+  return output;
 }
 
 module.exports = repeatStr;
+
+// Given a target string `str` and a positive integer `count`,
+// When the repeatStr function is called with these inputs,
+// Then it should:
+
+// Case: handle multiple repetitions:
+// Given a target string `str` and a positive integer `count` greater than 1,
+// When the repeatStr function is called with these inputs,
+// Then it should return a string that contains the original `str` repeated `count` times.
+
+// Case: handle count of 1:
+// Given a target string `str` and a `count` equal to 1,
+// When the repeatStr function is called with these inputs,
+// Then it should return the original `str` without repetition.
+
+// Case: Handle count of 0:
+// Given a target string `str` and a `count` equal to 0,
+// When the repeatStr function is called with these inputs,
+// Then it should return an empty string.
+
+// Case: Handle negative count:
+// Given a target string `str` and a negative integer `count`,
+// When the repeatStr function is called with these inputs,
+// Then it should throw an error, as negative counts are not valid.
