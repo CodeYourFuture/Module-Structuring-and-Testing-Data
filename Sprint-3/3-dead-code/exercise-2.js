91bb5c8fc5f07ce08c52fa60478489f0e3e8793d
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2026-07-20 10:25:45 +0100

    Remove dead code from exercise-2: unused capitalisedPets array and logPets function

--- Sprint-3/3-dead-code/exercise-2.js
+++ Sprint-3/3-dead-code/exercise-2.js
@@ -2,12 +2,6 @@
 // The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
 
 const pets = ["parrot", "hamster", "horse", "dog", "hamster", "cat", "hamster"];
-const capitalisedPets = pets.map((pet) => pet.toUpperCase());
-const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
-
-function logPets(petsArr) {
-  petsArr.forEach((pet) => console.log(pet));
-}
 
 function countAndCapitalisePets(petsArr) {
   const petCount = {};
@@ -23,6 +17,7 @@ function countAndCapitalisePets(petsArr) {
   return petCount;
 }
 
+const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
 const countedPetsStartingWithH = countAndCapitalisePets(petsStartingWithH);
 
 console.log(countedPetsStartingWithH); // { 'HAMSTER': 3, 'HORSE': 1 } <- Final console log
