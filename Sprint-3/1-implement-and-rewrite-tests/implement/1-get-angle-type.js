c7c95380eeb50ab2552be3f524a3553e448e895c
HM-127BTY <mills.hugh@yahoo.com>
2026-07-20 15:00:16 +0100

    Work on sprint 3 1-get-angle-type put in code using "if" function.

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -16,6 +16,17 @@
 
 function getAngleType(angle) {
   // TODO: Implement this function
+  if (angle < 90) {
+    return "Acute angle"
+  } else if (angle ===90) {
+    return "Right angle"
+  } else if (angle < 180) {
+    return "Obtuse angle"
+  } else if (angle ===180) {
+    return "Straight angle"
+  } else if (angle < 360) {
+    return "Invalid angle"
+  }
 }
 
 // The line below allows us to load the getAngleType function into tests in other files.
