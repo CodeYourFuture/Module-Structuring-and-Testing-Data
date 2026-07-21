3590fb10720aa805043cb4e56b7987f813e5d8d5
russom <russom.g.meskel@gmail.cpm>
2026-07-21 11:27:07 +0100

    An if statement added to the function to check if num is not negative or not a whole number or not a string first thing

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,15 +1,18 @@
 function getOrdinalNumber(num) {
+  if (num <= 0 || !Number.isInteger(num) || typeof num !== "number") {
+    return "error";
+  }
   const lastDigit = num % 10;
   const lastTwoDigits = num % 100;
   if (lastDigit === 1 && lastTwoDigits !== 11) {
     return `${num}st`;
   }
-    if (lastDigit === 2 && lastTwoDigits !== 12) {
+  if (lastDigit === 2 && lastTwoDigits !== 12) {
     return `${num}nd`;
   }
-   if (lastDigit === 3 && lastTwoDigits !== 13) {
-     return `${num}rd`;
-   }
+  if (lastDigit === 3 && lastTwoDigits !== 13) {
+    return `${num}rd`;
+  }
   return `${num}th`;
 }
 
