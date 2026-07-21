74d5e9f35d341bcd92843a4c5f9717575a264a38
russom <russom.g.meskel@gmail.cpm>
2026-07-21 10:59:16 +0100

    Function for first test case fir checking numbers ending with st implemented.

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,10 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastDigit = num % 10;
+  const lastTwoDigits = num % 100;
+  if (lastDigit === 1 && lastTwoDigits !== 11) {
+    return `${num}st`;
+  }
+  return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
