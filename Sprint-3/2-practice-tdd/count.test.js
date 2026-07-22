a57e153d37cb2654a9bd2e77d1537245bb120bac
eyob tech <eyobfitsum07@gmail.com>
2026-07-22 23:51:57 +0100

    Implement countChar with tests

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,14 +1,14 @@
 // implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
-// Given a string `str` and a single character `char` to search for,
+// Given a string str and a single character char to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
 
 // Scenario: Multiple Occurrences
-// Given the input string `str`,
-// And a character `char` that occurs one or more times in `str` (e.g., 'a' in 'aaaaa'),
+// Given the input string str,
+// And a character char that occurs one or more times in str (e.g., 'a' in 'aaaaa'),
 // When the function is called with these inputs,
-// Then it should correctly count occurrences of `char`.
+// Then it should correctly count occurrences of char.
 
 test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
@@ -18,7 +18,14 @@ test("should count multiple occurrences of a character", () => {
 });
 
 // Scenario: No Occurrences
-// Given the input string `str`,
-// And a character `char` that does not exist within `str`.
+// Given the input string str,
+// And a character char that does not exist within str.
 // When the function is called with these inputs,
-// Then it should return 0, indicating that no occurrences of `char` were found.
+// Then it should return 0, indicating that no occurrences of char were found.
+
+test("should return 0 when the character does not occur", () => {
+  const str = "aaaaa";
+  const char = "b";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
