4b91f08fd0272edd4187aed2f1e3b53dd5dbbb88
vmoratti <vmoratti@gmail.com>
2026-07-22 12:18:33 +0100

    remove 'TODO' comments
    Removed TODO comment and added a test case for zero denominator.

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -2,7 +2,6 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const isProperFraction = require("../implement/2-is-proper-fraction");
 
-// TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
 // Special case: denominator is zero
 test(`should return false when denominator is zero`, () => {
