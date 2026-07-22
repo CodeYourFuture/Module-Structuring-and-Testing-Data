6dace3cc8ee5ebee56b9ca8b52a457b17137eabd
vmoratti <vmoratti@gmail.com>
2026-07-22 12:44:22 +0100

    remove space after 'count' variable

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -2,7 +2,7 @@ function countChar(stringOfCharacters, findCharacter) {
     let count = 0;
     for (let char of stringOfCharacters) {
         if (char === findCharacter) {
-            count ++;
+            count++;
         }
     }
     return count
