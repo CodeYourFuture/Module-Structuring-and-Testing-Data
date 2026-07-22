514a13eaea95c15fee85b343b3f455a5f12a0692
eyob tech <eyobfitsum07@gmail.com>
2026-07-22 23:52:21 +0100

    Implement repeatStr with tests

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,15 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+function repeatStr(str, count) {
+  // Your implementation of this function must not call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat)
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+  if (count < 0) {
+    throw new Error("count must not be negative");
+  }
+
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatStr;
