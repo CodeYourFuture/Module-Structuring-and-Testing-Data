a5afe5427c78b5b3e4aab6dc0335db59d9d8305f
TaliaK <talia_k@live.co.uk>
2026-07-23 13:22:09 +0100

    solved debug 2.js

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,8 +1,7 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
-
+// =============> Num has been declared as 103. The function converts the number to a string and then returns the last number of that string. The return statement specifies the last digit of num which is already declared as 103 - so no matter what number someone puts in the temperate literals within the console.log statements 3 is always going to be returned.
 const num = 103;
 
 function getLastDigit() {
@@ -14,11 +13,15 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> The last digit of 42 is 3 / The last digit of 105 is 3 / The last digit of 806 is 3 /
+
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> As predicted, 3 is always going to be returned as the num was specified as 103 and the parameter within the function was not declared. To fix this const num = 103 needs to be removed and the variable num needs to be placed as a parameter within the function getLastDigit
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+// =============>
+  function getLastDigit(num) {
+return num.toString().slice(-1);
+  }
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
