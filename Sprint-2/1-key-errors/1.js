d13b10fbc8717b5ebad37e553607dc768c378465
TaliaK <talia_k@live.co.uk>
2026-07-23 11:15:08 +0100

    solved error 1.js

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,28 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> There are two errors I notice here. The first is that, similarly to the first exercise, the variable decimalNumber is being declared twice - first as the parameter of the function and then again with const decimalNumber.
+// JavaScript does not allow for a variable to be declared more than once.
+// The second error is the console.log section where it attempts to access a variable inside the function which will not work. The function is what needs to be called.
 
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
-
   return percentage;
 }
 
 console.log(decimalNumber);
 
-// =============> write your explanation here
+// =============> As the decimalNumber variable has already been declared within the function I removed the second const declaration as it is unnecessary.
+// I changed the console.log section to call the function and not the variable. I then tested this and got the expected result.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.3));
