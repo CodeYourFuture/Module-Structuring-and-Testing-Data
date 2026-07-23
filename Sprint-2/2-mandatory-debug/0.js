2873954211f9fdb700c0b60dd87b94aadfd956e0
TaliaK <talia_k@live.co.uk>
2026-07-23 11:33:28 +0100

    solved debug 0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> Right now the first console.log inside the function prints 320 - but the function does not have a return statement, so when it is used inside the template literal it will show as undefined. The code should use return a * b instead of console.log(a * b)
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,12 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> This code returned "The result of multiplying 10 and 32 is undefined" as predicted because there is no return statement within the function.
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> 
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  console.log(`The result of multiplying 10 and 32 is ${multiply(10, 21)}`)
