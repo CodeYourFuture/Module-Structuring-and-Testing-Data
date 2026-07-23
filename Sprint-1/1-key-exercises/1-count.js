3782ccbf6ab1640989b6cb3f3b852fe617e1df73
gideondefar <gideondefar@gmail.com>
2026-07-23 03:22:34 +0100

    docs: add multi-line comment explaining line 3 assignment

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+/* 
+   Line 3 is an assignment operation. The right side evaluates first.
+   The assignment operator (=) then overwrites the old value with the new result.
+*/
