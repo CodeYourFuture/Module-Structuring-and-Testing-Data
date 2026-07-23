a95dc1287c4e2c028c4245e00c382fe504df7262
TaliaK <talia_k@live.co.uk>
2026-07-23 15:11:34 +0100

    completed implement time-format.js

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -15,24 +15,26 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(61));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> Pad is called 3 times.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> 0 is the value assigned to num as the first pad is totalhours which calculates to be 0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> "00" - this is because it has been stated within the function that if the value's length is less than 2 which 0 is, then add another 0 at the beginning ("0" + numString;). Also toString converts 0 from a number to a string which is why I included ""
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> the last pad is remainingSeconds which is 1 so the value assigned to num is 1
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// =============> "01" - same logic as quested c. Also as "01" is not shorter than length of 2 the loop closes and doesn't run again
