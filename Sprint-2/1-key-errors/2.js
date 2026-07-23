150312b739b9381657f2d41b1e91eef128b73588
TaliaK <talia_k@live.co.uk>
2026-07-23 11:23:52 +0100

    solved error 2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,23 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
-
+// =============> The first error I see is the number 3 as the parameter inside the function. This is the error that JavaScript will flag as the parameter needs to be a variable name.
+// The second error I noticed is num + num - what is num? JavaScript will not know as it has not been declared.
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
 
-// =============> explain this error message here
+// =============> SyntaxError: Unexpected number
+
+// =============> As I expected JavaScript has flagged the use of a number as the function parameter, as it needs to be a variable name. 
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+// =============> 
+
+function square(num) {
+return num * num;
+}
 
 
