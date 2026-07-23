6c07a3eb730cba3ed5121559f56dbfb0ec567ff4
TaliaK <talia_k@live.co.uk>
2026-07-23 11:48:15 +0100

    solved error 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> As there is a ; after return this means this function will return 'undefined' as it will not be able to run a + b. There is no value after return. The console.log statement will be The sum of 10 and 32 is undefined.
 
 function sum(a, b) {
   return;
@@ -8,6 +8,9 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> As predicted the console.log statement returned The sum of 10 and 32 is undefined. To fix this I will remove the ; after return so that the function can return the sum of a + b
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> 
+function sum(a, b) {
+  return a + b;
+}
