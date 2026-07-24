c922c6950cf282deaea8735a9debbb00691643fc
Francesco Romano Monda <fr.monda@gmail.com>
2026-07-24 16:26:14 +0100

    Implement countChar, repeatStr, and getOrdinalNumber functions with TDD

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,15 @@
 function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+  if (count < 0) {
+    throw new Error("invalid");
+  }
+
+  let result = "";
+
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+
+  return result;
 }
 
 module.exports = repeatStr;
