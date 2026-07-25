822fa7a8e83db5c7e85f65eaa8b27c2945001cae
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-07-25 16:57:31 +0100

    Clean up sayHello function and fix logging
    Removed unreachable code and redundant variable.

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,27 +1,3 @@
-// Find the instances of unreachable and redundant code - remove them!
-// The sayHello function should continue to work for any reasonable input it's given.
-
-let testName = "Jerry";
-const greeting = "hello";
-
-function sayHello(greeting, name) {
-  const greetingStr = greeting + ", " + name + "!";
-  return `${greeting}, ${name}!`;
-  console.log(greetingStr);
-}
-
-testName = "Aman";
-
-const greetingMessage = sayHello(greeting, testName);
-
-console.log(greetingMessage); // 'hello, Aman!'
-
-//I have removed "const greetingStr = greeting + ", " + name + "!";" because it serves the same purpose as "return `${greeting}, ${name}!`;",
-//making it redundant.
-
-//Also, "console.log(greetingStr);" comes after  "return `${greeting}, ${name}!`;", making it unreachable inside the function, since eveything
-//after "return" is ignored.
-
 
 let testName = "Jerry";
 const greeting = "hello";
@@ -34,4 +10,4 @@ testName = "Khaliun";
 
 const greetingMessage = sayHello(greeting, testName);
 
-console.log(greetingMessage); // "hello, Khaliun!"
+console.log(greetingMessage); 
