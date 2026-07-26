b136e3493b616b7878adef215e9d62241c303150
ChinweP <exponentialresults1973@gmail.com>
2026-07-26 22:11:45 +0100

    Add ordinal number logic and reorder conditions to pass all tests

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,30 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastDigit = num % 10;
+  const lastTwoDigits = num % 100;
+
+   // Special cases: 11, 12, 13
+  if (lastTwoDigits === 11 || lastTwoDigits === 12 || lastTwoDigits === 13) {
+    return `${num}th`;
+  }
+
+// Numbers ending in 1
+  if (lastDigit === 1) {
+    return `${num}st`;
+  }
+
+  // Numbers ending in 2
+  if (lastDigit === 2) {
+    return `${num}nd`;
+  }
+
+  // Numbers ending in 3
+  if (lastDigit === 3) {
+    return `${num}rd`;
+  }
+
+  // Everything else
+  return `${num}th`;
 }
+
 
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,28 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+
+// Case 2: Numbers ending with 2 (but not 12)
+test("should append 'nd' for numbers ending with 2, except those ending with 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(102)).toEqual("102nd");
+});
+
+// Case 3: Numbers ending with 3 (but not 13)
+test("should append 'rd' for numbers ending with 3, except those ending with 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(503)).toEqual("503rd");
+});
+
+// Case 4: All other numbers → 'th'
+test("should append 'th' for all other numbers", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(11)).toEqual("11th"); // special case
+  expect(getOrdinalNumber(12)).toEqual("12th"); // special case
+  expect(getOrdinalNumber(13)).toEqual("13th"); // special case
+  expect(getOrdinalNumber(100)).toEqual("100th");
+});
+
