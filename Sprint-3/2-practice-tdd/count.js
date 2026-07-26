dd13d1afab547ffae16ae141bdfb30ad19d07d79
anitahy73 <anita.hy73@gmail.com>
2026-07-26 16:55:00 +0100

    Refactor countChar to use a loop for counting

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return stringOfCharacters.split(findCharacter).length - 1;
+  let count = 0;
+
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countChar;
