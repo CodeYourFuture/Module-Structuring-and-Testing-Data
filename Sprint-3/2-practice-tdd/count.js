9b176d2ef642f26f5dbbbf5918a310cf4bb634c5
d-odumosu <dt.odumosu@gmail.com>
2026-07-27 19:04:54 +0100

    refactored: removed .split on the string and iterated directly.

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,15 +1,15 @@
 function countChar(stringOfCharacters, findCharacter) {
-  if(stringOfCharacters === "" || findCharacter === ""){
-    throw new Error("string an char are not given")
+  if (stringOfCharacters === "" || findCharacter === "") {
+    throw new Error("string an char are not given");
   }
 
-  const str = stringOfCharacters.split("")
   let count = 0;
-  for(const char of str){
-    if(char === findCharacter){
-      count++
+  for (const char of stringOfCharacters) {
+    if (char === findCharacter) {
+      count++;
     }
   }
-    return count
+  return count;
 }
+console.log(countChar("aaaaaae", "a"));
 module.exports = countChar;
