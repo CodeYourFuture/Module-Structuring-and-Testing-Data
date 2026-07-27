1ede9b98767f965006a75f7afb12669ee9e1971e
d-odumosu <dt.odumosu@gmail.com>
2026-07-27 19:24:39 +0100

    fixed the convert to percentage function.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -23,9 +23,7 @@
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 function convertToPercentage(decimalNumber) {
-    decimalNumber = 0.5;
-    const percentage = `${decimalNumber * 100}%`;
-    return percentage;
+  return `${decimalNumber * 100}%`;
 }
-
-console.log(convertToPercentage(0.5))
+// i have also removed the reassignment of decimal number "decimalNumber = 0.5;" so we can pass any number in the function
+console.log(convertToPercentage(1.5));
