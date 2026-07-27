2aa80fd05e1f226487b4636129e386faced2b1ed
d-odumosu <dt.odumosu@gmail.com>
2026-07-27 19:12:37 +0100

    refactored: variable names changed to be more descriptive

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,31 +1,26 @@
 function getOrdinalNumber(num) {
-    const numberStr1 = String(num).slice(-1)
-    const numberStr2 = String(num).slice(-2)
+  const lastDigitString = String(num).slice(-1);
+  const lastTwoDiditString = String(num).slice(-2);
 
-    let number1 = Number(numberStr1)
-    let number2 = Number(numberStr2)
+  let lastDigit = Number(lastDigitString);
+  let lastTwoDigit = Number(lastTwoDiditString);
 
-    if (number2 === 11 || number2 === 12 || number2 === 13) {
-        return `${num}th`
+  if (lastTwoDigit === 11 || lastTwoDigit === 12 || lastTwoDigit === 13) {
+    return `${num}th`;
+  } else {
+    switch (lastDigit) {
+      case 1:
+        return `${num}st`;
+        break;
+      case 2:
+        return `${num}nd`;
+        break;
+      case 3:
+        return `${num}rd`;
+        break;
+      default:
+        return `${num}th`;
     }
-    else {
-
-        switch (number1) {
-            case 1:
-                return`${num}st`
-                break;
-            case 2:
-                return `${num}nd`
-                break;
-            case 3:
-                return `${num}rd`
-                break;
-            default:
-                return`${num}th`;
-        }
-    }
-
+  }
 }
-
-
 module.exports = getOrdinalNumber;
