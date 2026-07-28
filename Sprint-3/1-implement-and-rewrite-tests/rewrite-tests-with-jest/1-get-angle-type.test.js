0b6ad14b031ccbc6034bc4511346ec99602eff6f
Sahid Hussain <hussainsahid980@gmail.com>
2026-07-28 22:18:21 +0100

    get angle test done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -39,7 +39,7 @@ test(`should return "reflex angle" if angle is more than 180 and less than 360`,
 // tes(`should return "reflex angle" when straight is  `);
 // Case 6: Invalid angles
 test(`should return "invalid angle" if any of them don't match`, () => {
-  expect(getAngleType(380)).toEqual("Invalid");
+  expect(getAngleType(385)).toEqual("Invalid");
   expect(getAngleType(430)).toEqual("Invalid");
   expect(getAngleType(390)).toEqual("Invalid");
 });
