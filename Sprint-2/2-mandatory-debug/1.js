5ad1764b5c529fe9ee70657003e2008d65989a08
christelleb <christelleboten@gmail.com>
2026-07-28 17:41:40 +0100

    mandatory debug 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> write your prediction here: Line 5 and line 6 are not connected so as they are separated by ';', there is nothing that the function is returning.
+
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here: We have an undefined value for the sum of a and b because we are not returning anything.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
