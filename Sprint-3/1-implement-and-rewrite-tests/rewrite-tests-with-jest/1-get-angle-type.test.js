d6b8042336aadca2823da80af9caeea56a6acaa2
Sahid Hussain <hussainsahid980@gmail.com>
2026-07-28 22:09:39 +0100

    get angle test done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -6,7 +6,7 @@ const getAngleType = require("../implement/1-get-angle-type");
 // including boundary and invalid cases.
 
 // Case 1: Acute angles
-test(`should return "Acute angle" when (0 < angle < 90)`, () => {
+test(`should return "Acute angle" when ( angle > 0 and angle < 90)`, () => {
   // Test various acute angles, including boundary cases
   expect(getAngleType(1)).toEqual("Acute angle");
   expect(getAngleType(45)).toEqual("Acute angle");
@@ -14,7 +14,32 @@ test(`should return "Acute angle" when (0 < angle < 90)`, () => {
 });
 
 // Case 2: Right angle
+test(`should return "Right angle" when angle exactly equals 90`, () => {
+  expect(getAngleType(90)).toEqual("Right angle");
+});
+
 // Case 3: Obtuse angles
+describe(`should return "obtuse angle" when obtuse > 90 and obtuse < 180`, () => {
+  expect(getAngleType(95)).toEqual("Obtuse angle");
+  expect(getAngleType(99)).toEqual("Obtuse angle");
+  expect(getAngleType(105)).toEqual("Obtuse angle");
+});
 // Case 4: Straight angle
+
+test(`should return "straight angle" when straight is equals 180`, () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
 // Case 5: Reflex angles
+test(`should return "reflex angle" if angle is more than 180 and less than 360`, () => {
+  expect(getAngleType(181)).toEqual("Reflex angle");
+  expect(getAngleType(191)).toEqual("Reflex angle");
+  expect(getAngleType(211)).toEqual("Reflex angle");
+});
+
+// tes(`should return "reflex angle" when straight is  `);
 // Case 6: Invalid angles
+test(`should return "invalid angle" if any of them don't match`, () => {
+  expect(getAngleType(380)).toEqual("Invalid");
+  expect(getAngleType(430)).toEqual("Invalid");
+  expect(getAngleType(390)).toEqual("Invalid");
+});
