eac47a42d14a305356d0042995552de415e262a6
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2026-07-29 00:11:03 +0100

    Work on password validator

--- Sprint-3/4-stretch/password-validator.js
+++ Sprint-3/4-stretch/password-validator.js
@@ -1,12 +1,19 @@
 const previousPasswords = ["Pass1!", "Hello1#", "Secure1$"];
 
 function passwordValidator(password) {
-  if (password.length < 5) return false;
-  if (!/[A-Z]/.test(password)) return false;
-  if (!/[a-z]/.test(password)) return false;
-  if (!/[0-9]/.test(password)) return false;
-  if (!/[!#$%.*&]/.test(password)) return false;
-  if (previousPasswords.includes(password)) return false;
+  if (password.length < 5) {
+    return false;
+  } if (!/[A-Z]/.test(password)) {
+    return false;
+  } if (!/[a-z]/.test(password)) {
+    return false;
+  } if (!/[0-9]/.test(password)) {
+    return false;
+  } if (!/[!#$%.\*&]/.test(password)) {
+    return false;
+  } if (previousPasswords.includes(password)) {
+    return false;
+  }
   return true;
 }
 
