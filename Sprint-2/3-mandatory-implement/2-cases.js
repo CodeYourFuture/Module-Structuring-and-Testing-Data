b4147847a8021b88ee1702b787af1f6328c05bb8
christelleb <christelleboten@gmail.com>
2026-07-28 19:19:40 +0100

    Solution of 2-cases,js

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,11 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function upperCaseGroup(sentence){
+    const newSentence = sentence.toUpperCase();
+    return newSentence.replaceAll(" ","_");
+}
+
+console.log(upperCaseGroup("hello there"));
+console.log(upperCaseGroup("lord of the rings"));
