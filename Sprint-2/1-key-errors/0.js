ba77955ac62b4c4197976e0ea2abfa75d510cb63
Gideon Defar <gideondefar@gmail.com>
2026-07-29 06:58:29 +0100

    Fix the SyntaxError and write an explanation of why it happened.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -4,10 +4,17 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// =============> SyntaxError: Identifier 'str' has already been declared, 
+// So In JavaScript, we cannot declare a let variable with the same name as an existing parameter within the same scope. This causes a SyntaxError
+
+// =============> write your new code here
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise("hello"));
