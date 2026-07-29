e7a975e6421f58e6d4f9a81453248e03d84e363b
ChinweP <exponentialresults1973@gmail.com>
2026-07-29 01:40:50 +0100

    Clarify valid angle range in tests

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -38,7 +38,7 @@ test(`should return "Reflex angle" when (180 < angle < 360)`, () => {
 });
 
 // Case 6: Invalid angles
-test(`should return "Invalid angle" for angles outside valid range`, () => {
+test(`should return "Invalid angle" for angles outside valid range (1-359)`, () => {
   expect(getAngleType(-10)).toEqual("Invalid angle");
   expect(getAngleType(0)).toEqual("Invalid angle");
   expect(getAngleType(360)).toEqual("Invalid angle");
