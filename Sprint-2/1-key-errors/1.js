b8bb36c9e3625620718197b3b338d8d94f2ef6f9
Gideon Defar <gideondefar@gmail.com>
2026-07-29 21:17:49 +0100

    Fix duplicate parameter and scope errors

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -5,16 +5,27 @@
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// =============> JavaScript does not allow a parameter and a const variable inside the same scope to have the same name, so it gives an error.
+//console.log(decimalNumber);
+
+//decimalNumber only exists inside the function. It is a local variable, so it cannot be accessed outside the function. This causes a ReferenceError.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+ 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5));
