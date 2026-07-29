bd91f3bdaecc167b95474e65cd9701786f8f3ec9
Sahid Hussain <hussainsahid980@gmail.com>
2026-07-29 11:56:50 +0100

    proper fraction test implementation done

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,3 +8,18 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
+test(`should  return true when denominator is 5`, () => {
+  expect(isProperFraction(2, 5)).toEqual(true);
+});
+
+test(`should return true when denominator is 10`, () => {
+  expect(isProperFraction(4, 10)).toEqual(true);
+});
+
+test(`should return false when denominator is -1`, () => {
+  expect(isProperFraction(3, -1)).toEqual(false);
+});
+
+test(`should return false when denominator is 0`, () => {
+  expect(isProperFraction(5, 0)).toEqual(false);
+});
