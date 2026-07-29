4d2ea3788269ab159d85c9ffc666fcaec28d7cc8
Gideon Defar <gideondefar@gmail.com>
2026-07-29 22:21:03 +0100

    Fix invalid function parameter in square

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,16 +5,22 @@
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
-
+//SyntaxError: Unexpected number
 // =============> explain this error message here
+//A SyntaxError will occur because `3` cannot be used as a function parameter name.
+//Function parameters must be valid variable names, such as `num`.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
 
+console.log(square(3));
