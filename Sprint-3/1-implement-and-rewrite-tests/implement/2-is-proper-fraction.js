4b8bce64f967754b4228dc7301f977fd4fda1b7f
Gideon Defar <gideondefar@gmail.com>
2026-07-31 16:38:07 +0100

    Implement proper fraction function and tests

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -11,7 +11,7 @@
 // execute the code to ensure all tests pass.
 
 function isProperFraction(numerator, denominator) {
-  // TODO: Implement this function
+  return Math.abs(numerator) < Math.abs(denominator);
 }
 
 // The line below allows us to load the isProperFraction function into tests in other files.
@@ -31,3 +31,20 @@ function assertEquals(actualOutput, targetOutput) {
 
 // Example: 1/2 is a proper fraction
 assertEquals(isProperFraction(1, 2), true);
+
+assertEquals(isProperFraction(3, 4), true);
+assertEquals(isProperFraction(-1, 2), true);
+
+// Equal numerator and denominator
+assertEquals(isProperFraction(2, 2), false);
+
+// Improper fractions
+assertEquals(isProperFraction(3, 2), false);
+assertEquals(isProperFraction(5, 3), false);
+assertEquals(isProperFraction(-3, 2), false);
+
+// Zero numerator
+assertEquals(isProperFraction(0, 2), true);
+
+// Negative denominator
+assertEquals(isProperFraction(1, -2), true);
