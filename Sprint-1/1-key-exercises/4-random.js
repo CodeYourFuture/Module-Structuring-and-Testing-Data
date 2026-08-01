c66d6a4eb6df3545ddc227c1035f4b702a77252e
anitahy73 <anita.hy73@gmail.com>
2026-08-01 15:39:42 +0100

    Enhance comments for random number generation logic
    Added detailed comments explaining the random number generation process.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,8 @@ console.log (num);
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Math.random() : Generates a pseudorandom decimal number.  [0, 1)
+// Math.random() * (maximum - minimum + 1) : Scales the random value so its width equals the number of integers from `minimum` to `maximum`. [0, maximum - minimum + 1)                                                   
+// Math.floor(Math.random() * (maximum - minimum + 1)) : Floors the scaled value, producing a random integer starting at 0. Integers in [0, maximum - minimum + 1), i.e. {0, 1, ..., maximum - minimum}
+// Math.floor(Math.random() * (maximum - minimum + 1)) + minimum` : Shifts the previous result upward by `minimum`, producing a random integer between `minimum` and `maximum`, inclusive. Integers in [minimum, maximum + 1), i.e. {minimum, minimum + 1, ..., maximum}
