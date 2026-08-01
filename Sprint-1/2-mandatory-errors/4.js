fb2159a2758adcfb66e98bc9b3b4a2ca182de540
Gideon Defar <gideondefar@gmail.com>
2026-08-01 16:43:10 +0100

    Use consistent time format variable names

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,5 +1,4 @@
-const HourClockTime = "8:53pm";
-const hourClockTime = "20:53";
-
+const clockTime12Hour = "8:53pm";
+const clockTime24Hour = "20:53";
 // Naming convention: In JavaScript, variable names cannot begin with a number. 
 // It would crash instantly with a syntax error:
