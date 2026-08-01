3b81778144b50ea480edf10c6de4cf6da1f12531
mandip sanger <mandipsanger1@gmail.com>
2026-08-01 16:49:26 +0100

    tdd error fix

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -2,6 +2,14 @@ function getOrdinalNumber(num) {
   if (num % 10 === 1 && num % 100 !== 11) {
     return `${num}st`;
   }
-}
+  if (num % 10 === 2 && num % 100 !== 12) {
+    return `${num}nd`;
+  }
 
+  if (num % 10 === 3 && num % 100 !== 13) {
+    return `${num}rd`;
+  }
+
+  return `${num}th`;
+}
 module.exports = getOrdinalNumber;
