ec5846fa281a981c1a6b4d354be7d7ab0889d4c6
Gideon Defar <gideondefar@gmail.com>
2026-08-01 08:24:06 +0100

    Implement repeatStr without repeat method

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,18 @@
-function repeatStr() {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
+// function repeatStr() {
+   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+//   return "hellohellohello";
+// }
+
+// module.exports = repeatStr;
+function repeatStr(string, times) {
+  let result = "";
+
+  for (let i = 0; i < times; i++) {
+    result += string;
+  }
+
+  return result;
 }
 
 module.exports = repeatStr;
