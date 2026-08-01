6ca9d1fbb91667e3f8e3a162145ed5527b597ed5
Monsur Abdulrahman <monsuromotunde.abdulrahman@gmail.com>
2026-08-01 18:53:32 +0100

    Implement character counting and ordinal number formatting; update repeatStr function to avoid using String.prototype.repeat

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,11 @@
 function repeatStr() {
+  
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+  if (count < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
+  return str.repeat(count);
 }
 
 module.exports = repeatStr;
