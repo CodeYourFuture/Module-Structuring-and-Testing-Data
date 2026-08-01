02f9da3d62911a38b60e4497bfbf70afd7e3b24d
HM-127BTY <mills.hugh@yahoo.com>
2026-08-01 15:29:29 +0100

    Work on sprint 3 1-get-angle-type put in code using "if" function.

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,13 @@
+// function countChar(stringOfCharacters, findCharacter) {
+//   return 5
+// }
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let countChar = 0;
+  for (let stringNum = 0; stringNum < stringOfCharacters.length; stringNum++) {
+    if (findCharacter === stringOfCharacters[stringNum]){
+      countChar++
+    }
+  }
+  return countChar
 }
-
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -17,8 +17,29 @@ test("should count multiple occurrences of a character", () => {
   expect(count).toEqual(5);
 });
 
+test("should count multiple occurrences of a character", () => {
+  const str = "aaa  aa";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(5);
+});
+
+test("should count multiple occurrences of a character with random characters", () => {
+  const str = "ajhyabhaakaka";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(6);
+})
+
 // Scenario: No Occurrences
 // Given the input string `str`,
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
+test("should return 0 with no occurrence of the character", () => {
+  const str ="abcd";
+  const char = "e";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
