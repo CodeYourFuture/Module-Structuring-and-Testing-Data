587bb75c9556692b08c68c4d165a73ff22d720a6
mandip sanger <mandipsanger1@gmail.com>
2026-08-01 14:05:18 +0100

    fix errors

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -18,7 +18,26 @@ test("should count multiple occurrences of a character", () => {
 });
 
 // Scenario: No Occurrences
+test("should return 0 when the character does not occur in the string", () => {
+  // Given the input string `str`
+  const str = "hello";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+//   // And a character `char` that does not exist in the string
+//   const char = "z";
+
+//   // When the function is called with these inputs
+//   const count = countChar(str, char);
+
+//   // Then it should return 0
+//   expect(count).toEqual(0);
+// });
+
 // Given the input string `str`,
+
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
