0c03b73ce639ea7125b118035310da8b836dfb4c
Gideon Defar <gideondefar@gmail.com>
2026-08-02 19:39:55 +0100

    Handle negative repeat count

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -2,11 +2,12 @@
    // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
 //   return "hellohellohello";
-// }
-
-// module.exports = repeatStr;
 function repeatStr(string, times) {
   let result = "";
+
+  if (times < 0) {
+    throw new Error("Count cannot be negative");
+  }
 
   for (let i = 0; i < times; i++) {
     result += string;
