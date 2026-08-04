282f847f626c0645b60ed0d765da583012993ee7
Maryam Janjua <maryamjanjua65@gmail.com>
2026-08-04 20:43:57 +0100

    Attempt all TDD Tasks!

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,21 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  let getStr = num.toString();
+  let lastTwoDigit = getStr.slice(-2);
+  let lastDigit = getStr.slice(-1);
+  if(lastTwoDigit == "11" || lastTwoDigit == "12" || lastTwoDigit == "13"){
+    return getStr + "th"; 
+  }
+  else if(lastDigit =="1"){
+    return getStr + "st";
+  }
+  else if(lastDigit == "2"){
+    return getStr + "nd";
+  }
+  else if(lastDigit == "3"){
+    return getStr + "rd";
+  }
+  else{
+    return getStr + "th";
+  }
 }
-
 module.exports = getOrdinalNumber;
