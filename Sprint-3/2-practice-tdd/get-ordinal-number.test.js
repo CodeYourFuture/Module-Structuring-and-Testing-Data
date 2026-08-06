d4d113787bc8e0859ef722633913c4ea381066ad
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2026-08-06 17:45:16 +0100

    Use precise mathematical notation in getOrdinalNumber test descriptions

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -1,39 +1,27 @@
 const getOrdinalNumber = require("./get-ordinal-number");
 
-// Case 1: Numbers ending with 1 (but not 11)
-test("should append 'st' for numbers ending with 1, except those ending with 11", () => {
+test("should append 'st' for numbers where (num % 10 === 1) and (num % 100 !== 11)", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
 
-// Case 2: Numbers ending with 2 (but not 12)
-test("should append 'nd' for numbers ending with 2, except those ending with 12", () => {
+test("should append 'nd' for numbers where (num % 10 === 2) and (num % 100 !== 12)", () => {
   expect(getOrdinalNumber(2)).toEqual("2nd");
   expect(getOrdinalNumber(22)).toEqual("22nd");
-  expect(getOrdinalNumber(132)).toEqual("132nd");
+  expect(getOrdinalNumber(12)).toEqual("12th");
 });
 
-// Case 3: Numbers ending with 3 (but not 13)
-test("should append 'rd' for numbers ending with 3, except those ending with 13", () => {
+test("should append 'rd' for numbers where (num % 10 === 3) and (num % 100 !== 13)", () => {
   expect(getOrdinalNumber(3)).toEqual("3rd");
-  expect(getOrdinalNumber(33)).toEqual("33rd");
-  expect(getOrdinalNumber(133)).toEqual("133rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(13)).toEqual("13th");
 });
 
-// Case 4: Numbers ending with 11, 12, or 13 are special and take "th"
-test("should append 'th' for numbers ending with 11, 12, or 13", () => {
+test("should append 'th' for numbers where (num % 10 === 0, 4-9) or (num % 100 === 11, 12, 13)", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
   expect(getOrdinalNumber(11)).toEqual("11th");
   expect(getOrdinalNumber(12)).toEqual("12th");
   expect(getOrdinalNumber(13)).toEqual("13th");
-  expect(getOrdinalNumber(111)).toEqual("111th");
-  expect(getOrdinalNumber(112)).toEqual("112th");
-  expect(getOrdinalNumber(113)).toEqual("113th");
-});
-
-// Case 5: All other numbers
-test("should append 'th' for all other numbers", () => {
-  expect(getOrdinalNumber(4)).toEqual("4th");
-  expect(getOrdinalNumber(10)).toEqual("10th");
-  expect(getOrdinalNumber(100)).toEqual("100th");
+  expect(getOrdinalNumber(20)).toEqual("20th");
 });
