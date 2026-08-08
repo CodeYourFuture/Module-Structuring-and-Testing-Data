ba00f54e7133e9ced5f9ea76fd985820e8df5eee
Maryam Janjua <maryamjanjua65@gmail.com>
2026-08-08 12:38:53 +0100

    updated code

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,17 +1,15 @@
 function repeatStr(str, count) {
-  // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
-  // The goal is to re-implement that function, not to use it.
+  if (count < 0){
+    throw new Error("invalid count");
+  }
+
   let result="";
-  if (count >=1){
-    for(let i=1 ; i <= count ; i++){
+
+  for(let i=1 ; i <= count ; i++){
     result= str + result;
   }
+  
   return result;
-  }
-  else if (count == 0){
-    return "";
-  }
-  throw new Error("invalid count")
 }
 
 module.exports = repeatStr;
