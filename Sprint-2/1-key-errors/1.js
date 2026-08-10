8aa69c9ea99aa698d025adcacfaedbfde651a6b2
JorvanW <your.email@example.com>
2026-08-10 18:26:07 +0100

    removed line that affects code output

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
-// Why will an error occur when this program runs? 
-// =============> decimalNumber is a constant variable, and cannot be changed. 
+// Why will an error occur when this program runs?
+// =============> decimalNumber is a constant variable, and cannot be changed.
 // The code is attempting to change it which will cause an error.
 
 // Try playing computer with the example to work out what is going on
@@ -19,8 +19,7 @@ console.log(decimalNumber);
 
 // Finally, correct the code to fix the problem
 function convertToPercentage(decimalNumber) {
-  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
   return percentage;
 }
-console.log (convertToPercentage(0.5));
+console.log(convertToPercentage(0.5));
