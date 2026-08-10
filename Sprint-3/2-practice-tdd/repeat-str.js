70f506874d9bf9c212dce7488984a37bb2e9d91a
JorvanW <your.email@example.com>
2026-08-10 22:06:40 +0100

    removed unecessary line of code for repeat-str

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,15 +1,15 @@
 function repeatStr(str, times) {
-  if (times === 0) {
-    return "";
-  } else if (times < 0) {
+  if (times < 0) {
     throw new Error("error");
   }
 
   let repeatedString = "";
+
   for (let i = 0; i < times; i++) {
     repeatedString += str;
   }
+
   return repeatedString;
-} 
+}
 
 module.exports = repeatStr;
