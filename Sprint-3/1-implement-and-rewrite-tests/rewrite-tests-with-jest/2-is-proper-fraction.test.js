c874085c9a7ed41dc19ec61369eafd2196d80f50
Maryam Janjua <maryamjanjua65@gmail.com>
2026-08-12 13:08:12 +0100

    add more test cases for proper fraction

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,49 +7,41 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 // Denominator is zero
 test("should return false when denominator is zero", () => {
   expect(isProperFraction(1, 0)).toEqual(false);
+  expect(isProperFraction(-1, 0)).toEqual(false);
+  expect(isProperFraction(0, 0)).toEqual(false);
 });
 
-// Proper fractions
-test("should return true when numerator is less than denominator", () => {
+// Proper fractions with positive values
+test("should return true when the absolute value of the numerator is less than the absolute value of the denominator", () => {
   expect(isProperFraction(1, 2)).toEqual(true);
   expect(isProperFraction(2, 4)).toEqual(true);
   expect(isProperFraction(15, 30)).toEqual(true);
 });
 
 // Numerator is zero
-test("should return true when numerator is zero", () => {
+test("should return true when numerator is zero and denominator is non-zero", () => {
   expect(isProperFraction(0, 9)).toEqual(true);
+  expect(isProperFraction(0, -9)).toEqual(true);
 });
 
 // Equal numerator and denominator
-test("should return false when numerator is equal to denominator", () => {
+test("should return false when numerator and denominator have equal absolute values", () => {
   expect(isProperFraction(5, 5)).toEqual(false);
-});
-
-// Numerator is greater than denominator
-test("should return false when numerator is greater than denominator", () => {
-  expect(isProperFraction(6, 3)).toEqual(false);
-  expect(isProperFraction(10, 4)).toEqual(false);
-});
-
-// Negative numerator
-test("should return true when numerator is negative and forms a proper fraction", () => {
-  expect(isProperFraction(-1, 5)).toEqual(true);
-  expect(isProperFraction(-9, 10)).toEqual(true);
-});
-
-// Negative denominator
-test("should return true when denominator is negative and forms a proper fraction", () => {
-  expect(isProperFraction(1, -5)).toEqual(true);
-});
-
-// Both numerator and denominator are negative
-test("should handle both numerator and denominator being negative", () => {
-  expect(isProperFraction(-1, -5)).toEqual(true);
   expect(isProperFraction(-5, -5)).toEqual(false);
 });
 
-// Negative numerator with zero denominator
-test("should return false when denominator is zero with a negative numerator", () => {
-  expect(isProperFraction(-1, 0)).toEqual(false);
+// Negative values that form proper fractions
+test("should return true for negative values when the absolute numerator is less than the absolute denominator", () => {
+  expect(isProperFraction(-1, 2)).toEqual(true);
+  expect(isProperFraction(1, -2)).toEqual(true);
+  expect(isProperFraction(-1, -2)).toEqual(true);
+});
+
+// Positive and negative values that do not form proper fractions
+test("should return false when the absolute numerator is greater than or equal to the absolute denominator", () => {
+  expect(isProperFraction(-2, 1)).toEqual(false);
+  expect(isProperFraction(2, -1)).toEqual(false);
+  expect(isProperFraction(-2, -1)).toEqual(false);
+  expect(isProperFraction(6, 3)).toEqual(false);
+  expect(isProperFraction(10, 4)).toEqual(false);
 });
