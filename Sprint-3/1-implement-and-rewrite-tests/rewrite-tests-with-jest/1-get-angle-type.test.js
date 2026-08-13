4d941ffaca3407c721e0ceb83c5c189ffa84e232
JorvanW <your.email@example.com>
2026-08-13 16:42:05 +0100

    added more test cases

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -77,3 +77,10 @@ test("should return Invalid angle for negative angles", () => {
   expect(getAngleType(-90)).toEqual("Invalid angle");
   expect(getAngleType(-360)).toEqual("Invalid angle");
 });
+
+test(`should return "Invalid angle" for values just below 0 or just above 360`, () => {
+  expect(getAngleType(-0.1)).toEqual("Invalid angle");
+  expect(getAngleType(-0.001)).toEqual("Invalid angle");
+  expect(getAngleType(360.1)).toEqual("Invalid angle");
+  expect(getAngleType(360.001)).toEqual("Invalid angle");
+});
