01bb3dfc0dd4b8c647ea0474efdf6634ec0ce8df
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-13 12:14:06 +0200

    Fix countChar implementation and resolve test conflicts

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,11 +1,5 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  return stringOfCharacters.split("").filter(char => char === findCharacter).length;
 }
 
 module.exports = countChar;
- test( "should count multiple occurrences of a character ", function(){
-  const stringofcharacters = "aaaaa";
-  const findcharcaters = "a";
-  const count = countChar(stringofcharacters, findcharcaters);
-  expect(count).toEqual(5);
- });
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,24 +1,15 @@
 // implement a function countChar that counts the number of times a character occurs in a string
+// implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
 // Given a string `str` and a single character `char` to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
-function CountChar(str, char) {
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === char) {
-      count++;
-    }
-  }
-  return count;
-}
 
 // Scenario: Multiple Occurrences
 // Given the input string `str`,
 // And a character `char` that occurs one or more times in `str` (e.g., 'a' in 'aaaaa'),
 // When the function is called with these inputs,
 // Then it should correctly count occurrences of `char`.
-
 test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
   const char = "a";
@@ -31,9 +22,9 @@ test("should count multiple occurrences of a character", () => {
 // And a character `char` that does not exist within `str`.
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of `char` were found.
-test('should return 0 when the character does not occur in the string', () => {
-  const str = "hello";
-  const char = "x";
+test("should return 0 when the character does not occur", () => {
+  const str = "aaaaa";
+  const char = "b";
   const count = countChar(str, char);
-  expect(count).toEqual(5);
+  expect(count).toEqual(0);
 });
