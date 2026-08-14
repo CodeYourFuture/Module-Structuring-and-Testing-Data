5b95434440e0e371203a20bd977822027a1a3281
HM-127BTY <mills.hugh@yahoo.com>
2026-08-14 13:18:39 +0100

    Wokring on repeat-str.js and repeat-str.test.js

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,17 @@
-function repeatStr() {
+function repeatStr(str, count) {
+  let result = "";
+  if (count < 0) {
+    throw new Error("cannot be negative number")
+  }
+
+  for (let i=0; i < count; i++) {
+    result = result + str
+  }
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
-}
+  return result;
+  
+};
 
 module.exports = repeatStr;
+
--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,13 +20,33 @@ test("should repeat the string count times", () => {
 // Given a target string `str` and a `count` equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
+test("should return the string as the str for counting 1 time", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("hello");
+
+});
 
 // Case: Handle count of 0:
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
+test("should return empty string when count is 0", ()=> {
+  const str = "hello";
+  const count = 0;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("");
+
+});
 
 // Case: Handle negative count:
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
+test("should throw error when count is negative", ()=> {
+  const str="hello";
+  const count =-6;
+  expect(() => repeatStr(str, count)).toThrow("cannot be negative number");
+
+});
