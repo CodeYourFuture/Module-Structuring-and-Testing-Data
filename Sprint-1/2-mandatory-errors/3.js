5a3dacbacd84847ccf25e8c772556a60f866bbec
cyf <cyf@cyfs-MacBook-Pro.local>
2026-08-15 20:28:09 +0100

    Clean up Week 1 exercises

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,6 +1,6 @@
 const cardNumber = 4533787178994213;
 const last4Digits = cardNumber.toString().slice(-4);
-
+console.log(last4Digits);
 // I thought the issue might be something to do with the number given (-4) but it's actually the fact that cardNumber.slice is not a function.
 // Slice is not a function for numbers - it's for strings
 // To solve this we can turn the card number into a string by using toString() - this converts the number to a string temporarily rather than changing the card number to a string itself.
