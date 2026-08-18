1a326b8d9b3eee1d9072bc6fb3a1274696b0dfff
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-19 00:16:59 +0200

    Remove unrelated file changes not part of practice-tdd task

--- Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/2-is-proper-fraction.js
@@ -9,12 +9,25 @@
 // Acceptance criteria:
 // After you have implemented the function, write tests to cover all the cases, and
 // execute the code to ensure all tests pass.
-function isProperFraction(numerator, denominator) {
-  if (denominator === 0) {
-    return false;
-  }
 
-  return Math.abs(numerator) < Math.abs(denominator);
+function isProperFraction(numerator, denominator) {
+  // TODO: Implement this function
 }
 
+// The line below allows us to load the isProperFraction function into tests in other files.
+// This will be useful in the "rewrite tests with jest" step.
 module.exports = isProperFraction;
+
+// Here's our helper again
+function assertEquals(actualOutput, targetOutput) {
+  console.assert(
+    actualOutput === targetOutput,
+    `Expected ${actualOutput} to equal ${targetOutput}`
+  );
+}
+
+// TODO: Write tests to cover all cases.
+// What combinations of numerators and denominators should you test?
+
+// Example: 1/2 is a proper fraction
+assertEquals(isProperFraction(1, 2), true);
