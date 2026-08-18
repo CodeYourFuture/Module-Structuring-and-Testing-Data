33639ec111e0beede5b5805d0482c99a0a87c1a0
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-18 23:58:03 +0200

    Fix getOrdinalNumber suffix logic

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function getOrdinalNumber(num) {
   const lastTwoDigits = num % 100;
   const lastDigit = num % 10;
@@ -14,6 +15,24 @@ function getOrdinalNumber(num) {
     return `${num}rd`;
   } else {
     return `${num}th`;
+=======
+function getOrdinalNumber(number) {
+  const lastDigit = number % 10;
+  const lastTwoDigits = number % 100;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return number + "th";
+  }
+
+  if (lastDigit === 1) {
+    return number + "st";
+  } else if (lastDigit === 2) {
+    return number + "nd";
+  } else if (lastDigit === 3) {
+    return number + "rd";
+  } else {
+    return number + "th";
+>>>>>>> Stashed changes
   }
 }
 
