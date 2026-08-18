fcea904f5685f5ca83279af9bc4bc6e1c3818210
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-18 19:10:59 +0200

    made sure all the output of the function follow the same alphabet casing

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -1,41 +1,33 @@
 // This statement loads the getAngleType function you wrote in the implement directory.
-// We will use the same function, but write tests for it using Jest in this file.
 const getAngleType = require("../implement/1-get-angle-type");
 
-// TODO: Write tests in Jest syntax to cover all cases/outcomes,
-// including boundary and invalid cases.
-
-// Case 1: Acute angles
-test(`should return "Acute angle" when (0 < angle && angle  < 90)`, () => {
-  // Test various acute angles, including boundary cases
-  expect(getAngleType(1)).toEqual("Acute angle");
-  expect(getAngleType(45)).toEqual("Acute angle");
-  expect(getAngleType(89)).toEqual("Acute angle");
+test("returns acute angle for angles between 0 and 90", () => {
+  expect(getAngleType(1)).toEqual("acute angle");
+  expect(getAngleType(45)).toEqual("acute angle");
+  expect(getAngleType(89)).toEqual("acute angle");
 });
 
-// Case 2: Right angle
-test (`should return "right angle" when (angle === 90)`,() => {
-  // testing various angles ,including boundary cases
-  expect( getAngleType(90)).toEqual("right angle");
-  });
-// Case 3: Obtuse angles
-test (`should return "obtuse angle" when (90 < angle && angle < 180)`, () => {
-expect(getAngleType(91)).toEqual("obtuse angle");
-expect(getAngleType(125)).toEqual("obtuse angle");
-expect(getAngleType(172)).toEqual("obtuse angle");
+test("returns right angle for 90 degrees", () => {
+  expect(getAngleType(90)).toEqual("right angle");
 });
-// Case 4: Straight angle
-test(`should return "straight angle" when (angle === 180)`, () => {
+
+test("returns obtuse angle for angles between 90 and 180", () => {
+  expect(getAngleType(91)).toEqual("obtuse angle");
+  expect(getAngleType(125)).toEqual("obtuse angle");
+  expect(getAngleType(172)).toEqual("obtuse angle");
+});
+
+test("returns straight angle for 180 degrees", () => {
   expect(getAngleType(180)).toEqual("straight angle");
 });
-// Case 5: Reflex angles\
-test(`should return "reflex angle" when (180 < angle && angle < 360)`, () => {
+
+test("returns reflex angle for angles between 180 and 360", () => {
   expect(getAngleType(181)).toEqual("reflex angle");
   expect(getAngleType(270)).toEqual("reflex angle");
   expect(getAngleType(359)).toEqual("reflex angle");
 });
-// Case 6: Invalid angles
-test(`should return "Invalid angle" when (angle < 0 || angle > 360)`, () => {
-  expect(getAngleType(-1)).toEqual("Invalid angle");
-  expect(getAngleType(361)).toEqual("Invalid angle");
+
+test("returns invalid angle for angles outside the valid range", () => {
+  expect(getAngleType(-1)).toEqual("invalid angle");
+  expect(getAngleType(361)).toEqual("invalid angle");
 });
