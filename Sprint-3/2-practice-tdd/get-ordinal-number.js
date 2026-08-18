7cd35563d6a7eb3c998a79a8fed104aa5dd6fc75
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-19 00:06:56 +0200

    Implement getAngleType and fix getOrdinalNumber

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 function getOrdinalNumber(num) {
   const lastTwoDigits = num % 100;
   const lastDigit = num % 10;
@@ -15,24 +14,6 @@ function getOrdinalNumber(num) {
     return `${num}rd`;
   } else {
     return `${num}th`;
-=======
-function getOrdinalNumber(number) {
-  const lastDigit = number % 10;
-  const lastTwoDigits = number % 100;
-
-  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
-    return number + "th";
-  }
-
-  if (lastDigit === 1) {
-    return number + "st";
-  } else if (lastDigit === 2) {
-    return number + "nd";
-  } else if (lastDigit === 3) {
-    return number + "rd";
-  } else {
-    return number + "th";
->>>>>>> Stashed changes
   }
 }
 
