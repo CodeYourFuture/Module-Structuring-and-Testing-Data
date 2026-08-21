da4d7e70270800f3a6f42d0070e760d605e69ecc
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-08-21 18:11:55 +0100

    updated the explanation to meet with function return

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -27,15 +27,15 @@ console.log(formatTimeDisplay(61));
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> the first time pad was called with the totalHours the value is 00:01:01
+// =============> the first time pad was called with 61 the value is 00:01:01
 
 // c) What is the return value of pad is called for the first time?
 // =============> "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> 1
-// the last time, pad() is called with the remaining seconds which is value "1"
+// the last time, pad() is called with the remaining seconds which is value 1
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> "01"
-// the return value is 1 put as we are using pad would be adding leading "0"
+// =============> 1
+// the return value is "1" but as we are using pad would be adding a leading of "0" to become "01"
