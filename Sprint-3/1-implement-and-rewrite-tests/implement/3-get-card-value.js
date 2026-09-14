94099924c6100136bd22269f541e879ad959a98f
mandipsanger <mandipsanger1@gmail.com>
2026-09-14 18:56:35 +0100

    Implement getCardValue function logic

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -21,6 +21,7 @@
 // After you have implemented the function, write tests to cover all the cases, and
 // execute the code to ensure all tests pass.
 
+
 function getCardValue(card) {
   // TODO: Implement this function
   const validSuits = ["♠", "♥", "♦", "♣"];
@@ -39,18 +40,16 @@ function getCardValue(card) {
   }
 
   if (rank === "J" || rank === "Q" || rank === "K") {
-    return 10;
-  }
+  return 10;
+}
+
 if (Number(rank) >= 2 && Number(rank) <= 10) {
-  // ...
-}
-   {
-    return Number(rank);
-  }
-
-  throw new Error("Invalid card");
+  return Number(rank);
 }
 
+throw new Error("Invalid card");
+}
+  
 // The line below allows us to load the getCardValue function into tests in other files.
 // This will be useful in the "rewrite tests with jest" step.
 module.exports = getCardValue;
