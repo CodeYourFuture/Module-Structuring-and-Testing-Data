29dc85f92e7f7e275f16ad4b83f4036f717cbc31
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 17:08:18 +0100

    Add tests for proper fraction validation with Jest

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,3 +8,33 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
+// proper fractions
+test(`should return true when denominator is higher than numerator`, () => {
+  expect(isProperFraction(0, 1)).toEqual(true);
+  expect(isProperFraction(2, 7)).toEqual(true);
+  expect(isProperFraction(89, 101)).toEqual(true);
+});
+// Special case: numerator or denominator is negative, we consider the absolute values for fractions so ignore the negative signs
+test(`should return true when negative/positive numerator is less than the positive/negative denominator`, () => {
+  expect(isProperFraction(-20, -30)).toEqual(true);
+  expect(isProperFraction(-1, 2)).toEqual(true);
+  expect(isProperFraction(58, -68)).toEqual(true);
+});
+// Special case: numerator or denominator is negative, we consider the absolute values for fractions so ignore the negative signs
+test(`should return false when negative/positive numerator is greater than the positive/negative denominator`, () => {
+  expect(isProperFraction(-50, 10)).toEqual(false);
+  expect(isProperFraction(100, 2)).toEqual(false);
+  expect(isProperFraction(-1, -0)).toEqual(false);
+});
+// Special case: Since the abs(denominator) is 1, only a numerator of 0 should be proper.
+test("should handle denominator of 1", () => {
+  expect(isProperFraction(1, 1)).toEqual(false);
+  expect(isProperFraction(2, 1)).toEqual(false);
+});
+// Special case: proper fraction requires the numerator to be strictly less than the denominator (in absolute value).
+test("should return false when numerator equals denominator", () => {
+  expect(isProperFraction(5, 5)).toEqual(false);
+  expect(isProperFraction(-5, 5)).toEqual(false);
+  expect(isProperFraction(5, -5)).toEqual(false);
+  expect(isProperFraction(-5, -5)).toEqual(false);
+});
